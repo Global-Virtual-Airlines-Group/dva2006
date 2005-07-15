661f17341c2691cf539a6dfea5791c0586d4b65e
Luke Kolin <luke@deltava.org>
2005-07-15 20:12:12 +0000

    Added formatting constants
    git-svn-id: file:///home/svn/DVA2006@50 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/GeoLocation.java
+++ src/java/org/deltava/beans/GeoLocation.java
@@ -14,6 +14,24 @@ public interface GeoLocation {
     * Size of a degree in miles.
     */
    public static final double DEGREE_MILES = 69.16;
+   
+   /**
+    * Format the Latitude only.
+    * @see org.deltava.util.StringUtils#format(GeoLocation, boolean, int)
+    */
+   public static final int LATITUDE = 1;
+   
+   /**
+    * Format the Longitude only.
+    * @see org.deltava.util.StringUtils#format(GeoLocation, boolean, int)
+    */
+   public static final int LONGITUDE = 2;
+   
+   /**
+    * Format the Longitude and Latitude.
+    * @see org.deltava.util.StringUtils#format(GeoLocation, boolean, int)
+    */
+   public static final int ALL = 3;
 
 	public double getLatitude();
 	public double getLongitude();
