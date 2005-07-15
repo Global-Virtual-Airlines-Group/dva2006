e537866c83afd41c0c88ce8b27a592b2bb1c2eac
Luke Kolin <luke@deltava.org>
2005-07-15 20:13:48 +0000

    Added Google Mapping marker interface and tag library
    git-svn-id: file:///home/svn/DVA2006@53 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/MapEntry.java
@@ -0,0 +1,59 @@
+// Copyright 2005 Luke J. Kolin. All Rights Reserved.
+package org.deltava.beans;
+
+/**
+ * An interface to define icon formatting for entries that can be displayed in maps.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public interface MapEntry {
+   
+   /**
+    * Blue Google Maps icon, displayed as $static/img/maps/point_blue.png
+    */
+   public static final String BLUE = "blue";
+   
+   /**
+    * Green Google Maps icon, displayed as $static/img/maps/point_green.png
+    */
+   public static final String GREEN = "green";
+   
+   /**
+    * Orange Google Maps icon, displayed as $static/img/maps/point_orange.png
+    */
+   public static final String ORANGE = "orange";
+   
+   /**
+    * Purple Google Maps icon, displayed as $static/img/maps/point_purple.png
+    */
+   public static final String PURPLE = "purple";
+   
+   /**
+    * Red Google Maps icon, displayed as $static/img/maps/point_red.png
+    */
+   public static final String RED = "red";
+   
+   /**
+    * White Google Maps icon, displayed as $static/img/maps/point_white.png
+    */
+   public static final String WHITE = "white";
+   
+   /**
+    * Yellow Google Maps icon, displayed as $static/img/maps/point_yellow.png
+    */
+   public static final String YELLOW = "yellow";
+
+   /**
+    * Returns the icon color for this entry if displayed in a Google Map.
+    * @return the icon color
+    */
+   public String getIconColor();
+   
+   /**
+    * Returns the text to display in this marker's infobox if displayed in a Google Map.
+    * @return the infobox HTML text, or null if no infobox to be displayed
+    */
+   public String getInfoBox();
+}
---
+++ src/java/org/deltava/taglib/googlemap/InsertGoogleAPITag.java
@@ -0,0 +1,62 @@
+// Copyright 2005 Luke J. Kolin. All Rights Reserved.
+package org.deltava.taglib.googlemap;
+
+import javax.servlet.jsp.*;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.deltava.taglib.ContentHelper;
+
+import org.deltava.util.system.SystemData;
+
+/**
+ * A JSP Tag to insert a JavaScript link to the Google Maps API.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class InsertGoogleAPITag extends TagSupport {
+   
+
+   private int _apiVersion = 1;
+   
+   /**
+    * Sets the Google API version to pull down.
+    * @param ver the API version
+    */
+   public void setVersion(int ver) {
+      _apiVersion = ver;
+   }
+   
+   /**
+    * Renders the JSP tag.
+    * @return TagSupport.EVAL_PAGE
+    * @throws JspException if no Google Maps API key defined
+    */
+   public int doEndTag() throws JspException {
+      
+      // Get the API key
+      String apiKey = SystemData.get("security.key.googleMaps");
+      if (apiKey == null)
+         throw new JspException("Google Maps API key not defined");
+      
+      // Check if we've already included the content
+      if (ContentHelper.containsContent(pageContext, "JS", GoogleMapEntryTag.API_JS_NAME))
+         return EVAL_PAGE;
+      
+      JspWriter out = pageContext.getOut();
+      try {
+			out.print("<script language=\"JavaScript\" type=\"text/javascript\" src=\"http://maps.google.com/maps?file=api&amp;v=");
+			out.print(String.valueOf(_apiVersion));
+			out.print("&amp;key=");
+			out.print(apiKey);
+			out.print("\"></script>");
+      } catch (Exception e) {
+         throw new JspException(e);
+      }
+      
+      // Mark the content as added and return
+		ContentHelper.addContent(pageContext, "JS", GoogleMapEntryTag.API_JS_NAME);
+		return EVAL_PAGE;
+   }
+}
