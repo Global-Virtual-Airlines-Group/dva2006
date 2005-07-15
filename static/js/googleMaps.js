59cdcf651e60de5475887a3fa456aa0a2455db29
Luke Kolin <luke@deltava.org>
2005-07-15 00:13:53 +0000

    git-svn-id: file:///home/svn/DVA2006@31 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ static/js/googleMaps.js
@@ -0,0 +1,39 @@
+function googleMarker(imgPath, color, point, label)
+{
+var icon = new GIcon();
+icon.image = '/' + imgPath + '/maps/point_' + color + '.png';
+icon.shadow = '/' + imgPath + '/maps/shadow.png';
+icon.iconSize = new GSize(12, 20);
+icon.shadowSize = new GSize(22, 20);
+icon.iconAnchor = new GPoint(6, 20);
+icon.infoWindowAnchor = new GPoint(5, 1);
+var marker = new GMarker(point, icon);
+GEvent.addListener(marker, 'click', function() { marker.openInfoWindowHtml(label); });
+return marker;
+}
+
+function getDefaultZoom(distance)
+{
+if (distance > 6100) {
+	return 15;
+} else if (distance > 2900) {
+	return 14;
+} else if (distance > 1600) {
+	return 13;
+} else if (distance > 780) {
+	return 12;
+} else if (distance > 390) {
+	return 11;
+} else if (distance > 195) {
+	return 10;
+} else if (distance > 90) {
+	return 9;
+}
+
+return 8;
+}
+
+function airportLabel(name, icao, iata)
+{
+return '<b>' + name + '</b><br /><br />ICAO: ' + icao + '<br />IATA: ' + iata + '<br />';
+}
