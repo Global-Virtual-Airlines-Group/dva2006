a73ba5b8959c000c1478bcfdbeeb3255ae969f35
Luke Kolin <luke@deltava.org>
2005-07-18 21:46:44 +0000

    Added ACARSDataService
    git-svn-id: file:///home/svn/DVA2006@106 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/service/ACARSDataService.java
@@ -0,0 +1,112 @@
+// Copyright 2005 Luke J. Kolin. All Rights Reserved.
+package org.deltava.service;
+
+import java.util.*;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.deltava.beans.acars.*;
+
+import org.deltava.dao.GetACARSData;
+import org.deltava.dao.DAOException;
+
+import org.deltava.util.StringUtils;
+
+/**
+ * A Web Service to return ACARS flight data parameters.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class ACARSDataService extends WebDataService {
+
+   /* (non-Javadoc)
+    * @see org.deltava.service.WebService#execute(org.deltava.service.ServiceContext)
+    */
+   public int execute(ServiceContext ctx) throws ServiceException {
+
+      // Get the ACARS Flight ID
+      int id = 0;
+      try {
+         id = Integer.parseInt(ctx.getRequest().getParameter("id"));
+      } catch (NumberFormatException nfe) {
+         throw new ServiceException(HttpServletResponse.SC_BAD_REQUEST, "Invalid ID");
+      }
+      
+      // Get the ACARS data
+      List routeData = null;
+      try {
+         GetACARSData addao = new GetACARSData(_con);
+         routeData = addao.getRouteEntries(id);
+      } catch (DAOException de) {
+         throw new ServiceException(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, de.getMessage());
+      }
+      
+      // Write the CSV header
+      ctx.print("Date/Time,Latitude,Longitude,Altitude,Heading,Air Speed,Ground Speed,Vertical Speed,N1,N2,Flaps,");
+      ctx.println("NAV,HDG,APR,ALT,AT");
+      
+      // Format the ACARS data - filter out GeoPosition objects
+      for (Iterator i = routeData.iterator(); i.hasNext(); ) {
+         Object e = i.next();
+         if (e instanceof RouteEntry) {
+            RouteEntry entry = (RouteEntry) e;
+            ctx.print(StringUtils.format(entry.getDate(), "MM/dd/yyyy HH:mm:ss"));
+            ctx.print(",");
+            ctx.print(StringUtils.format(entry.getLatitude(), "##0.0000"));
+            ctx.print(",");
+            ctx.print(StringUtils.format(entry.getLongitude(), "##0.0000"));
+            ctx.print(",");
+            ctx.print(StringUtils.format(entry.getLatitude(), "#,##0"));
+            ctx.print(",");
+            ctx.print(StringUtils.format(entry.getHeading(), "000"));
+            ctx.print(",");
+            ctx.print(StringUtils.format(entry.getAirSpeed(), "##0"));
+            ctx.print(",");
+            ctx.print(StringUtils.format(entry.getGroundSpeed(), "#,##0"));
+            ctx.print(",");
+            ctx.print(StringUtils.format(entry.getVerticalSpeed(), "##0"));
+            ctx.print(",");
+            ctx.print(StringUtils.format(entry.getN1(), "##0.0"));
+            ctx.print(",");
+            ctx.print(StringUtils.format(entry.getN2(), "##0.0"));
+            ctx.print(",");
+            ctx.print((entry.getFlaps() == 0) ? "" : String.valueOf(entry.getFlaps()));
+            ctx.print(",");
+            ctx.print(entry.isFlagSet(ACARSFlags.FLAG_AP_NAV) ? "NAV," : ",");
+            ctx.print(entry.isFlagSet(ACARSFlags.FLAG_AP_HDG) ? "HDG," : ",");
+            ctx.print(entry.isFlagSet(ACARSFlags.FLAG_AP_APR) ? "APR," : ",");
+            ctx.print("-,");
+            //ctx.print(entry.isFlagSet(ACARSFlags.FLAG_AP_ALT) ? "ALT," : ",");
+            if (entry.isFlagSet(ACARSFlags.FLAG_AT_IAS)) {
+               ctx.println("IAS");
+            } else if (entry.isFlagSet(ACARSFlags.FLAG_AT_MACH)) {
+               ctx.println("MACH");
+            } else {
+               ctx.println("");
+            }
+         }
+      }
+      
+      // Write the response
+      try {
+         ctx.getResponse().setContentType("text/csv");
+         ctx.commit();
+      } catch (IOException ie) {
+         throw new ServiceException(HttpServletResponse.SC_CONFLICT, "I/O Error");
+      }
+      
+      // Write success code
+      return HttpServletResponse.SC_OK;
+   }
+
+   /**
+    * Returns if the Web Service requires authentication.
+    * @return TRUE
+    */
+   public final boolean isSecure() {
+      return true;
+   }
+}
