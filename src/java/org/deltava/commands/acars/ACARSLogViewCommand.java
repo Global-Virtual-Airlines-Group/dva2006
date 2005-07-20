2a0d3a3623bcc22a5f6009cb04aeb068eca3cf8b
Luke Kolin <luke@deltava.org>
2005-07-20 20:56:48 +0000

    Added ACARS logging commands
    git-svn-id: file:///home/svn/DVA2006@159 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/commands/acars/ACARSLogViewCommand.java
@@ -0,0 +1,56 @@
+// Copyright 2005 Luke J. Kolin. All Rights Reserved.
+package org.deltava.commands.acars;
+
+import java.util.*;
+import java.text.*;
+
+import org.deltava.commands.*;
+import org.deltava.util.ComboUtils;
+import org.deltava.util.StringUtils;
+
+/**
+ * A helper class for viewing ACARS logs.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public abstract class ACARSLogViewCommand extends AbstractViewCommand {
+   
+   private static final DateFormat _df = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+   private static final String[] SEARCH_TYPES = new String[] { "USR", "id", "DATE" };
+   protected static final int SEARCH_USR = 0;
+   protected static final int SEARCH_ID = 1;
+   protected static final int SEARCH_DATE = 2;
+   private static final List _searchTypes = ComboUtils.fromArray(new String[] { "Pilot ID", "User ID", "Date Range" },
+         SEARCH_TYPES);
+   
+   /**
+    * Calculates the search type from the request, and updates request attributes.
+    * @param ctx the Command context
+    * @return a search type constant
+    */
+   protected int getSearchType(CommandContext ctx) {
+      int searchType = StringUtils.arrayIndexOf(SEARCH_TYPES, ctx.getParameter("searchType"));
+      ctx.setAttribute("searchType", SEARCH_TYPES[searchType], REQUEST);
+      ctx.setAttribute("searchTypes", _searchTypes, REQUEST);
+      return searchType;
+   }
+   
+   /**
+    * Loads a date parameter from the request.
+    * @param ctx the Command context
+    * @param paramName the request parameter name
+    * @return the parsed date/time or null if it cannot be parsed
+    */
+   protected Date getDate(CommandContext ctx, String paramName) {
+      
+      try {
+         synchronized (_df) {
+            return _df.parse(ctx.getParameter(paramName));
+         }
+      } catch (ParseException pe) {
+         return null;
+      }
+   }
+}
---
+++ src/java/org/deltava/commands/acars/ConnectionPoolCommand.java
@@ -0,0 +1,35 @@
+// Copyright 2005 Luke J. Kolin. All Rights Reserved.
+package org.deltava.commands.acars;
+
+import org.deltava.beans.acars.ACARSAdminInfo;
+
+import org.deltava.commands.*;
+
+import org.deltava.util.system.SystemData;
+
+/**
+ * A Web Site Command to display ACARS connection pool information.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class ConnectionPoolCommand extends AbstractCommand {
+
+   /**
+    * Executes the command.
+    * @param ctx the Command context
+    * @throws CommandException if an error (typically database) occurs
+    */
+   public void execute(CommandContext ctx) throws CommandException {
+
+      // Get the ACARS Connection pool data and save in the request
+      ACARSAdminInfo acarsPool = (ACARSAdminInfo) SystemData.getObject(SystemData.ACARS_POOL);
+      ctx.setAttribute("acarsPool", acarsPool, REQUEST);
+
+      // Forward to the JSP
+      CommandResult result = ctx.getResult();
+      result.setURL("/jsp/acars/poolInfo.jsp");
+      result.setSuccess(true);
+   }
+}
