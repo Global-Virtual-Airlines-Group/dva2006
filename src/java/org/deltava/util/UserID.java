051ca6f5aeb37a684813e156716b1f3d0b794682
Luke Kolin <luke@deltava.org>
2005-07-20 04:56:24 +0000

    Added UserID to DVA2006
    git-svn-id: file:///home/svn/DVA2006@149 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/util/UserID.java
@@ -0,0 +1,68 @@
+// Copyright (c) 2005 Luke J. Kolin. All Rights Reserved.
+package org.deltava.util;
+
+/**
+ * A bean to parse user IDs.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class UserID {
+	
+	private String _airline;
+	private int _id;
+
+	/**
+	 * Parses a pilot code to determine the user ID.
+	 * @param code the pilot code
+	 */
+	public UserID(CharSequence code) {
+		super();
+		
+        if (code == null)
+            return;
+
+        StringBuffer pBuf = new StringBuffer();
+        StringBuffer cBuf = new StringBuffer();
+        for (int x = 0; x < code.length(); x++) {
+            char c = Character.toUpperCase(code.charAt(x));
+            if ("0123456789".indexOf(c) != -1) {
+                cBuf.append(c);
+            } else if ("ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(c) != -1) {
+                pBuf.append(c);
+            }
+        }
+
+        // Save the prefix and the code
+        _airline = pBuf.toString();
+        try {
+            _id = Integer.parseInt(cBuf.toString());
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException("Invalid Pilot Code - " + code);
+        }
+	}
+
+	/**
+	 * Returns the airline code for this user. 
+	 * @return the airline code
+	 */
+	public String getAirlineCode() {
+		return _airline;
+	}
+
+	/**
+	 * Returns the seniority number for this user.
+	 * @return the pilot code
+	 */
+	public int getUserID() {
+		return _id;
+	}
+	
+	/**
+	 * Returns the Pilot code.
+	 */
+	public String toString() {
+		return _airline + StringUtils.format(_id, "#000");
+	}
+}
