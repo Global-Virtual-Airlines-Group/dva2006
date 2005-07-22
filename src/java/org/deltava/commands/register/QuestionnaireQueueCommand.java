ef8a6db16cdbac2891cbca073616a38f6c8f54c9
Luke Kolin <luke@deltava.org>
2005-07-22 05:06:22 +0000

    Updated question offset, added questionnaire queue
    git-svn-id: file:///home/svn/DVA2006@206 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/commands/register/QuestionnaireQueueCommand.java
@@ -0,0 +1,58 @@
+// Copyright (c) 2005 Luke J. Kolin. All Rights Reserved.
+package org.deltava.commands.register;
+
+import java.util.*;
+import java.sql.Connection;
+
+import org.deltava.beans.testing.Examination;
+
+import org.deltava.commands.*;
+import org.deltava.dao.*;
+
+/**
+ * A Web Site Command to view pending Applicant Questionnaires.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class QuestionnaireQueueCommand extends AbstractCommand {
+
+	/**
+	 * Executes the command.
+	 * @param ctx the Command context
+	 * @throws CommandException if an error occurs
+	 */
+	public void execute(CommandContext ctx) throws CommandException {
+		try {
+			Connection con = ctx.getConnection();
+			
+			// Get the DAO and the questionnaire queue
+			GetQuestionnaire dao = new GetQuestionnaire(con);
+			List queue = dao.getPending();
+			
+			// Build a collection of applicant IDs
+			Set applicantIDs = new HashSet();
+			for (Iterator i = queue.iterator(); i.hasNext(); ) {
+				Examination exam = (Examination) i.next();
+				applicantIDs.add(new Integer(exam.getPilotID()));
+			}
+			
+			// Get the DAO and save the applicant IDs in the request
+			GetApplicant adao = new GetApplicant(con);
+			ctx.setAttribute("applicants", adao.getByID(applicantIDs, "APPLICANTS"), REQUEST);
+			
+			// Save the questionnaire queue
+			ctx.setAttribute("examQueue", queue, REQUEST);
+		} catch (DAOException de) {
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
+		
+		// Forward to the JSP
+		CommandResult result = ctx.getResult();
+		result.setURL("/jsp/register/questionnaireQueue.jsp");
+		result.setSuccess(true);
+	}
+}
