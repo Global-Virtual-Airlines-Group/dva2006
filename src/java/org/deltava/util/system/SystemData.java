d25dd1fe7f82f78f450c7ec809c82b2f5257f312
Luke Kolin <luke@deltava.org>
2005-07-24 21:07:57 +0000

    Initialize data hashmap to prevent NPEs on init
    git-svn-id: file:///home/svn/DVA2006@227 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/util/system/SystemData.java
+++ src/java/org/deltava/util/system/SystemData.java
@@ -18,179 +18,180 @@ import org.deltava.beans.schedule.Airport;
 
 public class SystemData implements Serializable {
 
-   private static final Logger log = Logger.getLogger(SystemData.class);
-   
-   public static final String AUTHENTICATOR = "security.auth.obj";
-   public static final String JDBC_POOL = "jdbc.pool";
-   public static final String TASK_POOL = "tasks.pool";
-   public static final String ACARS_POOL = "acars.pool";
+	private static final Logger log = Logger.getLogger(SystemData.class);
 
-   static final String CFG_NAME = "$CONFIGNAME$";
-   static final String LOADER_NAME = "$LOADERCLASS$";
+	public static final String AUTHENTICATOR = "security.auth.obj";
+	public static final String JDBC_POOL = "jdbc.pool";
+	public static final String TASK_POOL = "tasks.pool";
+	public static final String ACARS_POOL = "acars.pool";
 
-   private static SystemDataLoader _loader;
-   private static Map _properties;
+	static final String CFG_NAME = "$CONFIGNAME$";
+	static final String LOADER_NAME = "$LOADERCLASS$";
 
-   // This is a singleton
-   private SystemData() {
-   }
+	private static SystemDataLoader _loader;
+	private static Map _properties = new HashMap();;
 
-   /**
-    * Initializes SystemData object.
-    * @param loaderClassName the data loader class name
-    */
-   public final static synchronized void init(String loaderClassName) {
+	// This is a singleton
+	private SystemData() {
+		super();
+	}
 
-      // Get the data loader class
-      try {
-         Class ldClass = Class.forName(loaderClassName);
-         _loader = (SystemDataLoader) ldClass.newInstance();
-         log.debug("Instantiated " + loaderClassName);
-      } catch (Exception e) {
-         _loader = new XMLSystemDataLoader();
-         log.debug("Using default loader class " + _loader.getClass().getName());
-      }
+	/**
+	 * Initializes SystemData object.
+	 * @param loaderClassName the data loader class name
+	 */
+	public final static synchronized void init(String loaderClassName) {
 
-      try {
-         _properties = _loader.load();
-      } catch (IOException ie) {
-         Throwable ce = ie.getCause();
-         if (ce == null) {
-            log.warn("Error loading System Data - " + ie.getMessage());
-         } else {
-            log.warn("Error loading System Data - " + ce.getMessage());
-         }
+		// Get the data loader class
+		try {
+			Class ldClass = Class.forName(loaderClassName);
+			_loader = (SystemDataLoader) ldClass.newInstance();
+			log.debug("Instantiated " + loaderClassName);
+		} catch (Exception e) {
+			_loader = new XMLSystemDataLoader();
+			log.debug("Using default loader class " + _loader.getClass().getName());
+		}
 
-         _properties = new HashMap();
-      }
+		try {
+			_properties = _loader.load();
+		} catch (IOException ie) {
+			Throwable ce = ie.getCause();
+			if (ce == null) {
+				log.warn("Error loading System Data - " + ie.getMessage());
+			} else {
+				log.warn("Error loading System Data - " + ce.getMessage());
+			}
 
-      // Save the loader name and return
-      _properties.put(SystemData.LOADER_NAME, _loader.getClass().getName());
-   }
+			_properties = new HashMap();
+		}
 
-   /**
-    * Initializes the System data with the default loader.
-    */
-   public final static synchronized void init() {
-      init("DEFAULT");
-   }
+		// Save the loader name and return
+		_properties.put(SystemData.LOADER_NAME, _loader.getClass().getName());
+	}
 
-   /**
-    * Returns a property value.
-    * @param propertyName the property name
-    * @return the property
-    */
-   public static Object getObject(String propertyName) {
-      return _properties.get(propertyName);
-   }
+	/**
+	 * Initializes the System data with the default loader.
+	 */
+	public final static synchronized void init() {
+		init("DEFAULT");
+	}
 
-   /**
-    * Returns a boolean property value.
-    * @param propertyName the property name
-    * @return TRUE if the object is a boolean value and TRUE, otherwise FALSE
-    */
-   public static boolean getBoolean(String propertyName) {
-      Object obj = getObject(propertyName);
-      return (obj instanceof Boolean) ? ((Boolean) obj).booleanValue() : false;
-   }
+	/**
+	 * Returns a property value.
+	 * @param propertyName the property name
+	 * @return the property
+	 */
+	public static Object getObject(String propertyName) {
+		return _properties.get(propertyName);
+	}
 
-   /**
-    * Returns an integer property value.
-    * @param propertyName the property name
-    * @return the property value, or zero if not an integer
-    */
-   public static int getInt(String propertyName) {
-      Object obj = getObject(propertyName);
-      return (obj instanceof Number) ? ((Number) obj).intValue() : 0;
-   }
+	/**
+	 * Returns a boolean property value.
+	 * @param propertyName the property name
+	 * @return TRUE if the object is a boolean value and TRUE, otherwise FALSE
+	 */
+	public static boolean getBoolean(String propertyName) {
+		Object obj = getObject(propertyName);
+		return (obj instanceof Boolean) ? ((Boolean) obj).booleanValue() : false;
+	}
 
-   /**
-    * Returns a long integer property value. 
-    * @param propertyName the property name
-    * @return the property value, or zero if not a long
-    */
-   public static long getLong(String propertyName) {
-      Object obj = getObject(propertyName);
-      return (obj instanceof Long) ? ((Long) obj).longValue() : 0;
-   }
+	/**
+	 * Returns an integer property value.
+	 * @param propertyName the property name
+	 * @return the property value, or zero if not an integer
+	 */
+	public static int getInt(String propertyName) {
+		Object obj = getObject(propertyName);
+		return (obj instanceof Number) ? ((Number) obj).intValue() : 0;
+	}
 
-   /**
-    * Returns a floating point property value.
-    * @param propertyName the property name
-    * @return the property value, or zero if not a double
-    */
-   public static double getDouble(String propertyName) {
-      Object obj = getObject(propertyName);
-      return (obj instanceof Double) ? ((Double) obj).doubleValue() : 0;
-   }
+	/**
+	 * Returns a long integer property value.
+	 * @param propertyName the property name
+	 * @return the property value, or zero if not a long
+	 */
+	public static long getLong(String propertyName) {
+		Object obj = getObject(propertyName);
+		return (obj instanceof Long) ? ((Long) obj).longValue() : 0;
+	}
 
-   /**
-    * Returns a property value.
-    * @param propertyName the property name
-    * @return the property value, or null if not found
-    */
-   public static String get(String propertyName) {
-      Object obj = getObject(propertyName);
-      return (obj == null) ? null : obj.toString();
-   }
+	/**
+	 * Returns a floating point property value.
+	 * @param propertyName the property name
+	 * @return the property value, or zero if not a double
+	 */
+	public static double getDouble(String propertyName) {
+		Object obj = getObject(propertyName);
+		return (obj instanceof Double) ? ((Double) obj).doubleValue() : 0;
+	}
 
-   /**
-    * Adds a property.
-    * @param pName the property name
-    * @param value the property value
-    * @see SystemData#addAll(Map)
-    */
-   public static synchronized void add(String pName, Object value) {
-      log.debug("Adding value " + pName);
-      _properties.put(pName, value);
-   }
+	/**
+	 * Returns a property value.
+	 * @param propertyName the property name
+	 * @return the property value, or null if not found
+	 */
+	public static String get(String propertyName) {
+		Object obj = getObject(propertyName);
+		return (obj == null) ? null : obj.toString();
+	}
 
-   /**
-    * Adds a number of properties.
-    * @param data a Map of property names/values
-    * @see SystemData#add(String, Object)
-    */
-   public static synchronized void addAll(Map data) {
-      for (Iterator i = data.keySet().iterator(); i.hasNext();) {
-         String pName = (String) i.next();
-         log.debug("Adding value " + pName);
-         _properties.put(pName, data.get(pName));
-      }
-   }
+	/**
+	 * Adds a property.
+	 * @param pName the property name
+	 * @param value the property value
+	 * @see SystemData#addAll(Map)
+	 */
+	public static synchronized void add(String pName, Object value) {
+		log.debug("Adding value " + pName);
+		_properties.put(pName, value);
+	}
 
-   /**
-    * Returns an Airport object.
-    * @param airportCode the Airport code (IATA or ICAO)
-    * @return the Airport bean, or null if not found
-    * @throws NullPointerException if airportCode is null
-    * @throws IllegalStateException if the &quot;airports&quot; property has not been added
-    */
-   public static Airport getAirport(String airportCode) {
-      if (airportCode == null)
-         return null;
+	/**
+	 * Adds a number of properties.
+	 * @param data a Map of property names/values
+	 * @see SystemData#add(String, Object)
+	 */
+	public static synchronized void addAll(Map data) {
+		for (Iterator i = data.keySet().iterator(); i.hasNext();) {
+			String pName = (String) i.next();
+			log.debug("Adding value " + pName);
+			_properties.put(pName, data.get(pName));
+		}
+	}
 
-      if (!_properties.containsKey("airports"))
-         throw new IllegalStateException("Airports not Loaded");
+	/**
+	 * Returns an Airport object.
+	 * @param airportCode the Airport code (IATA or ICAO)
+	 * @return the Airport bean, or null if not found
+	 * @throws NullPointerException if airportCode is null
+	 * @throws IllegalStateException if the &quot;airports&quot; property has not been added
+	 */
+	public static Airport getAirport(String airportCode) {
+		if (airportCode == null)
+			return null;
 
-      Map airports = (Map) _properties.get("airports");
-      return (Airport) airports.get(airportCode.toUpperCase());
-   }
+		if (!_properties.containsKey("airports"))
+			throw new IllegalStateException("Airports not Loaded");
 
-   /**
-    * Returns an Airline object.
-    * @param airlineCode the Airline code
-    * @return the Airline bean, or null if not found
-    * @throws NullPointerException if airlineCode is null
-    * @throws IllegalStateException if the &quot;airlines&quot; property has not been added
-    */
-   public static Airline getAirline(String airlineCode) {
-      if (airlineCode == null)
-         return null;
+		Map airports = (Map) _properties.get("airports");
+		return (Airport) airports.get(airportCode.toUpperCase());
+	}
 
-      if (!_properties.containsKey("airlines"))
-         throw new IllegalStateException("Airlines not Loaded");
+	/**
+	 * Returns an Airline object.
+	 * @param airlineCode the Airline code
+	 * @return the Airline bean, or null if not found
+	 * @throws NullPointerException if airlineCode is null
+	 * @throws IllegalStateException if the &quot;airlines&quot; property has not been added
+	 */
+	public static Airline getAirline(String airlineCode) {
+		if (airlineCode == null)
+			return null;
 
-      Map airlines = (Map) _properties.get("airlines");
-      return (Airline) airlines.get(airlineCode.trim().toUpperCase());
-   }
+		if (!_properties.containsKey("airlines"))
+			throw new IllegalStateException("Airlines not Loaded");
+
+		Map airlines = (Map) _properties.get("airlines");
+		return (Airline) airlines.get(airlineCode.trim().toUpperCase());
+	}
 }
