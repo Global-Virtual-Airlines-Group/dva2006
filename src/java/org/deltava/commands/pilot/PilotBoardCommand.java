22f2f24886e216fb6a6d8872e01a48d64601c1e7
Luke Kolin <luke@deltava.org>
2005-07-25 21:13:27 +0000

    Added HQ bean
    git-svn-id: file:///home/svn/DVA2006@255 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/pilot/PilotBoardCommand.java
+++ src/java/org/deltava/commands/pilot/PilotBoardCommand.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 
 import org.deltava.beans.Pilot;
 import org.deltava.beans.GeoLocation;
+import org.deltava.beans.MapEntry;
 
 import org.deltava.beans.schedule.GeoPosition;
 import org.deltava.beans.stats.PilotLocation;
@@ -15,6 +16,9 @@ import org.deltava.commands.*;
 import org.deltava.dao.GetPilot;
 import org.deltava.dao.DAOException;
 
+import org.deltava.util.StringUtils;
+import org.deltava.util.system.SystemData;
+
 /**
  * A Web Site Command to display Pilot locations.
  * @author Luke
@@ -23,6 +27,41 @@ import org.deltava.dao.DAOException;
  */
 
 public class PilotBoardCommand extends AbstractCommand {
+   
+   private class SpecialLocation implements GeoLocation, MapEntry {
+      
+      private GeoPosition _gPos;
+      
+      public SpecialLocation(double lat, double lon) {
+         super();
+         _gPos = new GeoPosition(lat, lon);
+      }
+      
+      public double getLatitude() {
+         return _gPos.getLatitude();
+      }
+      
+      public double getLongitude() {
+         return _gPos.getLongitude();
+      }
+      
+      public String getIconColor() {
+         return MapEntry.BLUE;
+      }
+      
+      public String getInfoBox() {
+         StringBuffer buf = new StringBuffer("<span class=\"small bld\">");
+         buf.append(SystemData.get("airline.name"));
+         buf.append("</span><span class=\"small\"><br /><br />Position: ");
+         buf.append(StringUtils.format(_gPos, true, GeoLocation.ALL));
+         buf.append("<br /><a href=\"http://");
+         buf.append(SystemData.get("airline.url"));
+         buf.append("/\">http://");
+         buf.append(SystemData.get("airline.url"));
+         buf.append("/</a></span>");
+         return buf.toString();
+      }
+   }
 
 	/**
 	 * Executes the command.
@@ -43,7 +82,7 @@ public class PilotBoardCommand extends AbstractCommand {
 			double rndAmt = ((rnd.nextDouble() * 3) - 1) / GeoLocation.DEGREE_MILES;
 			
 			// Loop through the GeoLocations, apply the random adjuster and combine with the Pilot
-			Map pilotLocations = new HashMap(pilots.size());
+			Set pilotLocations = new HashSet(pilots.size());
 			for (Iterator i = pilots.keySet().iterator(); i.hasNext(); ) {
 				Integer id = (Integer) i.next();
 				GeoPosition gp = (GeoPosition) locations.get(id);
@@ -53,8 +92,12 @@ public class PilotBoardCommand extends AbstractCommand {
 				// Create the pilot location
 				Pilot usr = (Pilot) pilots.get(id);
 				if (usr != null)
-					pilotLocations.put(id, new PilotLocation(usr, gp));
+					pilotLocations.add(new PilotLocation(usr, gp));
 			}
+			
+			// Add the home base
+			pilotLocations.add(new SpecialLocation(SystemData.getDouble("airline.location.lat"), 
+			      SystemData.getDouble("airline.location.lng")));
 			
 			// Save the locations
 			ctx.setAttribute("locations", pilotLocations, REQUEST);
