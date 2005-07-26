24482d051c544589619c81d7bb2044b4f7d210cf
Luke Kolin <luke@deltava.org>
2005-07-26 21:35:01 +0000

    Added Live ACARS map
    git-svn-id: file:///home/svn/DVA2006@280 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/service/ACARSMapService.java
@@ -0,0 +1,67 @@
+// Copyright 2005 Luke J. Kolin. All Rights Reserved.
+package org.deltava.service;
+
+import java.io.IOException;
+import java.util.*;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.jdom.*;
+import org.jdom.output.*;
+
+import org.deltava.beans.MapEntry;
+import org.deltava.beans.acars.ACARSAdminInfo;
+
+import org.deltava.util.StringUtils;
+import org.deltava.util.system.SystemData;
+
+/**
+ * A Web Service to provide XML-formatted ACARS position data for Google Maps.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class ACARSMapService extends WebService {
+
+	/**
+	 * Executes the Web Service.
+	 * @param ctx the Web Service Context
+	 * @return the HTTP status code
+	 * @throws ServiceException if an error occurs
+	 */
+   public int execute(ServiceContext ctx) throws ServiceException {
+      
+      // Get the ACARS connection Pool
+      ACARSAdminInfo acarsPool = (ACARSAdminInfo) SystemData.getObject(SystemData.ACARS_POOL);
+      
+		// Generate the XML document
+		Document doc = new Document();
+		Element re = new Element("wsdata");
+		doc.setRootElement(re);
+		
+		// Add the items
+		for (Iterator i = acarsPool.getMapEntries().iterator(); i.hasNext(); ) {
+		   MapEntry entry = (MapEntry) i.next();
+		   Element e = new Element("aircraft");
+		   e.setAttribute("lat", StringUtils.format(entry.getLatitude(), "##0.00000"));
+		   e.setAttribute("lng", StringUtils.format(entry.getLongitude(), "##0.00000"));
+		   e.setAttribute("color", entry.getIconColor());
+		   e.addContent(new CDATA(StringUtils.escapeSlashes(entry.getInfoBox())));
+		   re.addContent(e);
+		}
+		
+		// Dump the XML to the output stream
+		XMLOutputter xmlOut = new XMLOutputter(Format.getPrettyFormat());
+		try {
+			ctx.getResponse().setContentType("text/xml");
+			ctx.println(xmlOut.outputString(doc));
+			ctx.commit();
+		} catch (IOException ie) {
+			throw new ServiceException(HttpServletResponse.SC_CONFLICT, "I/O Error");
+		}
+
+		// Return success code
+		return HttpServletResponse.SC_OK;
+   }
+}
