c8fdbf359702f8d84d8ed1ae96ea856f718669aa
Luke Kolin <luke@deltava.org>
2005-07-28 18:50:28 +0000

    Updated JavaDoc
    git-svn-id: file:///home/svn/DVA2006@310 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/stats/PilotLocation.java
+++ src/java/org/deltava/beans/stats/PilotLocation.java
@@ -8,40 +8,66 @@ import org.deltava.beans.MapEntry;
 import org.deltava.util.StringUtils;
 
 /**
- * A bean to store pilot locations.
+ * A bean to store pilot locations for displaying on a Google Map.
  * @author Luke
  * @version 1.0
  * @since 1.0
  */
 
-public class PilotLocation implements GeoLocation, MapEntry {
+public class PilotLocation implements MapEntry {
 	
 	private Pilot _usr;
 	private GeoLocation _position;
 
-	// TODO JavaDoc
+	/**
+	 * Creates a new Pilot location object.
+	 * @param usr the Pilot bean
+	 * @param loc the Pilot's location
+	 */
 	public PilotLocation(Pilot usr, GeoLocation loc) {
 		super();
 		_usr = usr;
 		_position = loc;
 	}
 	
+	/**
+	 * Returns the Pilot's latitude.
+	 * @return the latitude in degrees
+	 * @see PilotLocation#getLongitude()
+	 */
 	public double getLatitude() {
 		return _position.getLatitude();
 	}
 	
+	/**
+	 * Returns the Pilot's longitude.
+	 * @return the longitude in degrees
+	 * @see PilotLocation#getLatitude()
+	 */
 	public double getLongitude() {
 		return _position.getLongitude();
 	}
 	
+	/**
+	 * Returns the Pilot.
+	 * @return the Pilot bean
+	 */
 	public Pilot getUser() {
 		return _usr;
 	}
 	
+	/**
+	 * Display the Google Map icon color.
+	 * @return BLUE
+	 */
 	public String getIconColor() {
 		return MapEntry.BLUE;
 	}
 	
+	/**
+	 * Returns the Google Map info box text.
+	 * @return the info box text
+	 */
 	public String getInfoBox() {
 		StringBuffer buf = new StringBuffer("<span class=\"small\"><b>");
 		buf.append(_usr.getName());
