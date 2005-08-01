053959600fa2eae3281adf382305288e191f65f2
Luke Kolin <luke@deltava.org>
2005-08-01 20:58:38 +0000

    Added ACARS map XMLHttpRequest generator
    git-svn-id: file:///home/svn/DVA2006@355 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ static/js/acarsMap.js
@@ -0,0 +1,30 @@
+function generateXMLRequest()
+{
+// Build the XML Requester
+var xmlreq = GXmlHttp.create();
+xmlreq.open("GET", "acars_map.ws", true);
+xmlreq.onreadystatechange = function() {
+	if (xmlreq.readyState != 4) return false;
+
+	var points = new Array();
+	var xmlDoc = xmlreq.responseXML;
+	var ac = xmlDoc.documentElement.getElementsByTagName("aircraft");
+	for (var i = 0; i < ac.length; i++) {
+		var a = ac[i];
+		var p = new GPoint(parseFloat(a.getAttribute("lng")), parseFloat(a.getAttribute("lat")));
+		var m = googleMarker("${imgPath}",a.getAttribute("color"),p,a.Text);
+		points.push(m);
+	} // for
+	
+	// Remove positions from the map, and replace with the new positions
+	map.clearOverlays();
+	addMarkers(map, points);
+
+	// Enable the buttons
+	enableButton('RefreshButton');
+	enableButton('SettingsButton');
+	return true;
+} // function
+
+return xmlreq;
+}
