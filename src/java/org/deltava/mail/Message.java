b2a951a2bef32b42ccebec04c0d5193100e43615
Luke Kolin <luke@deltava.org>
2005-08-02 20:12:37 +0000

    Added debug code
    git-svn-id: file:///home/svn/DVA2006@379 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/mail/Message.java
+++ src/java/org/deltava/mail/Message.java
@@ -12,66 +12,66 @@ import org.deltava.util.system.SystemData;
 
 class Message {
 
-	private String _msgBody;
-	private MessageContext _ctx;
+   private String _msgBody;
+   private MessageContext _ctx;
 
-	/**
-	 * Creates a new message using a particular Message Context.
-	 * @param ctx the message context
-	 */
-	public Message(MessageContext ctx) {
-		super();
-		_ctx = ctx;
-	}
+   /**
+    * Creates a new message using a particular Message Context.
+    * @param ctx the message context
+    */
+   public Message(MessageContext ctx) {
+      super();
+      _ctx = ctx;
+   }
 
-	/**
-	 * Returns the message subject.
-	 * @return the subject prepended by the Airline Name
-	 */
-	public String getSubject() {
-		return SystemData.get("airline.name") + " " + _ctx.getTemplate().getSubject();
-	}
+   /**
+    * Returns the message subject.
+    * @return the subject prepended by the Airline Name
+    */
+   public String getSubject() {
+      StringBuffer buf = new StringBuffer(SystemData.get("airline.name"));
+      buf.append(' ');
+      buf.append(_ctx.getTemplate().getSubject());
+      return buf.toString();
+   }
 
-	/**
-	 * Returns the formatted message body text.
-	 * @return the body text, or null if {@link Message#format()} has not been called yet
-	 */
-	public String getBody() {
-		return _msgBody;
-	}
+   /**
+    * Returns the formatted message body text.
+    * @return the body text, or null if {@link Message#format()}has not been called yet
+    */
+   public String getBody() {
+      return _msgBody;
+   }
 
-	/**
-	 * Formats the message by replacing arguments in the message template with values from the message context.
-	 */
-	public void format() {
+   /**
+    * Formats the message by replacing arguments in the message template with values from the message context.
+    * @throws IllegalStateException if no message context or template exists
+    */
+   public void format() {
 
-		// Check that the message context has been set
-		if (_ctx == null)
-			throw new IllegalStateException("Message Context not set");
-		
-		// If there's no template, then abort
-		if (_ctx.getTemplate() == null)
-			return;
+      // Check that the message context has been set
+      if ((_ctx == null) || (_ctx.getTemplate() == null))
+         throw new IllegalStateException("Message Context not set");
 
-		// Load the Message template
-		StringBuffer buf = new StringBuffer(_ctx.getTemplate().getBody());
+      // Load the Message template
+      StringBuffer buf = new StringBuffer(_ctx.getTemplate().getBody());
 
-		// Parse the message template with data from the MessageContext
-		int spos = buf.indexOf("${");
-		while (spos != -1) {
-			int epos = buf.indexOf("}", spos);
+      // Parse the message template with data from the MessageContext
+      int spos = buf.indexOf("${");
+      while (spos != -1) {
+         int epos = buf.indexOf("}", spos);
 
-			// Only format if the end token can be found
-			if (epos > spos) {
-				String token = buf.substring(spos + 2, epos);
-				buf.replace(spos, epos + 1, _ctx.execute(token));
-				spos = buf.indexOf("${");
-			} else {
-				spos = buf.indexOf("${", spos);
-			}
-		}
-		
-		// Save the message body
-		_msgBody = buf.toString();
-	}
+         // Only format if the end token can be found
+         if (epos > spos) {
+            String token = buf.substring(spos + 2, epos);
+            buf.replace(spos, epos + 1, _ctx.execute(token));
+            spos = buf.indexOf("${");
+         } else {
+            spos = buf.indexOf("${", spos);
+         }
+      }
+
+      // Save the message body
+      _msgBody = buf.toString();
+   }
 }
