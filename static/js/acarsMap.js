9f53eac221456e6552a001758375f2ae18479235
Luke Kolin <luke@deltava.org>
2005-08-03 05:07:20 +0000

    Added CDATA processing
    git-svn-id: file:///home/svn/DVA2006@390 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/acarsMap.js
+++ static/js/acarsMap.js
@@ -1,4 +1,4 @@
-function generateXMLRequest()
+function generateXMLRequest(imgPath)
 {
 // Build the XML Requester
 var xmlreq = GXmlHttp.create();
@@ -9,19 +9,15 @@ xmlreq.onreadystatechange = function() {
 	var points = new Array();
 	var xmlDoc = xmlreq.responseXML;
 	var ac = xmlDoc.documentElement.getElementsByTagName("aircraft");
+	map.clearOverlays();
 	for (var i = 0; i < ac.length; i++) {
 		var a = ac[i];
+		var label = a.firstChild;
 		var p = new GPoint(parseFloat(a.getAttribute("lng")), parseFloat(a.getAttribute("lat")));
-		var m = googleMarker("${imgPath}",a.getAttribute("color"),p,a.Text);
-		points.push(m);
+		map.addOverlay(googleMarker(imgPath,a.getAttribute("color"),p,label.data));
 	} // for
 	
-	// Remove positions from the map, and replace with the new positions
-	map.clearOverlays();
-	addMarkers(map, points);
-
 	// Enable the buttons
-	enableButton('ToggleButton');
 	enableButton('RefreshButton');
 	enableButton('SettingsButton');
 	return true;
