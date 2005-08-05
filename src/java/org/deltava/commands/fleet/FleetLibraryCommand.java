d99125bad4f10fe88e38e1a4a1e07d2a0fac58dc
Luke Kolin <luke@deltava.org>
2005-08-05 20:17:30 +0000

    Display DVA/AFV fleet libraries together
    git-svn-id: file:///home/svn/DVA2006@435 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/fleet/FleetLibraryCommand.java
+++ src/java/org/deltava/commands/fleet/FleetLibraryCommand.java
@@ -12,6 +12,8 @@ import org.deltava.dao.DAOException;
 
 import org.deltava.security.command.FleetEntryAccessControl;
 
+import org.deltava.util.system.SystemData;
+
 /**
  * A Web Site Command to display the Fleet Library.
  * @author Luke
@@ -20,62 +22,71 @@ import org.deltava.security.command.FleetEntryAccessControl;
  */
 
 public class FleetLibraryCommand extends AbstractCommand {
-	
-	private static final Logger log = Logger.getLogger(FleetLibraryCommand.class);
 
-	/**
-     * Executes the command.
-     * @param ctx the Command context
-     * @throws CommandException if an unhandled error occurs
-     */
-    public void execute(CommandContext ctx) throws CommandException {
-       
-       // Check if we're viewing the library as an administrator
-       boolean isAdmin = "admin".equals(ctx.getCmdParameter(Command.OPERATION, null));
-       
-       // Check our access
-       FleetEntryAccessControl access = new FleetEntryAccessControl(ctx, null);
-       access.validate();
-       if (isAdmin && !access.getCanCreate())
-          throw new CommandSecurityException("Cannot update Fleet Library");
+   private static final Logger log = Logger.getLogger(FleetLibraryCommand.class);
 
-        List results = null;
-        try {
-            Connection con = ctx.getConnection();
-            
-            // Get the DAO and the Fleet Library
-            GetLibrary dao = new GetLibrary(con);
-            results = dao.getFleet();
-        } catch (DAOException de) {
-            throw new CommandException(de);
-        } finally {
-            ctx.release();
-        }
-        
-		// Validate our access to the results
-		for (Iterator i = results.iterator(); i.hasNext(); ) {
-			FleetEntry e = (FleetEntry) i.next();
-			access.setEntry(e);
-			access.validate();
-			
-			// If we cannot view this entry, remove it from the list
-			if (!access.getCanView())
-			   i.remove();
-			
-			// If the entry is not present on the file system, remove it
-			if (e.getSize() == 0) {
-			   log.warn("Resource " + e.getFullName() + " not found in file system!");
-			   if (!isAdmin)
-			      i.remove();
-			}
-		}
-		
-		// Save the results in the request
-		ctx.setAttribute("fleet", results, REQUEST);
-        
-        // Forward to the JSP
-        CommandResult result = ctx.getResult();
-        result.setURL(isAdmin ? "/jsp/fleet/installerLibrary.jsp" : "/jsp/fleet/fleetLibrary.jsp");
-        result.setSuccess(true);
-    }
+   /**
+    * Executes the command.
+    * @param ctx the Command context
+    * @throws CommandException if an unhandled error occurs
+    */
+   public void execute(CommandContext ctx) throws CommandException {
+
+      // Check if we're viewing the library as an administrator
+      boolean isAdmin = "admin".equals(ctx.getCmdParameter(Command.OPERATION, null));
+
+      // Check our access
+      FleetEntryAccessControl access = new FleetEntryAccessControl(ctx, null);
+      access.validate();
+      if (isAdmin && !access.getCanCreate())
+         throw new CommandSecurityException("Cannot update Fleet Library");
+
+      List results = null;
+      try {
+         Connection con = ctx.getConnection();
+
+         // Get the DAO and the Fleet Library
+         GetLibrary dao = new GetLibrary(con);
+         results = dao.getFleet(SystemData.get("airline.db"));
+
+         // Get the fleet libraries from the other airlines if we're not in admin mode
+         if (!isAdmin) {
+            Map dbs = (Map) SystemData.getObject("airlineDatabases");
+            for (Iterator i = dbs.keySet().iterator(); i.hasNext();) {
+               String dbName = (String) i.next();
+               results.addAll(dao.getFleet(dbName));
+            }
+         }
+      } catch (DAOException de) {
+         throw new CommandException(de);
+      } finally {
+         ctx.release();
+      }
+
+      // Validate our access to the results
+      for (Iterator i = results.iterator(); i.hasNext();) {
+         FleetEntry e = (FleetEntry) i.next();
+         access.setEntry(e);
+         access.validate();
+
+         // If we cannot view this entry, remove it from the list
+         if (!access.getCanView())
+            i.remove();
+
+         // If the entry is not present on the file system, remove it
+         if (e.getSize() == 0) {
+            log.warn("Resource " + e.getFullName() + " not found in file system!");
+            if (!isAdmin)
+               i.remove();
+         }
+      }
+
+      // Save the results in the request
+      ctx.setAttribute("fleet", results, REQUEST);
+
+      // Forward to the JSP
+      CommandResult result = ctx.getResult();
+      result.setURL(isAdmin ? "/jsp/fleet/installerLibrary.jsp" : "/jsp/fleet/fleetLibrary.jsp");
+      result.setSuccess(true);
+   }
 }
