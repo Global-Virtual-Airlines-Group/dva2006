b2711608e97095e471079f08b6a53f3687f80f01
Luke Kolin <luke@deltava.org>
2005-08-07 18:22:15 +0000

    Integrated with Inactivity table
    git-svn-id: file:///home/svn/DVA2006@448 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/tasks/InactivityUpdateTask.java
+++ src/java/org/deltava/tasks/InactivityUpdateTask.java
@@ -2,11 +2,13 @@
 package org.deltava.tasks;
 
 import java.util.*;
+import java.text.*;
 
 import org.apache.log4j.Logger;
 
 import org.deltava.beans.Pilot;
 import org.deltava.beans.StatusUpdate;
+import org.deltava.beans.system.InactivityPurge;
 import org.deltava.beans.system.MessageTemplate;
 
 import org.deltava.dao.*;
@@ -23,71 +25,124 @@ import org.deltava.util.system.SystemData;
  */
 
 public class InactivityUpdateTask extends DatabaseTask {
-   
-   private static final Logger log = Logger.getLogger(InactivityUpdateTask.class);
 
-   /**
-    * Initializes the Schedued Task.
-    */
-   public InactivityUpdateTask() {
-      super("Inactivity Status Update");
-   }
+	private static final Logger log = Logger.getLogger(InactivityUpdateTask.class);
+	private static final DateFormat _df = new SimpleDateFormat("MMMM dd yyyy");
 
-   /**
-    * Executes the task.
-    */
-   protected void execute() {
-      
-      // Get the inactivity cutoff time
-      int inactiveDays = SystemData.getInt("users.inactive_days");
-      Calendar cld = Calendar.getInstance();
-      cld.add(Calendar.DATE, inactiveDays * -1);
-      log.info("Executing");
+	/**
+	 * Initializes the Schedued Task.
+	 */
+	public InactivityUpdateTask() {
+		super("Inactivity Status Update");
+	}
 
-      try {
-         SetStatusUpdate sudao = new SetStatusUpdate(_con);
-         SetPilot pwdao = new SetPilot(_con);
-         
-         // Get the Message template
-         GetMessageTemplate mtdao = new GetMessageTemplate(_con);
-         MessageTemplate mt = mtdao.get("USERINACTIVE");
-         
-         // Figure out who we're operating as
-         GetPilotNotify dao = new GetPilotNotify(_con);
-         Pilot taskBy = dao.getByName(SystemData.get("users.tasks_by"));
-         
-         // Get the pilots to deactivate
-         List pilots = dao.getPilotsByLastLogin(cld.getTime());
-         for (Iterator i = pilots.iterator(); i.hasNext(); ) {
-            Pilot p = (Pilot) i.next();
-            log.info("Marking " + p.getName() + " Inactive");
-            
-            // Create the StatusUpdate bean
-            StatusUpdate upd = new StatusUpdate(p.getID(), StatusUpdate.STATUS_CHANGE);
-            upd.setAuthorID(taskBy.getID()); 
-            upd.setCreatedOn(new Date());
-            upd.setDescription("Marked Inactive due to no logins within " + inactiveDays + " days");
-            sudao.write(upd);
-            
-            // Create the Message Context
-            MessageContext mctxt = new MessageContext();
-            mctxt.setTemplate(mt);
-            mctxt.addData("user", taskBy);
-            mctxt.addData("pilot", p);
-            
-            // Deactivate the Pilot
-            p.setStatus(Pilot.INACTIVE);
-            pwdao.write(p);
-            
-            // Send notification message
-    		Mailer mailer = new Mailer(taskBy);
-    		mailer.setContext(mctxt);
-    		mailer.send(p);
-         }
-      } catch (DAOException de) {
-         log.error(de.getMessage());
-      }
-      
-      log.info("Completed");
-   }
+	/**
+	 * Executes the task.
+	 */
+	protected void execute() {
+
+		// Get the inactivity cutoff time
+		int inactiveDays = SystemData.getInt("users.inactive_days");
+		int notifyDays = SystemData.getInt("users.notify_days");
+		
+		// Check if we're in test mode
+		boolean isTest = SystemData.getBoolean("smtp.testMode");
+
+		try {
+			// Initialize the DAOs
+			GetInactivity idao = new GetInactivity(_con);
+			SetStatusUpdate sudao = new SetStatusUpdate(_con);
+			SetPilot pwdao = new SetPilot(_con);
+			SetInactivity iwdao = new SetInactivity(_con);
+
+			// Get the Message templates
+			GetMessageTemplate mtdao = new GetMessageTemplate(_con);
+			MessageTemplate imt = mtdao.get("USERINACTIVE");
+			MessageTemplate nmt = mtdao.get("USERNOTIFY");
+
+			// Figure out who we're operating as
+			GetInactivity dao = new GetInactivity(_con);
+			Pilot taskBy = dao.getByName(SystemData.get("users.tasks_by"));
+			
+			// Initialize the mailer
+			Mailer mailer = new Mailer(isTest ? null : taskBy);
+
+			// Get the pilots to deactivate
+			Collection purgeBeans = dao.getPurgeable(true);
+			Map pilots = dao.getByID(purgeBeans, "PILOTS");
+			for (Iterator i = purgeBeans.iterator(); i.hasNext();) {
+				InactivityPurge ip = (InactivityPurge) i.next();
+				Pilot p = (Pilot) pilots.get(new Integer(ip.getID()));
+				if (p != null) {
+					log.info("Marking " + p.getName() + " Inactive");
+
+					// Create the StatusUpdate bean
+					StatusUpdate upd = new StatusUpdate(p.getID(), StatusUpdate.STATUS_CHANGE);
+					upd.setAuthorID(taskBy.getID());
+					upd.setCreatedOn(new Date());
+					upd.setDescription("Marked Inactive due to no logins within " + inactiveDays + " days");
+					sudao.write(upd);
+
+					// Create the Message Context
+					MessageContext mctxt = new MessageContext();
+					mctxt.setTemplate(imt);
+					mctxt.addData("user", taskBy);
+					mctxt.addData("pilot", p);
+					mctxt.addData("lastLogin", (p.getLastLogin() == null) ? "NEVER" : _df.format(p.getLastLogin()));
+
+					// Deactivate the Pilot
+					p.setStatus(Pilot.INACTIVE);
+					pwdao.write(p);
+
+					// Send notification message
+					mailer.setContext(mctxt);
+					mailer.send(p);
+				} else {
+					log.warn("Spurious Purge entry for Pilot ID " + ip.getID());
+				}
+
+				// Clear the inactivity record
+				iwdao.delete(ip.getID());
+			}
+			
+			// Get the Pilots to notify
+			Collection nPilots = dao.getInactivePilots(notifyDays);
+			for (Iterator i = nPilots.iterator(); i.hasNext();) {
+				Pilot p = (Pilot) i.next();
+				
+				// Check if we've been notified already
+				InactivityPurge ip = idao.getInactivity(p.getID());
+				if ((ip == null) || (!ip.isNotified())) {
+					log.info("Notifying " + p.getName());
+					
+					// Create the StatusUpdate bean
+					StatusUpdate upd = new StatusUpdate(p.getID(), StatusUpdate.STATUS_CHANGE);
+					upd.setAuthorID(taskBy.getID());
+					upd.setCreatedOn(new Date());
+					upd.setDescription("Sent Reminder due to no logins within " + notifyDays + " days");
+					sudao.write(upd);
+
+					// Create the Message Context
+					MessageContext mctxt = new MessageContext();
+					mctxt.setTemplate(nmt);
+					mctxt.addData("user", taskBy);
+					mctxt.addData("p", p);
+					mctxt.addData("lastLogin", (p.getLastLogin() == null) ? "NEVER" : _df.format(p.getLastLogin()));
+
+					// Make sure we have a notification entry
+					iwdao.setInactivity(p.getID(), inactiveDays - notifyDays, true);
+					
+					// Send the message
+					mailer.setContext(mctxt);
+					mailer.send(p);
+				}
+			}
+		} catch (DAOException de) {
+			log.error(de.getMessage(), de);
+			throw new RuntimeException(de);
+		}
+
+		// Log completion
+		log.info("Completed");
+	}
 }
