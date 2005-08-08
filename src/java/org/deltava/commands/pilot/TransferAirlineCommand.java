41df35d2a9144e0caadac21643c70550964207db
Luke Kolin <luke@deltava.org>
2005-08-08 20:57:28 +0000

    Suggestions for James
    git-svn-id: file:///home/svn/DVA2006@485 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/pilot/TransferAirlineCommand.java
+++ src/java/org/deltava/commands/pilot/TransferAirlineCommand.java
@@ -1,7 +1,6 @@
 //Copyright (c) 2005 James Brickell. All Rights Reserved.
 package org.deltava.commands.pilot;
 
-import java.util.*;
 import java.sql.Connection;
 
 import org.apache.log4j.Logger;
@@ -12,7 +11,6 @@ import org.deltava.commands.*;
 import org.deltava.dao.*;
 import org.deltava.security.command.*;
 
-import org.deltava.util.*;
 import org.deltava.util.system.SystemData;
 
 /**
@@ -23,7 +21,10 @@ import org.deltava.util.system.SystemData;
  */
 
 public class TransferAirlineCommand extends AbstractCommand {
-	/**
+	
+   public static final Logger log = Logger.getLogger(TransferAirlineCommand.class);
+   
+   /**
      * Execute the command.
      * @param ctx the Command context
      * @throws CommandException if an unhandled error occrurs.
@@ -32,7 +33,13 @@ public class TransferAirlineCommand extends AbstractCommand {
 	public void execute(CommandContext ctx) throws CommandException {
 		
 		// Get PID
+	   // FIXME just use ctx.getID() - if none is provided your code will transfer the current pilot.
 		int id = (ctx.getID() == 0) ? ctx.getUser().getID() : ctx.getID();
+		
+		// Read a request parameter called "newAirline" to get the airline to change to
+		
+		// Get the airlineDatabases Map from SystemData and make sure the db name is contained
+		// within map.keySet() if not, throw an exception
 		
 		// Get connection from the pool
 		try {
@@ -40,7 +47,7 @@ public class TransferAirlineCommand extends AbstractCommand {
 			
 			// Get whichever pilot we're transferring
 			GetPilot rdao = new GetPilot(con);
-			Pilot p = rdao.get(id);
+			Pilot p = rdao.get(id); // call ctx.getID() here
 			if (p == null)
 				throw new CommandException("Invalid Pilot ID - " + id);
 			
@@ -49,21 +56,40 @@ public class TransferAirlineCommand extends AbstractCommand {
 			access.validate();
 			if (!access.getCanChangeStatus())
 				throw new CommandSecurityException("Insufficient access to transfer a pilot to another airline");
-				
-			//TODO Call DAO to Change Pilot's Airline
 			
+			// Start Transaction
 				
 			//TODO Change LDAP DN
+			// What I think we should do here is build the DN as cn= + p.getName() + ",ou=" + dbName + ",o=sce"
+			// Let's hard code it for now, and figure out the elegant way later
+			
+			// Create a new userData record and write it
+			
 			
 			// Change status at old airline to Transferred
-			// Luke - I updated the SetPilot DAO for this - committed also
 			SetPilot wdao = new SetPilot(con);
-			wdao.setTransferred(p.getID());
 			p.setStatus(Pilot.TRANSFERRED);
-				
+			wdao.setTransferred(p.getID());
+			
+			// Get the ID property from the UserData object and stuff it into the existing Pilot object
+			
+			// Write the new pilot record in the other database
+			
+			// Get the Authenticator
+			
+			// Calculate a random password, since we can't read the old password
+			
+			// Add the new DN to the authenticator with the new password
+			
+			// Remove the old DN
+			
+			// Commit transaction
+			
+			
 			// Save pilot in request
 			ctx.setAttribute("pilot", p, REQUEST);
 		} catch (DAOException de) {
+		   // Rollback transaction
 			throw new CommandException(de);
 		} finally {
 			ctx.release();
