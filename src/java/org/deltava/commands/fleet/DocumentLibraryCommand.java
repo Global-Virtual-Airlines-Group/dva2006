8147f455d3a83ededba3585e6d0a3951a516312c
Luke Kolin <luke@deltava.org>
2005-08-16 13:33:10 +0000

    Use AIRLINEINFO table to get other databases
    git-svn-id: file:///home/svn/DVA2006@527 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/fleet/DocumentLibraryCommand.java
+++ src/java/org/deltava/commands/fleet/DocumentLibraryCommand.java
@@ -18,8 +18,8 @@ import org.deltava.security.command.FleetEntryAccessControl;
 import org.deltava.util.system.SystemData;
 
 /**
- * A Web Site command to display the Document Library. Note that this command will display library entries
- * from other Airlines, with the proviso that <i>all files are in the same library path</i>.
+ * A Web Site command to display the Document Library. Note that this command will display library entries from other
+ * Airlines, with the proviso that <i>all files are in the same library path</i>.
  * @author Luke
  * @version 1.0
  * @since 1.0
@@ -28,41 +28,40 @@ import org.deltava.util.system.SystemData;
 public class DocumentLibraryCommand extends AbstractCommand {
 
 	private static final Logger log = Logger.getLogger(DocumentLibraryCommand.class);
-	
+
 	/**
-     * Executes the command.
-     * @param ctx the Command context
-     * @throws CommandException if an unhandled error occurs
-     */
+	 * Executes the command.
+	 * @param ctx the Command context
+	 * @throws CommandException if an unhandled error occurs
+	 */
 	public void execute(CommandContext ctx) throws CommandException {
-		
+
 		List results = null;
 		try {
 			Connection con = ctx.getConnection();
-			
-			// Get the DAO and the results
+
+			// Get the DAO
 			GetLibrary dao = new GetLibrary(con);
-			results = dao.getManuals(SystemData.get("airline.db"));
-			
+
 			// Get the document libraries from the other airlines
 			Map apps = (Map) SystemData.getObject("apps");
-         for (Iterator i = apps.values().iterator(); i.hasNext();) {
-            AirlineInformation info = (AirlineInformation) i.next();
-            results.addAll(dao.getManuals(info.getDB()));
-         }
+			for (Iterator i = apps.values().iterator(); i.hasNext();) {
+				AirlineInformation info = (AirlineInformation) i.next();
+				results.addAll(dao.getManuals(info.getDB()));
+			}
 		} catch (DAOException de) {
 			throw new CommandException(de);
 		} finally {
 			ctx.release();
 		}
-		
+
 		// Validate our access to the results
 		FleetEntryAccessControl access = new FleetEntryAccessControl(ctx, null);
-		for (Iterator i = results.iterator(); i.hasNext(); ) {
+		for (Iterator i = results.iterator(); i.hasNext();) {
 			FleetEntry e = (FleetEntry) i.next();
 			access.setEntry(e);
 			access.validate();
-			
+
 			// Check that the resource exists
 			if (e.getSize() == 0) {
 				log.warn("Resource " + e.getFullName() + " not found in file system!");
@@ -72,7 +71,7 @@ public class DocumentLibraryCommand extends AbstractCommand {
 				i.remove();
 			}
 		}
-		
+
 		// Save the results in the request
 		ctx.setAttribute("docs", results, REQUEST);
 
