2e41c160419bf1d4929c0769c505a7e1f3025562
Luke Kolin <luke@deltava.org>
2005-08-17 22:01:08 +0000

    Make charts editable
    git-svn-id: file:///home/svn/DVA2006@546 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/schedule/ChartCommand.java
+++ src/java/org/deltava/commands/schedule/ChartCommand.java
@@ -7,13 +7,11 @@ import org.deltava.beans.FileUpload;
 import org.deltava.beans.schedule.Chart;
 
 import org.deltava.commands.*;
-
-import org.deltava.dao.GetChart;
-import org.deltava.dao.SetChart;
-import org.deltava.dao.DAOException;
+import org.deltava.dao.*;
 
 import org.deltava.security.command.ChartAccessControl;
 
+import org.deltava.util.ComboUtils;
 import org.deltava.util.ImageInfo;
 import org.deltava.util.system.SystemData;
 
@@ -56,6 +54,7 @@ public class ChartCommand extends AbstractFormCommand {
 				// Load data from the request
 				c.setName(ctx.getParameter("name"));
 				c.setAirport(SystemData.getAirport(ctx.getParameter("airport")));
+				c.setType(ctx.getParameter("chartType"));
 			} else {
 				// Get our access
 				ChartAccessControl access = new ChartAccessControl(ctx);
@@ -64,6 +63,7 @@ public class ChartCommand extends AbstractFormCommand {
 					throw new CommandSecurityException("Cannot create Approach Chart");
 
 				c = new Chart(ctx.getParameter("name"), SystemData.getAirport(ctx.getParameter("airport")));
+				c.setType(ctx.getParameter("chartType"));
 
 				// Load the image data
 				FileUpload imgData = ctx.getFile("img");
@@ -75,7 +75,7 @@ public class ChartCommand extends AbstractFormCommand {
 				info.check();
 
 				// Set image properties
-				c.setType(info.getFormat());
+				c.setImgType(info.getFormat());
 				c.load(imgData.getBuffer());
 			}
 			
@@ -120,6 +120,9 @@ public class ChartCommand extends AbstractFormCommand {
 		boolean isOK = (isNew) ? access.getCanCreate() : access.getCanEdit();
 		if (!isOK)
 			throw new CommandSecurityException("Cannot create/edit Approach Chart");
+		
+		// Save chart types
+		ctx.setAttribute("chartTypes", ComboUtils.fromArray(Chart.TYPENAMES, Chart.TYPES), REQUEST);
 
 		// Get the command result and forward to the JSP if this is new
 		CommandResult result = ctx.getResult();
@@ -140,13 +143,14 @@ public class ChartCommand extends AbstractFormCommand {
 
 			// Save the chart in the request
 			ctx.setAttribute("chart", c, REQUEST);
-			ctx.setAttribute("access", access, REQUEST);
-
 		} catch (DAOException de) {
 			throw new CommandException(de);
 		} finally {
 			ctx.release();
 		}
+		
+		// Save chart access
+		ctx.setAttribute("access", access, REQUEST);
 
 		// Forward to the JSP
 		result.setURL("/jsp/schedule/chartEdit.jsp");
