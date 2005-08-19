5d74308e38c70f1a85c3abc685e66b786bc64d85
Luke Kolin <luke@deltava.org>
2005-08-19 17:12:56 +0000

    Write HTTP stats entries
    git-svn-id: file:///home/svn/DVA2006@581 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/SetSystemLog.java
+++ src/java/org/deltava/dao/SetSystemLog.java
@@ -3,8 +3,10 @@ package org.deltava.dao;
 
 import java.sql.*;
 
+import org.deltava.beans.stats.HTTPStatistics;
+
 /**
- * A Data Access Object to purge System Log entries.
+ * A Data Access Object to write HTTP statistics and purge System Log entries.
  * @author Luke
  * @version 1.0
  * @since 1.0
@@ -50,6 +52,26 @@ public class SetSystemLog extends DAO {
          int rowsUpdated = _ps.executeUpdate();
          _ps.close();
          return rowsUpdated;
+      } catch (SQLException se) {
+         throw new DAOException(se);
+      }
+   }
+   
+   /**
+    * Writes an HTTP statistics bean to the log database. This handles inserts and updates.
+    * @param stats the statistics bean
+    * @throws DAOException if a JDBC error occurs
+    */
+   public void write(HTTPStatistics stats) throws DAOException {
+      try {
+         prepareStatement("REPLACE INTO SYS_HTTPLOG (DATE, REQUESTS, HOMEHITS, EXECTIME, BANDWIDTH) "
+               + "VALUES (?, ?, ?, ?, ?)");
+         _ps.setTimestamp(1, createTimestamp(stats.getDate()));
+         _ps.setInt(2, stats.getRequests());
+         _ps.setInt(3, stats.getHomePageHits());
+         _ps.setInt(4, stats.getExecutionTime());
+         _ps.setLong(5, stats.getBandwidth());
+         executeUpdate(1);
       } catch (SQLException se) {
          throw new DAOException(se);
       }
