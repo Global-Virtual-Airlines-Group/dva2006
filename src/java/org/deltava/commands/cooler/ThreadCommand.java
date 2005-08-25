d0837dd613e1d5d7202c942a9354cbc09ee46e7d
Luke Kolin <luke@deltava.org>
2005-08-25 17:35:42 +0000

    Reset DAO limit to 0 on subsequent calls
    git-svn-id: file:///home/svn/DVA2006@619 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/cooler/ThreadCommand.java
+++ src/java/org/deltava/commands/cooler/ThreadCommand.java
@@ -25,113 +25,114 @@ import org.deltava.util.system.SystemData;
  */
 
 public class ThreadCommand extends AbstractCommand {
-	
-	private static final List SCORES = Arrays.asList(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"});
 
-    /**
-     * Executes the command.
-     * @param ctx the Command context
-     * @throws CommandException if an unhandled error occurs
-     */
-    public void execute(CommandContext ctx) throws CommandException {
-        
-        // Get the user for the channel list
-        Person p = ctx.getUser();
-        
-        // Get the default airline
-        AirlineInformation airline = SystemData.getApp(SystemData.get("airline.code"));
-        
-        try {
-            Connection con = ctx.getConnection();
-            
-            // Get the Pilot's airline
-            GetUserData uddao = new GetUserData(con);
-            if (p != null) {
-            	UserData usrData = uddao.get(p.getID());
-            	if (usrData != null)
-            		airline = SystemData.getApp(usrData.getAirlineCode());
-            }
-            
-            // Get the Message Thread
-            GetCoolerThreads tdao = new GetCoolerThreads(con);
-            MessageThread thread = tdao.getThread(ctx.getID());
-            if (thread == null)
-                throw new CommandException("Unknown Message Thread - " + ctx.getID());
-            
-            // Get the channel profile
-            GetCoolerChannels cdao = new GetCoolerChannels(con);
-            Channel c = cdao.get(thread.getChannel());
-            
-            // Check user access
-            CoolerThreadAccessControl ac = new CoolerThreadAccessControl(ctx);
-            ac.updateContext(thread, c);
-            ac.validate();
-            if (!ac.getCanRead())
-                throw new CommandSecurityException("Cannot read Message Thread " + ctx.getID());
-            
-            // If we have an image, load its metadata
-            if (thread.getImage() != 0) {
-            	GetGallery irdao = new GetGallery(con);
-            	Image img = irdao.getImageData(thread.getImage());
-            	ctx.setAttribute("img", img, REQUEST);
-            	
-            	// Get our access to the image
-            	GalleryAccessControl imgAccess = new GalleryAccessControl(ctx, img);
-            	imgAccess.validate();
-            	ctx.setAttribute("imgAccess", imgAccess, REQUEST);
-            }
-            
-			// Get the location of all the Pilots
-			UserDataMap udm = uddao.getByThread(thread.getID());
-			ctx.setAttribute("userData", udm, REQUEST);
+   private static final List SCORES = Arrays.asList(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" });
 
-			// Get the authors and online totals for the last post in each channel
-			Map pilots = new HashMap();
-			GetPilot pdao = new GetPilot(con);
-			GetFlightReports prdao = new GetFlightReports(con);
-			for (Iterator i = udm.getTableNames().iterator(); i.hasNext(); ) {
-				String dbTableName = (String) i.next();
-				StringTokenizer tkns = new StringTokenizer(dbTableName, ".");
-				String dbName = tkns.nextToken();
-				String tableName = tkns.nextToken();
-				
-				// Get the IDs from this table
-				Set IDs = new HashSet(udm.getByTable(dbTableName));
-				
-				// Get the pilots from each table and apply their online totals
-				Map pilotSubset = pdao.getByID(IDs, dbTableName);
-				if ("PILOTS".equals(tableName))
-					prdao.getOnlineTotals(pilotSubset, dbName);	
-				
-				pilots.putAll(pilotSubset);
-			}
-			
-			// Mark the thread as being read
-			SetCoolerMessage wdao = new SetCoolerMessage(con);
-			wdao.viewThread(thread.getID());
-			
-            // Save all channels in the thread for the move combobox
-            if (ac.getCanUnlock() || ac.getCanLock()) {
-                ctx.setAttribute("channel", c, REQUEST);
-                ctx.setAttribute("channels", cdao.getChannels(airline, false), REQUEST);
-            }
-            
-            // Save the thread, pilots and access controller in the request
-            ctx.setAttribute("thread", thread, REQUEST);
-            ctx.setAttribute("access", ac, REQUEST);
-            ctx.setAttribute("pilots", pilots, REQUEST);
-        } catch (DAOException de) {
-            throw new CommandException(de);
-        } finally {
-            ctx.release();
-        }
-        
-        // Save scores choices
-        ctx.setAttribute("scores", SCORES, REQUEST);
+   /**
+    * Executes the command.
+    * @param ctx the Command context
+    * @throws CommandException if an unhandled error occurs
+    */
+   public void execute(CommandContext ctx) throws CommandException {
 
-        // Forward to the JSP
-        CommandResult result = ctx.getResult();
-        result.setURL("/jsp/cooler/threadShow.jsp");
-        result.setSuccess(true);
-    }
+      // Get the user for the channel list
+      Person p = ctx.getUser();
+
+      // Get the default airline
+      AirlineInformation airline = SystemData.getApp(SystemData.get("airline.code"));
+
+      try {
+         Connection con = ctx.getConnection();
+
+         // Get the Pilot's airline
+         GetUserData uddao = new GetUserData(con);
+         if (p != null) {
+            UserData usrData = uddao.get(p.getID());
+            if (usrData != null)
+               airline = SystemData.getApp(usrData.getAirlineCode());
+         }
+
+         // Get the Message Thread
+         GetCoolerThreads tdao = new GetCoolerThreads(con);
+         MessageThread thread = tdao.getThread(ctx.getID());
+         if (thread == null)
+            throw new CommandException("Unknown Message Thread - " + ctx.getID());
+
+         // Get the channel profile
+         GetCoolerChannels cdao = new GetCoolerChannels(con);
+         Channel c = cdao.get(thread.getChannel());
+
+         // Check user access
+         CoolerThreadAccessControl ac = new CoolerThreadAccessControl(ctx);
+         ac.updateContext(thread, c);
+         ac.validate();
+         if (!ac.getCanRead())
+            throw new CommandSecurityException("Cannot read Message Thread " + ctx.getID());
+
+         // If we have an image, load its metadata
+         if (thread.getImage() != 0) {
+            GetGallery irdao = new GetGallery(con);
+            Image img = irdao.getImageData(thread.getImage());
+            ctx.setAttribute("img", img, REQUEST);
+
+            // Get our access to the image
+            GalleryAccessControl imgAccess = new GalleryAccessControl(ctx, img);
+            imgAccess.validate();
+            ctx.setAttribute("imgAccess", imgAccess, REQUEST);
+         }
+
+         // Get the location of all the Pilots
+         uddao.setQueryMax(0);
+         UserDataMap udm = uddao.getByThread(thread.getID());
+         ctx.setAttribute("userData", udm, REQUEST);
+
+         // Get the authors and online totals for the last post in each channel
+         Map pilots = new HashMap();
+         GetPilot pdao = new GetPilot(con);
+         GetFlightReports prdao = new GetFlightReports(con);
+         for (Iterator i = udm.getTableNames().iterator(); i.hasNext();) {
+            String dbTableName = (String) i.next();
+            StringTokenizer tkns = new StringTokenizer(dbTableName, ".");
+            String dbName = tkns.nextToken();
+            String tableName = tkns.nextToken();
+
+            // Get the IDs from this table
+            Set IDs = new HashSet(udm.getByTable(dbTableName));
+
+            // Get the pilots from each table and apply their online totals
+            Map pilotSubset = pdao.getByID(IDs, dbTableName);
+            if ("PILOTS".equals(tableName))
+               prdao.getOnlineTotals(pilotSubset, dbName);
+
+            pilots.putAll(pilotSubset);
+         }
+
+         // Mark the thread as being read
+         SetCoolerMessage wdao = new SetCoolerMessage(con);
+         wdao.viewThread(thread.getID());
+
+         // Save all channels in the thread for the move combobox
+         if (ac.getCanUnlock() || ac.getCanLock()) {
+            ctx.setAttribute("channel", c, REQUEST);
+            ctx.setAttribute("channels", cdao.getChannels(airline, false), REQUEST);
+         }
+
+         // Save the thread, pilots and access controller in the request
+         ctx.setAttribute("thread", thread, REQUEST);
+         ctx.setAttribute("access", ac, REQUEST);
+         ctx.setAttribute("pilots", pilots, REQUEST);
+      } catch (DAOException de) {
+         throw new CommandException(de);
+      } finally {
+         ctx.release();
+      }
+
+      // Save scores choices
+      ctx.setAttribute("scores", SCORES, REQUEST);
+
+      // Forward to the JSP
+      CommandResult result = ctx.getResult();
+      result.setURL("/jsp/cooler/threadShow.jsp");
+      result.setSuccess(true);
+   }
 }
