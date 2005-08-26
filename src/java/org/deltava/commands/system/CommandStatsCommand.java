8ba54d4125d782729e2131e3b26901781ae0b029
Luke Kolin <luke@deltava.org>
2005-08-26 18:31:16 +0000

    Added command statistics
    git-svn-id: file:///home/svn/DVA2006@624 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/commands/system/CommandStatsCommand.java
@@ -0,0 +1,60 @@
+// Copyright 2005 Luke J. Kolin. All Rights Reserved.
+package org.deltava.commands.system;
+
+import java.util.*;
+import java.sql.Connection;
+
+import org.deltava.commands.*;
+
+import org.deltava.dao.GetSystemData;
+import org.deltava.dao.DAOException;
+
+import org.deltava.util.ComboUtils;
+
+/**
+ * A Web Site Command to display Web Site Command statistics.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class CommandStatsCommand extends AbstractCommand {
+   
+   private static final List sortOptions = ComboUtils.fromArray(new String[] {"Command Name", "Average Time",
+         "Database Time", "Maximum Time", "Frequency", "Success"}, new String[] {"NAME", "AVGT DESC", "BE DESC",
+         "MAXTOTAL DESC", "MAXTOTAL DESC", "TC DESC", "SC DESC"});
+
+   /**
+    * Executes the command.
+    * @param ctx the Command context
+    * @throws CommandException if an unhandled error occurs
+    */
+   public void execute(CommandContext ctx) throws CommandException {
+      
+      // Get the sort column
+      String sortBy = ctx.getParameter("sortType");
+      if (sortBy == null)
+         sortBy = "NAME";
+
+      try {
+         Connection con = ctx.getConnection();
+         
+         // Get the DAO and the statistics
+         GetSystemData dao = new GetSystemData(con);
+         ctx.setAttribute("stats", dao.getCommandStats(sortBy).values(), REQUEST);
+      } catch (DAOException de) {
+         throw new CommandException(de);
+      } finally {
+         ctx.release();
+      }
+      
+      // Save sort options
+      ctx.setAttribute("sortType", sortBy, REQUEST);
+      ctx.setAttribute("sortOptions", sortOptions, REQUEST);
+
+      // Forward to the JSP
+      CommandResult result = ctx.getResult();
+      result.setURL("/jsp/admin/cmdStats.jsp");
+      result.setSuccess(true);
+   }
+}
