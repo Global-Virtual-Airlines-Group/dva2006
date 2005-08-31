1ca528d6a38e1e245c4538ba5d3cc6257594a2a6
Luke Kolin <luke@deltava.org>
2005-08-31 05:28:21 +0000

    Fix error where it wouldn't write past dates
    git-svn-id: file:///home/svn/DVA2006@672 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/SetStatusUpdate.java
+++ src/java/org/deltava/dao/SetStatusUpdate.java
@@ -14,56 +14,56 @@ import org.deltava.beans.StatusUpdate;
  */
 
 public class SetStatusUpdate extends DAO {
-   
-   private long _lastCreatedOn = 0;
-   
-    /**
-     * Initializes the Data Access Object.
-     * @param c the JDBC connection to use
-     */
-    public SetStatusUpdate(Connection c) {
-        super(c);
-    }
 
-    /**
-     * Writes the Status Update log entry to the database.
-     * @param update the Status Update entry
-     * @throws DAOException if a JDBC error occurs
-     */
-    public void write(StatusUpdate update) throws DAOException {
-       
-       // Check that the CREATED date is unique
-       long cd = update.getCreatedOn().getTime(); 
-       if (cd <= _lastCreatedOn) {
-          cd = ++_lastCreatedOn;
-       } else {
-          _lastCreatedOn = cd;
-       }
-       
-        try {
-           // Prepare the statement and write
-            prepareStatementWithoutLimits("INSERT INTO STATUS_UPDATES (PILOT_ID, AUTHOR_ID, CREATED, "
-                  + "TYPE, REMARKS) VALUES (?, ?, ?, ?, ?)");
-            _ps.setInt(1, update.getID());
-            _ps.setInt(2, update.getAuthorID());
-            _ps.setLong(3, cd);
-            _ps.setInt(4, update.getType());
-            _ps.setString(5, update.getDescription());
-            executeUpdate(1);
-        } catch (SQLException se) {
-            throw new DAOException(se);
-        }
-    }
-    
-    /**
-     * Writes a number of Status Update entries to the database.
-     * @param updates a List of StatusUpdates
-     * @throws DAOException if a JDBC error occurs
-     */
-    public void write(List updates) throws DAOException {
-    	for (Iterator i = updates.iterator(); i.hasNext(); ) {
-    		StatusUpdate upd = (StatusUpdate) i.next();
-    		write(upd);
-    	}
-    }
+	private long _lastCreatedOn = 0;
+
+	/**
+	 * Initializes the Data Access Object.
+	 * @param c the JDBC connection to use
+	 */
+	public SetStatusUpdate(Connection c) {
+		super(c);
+	}
+
+	/**
+	 * Writes the Status Update log entry to the database.
+	 * @param update the Status Update entry
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public void write(StatusUpdate update) throws DAOException {
+
+		// Check that the CREATED date is unique - this needs to accept past dates
+		long cd = update.getCreatedOn().getTime();
+		if (cd == _lastCreatedOn) {
+			cd = ++_lastCreatedOn;
+		} else {
+			_lastCreatedOn = cd;
+		}
+
+		try {
+			// Prepare the statement and write
+			prepareStatementWithoutLimits("INSERT INTO STATUS_UPDATES (PILOT_ID, AUTHOR_ID, CREATED, "
+					+ "TYPE, REMARKS) VALUES (?, ?, ?, ?, ?)");
+			_ps.setInt(1, update.getID());
+			_ps.setInt(2, update.getAuthorID());
+			_ps.setLong(3, cd);
+			_ps.setInt(4, update.getType());
+			_ps.setString(5, update.getDescription());
+			executeUpdate(1);
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+
+	/**
+	 * Writes a number of Status Update entries to the database.
+	 * @param updates a List of StatusUpdates
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public void write(List updates) throws DAOException {
+		for (Iterator i = updates.iterator(); i.hasNext();) {
+			StatusUpdate upd = (StatusUpdate) i.next();
+			write(upd);
+		}
+	}
 }
