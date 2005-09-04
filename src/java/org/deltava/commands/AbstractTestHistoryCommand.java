ca720a80e3fd835d6e29070cd99b212f14343177
Luke Kolin <luke@deltava.org>
2005-09-04 22:40:15 +0000

    Added security violation logging support
    git-svn-id: file:///home/svn/DVA2006@711 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/AbstractTestHistoryCommand.java
+++ src/java/org/deltava/commands/AbstractTestHistoryCommand.java
@@ -1,6 +1,7 @@
 // Copyright 2005 Luke J. Kolin. All Rights Reserved.
 package org.deltava.commands;
 
+import java.util.List;
 import java.sql.Connection;
 
 import org.deltava.beans.Pilot;
@@ -10,8 +11,8 @@ import org.deltava.beans.testing.TestingHistoryHelper;
 import org.deltava.dao.*;
 
 /**
- * A class to support Web Site Commands use a {@link TestingHistoryHelper} object to determine
- * what examinations/transfers a Pilot is eligible for.
+ * A class to support Web Site Commands use a {@link TestingHistoryHelper} object to determine what
+ * examinations/transfers a Pilot is eligible for.
  * @author Luke
  * @version 1.0
  * @since 1.0
@@ -19,28 +20,26 @@ import org.deltava.dao.*;
 
 public abstract class AbstractTestHistoryCommand extends AbstractCommand {
 
-   protected TestingHistoryHelper _testHistory;
-   
-   /**
-    * Populates the Testing History Helper by calling the proper DAOs in the right order.
-    * @param p the Pilot bean
-    * @param c the JDBC connection to use
-    * @throws DAOException if a JDBC error occurs
-    */
-   protected final void initTestHistory(Pilot p, Connection c) throws DAOException {
-      
-      // Load the PIREP beans if they haven't been loaded already
-      if (!p.isPopulated()) {
-         GetFlightReports frdao = new GetFlightReports(c);
-         p.setFlights(frdao.getByPilot(p.getID(), null));
-      }
-      
-      // Get the Pilot's equipment program
-      GetEquipmentType eqdao = new GetEquipmentType(c);
-      EquipmentType eq = eqdao.get(p.getEquipmentType());
-      
-      // Get the Pilot's examinations and check rides, and initialize the helper
-      GetExam exdao = new GetExam(c);
-      _testHistory = new TestingHistoryHelper(p, eq, exdao.getExams(p.getID()));
-   }
+	protected TestingHistoryHelper _testHistory;
+
+	/**
+	 * Populates the Testing History Helper by calling the proper DAOs in the right order.
+	 * @param p the Pilot bean
+	 * @param c the JDBC connection to use
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	protected final void initTestHistory(Pilot p, Connection c) throws DAOException {
+
+		// Load the PIREP beans
+		GetFlightReports frdao = new GetFlightReports(c);
+		List pireps = frdao.getByPilot(p.getID(), null);
+
+		// Get the Pilot's equipment program
+		GetEquipmentType eqdao = new GetEquipmentType(c);
+		EquipmentType eq = eqdao.get(p.getEquipmentType());
+
+		// Get the Pilot's examinations and check rides, and initialize the helper
+		GetExam exdao = new GetExam(c);
+		_testHistory = new TestingHistoryHelper(p, eq, exdao.getExams(p.getID()), pireps);
+	}
 }
