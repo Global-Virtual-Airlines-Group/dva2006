331ee705477e4fcfd82342db535f837570ef0f6b
Luke Kolin <luke@deltava.org>
2005-09-04 16:34:11 +0000

    Build 17
    git-svn-id: file:///home/svn/DVA2006@709 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -12,8 +12,8 @@ public interface VersionInfo {
     public static final int MAJOR = 1;
     public static final int MINOR = 0;
     
-    public static final int BUILD = 15;
-    public static final int RELEASE_CANDIDATE = 1;
+    public static final int BUILD = 17;
+    //public static final int RELEASE_CANDIDATE = 2;
     
     public static final String TXT_COPYRIGHT = "Copyright (c) 2004, 2005 Luke J. Kolin. All Rights Reserved.";
     public static final String HTML_COPYRIGHT = "Copyright &copy; 2004, 2005 Luke J. Kolin. All Rights Reserved.";
