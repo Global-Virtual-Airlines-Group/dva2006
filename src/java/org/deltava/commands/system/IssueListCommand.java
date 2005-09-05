fc5a045b66268168e09e34551ec8df1817d1d389
Luke Kolin <luke@deltava.org>
2005-09-05 18:46:04 +0000

    Default to open issues
    git-svn-id: file:///home/svn/DVA2006@732 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/system/IssueListCommand.java
+++ src/java/org/deltava/commands/system/IssueListCommand.java
@@ -42,8 +42,8 @@ public class IssueListCommand extends AbstractViewCommand {
         
         // Get issue status
         int issueStatus = -1;
-        if (ctx.getParameter("status") != null) {
-            String statusName = ctx.getParameter("status");
+        if (ctx.getCmdParameter(Command.OPERATION, null) != null) {
+            String statusName = (String) ctx.getCmdParameter(Command.OPERATION, null);
             for (int x = 0; x < Issue.STATUS.length; x++) {
                 if (Issue.STATUS[x].equals(statusName)) {
                     issueStatus = x;
