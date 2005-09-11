c01977080a2cb8bf8f865eaf1c2a198f86aedc88
Luke Kolin <luke@deltava.org>
2005-09-11 04:36:08 +0000

    Filter out spaces -> plus
    git-svn-id: file:///home/svn/DVA2006@929 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/security/ValidateEmailCommand.java
+++ src/java/org/deltava/commands/security/ValidateEmailCommand.java
@@ -10,6 +10,10 @@ import org.deltava.beans.system.UserData;
 import org.deltava.commands.*;
 import org.deltava.dao.*;
 
+import org.deltava.mail.AddressValidationHelper;
+
+import org.deltava.util.StringUtils;
+
 /**
  * A Web Site Command to validate e-mail addresses.
  * @author Luke
@@ -25,12 +29,19 @@ public class ValidateEmailCommand extends AbstractCommand {
 	 * @throws CommandException if an error occurs
 	 */
 	public void execute(CommandContext ctx) throws CommandException {
+		
+		// Since the ID might not be hex-encoded get it a different way
+		int id = 0;
+		try {
+			id = StringUtils.parseHex((String) ctx.getCmdParameter(ID, "0"));
+		} catch (Exception e) {
+			id = 0;
+		}
 
 		Person p = null;
 		AddressValidation av = null;
 		try {
 			Connection con = ctx.getConnection();
-			int id = ctx.getID();
 
 			// Get the User Data
 			GetUserData usrdao = new GetUserData(con);
@@ -60,7 +71,7 @@ public class ValidateEmailCommand extends AbstractCommand {
 
 		// Make sure we're either anonymous or the same user
 		if ((ctx.isAuthenticated()) && (ctx.getUser().getID() != p.getID()))
-				throw securityException(ctx.getUser() + " attempting to Validate for " + p.getName());
+			throw securityException(ctx.getUser() + " attempting to Validate for " + p.getName());
 
 		// Get the command result
 		CommandResult result = ctx.getResult();
@@ -73,7 +84,8 @@ public class ValidateEmailCommand extends AbstractCommand {
 		}
 
 		// If the hashes don't match, then stop us
-		if (!av.getHash().equals(ctx.getParameter("code"))) {
+		String code = AddressValidationHelper.formatHash(ctx.getParameter("code"));
+		if (!av.getHash().equals(code)) {
 		   ctx.setAttribute("person", p, REQUEST);
 			result.setURL("/jsp/register/eMailInvalid.jsp");
 			result.setSuccess(true);
@@ -85,6 +97,9 @@ public class ValidateEmailCommand extends AbstractCommand {
 
 		try {
 			Connection con = ctx.getConnection();
+			
+			// Start the transaction
+			ctx.startTX();
 
 			// Update the user record
 			if (p instanceof Applicant) {
@@ -98,7 +113,11 @@ public class ValidateEmailCommand extends AbstractCommand {
 			// Clear the invalid e-mail entry
 			SetAddressValidation wavdao = new SetAddressValidation(con);
 			wavdao.delete(av.getID());
+			
+			// Commit the transaction
+			ctx.commitTX();
 		} catch (DAOException de) {
+			ctx.rollbackTX();
 			throw new CommandException(de);
 		} finally {
 			ctx.release();
