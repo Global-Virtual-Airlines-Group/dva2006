80b56bf26cca09973bb4a0cd469610daf88f2d78
Luke Kolin <luke@deltava.org>
2005-09-11 04:40:57 +0000

    Allow sorting of list
    git-svn-id: file:///home/svn/DVA2006@930 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/main/UserListCommand.java
+++ src/java/org/deltava/commands/main/UserListCommand.java
@@ -1,15 +1,19 @@
+// Copyright (c) 2005 Luke J. Kolin. All Rights Reserved.
 package org.deltava.commands.main;
+
+import java.util.*;
 
 import org.deltava.commands.*;
 
+import org.deltava.comparators.PilotComparator;
 import org.deltava.security.UserPool;
+import org.deltava.util.ComboUtils;
 
 /**
  * A web site command to list logged in users.
  * @author Luke
  * @version 1.0
  * @since 1.0
- * Copyright (c) 2005 Luke J. Kolin. All Rights Reserved.
  */
 
 public class UserListCommand extends AbstractCommand {
@@ -17,12 +21,24 @@ public class UserListCommand extends AbstractCommand {
     /**
      * Executes the command.
      * @param ctx the Command context
-     * @throws CommandException if an unhandled error occurs
+     * @throws CommandException if an error occurs
      */
     public void execute(CommandContext ctx) throws CommandException {
+    	
+    	// Get the sort type
+    	String sortOpt = ctx.getParameter("sortOpt");
+    	if (sortOpt == null)
+    		sortOpt = PilotComparator.TYPES[PilotComparator.PILOTCODE];
+    	
+    	// Initialize the comparator
+    	Set users = new TreeSet(new PilotComparator(sortOpt));
 
-        // Save the users in the request
-        ctx.setAttribute("pilots", UserPool.getPilots(), REQUEST);
+        // Get and save the users in the request
+    	users.addAll(UserPool.getPilots());
+        ctx.setAttribute("pilots", users, REQUEST);
+        
+        // Save combobox options
+        ctx.setAttribute("sortOptions", ComboUtils.fromArray(PilotComparator.TYPES), REQUEST);
 
         // Forward to the JSP
         CommandResult result = ctx.getResult();
