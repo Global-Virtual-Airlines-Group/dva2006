1ae79b5f7ba857dd5d6a4ad321b0c3534f7e6f47
Luke Kolin <luke@deltava.org>
2005-09-19 15:39:30 +0000

    Rationalize calls to UserDataMap
    git-svn-id: file:///home/svn/DVA2006@1039 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/UserDataMap.java
+++ src/java/org/deltava/beans/system/UserDataMap.java
@@ -115,6 +115,18 @@ public class UserDataMap implements java.io.Serializable, Map {
 
 		return results;
 	}
+	
+	/**
+	 * Utility method to query wether a table is a Pilot or Applicant table.
+	 * @param tableName the table name, in either TABLE or DB.TABLE format
+	 * @return TRUE if the table should be queried by a Pilot DAO, otherwise FALSE
+	 */
+	public static boolean isPilotTable(String tableName) {
+	   if ((tableName != null) && (tableName.indexOf('.') != -1))
+	      tableName = tableName.substring(tableName.indexOf('.') + 1);
+	   
+	   return "PILOTS".equals(tableName);
+	}
 
 	/**
 	 * Returns the size of the collection.
