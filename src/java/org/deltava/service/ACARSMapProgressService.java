fb5b5f9f26670fe8e57d35419c557c67fa2ad3e4
Luke Kolin <luke@deltava.org>
2005-09-22 03:28:23 +0000

    Format outputted XML as ISO-8859-1
    git-svn-id: file:///home/svn/DVA2006@1107 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/service/ACARSMapProgressService.java
+++ src/java/org/deltava/service/ACARSMapProgressService.java
@@ -64,7 +64,7 @@ public class ACARSMapProgressService extends WebDataService {
 		}
 		
 		// Dump the XML to the output stream
-		XMLOutputter xmlOut = new XMLOutputter(Format.getPrettyFormat());
+		XMLOutputter xmlOut = new XMLOutputter(Format.getPrettyFormat().setEncoding("ISO-8859-1"));
 		try {
 			ctx.getResponse().setContentType("text/xml");
 			ctx.println(xmlOut.outputString(doc));
