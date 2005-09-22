6593f199b50950020bb75c55be8193e4169bc812
Luke Kolin <luke@deltava.org>
2005-09-22 02:15:33 +0000

    Fix regression preventing single flight deletion
    git-svn-id: file:///home/svn/DVA2006@1106 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/acars/FlightInfoDeleteCommand.java
+++ src/java/org/deltava/commands/acars/FlightInfoDeleteCommand.java
@@ -20,64 +20,70 @@ import org.deltava.util.StringUtils;
 
 public class FlightInfoDeleteCommand extends AbstractCommand {
 
-   /**
-    * Executes the command.
-    * @param ctx the Command context
-    * @throws CommandException if an error occurs
-    */
-   public void execute(CommandContext ctx) throws CommandException {
-      
-      // Get the flight IDs
-      List flightIDs = Arrays.asList(ctx.getRequest().getParameterValues("flightID"));
-      
-      Set deletedIDs = new HashSet();
-      Set skippedIDs = new HashSet();
-      try {
-         Connection con = ctx.getConnection();
-         
-         // Get the DAOs
-         GetACARSLog dao = new GetACARSLog(con);
-         GetFlightReports frdao = new GetFlightReports(con);
-         SetACARSLog wdao = new SetACARSLog(con);
-         
-         // Start the transaction
-         ctx.startTX();
-         
-         // Delete the connection entries
-         for (Iterator i = flightIDs.iterator(); i.hasNext(); ) {
-            int id = Integer.parseInt((String) i.next());
+	/**
+	 * Executes the command.
+	 * @param ctx the Command context
+	 * @throws CommandException if an error occurs
+	 */
+	public void execute(CommandContext ctx) throws CommandException {
 
-            // Get the flight information entry - make sure the flight doesn't have a PIREP linked to it
-            FlightInfo info = dao.getInfo(id);
-            ACARSFlightReport afr = frdao.getACARS(id);
-            if (info == null) {
-               skippedIDs.add(StringUtils.formatHex(id));
-            } else if (afr != null) {
-               skippedIDs.add(StringUtils.formatHex(id));
-            } else {
-               wdao.deleteInfo(info.getID());
-               deletedIDs.add(StringUtils.formatHex(id));
-            }
-         }
-         
-         // Commit the transaction
-         ctx.commitTX();
-      } catch (DAOException de) {
-         ctx.rollbackTX();
-         throw new CommandException(de);
-      } finally {
-         ctx.release();
-      }
-      
-      // Set status attributes
-      ctx.setAttribute("infoDelete", Boolean.TRUE, REQUEST);
-      ctx.setAttribute("deletedIDs", deletedIDs, REQUEST);
-      ctx.setAttribute("skippedIDs", skippedIDs, REQUEST);
+		// Get the flight IDs
+		String IDs[] = ctx.getRequest().getParameterValues("flightID");
+		List flightIDs = new ArrayList();
+		if (IDs == null) {
+			flightIDs.add(ctx.getParameter("id"));
+		} else {
+			flightIDs.addAll(Arrays.asList(IDs));			
+		}
 
-      // Forward to the JSP
-      CommandResult result = ctx.getResult();
-      result.setType(CommandResult.REQREDIRECT);
-      result.setURL("/jsp/acars/logEntryDelete.jsp");
-      result.setSuccess(true);
-   }
+		Set deletedIDs = new HashSet();
+		Set skippedIDs = new HashSet();
+		try {
+			Connection con = ctx.getConnection();
+
+			// Get the DAOs
+			GetACARSLog dao = new GetACARSLog(con);
+			GetFlightReports frdao = new GetFlightReports(con);
+			SetACARSLog wdao = new SetACARSLog(con);
+
+			// Start the transaction
+			ctx.startTX();
+
+			// Delete the connection entries
+			for (Iterator i = flightIDs.iterator(); i.hasNext();) {
+				int id = Integer.parseInt((String) i.next());
+
+				// Get the flight information entry - make sure the flight doesn't have a PIREP linked to it
+				FlightInfo info = dao.getInfo(id);
+				ACARSFlightReport afr = frdao.getACARS(id);
+				if (info == null) {
+					skippedIDs.add(StringUtils.formatHex(id));
+				} else if (afr != null) {
+					skippedIDs.add(StringUtils.formatHex(id));
+				} else {
+					wdao.deleteInfo(info.getID());
+					deletedIDs.add(StringUtils.formatHex(id));
+				}
+			}
+
+			// Commit the transaction
+			ctx.commitTX();
+		} catch (DAOException de) {
+			ctx.rollbackTX();
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
+
+		// Set status attributes
+		ctx.setAttribute("infoDelete", Boolean.TRUE, REQUEST);
+		ctx.setAttribute("deletedIDs", deletedIDs, REQUEST);
+		ctx.setAttribute("skippedIDs", skippedIDs, REQUEST);
+
+		// Forward to the JSP
+		CommandResult result = ctx.getResult();
+		result.setType(CommandResult.REQREDIRECT);
+		result.setURL("/jsp/acars/logEntryDelete.jsp");
+		result.setSuccess(true);
+	}
 }
