575c9267732bd6fd554e4c67f7e2fe65bf2ca21e
Luke Kolin <luke@deltava.org>
2005-09-22 21:30:21 +0000

    Add new scripts
    git-svn-id: file:///home/svn/DVA2006@1114 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ scripts/unix/site_down.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Script to mark the web site as down.
+# (c) 2005 Delta Virtual Airlines.
+#
+
+mv /usr/local/www/deltava.org/www/index.bad.html /usr/local/www/deltava.org/www/index.html
