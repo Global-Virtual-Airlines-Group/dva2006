f64752564af9dd4af0791f8beba29d11510b151b
Luke Kolin <luke@deltava.org>
2005-09-22 21:42:15 +0000

    Support Pilot email validation/invalidation
    git-svn-id: file:///home/svn/DVA2006@1119 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/commands/security/InvalidateEmailCommand.java
@@ -0,0 +1,78 @@
+// Copyright 2005 Luke J. Kolin. All Rights Reserved.
+package org.deltava.commands.security;
+
+import java.sql.Connection;
+
+import org.deltava.beans.Pilot;
+import org.deltava.beans.EMailAddress;
+import org.deltava.beans.system.AddressValidation;
+
+import org.deltava.commands.*;
+import org.deltava.dao.*;
+
+/**
+ * A Web Site Command to invalidate a user's e-mail address.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class InvalidateEmailCommand extends AbstractCommand {
+
+   /**
+    * Execute the command.
+    * @param ctx the Command context
+    * @throws CommandException if an unhandled error occrurs.
+    */
+   public void execute(CommandContext ctx) throws CommandException {
+
+      try {
+         Connection con = ctx.getConnection();
+         
+         // Get the Pilot
+         GetPilot dao = new GetPilot(con);
+         Pilot p = dao.get(ctx.getID());
+         if (p == null)
+            throw new CommandException("Invalid Pilot - " + ctx.getID());
+         
+         // Check if the address has already been invalidated
+         GetAddressValidation avdao = new GetAddressValidation(con);
+         AddressValidation addrValid = avdao.get(p.getID());
+         if (addrValid == null) {
+            // Invalidate the e-mail address and create the validation entry
+            p.setEmail(EMailAddress.INVALID_ADDR);
+   			addrValid = new AddressValidation(p.getID(), EMailAddress.INVALID_ADDR);
+            addrValid.setHash(EMailAddress.INVALID_ADDR);
+            
+            // Start the transaction
+            ctx.startTX();
+            
+            // Write the e-mail address
+            SetPilot pwdao = new SetPilot(con);
+            pwdao.write(p);
+            
+            // Write the address validation entry
+            SetAddressValidation avwdao = new SetAddressValidation(con);
+            avwdao.write(addrValid);
+            
+            // Commit the transaction
+            ctx.commitTX();
+         } else {
+            ctx.setAttribute("alreadyInvalid", Boolean.TRUE, REQUEST);
+         }
+         
+         // Save the pilot in the request
+         ctx.setAttribute("pilot", p, REQUEST);
+      } catch (DAOException de) {
+         ctx.rollbackTX();
+         throw new CommandException(de);
+      } finally {
+         ctx.release();
+      }
+
+      // Forward to the JSP
+      CommandResult result = ctx.getResult();
+      result.setURL("/jsp/register/emailInvalidate.jsp");
+      result.setSuccess(true);
+   }
+}
