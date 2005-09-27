da19acba775f285d972083b3f99a998e58228dcf
Luke Kolin <luke@deltava.org>
2005-09-27 21:00:08 +0000

    Add check ride scripts
    git-svn-id: file:///home/svn/DVA2006@1182 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ jsp/testing/crScript.jsp
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<%@ page session="false" %>
+<%@ page isELIgnored="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="/WEB-INF/dva_content.tld" prefix="content" %>
+<%@ taglib uri="/WEB-INF/dva_html.tld" prefix="el" %>
+<%@ taglib uri="/WEB-INF/dva_format.tld" prefix="fmt" %>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Check Ride Script</title>
+<content:css name="main" browserSpecific="true" />
+<content:css name="form" />
+<content:pics />
+<content:js name="common" />
+<script language="JavaScript" type="text/javascript">
+function validate(form)
+{
+if (!checkSubmit()) return false;
+if (!validateCombo(form.eqType, 'Aircraft Type')) return false;
+if (!validateCombo(form.programType, 'Equipment Program')) return false;
+if (!validateText(form.msgText, 15, 'Check Ride content')) return false;
+
+setSubmit();
+disableButton('SaveButton');
+return true;
+}
+</script>
+</head>
+<content:copyright visible="false" />
+<body>
+<%@ include file="/jsp/main/header.jsp" %> 
+<%@ include file="/jsp/main/sideMenu.jsp" %>
+<content:sysdata var="actypes" name="eqtypes" />
+
+<!-- Main Body Frame -->
+<div id="main">
+<el:form action="crscript.do" op="save" method="post" validate="return validate(this)">
+<el:table className="form" pad="default" space="default">
+<!-- Title Bar -->
+<tr class="title caps">
+ <td colspan="2">CHECK RIDE SCRIPT</td>
+</tr>
+<tr>
+ <td class="label">Aircraft Type</td>
+ <td class="data"><el:combo name="eqType" idx="*" size="1" options="${empty script ? actypes : script.equipmentType}" firstEntry="-" value="${script.equipmentType}" /></td>
+</tr>
+<tr>
+ <td class="label">Equipment Program</td>
+ <td class="data"><el:combo name="programType" idx="*" size="1" options="${eqTypes}" firstEntry="-" value="${script.program}" /></td>
+</tr>
+<tr>
+ <td class="label" valign="top">Script Text</td>
+ <td class="data"><el:textbox name="msgText" idx="*" width="120" height="10">${script.description}</el:textbox></td>
+</tr>
+</el:table>
+
+<!-- Button bar -->
+<el:table className="bar" pad="default" space="default">
+<tr>
+ <td><el:cmdbutton url="crscript" op="save" label="SAVE CHECK RIDE SCRIPT" /></td>
+</tr>
+</el:table>
+</el:form>
+<br />
+<content:copyright />
+</div>
+</body>
+</html>
---
+++ src/java/org/deltava/commands/testing/CheckRideScriptCommand.java
@@ -0,0 +1,127 @@
+// Copyright 2005 Luke J. Kolin. All Rights Reserved.
+package org.deltava.commands.testing;
+
+import java.sql.Connection;
+
+import org.deltava.beans.testing.CheckRideScript;
+
+import org.deltava.commands.*;
+import org.deltava.dao.*;
+
+import org.deltava.security.command.CheckrideScriptAccessControl;
+
+/**
+ * A Web Site Command to update Check Ride scripts.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class CheckRideScriptCommand extends AbstractFormCommand {
+
+   /**
+    * Callback method called when saving the script.
+    * @param ctx the Command context
+    * @throws CommandException if an error occurs
+    */
+   protected void execSave(CommandContext ctx) throws CommandException {
+      try {
+         Connection con = ctx.getConnection();
+         
+         // Get the DAO and the existing script
+         GetExamProfiles dao = new GetExamProfiles(con);
+         CheckRideScript sc = dao.getScript(ctx.getParameter("eqType"));
+         if (sc == null)
+            sc = new CheckRideScript(ctx.getParameter("eqType"));
+         
+         // Load the program and description
+         sc.setProgram(ctx.getParameter("programType"));
+         sc.setDescription(ctx.getParameter("msgText"));
+         
+         // Calculate our access
+         CheckrideScriptAccessControl access = new CheckrideScriptAccessControl(ctx, sc);
+         access.validate();
+         if (!access.getCanEdit())
+            throw securityException("Cannot save Check Ride script");
+         
+         // Get the DAO and update the script
+         SetExamProfile wdao = new SetExamProfile(con);
+         wdao.write(sc);
+         
+         // Save status attribute
+         ctx.setAttribute("script", sc, REQUEST);
+      } catch (DAOException de) {
+         throw new CommandException(de);
+      } finally {
+         ctx.release();
+      }
+
+      // Forward to the JSP
+      CommandResult result = ctx.getResult();
+      result.setType(CommandResult.REQREDIRECT);
+      result.setURL("/jsp/testing/profileUpdate.jsp");
+      result.setSuccess(true);
+   }
+
+   /**
+    * Callback method called when editing the script.
+    * @param ctx the Command context
+    * @throws CommandException if an error occurs
+    */
+   protected void execEdit(CommandContext ctx) throws CommandException {
+      
+      String id = (String) ctx.getCmdParameter(ID, null);
+      try {
+         Connection con = ctx.getConnection();
+         
+         // Get the DAO and the script
+         GetExamProfiles dao = new GetExamProfiles(con);
+         if (id != null) {
+            CheckRideScript sc = dao.getScript(id);
+            if (sc == null)
+               throw new CommandException("Invalid Check Ride script - " + id);
+         
+            // Calculate our access
+            CheckrideScriptAccessControl access = new CheckrideScriptAccessControl(ctx, sc);
+         	access.validate();
+         	if (!access.getCanEdit())
+         	   throw securityException("Cannot edit Check Ride script");
+         	
+            // Save in the request
+            ctx.setAttribute("script", sc, REQUEST);
+            ctx.setAttribute("access", access, REQUEST);
+         } else {
+            // Calculate our access
+            CheckrideScriptAccessControl access = new CheckrideScriptAccessControl(ctx, null);
+         	access.validate();
+         	if (!access.getCanCreate())
+         	   throw securityException("Cannot create Check Ride script");
+         	
+         	// Save in request
+         	ctx.setAttribute("access", access, REQUEST);
+         }
+         
+         // Load equipment types
+         GetEquipmentType eqdao = new GetEquipmentType(con);
+         ctx.setAttribute("eqTypes", eqdao.getAll(), REQUEST);
+      } catch (DAOException de) {
+         throw new CommandException(de);
+      } finally {
+         ctx.release();
+      }
+
+      // Forward to the JSP
+      CommandResult result = ctx.getResult();
+      result.setURL("/jsp/testing/crScript.jsp");
+      result.setSuccess(true);
+   }
+
+   /**
+    * Callback method called when reading the script. <i>NOT IMPLEMENTED</i>
+    * @param ctx the Command context
+    * @throws UnsupportedOperationException
+    */
+   protected void execRead(CommandContext ctx) throws CommandException {
+      throw new UnsupportedOperationException();
+   }
+}
