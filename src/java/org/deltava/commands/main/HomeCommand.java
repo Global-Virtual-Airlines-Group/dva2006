b583d63c725d278740536881012b5b722eae12bd
Luke Kolin <luke@deltava.org>
2005-09-30 20:20:47 +0000

    Redirect if invalid hostname
    git-svn-id: file:///home/svn/DVA2006@1205 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/main/HomeCommand.java
+++ src/java/org/deltava/commands/main/HomeCommand.java
@@ -9,6 +9,8 @@ import org.deltava.beans.Notice;
 import org.deltava.commands.*;
 import org.deltava.dao.*;
 
+import org.deltava.util.system.SystemData;
+
 /**
  * A Web Site Command to display the home page.
  * @author Luke
@@ -18,50 +20,61 @@ import org.deltava.dao.*;
 
 public class HomeCommand extends AbstractCommand {
 
-    /**
-     * Executes the command.
-     * @param ctx the Command context
-     * @throws CommandException if an error (typically database) occurs
-     */
-    public void execute(CommandContext ctx) throws CommandException {
-        try {
-            Connection con = ctx.getConnection();
-            
-            // Get the system news and save in the request
-            GetNews nwdao = new GetNews(con);
-            nwdao.setQueryMax(5);
-            ctx.setAttribute("latestNews", nwdao.getNews(), REQUEST);
-            
-            // Get the newest pilots and save in the request
-            GetPilot daoP = new GetPilot(con);
-            daoP.setQueryMax(10);
-            ctx.setAttribute("latestPilots", daoP.getNewestPilots(), REQUEST);
-            
-            // Get the HTTP statistics and save in the request
-            GetSystemData sysdao = new GetSystemData(con);
-            ctx.setAttribute("httpStats", sysdao.getHTTPTotals(), REQUEST);
-            
-            // Get new/active NOTAMs since last login
-            if (ctx.isAuthenticated()) {
-            	Person usr = ctx.getUser();
-            	Collection notams = nwdao.getActiveNOTAMs();
-            	for (Iterator i = notams.iterator(); i.hasNext(); ) {
-            		Notice ntm = (Notice) i.next();
-            		if (ntm.getDate().before(usr.getLastLogin()))
-            			i.remove();
-            	}
-            	
-            	ctx.setAttribute("notams", notams, REQUEST);
+   /**
+    * Executes the command.
+    * @param ctx the Command context
+    * @throws CommandException if an error (typically database) occurs
+    */
+   public void execute(CommandContext ctx) throws CommandException {
+
+      // Get Command result
+      CommandResult result = ctx.getResult();
+      
+      // Check that the hostname is correct
+      if (!ctx.getRequest().getServerName().equals(SystemData.get("airline.url"))) {
+         result.setType(CommandResult.REDIRECT);
+         result.setURL(ctx.getRequest().getProtocol() + "://" + SystemData.get("airline.url") + "/");
+         result.setSuccess(true);
+         return;
+      }
+
+      try {
+         Connection con = ctx.getConnection();
+
+         // Get the system news and save in the request
+         GetNews nwdao = new GetNews(con);
+         nwdao.setQueryMax(5);
+         ctx.setAttribute("latestNews", nwdao.getNews(), REQUEST);
+
+         // Get the newest pilots and save in the request
+         GetPilot daoP = new GetPilot(con);
+         daoP.setQueryMax(10);
+         ctx.setAttribute("latestPilots", daoP.getNewestPilots(), REQUEST);
+
+         // Get the HTTP statistics and save in the request
+         GetSystemData sysdao = new GetSystemData(con);
+         ctx.setAttribute("httpStats", sysdao.getHTTPTotals(), REQUEST);
+
+         // Get new/active NOTAMs since last login
+         if (ctx.isAuthenticated()) {
+            Person usr = ctx.getUser();
+            Collection notams = nwdao.getActiveNOTAMs();
+            for (Iterator i = notams.iterator(); i.hasNext();) {
+               Notice ntm = (Notice) i.next();
+               if (ntm.getDate().before(usr.getLastLogin()))
+                  i.remove();
             }
-        } catch (DAOException de) {
-            throw new CommandException(de);
-        } finally {
-            ctx.release();
-        }
-        
-        // Redirect to the home page
-        CommandResult result = ctx.getResult();
-        result.setURL("/jsp/main/home.jsp");
-        result.setSuccess(true);
-    }
+
+            ctx.setAttribute("notams", notams, REQUEST);
+         }
+      } catch (DAOException de) {
+         throw new CommandException(de);
+      } finally {
+         ctx.release();
+      }
+
+      // Redirect to the home page
+      result.setURL("/jsp/main/home.jsp");
+      result.setSuccess(true);
+   }
 }
