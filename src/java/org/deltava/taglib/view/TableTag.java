15369e84e938b01593da6686690562da4efe6cb0
Luke Kolin <luke@deltava.org>
2005-10-10 14:50:04 +0000

    Make size a numeric parameter
    git-svn-id: file:///home/svn/DVA2006@1299 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/view/TableTag.java
+++ src/java/org/deltava/taglib/view/TableTag.java
@@ -1,4 +1,7 @@
+// Copyright (c) 2005 Luke J. Kolin. All Rights Reserved.
 package org.deltava.taglib.view;
+
+import javax.servlet.jsp.JspException;
 
 import org.deltava.util.system.SystemData;
 
@@ -11,31 +14,70 @@ import org.deltava.util.system.SystemData;
 
 public class TableTag extends org.deltava.taglib.html.TableTag {
 
-    private String _viewCommandName;
-    private int _viewSize;
-    
-    public TableTag() {
-        super();
-        _viewSize = SystemData.getInt("html.table.viewSize");
-    }
+   private String _viewCommandName;
+   private int _viewSize;
 
-    public void setCmd(String name) {
-        _viewCommandName = name.toLowerCase(); 
-    }
-    
-    public void setSize(String rawValue) {
-        try {
-            int tmpValue = Integer.parseInt(rawValue);
-            if (tmpValue > 0)
-                _viewSize = tmpValue;
-        } catch (NumberFormatException nfe) { }
-    }
-    
-    String getCmd() {
-        return _viewCommandName;
-    }
-    
-    int size() {
-        return _viewSize;
-    }
+   /**
+    * Creates a new View Table tag.
+    */
+   public TableTag() {
+      super();
+      release();
+   }
+
+   /**
+    * Sets the Web Site Command to execute when scrolling through the view.
+    * @param name the command ID
+    * @see TableTag#getCmd()
+    */
+   public void setCmd(String name) {
+      _viewCommandName = name.toLowerCase();
+   }
+
+   /**
+    * Sets the number of view rows to display.
+    * @param size the number of rows
+    * @see TableTag#size()
+    */
+   public void setSize(int size) {
+      if (size > 0)
+         _viewSize = size;
+   }
+   
+   /**
+    * Renders the tag to the JSP output stream. This ensures that state is released properly.
+    * @return EVAL_PAGE
+    * @throws JspException if an error occurs.
+    */
+   public int doEndTag() throws JspException {
+      int result = super.doEndTag();
+      release();
+      return result;
+   }
+   
+   /**
+    * Releases the tag's state variables.
+    */
+   public void release() {
+      super.release();
+      _viewSize = SystemData.getInt("html.table.viewSize");
+   }
+
+   /**
+    * Returns the view command ID (for unit tests).
+    * @return the command ID
+    * @see TableTag#setCmd(String)
+    */
+   protected String getCmd() {
+      return _viewCommandName;
+   }
+
+   /**
+    * Returns the view scroll window size (for unit tests)
+    * @return the number of rows
+    * @see TableTag#setSize(int)
+    */
+   protected int size() {
+      return _viewSize;
+   }
 }
