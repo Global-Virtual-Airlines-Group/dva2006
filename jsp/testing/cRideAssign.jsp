475652db276652ae59b64ca03849bb49beea8810
Luke Kolin <luke@deltava.org>
2005-10-12 21:23:15 +0000

    Add naked check rides
    git-svn-id: file:///home/svn/DVA2006@1340 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ jsp/testing/cRideAssign.jsp
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<%@ page session="false" %>
+<%@ page isELIgnored="false" %>
+<%@ taglib uri="/WEB-INF/dva_content.tld" prefix="content" %>
+<%@ taglib uri="/WEB-INF/dva_html.tld" prefix="el" %>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Assign Check Ride for ${pilot.name}</title>
+<content:css name="main" browserSpecific="true" />
+<content:css name="form" />
+<content:pics />
+<content:js name="common" />
+<script language="JavaScript" type="text/javascript">
+function validate(form)
+{
+if (!checkSubmit()) return false;
+if (!validateCombo(form.crType, 'Aircraft Type')) return false;
+if (!validateCombo(form.eqType, 'Equimpment Program')) return false;
+if (!validateText(form.comments, 25, 'Check Ride Comments')) return false;
+
+setSubmit();
+disableButton('ProfileButton');
+disableButton('AssignButton');
+return true;
+}
+</script>
+</head>
+<content:copyright visible="false" />
+<body>
+<%@ include file="/jsp/main/header.jsp" %> 
+<%@ include file="/jsp/main/sideMenu.jsp" %>
+<content:sysdata var="actypes" name="eqtypes" />
+
+<!-- Main Body Frame -->
+<div id="main">
+<el:form action="nakedassign.do" method="post" linkID="0x${pilot.ID}" validate="return validate(this)">
+<el:table className="form" pad="default" space="default">
+<tr class="title caps">
+ <td colspan="2">CHECK RIDE - ${pilot.name}</td>
+</tr>
+<tr>
+ <td class="label">Equipment Program</td>
+ <td class="data"><el:combo name="eqType" idx="*" size="1" firstEntry="-" options="${eqTypes}" value="${param.eqType}" /></td>
+</tr>
+<tr>
+ <td class="label">Aircraft Type</td>
+ <td class="data"><el:combo name="crType" idx="*" size="1" firstEntry="-" options="${empty eqType ? actypes : eqType.primaryRatings}" value="${param.crType}" /></td>
+</tr>
+<tr>
+ <td class="label">Comments</td>
+ <td class="data"><el:textbox name="comments" idx="*" width="120" height="4"></el:textbox></td>
+</tr>
+<tr>
+ <td class="label">&nbsp;</td>
+ <td class="data"><el:box name="useScript" idx="*" value="true" label="Append Check Ride script to comments" /></td>
+</tr>
+</el:table>
+
+<!-- Button Bar -->
+<el:table className="bar" space="default" pad="default">
+<tr>
+ <td><el:cmdbutton ID="ProfileButton" url="profile" linkID="0x${pilot.ID}" label="VIEW PROFILE" /> 
+<el:button ID="AssignButton" type="submit" className="BUTTON" label="ASSIGN CHECK RIDE" /></td>
+</tr>
+</el:table>
+</el:form>
+<br />
+<content:copyright />
+</div>
+</body>
+</html>
---
+++ src/java/org/deltava/commands/testing/NakedCheckRideCommand.java
@@ -0,0 +1,153 @@
+// Copyright 2005 Luke J. Kolin. All Rights Reserved.
+package org.deltava.commands.testing;
+
+import java.util.*;
+import java.sql.Connection;
+
+import org.deltava.beans.*;
+import org.deltava.beans.testing.*;
+
+import org.deltava.commands.*;
+import org.deltava.dao.*;
+import org.deltava.mail.*;
+
+/**
+ * A Web Site Command to assign Check Rides not linked to a Transfer Request.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class NakedCheckRideCommand extends AbstractCommand {
+
+   /**
+    * Executes the command.
+    * @param ctx the Command context
+    * @throws CommandException if an unhandled error occurs
+    */
+   public void execute(CommandContext ctx) throws CommandException {
+
+		// Create the messaging context
+		MessageContext mctxt = new MessageContext();
+		mctxt.addData("user", ctx.getUser());
+		
+		// Get command result
+		CommandResult result = ctx.getResult();
+		
+		Pilot p = null;
+		try {
+		   Connection con = ctx.getConnection();
+		   
+		   // Get the Pilot we are assigning the Check Ride to
+		   GetPilot pdao = new GetPilot(con);
+		   p = pdao.get(ctx.getID());
+		   if (p == null)
+		      throw new CommandException("Invalid Pilot ID - " + ctx.getID());
+		   
+		   // Check if we already have a pending checkride
+		   boolean hasRide = false;
+		   GetExam exdao = new GetExam(con);
+		   Collection cRides = exdao.getCheckRides(p.getID());
+		   for (Iterator i = cRides.iterator(); i.hasNext() && !hasRide; ) {
+		      CheckRide cr = (CheckRide) i.next();
+		      if (cr.getStatus() != Test.SCORED) {
+		         hasRide = true;
+		         ctx.setAttribute("checkRide", cr, REQUEST);
+		      }
+		   }
+
+		   // If we already have a pending checkride, then send back an error
+		   if (hasRide) {
+		      ctx.release();
+		      ctx.setAttribute("isRideAlreadyAssigned", Boolean.TRUE, REQUEST);
+		      
+		      // Forward to the JSP
+		      result.setURL("/jsp/testing/cRideUpdate.jsp");
+		      result.setSuccess(true);
+		      return;
+		   }
+		   
+		   // Get all equipment types
+		   GetEquipmentType eqdao = new GetEquipmentType(con);
+		   ctx.setAttribute("eqTypes", eqdao.getActive(), REQUEST);
+		   
+		   // If we are not doing a POST, then redirect
+		   if (ctx.getParameter("eqType") == null) {
+		      ctx.release();
+		      
+		      // Forward to the JSP
+		      result.setURL("/jsp/testing/cRideAssign.jsp");
+		      result.setSuccess(true);
+		      return;
+		   }
+		   
+		   // Get the equipment type for the Check Ride
+		   EquipmentType eqType = eqdao.get(ctx.getParameter("eqType"));
+		   if (eqType == null)
+		      throw new CommandException("Invalid Equipment Program - " + ctx.getParameter("eqType"));
+		   
+		   // Make sure the assigned type is part of the primary ratings
+		   String acType = ctx.getParameter("crType");
+		   if (!eqType.getPrimaryRatings().contains(acType)) {
+		      ctx.release();
+		      ctx.setAttribute("eqType", eqType, REQUEST);
+		      
+		      // Forward to the JSP
+		      result.setURL("/jsp/testing/cRideAssign.jsp");
+		      result.setSuccess(true);
+		      return;
+		   }
+
+		   // Check if we are using the script
+			String comments = ctx.getParameter("comments");
+			boolean useScript = Boolean.valueOf(ctx.getParameter("useScript")).booleanValue();
+			if (useScript) {
+			   GetExamProfiles epdao = new GetExamProfiles(con);
+			   CheckRideScript sc = epdao.getScript(ctx.getParameter("crType"));
+			   if (sc != null)
+			      comments = comments + "\n\n" + sc.getDescription();
+			}
+		   
+		   // Create the checkride bean
+		   CheckRide cr = new CheckRide(acType + " Check Ride");
+			cr.setDate(new java.util.Date());
+			cr.setPilotID(ctx.getID());
+			cr.setScorerID(ctx.getUser().getID());
+			cr.setStatus(Test.NEW);
+			cr.setStage(eqType.getStage());
+			cr.setComments(comments);
+			
+			// Get the message template
+			GetMessageTemplate mtdao = new GetMessageTemplate(con);
+			mctxt.setTemplate(mtdao.get("RIDEASSIGN"));
+			mctxt.addData("pilot", p);
+			mctxt.addData("eqType", eqType);
+			mctxt.addData("checkRide", cr);
+
+			// Write the checkride to the database
+			SetExam exwdao = new SetExam(con);
+			exwdao.write(cr);
+
+			// Save the checkride in the request
+			ctx.setAttribute("pilot", p, REQUEST);
+			ctx.setAttribute("checkRide", cr, REQUEST);
+		} catch (DAOException de) {
+		   throw new CommandException(de);
+		} finally {
+		   ctx.release();
+		}
+		
+		// Send notification message
+		Mailer mailer = new Mailer(ctx.getUser());
+		mailer.setContext(mctxt);
+		mailer.send(p);
+
+		// Update status for the JSP
+		ctx.setAttribute("isAssign", Boolean.TRUE, REQUEST);
+
+		// Forward to the JSP
+		result.setType(CommandResult.REQREDIRECT);
+		result.setURL("/jsp/testing/cRideUpdate.jsp");
+		result.setSuccess(true);
+   }
+}
