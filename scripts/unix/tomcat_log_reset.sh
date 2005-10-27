89db3bf31e65f6dd2c956e7c0b637a1b19dc9eb6
Luke Kolin <luke@deltava.org>
2005-10-27 20:05:16 +0000

    Fix scripts and uploading
    git-svn-id: file:///home/svn/DVA2006@1522 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- scripts/unix/tomcat_log_reset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Delta Virtual Airlines Tomcat log reset script
-# (c) 2005 Luke J. Kolin. All Rights Reserved.
-
-rm /usr/local/jakarta-tomcat5.0/logs/catalina.out
-touch /usr/local/jakarta-tomcat5.0/logs/catalina.out
-chown 80 /usr/local/jakarta-tomcat5.0/logs/catalina.out
