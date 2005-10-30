7b97f945fc6163ed5480ebd111ff13565975357a
Luke Kolin <luke@deltava.org>
2005-10-30 05:06:58 +0000

    Log Google Maps API usage
    git-svn-id: file:///home/svn/DVA2006@1528 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/googlemap/InsertGoogleAPITag.java
+++ src/java/org/deltava/taglib/googlemap/InsertGoogleAPITag.java
@@ -16,47 +16,61 @@ import org.deltava.util.system.SystemData;
  */
 
 public class InsertGoogleAPITag extends TagSupport {
-   
 
-   private int _apiVersion = 1;
-   
-   /**
-    * Sets the Google API version to pull down.
-    * @param ver the API version
-    */
-   public void setVersion(int ver) {
-      _apiVersion = ver;
-   }
-   
-   /**
-    * Renders the JSP tag.
-    * @return TagSupport.EVAL_PAGE
-    * @throws JspException if no Google Maps API key defined
-    */
-   public int doEndTag() throws JspException {
-      
-      // Get the API key
-      String apiKey = SystemData.get("security.key.googleMaps");
-      if (apiKey == null)
-         throw new JspException("Google Maps API key not defined");
-      
-      // Check if we've already included the content
-      if (ContentHelper.containsContent(pageContext, "JS", GoogleMapEntryTag.API_JS_NAME))
-         return EVAL_PAGE;
-      
-      JspWriter out = pageContext.getOut();
-      try {
-			out.print("<script language=\"JavaScript\" type=\"text/javascript\" src=\"http://maps.google.com/maps?file=api&amp;v=");
+	public static final String USAGE_ATTR_NAME = "$googleMapUsage$";
+	
+	private int _usageCount;
+	private int _apiVersion = 1;
+
+	/**
+	 * Sets the Google API version to pull down.
+	 * @param ver the API version
+	 */
+	public void setVersion(int ver) {
+		_apiVersion = ver;
+	}
+
+	/**
+	 * Increments and adds the usage count to the application context.
+	 * @return TagSupport.SKIP_BODY
+	 * @throws JspException if an error occurs
+	 */
+	public int doStartTag() throws JspException {
+		_usageCount++;
+		pageContext.setAttribute(USAGE_ATTR_NAME, new Integer(_usageCount), PageContext.APPLICATION_SCOPE);
+		return super.doStartTag();
+	}
+	
+	/**
+	 * Renders the JSP tag.
+	 * @return TagSupport.EVAL_PAGE
+	 * @throws JspException if no Google Maps API key defined
+	 */
+	public int doEndTag() throws JspException {
+
+		// Get the API key
+		String apiKey = SystemData.get("security.key.googleMaps");
+		if (apiKey == null)
+			throw new JspException("Google Maps API key not defined");
+
+		// Check if we've already included the content
+		if (ContentHelper.containsContent(pageContext, "JS", GoogleMapEntryTag.API_JS_NAME))
+			return EVAL_PAGE;
+
+		JspWriter out = pageContext.getOut();
+		try {
+			out
+					.print("<script language=\"JavaScript\" type=\"text/javascript\" src=\"http://maps.google.com/maps?file=api&amp;v=");
 			out.print(String.valueOf(_apiVersion));
 			out.print("&amp;key=");
 			out.print(apiKey);
 			out.print("\"></script>");
-      } catch (Exception e) {
-         throw new JspException(e);
-      }
-      
-      // Mark the content as added and return
+		} catch (Exception e) {
+			throw new JspException(e);
+		}
+
+		// Mark the content as added and return
 		ContentHelper.addContent(pageContext, "JS", GoogleMapEntryTag.API_JS_NAME);
 		return EVAL_PAGE;
-   }
+	}
 }
