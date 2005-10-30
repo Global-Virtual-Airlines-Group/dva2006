de578382189a1de90241562200a300fd2b32ba51
Luke Kolin <luke@deltava.org>
2005-10-30 05:09:13 +0000

    Add 3D GeospaceLocation
    git-svn-id: file:///home/svn/DVA2006@1530 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/GeospaceLocation.java
@@ -0,0 +1,14 @@
+// Copyright (c) 2005 Delta Virtual Airlines. All Rights Reserved.
+package org.deltava.beans;
+
+/**
+ * An interface to define three-dimensional GeoLocations.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public interface GeospaceLocation extends GeoLocation {
+
+	public int getAltitude();
+}
