a796ea565c36a3721b805300ce900b437da432d3
Luke Kolin <luke@deltava.org>
2005-10-31 04:24:39 +0000

    Fix SQL
    git-svn-id: file:///home/svn/DVA2006@1553 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetACARSLog.java
+++ src/java/org/deltava/dao/GetACARSLog.java
@@ -82,8 +82,8 @@ public class GetACARSLog extends GetACARSData {
          prepareStatement("SELECT C.ID, C.PILOT_ID, C.DATE, INET_NTOA(C.REMOTE_ADDR), C.REMOTE_HOST, "
                + "C.CLIENT_BUILD, COUNT(DISTINCT M.ID) AS MC, COUNT(DISTINCT F.ID) AS FC, COUNT(P.CON_ID) AS PC "
                + "FROM acars.CONS C LEFT JOIN acars.FLIGHTS F ON (C.ID=F.CON_ID) LEFT JOIN acars.MESSAGES M ON "
-               + "(C.ID=M.CON_ID) LEFT JOIN acars.POSITIONS P ON (C.ID=P.CON_ID) GROUP BY C.ID WHERE "
-               + "(C.DATE < DATE_SUB(NOW(), INTERVAL ? HOUR)) HAVING (MC=0) AND (FC=0) AND (PC=0) "
+               + "(C.ID=M.CON_ID) LEFT JOIN acars.POSITIONS P ON (C.ID=P.CON_ID)  WHERE (C.DATE < "
+               + "DATE_SUB(NOW(), INTERVAL ? HOUR)) GROUP BY C.ID HAVING (MC=0) AND (FC=0) AND (PC=0) "
                + "ORDER BY C.DATE");
          _ps.setInt(1, cutoff);
          return executeConnectionInfo();
