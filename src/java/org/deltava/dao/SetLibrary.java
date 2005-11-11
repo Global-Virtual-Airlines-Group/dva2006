1616b682f7503bc09ebcc0c7a98e6387d32d3726
Luke Kolin <luke@deltava.org>
2005-11-11 18:25:38 +0000

    Fix SQL
    git-svn-id: file:///home/svn/DVA2006@1657 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/SetLibrary.java
+++ src/java/org/deltava/dao/SetLibrary.java
@@ -82,7 +82,7 @@ public class SetLibrary extends DAO {
 		try {
 		   if (i.getDownloadCount() == 0) {
 		      prepareStatement("REPLACE INTO FLEET (NAME, IMG, FILESIZE, MAJOR, MINOR, SUBMINOR, SECURITY, "
-						+ "SECURITY, CODE, BODY, FILENAME) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+						+ "CODE, BODY, FILENAME) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 		   } else {
 		      prepareStatement("UPDATE FLEET SET NAME=?, IMG=?, FILESIZE=?, MAJOR=?, MINOR=?, SUBMINOR=?, "
 					+ "SECURITY=?, CODE=?, BODY=? WHERE (FILENAME=?)");
