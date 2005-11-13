d89025749298f27fd6d4622d77571cdc750585fd
Luke Kolin <luke@deltava.org>
2005-11-13 17:11:42 +0000

    Allow rating-only Transfer Requests
    git-svn-id: file:///home/svn/DVA2006@1667 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/admin/TransferApproveCommand.java
+++ src/java/org/deltava/commands/admin/TransferApproveCommand.java
@@ -28,7 +28,7 @@ import org.deltava.util.system.SystemData;
  */
 
 public class TransferApproveCommand extends AbstractCommand {
-	
+
 	/**
 	 * Executes the command.
 	 * @param ctx the Command context
@@ -61,48 +61,60 @@ public class TransferApproveCommand extends AbstractCommand {
 			usr = pdao.get(txreq.getID());
 			if (usr == null)
 				throw new CommandException("Invalid Pilot - " + txreq.getID());
-
-			// Get the old/new Equipment Programs
-			String eqType = ctx.getParameter("eqType");
+			
+			// Get the current equipment program
 			GetEquipmentType eqdao = new GetEquipmentType(con);
 			EquipmentType currentEQ = eqdao.get(usr.getEquipmentType());
-			EquipmentType newEQ = eqdao.get(eqType);
-			if ((currentEQ == null) || (newEQ == null))
-				throw new CommandException("Invalid Equipment Program - " + eqType + " / " + usr.getEquipmentType());
-
-			// Validate the rank
-			String rank = ctx.getParameter("rank");
-			if (!newEQ.getRanks().contains(rank))
-				throw new CommandException("Invalid Rank - " + rank);
 			
 			// Get the new ratings
-			Set newRatings = new TreeSet(CollectionUtils.loadList(ctx.getRequest().getParameterValues("ratings"), usr.getRatings()));
-			
-			// Check if we're doing a promotion or a rating change
-			RankComparator rCmp = new RankComparator((List) SystemData.getObject("ranks"));
-			rCmp.setRank2(usr.getRank(), currentEQ.getStage());
-			rCmp.setRank1(rank, newEQ.getStage());
-			
-			// Update the equipment program and rank
-			usr.setEquipmentType(newEQ.getName());
-			usr.setRank(rank);
-
-			// Write the promotion status update
 			List updates = new ArrayList();
-			if (rCmp.compare() > 0) {
-				boolean eqChange = !usr.getEquipmentType().equals(newEQ.getName());
-				int promoType = eqChange ? StatusUpdate.EXTPROMOTION : StatusUpdate.INTPROMOTION;
-				StatusUpdate upd = new StatusUpdate(usr.getID(), promoType);
-				upd.setAuthorID(ctx.getUser().getID());
-				upd.setDescription("Promoted to " + usr.getRank() + ", " + usr.getEquipmentType());
-				updates.add(upd);
+			Set newRatings = new TreeSet(CollectionUtils.loadList(ctx.getRequest().getParameterValues("ratings"),
+					usr.getRatings()));
+
+			// Check if we're switching programs
+			String eqType = ctx.getParameter("eqType");
+			if (eqType != null) {
+				EquipmentType newEQ = eqdao.get(eqType);
+				if ((currentEQ == null) || (newEQ == null))
+					throw new CommandException("Invalid Equipment Program - " + eqType + " / " + usr.getEquipmentType());
+
+				// Validate the rank
+				String rank = ctx.getParameter("rank");
+				if (!newEQ.getRanks().contains(rank))
+					throw new CommandException("Invalid Rank - " + rank);
+
+				// Check if we're doing a promotion or a rating change
+				RankComparator rCmp = new RankComparator((List) SystemData.getObject("ranks"));
+				rCmp.setRank2(usr.getRank(), currentEQ.getStage());
+				rCmp.setRank1(rank, newEQ.getStage());
+
+				// Update the equipment program and rank
+				usr.setEquipmentType(newEQ.getName());
+				usr.setRank(rank);
+
+				// Write the promotion status update
+				if (rCmp.compare() > 0) {
+					boolean eqChange = !usr.getEquipmentType().equals(newEQ.getName());
+					int promoType = eqChange ? StatusUpdate.EXTPROMOTION : StatusUpdate.INTPROMOTION;
+					StatusUpdate upd = new StatusUpdate(usr.getID(), promoType);
+					upd.setAuthorID(ctx.getUser().getID());
+					upd.setDescription("Promoted to " + usr.getRank() + ", " + usr.getEquipmentType());
+					updates.add(upd);
+				} else {
+					StatusUpdate upd = new StatusUpdate(usr.getID(), StatusUpdate.RANK_CHANGE);
+					upd.setAuthorID(ctx.getUser().getID());
+					upd.setDescription("Rank changed to " + usr.getRank() + ", " + usr.getEquipmentType());
+					updates.add(upd);
+				}
+				
+				// Add the new equipment program to the message context and request
+				mctxt.addData("eqType", newEQ);
+				ctx.setAttribute("eqType", newEQ, REQUEST);
 			} else {
-				StatusUpdate upd = new StatusUpdate(usr.getID(), StatusUpdate.RANK_CHANGE);
-				upd.setAuthorID(ctx.getUser().getID());
-				upd.setDescription("Rank changed to " + usr.getRank() + ", " + usr.getEquipmentType());
-				updates.add(upd);
+				mctxt.addData("eqType", currentEQ);
+				ctx.setAttribute("eqType", currentEQ, REQUEST);
 			}
-			
+
 			// Figure out what ratings have been added
 			Collection addedRatings = CollectionUtils.getDelta(newRatings, usr.getRatings());
 			if (!addedRatings.isEmpty()) {
@@ -112,34 +124,33 @@ public class TransferApproveCommand extends AbstractCommand {
 				upd.setDescription("Ratings added: " + StringUtils.listConcat(addedRatings, ", "));
 				updates.add(upd);
 			}
-			
-            // Figure out what ratings have been removed
-            Collection removedRatings = CollectionUtils.getDelta(usr.getRatings(), newRatings);
-            if (!removedRatings.isEmpty()) {
-               ctx.setAttribute("removedRatings", removedRatings, REQUEST);
-               usr.removeRatings(removedRatings);
 
-               // Note the changed ratings
-               StatusUpdate upd = new StatusUpdate(usr.getID(), StatusUpdate.RATING_REMOVE);
-               upd.setAuthorID(ctx.getUser().getID());
-               upd.setDescription("Ratings removed: " + StringUtils.listConcat(removedRatings, ", "));
-               updates.add(upd);
-            }
-            
-            // Update the ratings
-            usr.addRatings(newRatings);
+			// Figure out what ratings have been removed
+			Collection removedRatings = CollectionUtils.getDelta(usr.getRatings(), newRatings);
+			if (!removedRatings.isEmpty()) {
+				ctx.setAttribute("removedRatings", removedRatings, REQUEST);
+				usr.removeRatings(removedRatings);
+
+				// Note the changed ratings
+				StatusUpdate upd = new StatusUpdate(usr.getID(), StatusUpdate.RATING_REMOVE);
+				upd.setAuthorID(ctx.getUser().getID());
+				upd.setDescription("Ratings removed: " + StringUtils.listConcat(removedRatings, ", "));
+				updates.add(upd);
+			}
+
+			// Update the ratings
+			usr.addRatings(newRatings);
 
 			// Save the message context
 			mctxt.addData("pilot", usr);
-			mctxt.addData("eqType", newEQ);
-			
+
 			// Get the message template
 			GetMessageTemplate mtdao = new GetMessageTemplate(con);
 			mctxt.setTemplate(mtdao.get("XFERAPPROVE"));
 
 			// Use a SQL Transaction
 			ctx.startTX();
-			
+
 			// Save the Pilot
 			SetPilot pwdao = new SetPilot(con);
 			pwdao.write(usr);
@@ -158,7 +169,6 @@ public class TransferApproveCommand extends AbstractCommand {
 			// Write status attributes to the request
 			ctx.setAttribute("isApprove", Boolean.TRUE, REQUEST);
 			ctx.setAttribute("pilot", usr, REQUEST);
-			ctx.setAttribute("eqType", newEQ, REQUEST);
 		} catch (DAOException de) {
 			ctx.rollbackTX();
 			throw new CommandException(de);
