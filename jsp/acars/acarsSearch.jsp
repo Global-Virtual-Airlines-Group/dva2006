61bf063337e395d56892967a471cba6f4406f157
Luke Kolin <luke@deltava.org>
2005-11-25 23:38:08 +0000

    Add ACARS Flight Search
    git-svn-id: file:///home/svn/DVA2006@1771 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ jsp/acars/acarsSearch.jsp
@@ -0,0 +1,147 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<%@ page session="false" %>
+<%@ page isELIgnored="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="/WEB-INF/dva_content.tld" prefix="content" %>
+<%@ taglib uri="/WEB-INF/dva_html.tld" prefix="el" %>
+<%@ taglib uri="/WEB-INF/dva_view.tld" prefix="view" %>
+<%@ taglib uri="/WEB-INF/dva_format.tld" prefix="fmt" %>
+<%@ taglib uri="/WEB-INF/dva_jspfunc.tld" prefix="fn" %>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title><content:airline /> ACARS Flight Report Search</title>
+<content:css name="main" browserSpecific="true" />
+<content:css name="form" />
+<content:css name="view" />
+<content:pics />
+<content:js name="common" />
+<content:js name="datePicker" />
+<script language="JavaScript" type="text/javascript">
+function validate(form)
+{
+if (!checkSubmit()) return false;
+if (!validateCombo(form.searchType, 'Search Type')) return false;
+
+setSubmit();
+disableButton('MapButton');
+disableButton('SearchButton');
+return true;
+}
+
+function showMap(form)
+{
+if (!checkSubmit()) return false;
+
+// Build the URL
+var isChecked = false;
+var url = 'acars_earth.ws?showData=false&id=';
+if (form.doMap.length) {
+	for (var x = 0; x < form.doMap.length; x++) {
+		isChecked = isChecked || form.doMap[x].checked;
+		if (form.doMap[x].checked)
+			url = url + form.doMap[x].value + ',';
+	}
+	
+	// Strip trailing comma
+	url = url.substring(0, url.length - 1);
+} else {
+	isChecked = form.doMap.checked;
+	if (form.doMap.checked)
+		url = url + form.doMap.value;
+}
+
+// Check if we've selected a single flight
+if (!isChecked) {
+	alert('At least one Flight Report must be selected to view in Google Earth.');
+	return false;
+}
+
+// Display the url
+self.location = url;
+return false;
+}
+</script>
+</head>
+<content:copyright visible="false" />
+<body>
+<content:page>
+<%@ include file="/jsp/main/header.jsp" %> 
+<%@ include file="/jsp/main/sideMenu.jsp" %>
+
+<!-- Main Body Frame -->
+<content:region id="main">
+<el:form action="acarsprsearch.do" method="post" validate="return validate(this)">
+<el:table className="form" space="default" pad="default">
+<tr class="title caps">
+ <td colspan="4">ACARS FLIGHT REPORTS</td>
+</tr>
+<tr>
+ <td class="label">Search Type</td>
+ <td class="data" colspan="3"><el:combo name="searchType" idx="*" size="1" firstEntry="-" options="${searchTypes}" value="${param.searchType}" /></td>
+</tr>
+<tr>
+ <td class="label">Online Event</td>
+ <td class="data"><c:if test="${empty events}>NONE</c:if>
+<c:if test="${!empty events}"><el:combo name="eventID" idx="*" size="1" options="${events}" value="${param.eventID}" /></c:if></td>
+ <td class="label">Flight Date</td>
+ <td class="data"><el:text name="flightDate" idx="*" size="8" max="10" value="${param.flightDate}" />
+ <el:button className="BUTTON" label="CALENDAR" onClick="void show_calendar('forms[0].flightDate')" /></td>
+</tr>
+<tr class="title">
+ <td colspan="4" class="mid"><el:button ID="SearchButton" type="submit" className="BUTTON" label="SEARCH FLIGHTS" /></td>
+</tr>
+</el:table>
+</el:form>
+<c:if test="${doSearch}">
+<el:form action="acarsprsearch.do" method="get" validate="return showMap(this)">
+<view:table className="view" space="default" pad="default" cmd="acarsprsearch">
+<!-- Search Results -->
+<tr class="title">
+ <td colspan="6" class="left caps">SEARCH RESULTS</td>
+</tr>
+<c:if test="${empty viewContext.results}">
+<tr>
+ <td colspan="6" class="pri bld">NO ACARS FLIGHT REPORTS WERE FOUND.</td>
+</tr>
+<tr class="title">
+ <td colspan="6">&nbsp;</td>
+</tr>
+</c:if>
+<c:if test="${!empty viewContext.results}">
+<!-- Table Header Data -->
+<tr class="title">
+ <td width="10%">DATE</td>
+ <td width="5%">MAP</td>
+ <td width="15%">FLIGHT NUMBER</td>
+ <td width="45%">AIRPORT NAMES</td>
+ <td width="10%">EQUIPMENT</td>
+ <td>DURATION</td>
+</tr>
+
+<!-- Table Flight Report Data -->
+<c:forEach var="pirep" items="${viewContext.results}">
+<view:row entry="${pirep}">
+ <td class="title"><fmt:date date="${pirep.date}" fmt="d" default="-" /></td>
+ <td><el:box name="doMap" value="${fn:ACARS_ID(pirep)}" idx="*" label="" /></td>
+ <td><el:cmd className="bld" url="pirep" linkID="0x${pirep.ID}">${pirep.flightCode}</el:cmd></td>
+ <td class="small">${pirep.airportD.name} (<fmt:airport airport="${pirep.airportD}" />) - 
+ ${pirep.airportA.name} (<fmt:airport airport="${pirep.airportA}" />)</td>
+ <td class="sec">${pirep.equipmentType}</td>
+ <td><fmt:dec fmt="#0.0" value="${pirep.length / 10}" /> hours</td>
+</view:row>
+</c:forEach>
+
+</tr>
+<tr class="title">
+ <td colspan="6"><view:pgUp />&nbsp;<view:pgDn /> <el:button ID="MapButton" type="submit" className="BUTTON" label="DISPLAY USING GOOGLE EARTH" /></td>
+</tr>
+</c:if>
+</view:table>
+</el:form>
+</c:if>
+<br />
+<content:copyright />
+</content:region>
+</content:page>
+</body>
+</html>
--- src/java/org/deltava/service/ACARSEarthService.java
+++ src/java/org/deltava/service/ACARSEarthService.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 import org.jdom.*;
 
 import org.deltava.beans.*;
@@ -24,7 +26,73 @@ import org.deltava.util.*;
  */
 
 public class ACARSEarthService extends WebDataService {
+	
+	private static final GoogleEarthColor[] COLORS = {GoogleEarthColor.make(0, 208, 184), GoogleEarthColor.make(0, 205, 0),
+		GoogleEarthColor.make(240, 48, 48), GoogleEarthColor.make(240, 128, 80), GoogleEarthColor.make(240, 16, 240),
+		GoogleEarthColor.make(0, 240, 240), GoogleEarthColor.make(240, 240, 64) };
 
+	private static final Logger log = Logger.getLogger(ACARSEarthService.class);
+
+	private static class GoogleEarthColor {
+
+		private int _blue;
+		private int _green;
+		private int _red;
+		private int _alpha;
+		
+		public static GoogleEarthColor make(int red, int green, int blue, int alpha) {
+			return new GoogleEarthColor(red, green, blue, alpha);
+		}
+		
+		public static GoogleEarthColor make(int red, int green, int blue) {
+			return new GoogleEarthColor(red, green, blue, 0);
+		}
+		
+		private GoogleEarthColor(int red, int green, int blue, int alpha) {
+			super();
+			_red = (red > 255) ? 255 : red;
+			_green = (green > 255) ? 255: green;
+			_blue = (blue > 255) ? 255 : blue;
+			_alpha = (alpha > 255) ? 255 : alpha;
+		}
+
+		public int getRed() {
+			return _red;
+		}
+		
+		public int getGreen() {
+			return _green;
+		}
+		
+		public int getBlue() {
+			return _blue;
+		}
+		
+		public int getAlpha() {
+			return _alpha;
+		}
+		
+		/**
+		 * Helper method to generate a hex string without 0x and two characters long.
+		 */
+		private String formatHex(int value) {
+			String tmp = StringUtils.formatHex(value).substring(2);
+			return (tmp.length() == 1) ? "0" + tmp : tmp;
+		}
+		
+		public String toString() {
+			StringBuilder buf = new StringBuilder(formatHex(_blue));
+			buf.append(formatHex(_green));
+			buf.append(formatHex(_red));
+			buf.append(formatHex(_alpha));
+			return buf.toString();
+		}
+		
+		public GoogleEarthColor dim(int factor) {
+			return new GoogleEarthColor(_red / factor, _green / factor, _blue / factor, _alpha / factor);
+		}
+	}
+	
 	/**
 	 * Executes the Web Service, writing ACARS flight data in KML format.
 	 * @param ctx the Web Service Context
@@ -33,25 +101,40 @@ public class ACARSEarthService extends WebDataService {
 	 */
 	public int execute(ServiceContext ctx) throws ServiceException {
 
-		// Get the ACARS Flight ID
-		int id = 0;
-		try {
-			id = Integer.parseInt(ctx.getParameter("id"));
-		} catch (NumberFormatException nfe) {
-			throw new ServiceException(HttpServletResponse.SC_BAD_REQUEST, "Invalid ID");
+		// Get the ACARS Flight IDs
+		Collection<Integer> IDs = new TreeSet<Integer>();
+		Collection<String> rawIDs = StringUtils.split(ctx.getParameter("id"), ",");
+		for (Iterator<String> i = rawIDs.iterator(); i.hasNext();) {
+			String rawID = i.next();
+			try {
+				IDs.add(new Integer(StringUtils.parseHex(rawID)));
+			} catch (NumberFormatException nfe) {
+				log.warn("Invalid ACARS flight ID - " + rawID);
+			}
 		}
 
-		// Get the ACARS data
-		List routeData = null;
-		FlightInfo info = null;
+		// Check if we add position records
+		boolean showData = Boolean.valueOf(ctx.getParameter("showData")).booleanValue();
+
+		// Get ACARS data for the flights
+		GetACARSData dao = new GetACARSData(_con);
+		Map<FlightInfo,Collection<RouteEntry>> flightData = new LinkedHashMap<FlightInfo,Collection<RouteEntry>>();
 		try {
-			GetACARSData dao = new GetACARSData(_con);
-			info = dao.getInfo(id);
-			routeData = (info == null) ? Collections.EMPTY_LIST : dao.getRouteEntries(id, true, info.getArchived());
+			for (Iterator<Integer> i = IDs.iterator(); i.hasNext(); ) {
+				int id = i.next().intValue();
+				FlightInfo info = dao.getInfo(id);
+				if (info != null) {
+					@SuppressWarnings("unchecked")
+					Collection<RouteEntry> routeData = dao.getRouteEntries(id, true, info.getArchived());
+					flightData.put(info, routeData);
+				} else {
+					log.warn("Cannot find ACARS flight " + id);
+				}
+			}
 		} catch (DAOException de) {
 			throw new ServiceException(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, de.getMessage());
 		}
-		
+
 		// Build the XML document
 		Document doc = new Document();
 		Element ke = new Element("kml");
@@ -59,108 +142,34 @@ public class ACARSEarthService extends WebDataService {
 		Element de = new Element("Document");
 		ke.addContent(de);
 		
-		// Generate the start airport placemark
-		Airport aD = info.getAirportD();
-		Element ade = new Element("Placemark");
-		ade.addContent(XMLUtils.createElement("description", "Departed from " + aD.getName()));
-		ade.addContent(XMLUtils.createElement("name", aD.getName() + " (" + aD.getICAO() + ")"));
-		ade.addContent(XMLUtils.createElement("visibility", "1"));
-		Element ads = new Element("Style");
-		ads.addContent(addIcon(2, 0, 1)); // airplane icon
-		ade.addContent(ads);
-		Element adp = new Element("Point");
-		adp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(aD)));
-		ade.addContent(adp);
-		ade.addContent(addLookAt(aD, 1500, -1));
-		de.addContent(ade);
-		
-		// Format the route
-		Element le = new Element("Placemark");
-		le.addContent(XMLUtils.createElement("name", "Flight Route"));
-		le.addContent(XMLUtils.createElement("description", String.valueOf(routeData.size()) + " Position Records"));
-		le.addContent(XMLUtils.createElement("visibility", "1"));
-		Element ls = new Element("Style");
-		ls.addContent(XMLUtils.createElement("LineStyle", "color", "b8d10000"));
-		ls.addContent(XMLUtils.createElement("PolyStyle", "color", "2fa10000"));
-		le.addContent(ls);
-		Element lse = new Element("LineString");
-		lse.addContent(XMLUtils.createElement("extrude", "1"));
-		lse.addContent(XMLUtils.createElement("altitudeMode", "relativeToGround"));
-		lse.addContent(XMLUtils.createElement("tessellate", "1"));
-		
-		// Format all of the coordinates
-		StringBuilder buf = new StringBuilder();
-		for (Iterator i = routeData.iterator(); i.hasNext(); ) {
-			RouteEntry entry = (RouteEntry) i.next();
-			buf.append(GeoUtils.format3D(entry, entry.getRadarAltitude()));
-			buf.append(" \n");
-		}
-		
-		// Save the coordinates
-		lse.addContent(XMLUtils.createElement("coordinates", buf.toString()));
-		le.addContent(lse);
-		de.addContent(le);
-		
-		// Format the positions
-		Element fe = new Element("Folder");
-		fe.addContent(XMLUtils.createElement("name", "Position Records"));
-		fe.addContent(XMLUtils.createElement("visibility", "0"));
-		de.addContent(fe);
-		int pos = 0;
-		for (Iterator i = routeData.iterator(); i.hasNext(); ) {
-			RouteEntry entry = (RouteEntry) i.next();
-			Element pe = new Element("Placemark");
-			pe.addContent(XMLUtils.createElement("name", "Route Point #" + String.valueOf(++pos)));
-			pe.addContent(XMLUtils.createElement("description", entry.getInfoBox(), true));
-			pe.addContent(XMLUtils.createElement("visibility", "0"));
-			Element ps = new Element("Style");
-			Element pis = addIcon(2, 0, 0); // info icon
-			pis.addContent(XMLUtils.createElement("scale", "0.70"));
-			pis.addContent(XMLUtils.createElement("heading", StringUtils.format(entry.getHeading(), "##0.00")));
-			ps.addContent(pis);
-			pe.addContent(ps);
-			Element pp = new Element("Point");
-			if (entry.isFlagSet(ACARSFlags.FLAG_ONGROUND)) {
-				pp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format2D(entry)));
-				pp.addContent(XMLUtils.createElement("altitudeMode", "clampedToGround"));
-			} else if (entry.getRadarAltitude() < 1000) {
-				pp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(entry, entry.getRadarAltitude())));
-				pp.addContent(XMLUtils.createElement("altitudeMode", "relativeToGround"));
-			} else {
-				pp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(entry, entry.getAltitude())));
-				pp.addContent(XMLUtils.createElement("altitudeMode", "absolute"));
+		// Add the ACARS data
+		int colorOfs = -1;
+		for (Iterator<FlightInfo> i = flightData.keySet().iterator(); i.hasNext(); ) {
+			FlightInfo info = i.next();
+			Collection<Element> fData = createFlight(info, flightData.get(info), showData, COLORS[++colorOfs]);
+			Element fe = de;
+			if (flightData.size() > 1) {
+				fe = new Element("Folder");
+				fe.addContent(XMLUtils.createElement("name", "Flight " + info.getID()));
+				fe.addContent(XMLUtils.createElement("visibility", "1"));
+				de.addContent(fe);
 			}
-
-			pe.addContent(pp);
-			pe.addContent(addLookAt(entry, entry.getAltitude(), entry.getHeading()));
-			fe.addContent(pe);
+			
+			// Add the children
+			for (Iterator<Element> ci = fData.iterator(); ci.hasNext(); )
+				fe.addContent(ci.next());
 		}
-		
-		// Generate the end airport placemark
-		Airport aA = info.getAirportA();
-		Element aae = new Element("Placemark");
-		aae.addContent(XMLUtils.createElement("description", "Landed at " + aA.getName()));
-		aae.addContent(XMLUtils.createElement("name", aA.getName() + " (" + aA.getICAO() + ")"));
-		aae.addContent(XMLUtils.createElement("visibility", "1"));
-		Element aas = new Element("Style");
-		aas.addContent(addIcon(2, 0, 1)); // airplane icon
-		aae.addContent(aas);
-		Element aap = new Element("Point");
-		aap.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(aA)));
-		aae.addContent(aap);
-		aae.addContent(addLookAt(aA, 1500, -1));
-		de.addContent(aae);
-		
+
 		// Write the XML
 		try {
-			ctx.getResponse().setHeader("Content-disposition", "attachment; filename=acars" + id + ".kml");
+			ctx.getResponse().setHeader("Content-disposition", "attachment; filename=acarsFlights.kml");
 			ctx.getResponse().setContentType("application/vnd.google-earth.kml+xml");
 			ctx.println(XMLUtils.format(doc, "ISO-8859-1"));
 			ctx.commit();
 		} catch (IOException ie) {
 			throw new ServiceException(HttpServletResponse.SC_CONFLICT, "I/O Error");
 		}
-		
+
 		// Return success code
 		return HttpServletResponse.SC_OK;
 	}
@@ -172,14 +181,14 @@ public class ACARSEarthService extends WebDataService {
 		Element re = new Element("IconStyle");
 		Element e = new Element("Icon");
 		e.addContent(XMLUtils.createElement("href", "root://icons/palette-" + palette + ".png", true));
-		e.addContent(XMLUtils.createElement("x", String.valueOf(pX *32)));
-		e.addContent(XMLUtils.createElement("y", String.valueOf(pY *32)));
+		e.addContent(XMLUtils.createElement("x", String.valueOf(pX * 32)));
+		e.addContent(XMLUtils.createElement("y", String.valueOf(pY * 32)));
 		e.addContent(XMLUtils.createElement("w", "32"));
 		e.addContent(XMLUtils.createElement("h", "32"));
 		re.addContent(e);
 		return re;
 	}
-	
+
 	/**
 	 * Helper method to generate a LookAt element.
 	 */
@@ -197,5 +206,115 @@ public class ACARSEarthService extends WebDataService {
 		}
 
 		return e;
+	}
+
+	/**
+	 * Helper method to turn a flight record into a KML element.
+	 */
+	private Collection<Element> createFlight(FlightInfo info, Collection<RouteEntry> routeData, 
+			boolean showPositionData, GoogleEarthColor routeColor) {
+
+		// Create results
+		Collection<Element> results = new ArrayList<Element>();
+
+		// Generate the start airport placemark
+		Airport aD = info.getAirportD();
+		Element ade = new Element("Placemark");
+		ade.addContent(XMLUtils.createElement("description", "Departed from " + aD.getName()));
+		ade.addContent(XMLUtils.createElement("name", aD.getName() + " (" + aD.getICAO() + ")"));
+		ade.addContent(XMLUtils.createElement("visibility", "1"));
+		Element ads = new Element("Style");
+		ads.addContent(addIcon(2, 0, 1)); // airplane icon
+		ade.addContent(ads);
+		Element adp = new Element("Point");
+		adp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(aD)));
+		ade.addContent(adp);
+		ade.addContent(addLookAt(aD, 480, -1));
+		results.add(ade);
+
+		// Format the route
+		Element le = new Element("Placemark");
+		le.addContent(XMLUtils.createElement("name", "Flight Route"));
+		le.addContent(XMLUtils.createElement("description", String.valueOf(routeData.size()) + " Position Records"));
+		le.addContent(XMLUtils.createElement("visibility", "1"));
+		Element ls = new Element("Style");
+		Element lce = XMLUtils.createElement("LineStyle", "color", routeColor.toString());
+		lce.addContent(XMLUtils.createElement("width", "2"));
+		ls.addContent(lce);
+		ls.addContent(XMLUtils.createElement("PolyStyle", "color", routeColor.dim(3).toString()));
+		le.addContent(ls);
+		Element lse = new Element("LineString");
+		lse.addContent(XMLUtils.createElement("extrude", "1"));
+		lse.addContent(XMLUtils.createElement("altitudeMode", "relativeToGround"));
+		lse.addContent(XMLUtils.createElement("tessellate", "1"));
+
+		// Format all of the coordinates
+		StringBuilder buf = new StringBuilder();
+		for (Iterator i = routeData.iterator(); i.hasNext();) {
+			RouteEntry entry = (RouteEntry) i.next();
+			buf.append(GeoUtils.format3D(entry, entry.getRadarAltitude()));
+			buf.append(" \n");
+		}
+
+		// Save the coordinates
+		lse.addContent(XMLUtils.createElement("coordinates", buf.toString()));
+		le.addContent(lse);
+		results.add(le);
+
+		// Generate the end airport placemark
+		Airport aA = info.getAirportA();
+		Element aae = new Element("Placemark");
+		aae.addContent(XMLUtils.createElement("description", "Landed at " + aA.getName()));
+		aae.addContent(XMLUtils.createElement("name", aA.getName() + " (" + aA.getICAO() + ")"));
+		aae.addContent(XMLUtils.createElement("visibility", "1"));
+		Element aas = new Element("Style");
+		aas.addContent(addIcon(2, 0, 1)); // airplane icon
+		aae.addContent(aas);
+		Element aap = new Element("Point");
+		aap.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(aA)));
+		aae.addContent(aap);
+		aae.addContent(addLookAt(aA, 1500, -1));
+		results.add(aae);
+
+		// Format the positions
+		if (showPositionData) {
+			Element fe = new Element("Folder");
+			fe.addContent(XMLUtils.createElement("name", "Position Records"));
+			fe.addContent(XMLUtils.createElement("visibility", "0"));
+			results.add(fe);
+			int pos = 0;
+			for (Iterator<RouteEntry> i = routeData.iterator(); i.hasNext();) {
+				RouteEntry entry = i.next();
+				Element pe = new Element("Placemark");
+				pe.addContent(XMLUtils.createElement("name", "Route Point #" + String.valueOf(++pos)));
+				pe.addContent(XMLUtils.createElement("description", entry.getInfoBox(), true));
+				pe.addContent(XMLUtils.createElement("visibility", "0"));
+				Element ps = new Element("Style");
+				Element pis = addIcon(2, 0, 0); // info icon
+				pis.addContent(XMLUtils.createElement("scale", "0.70"));
+				pis.addContent(XMLUtils.createElement("heading", StringUtils.format(entry.getHeading(), "##0.00")));
+				ps.addContent(pis);
+				pe.addContent(ps);
+				Element pp = new Element("Point");
+				if (entry.isFlagSet(ACARSFlags.FLAG_ONGROUND)) {
+					pp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format2D(entry)));
+					pp.addContent(XMLUtils.createElement("altitudeMode", "clampedToGround"));
+				} else if (entry.getRadarAltitude() < 1000) {
+					pp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(entry, entry
+							.getRadarAltitude())));
+					pp.addContent(XMLUtils.createElement("altitudeMode", "relativeToGround"));
+				} else {
+					pp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(entry, entry.getAltitude())));
+					pp.addContent(XMLUtils.createElement("altitudeMode", "absolute"));
+				}
+
+				pe.addContent(pp);
+				pe.addContent(addLookAt(entry, entry.getAltitude(), entry.getHeading()));
+				fe.addContent(pe);
+			}
+		}
+		
+		// Return results
+		return results;
 	}
 }
