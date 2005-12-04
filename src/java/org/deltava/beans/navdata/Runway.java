a31abb8591566df107fabf2e4a34cd1eadb92b6a
Luke Kolin <luke@deltava.org>
2005-12-04 21:17:40 +0000

    Fix Google Maps on Firefox 1.5
    git-svn-id: file:///home/svn/DVA2006@1832 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/navdata/Runway.java
+++ src/java/org/deltava/beans/navdata/Runway.java
@@ -12,106 +12,110 @@ import org.deltava.util.StringUtils;
 
 public class Runway extends NavigationDataBean {
 
-   private int _length;
-   private int _heading;
-   private String _freq;
+	private int _length;
+	private int _heading;
+	private String _freq;
 
-   /**
-    * Creates a new Runway bean.
-    * @param lat the latitude of the start of the runway
-    * @param lon the longitude of the start of the runway
-    */
-   public Runway(double lat, double lon) {
-      super(RUNWAY, lat, lon);
-   }
+	/**
+	 * Creates a new Runway bean.
+	 * @param lat the latitude of the start of the runway
+	 * @param lon the longitude of the start of the runway
+	 */
+	public Runway(double lat, double lon) {
+		super(RUNWAY, lat, lon);
+	}
 
-   /**
-    * Returns the length of the runway.
-    * @return the length in feet
-    * @see Runway#setLength(int)
-    */
-   public int getLength() {
-      return _length;
-   }
+	/**
+	 * Returns the length of the runway.
+	 * @return the length in feet
+	 * @see Runway#setLength(int)
+	 */
+	public int getLength() {
+		return _length;
+	}
 
-   /**
-    * Returns the runway heading.
-    * @return the heading in degrees
-    * @see Runway#setHeading(int)
-    */
-   public int getHeading() {
-      return _heading;
-   }
+	/**
+	 * Returns the runway heading.
+	 * @return the heading in degrees
+	 * @see Runway#setHeading(int)
+	 */
+	public int getHeading() {
+		return _heading;
+	}
 
-   /**
-    * Returns the frequency of the runway's ILS.
-    * @return the frequency
-    * @see Runway#setFrequency(String)
-    */
-   public String getFrequency() {
-      return _freq;
-   }
+	/**
+	 * Returns the frequency of the runway's ILS.
+	 * @return the frequency
+	 * @see Runway#setFrequency(String)
+	 */
+	public String getFrequency() {
+		return _freq;
+	}
 
-   /**
-    * Updates the length of the runway.
-    * @param len the length in feet
-    * @throws IllegalArgumentException if len is zero, negative or > 25000
-    * @see Runway#getLength()
-    */
-   public void setLength(int len) {
-      if ((len < 1) || (len > 25000)) throw new IllegalArgumentException("Length cannot be < 1 or > 25000");
+	/**
+	 * Updates the length of the runway.
+	 * @param len the length in feet
+	 * @throws IllegalArgumentException if len is zero, negative or > 25000
+	 * @see Runway#getLength()
+	 */
+	public void setLength(int len) {
+		if ((len < 1) || (len > 25000))
+			throw new IllegalArgumentException("Length cannot be < 1 or > 25000");
 
-      _length = len;
-   }
+		_length = len;
+	}
 
-   /**
-    * Updates the runway heading.
-    * @param hdg the heading in degrees
-    * @throws IllegalArgumentException if hdg is negative or > 360
-    * @see Runway#getHeading()
-    */
-   public void setHeading(int hdg) {
-      if ((hdg < 0) || (hdg > 360)) throw new IllegalArgumentException("Invalid Heading - " + hdg);
+	/**
+	 * Updates the runway heading.
+	 * @param hdg the heading in degrees
+	 * @throws IllegalArgumentException if hdg is negative or > 360
+	 * @see Runway#getHeading()
+	 */
+	public void setHeading(int hdg) {
+		if ((hdg < 0) || (hdg > 360))
+			throw new IllegalArgumentException("Invalid Heading - " + hdg);
 
-      _heading = hdg;
-   }
+		_heading = hdg;
+	}
 
-   /**
-    * Updates the frequency of the runway's ILS.
-    * @param freq the frequency, or null if no ILS
-    * @see Runway#getFrequency()
-    */
-   public void setFrequency(String freq) {
-      _freq = freq;
-   }
+	/**
+	 * Updates the frequency of the runway's ILS.
+	 * @param freq the frequency, or null if no ILS
+	 * @see Runway#getFrequency()
+	 */
+	public void setFrequency(String freq) {
+		_freq = freq;
+	}
 
-   /**
-    * Return the default Google Maps icon color.
-    * @return org.deltava.beans.MapEntry.YELLOW
-    */
-   public String getIconColor() {
-      return YELLOW;
-   }
+	/**
+	 * Return the default Google Maps icon color.
+	 * @return org.deltava.beans.MapEntry.YELLOW
+	 */
+	public String getIconColor() {
+		return YELLOW;
+	}
 
-   /**
-    * Returns the default Google Maps infobox text.
-    * @return an HTML String
-    */
-   public String getInfoBox() {
-      StringBuilder buf = new StringBuilder(getHTMLTitle());
-      buf.append("Heading: ");
-      buf.append(StringUtils.format(_heading, "000"));
-      buf.append("<br />Length:");
-      buf.append(StringUtils.format(_length, "#,##0"));
+	/**
+	 * Returns the default Google Maps infobox text.
+	 * @return an HTML String
+	 */
+	public String getInfoBox() {
+		StringBuilder buf = new StringBuilder("<span class=\"mapInfoBox\">");
+		buf.append(getHTMLTitle());
+		buf.append("Heading: ");
+		buf.append(StringUtils.format(_heading, "000"));
+		buf.append("<br />Length:");
+		buf.append(StringUtils.format(_length, "#,##0"));
 
-      // Add ILS frequency if found
-      if (_freq != null) {
-         buf.append("<br />ILS Frequency: ");
-         buf.append(_freq);
-         buf.append("<br />");
-      }
+		// Add ILS frequency if found
+		if (_freq != null) {
+			buf.append("<br />ILS Frequency: ");
+			buf.append(_freq);
+			buf.append("<br />");
+		}
 
-      buf.append(getHTMLPosition());
-      return buf.toString();
-   }
+		buf.append(getHTMLPosition());
+		buf.append("</span>");
+		return buf.toString();
+	}
 }
