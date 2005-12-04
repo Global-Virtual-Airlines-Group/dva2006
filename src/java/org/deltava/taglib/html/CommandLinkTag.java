3fefadb8096dc777b399a56f0a9e10b47c432917
Luke Kolin <luke@deltava.org>
2005-12-04 21:15:45 +0000

    Don't render HREFs where linkID=0x0
    git-svn-id: file:///home/svn/DVA2006@1827 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/html/CommandLinkTag.java
+++ src/java/org/deltava/taglib/html/CommandLinkTag.java
@@ -14,78 +14,80 @@ import javax.servlet.jsp.JspException;
 
 public class CommandLinkTag extends LinkTag {
 
-    private String _cmdName;
-    private String _cmdParam;
-    private String _cmdOpName;
+	private String _cmdName;
+	private String _cmdParam;
+	private String _cmdOpName;
 
-    /**
-     * Creates a new CommandLink Tag.
-     */
-    public CommandLinkTag() {
-        super();
-    }
+	/**
+	 * Creates a new CommandLink Tag.
+	 */
+	public CommandLinkTag() {
+		super();
+	}
 
-    /**
-     * Sets the ID parameter for the command invocation. If it starts with &quot;0x&quot; then turn the rest
-     * of the string into a hexadecimal number string.
-     * @param id the parameter
-     * @see org.deltava.commands.CommandContext#getCmdParameter(int, Object)
-     */
-    public void setLinkID(String id) {
-        if (id.startsWith("0x")) {
-            try {
-                _cmdParam = "0x" + Integer.toString(Integer.parseInt(id.substring(2)), 16).toUpperCase();
-            } catch (NumberFormatException nfe) {
-                _cmdParam = id;
-            }
-        } else {
-            _cmdParam = id;
-        }
-    }
+	/**
+	 * Sets the ID parameter for the command invocation. If it starts with &quot;0x&quot; then turn the rest of the
+	 * string into a hexadecimal number string.
+	 * @param id the parameter
+	 * @see org.deltava.commands.CommandContext#getCmdParameter(int, Object)
+	 */
+	public void setLinkID(String id) {
+		if (id.startsWith("0x")) {
+			if (!"0x0".equals(id)) {
+				try {
+					_cmdParam = "0x" + Integer.toString(Integer.parseInt(id.substring(2)), 16).toUpperCase();
+				} catch (NumberFormatException nfe) {
+					_cmdParam = id;
+				}
+			}
+		} else {
+			_cmdParam = id;
+		}
+	}
 
-    /**
-     * Sets the operation parameter for the command invocation.
-     * @param opName the operation name
-     */
-    public void setOp(String opName) {
-        _cmdOpName = opName;
-    }
+	/**
+	 * Sets the operation parameter for the command invocation.
+	 * @param opName the operation name
+	 */
+	public void setOp(String opName) {
+		_cmdOpName = opName;
+	}
 
-    /**
-     * Sets the command name.
-     * @param url the command name
-     */
-    public void setUrl(String url) {
-        _cmdName = url.toLowerCase() + ".do";
-    }
+	/**
+	 * Sets the command name.
+	 * @param url the command name
+	 */
+	public void setUrl(String url) {
+		_cmdName = url.toLowerCase() + ".do";
+	}
 
-    public final int doStartTag() throws JspException {
-        StringBuilder url = new StringBuilder(_cmdName);
+	public final int doStartTag() throws JspException {
+		StringBuilder url = new StringBuilder(_cmdName);
 
-        try {
-            if (_cmdParam != null) {
-                url.append("?id=");
-                url.append(URLEncoder.encode(_cmdParam, "UTF-8"));
-            }
+		try {
+			if (_cmdParam != null) {
+				url.append("?id=");
+				url.append(URLEncoder.encode(_cmdParam, "UTF-8"));
+			}
 
-            if (_cmdOpName != null) {
-                url.append((_cmdParam == null) ? "?" : "&amp;");
-                url.append("op=");
-                url.append(URLEncoder.encode(_cmdOpName, "UTF-8"));
-            }
+			if (_cmdOpName != null) {
+				url.append((_cmdParam == null) ? "?" : "&amp;");
+				url.append("op=");
+				url.append(URLEncoder.encode(_cmdOpName, "UTF-8"));
+			}
 
-            // Update the HREF and call the superclass renderer
-            _attrs.put("href", url.toString());
-        } catch (UnsupportedEncodingException uee) {
-            throw new JspException("UTF-8 encoding not supported - Laws of Universe no longer apply");
-        }
+			// Update the HREF and call the superclass renderer
+			_attrs.put("href", url.toString());
+		} catch (UnsupportedEncodingException uee) {
+			throw new JspException("UTF-8 encoding not supported - Laws of Universe no longer apply");
+		}
 
-        return super.doStartTag();
-    }
-    
-    public int doEndTag() throws JspException {
-       super.doEndTag();
-       release();
-       return EVAL_PAGE;
-    }
+		return super.doStartTag();
+	}
+
+	public int doEndTag() throws JspException {
+		super.doEndTag();
+		release();
+		return EVAL_PAGE;
+	}
 }
