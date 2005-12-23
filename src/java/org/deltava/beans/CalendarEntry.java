2933f6d30f1b533601655a1e1a909f190a6cd8d4
Luke Kolin <luke@deltava.org>
2005-12-23 20:21:15 +0000

    Start of calendar support
    git-svn-id: file:///home/svn/DVA2006@1954 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/CalendarEntry.java
@@ -0,0 +1,16 @@
+// Copyright (c) 2005 Global Virtual Airline Group. All Rights Reserved.
+package org.deltava.beans;
+
+import java.util.Date;
+
+/**
+ * An interface to define beans that appear in a Calendar view table.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public interface CalendarEntry {
+	
+	public Date getDate();
+}
---
+++ src/java/org/deltava/comparators/CalendarEntryComparator.java
@@ -0,0 +1,24 @@
+// Copyright (c) 2005 Global Virtual Airline Group. All Rights Reserved.
+package org.deltava.comparators;
+
+import java.util.Comparator;
+
+import org.deltava.beans.CalendarEntry;
+
+/**
+ * A comparator to compare Calendar view entries.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class CalendarEntryComparator implements Comparator<CalendarEntry> {
+
+	/**
+	 * Compares two Calendar entries by comparing their dates.
+	 * @see Comparator#compare(Object, Object)
+	 */
+	public int compare(CalendarEntry e1, CalendarEntry e2) {
+		return e1.getDate().compareTo(e2.getDate());
+	}
+}
