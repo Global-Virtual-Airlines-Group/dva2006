89f4caf0e46475bddc4f7939655b0170cebd44cf
Luke Kolin <luke@deltava.org>
2006-01-10 14:17:31 +0000

    View command log
    git-svn-id: file:///home/svn/DVA2006@2019 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/system/CommandLogViewCommand.java
+++ src/java/org/deltava/commands/system/CommandLogViewCommand.java
@@ -1,15 +1,14 @@
 // Copyright 2005 Luke J. Kolin. All Rights Reserved.
 package org.deltava.commands.system;
 
-import java.util.*;
 import java.sql.Connection;
 
+import org.deltava.beans.Pilot;
+
 import org.deltava.commands.*;
+import org.deltava.dao.*;
 
-import org.deltava.dao.GetSystemLog;
-import org.deltava.dao.DAOException;
-
-import org.deltava.util.StringUtils;
+import org.deltava.util.system.SystemData;
 
 /**
  * A Web Site Command to view the Command History Log.
@@ -42,23 +41,36 @@ public class CommandLogViewCommand extends AbstractViewCommand {
       try {
          Connection con = ctx.getConnection();
          
+         // If we're searching for a pilot name, get it
+         int id = ctx.getID();
+         if ((id == 0) && (ctx.getParameter("pilotName") != null)) {
+        	 GetPilot pdao = new GetPilot(con);
+        	 Pilot usr = pdao.getByName(ctx.getParameter("pilotName"), SystemData.get("airline.db"));
+        	 if (usr != null) {
+        		 ctx.setAttribute("pilot", usr, REQUEST);
+        		 id = usr.getID();
+        	 }
+         }
+         
          // Get the DAO and the log entries
-         GetSystemLog dao = new GetSystemLog(con);
+         GetSystemData dao = new GetSystemData(con);
          dao.setQueryStart(vc.getStart());
          dao.setQueryMax(vc.getCount());
          
          // Do the query
-         if (ctx.getID() != 0) {
-        	 
+         if (id != 0) {
+        	 vc.setResults(dao.getCommands(id));
          } else {
-        	 
+        	 vc.setResults(dao.getCommands(addr));
          }
-         
       } catch (DAOException de) {
          throw new CommandException(de);
       } finally {
          ctx.release();
       }
+      
+      // Set search attribute
+      ctx.setAttribute("doSearch", Boolean.TRUE, REQUEST);
       
       // Forward to the JSP
       result.setURL("/jsp/admin/cmdLogView.jsp");
