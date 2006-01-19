d5d3cde9ac98a2d242988a0b7f707fadb384d500
Luke Kolin <luke@deltava.org>
2006-01-19 22:39:44 +0000

    Add pilot LDAP alias
    git-svn-id: file:///home/svn/DVA2006@2096 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/PilotReadDAO.java
+++ src/java/org/deltava/dao/PilotReadDAO.java
@@ -285,23 +285,24 @@ abstract class PilotReadDAO extends PilotDAO {
 			p.setAirportCodeType(rs.getInt(33));
 			p.setMapType(rs.getInt(34));
 			p.setNoExams(rs.getBoolean(35));
+			p.setLDAPName(rs.getString(36));
 
-			// Check if this result set has a column 36-39, which is the PIREP totals
-			if (columnCount > 38) {
-				p.setLegs(rs.getInt(36));
-				p.setMiles(rs.getLong(37));
-				p.setHours(rs.getDouble(38));
-				p.setLastFlight(expandDate(rs.getDate(39)));
+			// Check if this result set has a column 37-40, which is the PIREP totals
+			if (columnCount > 39) {
+				p.setLegs(rs.getInt(37));
+				p.setMiles(rs.getLong(38));
+				p.setHours(rs.getDouble(39));
+				p.setLastFlight(expandDate(rs.getDate(40)));
 			}
 
-			// Check if this result set has a column 40, which is the signature ID
-			if (columnCount > 39)
-				p.setHasSignature((rs.getInt(40) != 0));
+			// Check if this result set has a column 41, which is the signature ID
+			if (columnCount > 40)
+				p.setHasSignature((rs.getInt(41) != 0));
 
-			// CHeck if this result set has a column 41/42, which are online legs/hours
-			if (columnCount > 41) {
-				p.setOnlineLegs(rs.getInt(41));
-				p.setOnlineHours(rs.getDouble(42));
+			// CHeck if this result set has columns 42/43, which are online legs/hours
+			if (columnCount > 42) {
+				p.setOnlineLegs(rs.getInt(42));
+				p.setOnlineHours(rs.getDouble(43));
 			}
 
 			// Add the pilot
