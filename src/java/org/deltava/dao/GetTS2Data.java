0d2641e35f4404ad0577ad819495e3b919010ec0
Luke Kolin <luke@deltava.org>
2006-01-20 22:52:12 +0000

    Add TS2 Admin interface
    git-svn-id: file:///home/svn/DVA2006@2108 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetTS2Data.java
+++ src/java/org/deltava/dao/GetTS2Data.java
@@ -96,6 +96,58 @@ public class GetTS2Data extends DAO {
 	}
 	
 	/**
+	 * Returns all TeamSpeak 2 users authorized to access a virtual server.
+	 * @param serverID the server database ID
+	 * @return a Collection of User profiles
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public Collection<User> getUsers(int serverID) throws DAOException {
+		try {
+			prepareStatement("SELECT * FROM teamspeak.ts2_clients WHERE (i_client_server_id=?) ORDER BY s_client_name");
+			_ps.setInt(1, serverID);
+			return executeUsers();
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+	
+	/**
+	 * Returns all TeamSpeak 2 virtual server profiles.
+	 * @return a Collection of Server beans
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public Collection<Server> getServers() throws DAOException {
+		try {
+			prepareStatement("SELECT i_server_id, s_server_name, s_server_welcomemessage, i_server_maxusers, "
+					+ "i_server_udpport, s_server_password, b_server_active, dt_server_created, s_server_description "
+					+ "FROM teamspeak.ts2_servers");
+			return executeServers();
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+	
+	/**
+	 * Returns a TeamSpeak 2 virtual server profile.
+	 * @param id the Server database ID
+	 * @return a Server bean, or null if not found
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public Server getServer(int id) throws DAOException {
+		try {
+			setQueryMax(1);
+			prepareStatement("SELECT i_server_id, s_server_name, s_server_welcomemessage, i_server_maxusers, "
+					+ "i_server_udpport, s_server_password, b_server_active, dt_server_created, s_server_description "
+					+ "FROM teamspeak.ts2_servers WHERE (i_server_id=?)");
+			_ps.setInt(1, id);
+			List<Server> results = executeServers();
+			return results.isEmpty() ? null : results.get(0);
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+	
+	/**
 	 * Helper method to parse Channel result sets.
 	 */
 	private List<Channel> executeChannels() throws SQLException {
@@ -139,6 +191,7 @@ public class GetTS2Data extends DAO {
 			usr.setID(rs.getInt(1));
 			usr.setServerID(rs.getInt(2));
 			usr.setServerAdmin(rs.getInt(3) == -1);
+			usr.setPassword(rs.getString(5));
 			usr.setCreatedOn(rs.getTimestamp(6));
 			usr.setLastOnline(rs.getTimestamp(7));
 			
@@ -151,5 +204,44 @@ public class GetTS2Data extends DAO {
 		rs.close();
 		_ps.close();
 		return results;
+	}
+	
+	private List<Server> executeServers() throws SQLException {
+		Map<Integer, Server> results = new TreeMap<Integer, Server>();
+		
+		// Execute the query
+		ResultSet rs = _ps.executeQuery();
+		while (rs.next()) {
+			Server srv = new Server(rs.getString(2));
+			srv.setID(rs.getInt(1));
+			srv.setWelcomeMessage(rs.getString(3));
+			srv.setMaxUsers(rs.getInt(4));
+			srv.setPort(rs.getInt(5));
+			srv.setPassword(rs.getString(6));
+			srv.setActive(rs.getInt(7) == -1);
+			srv.setCreatedOn(rs.getTimestamp(8));
+			srv.setDescription(rs.getString(9));
+			
+			// Add to results
+			results.put(new Integer(srv.getID()), srv);
+		}
+		
+		// Clean up first query
+		rs.close();
+		_ps.close();
+		
+		// Load roles for each server
+		prepareStatementWithoutLimits("SELECT * FROM ts2_server_roles");
+		rs = _ps.executeQuery();
+		while (rs.next()) {
+			Server srv = results.get(new Integer(rs.getInt(1)));
+			if (srv != null)
+				srv.addRole(rs.getString(2));
+		}
+		
+		// Clean up and return
+		rs.close();
+		_ps.close();
+		return new ArrayList<Server>(results.values());
 	}
 }
