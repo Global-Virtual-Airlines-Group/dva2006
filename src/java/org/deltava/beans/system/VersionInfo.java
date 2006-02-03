709be5dda5199bd7f4021219a209d97deea5a56d
Luke Kolin <luke@deltava.org>
2006-02-03 18:44:40 +0000

    Add final release support
    git-svn-id: file:///home/svn/DVA2006@2218 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -13,8 +13,9 @@ public interface VersionInfo {
     public static final int MAJOR = 1;
     public static final int MINOR = 0;
     
-    public static final int BUILD = 67;
-    public static final int RELEASE_CANDIDATE = 17;
+    public static final int BUILD = 68;
+    public static final int RELEASE_CANDIDATE = 18;
+    public static final boolean FINAL = false;
     
     public static final String TXT_COPYRIGHT = "Copyright (c) 2004, 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.";
     public static final String HTML_COPYRIGHT = "Copyright &copy; 2004, 2005, 2006 <a rel=\"external\" href=\"http://www.gvagroup.org/\">Global Virtual Airlines Group</a>. All Rights Reserved.";
