9ba93c272b16500161aa4f60c76117b9abe1ad6f
Luke Kolin <luke@deltava.org>
2006-02-09 22:48:02 +0000

    Initial Academy newsletter support
    git-svn-id: file:///home/svn/DVA2006@2261 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetLibrary.java
+++ src/java/org/deltava/dao/GetLibrary.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2005 Luke J. Kolin. All Rights Reserved.
+// Copyright (c) 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao;
 
 import java.io.File;
@@ -45,82 +45,6 @@ public class GetLibrary extends DAO {
 		try {
 			prepareStatement(sqlBuf.toString());
 			return loadInstallers();
-		} catch (SQLException se) {
-			throw new DAOException(se);
-		}
-	}
-
-	/**
-	 * Returns the contents of the Document Library. This takes a database name so we can display the contents of other
-	 * airlines' libraries.
-	 * @param dbName the database name
-	 * @return a Collection of Manual beans
-	 * @throws DAOException if a JDBC error occurs
-	 */
-	public Collection<Manual> getManuals(String dbName) throws DAOException {
-
-		// Build the SQL statement
-		StringBuilder sqlBuf = new StringBuilder("SELECT D.*, COUNT(L.FILENAME) FROM ");
-		sqlBuf.append(dbName.toLowerCase());
-		sqlBuf.append(".DOCS D LEFT JOIN ");
-		sqlBuf.append(dbName.toLowerCase());
-		sqlBuf.append(".DOWNLOADS L ON (D.FILENAME=L.FILENAME) WHERE (D.NEWSLETTER=0) GROUP BY D.NAME");
-
-		try {
-			prepareStatement(sqlBuf.toString());
-			return loadManuals();
-		} catch (SQLException se) {
-			throw new DAOException(se);
-		}
-	}
-	
-	/**
-	 * Returns all newsletters from the Document Library. This takes a database name so we can display the contents of other
-	 * airlines' libraries.
-	 * @param dbName the database name
-	 * @return a Collection of Manual beans
-	 * @throws DAOException if a JDBC error occurs
-	 */
-	public Collection<Manual> getNewsletters(String dbName) throws DAOException {
-	   
-		// Build the SQL statement
-		StringBuilder sqlBuf = new StringBuilder("SELECT D.*, COUNT(L.FILENAME) FROM ");
-		sqlBuf.append(dbName.toLowerCase());
-		sqlBuf.append(".DOCS D LEFT JOIN ");
-		sqlBuf.append(dbName.toLowerCase());
-		sqlBuf.append(".DOWNLOADS L ON (D.FILENAME=L.FILENAME) WHERE (D.NEWSLETTER=1) GROUP BY D.NAME");
-
-		try {
-			prepareStatement(sqlBuf.toString());
-			return loadManuals();
-		} catch (SQLException se) {
-			throw new DAOException(se);
-		}
-	}
-
-	/**
-	 * Returns metadata about a specific Manual <i>in the current database</i>.
-	 * @param fName the filename
-	 * @return a Manual, or null if not found
-	 * @throws DAOException if a JDBC error occurs
-	 */
-	public Manual getManual(String fName, String dbName) throws DAOException {
-	   
-	   // Build the SQL statement
-	   StringBuilder sqlBuf = new StringBuilder("SELECT D.*, COUNT(L.FILENAME) FROM ");
-	   sqlBuf.append(dbName.toLowerCase());
-	   sqlBuf.append(".DOCS D LEFT JOIN ");
-	   sqlBuf.append(dbName.toLowerCase());
-	   sqlBuf.append(".DOWNLOADS L ON (D.FILENAME=L.FILENAME) WHERE (D.FILENAME=?) GROUP BY D.NAME");
-	   
-		try {
-		   setQueryMax(1);
-			prepareStatement(sqlBuf.toString());
-			_ps.setString(1, fName);
-
-			// Get results - if empty return null
-			List results = loadManuals();
-			return results.isEmpty() ? null : (Manual) results.get(0);
 		} catch (SQLException se) {
 			throw new DAOException(se);
 		}
@@ -258,38 +182,6 @@ public class GetLibrary extends DAO {
 	   rs.close();
 	   _ps.close();
 	   return results;
-	}
-
-	/**
-	 * Helper method to load from the Document Library table.
-	 */
-	private List<Manual> loadManuals() throws SQLException {
-
-		// Execute the query
-		ResultSet rs = _ps.executeQuery();
-		boolean hasTotals = (rs.getMetaData().getColumnCount() > 7);
-
-		// Iterate through the result set
-		List<Manual> results = new ArrayList<Manual>();
-		while (rs.next()) {
-			File f = new File(SystemData.get("path.library"), rs.getString(1));
-			Manual doc = new Manual(f.getPath());
-			doc.setName(rs.getString(2));
-			doc.setVersion(rs.getInt(4));
-			doc.setSecurity(rs.getInt(5));
-			doc.setDescription(rs.getString(6));
-			doc.setIsNewsletter(rs.getBoolean(7));
-			if (hasTotals)
-				doc.setDownloadCount(rs.getInt(8));
-
-			// Add to results
-			results.add(doc);
-		}
-
-		// Clean up and return
-		rs.close();
-		_ps.close();
-		return results;
 	}
 
 	/**
