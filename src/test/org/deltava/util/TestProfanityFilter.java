676b660afdbca92713621a4521b0e11e26a971fd
Luke Kolin <luke@deltava.org>
2006-02-15 04:09:36 +0000

    Misc fixes and unit test
    git-svn-id: file:///home/svn/DVA2006@2292 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/test/org/deltava/util/TestProfanityFilter.java
@@ -0,0 +1,36 @@
+package org.deltava.util;
+
+import java.io.*;
+import java.util.*;
+
+import junit.framework.TestCase;
+
+public class TestProfanityFilter extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		// Load the bad words
+		InputStream is = ConfigLoader.getStream("/etc/profanity.txt");
+		LineNumberReader lr = new LineNumberReader(new InputStreamReader(is));
+		Collection<String> words = new LinkedHashSet<String>();
+		while (lr.ready())
+			words.add(lr.readLine());
+
+		ProfanityFilter.init(words);
+	}
+
+	protected void tearDown() throws Exception {
+		ProfanityFilter.clear();
+		super.tearDown();
+	}
+
+	public void testFlag() {
+		assertTrue(ProfanityFilter.flag("You are a filthy mother fucker"));
+		assertFalse(ProfanityFilter.flag("A sucker or an idiot is born every minute"));
+	}
+	
+	public void testFilter() {
+		assertEquals("You are a filthy ****", ProfanityFilter.filter("You are a filthy mother fucker"));
+	}
+}
