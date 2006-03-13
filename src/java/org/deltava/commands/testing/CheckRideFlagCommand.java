7beeb516754dcb3024517512ce0cf7a2110f1b01
Luke Kolin <luke@deltava.org>
2006-03-13 14:13:59 +0000

    Allow marking as CheckRide
    git-svn-id: file:///home/svn/DVA2006@2460 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/commands/testing/CheckRideFlagCommand.java
@@ -0,0 +1,98 @@
+// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.commands.testing;
+
+import java.util.*;
+import java.sql.Connection;
+
+import org.deltava.beans.*;
+import org.deltava.beans.testing.*;
+
+import org.deltava.commands.*;
+import org.deltava.dao.*;
+
+import org.deltava.util.system.SystemData;
+
+/**
+ * A Web Site Command to retroactively flag a Flight Report as a Check Ride.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class CheckRideFlagCommand extends AbstractCommand {
+
+	/**
+	 * Executes the command.
+	 * @param ctx the Command context
+	 * @throws CommandException if an unhandled error occurs
+	 */
+	public void execute(CommandContext ctx) throws CommandException {
+
+		try {
+			Connection con = ctx.getConnection();
+			
+			// Get the Flight Report
+			GetFlightReports frdao = new GetFlightReports(con);
+			FlightReport fr = frdao.get(ctx.getID());
+			if (fr == null)
+				throw notFoundException("Invalid Flight Report - " + ctx.getID());
+			else if (!(fr instanceof ACARSFlightReport)) {
+				CommandException ce = new CommandException("Flight Report does not use ACARS");
+				ce.setLogStackDump(false);
+				throw ce;
+			}
+			
+			// Look for a check ride record - if not found, create a new check ride
+			GetExam exdao = new GetExam(con);
+			CheckRide cr = exdao.getCheckRide(SystemData.get("airline.db"), fr.getDatabaseID(FlightReport.DBID_PILOT),
+					fr.getEquipmentType(), Test.NEW);
+			if (cr == null) {
+				cr = new CheckRide(fr.getEquipmentType() + " Check Ride");
+				cr.setAircraftType(fr.getEquipmentType());
+				cr.setDate(fr.getCreatedOn());
+				cr.setFlightID(fr.getDatabaseID(FlightReport.DBID_ACARS));
+				cr.setStatus(Test.SUBMITTED);
+				cr.setSubmittedOn(fr.getDate());
+				
+				// Determine the equipment type based on the primary type
+				GetEquipmentType eqdao = new GetEquipmentType(con);
+				Collection<String> eqTypes = eqdao.getPrimaryTypes(SystemData.get("airline.db"), fr.getEquipmentType());
+				if (eqTypes.isEmpty())
+					throw notFoundException("No Equipment Type for " + fr.getEquipmentType());
+				
+				// Set the equipment type
+				cr.setEquipmentType(eqTypes.iterator().next());
+			} else {
+				cr.setFlightID(fr.getDatabaseID(FlightReport.DBID_ACARS));
+			}
+			
+			// Update the flight report
+			fr.setAttribute(FlightReport.ATTR_CHECKRIDE, true);
+			
+			// Start the transaction
+			ctx.startTX();
+			
+			// Save the flight report
+			SetFlightReport fwdao = new SetFlightReport(con);
+			fwdao.write(fr);
+			
+			// Save the check ride
+			SetExam ewdao = new SetExam(con);
+			ewdao.write(cr);
+			
+			// Commit the transaction
+			ctx.commitTX();
+		} catch (DAOException de) {
+			ctx.rollbackTX();
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
+		
+		// Forward to the JSP
+		CommandResult result = ctx.getResult();
+		result.setType(CommandResult.REDIRECT);
+		result.setURL("pirep", "read", ctx.getID());
+		result.setSuccess(true);
+	}
+}
