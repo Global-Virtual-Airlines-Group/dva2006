d1c83c1c0f6baeb0b8110c160d93067ae8034263
Luke Kolin <luke@deltava.org>
2006-03-24 05:47:09 +0000

    AJAX pilot map
    git-svn-id: file:///home/svn/DVA2006@2530 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/pilotMap.js
+++ static/js/pilotMap.js
@@ -1,17 +1,16 @@
 function generateXMLRequest(imgPath)
 {
 // Build the XML Requester
-var d = new Date();
 var xmlreq = GXmlHttp.create();
-xmlreq.open("GET", "pilotmap.ws?time=" + d.getTime(), true);
+xmlreq.open("GET", "pilotmap.ws", true);
 xmlreq.onreadystatechange = function() {
 	if (xmlreq.readyState != 4) return false;
+	var isLoading = getElement('isLoading');
+	isLoading.innerHTML = ' - REDRAWING...';
 	
 	// Parse the XML
 	var xmlDoc = xmlreq.responseXML;
 	var ac = xmlDoc.documentElement.getElementsByTagName("pilot");
-	map.clearOverlays();
-	pMarkers = new Array();
 	for (var i = 0; i < ac.length; i++) {
 		var a = ac[i];
 		var label = a.firstChild;
@@ -20,18 +19,65 @@ xmlreq.onreadystatechange = function() {
 		mrk.infoLabel = label.data;
 		mrk.rank = a.getAttribute("rank");
 		mrk.eqType = a.getAttribute("eqType");
+		mrk.infoShow = showInfo;
+		mrk.isVisible = true;
 		
-		// Set the the click handler
-		GEvent.bind(mrk, 'click', mrk, mrk.openInfoWindowHtml(mrk.infoLabel));
+		// Set the the click handler and add to the map
+		GEvent.bind(mrk, 'click', mrk, mrk.infoShow);
 		map.addOverlay(mrk);
-		pMarkers.push(mrk);
+		
+		// Save in the eqType and rank hashmaps
+		if (pMarkers[mrk.rank]) pMarkers[mrk.rank].push(mrk);
+		if (pMarkers[mrk.eqType]) pMarkers[mrk.eqType].push(mrk);
 	} // for
 	
 	// Restore the checkboxes
 	enableElement('eq', true);
 	enableElement('rnk', true);
+	isLoading.innerHTML = '';
 	return true;
 } // function
 
 return xmlreq;
+}
+
+function showInfo()
+{
+this.openInfoWindowHtml(this.infoLabel);
+return true;
+}
+
+function updateMarkers(checkbox)
+{
+var f = document.forms[0];
+
+// Get the markers we are going to add or remove
+var markers = pMarkers[checkbox.value];
+if (!markers) return false;
+
+// Load equipment types
+var showOptions = new Array();
+for (var x = 0; x < f.eqTypes.length; x++)
+	showOptions[f.eqTypes[x].value] = f.eqTypes[x];
+
+// Load rank names
+for (var x = 0; x < f.ranks.length; x++)
+	showOptions[f.ranks[x].value] = f.ranks[x];
+
+// Add or remove the markers
+for (var x = 0; x < markers.length; x++) {
+	var mrk = markers[x];
+	if (checkbox.checked && (!mrk.isVisible)) {
+		mrk.isVisible = true;
+		map.addOverlay(mrk);
+	} else if ((!checkbox.checked) && mrk.isVisible) {
+		var showMe = (showOptions[mrk.eqType].checked || showOptions[mrk.rank].checked);
+		if (!showMe) {
+			mrk.isVisible = false;
+			map.removeOverlay(mrk);
+		}
+	}
+}
+
+return true;
 }
