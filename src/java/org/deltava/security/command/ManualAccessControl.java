6269cbe1beff479ee84c6bf9e9bb5de506b3f2ea
Luke Kolin <luke@deltava.org>
2006-03-27 03:21:17 +0000

    Don't display Fleet Academy document unless in Course
    git-svn-id: file:///home/svn/DVA2006@2531 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/security/command/ManualAccessControl.java
@@ -0,0 +1,71 @@
+// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.security.command;
+
+import java.util.*;
+
+import org.deltava.beans.academy.*;
+import org.deltava.beans.fleet.*;
+
+import org.deltava.security.SecurityContext;
+
+import org.deltava.util.CollectionUtils;
+
+/**
+ * An Access Controller for Document Library entries.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class ManualAccessControl extends FleetEntryAccessControl {
+
+	private Collection<Course> _courses;
+
+	/**
+	 * Initializes the access controller.
+	 * @param ctx the security context
+	 * @param courses the User's active Flight Academy courses
+	 */
+	public ManualAccessControl(SecurityContext ctx, Collection<Course> courses) {
+		super(ctx, null);
+		_courses = courses;
+	}
+
+	/**
+	 * Updates the Fleet Entry to validate access to.
+	 * @param e the entry
+	 * @throws SecurityException if e is not a manual
+	 */
+	public final void setEntry(LibraryEntry e) {
+		if (!(e instanceof Manual))
+			throw new SecurityException("Invalid object - " + e.getClass().getSimpleName());
+
+		super.setEntry(e);
+	}
+
+	/**
+	 * Calculates access rights.
+	 */
+	public void validate() {
+		super.validate();
+
+		// If the entry has certifications attached, see if we have any courses.
+		Manual m = (Manual) _entry;
+		if (CollectionUtils.isEmpty(m.getCertifications()))
+			return;
+
+		// If we're an instructor/HR/Fleet, we're OK
+		if (_ctx.isUserInRole("Instructor") || _ctx.isUserInRole("HR") || _ctx.isUserInRole("Fleet"))
+			return;
+
+		// Check if we have any active courses
+		for (Iterator<Course> i = _courses.iterator(); i.hasNext();) {
+			Course c = i.next();
+			if (m.getCertifications().contains(c.getName()))
+				return;
+		}
+
+		// If we got this far, then disable read access
+		_canView = false;
+	}
+}
