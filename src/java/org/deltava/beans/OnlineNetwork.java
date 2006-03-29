4f24ee48fc380745499a159ca5d096591cfb0678
Luke Kolin <luke@deltava.org>
2006-03-29 16:30:36 +0000

    Add online network constant interface
    git-svn-id: file:///home/svn/DVA2006@2558 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/OnlineNetwork.java
@@ -0,0 +1,16 @@
+// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans;
+
+/**
+ * An interface to store Online Network constants.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public interface OnlineNetwork {
+
+	public static final String VATSIM = "VATSIM";
+	public static final String IVAO = "IVAO";
+	public static final String FPI = "FPI";
+}
