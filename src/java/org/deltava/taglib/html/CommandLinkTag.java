95591fc079fafd122886a83dc0cd0d948c86d310
Luke Kolin <luke@deltava.org>
2006-04-06 02:50:40 +0000

    Add startDate
    git-svn-id: file:///home/svn/DVA2006@2624 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/html/CommandLinkTag.java
+++ src/java/org/deltava/taglib/html/CommandLinkTag.java
@@ -1,6 +1,7 @@
-// Copyright (c) 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.html;
 
+import java.text.*;
 import java.util.*;
 
 import java.io.UnsupportedEncodingException;
@@ -9,7 +10,7 @@ import java.net.URLEncoder;
 import javax.servlet.jsp.JspException;
 
 /**
- * A JSP tag to create a link to a web site command.
+ * A JSP tag to create a link to a Web Site Command.
  * @author Luke
  * @version 1.0
  * @since 1.0
@@ -65,6 +66,15 @@ public class CommandLinkTag extends LinkTag {
 	 */
 	public void setUrl(String url) {
 		_cmdName = url.toLowerCase() + ".do";
+	}
+	
+	/**
+	 * Sets the start date if this is linking to a Calendar command.
+	 * @param dt the calendar start date/time
+	 */
+	public void setStartDate(Date dt) {
+		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+		_cmdParams.put("startDate", df.format(dt));
 	}
 	
 	/**
