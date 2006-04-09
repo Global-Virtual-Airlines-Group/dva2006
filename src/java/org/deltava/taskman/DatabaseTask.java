28c5d77adc34b587f69d334d0e00158cfd4a97a6
Luke Kolin <luke@deltava.org>
2006-04-09 15:49:39 +0000

    Add event assignment task and TX support to DatabaseTask
    git-svn-id: file:///home/svn/DVA2006@2643 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taskman/DatabaseTask.java
+++ src/java/org/deltava/taskman/DatabaseTask.java
@@ -1,7 +1,9 @@
-// Copyright 2005 Luke J. Kolin. All Rights Reserved.
+// Copyright 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taskman;
 
-import java.sql.Connection;
+import java.sql.*;
+
+import org.deltava.jdbc.TransactionException;
 
 /**
  * A class to support scheduled Tasks that access the database.
@@ -11,22 +13,84 @@ import java.sql.Connection;
  */
 
 public abstract class DatabaseTask extends Task {
-   
-   protected Connection _con;
-   
-   /**
-    * Creates a new Database Task.
-    * @param name the task name
-    */
-   protected DatabaseTask(String name, Class loggerClass) {
-      super(name, loggerClass);
-   }
 
-   /**
-    * Sets the JDBC connection that this Task will use.
-    * @param c the JDBC connection
-    */
-   public void setConnection(Connection c) {
-      _con = c;
-   }
+	protected Connection _con;
+	private boolean _manualCommit = false;
+	private boolean _oldCommitLevel;
+
+	/**
+	 * Creates a new Database Task.
+	 * @param name the task name
+	 */
+	protected DatabaseTask(String name, Class loggerClass) {
+		super(name, loggerClass);
+	}
+
+	/**
+	 * Sets the JDBC connection that this Task will use.
+	 * @param c the JDBC connection
+	 */
+	public void setConnection(Connection c) {
+		_con = c;
+		try {
+			_manualCommit = !c.getAutoCommit();
+		} catch (Exception e) {
+		}
+	}
+	
+	/**
+	 * Marks the start of a multi-step database transaction. This turns off the autoCommit property of the JDBC
+	 * connection, if it is already set.
+	 * @throws TransactionException if a JDBC error occurs.
+	 * @see DatabaseTask#commitTX()
+	 * @see DatabaseTask#rollbackTX()
+	 * @see Connection#setAutoCommit(boolean)
+	 */
+	protected void startTX() throws TransactionException {
+		if (!_manualCommit) {
+			try {
+				_oldCommitLevel = _con.getAutoCommit();
+				_con.setAutoCommit(false);
+			} catch (SQLException se) {
+				throw new TransactionException(se);
+			}
+		}
+	}
+
+	/**
+	 * Commits a multi-step transaction to the database. This calls {@link Connection#commit()}on the JDBC connection,
+	 * and then restores the old autoCommit property for the Connection.
+	 * @throws TransactionException if a JDBC error occurs
+	 * @see DatabaseTask#startTX()
+	 * @see DatabaseTask#rollbackTX()
+	 * @see Connection#setAutoCommit(boolean)
+	 */
+	protected void commitTX() throws TransactionException {
+		if (!_manualCommit) {
+			try {
+				_con.commit();
+				_con.setAutoCommit(_oldCommitLevel);
+			} catch (SQLException se) {
+				throw new TransactionException(se);
+			}
+		}
+	}
+
+	/**
+	 * Rolls back a multi-step transaction before it is completed. This calls {@link Connection#rollback()}on the JDBC
+	 * connection, and then resotres the old autoCommit property for the Connection. Since this is designed to be called
+	 * in catch blocks, it eats exceptions.
+	 * @see DatabaseTask#startTX()
+	 * @see DatabaseTask#commitTX()
+	 * @see Connection#setAutoCommit(boolean)
+	 */
+	protected void rollbackTX() {
+		if (!_manualCommit) {
+			try {
+				_con.rollback();
+				_con.setAutoCommit(_oldCommitLevel);
+			} catch (Exception e) {
+			}
+		}
+	}
 }
---
+++ src/java/org/deltava/tasks/EventAssignTask.java
@@ -0,0 +1,152 @@
+// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.tasks;
+
+import java.util.*;
+
+import org.deltava.beans.*;
+import org.deltava.beans.assign.*;
+import org.deltava.beans.event.*;
+import org.deltava.beans.system.*;
+
+import org.deltava.dao.*;
+import org.deltava.mail.*;
+
+import org.deltava.taskman.DatabaseTask;
+import org.deltava.util.system.SystemData;
+
+/**
+ * A Scheduled Task to automatically assign flghts to Online Event participants
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class EventAssignTask extends DatabaseTask {
+
+	/**
+	 * Initializes the Scheduled Task.
+	 */
+	public EventAssignTask() {
+		super("Event Assignment", EventAssignTask.class);
+	}
+	
+	private boolean hasFlightReports(int eventID, UserDataMap udmap) throws DAOException {
+		
+		// Load the flight reports
+		GetFlightReports frdao = new GetFlightReports(_con);
+		Collection<FlightReport> pireps = new ArrayList<FlightReport>();
+		for (Iterator<String> i = udmap.getTableNames().iterator(); i.hasNext(); ) {
+			String tableName = i.next();
+			pireps.addAll(frdao.getByEvent(eventID, tableName));
+		}
+		
+		return !pireps.isEmpty();
+	}
+
+	/**
+	 * Executes the Task.
+	 */
+	protected void execute() {
+		
+		// Create the message context
+		MessageContext mctxt = new MessageContext();
+		try {
+			// Determining who we are operating as
+			GetPilot pdao = new GetPilot(_con);
+			Pilot from = pdao.getByName(SystemData.get("online.events_assigned_by"), SystemData.get("airline.db"));
+			mctxt.addData("user", from);
+			
+			// Get the DAOs
+			GetEvent dao = new GetEvent(_con);
+			GetUserData usrdao = new GetUserData(_con);
+			
+			// Initialize the message template
+			GetMessageTemplate mtdao = new GetMessageTemplate(_con);
+			mctxt.setTemplate(mtdao.get("EVENTASSIGN"));
+			
+			// Get the events to check
+			Collection<Event> events = dao.getAssignableEvents();
+			for (Iterator<Event> i = events.iterator(); i.hasNext(); ) {
+				Event e = i.next();
+				UserDataMap usrmap = usrdao.getByEvent(e.getID());
+				
+				// Determine if we need to asign flights
+				if ((!e.getSignups().isEmpty()) && (!hasFlightReports(e.getID(), usrmap))) {
+					mctxt.addData("event", e);
+					
+					// Get the write DAOs
+					SetFlightReport fwdao = new SetFlightReport(_con);
+					SetAssignment awdao = new SetAssignment(_con);
+					
+					// Start the transaction
+					startTX();
+					
+					// Get the signups for this event
+					log.info("Assigning flights for Event " + e.getName());
+					for (Iterator<Signup> si = e.getSignups().iterator(); si.hasNext(); ) {
+						Signup s = si.next();
+
+						// Get the Pilot
+						UserData usrData = (UserData) usrmap.get(s.getPilotID());
+						Pilot usr = pdao.get(usrData);
+						mctxt.addData("pilot", usr);
+
+						// Create a Flight Assignment
+						AssignmentInfo ai = new AssignmentInfo(s.getEquipmentType());
+						ai.setAssignDate(new Date());
+						ai.setPilotID(s.getPilotID());
+						ai.setEventID(e.getID());
+						ai.setStatus(AssignmentInfo.RESERVED);
+						
+						// Create an Assignment Leg
+						AssignmentLeg leg = new AssignmentLeg(SystemData.getAirline(usrData.getAirlineCode()), usr.getPilotNumber(),1);
+						leg.setEquipmentType(s.getEquipmentType());
+						leg.setAirportD(s.getAirportD());
+						leg.setAirportA(s.getAirportA());
+						ai.addAssignment(leg);
+						
+						// Write the Flight Assignment
+						awdao.write(ai, usrData.getDB());
+
+						// Create a Flight Report
+						FlightReport fr = new FlightReport(leg);
+						fr.setDatabaseID(FlightReport.DBID_PILOT, s.getPilotID());
+						fr.setDatabaseID(FlightReport.DBID_ASSIGN, ai.getID());
+						fr.setDatabaseID(FlightReport.DBID_EVENT, e.getID());
+						fr.setDate(e.getStartTime());
+						switch (e.getNetwork()) {
+							case Event.NET_VATSIM :
+								fr.setAttribute(FlightReport.ATTR_VATSIM, true);
+								break;
+								
+							case Event.NET_IVAO :
+								fr.setAttribute(FlightReport.ATTR_IVAO, true);
+								break;
+						}
+						
+						// Write the Flight Report to the proper database
+						fwdao.write(fr, usrData.getDB());
+						mctxt.addData("pirep", fr);
+						
+						// Log flight
+						log.info("Assigned Flight for " + usr.getName());
+						
+						// Send the message
+						Mailer mailer = new Mailer(from);
+						mailer.setContext(mctxt);
+						mailer.send(usr);
+					}
+					
+					// Commit the transaction
+					commitTX();
+				}
+			}
+		} catch (DAOException de) {
+			rollbackTX();
+			log.error(de.getMessage(), de);
+		}
+
+		// Log completion
+		log.info("Processing Complete");
+	}
+}
