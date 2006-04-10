500b45400ea80c1d286c5667d4a3125977804fb4
Luke Kolin <luke@deltava.org>
2006-04-10 04:06:13 +0000

    Update ACARS interface to web application
    git-svn-id: file:///home/svn/DVA2006@2648 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/acars/ACARSAdminInfo.java
+++ src/java/org/deltava/beans/acars/ACARSAdminInfo.java
@@ -12,8 +12,33 @@ import java.util.Collection;
 
 public interface ACARSAdminInfo {
 
-   public Collection getPoolInfo();
-   public Collection<RouteEntry> getMapEntries();
-   public Collection getBanInfo();
-   public boolean isEmpty();
+	/**
+	 * Returns all current Connection data.
+	 * @return a Collection of ACARSConnection beans
+	 */
+	public Collection getPoolInfo();
+
+	/**
+	 * Returns the positions of all ACARS flights.
+	 * @return a Collection of RouteEntry beans
+	 */
+	public Collection<RouteEntry> getMapEntries();
+
+	/**
+	 * Returns the curent ACARS Flight IDs.
+	 * @return a Collection of Integer flight IDs
+	 */
+	public Collection<Integer> getFlightIDs();
+
+	/**
+	 * Returns all live ACARS connection bans.
+	 * @return a Collection of ban objects
+	 */
+	public Collection getBanInfo();
+
+	/**
+	 * Returns if there are any ACARS connections.
+	 * @return TRUE if there is at least one connection, otherwise FALSE
+	 */
+	public boolean isEmpty();
 }
