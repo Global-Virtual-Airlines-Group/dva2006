4011d4edb0baa94b4326195282115da01cf6e242
Luke Kolin <luke@deltava.org>
2006-04-11 21:29:02 +0000

    Live ACARS google map service
    git-svn-id: file:///home/svn/DVA2006@2664 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/service/GoogleEarthService.java
+++ src/java/org/deltava/service/GoogleEarthService.java
@@ -5,7 +5,6 @@ import java.util.*;
 
 import org.jdom.Element;
 
-import org.deltava.beans.GeoLocation;
 import org.deltava.beans.acars.*;
 import org.deltava.beans.navdata.NavigationDataBean;
 import org.deltava.beans.schedule.Airport;
@@ -80,81 +79,86 @@ public abstract class GoogleEarthService extends WebDataService {
 			return buf.toString();
 		}
 		
-		public GoogleEarthColor dim(int factor) {
-			return new GoogleEarthColor(_red / factor, _green / factor, _blue / factor, _alpha / factor);
+		public GoogleEarthColor dim(float factor) {
+			return new GoogleEarthColor(Math.round(_red / factor), Math.round(_green / factor),
+					Math.round(_blue / factor), Math.round(_alpha / factor));
 		}
 	}
 	
 	/**
-	 * Helper method to generate an icon element.
-	 * @param palette the Google Earth pallete
-	 * @param pX the icon x-offset in the palette
-	 * @param pY the icon y-offset in the palette
+	 * Helper method to generate an airport Placemark element.
+	 * @param a the Airport to render
+	 * @param desc the placemark description
+	 * @return a KML Placemark element
 	 */
-	protected Element addIcon(int palette, int pX, int pY) {
-		Element re = new Element("IconStyle");
-		Element e = new Element("Icon");
-		e.addContent(XMLUtils.createElement("href", "root://icons/palette-" + palette + ".png", true));
-		e.addContent(XMLUtils.createElement("x", String.valueOf(pX * 32)));
-		e.addContent(XMLUtils.createElement("y", String.valueOf(pY * 32)));
-		e.addContent(XMLUtils.createElement("w", "32"));
-		e.addContent(XMLUtils.createElement("h", "32"));
-		re.addContent(e);
-		return re;
-	}
-
-	/**
-	 * Helper method to generate a LookAt element.
-	 * @param loc the location co-ordinates
-	 * @param altitude the altitude of the eyepoint
-	 * @param heading the direction of the eyepoint
-	 */
-	protected Element addLookAt(GeoLocation loc, int altitude, int heading) {
-		Element e = new Element("LookAt");
-		e.addContent(XMLUtils.createElement("longitude", StringUtils.format(loc.getLongitude(), "##0.0000")));
-		e.addContent(XMLUtils.createElement("latitude", StringUtils.format(loc.getLatitude(), "##0.0000")));
-		e.addContent(XMLUtils.createElement("range", StringUtils.format(0.3048d * altitude, "##0.000")));
-		if (heading == -1) {
-			e.addContent(XMLUtils.createElement("heading", "0.00"));
-			e.addContent(XMLUtils.createElement("tilt", "85.0000"));
-		} else {
-			e.addContent(XMLUtils.createElement("heading", StringUtils.format(heading, "##0.00")));
-			e.addContent(XMLUtils.createElement("tilt", "55.0000"));
-		}
-
-		return e;
-	}
-	
-	/**
-	 * Helper method to turn a flight record into a KML element.
-	 * @param info the ACARS Flight Information bean for the flight
-	 * @param showPositionData TRUE if position data should be shown, otherwise FALSE
-	 * @param routeColor the color of the route line
-	 */
-	protected Collection<Element> createFlight(FlightInfo info, boolean showPositionData, GoogleEarthColor routeColor) {
-
-		// Create results
-		Collection<Element> results = new ArrayList<Element>();
-
-		// Generate the start airport placemark
-		Airport aD = info.getAirportD();
+	protected Element createAirport(Airport a, String desc) {
+		
+		// Create the elemnet
 		Element ade = new Element("Placemark");
-		ade.addContent(XMLUtils.createElement("description", "Departed from " + aD.getName()));
-		ade.addContent(XMLUtils.createElement("name", aD.getName() + " (" + aD.getICAO() + ")"));
+		ade.addContent(XMLUtils.createElement("description", desc));
+		ade.addContent(XMLUtils.createElement("name", a.getName() + " (" + a.getICAO() + ")"));
 		ade.addContent(XMLUtils.createElement("visibility", "1"));
 		Element ads = new Element("Style");
-		ads.addContent(addIcon(2, 0, 1)); // airplane icon
+		ads.addContent(KMLUtils.createIcon(2, 0, 1)); // airplane icon
 		ade.addContent(ads);
 		Element adp = new Element("Point");
-		adp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(aD)));
+		adp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(a)));
 		ade.addContent(adp);
-		ade.addContent(addLookAt(aD, 480, -1));
-		results.add(ade);
+		ade.addContent(KMLUtils.createLookAt(a, a.getAltitude() + 1000, -1));
+		return ade;
+	}
+	
+	/**
+	 * Helper method to generate an aircraft Placemark element.
+	 * @param name the entry title/name
+	 * @param entry the position/flight data
+	 * @return a KML Placemark element
+	 */
+	protected Element createAircraft(String name, RouteEntry entry) {
+		
+		// Create the elemnet
+		Element ae = new Element("Placemark");
+		ae.addContent(XMLUtils.createElement("description", entry.getInfoBox(), true));
+		ae.addContent(XMLUtils.createElement("name", name));
+		ae.addContent(XMLUtils.createElement("visibility", "1"));
+		Element ads = new Element("Style");
+		Element ais = KMLUtils.createIcon(2, 0, 0); // airplane icon
+		ais.addContent(XMLUtils.createElement("scale", "0.70"));
+		ais.addContent(XMLUtils.createElement("heading", StringUtils.format(entry.getHeading(), "##0.00")));
+		ads.addContent(ais);
+		ae.addContent(ads);
 
-		// Format the route
+		// Create the actual point
+		Element pe = new Element("Point");
+		pe.addContent(KMLUtils.createLookAt(entry, entry.getAltitude() + 1500, entry.getHeading()));
+		if (entry.isFlagSet(ACARSFlags.FLAG_ONGROUND)) {
+			pe.addContent(XMLUtils.createElement("coordinates", GeoUtils.format2D(entry)));
+			pe.addContent(XMLUtils.createElement("altitudeMode", "clampedToGround"));
+		} else if (entry.getRadarAltitude() < 1000) {
+			pe.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(entry, entry.getRadarAltitude())));
+			pe.addContent(XMLUtils.createElement("altitudeMode", "relativeToGround"));
+		} else {
+			pe.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(entry, entry.getAltitude())));
+			pe.addContent(XMLUtils.createElement("altitudeMode", "absolute"));
+		}
+		
+		// Add the point and return
+		ae.addContent(pe);
+		return ae;
+	}
+	
+	/**
+	 * Helper method to render flight progress as a KML Placemark / LineString element.
+	 * @param positions a Collection of RouteEntry beans
+	 * @param routeColor the line/fill color
+	 * @return a KML Placemark element containing the flight progress
+	 */
+	protected Element createProgress(Collection<RouteEntry> positions, GoogleEarthColor routeColor) {
+		
+		// Set the placemark options
 		Element le = new Element("Placemark");
 		le.addContent(XMLUtils.createElement("name", "Flight Route"));
-		le.addContent(XMLUtils.createElement("description", String.valueOf(info.getRouteData().size()) + " Position Records"));
+		le.addContent(XMLUtils.createElement("description", String.valueOf(positions.size()) + " Position Records"));
 		le.addContent(XMLUtils.createElement("visibility", "1"));
 		Element ls = new Element("Style");
 		Element lce = XMLUtils.createElement("LineStyle", "color", routeColor.toString());
@@ -169,76 +173,65 @@ public abstract class GoogleEarthService extends WebDataService {
 
 		// Format all of the coordinates
 		StringBuilder buf = new StringBuilder();
-		for (Iterator<RouteEntry> i = info.getRouteData().iterator(); i.hasNext();) {
+		for (Iterator<RouteEntry> i = positions.iterator(); i.hasNext();) {
 			RouteEntry entry = i.next();
 			buf.append(GeoUtils.format3D(entry, entry.getRadarAltitude()));
 			buf.append(" \n");
 		}
 
-		// Save the coordinates
+		// Save the coordinates and return
 		lse.addContent(XMLUtils.createElement("coordinates", buf.toString()));
 		le.addContent(lse);
-		results.add(le);
-
-		// Generate the end airport placemark
-		Airport aA = info.getAirportA();
-		Element aae = new Element("Placemark");
-		aae.addContent(XMLUtils.createElement("description", "Landed at " + aA.getName()));
-		aae.addContent(XMLUtils.createElement("name", aA.getName() + " (" + aA.getICAO() + ")"));
-		aae.addContent(XMLUtils.createElement("visibility", "1"));
-		Element aas = new Element("Style");
-		aas.addContent(addIcon(2, 0, 1)); // airplane icon
-		aae.addContent(aas);
-		Element aap = new Element("Point");
-		aap.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(aA)));
-		aae.addContent(aap);
-		aae.addContent(addLookAt(aA, 1500, -1));
-		results.add(aae);
-
-		// Format the positions
-		if (showPositionData) {
-			Element fe = new Element("Folder");
-			fe.addContent(XMLUtils.createElement("name", "Position Records"));
-			fe.addContent(XMLUtils.createElement("visibility", "0"));
-			results.add(fe);
-			int pos = 0;
-			for (Iterator<RouteEntry> i = info.getRouteData().iterator(); i.hasNext();) {
-				RouteEntry entry = i.next();
-				Element pe = new Element("Placemark");
-				pe.addContent(XMLUtils.createElement("name", "Route Point #" + String.valueOf(++pos)));
-				pe.addContent(XMLUtils.createElement("description", entry.getInfoBox(), true));
-				pe.addContent(XMLUtils.createElement("visibility", "0"));
-				Element ps = new Element("Style");
-				Element pis = addIcon(2, 0, 0); // info icon
-				pis.addContent(XMLUtils.createElement("scale", "0.70"));
-				pis.addContent(XMLUtils.createElement("heading", StringUtils.format(entry.getHeading(), "##0.00")));
-				ps.addContent(pis);
-				pe.addContent(ps);
-				Element pp = new Element("Point");
-				if (entry.isFlagSet(ACARSFlags.FLAG_ONGROUND)) {
-					pp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format2D(entry)));
-					pp.addContent(XMLUtils.createElement("altitudeMode", "clampedToGround"));
-				} else if (entry.getRadarAltitude() < 1000) {
-					pp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(entry, entry
-							.getRadarAltitude())));
-					pp.addContent(XMLUtils.createElement("altitudeMode", "relativeToGround"));
-				} else {
-					pp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(entry, entry.getAltitude())));
-					pp.addContent(XMLUtils.createElement("altitudeMode", "absolute"));
-				}
-
-				pe.addContent(pp);
-				pe.addContent(addLookAt(entry, entry.getAltitude(), entry.getHeading()));
-				fe.addContent(pe);
+		return le;
+	}
+	
+	/**
+	 * Helper method to generate flight position data as a KML Folder element.
+	 * @param positions a Collection of PositionEntry beans
+	 * @param isVisible TRUE if the data points should be visible, otherwise FALSE 
+	 * @return a KML Folder element
+	 */
+	protected Element createPositionData(Collection<RouteEntry> positions, boolean isVisible) {
+		
+		// Create the folder
+		Element fe = new Element("Folder");
+		fe.addContent(XMLUtils.createElement("name", "Position Records"));
+		fe.addContent(XMLUtils.createElement("visibility", isVisible ? "1" : "0"));
+		
+		// Render the position data
+		int pos = 0;
+		for (Iterator<RouteEntry> i = positions.iterator(); i.hasNext();) {
+			RouteEntry entry = i.next();
+			Element pe = new Element("Placemark");
+			pe.addContent(XMLUtils.createElement("name", "Route Point #" + String.valueOf(++pos)));
+			pe.addContent(XMLUtils.createElement("description", entry.getInfoBox(), true));
+			pe.addContent(XMLUtils.createElement("visibility", isVisible ? "1" : "0"));
+			Element ps = new Element("Style");
+			Element pis = KMLUtils.createIcon(2, 0, 0); // info icon
+			pis.addContent(XMLUtils.createElement("scale", "0.70"));
+			pis.addContent(XMLUtils.createElement("heading", StringUtils.format(entry.getHeading(), "##0.00")));
+			ps.addContent(pis);
+			pe.addContent(ps);
+			Element pp = new Element("Point");
+			if (entry.isFlagSet(ACARSFlags.FLAG_ONGROUND)) {
+				pp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format2D(entry)));
+				pp.addContent(XMLUtils.createElement("altitudeMode", "clampedToGround"));
+			} else if (entry.getRadarAltitude() < 1000) {
+				pp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(entry, entry
+						.getRadarAltitude())));
+				pp.addContent(XMLUtils.createElement("altitudeMode", "relativeToGround"));
+			} else {
+				pp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(entry, entry.getAltitude())));
+				pp.addContent(XMLUtils.createElement("altitudeMode", "absolute"));
 			}
+
+			pe.addContent(pp);
+			pe.addContent(KMLUtils.createLookAt(entry, entry.getAltitude(), entry.getHeading()));
+			fe.addContent(pe);
 		}
-		
-		// Format the flight plan data
-		if (info.hasPlanData())
-			results.add(createFlightRoute("Flight Plan", info.getPlanData(), false));
-		
-		// Return results
-		return results;
+
+		// Return the folder element
+		return fe;
 	}
 	
 	/**
@@ -283,7 +276,7 @@ public abstract class GoogleEarthService extends WebDataService {
 			pp.addContent(XMLUtils.createElement("coordinates", GeoUtils.format3D(wp, 10)));
 			pp.addContent(XMLUtils.createElement("altitudeMode", "relativeToGround"));
 			pe.addContent(pp);
-			pe.addContent(addLookAt(wp, 4500, rnd.nextInt(360)));
+			pe.addContent(KMLUtils.createLookAt(wp, 4500, rnd.nextInt(360)));
 			
 			// Format route point
 			pbuf.append(GeoUtils.format3D(wp, 0));
@@ -295,14 +288,14 @@ public abstract class GoogleEarthService extends WebDataService {
 			switch (wp.getType()) {
 				case NavigationDataBean.NDB:
 				case NavigationDataBean.VOR:
-					pis = addIcon(4, 0, 1);
+					pis = KMLUtils.createIcon(4, 0, 1);
 					pis.addContent(XMLUtils.createElement("scale", "0.60"));
 					ps.addContent(pis);
 					pe.addContent(ps);
 					break;
 					
 				case NavigationDataBean.INT:
-					pis = addIcon(4, 4, 0); // info icon
+					pis = KMLUtils.createIcon(4, 4, 0); // info icon
 					pis.addContent(XMLUtils.createElement("scale", "0.50"));
 					ps.addContent(pis);
 					pe.addContent(ps);
