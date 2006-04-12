ec9f9329c75804a0934a45d553c73b1bbd6a8a62
Luke Kolin <luke@deltava.org>
2006-04-12 19:37:52 +0000

    Build FIR boundary overlay for Google Earth
    git-svn-id: file:///home/svn/DVA2006@2674 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/test/org/deltava/util/TestKMLMerge.java
@@ -0,0 +1,64 @@
+package org.deltava.util;
+
+import java.io.*;
+
+import org.jdom.*;
+import org.jdom.input.*;
+
+import junit.framework.TestCase;
+
+public class TestKMLMerge extends TestCase {
+
+	private Document _doc;
+	private Element _de;
+	
+	private class KMLFilter implements FileFilter {
+		
+		public boolean accept(File f) {
+			return f.getName().endsWith(".kml");
+		}
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		// Build the XML document
+		_doc = new Document();
+		Element ke = new Element("kml");
+		_doc.setRootElement(ke);
+		_de = new Element("Document");
+		ke.addContent(_de);
+	}
+	
+	public void testFIRMerge() throws IOException {
+		
+		SAXBuilder builder = new SAXBuilder();
+		
+		File f = new File("c:\\temp\\fir");
+		File[] files = f.listFiles(new KMLFilter());
+		for (int x = 0; x < files.length; x++) {
+			File fn = files[x];
+			System.out.println("Processing " + fn.getName());
+			
+			try {
+				Document doc = builder.build(fn);
+				Element ke = doc.getRootElement();
+				Element pe = ke.getChild("Placemark", ke.getNamespace());
+				if (pe == null) {
+					System.err.println("No Placemark element");
+				} else {
+					Element pe2 = (Element) pe.clone();
+					KMLUtils.setVisibility(pe2, false);
+					_de.addContent(pe2);
+				}
+			} catch (JDOMException je) {
+				System.err.println("Error - " + je.getMessage());
+			}
+		}
+		
+		// Write the XML
+		PrintWriter pw = new PrintWriter(new FileWriter("c:\\temp\\firs.kml"));
+		pw.println(XMLUtils.format(_doc));
+		pw.close();
+	}
+}
