f86482192795efdb71cd0eeb95a5ce1a97877b82
Luke Kolin <luke@deltava.org>
2006-04-12 19:38:18 +0000

    Add normalize and bearingPoint
    git-svn-id: file:///home/svn/DVA2006@2675 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/util/TestGeoUtils.java
+++ src/test/org/deltava/util/TestGeoUtils.java
@@ -3,19 +3,13 @@ package org.deltava.util;
 
 import java.util.*;
 
-import org.hansel.CoverageDecorator;
-
-import junit.framework.Test;
 import junit.framework.TestCase;
 
+import org.deltava.beans.GeoLocation;
 import org.deltava.beans.schedule.GeoPosition;
 
 public class TestGeoUtils extends TestCase {
 
-	public static Test suite() {
-		return new CoverageDecorator(TestGeoUtils.class, new Class[] { GeoUtils.class });
-	}
-	
 	public void testGreatCircle() {
 		GeoPosition ogg = new GeoPosition(20.8986, -156.4305);
 		GeoPosition lax = new GeoPosition(33.9425, -118.4080);
@@ -28,5 +22,22 @@ public class TestGeoUtils extends TestCase {
 			assertTrue(gp.distanceTo(gp2) < 100);
 			gp = gp2;
 		}
+	}
+	
+	public void testNormalize() {
+		assertEquals(1, GeoUtils.normalize(361), 0.0);
+		assertEquals(116, GeoUtils.normalize(116), 0.0);
+		assertEquals(359, GeoUtils.normalize(-1), 0.0);
+		assertEquals(116, GeoUtils.normalize(476), 0.0);
+		assertEquals(116, GeoUtils.normalize(836), 0.0);
+	}
+	
+	public void testBearingPoint() {
+		GeoLocation l1 = new GeoPosition(0.00, 0.00);
+		
+		GeoLocation l2 = GeoUtils.bearingPoint(l1, GeoLocation.DEGREE_MILES, 90);
+		assertEquals(GeoLocation.DEGREE_MILES, GeoUtils.distance(l1, l2), 0.5);
+		GeoLocation l3 = GeoUtils.bearingPoint(l1, 15, -40);
+		assertEquals(15, GeoUtils.distance(l1, l3), 0.5);
 	}
 }
