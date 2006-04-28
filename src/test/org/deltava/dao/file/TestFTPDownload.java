11a96232c1dfaaee41d6f5487dc7fdae2b0f7a66
Luke Kolin <luke@deltava.org>
2006-04-28 20:50:28 +0000

    Innovata FTP download support
    git-svn-id: file:///home/svn/DVA2006@2727 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/test/org/deltava/dao/file/TestFTPDownload.java
@@ -0,0 +1,40 @@
+package org.deltava.dao.file;
+
+import junit.framework.TestCase;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.deltava.util.system.SystemData;
+
+import com.enterprisedt.net.ftp.*;
+
+public class TestFTPDownload extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		PropertyConfigurator.configure("data/log4j.test.properties");
+		SystemData.init("org.deltava.util.system.XMLSystemDataLoader");
+	}
+
+	public void testDownload() throws Exception {
+		assertNotNull(SystemData.get("schedule.innovata.download.host"));
+		assertNotNull(SystemData.get("schedule.innovata.download.user"));
+		assertNotNull(SystemData.get("schedule.innovata.download.pwd"));
+		assertNotNull(SystemData.get("schedule.innovata.download.file"));
+		
+		// Create the session and connect
+		FTPClient ftp = new FTPClient();
+		ftp.setRemoteHost(SystemData.get("schedule.innovata.download.host"));
+		ftp.connect();
+		assertTrue(ftp.connected());
+		
+		// Log in
+		try {
+			ftp.login(SystemData.get("schedule.innovata.download.user"), SystemData.get("schedule.innovata.download.pwd"));
+			byte[] buffer = ftp.get(SystemData.get("schedule.innovata.download.file"));
+			assertNotNull(buffer);
+			ftp.quit();
+		} catch (FTPException fe) {
+			fail(fe.getMessage());
+		}
+	}
+}
