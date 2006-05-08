c4ca5e504d504f5d4e702329254c0f10ac45f43f
Luke Kolin <luke@deltava.org>
2006-05-08 22:10:57 +0000

    Add performance metrics dashboard
    git-svn-id: file:///home/svn/DVA2006@2747 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/stats/PerformanceMetrics.java
@@ -0,0 +1,113 @@
+// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.stats;
+
+/**
+ * A bean to track performance metrics data.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class PerformanceMetrics implements java.io.Serializable, Comparable {
+
+	private String _name;
+	private double _avg;
+	private double _max;
+	private double _min;
+	private long _count;
+	
+	/**
+	 * Creates a new performance metrics bean.
+	 * @param catName the category name
+	 * @throws NullPointerException if catName is null
+	 * @see PerformanceMetrics#getName()
+	 */
+	public PerformanceMetrics(String catName) {
+		super();
+		setName(catName);
+	}
+	
+	/**
+	 * Returns the category name.
+	 * @return the name
+	 */
+	public String getName() {
+		return _name;
+	}
+	
+	/**
+	 * Returns the category count.
+	 * @return the count
+	 */
+	public long getCount() {
+		return _count;
+	}
+	
+	/**
+	 * Returns the average.
+	 * @return the average
+	 */
+	public double getAverage() {
+		return _avg;
+	}
+	
+	/**
+	 * Returns the maximum value.
+	 * @return the maximum
+	 */
+	public double getMaximum() {
+		return _max;
+	}
+	
+	/**
+	 * Returns the minimum value.
+	 * @return the minimum
+	 */
+	public double getMinimum() {
+		return _min;
+	}
+	
+	/**
+	 * Updates the category name.
+	 * @param name the category name
+	 * @throws NullPointerException if name is null
+	 */
+	public void setName(String name) {
+		_name = name.trim();
+	}
+	
+	/**
+	 * Updates the average value.
+	 * @param avg the average
+	 */
+	public void setAverage(double avg) {
+		_avg = avg;
+	}
+	
+	/**
+	 * Updates the category count.
+	 * @param count the count
+	 */
+	public void setCount(long count) {
+		_count = count;
+	}
+	
+	/**
+	 * Updates the data series limits.
+	 * @param mn the minimum
+	 * @param mx the maximum
+	 */
+	public void setLimits(double mn, double mx) {
+		_max = mx;
+		_min = mn;
+	}
+
+	/**
+	 * Compares two entries by comparing their category names.
+	 * @see Comparable#compareTo(Object)
+	 */
+	public int compareTo(Object o) {
+		PerformanceMetrics m2 = (PerformanceMetrics) o;
+		return _name.compareTo(m2._name);
+	}
+}
