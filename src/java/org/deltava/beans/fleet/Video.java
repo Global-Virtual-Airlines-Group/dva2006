0184c5cdb7ff362154b245ce38fc497b365b47cc
Luke Kolin <luke@deltava.org>
2006-05-10 21:46:08 +0000

    Initial video support
    git-svn-id: file:///home/svn/DVA2006@2757 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/fleet/Video.java
@@ -0,0 +1,57 @@
+// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.fleet;
+
+import org.deltava.util.StringUtils;
+
+/**
+ * A bean to store video data.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class Video extends FileEntry {
+	
+	public static final int WMF = 0;
+	public static final int AVI = 1;
+	
+	public static final String[] TYPES = {"WMF", "AVI" };
+	
+	private int _type;
+
+	/**
+	 * Creates a new video bean. 
+	 * @param fName the file name
+	 * @throws NullPointerException if fName is null
+	 * @throws IllegalArgumentException if fName has an unknown extension
+	 */
+	public Video(String fName) {
+		super(fName);
+		setType(fName);
+	}
+
+	/**
+	 * Returns the video type.
+	 * @return the type code
+	 */
+	public int getType() {
+		return _type;
+	}
+	
+	/**
+	 * Updates the video type.
+	 * @param fName the file name
+	 * @throws IllegalArgumentException if fName has an unknown extension
+	 */
+	public void setType(String fName) {
+		if (fName.indexOf('.') == 1)
+			throw new IllegalArgumentException("Invalid file name - " + fName);
+		
+		String ext = fName.substring(fName.lastIndexOf('.') + 1).toUpperCase();
+		int videoType = StringUtils.arrayIndexOf(TYPES, ext);
+		if (videoType == -1)
+			throw new IllegalArgumentException("Invalid extension - " + fName);
+		
+		_type = videoType;
+	}
+}
