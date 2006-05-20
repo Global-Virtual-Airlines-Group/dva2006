3789a8e720f502ab57c8f0bda19f80ee4cda6d8c
Luke Kolin <luke@deltava.org>
2006-05-20 19:44:44 +0000

    Add ACARS statistics
    git-svn-id: file:///home/svn/DVA2006@2798 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/beans/stats/TestFlightStatsEntry.java
+++ src/test/org/deltava/beans/stats/TestFlightStatsEntry.java
@@ -20,18 +20,22 @@ public class TestFlightStatsEntry extends TestCase {
 
 	public void testProperties() {
 		_e = new FlightStatsEntry("CRJ-200", 10, 310, 5010);
+		_e.setACARSLegs(8);
 		assertEquals("CRJ-200", _e.getLabel());
 		assertEquals(10, _e.getLegs());
+		assertEquals(8, _e.getACARSLegs());
 		assertEquals(310, _e.getHours(), 0.001);
 		assertEquals(5010, _e.getMiles());
 		assertEquals(31.0, _e.getAvgHours(), 0.001);
 		assertEquals(501, _e.getAvgMiles(), 0.001);
+		assertEquals(0.8, _e.getACARSPercent(), 0.00);
 	}
 	
 	public void testZeroLegs() {
 		_e = new FlightStatsEntry("CRJ-200", 0, 310, 5010);
 		assertEquals(0.0, _e.getAvgHours(), 0.00);
 		assertEquals(0.0, _e.getAvgMiles(), 0.00);
+		assertEquals(0.0, _e.getACARSPercent(), 0.00);
 	}
 	
 	public void testComparator() {
