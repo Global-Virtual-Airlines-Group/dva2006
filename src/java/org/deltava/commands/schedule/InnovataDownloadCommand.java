34425eedcf22091fd94fb4a80d8729db2a3319db
Luke Kolin <luke@deltava.org>
2006-05-25 21:16:33 +0000

    Allow multiple file downloads
    git-svn-id: file:///home/svn/DVA2006@2823 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/schedule/InnovataDownloadCommand.java
+++ src/java/org/deltava/commands/schedule/InnovataDownloadCommand.java
@@ -11,6 +11,7 @@ import org.deltava.commands.*;
 import org.deltava.dao.DAOException;
 import org.deltava.dao.file.innovata.GetSchedule;
 
+import org.deltava.util.*;
 import org.deltava.util.ftp.*;
 import org.deltava.util.system.SystemData;
 
@@ -29,74 +30,54 @@ public class InnovataDownloadCommand extends ScheduleImportCommand {
 	 * @throws CommandException if an unhandled error occurs
 	 */
 	public void execute(CommandContext ctx) throws CommandException {
-
-		// Determine the date of the locally cached file
-		String fileName = SystemData.get("schedule.innovata.download.file");
-		File cf = new File(SystemData.get("schedule.innovata.cache"), fileName);
-		Date ldt = cf.isFile() ? new Date(cf.lastModified()) : null;
 		
-		// Connect to the FTP server and download the file
-		InputStream is = null;
-		FTPConnection con = new FTPConnection(SystemData.get("schedule.innovata.download.host"));
+		// Get the file name(s) to download and init the cache
+		List<String> fileNames = StringUtils.split(SystemData.get("schedule.innovata.download.file"), ",");
+		FTPCache cache = new FTPCache(SystemData.get("schedule.innovata.cache"));
+		cache.setCredentials(SystemData.get("schedule.innovata.download.user"), SystemData.get("schedule.innovata.download.pwd"));
+		
+		// Connect to the FTP server and download the files as needed
 		try {
-			con.connect(SystemData.get("schedule.innovata.download.user"), SystemData.get("schedule.innovata.download.pwd"));
-			log.info("Connected to " + con.getClient().getRemoteHost());
-			
-			// Check the remote file date
-			Date rdt = con.getTimestamp("", fileName);
-			if (rdt == null) {
-				log.warn("Cannot find " + fileName + "!");
-				throw notFoundException("Cannot find FTP schedule data");
-			}
-				 
-			// Download the file
 			Collection<String> msgs = new ArrayList<String>();
-			if ((ldt == null) || (ldt.before(rdt))) {
-				long now = System.currentTimeMillis();
-				log.info("Downloading " + cf.getName() + ", local=" + ldt + ", remote=" + rdt);
-				msgs.add("Downloading " + cf.getName() + ", local=" + ldt + ", remote=" + rdt + " from " + con.getClient().getRemoteHost());
-				is = con.get(fileName, cf);
-				cf.setLastModified(rdt.getTime());
-				log.info("Download Complete. " + is.available() + " bytes, " + (System.currentTimeMillis() - now) + " ms");
-				msgs.add("Download Complete. " + is.available() + " bytes, " + (System.currentTimeMillis() - now) + " ms");
-			} else {
-				log.info("Using local copy " + cf.getAbsolutePath());
-				msgs.add("Using local copy " + cf.getAbsolutePath());
-				ctx.setAttribute("innovataCache", Boolean.TRUE, REQUEST);
-				is = new FileInputStream(cf);
+			Collection<ScheduleEntry> entries = new ArrayList<ScheduleEntry>();
+			
+			// Download the files
+			InputStream is = null;
+			boolean isCached = false;
+			for (Iterator<String> i = fileNames.iterator(); i.hasNext(); ) {
+				String fileName = i.next();
+				is = cache.getFile(fileName);
+				
+				// Get download infor
+				FTPDownloadData ftpInfo = cache.getDownloadInfo();
+				isCached |= ftpInfo.isCached();
+				if (ftpInfo.isCached()) {
+					msgs.add("Using local copy of " + fileName);
+				} else {
+					msgs.add("Downloaded " + fileName + ", " + ftpInfo.getSize() + " bytes, " + ftpInfo.getSpeed() + " bytes/sec");
+				}
+				
+				// Initialize the DAO
+				GetSchedule dao = new GetSchedule(is);
+				dao.setAirlines(SystemData.getAirlines().values());
+				dao.setAirports(SystemData.getAirports().values());
+				dao.setPartners(_codeShareInfo);
+
+				// Load the schedule data
+				dao.load();
+				entries.addAll(dao.process());
+				msgs.addAll(dao.getErrorMessages());
 			}
 			
-			// If we have no input stream, abort
-			if (is == null)
-				throw notFoundException("No schedule data found");
-			
-			// Initialize the DAO
-			GetSchedule dao = new GetSchedule(is);
-			dao.setAirlines(SystemData.getAirlines().values());
-			dao.setAirports(SystemData.getAirports().values());
-			dao.setPartners(_codeShareInfo);
-			
-			// Load the schedule data
-			dao.load();
-			Collection<ScheduleEntry> entries = dao.process();
-			msgs.addAll(dao.getErrorMessages());
+			// Save the cache status
+			ctx.setAttribute("innovataCache", Boolean.valueOf(isCached), REQUEST);
 
 			// Save the data in the session
 			ctx.setAttribute("entries", entries, SESSION);
 			ctx.setAttribute("schedType", SCHED_TYPES[INNOVATA], SESSION);
 			ctx.setAttribute("errors", msgs, SESSION);
-		} catch (IOException ie) {
-			log.warn(ie.getMessage());
 		} catch (DAOException de) {
 			throw new CommandException(de);
-		} finally {
-			con.close();
-			try {
-				if (is != null)
-					is.close();
-			} catch (IOException ie) {
-				// empty
-			}
 		}
 
 		// Forward to the JSP
---
+++ src/java/org/deltava/util/ftp/FTPCache.java
@@ -0,0 +1,111 @@
+// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.util.ftp;
+
+import java.io.*;
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class FTPCache {
+
+	private static final Logger log = Logger.getLogger(FTPCache.class);
+
+	private String _cachePath;
+	private String _host;
+	private String _user;
+	private String _pwd;
+
+	private FTPDownloadData _fileInfo;
+
+	/**
+	 * Initializes the FTP cache bean.
+	 * @param path the local cache directory
+	 */
+	public FTPCache(String path) {
+		super();
+		_cachePath = path;
+	}
+
+	/**
+	 * Sets the host name of the remote FTP server.
+	 * @param host the FTP server host name
+	 */
+	public void setHost(String host) {
+		_host = host;
+	}
+
+	/**
+	 * Sets the credentials used to connect to the remote FTP server.
+	 * @param user the user ID
+	 * @param pwd the password
+	 */
+	public void setCredentials(String user, String pwd) {
+		_user = user;
+		_pwd = pwd;
+	}
+
+	/**
+	 * Returns data about the last file downloaded.
+	 * @return an FTPDownloadData bean, or null if no file downloaded
+	 */
+	public FTPDownloadData getDownloadInfo() {
+		return _fileInfo;
+	}
+
+	/**
+	 * Checks the cache for a file, and downloads a new copy if not found or the remote copy is newer.
+	 * @param fileName the file name
+	 * @return an InputStream to the file data
+	 * @throws FTPClientException if an error occurs
+	 */
+	public InputStream getFile(String fileName) throws FTPClientException {
+
+		// Get the local file information
+		File cf = new File(_cachePath, fileName);
+		Date ldt = cf.isFile() ? new Date(cf.lastModified()) : null;
+
+		// Init the FTPConnection object
+		FTPConnection con = new FTPConnection(_host);
+		con.connect(_user, _pwd);
+		log.info("Connected to " + _host);
+
+		// Check the remote file date
+		Date rdt = con.getTimestamp("", fileName);
+		if (rdt == null)
+			throw new FTPClientException("Cannot find " + fileName + " on " + _host);
+
+		InputStream is = null;
+		try {
+			if ((ldt == null) || (ldt.before(rdt))) {
+				long now = System.currentTimeMillis();
+				log.info("Downloading " + cf.getName() + ", local=" + ldt + ", remote=" + rdt);
+				is = con.get(fileName, cf);
+				cf.setLastModified(rdt.getTime());
+				long time = (System.currentTimeMillis() - now);
+				log.info("Download Complete. " + is.available() + " bytes, " + time + " ms");
+				_fileInfo = new FTPDownloadData(fileName, is.available(), time);
+			} else {
+				log.info("Using local copy " + cf.getAbsolutePath());
+				is = new FileInputStream(cf);
+				_fileInfo = new FTPDownloadData(cf);
+			}
+		} catch (IOException ie) {
+			log.warn(ie.getMessage());
+		} finally {
+			con.close();
+		}
+
+		// If we have no input stream, abort
+		if (is == null)
+			throw new FTPClientException("Cannot download " + fileName);
+
+		// Return the stream
+		return is;
+	}
+}
