f1b84849668e2373f15169c158abd4f1f4313caa
Luke Kolin <luke@deltava.org>
2006-06-07 05:07:25 +0000

    Initial Route Map
    git-svn-id: file:///home/svn/DVA2006@2906 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/schedule/RouteMapCommand.java
+++ src/java/org/deltava/commands/schedule/RouteMapCommand.java
@@ -17,8 +17,6 @@ import org.deltava.util.system.SystemData;
 
 public class RouteMapCommand extends AbstractCommand {
 	
-	private static final String[] COLORS = {"blue", "black", "green", "red", "purple", "grey", "brown", "orange", "pink", "yellow"};
-
 	/**
 	 * Executes the command.
 	 * @param ctx the Command context
@@ -52,34 +50,13 @@ public class RouteMapCommand extends AbstractCommand {
 			}
 		}
 		
-		// Map the results to icon colors
-		int ofs = 0;
-		Map<String, String> aColors = new HashMap<String, String>();
-		for (Iterator<Airline> i = results.keySet().iterator(); i.hasNext(); ) {
-			Airline a = i.next();
-			aColors.put(a.getCode(), COLORS[ofs]);
-			ofs++;
-			if (ofs == COLORS.length)
-				ofs = 0; 
-		}
-		
-		// Order the airlines, with ours first
-		List<Airline> sortedAirlines = new ArrayList<Airline>(airlines);
-		ofs = sortedAirlines.indexOf(SystemData.getAirline(SystemData.get("airline.code")));
-		if (ofs != -1) {
-			Airline a = sortedAirlines.get(ofs);
-			sortedAirlines.remove(ofs);
-			sortedAirlines.add(0, a);
-		}
-		
 		// Save the map center
 		ctx.setAttribute("mapCenter", new GeoPosition(SystemData.getDouble("airline.location.lat", 40), 
 				SystemData.getDouble("airline.location.lng", -85)), REQUEST);
 		
 		// Save the airport/color maps in the request
-		ctx.setAttribute("airlines", sortedAirlines, REQUEST);
+		ctx.setAttribute("airlines", airlines, REQUEST);
 		ctx.setAttribute("airportMap", results, REQUEST);
-		ctx.setAttribute("airportColors", aColors, REQUEST);
 		
 		// Forward to the JSP
 		CommandResult result = ctx.getResult();
---
+++ src/java/org/deltava/service/ScheduleAirportService.java
@@ -0,0 +1,84 @@
+// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.service;
+
+import java.util.*;
+import java.io.IOException;
+
+import static javax.servlet.http.HttpServletResponse.*;
+
+import org.jdom.*;
+
+import org.deltava.beans.schedule.*;
+import org.deltava.dao.*;
+
+import org.deltava.util.*;
+import org.deltava.util.system.SystemData;
+
+/**
+ * A Web Service to list all airports serviced by a particular Airline.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class ScheduleAirportService extends WebDataService {
+
+	/**
+	 * Executes the Web Service.
+	 * @param ctx the Web Service context
+	 * @return the HTTP status code
+	 * @throws ServiceException if an error occurs
+	 */
+	public int execute(ServiceContext ctx) throws ServiceException {
+		
+		// Get the airline
+		Airline al = SystemData.getAirline(ctx.getParameter("airline"));
+		if (al == null)
+			throw new ServiceException(SC_NOT_FOUND, "Unknown Airline - " + ctx.getParameter("airline"));
+
+		Collection<Airport> airports = null;
+		try {
+			GetSchedule dao = new GetSchedule(_con);
+			airports = dao.getOriginAirports(al);
+		} catch (DAOException de) {
+			throw new ServiceException(SC_INTERNAL_SERVER_ERROR, de.getMessage());
+		}
+		
+		// Generate the XML document
+		Document doc = new Document();
+		Element re = new Element("wsdata");
+		doc.setRootElement(re);
+
+		// Write the entries
+		for (Iterator<Airport> i = airports.iterator(); i.hasNext(); ) {
+			Airport a = i.next();
+			Element e = XMLUtils.createElement("airport", a.getInfoBox(), true);
+			e.setAttribute("icao", a.getICAO());
+			e.setAttribute("iata", a.getIATA());
+			e.setAttribute("lat", StringUtils.format(a.getLatitude(), "##0.00000"));
+			e.setAttribute("lng", StringUtils.format(a.getLongitude(), "##0.00000"));
+			e.setAttribute("color", al.getColor());
+			re.addContent(e);
+		}
+		
+		// Dump the XML to the output stream
+		try {
+			ctx.getResponse().setContentType("text/xml");
+			ctx.println(XMLUtils.format(doc, "ISO-8859-1"));
+			ctx.commit();
+		} catch (IOException ie) {
+			throw new ServiceException(SC_CONFLICT, "I/O Error");
+		}
+
+		// Return success code
+		return SC_OK;
+	}
+
+	/**
+	 * Tells the Web Service Servlet not to log invocations of this service.
+	 * @return FALSE
+	 */
+	public final boolean isLogged() {
+		return false;
+	}
+}
