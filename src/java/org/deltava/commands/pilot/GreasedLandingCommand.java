73f066b6d4a65a891860d5e3846251cae62e2aba
Luke Kolin <luke@deltava.org>
2006-06-08 21:04:38 +0000

    Allow Greased Landing categorization by aircraft
    git-svn-id: file:///home/svn/DVA2006@2914 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/pilot/GreasedLandingCommand.java
+++ src/java/org/deltava/commands/pilot/GreasedLandingCommand.java
@@ -1,15 +1,13 @@
-// Copyright (c) 2005 Luke J. Kolin. All Rights Reserved.
+// Copyright 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.pilot;
 
 import java.util.*;
 import java.sql.Connection;
 
 import org.deltava.commands.*;
+import org.deltava.dao.*;
 
-import org.deltava.dao.GetFlightReportRecognition;
-import org.deltava.dao.DAOException;
-
-import org.deltava.util.ComboUtils;
+import org.deltava.util.*;
 
 /**
  * A Web Site Command to display the smoothest landings.
@@ -22,6 +20,8 @@ public class GreasedLandingCommand extends AbstractViewCommand {
 
    private static final List DATE_FILTER = ComboUtils.fromArray(new String[] { "All Landings", "30 Days", "60 Days",
          "90 Days" }, new String[] { "0", "30", "60", "90" });
+   
+   private static final int MIN_ACARS_CLIENT = 61;
 
    /**
     * Executes the command.
@@ -33,24 +33,29 @@ public class GreasedLandingCommand extends AbstractViewCommand {
       // Load the view context
       ViewContext vc = initView(ctx, 25);
 
-      // Check how many days back to search
-      int daysBack = 30;
-      try {
-         daysBack = Integer.parseInt(ctx.getParameter("days"));
-      } catch (Exception e) {
-         daysBack = 0;
-      }
-
-      // Check staff member filter
-      boolean isStaff = Boolean.valueOf(ctx.getParameter("doStaff")).booleanValue();
+      // Check equipment type and how many days back to search
+      String eqType = ctx.getParameter("eqType");
+      int daysBack = StringUtils.parse(ctx.getParameter("days"), 30);
       try {
          Connection con = ctx.getConnection();
 
          // Get the DAO and the results
          GetFlightReportRecognition dao = new GetFlightReportRecognition(con);
          dao.setQueryMax(vc.getCount());
-         List results = isStaff ? dao.getStaffReports(daysBack, 61) : dao.getGreasedLandings(daysBack, 61);
-         ctx.setAttribute("pireps", results, REQUEST);
+         if (StringUtils.isEmpty(eqType)) {
+        	 vc.setResults(dao.getGreasedLandings(daysBack, MIN_ACARS_CLIENT ));
+         } else if ("staff".equals(eqType)) {
+        	 vc.setResults(dao.getStaffReports(daysBack, MIN_ACARS_CLIENT));
+         } else {
+        	 vc.setResults(dao.getGreasedLandings(eqType, daysBack, MIN_ACARS_CLIENT));
+         }
+         
+         // Save equipment choices
+         List<Object> eqTypes = new ArrayList<Object>();
+         eqTypes.add(ComboUtils.fromString("All Aircraft", ""));
+         eqTypes.add(ComboUtils.fromString("Staff Members", "staff"));
+         eqTypes.addAll(dao.getACARSEquipmentTypes(30, MIN_ACARS_CLIENT));
+         ctx.setAttribute("eqTypes", eqTypes, REQUEST);
       } catch (DAOException de) {
          throw new CommandException(de);
       } finally {
@@ -58,7 +63,6 @@ public class GreasedLandingCommand extends AbstractViewCommand {
       }
 
       // Save combobox choices
-      ctx.setAttribute("isStaff", Boolean.valueOf(isStaff), REQUEST);
       ctx.setAttribute("dateFilter", DATE_FILTER, REQUEST);
 
       // Forward to the JSP
