220798d439b876e401cf8fb5bbb3053417b47f4d
Luke Kolin <luke@deltava.org>
2006-06-15 14:56:45 +0000

    Fix JavaDoc
    git-svn-id: file:///home/svn/DVA2006@2945 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/functions/OnlineEventFunctions.java
+++ src/java/org/deltava/taglib/functions/OnlineEventFunctions.java
@@ -1,6 +1,9 @@
-// Copyright (c) 2005 Luke J. Kolin. All Rights Reserved.
+// Copyright 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.functions;
 
+import java.util.Iterator;
+
+import org.deltava.beans.assign.AssignmentInfo;
 import org.deltava.beans.event.Event;
 
 /**
@@ -9,18 +12,44 @@ import org.deltava.beans.event.Event;
  * @version 1.0
  * @since 1.0
  */
- 
+
 public class OnlineEventFunctions {
 
+	/**
+	 * Returns wether an Online Event is in progress.
+	 * @param e the Event bean
+	 * @return TRUE if the Event is active, otherwise FALSE
+	 */
 	public static boolean isOpen(Event e) {
 		return (e.getStatus() == Event.ACTIVE);
 	}
-	
+
+	/**
+	 * Returns wether a Pilot is signed up for an Online Event. <i>The Signups for the Online Event must be
+	 * populated prior to this being called</i>.
+	 * @param e the Event bean
+	 * @param pilotID the Pilot's database ID
+	 * @return TRUE if the Pilot is signed up, otherwise FALSE
+	 * @see Event#isSignedUp(int)
+	 */
 	public static boolean isSignedUp(Event e, int pilotID) {
 		return e.isSignedUp(pilotID);
 	}
-	
+
+	/**
+	 * Returns wether a Pilot has a Flight Assignment for an Online Event. <i>The Flight Assignments for the Online
+	 * Event must be populated prior to this being called</i>.
+	 * @param e the Event bean
+	 * @param pilotID the Pilot's database ID
+	 * @return TRUE if the Pilot has a Flight Assignment, otherwise FALSE
+	 */
 	public static boolean isAssigned(Event e, int pilotID) {
-		return e.isAssigned(pilotID);
+		for (Iterator<AssignmentInfo> i = e.getAssignments().iterator(); i.hasNext();) {
+			AssignmentInfo ai = i.next();
+			if (ai.getPilotID() == pilotID)
+				return true;
+		}
+
+		return false;
 	}
 }
