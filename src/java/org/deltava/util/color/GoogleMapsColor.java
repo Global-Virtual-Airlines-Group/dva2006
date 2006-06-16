e4fc8037a182cb92ef6b3061cc67a45cc10ed3d2
Luke Kolin <luke@deltava.org>
2006-06-16 18:03:51 +0000

    Fix JavaDoc
    git-svn-id: file:///home/svn/DVA2006@2954 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/util/color/GoogleMapsColor.java
+++ src/java/org/deltava/util/color/GoogleMapsColor.java
@@ -12,10 +12,22 @@ import java.util.StringTokenizer;
 
 public class GoogleMapsColor extends GoogleColor {
 
+	/**
+	 * Creates a Google Maps color with default opacity (255).
+	 * @param red the RGB red value
+	 * @param green the RGB green value
+	 * @param blue the RGB blue value
+	 */
 	public GoogleMapsColor(int red, int green, int blue) {
 		super(red, green, blue, 255);
 	}
 
+	/**
+	 * Creates a Google Maps color from an RGB string.
+	 * @param colors a dash-delimited string, in format R-G-B
+	 * @throws NumberFormatException if the string is invalid
+	 * @see org.deltava.beans.MapEntry#LINECOLORS
+	 */
 	public GoogleMapsColor(String colors) {
 		super(0, 0, 0, 255);
 		StringTokenizer tkns = new StringTokenizer(colors, "-");
@@ -24,6 +36,10 @@ public class GoogleMapsColor extends GoogleColor {
 		_blue = Integer.parseInt(tkns.nextToken());
 	}
 	
+	/**
+	 * Renders the color in a format to be passed to the Goolge Maps JavaScript API.
+	 * @return the rendered color
+	 */
 	public String toString() {
 		StringBuilder buf = new StringBuilder('#');
 		buf.append(formatHex(_red));
