c53b2b4e2643711302da0bc0c3b8c8221ac426fa
Luke Kolin <luke@deltava.org>
2006-06-16 18:03:51 +0000

    Fix JavaDoc
    git-svn-id: file:///home/svn/DVA2006@2954 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/util/color/GoogleColor.java
+++ src/java/org/deltava/util/color/GoogleColor.java
@@ -12,11 +12,33 @@ import org.deltava.util.StringUtils;
 
 public abstract class GoogleColor {
 
+	/**
+	 * The RGB blue value.
+	 */
 	protected int _blue;
+	
+	/**
+	 * The RGB green value.
+	 */
 	protected int _green;
+	
+	/**
+	 * The RGB red value.
+	 */
 	protected int _red;
+	
+	/**
+	 * The transparency value.
+	 */
 	protected int _alpha;
 	
+	/**
+	 * Initializes the color bean.
+	 * @param red the red intensity, from 0 to 255
+	 * @param green the geen intensity, from 0 to 255
+	 * @param blue the blue intensity, from 0 to 255
+	 * @param alpha the transparency, from 0 to 255
+	 */
 	protected GoogleColor(int red, int green, int blue, int alpha) {
 		super();
 		_red = (red > 255) ? 255 : red;
@@ -25,22 +47,45 @@ public abstract class GoogleColor {
 		_alpha = (alpha > 255) ? 255 : alpha;
 	}
 	
+	/**
+	 * Initializes the color bean
+	 * @param red the red intensity, from 0 to 255
+	 * @param green the geen intensity, from 0 to 255
+	 * @param blue the blue intensity, from 0 to 255
+	 * @param alpha the transparancy, as a fraction between 0 and 1
+	 */
 	protected GoogleColor(int red, int green, int blue, float alpha) {
 		this(red, green, blue, Math.round(alpha * 255));
 	}
 	
+	/**
+	 * Returns the red intensity.
+	 * @return the red intensity value, from 0 to 255
+	 */
 	public int getRed() {
 		return _red;
 	}
 	
+	/**
+	 * Returns the green intensity.
+	 * @return the green intensity value, from 0 to 255
+	 */
 	public int getGreen() {
 		return _green;
 	}
 	
+	/**
+	 * Returns the blue intensity.
+	 * @return the blue intensity value, from 0 to 255
+	 */
 	public int getBlue() {
 		return _blue;
 	}
 	
+	/**
+	 * Returns the transparency.
+	 * @return the transparency value, from 0 to 255
+	 */
 	public int getAlpha() {
 		return _alpha;
 	}
--- src/java/org/deltava/util/color/GoogleEarthColor.java
+++ src/java/org/deltava/util/color/GoogleEarthColor.java
@@ -10,19 +10,41 @@ package org.deltava.util.color;
 
 public class GoogleEarthColor extends GoogleColor {
 	
+	/**
+	 * Creates a Google Earth color.
+	 * @param red the RGB red value
+	 * @param green the RGB green value
+	 * @param blue the RGB blue value
+	 * @param alpha the transparency value
+	 */
 	public GoogleEarthColor(int red, int green, int blue, int alpha) {
 		super(red, green, blue, alpha);
 	}
 	
+	/**
+	 * Creates a Google Earth color with default semi-transparency (128).
+	 * @param red the RGB red value
+	 * @param green the RGB green value
+	 * @param blue the RGB blue value
+	 */
 	public GoogleEarthColor(int red, int green, int blue) {
 		super(red, green, blue, 128);
 	}
 
+	/**
+	 * Dims the color.
+	 * @param factor the factor to divide all intensities by
+	 * @return a dimmed clone of the color
+	 */
 	public GoogleEarthColor dim(float factor) {
 		return new GoogleEarthColor(Math.round(_red / factor), Math.round(_green / factor),
 				Math.round(_blue / factor), Math.round(_alpha / factor));
 	}
 
+	/**
+	 * Renders the color to a string, suitable for insertion in KML documents.
+	 * @return the KML-formatted color value
+	 */
 	public String toString() {
 		StringBuilder buf = new StringBuilder(formatHex(_alpha));
 		buf.append(formatHex(_blue));
