725f0753ae393c34e2a6d685caa5a4c569980d27
Luke Kolin <luke@deltava.org>
2006-06-22 14:11:58 +0000

    Allow any departure/arrival time and legs to 8
    git-svn-id: file:///home/svn/DVA2006@2991 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/Flight.java
+++ src/java/org/deltava/beans/Flight.java
@@ -150,12 +150,12 @@ public abstract class Flight extends DatabaseBean implements Comparable {
     /**
      * Sets the Flight leg for this flight
      * @param leg the Flight leg
-     * @throws IllegalArgumentException if the flight leg is negative or > 6
+     * @throws IllegalArgumentException if the flight leg is negative or > 8
      * @see Flight#getLeg()
      */
     public void setLeg(int leg) {
-        if ((leg < 0) || (leg > 6))
-            throw new IllegalArgumentException("Flight Leg cannot be < 0 || > 6");
+        if ((leg < 0) || (leg > 8))
+            throw new IllegalArgumentException("Flight Leg cannot be < 0 || > 8");
 
         _leg = leg;
     }
