fb99832512f260c5c1e70666aa10200b31b5203e
Luke Kolin <luke@deltava.org>
2006-06-26 04:35:49 +0000

    Add membership statistics
    git-svn-id: file:///home/svn/DVA2006@3007 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/stats/MembershipTotals.java
@@ -0,0 +1,41 @@
+// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.stats;
+
+import java.util.Date;
+
+import org.deltava.beans.DatabaseBean;
+
+/**
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class MembershipTotals extends DatabaseBean implements Comparable {
+
+	private Date _dt;
+	private int _count;
+	
+	/**
+	 * 
+	 */
+	public MembershipTotals(Date dt) {
+		super();
+		_dt = dt;
+	}
+	
+	public Date getDate() {
+		return _dt;
+	}
+	
+	public int getCount() {
+		return _count;
+	}
+	
+	public void setCount(int count) {
+		if (count < 0)
+			throw new IllegalArgumentException("Invalid count - " + count);
+		
+		_count = count;
+	}
+}
