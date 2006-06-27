72da110f3d03478bc1ff8b2a2a9775a00910d5d3
Luke Kolin <luke@deltava.org>
2006-06-27 21:14:03 +0000

    Fix cross-airline channel access
    git-svn-id: file:///home/svn/DVA2006@3015 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/security/command/AccessControlUser.java
+++ src/test/org/deltava/security/command/AccessControlUser.java
@@ -1,36 +1,24 @@
-// Copyright 2005 Luke J. Kolin. All Rights Reserved.
+// Copyright 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.security.command;
 
 import java.util.*;
 
-import org.deltava.beans.Person;
+import org.deltava.beans.Pilot;
 
-public class AccessControlUser extends Person {
-   
-   private List<String> _roles = new ArrayList<String>();
-   
-   public AccessControlUser(String fName, String lName) {
-      super(fName, lName);
-      _roles.add("User");
-   }
-   
-   public String getRowClassName() {
-      return null;
-   }
-   
-   public boolean isInRole(String roleName) {
-      return (_roles.contains("Admin") || "*".equals(roleName) || _roles.contains(roleName));
-   }
-   
-   public void addRole(String roleName) {
-      _roles.add(roleName);
-   }
-   
-   public void removeRole(String roleName) {
-      _roles.remove(roleName);
-   }
-   
-   public Collection<String> getRoles() {
-      return _roles;
-   }
+public class AccessControlUser extends Pilot {
+
+	public AccessControlUser(String fName, String lName) {
+		super(fName, lName);
+		addRole("User");
+	}
+
+	public String getRowClassName() {
+		return null;
+	}
+
+	public void removeRole(String roleName) {
+		Collection<String> roles = new HashSet<String>();
+		roles.add(roleName);
+		removeRoles(roles);
+	}
 }
--- src/test/org/deltava/security/command/TestCoolerChannelAccessControl.java
+++ src/test/org/deltava/security/command/TestCoolerChannelAccessControl.java
@@ -5,110 +5,135 @@ import org.hansel.CoverageDecorator;
 
 import org.deltava.beans.cooler.Channel;
 
+import org.deltava.util.system.*;
+
 public class TestCoolerChannelAccessControl extends AccessControlTestCase {
 
-   private CoolerChannelAccessControl _ac;
-   private Channel _c;
-   
-   public static Test suite() {
-      return new CoverageDecorator(TestCoolerChannelAccessControl.class, 
-            new Class[] { AccessControl.class, CoolerChannelAccessControl.class } );
-  }
-   
-   protected void setUp() throws Exception {
-      super.setUp();
-      _c = new Channel("Name");
-      _ac = new CoolerChannelAccessControl(_ctxt, _c);
-   }
+	private CoolerChannelAccessControl _ac;
+	private Channel _c;
 
-   protected void tearDown() throws Exception {
-      _c = null;
-      _ac = null;
-      super.tearDown();
-   }
+	public static Test suite() {
+		return new CoverageDecorator(TestCoolerChannelAccessControl.class, new Class[] { AccessControl.class,
+				CoolerChannelAccessControl.class });
+	}
 
-   public void testAccess() throws Exception {
-      _c.addRole("Pilot");
-      _ac.validate();
-      
-      assertFalse(_ac.getCanAccess());
-      assertFalse(_ac.getCanPost());
-      assertFalse(_ac.getCanRead());
-      assertFalse(_ac.getCanEdit());
+	protected void setUp() throws Exception {
+		super.setUp();
+		SystemData.init(MockSystemDataLoader.class.getName());
+		_c = new Channel("Name");
+		_c.addAirline("DVA");
+		_ac = new CoolerChannelAccessControl(_ctxt, _c);
+	}
 
-      _user.addRole("Pilot");
-      _ac.validate();
-      
-      assertTrue(_ac.getCanAccess());
-      assertTrue(_ac.getCanPost());
-      assertFalse(_ac.getCanRead());
-      assertFalse(_ac.getCanEdit());
-   }
-   
-   public void testWildcardAccess() throws Exception {
-      _c.addRole("*");
-      _ac.validate();
-      
-      assertTrue(_ac.getCanAccess());
-      assertTrue(_ac.getCanPost());
-      assertFalse(_ac.getCanRead());
-      assertFalse(_ac.getCanEdit());
-   }
-   
-   public void testAdminAccess() throws Exception {
-      _user.addRole("Admin");
-      _ac.validate();
-      
-      assertTrue(_ac.getCanAccess());
-      assertTrue(_ac.getCanPost());
-      assertTrue(_ac.getCanRead());
-      assertTrue(_ac.getCanEdit());
-   }
-   
-   public void testAnonymousAccess() throws Exception {
-      _ctxt.logoff();
-      _ac.validate();
-      
-      assertFalse(_ac.getCanAccess());
-      assertFalse(_ac.getCanPost());
-      assertFalse(_ac.getCanRead());
-      assertFalse(_ac.getCanEdit());
-      
-      _c.addRole("Anonymous");
-      _ac.validate();
+	protected void tearDown() throws Exception {
+		_c = null;
+		_ac = null;
+		super.tearDown();
+	}
 
-      assertTrue(_ac.getCanAccess());
-      assertFalse(_ac.getCanPost());
-      assertFalse(_ac.getCanRead());
-      assertFalse(_ac.getCanEdit());
-   }
-   
-   public void testNoChannelAccess() throws Exception {
-      _ac = new CoolerChannelAccessControl(_ctxt, null);
-      _ac.validate();
-      
-      assertTrue(_ac.getCanAccess());
-      assertTrue(_ac.getCanPost());
-      assertFalse(_ac.getCanRead());
-      assertFalse(_ac.getCanEdit());
-      
-      _user.addRole("Admin");
-      _ac.validate();
-      assertTrue(_ac.getCanAccess());
-      assertTrue(_ac.getCanPost());
-      assertFalse(_ac.getCanRead());
-      assertFalse(_ac.getCanEdit());
-      
-      _ctxt.logoff();
-      _ac.validate();
-      
-      assertTrue(_ac.getCanAccess());
-      assertFalse(_ac.getCanPost());
-      assertFalse(_ac.getCanRead());
-      assertFalse(_ac.getCanEdit());
-   }
-   
-   public void testContextValidation() {
-      doContextValidation(new CoolerChannelAccessControl(null, _c));
-   }
+	public void testAccess() throws Exception {
+		_c.addRole("X");
+		assertFalse(_user.isInRole("X"));
+		_ac.validate();
+
+		assertFalse(_ac.getCanAccess());
+		assertFalse(_ac.getCanPost());
+		assertFalse(_ac.getCanRead());
+		assertFalse(_ac.getCanEdit());
+
+		_user.addRole("X");
+		_ac.validate();
+
+		assertTrue(_ac.getCanAccess());
+		assertTrue(_ac.getCanPost());
+		assertFalse(_ac.getCanRead());
+		assertFalse(_ac.getCanEdit());
+	}
+
+	public void testWildcardAccess() throws Exception {
+		_c.addRole("*");
+		_ac.validate();
+
+		assertTrue(_ac.getCanAccess());
+		assertTrue(_ac.getCanPost());
+		assertFalse(_ac.getCanRead());
+		assertFalse(_ac.getCanEdit());
+	}
+
+	public void testAdminAccess() throws Exception {
+		_user.addRole("Admin");
+		_ac.validate();
+
+		assertTrue(_ac.getCanAccess());
+		assertTrue(_ac.getCanPost());
+		assertTrue(_ac.getCanRead());
+		assertTrue(_ac.getCanEdit());
+	}
+
+	public void testAnonymousAccess() throws Exception {
+		_ctxt.logoff();
+		_ac.validate();
+
+		assertFalse(_ac.getCanAccess());
+		assertFalse(_ac.getCanPost());
+		assertFalse(_ac.getCanRead());
+		assertFalse(_ac.getCanEdit());
+
+		_c.addRole("Anonymous");
+		_ac.validate();
+
+		assertTrue(_ac.getCanAccess());
+		assertFalse(_ac.getCanPost());
+		assertFalse(_ac.getCanRead());
+		assertFalse(_ac.getCanEdit());
+	}
+
+	public void testNoChannelAccess() throws Exception {
+		_ac = new CoolerChannelAccessControl(_ctxt, null);
+		_ac.validate();
+
+		assertTrue(_ac.getCanAccess());
+		assertTrue(_ac.getCanPost());
+		assertFalse(_ac.getCanRead());
+		assertFalse(_ac.getCanEdit());
+
+		_user.addRole("Admin");
+		_ac.validate();
+		assertTrue(_ac.getCanAccess());
+		assertTrue(_ac.getCanPost());
+		assertFalse(_ac.getCanRead());
+		assertTrue(_ac.getCanEdit());
+
+		_ctxt.logoff();
+		_ac.validate();
+
+		assertTrue(_ac.getCanAccess());
+		assertFalse(_ac.getCanPost());
+		assertFalse(_ac.getCanRead());
+		assertFalse(_ac.getCanEdit());
+	}
+
+	public void testOtherAirlineAccess() throws Exception {
+		_c.addRole("Pilot");
+		_user.setPilotCode("AFV123");
+		assertFalse(_c.getAirlines().contains("AFV"));
+		_ac.validate();
+
+		assertFalse(_ac.getCanAccess());
+		assertFalse(_ac.getCanPost());
+		assertFalse(_ac.getCanRead());
+		assertFalse(_ac.getCanEdit());
+
+		_c.addAirline("AFV");
+		_ac.validate();
+		
+		assertTrue(_ac.getCanAccess());
+		assertTrue(_ac.getCanPost());
+		assertFalse(_ac.getCanRead());
+		assertFalse(_ac.getCanEdit());
+	}
+
+	public void testContextValidation() {
+		doContextValidation(new CoolerChannelAccessControl(null, _c));
+	}
 }
