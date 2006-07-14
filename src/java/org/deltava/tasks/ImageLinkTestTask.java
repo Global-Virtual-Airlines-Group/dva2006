ed46d0006d16ae463f8646f20fcd5ac34ce8855d
Luke Kolin <luke@deltava.org>
2006-07-14 20:11:02 +0000

    Add Image URL validation task
    git-svn-id: file:///home/svn/DVA2006@3050 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/tasks/ImageLinkTestTask.java
@@ -0,0 +1,81 @@
+// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.tasks;
+
+import java.net.*;
+import java.util.*;
+import java.io.IOException;
+
+import org.deltava.dao.*;
+
+import org.deltava.taskman.DatabaseTask;
+import org.deltava.util.http.HttpTimeoutHandler;
+
+/**
+ * A Scheduled Task to validate the integrity of Water Cooler Image URLs.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class ImageLinkTestTask extends DatabaseTask {
+
+	/**
+	 * Initializes the task.
+	 */
+	public ImageLinkTestTask() {
+		super("Image URL Test", ImageLinkTestTask.class);
+	}
+
+	/**
+	 * Executes the Task.
+	 * @see org.deltava.taskman.Task#execute()
+	 */
+	protected void execute() {
+		try {
+			GetCoolerLinks dao = new GetCoolerLinks(_con);
+			Collection<Integer> ids = dao.getThreads();
+			
+			// Loop through the threads
+			SetCoolerLinks wdao = new SetCoolerLinks(_con);
+			for (Iterator<Integer> i = ids.iterator(); i.hasNext(); ) {
+				Integer id = i.next();
+				
+				// Get the images
+				Collection<String> urls = dao.getURLs(id.intValue());
+				for (Iterator<String> ui = urls.iterator(); ui.hasNext(); ) {
+					String imgURL = ui.next();
+					boolean isOK = true;
+					
+					try {
+						URL url = new URL(null, imgURL, new HttpTimeoutHandler(1950));
+						HttpURLConnection urlcon = (HttpURLConnection) url.openConnection();
+						urlcon.setRequestMethod("HEAD");
+						urlcon.connect();
+						
+						// Validate the result code
+						int resultCode = urlcon.getResponseCode();
+						if (resultCode != HttpURLConnection.HTTP_OK) {
+							log.warn("Invalid Image HTTP result code - " + resultCode);
+							isOK = false;
+						}
+					} catch (MalformedURLException mue) {
+						isOK = false;
+						log.warn("Invalid URL - " + imgURL);
+					} catch (IOException ie) {
+						isOK = false;
+						log.warn("Error validating " + imgURL + " - " + ie.getMessage());
+					}
+					
+					// If it's invalid, nuke it
+					if (!isOK)
+						wdao.delete(id.intValue(), imgURL);
+				}
+			}
+		} catch (DAOException de) {
+			log.error("Error validating images - " + de.getMessage(), de);
+		}
+
+		// Log completion
+		log.info("Processing Complete");
+	}
+}
