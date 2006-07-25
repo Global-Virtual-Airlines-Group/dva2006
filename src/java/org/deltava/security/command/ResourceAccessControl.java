5a3af3598ca7fc4a8e576399e123b8be784753bd
Luke Kolin <luke@deltava.org>
2006-07-25 19:19:06 +0000

    Misc resource updates
    git-svn-id: file:///home/svn/DVA2006@3093 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/security/command/ResourceAccessControl.java
@@ -0,0 +1,74 @@
+// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.security.command;
+
+import org.deltava.beans.fleet.Resource;
+
+import org.deltava.security.SecurityContext;
+
+/**
+ * An Access Controller for Web Resources.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class ResourceAccessControl extends AccessControl {
+
+	private Resource _r;
+	private boolean _canCreate;
+	private boolean _canEdit;
+	private boolean _canDelete;
+
+	/**
+	 * Initializes the Access Controller.
+	 * @param ctx the Security context
+	 * @param r the Web Resource
+	 */
+	public ResourceAccessControl(SecurityContext ctx, Resource r) {
+		super(ctx);
+		_r = r;
+	}
+
+	/**
+	 * Calculates Access roles.
+	 */
+	public void validate() {
+		validateContext();
+
+		// Check create access
+		_canCreate = _ctx.isUserInRole("Pilot");
+		if (_r == null) {
+			_canEdit = _canCreate;
+			return;
+		}
+
+		// Validate edit/delete rights
+		boolean isMine = (_r.getAuthorID() == _ctx.getUser().getID());
+		_canEdit = _ctx.isUserInRole("HR") || _ctx.isUserInRole("Fleet") || (isMine && !_r.getPublic());
+		_canDelete = _canEdit;
+	}
+
+	/**
+	 * Returns wether a new Web Resource can be created.
+	 * @return TRUE if a Resource can be created, otherwise FALSE
+	 */
+	public boolean getCanCreate() {
+		return _canCreate;
+	}
+
+	/**
+	 * Returns wether the Web Resource can be edited.
+	 * @return TRUE if the Resource can be edited, otherwise FALSE
+	 */
+	public boolean getCanEdit() {
+		return _canEdit;
+	}
+
+	/**
+	 * Returns wether the Web Resource can be deleted.
+	 * @return TRUE if the Resource can be deleted, otherwise FALSE
+	 */
+	public boolean getCanDelete() {
+		return _canDelete;
+	}
+}
