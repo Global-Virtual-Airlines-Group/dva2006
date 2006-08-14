a03b7e3d17899e084fb16940c9437c3a90d36812
Luke Kolin <luke@deltava.org>
2006-08-14 19:23:34 +0000

    Update JavaMail to 1.4
    git-svn-id: file:///home/svn/DVA2006@3174 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/util/TestBase64.java
+++ src/test/org/deltava/util/TestBase64.java
@@ -1,15 +1,29 @@
 package org.deltava.util;
 
+import java.util.Random;
+import java.lang.reflect.Method;
+
 import junit.framework.TestCase;
 
 public class TestBase64 extends TestCase {
     
 	private static final byte[] enc2 = {1, 2, 4, 5, 6, 7, 8, 11, 127, 126};
-    private static final String enc = "The Quick Brown Fox jumped over the 'Lazy Dog'"; 
-
-    public TestBase64() {
-        super();
+    private static final String enc = "The Quick Brown Fox jumped over the 'Lazy Dog'";
+    
+    private static final String LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private byte[] data;
+    
+    protected void tearDown() throws Exception {
+    	data = null;
+    	super.tearDown();
     }
+
+	private void initData() {
+		Random rnd = new Random();
+		data = new byte[13107200];
+    	for (int x = 0; x < data.length; x++)
+    		data[x] = (byte) LETTERS.charAt(rnd.nextInt(LETTERS.length()));
+	}
 
     public void testString() {
         String tmp = Base64.encode(enc);
@@ -22,5 +36,36 @@ public class TestBase64 extends TestCase {
     	assertEquals(enc2.length, enc3.length);
     	for (int x = 0; x < enc2.length; x++)
     		assertEquals(enc2[x], enc3[x]);
+    }
+    
+    public void testPerformance() {
+    	initData();
+    	
+    	// Encode using standard Base64
+    	long now = System.currentTimeMillis();
+    	String enc_1 = Base64.encode(data);
+    	assertNotNull(enc_1);
+    	enc_1 = null;
+    	long time1 = System.currentTimeMillis() - now;
+    	System.out.println("Base64 took " + time1 + " ms");
+    }
+    
+    public void testJavaMailPerformance() throws Exception {
+    	initData();
+    	
+    	try {
+    		Class c = Class.forName("com.sun.mail.util.BASE64EncoderStream");
+    		Method m = c.getDeclaredMethod("encode", byte[].class);
+    		
+    		long now2 = System.currentTimeMillis();
+    		String enc_2 = new String((byte[]) m.invoke(null, data));
+    		assertNotNull(enc_2);
+    		long time2 = System.currentTimeMillis() - now2;
+    		System.out.println("JavaMail took " + time2 + " ms");
+    	} catch (NoSuchMethodException nsme) {
+    		fail("JavaMail not installed");
+    	} catch (ClassNotFoundException cnfe) {
+    		fail("JavaMail not installed");
+    	}
     }
 }
