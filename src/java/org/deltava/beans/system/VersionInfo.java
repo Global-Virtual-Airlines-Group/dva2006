86ed7db290fab13c5029503004b8d7dc96bbc8fa
Luke Kolin <luke@deltava.org>
2006-09-18 03:13:05 +0000

    Build 109
    git-svn-id: file:///home/svn/DVA2006@3308 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -13,11 +13,11 @@ public interface VersionInfo {
     public static final int MAJOR = 1;
     public static final int MINOR = 0;
     
-    public static final int BUILD = 108;
-    public static final int RELEASE_CANDIDATE = 46;
+    public static final int BUILD = 109;
+    public static final int RELEASE_CANDIDATE = 47;
     public static final boolean FINAL = false;
     
-    public static final String TXT_COPYRIGHT = "Copyright (c) 2004, 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.";
+    public static final String TXT_COPYRIGHT = "Copyright 2004, 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.";
     public static final String HTML_COPYRIGHT = "Copyright &copy; 2004, 2005, 2006 <a rel=\"external\" class=\"small\" href=\"http://www.gvagroup.org/\">Global Virtual Airlines Group</a>. All Rights Reserved.";
     
     public static final String APPNAME = "(Golgotha v" + String.valueOf(MAJOR) + "." + String.valueOf(MINOR) + ")";
