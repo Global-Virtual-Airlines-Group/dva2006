0a5ed22f4e04aabfe76b2af30e54cd9e25261882
Luke Kolin <luke@deltava.org>
2006-10-25 02:00:00 +0000

    Build 113
    git-svn-id: file:///home/svn/DVA2006@3436 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -13,7 +13,7 @@ public interface VersionInfo {
     public static final int MAJOR = 1;
     public static final int MINOR = 0;
     
-    public static final int BUILD = 112;
+    public static final int BUILD = 113;
     // public static final int RELEASE_CANDIDATE = 49;
     public static final boolean FINAL = false;
     
