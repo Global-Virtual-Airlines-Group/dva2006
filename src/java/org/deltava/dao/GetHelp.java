c76be85bb324277575ca89ba81f762693dd69fc2
Luke Kolin <luke@deltava.org>
2006-11-01 17:54:52 +0000

    Don't load comments in FAQ if empty
    git-svn-id: file:///home/svn/DVA2006@3462 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetHelp.java
+++ src/java/org/deltava/dao/GetHelp.java
@@ -58,7 +58,7 @@ public class GetHelp extends DAO {
 			throw new DAOException(se);
 		}
 	}
-	
+
 	/**
 	 * Returns a particular Help Desk Issue. <i>This loads the Issue Comments</i>.
 	 * @param id the database ID
@@ -70,15 +70,15 @@ public class GetHelp extends DAO {
 			setQueryMax(1);
 			prepareStatement("SELECT * FROM HELPDESK WHERE (ID=?)");
 			_ps.setInt(1, id);
-			
+
 			// Do the query and return the first result
 			List<Issue> results = executeIssue();
 			if (results.isEmpty())
 				return null;
-			
+
 			// Get the issue
 			Issue i = results.get(0);
-			
+
 			// Load the comments
 			prepareStatementWithoutLimits("SELECT * FROM HELPDESK_COMMENTS WHERE (ID=?) ORDER BY CREATED_ON");
 			_ps.setInt(1, id);
@@ -90,7 +90,7 @@ public class GetHelp extends DAO {
 				ic.setBody(rs.getString(5));
 				i.addComment(ic);
 			}
-			
+
 			// Clean up and return
 			rs.close();
 			_ps.close();
@@ -99,7 +99,7 @@ public class GetHelp extends DAO {
 			throw new DAOException(se);
 		}
 	}
-	
+
 	/**
 	 * Returns all Help Desk Issues.
 	 * @return a Collection of Issue beans
@@ -115,7 +115,7 @@ public class GetHelp extends DAO {
 			throw new DAOException(se);
 		}
 	}
-	
+
 	/**
 	 * Returns all Help Desk Issues for a Pilot, or Public Issues.
 	 * @param id the Pilot's database ID
@@ -123,7 +123,7 @@ public class GetHelp extends DAO {
 	 * @throws DAOException if a JDBC error occurs
 	 */
 	public Collection<Issue> getByPilot(int id, boolean showPublic) throws DAOException {
-		
+
 		// Build the SQL statement
 		StringBuilder sqlBuf = new StringBuilder("SELECT I.*, COUNT(IC.ID), MAX(IC.CREATED_ON), (SELECT AUTHOR "
 				+ "FROM HELPDESK_COMMENTS IC WHERE (I.ID=IC.ID) ORDER BY IC.CREATED_ON DESC LIMIT 1) AS LC "
@@ -131,9 +131,9 @@ public class GetHelp extends DAO {
 				+ "(I.ASSIGNEDTO=?) ");
 		if (showPublic)
 			sqlBuf.append("OR (I.ISPUBLIC=?) ");
-		
+
 		sqlBuf.append("GROUP BY I.ID ORDER BY I.STATUS, I.CREATED_ON");
-		
+
 		try {
 			prepareStatement(sqlBuf.toString());
 			_ps.setInt(1, id);
@@ -146,7 +146,7 @@ public class GetHelp extends DAO {
 			throw new DAOException(se);
 		}
 	}
-	
+
 	/**
 	 * Returns all active Help Desk Issues.
 	 * @return a Collection of Issue beans
@@ -164,7 +164,7 @@ public class GetHelp extends DAO {
 			throw new DAOException(se);
 		}
 	}
-	
+
 	/**
 	 * Returns the Help Desk FAQ Issues.
 	 * @return a Collection of Issue beans
@@ -175,36 +175,39 @@ public class GetHelp extends DAO {
 			prepareStatement("SELECT * FROM HELPDESK WHERE (ISFAQ=?)");
 			_ps.setBoolean(1, true);
 			Map<Integer, Issue> results = CollectionUtils.createMap(executeIssue(), "ID");
-			
+
 			// Build the FAQ answer comments query
-			StringBuilder sqlBuf = new StringBuilder("SELECT * FROM HELPDESK_COMMENTS WHERE ID IN (");
-			for (Iterator<Integer> i = results.keySet().iterator(); i.hasNext(); ) {
-				Integer id = i.next();
-				sqlBuf.append(id.toString());
-				if (i.hasNext())
-					sqlBuf.append(',');
+			if (!results.isEmpty()) {
+				StringBuilder sqlBuf = new StringBuilder("SELECT * FROM HELPDESK_COMMENTS WHERE ID IN (");
+				for (Iterator<Integer> i = results.keySet().iterator(); i.hasNext();) {
+					Integer id = i.next();
+					sqlBuf.append(id.toString());
+					if (i.hasNext())
+						sqlBuf.append(',');
+				}
+
+				// Execute the Query
+				sqlBuf.append(")");
+				prepareStatementWithoutLimits(sqlBuf.toString());
+				ResultSet rs = _ps.executeQuery();
+				while (rs.next()) {
+					IssueComment ic = new IssueComment(rs.getInt(2));
+					ic.setID(rs.getInt(1));
+					ic.setCreatedOn(rs.getTimestamp(3));
+					ic.setFAQ(rs.getBoolean(4));
+					ic.setBody(rs.getString(5));
+
+					// Stuff into issue
+					Issue i = results.get(new Integer(ic.getID()));
+					if (i != null)
+						i.addComment(ic);
+				}
+
+				// Clean up and return
+				rs.close();
+				_ps.close();
 			}
 			
-			// Execute the Query
-			sqlBuf.append(")");
-			prepareStatementWithoutLimits(sqlBuf.toString());
-			ResultSet rs = _ps.executeQuery();
-			while (rs.next()) {
-				IssueComment ic = new IssueComment(rs.getInt(2));
-				ic.setID(rs.getInt(1));
-				ic.setCreatedOn(rs.getTimestamp(3));
-				ic.setFAQ(rs.getBoolean(4));
-				ic.setBody(rs.getString(5));
-				
-				// Stuff into issue
-				Issue i = results.get(new Integer(ic.getID()));
-				if (i != null)
-					i.addComment(ic);
-			}
-			
-			// Clean up and return
-			rs.close();
-			_ps.close();
 			return results.values();
 		} catch (SQLException se) {
 			throw new DAOException(se);
@@ -229,16 +232,16 @@ public class GetHelp extends DAO {
 		_ps.close();
 		return results;
 	}
-	
+
 	/**
 	 * Helper method to parse Issue result sets.
 	 */
 	private List<Issue> executeIssue() throws SQLException {
-		
+
 		// Execute the query
 		ResultSet rs = _ps.executeQuery();
 		boolean hasCount = (rs.getMetaData().getColumnCount() > 12);
-		
+
 		// Load results
 		List<Issue> results = new ArrayList<Issue>();
 		while (rs.next()) {
@@ -257,11 +260,11 @@ public class GetHelp extends DAO {
 				i.setLastComment(rs.getTimestamp(12));
 				i.setLastCommentAuthorID(rs.getInt(13));
 			}
-			
+
 			// Add to results
 			results.add(i);
 		}
-		
+
 		// Clean up and return
 		rs.close();
 		_ps.close();
