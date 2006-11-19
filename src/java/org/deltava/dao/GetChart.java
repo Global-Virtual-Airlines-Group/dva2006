36494d6748f88fb8f3b6827125afc555b18a90c2
Luke Kolin <luke@deltava.org>
2006-11-19 22:46:46 +0000

    Clean up Charts DAOs
    git-svn-id: file:///home/svn/DVA2006@3547 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetChart.java
+++ src/java/org/deltava/dao/GetChart.java
@@ -4,12 +4,7 @@ package org.deltava.dao;
 import java.util.*;
 import java.sql.*;
 
-import java.sql.Connection;
-
-import org.deltava.beans.schedule.Airport;
-import org.deltava.beans.schedule.Chart;
-
-import org.deltava.util.cache.*;
+import org.deltava.beans.schedule.*;
 import org.deltava.util.system.SystemData;
 
 /**
@@ -20,196 +15,166 @@ import org.deltava.util.system.SystemData;
  */
 
 public class GetChart extends DAO {
-   
-   static Cache _cache = new AgingCache(48);
 
-    /**
-     * Creates the DAO with a JDBC connection.
-     * @param c the JDBC connection to use
-     */
-    public GetChart(Connection c) {
-        super(c);
-    }
+	/**
+	 * Creates the DAO with a JDBC connection.
+	 * @param c the JDBC connection to use
+	 */
+	public GetChart(Connection c) {
+		super(c);
+	}
 
-    /**
-     * Returns all Airports with available charts.
-     * @return a List of Airports
-     * @throws DAOException if a JDBC error occurs
-     */
-    public List<Airport> getAirports() throws DAOException {
-        try {
-            prepareStatementWithoutLimits("SELECT DISTINCT C.IATA FROM common.CHARTS C, common.AIRPORTS A " +
-            		"WHERE (C.IATA=A.IATA) ORDER BY A.NAME");
-            
-            // Get the airport info map
-            Map airports = (Map) SystemData.getObject("airports");
-            
-            // Execute the query
-            ResultSet rs = _ps.executeQuery();
-            List<Airport> results = new ArrayList<Airport>();
-            
-            // Iterate through the results
-            while (rs.next()) {
-                Airport a = (Airport) airports.get(rs.getString(1));
-                if (a != null)
-                	results.add(a);
-            }
-            
-            // Clean up and return
-            rs.close();
-            _ps.close();
-            return results;
-        } catch (SQLException se) {
-            throw new DAOException(se);
-        }
-    }
-    
-    /**
-     * Returns all Charts for a particular Airport code.
-     * @param iataCode the Airport's IATA code
-     * @return a List of Chart objects
-     * @throws DAOException if a JDBC error occurs
-     * @see GetChart#getCharts(Airport)
-     */
-    public List<Chart> getCharts(String iataCode) throws DAOException {
-        try {
-            // Prepare the statement
-            prepareStatement("SELECT ID, NAME, IATA, TYPE, SIZE FROM common.CHARTS WHERE (IATA=?) ORDER BY NAME");
-            _ps.setString(1, iataCode);
-            
-            // Execute the query
-            return execute();
-        } catch (SQLException se) {
-            throw new DAOException(se);
-        }
-    }
-    
-    /**
-     * Returns all Charts for a particular Airport.
-     * @param a the Airport object
-     * @return a List of Chart objects
-     * @throws DAOException if a JDBC error occurs
-     * @see GetChart#getCharts(String)
-     */
-    public List<Chart> getCharts(Airport a) throws DAOException {
-        return getCharts(a.getIATA());
-    }
-    
-    /**
-     * Returns all Charts for a particular Event.
-     * @param eventID the event Database ID
-     * @return a List of Chart objects
-     * @throws DAOException if a JDBC error occurs
-     */
-    public List<Chart> getChartsByEvent(int eventID) throws DAOException {
-       try {
-          prepareStatement("SELECT C.ID, C.NAME, C.IATA, C.TYPE, C.SIZE FROM common.CHARTS C, "
-          		+ "common.EVENT_CHARTS EC WHERE (EC.ID=?) AND (C.ID=EC.CHART) ORDER BY C.NAME");
-          _ps.setInt(1, eventID);
-          
-          // Execute the query
-          return execute();
-       } catch (SQLException se) {
-          throw new DAOException(se);
-       }
-    }
-    
-    /**
-     * Returns a Chart based on its Database ID. 
-     * @param id the database id
-     * @return the Chart
-     * @throws DAOException if a JDBC error occurs
-     */
-    public Chart get(int id) throws DAOException {
-       
-       // Check the cache
-       Chart result = (Chart) _cache.get(new Integer(id));
-       if (result != null)
-          return result;
-       
-        try {
-            // Prepare the statement
-            prepareStatement("SELECT ID, NAME, IATA, TYPE, SIZE FROM common.CHARTS WHERE (ID=?)");
-            _ps.setInt(1, id);
-            _ps.setMaxRows(1);
-            
-            // Execute the query
-            List<Chart> results = execute();
-            return (results.isEmpty()) ? null : results.get(0);
-        } catch (SQLException se) {
-            throw new DAOException(se);
-        }
-    }
-    
-    /**
-     * Retrieves Charts based on a group of database IDs.
-     * @param IDs a Collection of database IDs as Integers
-     * @return a Collection of Charts
-     * @throws DAOException if a JDBC error occurs
-     */
-    public Collection<Chart> getByIDs(Collection<Integer> IDs) throws DAOException {
-       
-       // Build the SQL statement
-       StringBuilder sqlBuf = new StringBuilder("SELECT ID, NAME, IATA, TYPE, SIZE FROM common.CHARTS WHERE (ID IN (");
-       
-       // Check if we're in the cache
-       int querySize = 0;
-       Collection<Chart> results = new TreeSet<Chart>();
-       for (Iterator<Integer> i = IDs.iterator(); i.hasNext(); ) {
-          Integer id = i.next();
-          Chart c = (Chart) _cache.get(id);
-          if (c != null) {
-             results.add(c);
-             i.remove();
-          } else {
-             querySize++;
-             sqlBuf.append(String.valueOf(id));
-             sqlBuf.append(',');
-          }
-       }
-       
-       // If we are getting everything from the cache, return the results
-       if (querySize == 0)
-          return results;
-       
-       // Clear off the trailing comma
-       if (sqlBuf.charAt(sqlBuf.length() - 1) == ',') sqlBuf.setLength(sqlBuf.length() - 1);
-       sqlBuf.append("))");
-       setQueryMax(querySize);
-       
-       // Load from the database
-       try {
-          prepareStatement(sqlBuf.toString());
-          results.addAll(execute());
-       } catch (SQLException se) {
-          throw new DAOException(se);
-       }
-       
-       return results;
-    }
-    
-    /**
-     * Helper method to load chart metadata.
-     */
-    private List<Chart> execute() throws SQLException {
-       List<Chart> results = new ArrayList<Chart>();
-       
-       // Execute the query
-       ResultSet rs = _ps.executeQuery();
-       while (rs.next()) {
-          Chart c = new Chart(rs.getString(2), SystemData.getAirport(rs.getString(3)));
-          c.setID(rs.getInt(1));
-          c.setType(rs.getInt(4));
-          c.setSize(rs.getInt(5));
-          
-          // Add to results and cache
-          results.add(c);
-          _cache.add(c);
-       }
-       
-       // Clean up and return
-       rs.close();
-       _ps.close();
-       return results;
-    }
+	/**
+	 * Returns all Airports with available charts.
+	 * @return a List of Airports
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public List<Airport> getAirports() throws DAOException {
+		try {
+			prepareStatementWithoutLimits("SELECT DISTINCT C.IATA FROM common.CHARTS C, common.AIRPORTS A "
+					+ "WHERE (C.IATA=A.IATA) ORDER BY A.NAME");
+
+			// Execute the query
+			ResultSet rs = _ps.executeQuery();
+			List<Airport> results = new ArrayList<Airport>();
+
+			// Iterate through the results
+			while (rs.next()) {
+				Airport a = SystemData.getAirport(rs.getString(1));
+				if (a != null)
+					results.add(a);
+			}
+
+			// Clean up and return
+			rs.close();
+			_ps.close();
+			return results;
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+
+	/**
+	 * Returns all Charts for a particular Airport code.
+	 * @param iataCode the Airport's IATA code
+	 * @return a List of Chart objects
+	 * @throws DAOException if a JDBC error occurs
+	 * @see GetChart#getCharts(Airport)
+	 */
+	public List<Chart> getCharts(String iataCode) throws DAOException {
+		try {
+			// Prepare the statement
+			prepareStatement("SELECT ID, NAME, IATA, TYPE, SIZE FROM common.CHARTS WHERE (IATA=?) ORDER BY NAME");
+			_ps.setString(1, iataCode);
+
+			// Execute the query
+			return execute();
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+
+	/**
+	 * Returns all Charts for a particular Airport.
+	 * @param a the Airport object
+	 * @return a List of Chart objects
+	 * @throws DAOException if a JDBC error occurs
+	 * @see GetChart#getCharts(String)
+	 */
+	public List<Chart> getCharts(Airport a) throws DAOException {
+		return getCharts(a.getIATA());
+	}
+
+	/**
+	 * Returns all Charts for a particular Event.
+	 * @param eventID the event Database ID
+	 * @return a List of Chart objects
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public List<Chart> getChartsByEvent(int eventID) throws DAOException {
+		try {
+			prepareStatement("SELECT C.ID, C.NAME, C.IATA, C.TYPE, C.SIZE FROM common.CHARTS C, "
+					+ "common.EVENT_CHARTS EC WHERE (EC.ID=?) AND (C.ID=EC.CHART) ORDER BY C.NAME");
+			_ps.setInt(1, eventID);
+
+			// Execute the query
+			return execute();
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+
+	/**
+	 * Returns a Chart based on its Database ID.
+	 * @param id the database id
+	 * @return the Chart
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public Chart get(int id) throws DAOException {
+		try {
+			// Prepare the statement
+			prepareStatement("SELECT ID, NAME, IATA, TYPE, SIZE FROM common.CHARTS WHERE (ID=?)");
+			_ps.setInt(1, id);
+			_ps.setMaxRows(1);
+
+			// Execute the query
+			List<Chart> results = execute();
+			return (results.isEmpty()) ? null : results.get(0);
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+
+	/**
+	 * Retrieves Charts based on a group of database IDs.
+	 * @param IDs a Collection of database IDs as Integers
+	 * @return a Collection of Charts
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public Collection<Chart> getByIDs(Collection<Integer> IDs) throws DAOException {
+
+		// Build the SQL statement
+		StringBuilder sqlBuf = new StringBuilder("SELECT ID, NAME, IATA, TYPE, SIZE FROM common.CHARTS WHERE (ID IN (");
+		for (Iterator<Integer> i = IDs.iterator(); i.hasNext();) {
+			Integer id = i.next();
+			sqlBuf.append(String.valueOf(id));
+			sqlBuf.append(',');
+		}
+
+		// Clear off the trailing comma
+		if (sqlBuf.charAt(sqlBuf.length() - 1) == ',')
+			sqlBuf.setLength(sqlBuf.length() - 1);
+		sqlBuf.append("))");
+		setQueryMax(IDs.size());
+
+		// Load from the database
+		try {
+			prepareStatement(sqlBuf.toString());
+			return execute();
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+
+	/**
+	 * Helper method to load chart metadata.
+	 */
+	private List<Chart> execute() throws SQLException {
+		List<Chart> results = new ArrayList<Chart>();
+
+		// Execute the query
+		ResultSet rs = _ps.executeQuery();
+		while (rs.next()) {
+			Chart c = new Chart(rs.getString(2), SystemData.getAirport(rs.getString(3)));
+			c.setID(rs.getInt(1));
+			c.setType(rs.getInt(4));
+			c.setSize(rs.getInt(5));
+			results.add(c);
+		}
+
+		// Clean up and return
+		rs.close();
+		_ps.close();
+		return results;
+	}
 }
--- src/java/org/deltava/dao/SetChart.java
+++ src/java/org/deltava/dao/SetChart.java
@@ -1,4 +1,4 @@
-// Copyright 2005 Luke J. Kolin. All Rights Reserved.
+// Copyright 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao;
 
 import java.sql.*;
@@ -14,83 +14,74 @@ import org.deltava.beans.schedule.Chart;
 
 public class SetChart extends DAO {
 
-   /**
-    * Initializes the Data Access Object.
-    * @param c the JDBC connection to use
-    */
-   public SetChart(Connection c) {
-      super(c);
-   }
+	/**
+	 * Initializes the Data Access Object.
+	 * @param c the JDBC connection to use
+	 */
+	public SetChart(Connection c) {
+		super(c);
+	}
 
-   /**
-    * Adds an Approach Chart to the database.
-    * @param c the Approach Chart
-    * @throws DAOException if a JDBC error occurs
-    */
-   public void write(Chart c) throws DAOException {
-      
-      // Make sure this is a new chart
-      if (c.getID() != 0)
-         throw new DAOException("Chart already exists");
-      
-      try {
-         prepareStatement("INSERT INTO CHARTS (IATA, TYPE, NAME, SIZE, IMG) VALUES (?, ?, ?, ?, ?)");
-         _ps.setString(1, c.getAirport().getIATA());
-         _ps.setInt(2, c.getImgType());
-         _ps.setString(3, c.getName());
-         _ps.setInt(4, c.getSize());
-         _ps.setBinaryStream(5, c.getInputStream(), c.getSize());
-         
-         // Update the database
-         executeUpdate(1);
-         
-         // Get the database ID
-         c.setID(getNewID());
-      } catch (SQLException se) {
-         throw new DAOException(se);
-      }
-      
-      // Clear the cache
-      GetChart._cache.remove(c.cacheKey());
-   }
-   
-   /**
-    * Updates an Approach Chart's metadata.
-    * @param c the Approach Chart
-    * @throws DAOException if a JDBC error occurs
-    */
-   public void update(Chart c) throws DAOException {
-      try {
-         prepareStatement("UPDATE CHARTS SET IATA=?, NAME=? WHERE (ID=?)");
-         _ps.setString(1, c.getAirport().getIATA());
-         _ps.setString(2, c.getName());
-         _ps.setInt(3, c.getID());
-         
-         // Update the database
-         executeUpdate(1);
-      } catch (SQLException se) {
-         throw new DAOException(se);
-      }
-      
-      // Clear the cache
-      GetChart._cache.remove(c.cacheKey());
-   }
-   
-   /**
-    * Deletes an Approach Chart from the Database.
-    * @param id the Chart database ID
-    * @throws DAOException if a JDBC error occurs
-    */
-   public void delete(int id) throws DAOException {
-      try {
-         prepareStatement("DELETE FROM CHARTS WHERE (ID=?)");
-         _ps.setInt(1, id);
-         executeUpdate(1);
-      } catch (SQLException se) {
-         throw new DAOException(se);
-      }
-      
-      // Clear the cache
-      GetChart._cache.remove(new Integer(id));
-   }
+	/**
+	 * Adds an Approach Chart to the database.
+	 * @param c the Approach Chart
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public void write(Chart c) throws DAOException {
+
+		// Make sure this is a new chart
+		if (c.getID() != 0)
+			throw new DAOException("Chart already exists");
+
+		try {
+			prepareStatement("INSERT INTO CHARTS (IATA, TYPE, NAME, SIZE, IMG) VALUES (?, ?, ?, ?, ?)");
+			_ps.setString(1, c.getAirport().getIATA());
+			_ps.setInt(2, c.getImgType());
+			_ps.setString(3, c.getName());
+			_ps.setInt(4, c.getSize());
+			_ps.setBinaryStream(5, c.getInputStream(), c.getSize());
+
+			// Update the database
+			executeUpdate(1);
+
+			// Get the database ID
+			c.setID(getNewID());
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+
+	/**
+	 * Updates an Approach Chart's metadata.
+	 * @param c the Approach Chart
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public void update(Chart c) throws DAOException {
+		try {
+			prepareStatement("UPDATE CHARTS SET IATA=?, NAME=? WHERE (ID=?)");
+			_ps.setString(1, c.getAirport().getIATA());
+			_ps.setString(2, c.getName());
+			_ps.setInt(3, c.getID());
+
+			// Update the database
+			executeUpdate(1);
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+
+	/**
+	 * Deletes an Approach Chart from the Database.
+	 * @param id the Chart database ID
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public void delete(int id) throws DAOException {
+		try {
+			prepareStatement("DELETE FROM CHARTS WHERE (ID=?)");
+			_ps.setInt(1, id);
+			executeUpdate(1);
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
 }
