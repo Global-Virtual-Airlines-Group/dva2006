a586dd59fb7c25d9737958cf3e6b7629f579b32e
Luke Kolin <luke@deltava.org>
2006-11-28 22:15:06 +0000

    Thread Read marks
    git-svn-id: file:///home/svn/DVA2006@3618 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/cooler/ThreadListCommand.java
+++ src/java/org/deltava/commands/cooler/ThreadListCommand.java
@@ -29,114 +29,115 @@ public class ThreadListCommand extends AbstractViewCommand {
 	 * @param ctx the Command context
 	 * @throws CommandException if an unhandled error occurs
 	 */
-public void execute(CommandContext ctx) throws CommandException {
+	public void execute(CommandContext ctx) throws CommandException {
 
-      // Get the user for the channel list
-      Person p = ctx.getUser();
+		// Get the user for the channel list
+		Person p = ctx.getUser();
 
-      // Get the default airline
-      AirlineInformation airline = SystemData.getApp(SystemData.get("airline.code"));
+		// Get the default airline
+		AirlineInformation airline = SystemData.getApp(SystemData.get("airline.code"));
 
-      // Check if we want to display image threads
-      boolean showImgThreads = (p instanceof Pilot) ? ((Pilot) p).getShowSSThreads() : true;
+		// Check if we want to display image threads
+		boolean showImgThreads = (p instanceof Pilot) ? ((Pilot) p).getShowSSThreads() : true;
 
-      // Get/set start/count parameters and channel name
-      ViewContext vc = initView(ctx);
-      String cName = (String) ctx.getCmdParameter(ID, "General Discussion");
+		// Get/set start/count parameters and channel name
+		ViewContext vc = initView(ctx);
+		String cName = (String) ctx.getCmdParameter(ID, "General Discussion");
 
-      try {
-         Connection con = ctx.getConnection();
+		try {
+			Connection con = ctx.getConnection();
 
-         // Get the Pilot's airline
-         GetUserData uddao = new GetUserData(con);
-         if (p != null) {
-            UserData usrData = uddao.get(p.getID());
-            if (usrData != null)
-               airline = SystemData.getApp(usrData.getAirlineCode());
-         }
+			// Get the Pilot's airline
+			GetUserData uddao = new GetUserData(con);
+			if (p != null) {
+				UserData usrData = uddao.get(p.getID());
+				if (usrData != null)
+					airline = SystemData.getApp(usrData.getAirlineCode());
+			}
 
-         // Get the channel DAO and the list of channels
-         GetCoolerChannels dao = new GetCoolerChannels(con);
-         Channel ch = dao.get(cName);
-         dao.setQueryMax(0);
-         ctx.setAttribute("channel", ch, REQUEST);
-         ctx.setAttribute("channels", dao.getChannels(airline, ctx.getRoles()), REQUEST);
+			// Get the channel DAO and the list of channels
+			GetCoolerChannels dao = new GetCoolerChannels(con);
+			Channel ch = dao.get(cName);
+			dao.setQueryMax(0);
+			ctx.setAttribute("channel", ch, REQUEST);
+			ctx.setAttribute("channels", dao.getChannels(airline, ctx.getRoles()), REQUEST);
 
-         // Initialize the channel access controller
-         CoolerChannelAccessControl cAccess = new CoolerChannelAccessControl(ctx, ch);
-         cAccess.validate();
-         ctx.setAttribute("channelAccess", cAccess, REQUEST);
+			// Initialize the channel access controller
+			CoolerChannelAccessControl cAccess = new CoolerChannelAccessControl(ctx, ch);
+			cAccess.validate();
+			ctx.setAttribute("channelAccess", cAccess, REQUEST);
 
-         // Get the Message Threads for this channel - add by 10% for filtering
-         GetCoolerThreads tdao = new GetCoolerThreads(con);
-         tdao.setQueryStart(vc.getStart());
-         tdao.setQueryMax(Math.round(vc.getCount() * 1.275f));
+			// Get the Message Threads for this channel - add by 10% for filtering
+			GetCoolerThreads tdao = new GetCoolerThreads(con);
+			tdao.setQueryStart(vc.getStart());
+			tdao.setQueryMax(Math.round(vc.getCount() * 1.275f));
 
-         // Initialize the access controller and the set to store pilot IDs
-         Collection<Integer> pilotIDs = new HashSet<Integer>();
-         CoolerThreadAccessControl ac = new CoolerThreadAccessControl(ctx);
-         
-         // Figure out what message threads to display
-         List<MessageThread> threads = null;
-         if (ch != null)
-        	 threads = tdao.getByChannel(cName, showImgThreads);
-         else if (Channel.SHOTS.equals(cName)) {
-        	 ctx.setAttribute("channelName", Channel.SHOTS.getComboName(), REQUEST);
-        	 ctx.setAttribute("channel", Channel.SHOTS, REQUEST);
-        	 threads = tdao.getScreenShots();
-         } else {
-        	 ctx.setAttribute("channelName", Channel.ALL.getComboName(), REQUEST);
-        	 threads = tdao.getByChannel(null, showImgThreads);
-         }
-         
-         // Validate our access to each thread
-         for (Iterator<MessageThread> i = threads.iterator(); i.hasNext();) {
-            MessageThread thread = i.next();
+			// Figure out what message threads to display
+			List<MessageThread> threads = null;
+			if (ch != null)
+				threads = tdao.getByChannel(cName, showImgThreads);
+			else if (Channel.SHOTS.equals(cName)) {
+				ctx.setAttribute("channelName", Channel.SHOTS.getComboName(), REQUEST);
+				ctx.setAttribute("channel", Channel.SHOTS, REQUEST);
+				threads = tdao.getScreenShots();
+			} else {
+				ctx.setAttribute("channelName", Channel.ALL.getComboName(), REQUEST);
+				threads = tdao.getByChannel(null, showImgThreads);
+			}
+			
+			// Initialize the access controller and the set to store pilot IDs
+			Collection<Integer> pilotIDs = new HashSet<Integer>();
+			CoolerThreadAccessControl ac = new CoolerThreadAccessControl(ctx);
 
-            // Get this thread's channel and see if we can read it
-            Channel c = dao.get(thread.getChannel());
-            ac.updateContext(thread, c);
-            ac.validate();
+			// Validate our access to each thread
+			for (Iterator<MessageThread> i = threads.iterator(); i.hasNext();) {
+				MessageThread thread = i.next();
 
-            // If we cannot read the thread, remove it from the results, otherwise load the pilot profiles
-            if (!ac.getCanRead()) {
-               i.remove();
-            } else {
-               pilotIDs.add(new Integer(thread.getAuthorID()));
-               pilotIDs.add(new Integer(thread.getLastUpdateID()));
-            }
-         }
+				// Get this thread's channel and see if we can read it
+				Channel c = dao.get(thread.getChannel());
+				ac.updateContext(thread, c);
+				ac.validate();
 
-         // Get the location of all the Pilots
-         UserDataMap udm = uddao.get(pilotIDs);
-         ctx.setAttribute("userData", udm, REQUEST);
+				// If we cannot read the thread, remove it from the results, otherwise load the pilot profiles
+				if (!ac.getCanRead()) {
+					i.remove();
+				} else {
+					pilotIDs.add(new Integer(thread.getAuthorID()));
+					pilotIDs.add(new Integer(thread.getLastUpdateID()));
+				}
+			}
 
-         // Get the authors for the last post in each channel
-         Map<Integer, Person> authors = new HashMap<Integer, Person>();
-         GetPilot pdao = new GetPilot(con);
-         GetApplicant adao = new GetApplicant(con);
-         for (Iterator<String> i = udm.getTableNames().iterator(); i.hasNext();) {
-            String tableName = i.next();
-            if (tableName.endsWith("APPLICANTS")) {
-               authors.putAll(adao.getByID(udm.getByTable(tableName), tableName));
-            } else {
-               authors.putAll(pdao.getByID(udm.getByTable(tableName), tableName));
-            }
-         }
+			// Get the location of all the Pilots
+			UserDataMap udm = uddao.get(pilotIDs);
+			ctx.setAttribute("userData", udm, REQUEST);
 
-         // Get the pilot IDs in the returned threads
-         ctx.setAttribute("pilots", authors, REQUEST);
+			// Get the authors for the last post in each channel
+			Map<Integer, Person> authors = new HashMap<Integer, Person>();
+			GetPilot pdao = new GetPilot(con);
+			GetApplicant adao = new GetApplicant(con);
+			for (Iterator<String> i = udm.getTableNames().iterator(); i.hasNext();) {
+				String tableName = i.next();
+				if (tableName.endsWith("APPLICANTS")) {
+					authors.putAll(adao.getByID(udm.getByTable(tableName), tableName));
+				} else {
+					authors.putAll(pdao.getByID(udm.getByTable(tableName), tableName));
+				}
+			}
 
-         // Save in the view context - If threads is still larger than the max, then cut it off
-         vc.setResults((threads.size() > vc.getCount()) ? threads.subList(0, vc.getCount()) : threads);
-      } catch (DAOException de) {
-         throw new CommandException(de);
-      } finally {
-         ctx.release();
-      }
+			// Get the pilot IDs in the returned threads
+			ctx.setAttribute("pilots", authors, REQUEST);
 
-      // Forward to JSP
-      CommandResult result = ctx.getResult();
-      result.setURL("/jsp/cooler/threadList.jsp");
-      result.setSuccess(true);
-   }}
+			// Save in the view context - If threads is still larger than the max, then cut it off
+			vc.setResults((threads.size() > vc.getCount()) ? threads.subList(0, vc.getCount()) : threads);
+		} catch (DAOException de) {
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
+
+		// Forward to JSP
+		CommandResult result = ctx.getResult();
+		result.setURL("/jsp/cooler/threadList.jsp");
+		result.setSuccess(true);
+	}
+}
