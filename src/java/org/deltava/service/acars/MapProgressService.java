fa94c2123a9f17a6d938aa6bb977eab4ac926093
Luke Kolin <luke@deltava.org>
2006-12-11 15:02:08 +0000

    Fix NPE
    git-svn-id: file:///home/svn/DVA2006@3698 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/service/acars/MapProgressService.java
+++ src/java/org/deltava/service/acars/MapProgressService.java
@@ -36,10 +36,10 @@ public class MapProgressService extends WebDataService {
 		int id = StringUtils.parse(ctx.getParameter("id"), 0);
 		if (id < 1)
 			return SC_NOT_FOUND;
-		
+
 		// Determine if we show the route
 		boolean doRoute = Boolean.valueOf(ctx.getParameter("route")).booleanValue();
-		
+
 		// Get the DAO and the route data
 		FlightInfo info = null;
 		Collection<GeoLocation> routePoints = null;
@@ -47,41 +47,43 @@ public class MapProgressService extends WebDataService {
 		try {
 			GetACARSData dao = new GetACARSData(_con);
 			routePoints = dao.getRouteEntries(id, false, false);
-			
+
 			// Load the route and the route waypoints
 			info = dao.getInfo(id);
 			if ((info != null) && doRoute) {
 				GetNavRoute navdao = new GetNavRoute(_con);
 				routeWaypoints = navdao.getRouteWaypoints(info.getRoute());
 			} else {
-				routeWaypoints = new HashSet<MapEntry>(); 
+				routeWaypoints = new HashSet<MapEntry>();
 			}
 		} catch (DAOException de) {
 			throw error(SC_INTERNAL_SERVER_ERROR, de.getMessage());
 		}
-		
+
 		// Generate the XML document
 		Document doc = new Document();
 		Element re = new Element("wsdata");
 		doc.setRootElement(re);
-		
+
 		// Determine if we cross the International Date Line
-		double longD = info.getAirportD().getLongitude();
-		double longA = info.getAirportA().getLongitude();
-		boolean crossIDL = ((longD > 80) && (longA < -40)) || ((longD < -40) && (longA > 80));
-		re.setAttribute("crossIDL", String.valueOf(crossIDL));
+		if (info != null) {
+			double longD = info.getAirportD().getLongitude();
+			double longA = info.getAirportA().getLongitude();
+			boolean crossIDL = ((longD > 80) && (longA < -40)) || ((longD < -40) && (longA > 80));
+			re.setAttribute("crossIDL", String.valueOf(crossIDL));
+		}
 
 		// Write the positions
-		for (Iterator<GeoLocation> i = routePoints.iterator(); i.hasNext(); ) {
+		for (Iterator<GeoLocation> i = routePoints.iterator(); i.hasNext();) {
 			GeoLocation entry = i.next();
 			Element e = new Element("pos");
 			e.setAttribute("lat", StringUtils.format(entry.getLatitude(), "##0.00000"));
 			e.setAttribute("lng", StringUtils.format(entry.getLongitude(), "##0.00000"));
 			re.addContent(e);
 		}
-		
+
 		// Write the route
-		for (Iterator<? extends MapEntry> i = routeWaypoints.iterator(); i.hasNext(); ) {
+		for (Iterator<? extends MapEntry> i = routeWaypoints.iterator(); i.hasNext();) {
 			MapEntry entry = i.next();
 			Element e = XMLUtils.createElement("route", entry.getInfoBox(), true);
 			e.setAttribute("lat", StringUtils.format(entry.getLatitude(), "##0.00000"));
@@ -89,7 +91,7 @@ public class MapProgressService extends WebDataService {
 			e.setAttribute("color", entry.getIconColor());
 			re.addContent(e);
 		}
-		
+
 		// Dump the XML to the output stream
 		try {
 			ctx.getResponse().setContentType("text/xml");
