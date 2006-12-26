b2ac4bd9bbe57d40db22e9ddc1cc947809ef29a7
Luke Kolin <luke@deltava.org>
2006-12-26 05:36:13 +0000

    Generate proper AirportServiceMap
    git-svn-id: file:///home/svn/DVA2006@3775 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetScheduleInfo.java
+++ src/java/org/deltava/dao/GetScheduleInfo.java
@@ -4,7 +4,9 @@ package org.deltava.dao;
 import java.sql.*;
 import java.util.*;
 
-import org.deltava.beans.schedule.Airline;
+import org.deltava.beans.schedule.*;
+
+import org.deltava.util.system.SystemData;
 
 /**
  * A Data Access Object to extract Flight Schedule data.
@@ -24,7 +26,7 @@ public class GetScheduleInfo extends DAO {
 	}
 
 	/**
-	 * Returns all flight numbers in a particular range. 
+	 * Returns all flight numbers in a particular range.
 	 * @param a the Airline bean
 	 * @param start the start of the range, or zero if none specified
 	 * @param end the end of the range, or zero if none specified
@@ -32,7 +34,7 @@ public class GetScheduleInfo extends DAO {
 	 * @throws DAOException if a JDBC error occurs
 	 */
 	public Collection<Integer> getFlightNumbers(Airline a, int start, int end) throws DAOException {
-		
+
 		// Build the SQL statement
 		StringBuilder sqlBuf = new StringBuilder("SELECT DISTINCT FLIGHT FROM SCHEDULE WHERE (AIRLINE=?)");
 		Collection<String> params = new ArrayList<String>();
@@ -40,18 +42,18 @@ public class GetScheduleInfo extends DAO {
 			params.add("(FLIGHT > ?)");
 		if (end > 0)
 			params.add("(FLIGHT < ?)");
-		
+
 		// Add parameters
 		if (!params.isEmpty()) {
-			for (Iterator<String> i = params.iterator(); i.hasNext(); ) {
+			for (Iterator<String> i = params.iterator(); i.hasNext();) {
 				String p = i.next();
 				sqlBuf.append(" AND ");
 				sqlBuf.append(p);
 			}
 		}
-		
+
 		sqlBuf.append(" ORDER BY FLIGHT");
-		
+
 		try {
 			prepareStatement(sqlBuf.toString());
 			_ps.setString(1, a.getCode());
@@ -59,13 +61,13 @@ public class GetScheduleInfo extends DAO {
 				_ps.setInt(2, start);
 			if (end > 0)
 				_ps.setInt(3, end);
-			
+
 			// Execute the query
 			ResultSet rs = _ps.executeQuery();
 			Collection<Integer> results = new LinkedHashSet<Integer>();
 			while (rs.next())
 				results.add(new Integer(rs.getInt(1)));
-			
+
 			// Clean up and return
 			rs.close();
 			_ps.close();
@@ -74,10 +76,10 @@ public class GetScheduleInfo extends DAO {
 			throw new DAOException(se);
 		}
 	}
-	
+
 	/**
 	 * Returns the next available Leg number for a Flight.
-	 * @param a the Airline bean 
+	 * @param a the Airline bean
 	 * @param flightNumber the flight number
 	 * @return the next available leg number
 	 * @throws DAOException if a JDBC error occurs
@@ -86,20 +88,50 @@ public class GetScheduleInfo extends DAO {
 	public int getNextLeg(Airline a, int flightNumber) throws DAOException {
 		if (flightNumber < 1)
 			throw new IllegalArgumentException("Invalid Flight Number -  " + flightNumber);
-		
+
 		try {
 			prepareStatement("SELECT MAX(LEG) FROM SCHEDULE WHERE (AIRLINE=?) AND (FLIGHT=?)");
 			_ps.setString(1, a.getCode());
 			_ps.setInt(2, flightNumber);
-			
+
 			// Do the query
 			ResultSet rs = _ps.executeQuery();
 			int leg = (rs.next()) ? rs.getInt(1) : 0;
-			
+
 			// Clean up, increment and return
 			rs.close();
 			_ps.close();
 			return ++leg;
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+
+	/**
+	 * Returns a map of serviced Airports by Airline.
+	 * @return an AirportServiceMap bean
+	 * @throws DAOException if a JDBC error occurs
+	 * @see AirportServiceMap
+	 */
+	public AirportServiceMap getRoutePairs() throws DAOException {
+		AirportServiceMap svcMap = new AirportServiceMap();
+
+		try {
+			prepareStatementWithoutLimits("SELECT DISTINCT AIRLINE, AIRPORT_D, AIRPORT_A FROM SCHEDULE "
+					+ "ORDER BY AIRPORT_D, AIRPORT_A");
+
+			// Do the query
+			ResultSet rs = _ps.executeQuery();
+			while (rs.next()) {
+				Airline a = SystemData.getAirline(rs.getString(1));
+				svcMap.add(a, SystemData.getAirport(rs.getString(2)));
+				svcMap.add(a, SystemData.getAirport(rs.getString(3)));
+			}
+			
+			// Clean up and return
+			rs.close();
+			_ps.close();
+			return svcMap;
 		} catch (SQLException se) {
 			throw new DAOException(se);
 		}
