67f481677e9ed0918696bf5c0a54927ab32db41c
Luke Kolin <luke@deltava.org>
2006-12-29 15:23:17 +0000

    Set pilot attribute when redirecting to assignUpdate.jsp
    git-svn-id: file:///home/svn/DVA2006@3799 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/assign/AssignmentDeleteCommand.java
+++ src/java/org/deltava/commands/assign/AssignmentDeleteCommand.java
@@ -22,44 +22,45 @@ import org.deltava.security.command.AssignmentAccessControl;
 
 public class AssignmentDeleteCommand extends AbstractCommand {
 
-   /**
-    * Executes the command.
-    * @param ctx the Command context
-    * @throws CommandException if an error occurs
-    */
-   public void execute(CommandContext ctx) throws CommandException {
+	/**
+	 * Executes the command.
+	 * @param ctx the Command context
+	 * @throws CommandException if an error occurs
+	 */
+	public void execute(CommandContext ctx) throws CommandException {
 
-      try {
-         Connection con = ctx.getConnection();
-         
-         // Get the DAO and the Assignment
-         GetAssignment dao = new GetAssignment(con);
-         AssignmentInfo assign = dao.get(ctx.getID());
-         if (assign == null)
-            throw notFoundException("Invalid Flight Assignment - " + ctx.getID());
-         
-         // Calculate our access
-         AssignmentAccessControl access = new AssignmentAccessControl(ctx, assign);
-         access.validate();
-         if (!access.getCanDelete())
-            throw securityException("Cannot delete Flight Assignment " + ctx.getID());
-         
-         // Delete the assignment
-         SetAssignment wdao = new SetAssignment(con);
-         wdao.delete(assign);
-      } catch (DAOException de) {
-         throw new CommandException(de);
-      } finally {
-         ctx.release();
-      }
-      
-      // Update status for JSP
-      ctx.setAttribute("isDelete", Boolean.TRUE, REQUEST);
+		try {
+			Connection con = ctx.getConnection();
 
-      // Forward to the JSP
-      CommandResult result = ctx.getResult();
-      result.setURL("/jsp/assign/assignUpdate.jsp");
-      result.setType(CommandResult.REQREDIRECT);
-      result.setSuccess(true);
-   }
+			// Get the DAO and the Assignment
+			GetAssignment dao = new GetAssignment(con);
+			AssignmentInfo assign = dao.get(ctx.getID());
+			if (assign == null)
+				throw notFoundException("Invalid Flight Assignment - " + ctx.getID());
+
+			// Calculate our access
+			AssignmentAccessControl access = new AssignmentAccessControl(ctx, assign);
+			access.validate();
+			if (!access.getCanDelete())
+				throw securityException("Cannot delete Flight Assignment " + ctx.getID());
+
+			// Delete the assignment
+			SetAssignment wdao = new SetAssignment(con);
+			wdao.delete(assign);
+		} catch (DAOException de) {
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
+
+		// Update status for JSP
+		ctx.setAttribute("pilot", ctx.getUser(), REQUEST);
+		ctx.setAttribute("isDelete", Boolean.TRUE, REQUEST);
+
+		// Forward to the JSP
+		CommandResult result = ctx.getResult();
+		result.setURL("/jsp/assign/assignUpdate.jsp");
+		result.setType(CommandResult.REQREDIRECT);
+		result.setSuccess(true);
+	}
 }
