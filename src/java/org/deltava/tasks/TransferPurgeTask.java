edcd4c003aed3981f272de2c8d9dca5f5f5db475
Luke Kolin <luke@deltava.org>
2007-01-01 05:50:24 +0000

    Add transfer request purge task
    git-svn-id: file:///home/svn/DVA2006@3808 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/tasks/TransferPurgeTask.java
@@ -0,0 +1,106 @@
+// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.tasks;
+
+import java.util.*;
+import java.sql.Connection;
+
+import org.deltava.beans.*;
+import org.deltava.beans.system.*; 
+import org.deltava.beans.testing.*;
+
+import org.deltava.dao.*;
+import org.deltava.mail.*;
+
+import org.deltava.taskman.DatabaseTask;
+
+import org.deltava.util.system.SystemData;
+
+/**
+ * A Scheduled Task to automatically purge old Transfer Requests.
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class TransferPurgeTask extends DatabaseTask {
+
+	/**
+	 * Initializes the Task.
+	 */
+	public TransferPurgeTask() {
+		super("Transfer Request Purge", TransferPurgeTask.class);
+	}
+
+	/**
+	 * Executes the task.
+	 */
+	protected void execute() {
+		log.info("Starting");
+		int purgeInterval = SystemData.getInt("users.transfer_max", 30);
+
+		try {
+			Connection con = getConnection();
+			GetTransferRequest txdao = new GetTransferRequest(con);
+			Collection<TransferRequest> oldTX = txdao.getAged(purgeInterval);
+			
+			// Get the Message template
+			GetMessageTemplate mtdao = new GetMessageTemplate(con);
+			MessageTemplate mt = mtdao.get("XFERREJECT");
+			
+			// Get the Pilot read DAO
+			GetPilot pdao = new GetPilot(con);
+			Pilot taskBy = pdao.getByName(SystemData.get("users.tasks_by"), SystemData.get("airline.db"));
+			
+			// Loop through the old transfers
+			GetExam exdao = new GetExam(con);
+			SetExam exwdao = new SetExam(con);
+			SetStatusUpdate swdao = new SetStatusUpdate(con);
+			SetTransferRequest txwdao = new SetTransferRequest(con);
+			for (Iterator<TransferRequest> i = oldTX.iterator(); i.hasNext(); ) {
+				TransferRequest tx = i.next();
+				Pilot p = pdao.get(tx.getID());
+
+				// Make a status update
+				StatusUpdate upd = new StatusUpdate(tx.getID(), StatusUpdate.COMMENT);
+				upd.setAuthorID(taskBy.getID());
+				upd.setDescription("Transfer to " + tx.getEquipmentType() + " program Rejected after " + purgeInterval + " days");
+				
+				// Get the check ride (if any) and then delete
+				CheckRide cr = exdao.getCheckRide(tx.getCheckRideID());
+				if ((cr == null) || (cr.getStatus() == Test.NEW)) {
+					startTX();
+					
+					// Delete the checkride and the transfer request
+					exwdao.delete(cr);
+					txwdao.delete(tx.getID());
+					swdao.write(upd);
+					
+					// Commit the transaction
+					commitTX();
+					
+					// Log the deletion
+					log.warn("Deleting transfer to " + tx.getEquipmentType() + " by " + p.getName() + " after " + purgeInterval + " days");
+					
+					// Create the e-mail message
+					MessageContext mctxt = new MessageContext();
+					mctxt.setTemplate(mt);
+					mctxt.addData("user", taskBy);
+					mctxt.addData("pilot", p);
+					mctxt.addData("txReq", tx);
+					mctxt.addData("rejectComments", "Transfer automatically deleted after " + purgeInterval + " days");
+
+					// Send a notification message
+					Mailer mailer = new Mailer(taskBy);
+					mailer.setContext(mctxt);
+					mailer.send(p);
+				}
+			}
+		} catch (DAOException de) {
+			log.error(de.getMessage(), de);
+		} finally {
+			release();
+		}
+		
+		log.info("Completed");
+	}
+}
