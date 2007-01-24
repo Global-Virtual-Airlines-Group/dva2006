1384c662f6e054f5b38e6f9dbead9fd6152bfd7e
Luke Kolin <luke@deltava.org>
2007-01-24 19:32:51 +0000

    Build 128
    git-svn-id: file:///home/svn/DVA2006@3879 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -13,8 +13,8 @@ public interface VersionInfo {
     public static final int MAJOR = 1;
     public static final int MINOR = 0;
     
-    public static final int BUILD = 127;
-    public static final int RELEASE_CANDIDATE = 3;
+    public static final int BUILD = 128;
+    // public static final int RELEASE_CANDIDATE = 3;
     public static final boolean FINAL = false;
     
     public static final String TXT_COPYRIGHT = "Copyright 2004, 2005, 2006, 2007 Global Virtual Airlines Group. All Rights Reserved.";
