bf4c5a68d18cc9f3664aaf3b1b0a6ab48b858324
Luke Kolin <luke@deltava.org>
2007-01-25 20:33:06 +0000

    Don't let Moderator role access all threads
    git-svn-id: file:///home/svn/DVA2006@3887 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/security/command/CoolerThreadAccessControl.java
+++ src/java/org/deltava/security/command/CoolerThreadAccessControl.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.security.command;
 
 import java.util.List;
@@ -91,7 +91,7 @@ public final class CoolerThreadAccessControl extends AccessControl {
         boolean hasVoted = (_ctx.getUser() != null) && _mt.hasVoted(_ctx.getUser().getID());
         
         // Validate if we can read the thread
-        _canRead = _ctx.isUserInRole("Moderator") || (channelAccess && !_mt.getHidden());
+        _canRead = _ctx.isUserInRole("Admin") || (channelAccess && !_mt.getHidden());
         _canReply = _ctx.isAuthenticated() && _canRead && (isModerator || !isClosed);
         _canVote = _canReply && !_mt.getOptions().isEmpty() && !hasVoted;
         _canLock = channelAccess && !isClosed && isModerator;
