047fedbf2e56882556dbba6fc3c9f6f968aaa85d
Luke Kolin <luke@deltava.org>
2007-02-10 04:03:37 +0000

    JavaDoc updates
    git-svn-id: file:///home/svn/DVA2006@3976 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/schedule/AirportServiceMap.java
+++ src/java/org/deltava/beans/schedule/AirportServiceMap.java
@@ -1,4 +1,4 @@
-// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2006, 2007 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.schedule;
 
 import java.util.*;
@@ -6,8 +6,8 @@ import java.util.*;
 /**
  * A bean to track which Airports are served by particular Airlines. 
  * @author Luke
- * @verison 1.0
- * @sine 1.0
+ * @version 1.0
+ * @since 1.0
  */
 
 public class AirportServiceMap {
