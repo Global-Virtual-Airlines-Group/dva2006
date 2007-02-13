15ab408f0c0a86ed3786e250544eb292c9aadc98
Luke Kolin <luke@deltava.org>
2007-02-13 16:43:28 +0000

    Fix to match changes to ACARS tables
    git-svn-id: file:///home/svn/DVA2006@3990 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/SetACARSData.java
+++ src/java/org/deltava/dao/SetACARSData.java
@@ -53,21 +53,20 @@ public class SetACARSData extends DAO {
 	 */
 	public void createFlight(FlightInfo info) throws DAOException {
 		try {
-			prepareStatement("INSERT INTO acars.FLIGHTS (CON_ID, FLIGHT_NUM, CREATED, END_TIME, EQTYPE, CRUISE_ALT, "
-					+ "AIRPORT_D, AIRPORT_A, ROUTE, REMARKS, FSVERSION, OFFLINE, PIREP) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-			_ps.setLong(1, info.getConnectionID());
-			_ps.setString(2, info.getFlightCode());
-			_ps.setTimestamp(3, createTimestamp(info.getStartTime()));
-			_ps.setTimestamp(4, createTimestamp(info.getEndTime()));
-			_ps.setString(5, info.getEquipmentType());
-			_ps.setString(6, info.getAltitude());
-			_ps.setString(7, info.getAirportD().getIATA());
-			_ps.setString(8, info.getAirportA().getIATA());
-			_ps.setString(9, info.getRoute());
-			_ps.setString(10, info.getRemarks());
-			_ps.setInt(11, info.getFSVersion());
-			_ps.setBoolean(12, info.getOffline());
-			_ps.setBoolean(13, true);
+			prepareStatement("INSERT INTO acars.FLIGHTS (FLIGHT_NUM, CREATED, END_TIME, EQTYPE, CRUISE_ALT, AIRPORT_D, "
+					+ "AIRPORT_A, ROUTE, REMARKS, FSVERSION, OFFLINE, PIREP) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			_ps.setString(1, info.getFlightCode());
+			_ps.setTimestamp(2, createTimestamp(info.getStartTime()));
+			_ps.setTimestamp(3, createTimestamp(info.getEndTime()));
+			_ps.setString(4, info.getEquipmentType());
+			_ps.setString(5, info.getAltitude());
+			_ps.setString(6, info.getAirportD().getIATA());
+			_ps.setString(7, info.getAirportA().getIATA());
+			_ps.setString(8, info.getRoute());
+			_ps.setString(9, info.getRemarks());
+			_ps.setInt(10, info.getFSVersion());
+			_ps.setBoolean(11, info.getOffline());
+			_ps.setBoolean(12, true);
 			executeUpdate(1);
 			
 			// Since we're writing a new entry, get the database ID
@@ -80,49 +79,47 @@ public class SetACARSData extends DAO {
 	
 	/**
 	 * Writes a Flight's position entries to the database.
-	 * @param cid the Connection ID
 	 * @param flightID the Flight ID
 	 * @param entries a Collection of RouteEntry beans
 	 * @throws DAOException if a JDBC error occurs
 	 */
-	public void writePositions(long cid, int flightID, Collection<RouteEntry> entries) throws DAOException {
+	public void writePositions(int flightID, Collection<RouteEntry> entries) throws DAOException {
 		try {
-			prepareStatementWithoutLimits("REPLACE INTO acars.POSITIONS (CON_ID, FLIGHT_ID, REPORT_TIME, TIME_MS, LAT, LNG, B_ALT, "
+			prepareStatementWithoutLimits("REPLACE INTO acars.POSITIONS (FLIGHT_ID, REPORT_TIME, TIME_MS, LAT, LNG, B_ALT, "
 					+ "R_ALT, HEADING, ASPEED, GSPEED, VSPEED, N1, N2, MACH, FUEL, PHASE, SIM_RATE, FLAGS, FLAPS, PITCH, BANK, "
-					+ "FUELFLOW, WIND_HDG, WIND_SPEED, AOA, GFORCE, FRAMERATE) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, "
+					+ "FUELFLOW, WIND_HDG, WIND_SPEED, AOA, GFORCE, FRAMERATE) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, "
 					+ "?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 			
 			// Loop through the positions
-			_ps.setLong(1, cid);
-			_ps.setInt(2, flightID);
+			_ps.setInt(1, flightID);
 			for (Iterator<RouteEntry> i = entries.iterator(); i.hasNext(); ) {
 				RouteEntry re = i.next();
-				_ps.setTimestamp(3, createTimestamp(re.getDate()));
-				_ps.setInt(4, CalendarUtils.getInstance(re.getDate()).get(Calendar.MILLISECOND));
-				_ps.setDouble(5, re.getLatitude());
-				_ps.setDouble(6, re.getLongitude());
-				_ps.setInt(7, re.getAltitude());
-				_ps.setInt(8, re.getRadarAltitude());
-				_ps.setInt(9, re.getHeading());
-				_ps.setInt(10, re.getAirSpeed());
-				_ps.setInt(11, re.getGroundSpeed());
-				_ps.setInt(12, re.getVerticalSpeed());
-				_ps.setDouble(13, re.getN1());
-				_ps.setDouble(14, re.getN2());
-				_ps.setDouble(15, re.getMach());
-				_ps.setInt(16, re.getFuelRemaining());
-				_ps.setInt(17, re.getPhase());
-				_ps.setInt(18, re.getSimRate());
-				_ps.setInt(19, re.getFlags());
-				_ps.setInt(20, re.getFlaps());
-				_ps.setDouble(21, re.getPitch());
-				_ps.setDouble(22, re.getBank());
-				_ps.setInt(23, re.getFuelFlow());
-				_ps.setInt(24, re.getWindHeading());
-				_ps.setInt(25, re.getWindSpeed());
-				_ps.setDouble(26, re.getAOA());
-				_ps.setDouble(27, re.getG());
-				_ps.setInt(28, re.getFrameRate());
+				_ps.setTimestamp(2, createTimestamp(re.getDate()));
+				_ps.setInt(3, CalendarUtils.getInstance(re.getDate()).get(Calendar.MILLISECOND));
+				_ps.setDouble(4, re.getLatitude());
+				_ps.setDouble(5, re.getLongitude());
+				_ps.setInt(6, re.getAltitude());
+				_ps.setInt(7, re.getRadarAltitude());
+				_ps.setInt(8, re.getHeading());
+				_ps.setInt(9, re.getAirSpeed());
+				_ps.setInt(10, re.getGroundSpeed());
+				_ps.setInt(11, re.getVerticalSpeed());
+				_ps.setDouble(12, re.getN1());
+				_ps.setDouble(13, re.getN2());
+				_ps.setDouble(14, re.getMach());
+				_ps.setInt(15, re.getFuelRemaining());
+				_ps.setInt(16, re.getPhase());
+				_ps.setInt(17, re.getSimRate());
+				_ps.setInt(18, re.getFlags());
+				_ps.setInt(19, re.getFlaps());
+				_ps.setDouble(20, re.getPitch());
+				_ps.setDouble(21, re.getBank());
+				_ps.setInt(22, re.getFuelFlow());
+				_ps.setInt(23, re.getWindHeading());
+				_ps.setInt(24, re.getWindSpeed());
+				_ps.setDouble(25, re.getAOA());
+				_ps.setDouble(26, re.getG());
+				_ps.setInt(27, re.getFrameRate());
 				_ps.addBatch();
 			}
 			
