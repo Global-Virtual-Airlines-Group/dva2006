8f51d80eab7f5e341fca06998ffc2d94340d3019
Luke Kolin <luke@deltava.org>
2007-02-21 03:54:59 +0000

    Misc fixes
    git-svn-id: file:///home/svn/DVA2006@4008 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/security/FSDAuthenticator.java
+++ src/java/org/deltava/security/FSDAuthenticator.java
@@ -17,75 +17,67 @@ import org.deltava.util.*;
  */
 
 public class FSDAuthenticator implements Authenticator {
-	
+
 	private static final Logger log = Logger.getLogger(FSDAuthenticator.class);
-	
+
 	private final Properties _props = new Properties();
 	private File _certFile;
 	private final Map<Integer, FSDCert> _certs = Collections.synchronizedMap(new LinkedHashMap<Integer, FSDCert>());
-	private final Collection<FSDCert> _atcCerts = new LinkedHashSet<FSDCert>();
 	private final Map<String, Integer> _roleLevels = new HashMap<String, Integer>();
-	
+
 	private static final int MAX_CERT_LEVEL = 12;
-	protected static final String[] CERT_TYPES = {"P", "A"};
-	private static final String[] LEVEL_NAMES = {"Disabled", "Pilot", "Student 1", "Student 2", "Student 3", "Controller 1", "Controller 2",
-		"Controller 3", "Instructor 1", "Instructor 2", "Instructor 3", "Supervisor", "Administrator" };
-	
-	private static final int PILOT = 0;
-	private static final int ATC = 1;
-	
+	private static final String[] LEVEL_NAMES = { "Disabled", "Pilot", "Student 1", "Student 2", "Student 3",
+			"Controller 1", "Controller 2", "Controller 3", "Instructor 1", "Instructor 2", "Instructor 3",
+			"Supervisor", "Administrator" };
+
 	private class FSDCert extends DatabaseBean {
-		
-		private int _type;
+
 		private String _userID;
 		private String _pwd;
 		private int _level;
-		
-		FSDCert(int type, String userID, String pwd, int level) {
+
+		FSDCert(String userID, String pwd, int level) {
 			super();
-			_type = type;
 			_userID = userID.toUpperCase();
 			_pwd = pwd;
 			_level = Math.min(Math.max(1, level), MAX_CERT_LEVEL);
 		}
-		
+
 		public String getUserID() {
 			return _userID;
 		}
-		
+
 		public String getPassword() {
 			return _pwd;
 		}
-		
+
 		public int getLevel() {
 			return _level;
 		}
-		
+
 		public void setPassword(String pwd) {
 			_pwd = pwd;
 		}
-		
+
 		public int hashCode() {
 			return _userID.hashCode();
 		}
-		
+
 		public String toString() {
-			StringBuilder buf = new StringBuilder(CERT_TYPES[_type]);
-			buf.append(' ');
-			buf.append(_userID);
+			StringBuilder buf = new StringBuilder(_userID);
 			buf.append(' ');
 			buf.append(_pwd);
 			buf.append(' ');
 			buf.append(String.valueOf(_level));
 			if (getID() > 0) {
-				buf.append("; ");
+				buf.append(" ;");
 				buf.append(String.valueOf(getID()));
 			}
-			
+
 			return buf.toString();
 		}
 	}
-	
+
 	/**
 	 * Initialize the Authenticator and populate its user list from a file.
 	 * @param propsFile the properties file to use
@@ -101,58 +93,57 @@ public class FSDAuthenticator implements Authenticator {
 			log.error("Error loading " + propsFile + " - " + ie.getMessage());
 			throw new SecurityException(ie.getMessage());
 		}
-		
+
 		// Load the role mappings
 		for (int x = 1; x <= MAX_CERT_LEVEL; x++) {
 			String roleNames = _props.getProperty("fsd.level." + String.valueOf(x), "");
 			Collection<String> roles = StringUtils.split(roleNames, ",");
-			for (Iterator<String> i = roles.iterator(); i.hasNext(); )
+			for (Iterator<String> i = roles.iterator(); i.hasNext();)
 				_roleLevels.put(i.next(), new Integer(x));
 		}
-		
+
 		// Check for the certificate file
 		_certFile = new File(_props.getProperty("fsd.cert"));
 		if (!_certFile.exists()) {
 			log.warn("Cannot open " + _certFile.getPath() + " - not found");
 			return;
 		}
-		
+
 		// Load the certificates
 		try {
 			LineNumberReader br = new LineNumberReader(new FileReader(_certFile));
 			while (br.ready()) {
-				StringTokenizer tkns = new StringTokenizer(br.readLine(), " ");
-				if (tkns.countTokens() < 5)
-					log.warn("Invalid token count on Line " + br.getLineNumber() + " tokens=" + tkns.countTokens());
-				else {
-					int certType = StringUtils.arrayIndexOf(CERT_TYPES, tkns.nextToken().toUpperCase());
-					String userID = tkns.nextToken();
-					try {
-						FSDCert cert = new FSDCert(certType, userID, tkns.nextToken(), StringUtils.parse(tkns.nextToken(), 1));
-						if (certType != ATC) {
+				String data = br.readLine();
+				if (!data.startsWith(";")) {
+					StringTokenizer tkns = new StringTokenizer(data, " ");
+					if (tkns.countTokens() < 4)
+						log.warn("Invalid token count on Line " + br.getLineNumber() + " tokens=" + tkns.countTokens());
+					else {
+						String userID = tkns.nextToken();
+						try {
+							FSDCert cert = new FSDCert(userID, tkns.nextToken(), StringUtils.parse(tkns.nextToken(), 1));
 							String dbID = tkns.nextToken().substring(1);
 							cert.setID(StringUtils.parse(dbID, 0));
 							_certs.put(new Integer(cert.getID()), cert);
-						} else
-							_atcCerts.add(cert);
-					} catch (IllegalArgumentException iae) {
-						log.warn("Invalid database ID for " + userID + " - " + iae.getMessage());
+						} catch (IllegalArgumentException iae) {
+							log.warn("Invalid database ID for " + userID + " - " + iae.getMessage());
+						}
 					}
 				}
 			}
-			
+
 			br.close();
 		} catch (IOException ie) {
 			log.warn("Error loading " + _props.getProperty("fsd.cert") + " - " + ie.getMessage());
 		}
-		
+
 		log.info("Loaded " + _certs.size() + " user records");
 	}
 
 	/**
 	 * Authenticates the user by searching for the directory name and then comparing the existing password on file to
-	 * the one specified. Even if the credentials match, authentication will fail if the user's roles do not translate to at
-	 * least Level 1.
+	 * the one specified. Even if the credentials match, authentication will fail if the user's roles do not translate
+	 * to at least Level 1.
 	 * @param usr the User bean
 	 * @param pwd the user's password
 	 * @throws SecurityException if authentication fails
@@ -163,7 +154,7 @@ public class FSDAuthenticator implements Authenticator {
 		int level = getUserLevel(usr);
 		if (level == 0)
 			throw new SecurityException(usr.getName() + " not authorized to access FSD");
-		
+
 		// Get the user
 		FSDCert cert = _certs.get(new Integer(usr.getID()));
 		if (cert == null)
@@ -173,10 +164,10 @@ public class FSDAuthenticator implements Authenticator {
 		if (!cert.getPassword().equals(pwd))
 			throw new SecurityException("Cannot authenticate " + usr.getName() + " - Invalid Credentials");
 	}
-	
+
 	/**
-	 * Checks wether this Authenticator accepts a User. The user must be a member of a role that maps
-	 * to at least Level 1.
+	 * Checks wether this Authenticator accepts a User. The user must be a member of a role that maps to at least Level
+	 * 1.
 	 * @param usr the user bean
 	 * @return TRUE if the User is a member of a Role mapping to at least Level 1, otherwise FALSE
 	 */
@@ -203,7 +194,7 @@ public class FSDAuthenticator implements Authenticator {
 		FSDCert cert = _certs.get(new Integer(usr.getID()));
 		if (cert == null)
 			throw new SecurityException(usr.getName() + " not found!");
-		
+
 		// Update the password and save
 		cert.setPassword(pwd);
 		save();
@@ -216,21 +207,22 @@ public class FSDAuthenticator implements Authenticator {
 	 * @throws SecurityException if an error occurs
 	 */
 	public void addUser(Person usr, String pwd) throws SecurityException {
-		
+
 		// Get the user's level; abort if they do not have access
 		int level = getUserLevel(usr);
 		if ((level == 0) || (!(usr instanceof Pilot)))
 			return;
-		
+
 		// Add the user
 		Pilot p = (Pilot) usr;
-		FSDCert cert = new FSDCert(PILOT, p.getPilotCode(), pwd, level);
+		FSDCert cert = new FSDCert(p.getPilotCode(), pwd, level);
 		cert.setID(usr.getID());
 		_certs.put(new Integer(cert.getID()), cert);
 		save();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
 	 * @see org.deltava.security.Authenticator#rename(org.deltava.beans.Person, java.lang.String)
 	 */
 	public void rename(Person usr, String newName) throws SecurityException {
@@ -251,25 +243,25 @@ public class FSDAuthenticator implements Authenticator {
 		_certs.remove(new Integer(usr.getID()));
 		save();
 	}
-	
+
 	/**
 	 * Helper method to return the user's access level based on their security roles.
 	 */
 	private int getUserLevel(Person usr) {
 		if (usr == null)
 			return 0;
-		
+
 		int result = 0;
-		for (Iterator<String> i = usr.getRoles().iterator(); i.hasNext(); ) {
+		for (Iterator<String> i = usr.getRoles().iterator(); i.hasNext();) {
 			String role = i.next();
 			Integer roleLevel = _roleLevels.get(role);
 			if (roleLevel != null)
 				result = Math.max(result, roleLevel.intValue());
 		}
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * Helper method to save the certs to disk.
 	 */
@@ -277,21 +269,21 @@ public class FSDAuthenticator implements Authenticator {
 		Collection<FSDCert> certs = new LinkedHashSet<FSDCert>(_certs.values());
 		try {
 			PrintWriter pw = new PrintWriter(_certFile);
-			
+
 			// Write file header
 			pw.println("; Auto-Generated FSD certs on " + new Date().toString());
 			pw.println(";");
 			pw.println("; User levels");
 			for (int x = 0; x < LEVEL_NAMES.length; x++)
 				pw.println("; " + String.valueOf(x) + " = " + LEVEL_NAMES[x]);
-			
+
 			// Write cert data
 			pw.println(";");
-			for (Iterator<FSDCert> i = certs.iterator(); i.hasNext(); ) {
+			for (Iterator<FSDCert> i = certs.iterator(); i.hasNext();) {
 				FSDCert cert = i.next();
 				pw.println(cert.toString());
 			}
-			
+
 			pw.close();
 		} catch (IOException ie) {
 			throw new SecurityException(ie);
