15333d0025e386c157d90eba8a3bce6bbf7781d4
Luke Kolin <luke@deltava.org>
2007-04-05 05:18:28 +0000

    Fix join
    git-svn-id: file:///home/svn/DVA2006@4145 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetACARSLog.java
+++ src/java/org/deltava/dao/GetACARSLog.java
@@ -43,7 +43,7 @@ public class GetACARSLog extends GetACARSData {
 		// Build the SQL statement
 		StringBuilder buf = new StringBuilder("SELECT C.ID, C.PILOT_ID, C.DATE, INET_NTOA(C.REMOTE_ADDR), "
 				+ "C.REMOTE_HOST, C.CLIENT_BUILD, COUNT(DISTINCT F.ID), COUNT(P.CON_ID) FROM acars.CONS C "
-				+ "LEFT JOIN acars.FLIGHTS F ON (C.ID=F.CON_ID) LEFT JOIN acars.POSITIONS P ON (F.ID=P.CON_ID)");
+				+ "LEFT JOIN acars.FLIGHTS F ON (C.ID=F.CON_ID) LEFT JOIN acars.POSITIONS P ON (F.ID=P.FLIGHT_ID)");
 
 		// Add the terms
 		if (!terms.isEmpty()) {
