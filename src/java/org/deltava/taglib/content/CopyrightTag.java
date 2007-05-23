df646920625845bf346305f03445365a8a69db91
Luke Kolin <luke@deltava.org>
2007-05-23 02:17:46 +0000

    Update FINAL handling
    git-svn-id: file:///home/svn/DVA2006@4237 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/content/CopyrightTag.java
+++ src/java/org/deltava/taglib/content/CopyrightTag.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.content;
 
 import java.lang.reflect.Field;
@@ -19,106 +19,108 @@ import org.deltava.util.system.SystemData;
 
 public class CopyrightTag extends TagSupport {
 
-   private static int _rcBuild = -1;
-   private boolean _visible = true;
+	private static int _rcBuild = -1;
+	private boolean _visible = true;
 
-   /**
-    * Marks the copyright tag as visible instead of embedded in the HTML code.
-    * @param isVisible TRUE if the tag should be visible, otherwise FALSE
-    */
-   public void setVisible(boolean isVisible) {
-      _visible = isVisible;
-   }
+	/**
+	 * Marks the copyright tag as visible instead of embedded in the HTML code.
+	 * @param isVisible TRUE if the tag should be visible, otherwise FALSE
+	 */
+	public void setVisible(boolean isVisible) {
+		_visible = isVisible;
+	}
 
-   private void displayCopyrightComment() throws Exception {
-      JspWriter jw = pageContext.getOut();
-      jw.print("<!-- ");
-      jw.print(pageContext.getServletContext().getServletContextName());
-      jw.print(" ");
-      jw.print(VersionInfo.APPNAME);
-      jw.print(" ");
-      jw.print(VersionInfo.TXT_COPYRIGHT);
-      jw.print(" (Build ");
-      jw.print(String.valueOf(VersionInfo.BUILD));
-      if (_rcBuild > 0) {
-         jw.print(" Release Candidate ");
-         jw.print(String.valueOf(_rcBuild));
-      }
+	private void displayCopyrightComment() throws Exception {
+		JspWriter jw = pageContext.getOut();
+		jw.print("<!-- ");
+		jw.print(pageContext.getServletContext().getServletContextName());
+		jw.print(" ");
+		jw.print(VersionInfo.APPNAME);
+		jw.print(" ");
+		jw.print(VersionInfo.TXT_COPYRIGHT);
+		jw.print(" (Build ");
+		jw.print(String.valueOf(VersionInfo.BUILD));
+		if (_rcBuild > 0) {
+			jw.print(" Release Candidate ");
+			jw.print(String.valueOf(_rcBuild));
+		} else if (VersionInfo.FINAL)
+			jw.print("-FINAL");
 
-      jw.print(") -->");
-   }
+		jw.print(") -->");
+	}
 
-   private void displayCopyright() throws Exception {
-      JspWriter jw = pageContext.getOut();
-      jw.println("<hr />");
-      jw.print("<div class=\"small\">");
-      jw.print(pageContext.getServletContext().getServletContextName());
-      jw.print(" ");
-      jw.print(VersionInfo.APPNAME + " " + VersionInfo.HTML_COPYRIGHT + " (Build " + VersionInfo.BUILD);
-      if (_rcBuild > 0) {
-          jw.print(" Release Candidate ");
-          jw.print(String.valueOf(_rcBuild));
-       } else if (VersionInfo.FINAL) {
-    	   jw.print(" FINAL");
-       }
-      
-      jw.print(")</div>");
-      
-      // Display disclaimer
-      String disclaimer = SystemData.get("airline.copyright");
-      if (disclaimer != null) {
-    	  jw.print("\n<span class=\"copyright small\">");
-    	  jw.print(disclaimer);
-    	  jw.print("</span>");
-      }
-   }
+	private void displayCopyright() throws Exception {
+		JspWriter jw = pageContext.getOut();
+		jw.println("<hr />");
+		jw.print("<div class=\"small\">");
+		jw.print(pageContext.getServletContext().getServletContextName());
+		jw.print(" ");
+		jw.print(VersionInfo.APPNAME + " " + VersionInfo.HTML_COPYRIGHT + " (Build " + VersionInfo.BUILD);
+		if (_rcBuild > 0) {
+			jw.print(" Release Candidate ");
+			jw.print(String.valueOf(_rcBuild));
+		} else if (VersionInfo.FINAL)
+			jw.print("-FINAL");
 
-   /**
-    * Checks for optional data in VersionInfo constants.
-    * @return TagSupport.SKIP_BODY always
-    * @throws JspException if an error occurs
-    */
-   public int doStartTag() throws JspException {
-      if (_rcBuild >= 0)
-         return SKIP_BODY;
+		jw.print(")</div>");
 
-      // Get the release candidate
-      try {
-         Field f = VersionInfo.class.getField("RELEASE_CANDIDATE");
-         _rcBuild = f.getInt(null);
-      } catch (Exception e) {
-         _rcBuild = 0;
-      }
+		// Display disclaimer
+		String disclaimer = SystemData.get("airline.copyright");
+		if (disclaimer != null) {
+			jw.print("\n<span class=\"copyright small\">");
+			jw.print(disclaimer);
+			jw.print("</span>");
+		}
+	}
 
-      return SKIP_BODY;
-   }
+	/**
+	 * Checks for optional data in VersionInfo constants.
+	 * @return TagSupport.SKIP_BODY always
+	 * @throws JspException if an error occurs
+	 */
+	public int doStartTag() throws JspException {
 
-   /**
-    * Renders the copyright tag to the JSP output stream.
-    * @return TagSupport.EVAL_PAGE always
-    * @throws JspException if an I/O error occurs
-    */
-   public int doEndTag() throws JspException {
-      try {
-         if (_visible) {
-            displayCopyright();
-         } else {
-            displayCopyrightComment();
-         }
-      } catch (Exception e) {
-         throw new JspException("Error writing " + getClass().getName(), e);
-      }
+		// Get the release candidate
+		synchronized (CopyrightTag.class) {
+			if (_rcBuild >= 0)
+				return SKIP_BODY;
 
-      // Release state and return
-      release();
-      return EVAL_PAGE;
-   }
+			try {
+				Field f = VersionInfo.class.getField("RELEASE_CANDIDATE");
+				_rcBuild = f.getInt(null);
+			} catch (Exception e) {
+				_rcBuild = 0;
+			}
+		}
 
-   /**
-    * Release's the tag's state variables.
-    */
-   public void release() {
-      super.release();
-      _visible = true;
-   }
+		return SKIP_BODY;
+	}
+
+	/**
+	 * Renders the copyright tag to the JSP output stream.
+	 * @return TagSupport#EVAL_PAGE always
+	 * @throws JspException if an error occurs
+	 */
+	public int doEndTag() throws JspException {
+		try {
+			if (_visible)
+				displayCopyright();
+			else
+				displayCopyrightComment();
+		} catch (Exception e) {
+			throw new JspException("Error writing " + getClass().getName(), e);
+		} finally {
+			release();
+		}
+
+		return EVAL_PAGE;
+	}
+
+	/**
+	 * Releases the tag's state variables.
+	 */
+	public void release() {
+		super.release();
+		_visible = true;
+	}
 }
