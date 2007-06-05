bc36cbea1b69079886b6bf630992841f6e5366a5
Luke Kolin <luke@deltava.org>
2007-06-05 03:28:23 +0000

    Build 148
    git-svn-id: file:///home/svn/DVA2006@4273 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -13,7 +13,7 @@ public interface VersionInfo {
     public static final int MAJOR = 1;
     public static final int MINOR = 0;
     
-    public static final int BUILD = 147;
+    public static final int BUILD = 148;
     public static final int RELEASE_CANDIDATE = 12;
     public static final boolean FINAL = false;
     
