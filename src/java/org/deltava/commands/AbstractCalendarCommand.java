49c7214c1ebde5a4a6a87ed580b2e76681cdc36b
Luke Kolin <luke@deltava.org>
2007-06-17 13:58:19 +0000

    Fix calendars for people east of GMT
    git-svn-id: file:///home/svn/DVA2006@4305 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/AbstractCalendarCommand.java
+++ src/java/org/deltava/commands/AbstractCalendarCommand.java
@@ -1,8 +1,7 @@
-// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2006, 2007 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands;
 
 import java.util.*;
-import java.text.*;
 
 import org.deltava.beans.*;
 
@@ -18,7 +17,6 @@ import org.deltava.util.system.SystemData;
 
 public abstract class AbstractCalendarCommand extends AbstractCommand {
 
-	private static final DateFormat _df = new SimpleDateFormat("MM/dd/yyyy");
 	private static final List<ComboAlias> TYPE_OPTIONS = ComboUtils.fromArray(new String[] {"Month", "Week"}, 
 			new String[] { "31", "7"});
 	
@@ -72,11 +70,10 @@ public abstract class AbstractCalendarCommand extends AbstractCommand {
 	protected CalendarContext initCalendar(CommandContext ctx) {
 		
 		// Get the number of days and start date
-		int days = 31;
+		int days = StringUtils.parse((String) ctx.getCmdParameter(OPERATION, null), 31);
 		Date startDate = null;
 		try {
-			startDate = _df.parse(ctx.getParameter("startDate"));
-			days = Integer.parseInt((String) ctx.getCmdParameter(OPERATION, "31"));
+			startDate = StringUtils.parseDate(ctx.getParameter("startDate"), "MM/dd/yyyy");
 		} catch (Exception e) {
 			startDate = new Date();
 		}
@@ -84,13 +81,15 @@ public abstract class AbstractCalendarCommand extends AbstractCommand {
 		// Calculate the proper start date
 		startDate = (days == 7) ? getStartOfWeek(startDate) : getStartOfMonth(startDate);
 		
-		// Create the current date in the user's local time and determine what the local equivalent is
+		// Create the current date in the user's local time and determine what the local equivalent is - this will
+		// be used to load in calendar entries, but the startDate object in the request should be the user's local
+		// time and not translated to server time
 		TZInfo tz = ctx.isAuthenticated() ? ctx.getUser().getTZ() : TZInfo.get(SystemData.get("time.timezone"));
 		DateTime ldt = new DateTime(startDate, tz);
 		ldt.convertTo(TZInfo.local());
 
 		// Save the calendar options in the request
-		ctx.setAttribute("startDate", ldt.getDate(), REQUEST);
+		ctx.setAttribute("startDate", startDate, REQUEST);
 		ctx.setAttribute("typeOptions", TYPE_OPTIONS, REQUEST);
 		
 		// Build and return the Calendar context
