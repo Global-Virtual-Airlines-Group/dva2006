34e9b4c0f0219915d9c0920fa791c0636340221a
Luke Kolin <luke@deltava.org>
2007-10-14 00:07:39 +0000

    Make type lowercase
    git-svn-id: file:///home/svn/DVA2006@4586 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/html/ButtonTag.java
+++ src/java/org/deltava/taglib/html/ButtonTag.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2005 Global Virtual Airline Group. All Rights Reserved.
+// Copyright 2005, 2007 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.html;
 
 import javax.servlet.jsp.JspException;
@@ -12,69 +12,67 @@ import javax.servlet.jsp.JspException;
 
 public class ButtonTag extends ElementTag {
 
-    /**
-     * Create a new Button element tag.
-     */
-    public ButtonTag() {
-        super("input");
-        _data.setAttribute("type", "button");
-    }
+	/**
+	 * Create a new Button element tag.
+	 */
+	public ButtonTag() {
+		super("input");
+		_data.setAttribute("type", "button");
+	}
 
-    /**
-     * Generates the XHTML for this button.
-     * @throws JspException if an I/O error occurs
-     */
-    public int doEndTag() throws JspException {
-        
-        // Do a proper XHTML closing tag        
-        try {
-            _out.print(_data.open(true, true));
-        } catch (Exception e) {
-            throw new JspException(e);
-        }
-        
-        // Release state and return
-        release();
-        return EVAL_PAGE;
-    }
-    
-    /**
-     * Releases state and readies the tag for another invocation.
-     */
-    public void release() {
-        super.release();
-        _data.setAttribute("type", "button");
-    }
-    
-    /**
-     * Sets the label for this button.
-     * @param label the button label
-     */
-    public void setLabel(String label) {
-    	_data.setAttribute("value", label);
-    }
-    
-    /**
-     * Sets the button type. This is either SUBMIT or RESET.
-     * @param type the button type
-     */
-    public void setType(String type) {
-    	_data.setAttribute("type", type);
-    }
-    
-    /**
-     * Sets the JavaScript code to execute when the button is clicked.
-     * @param js the JavaScript code
-     */
-    public void setOnClick(String js) {
-    	_data.setAttribute("onclick", js);
-    }
-    
-    /**
-     * Sets the keyboard shortcut for this button.
-     * @param accessKey the Unicode value for this key combination
-     */
-    public void setKey(String accessKey) {
-    	_data.setAttribute("accesskey", accessKey);
-    }
+	/**
+	 * Generates the XHTML for this button.
+	 * @throws JspException if an I/O error occurs
+	 */
+	public int doEndTag() throws JspException {
+		try {
+			_out.print(_data.open(true, true));
+		} catch (Exception e) {
+			throw new JspException(e);
+		}
+
+		// Release state and return
+		release();
+		return EVAL_PAGE;
+	}
+
+	/**
+	 * Releases state and readies the tag for another invocation.
+	 */
+	public void release() {
+		super.release();
+		_data.setAttribute("type", "button");
+	}
+
+	/**
+	 * Sets the label for this button.
+	 * @param label the button label
+	 */
+	public void setLabel(String label) {
+		_data.setAttribute("value", label);
+	}
+
+	/**
+	 * Sets the button type. This is either SUBMIT or RESET.
+	 * @param type the button type
+	 */
+	public void setType(String type) {
+		_data.setAttribute("type", type.toLowerCase());
+	}
+
+	/**
+	 * Sets the JavaScript code to execute when the button is clicked.
+	 * @param js the JavaScript code
+	 */
+	public void setOnClick(String js) {
+		_data.setAttribute("onclick", js);
+	}
+
+	/**
+	 * Sets the keyboard shortcut for this button.
+	 * @param accessKey the Unicode value for this key combination
+	 */
+	public void setKey(String accessKey) {
+		_data.setAttribute("accesskey", accessKey);
+	}
 }
