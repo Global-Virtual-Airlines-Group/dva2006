90a4da133691f8c6f2c20efeed9282633d9ea9a7
Luke Kolin <luke@deltava.org>
2007-11-20 15:34:26 +0000

    Allow GZIP content
    git-svn-id: file:///home/svn/DVA2006@4739 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/FileUpload.java
+++ src/java/org/deltava/beans/FileUpload.java
@@ -1,12 +1,15 @@
-// Copyright 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans;
 
 import java.io.*;
+import java.util.zip.*;
 
 /**
- * A bean to store File Upload data in an HTTP servlet request.
+ * A bean to store File Upload data in an HTTP servlet request. This bean can automatically
+ * detect GZipped content and creates the appropriate decompresser output stream. This
+ * allows reduced memory usage when uploading large files.
  * @author Luke
- * @version 1.0
+ * @version 2.0
  * @since 1.0
  */
 
@@ -19,10 +22,11 @@ public class FileUpload {
      * Creates a new bean with a given name.
      * @param name the name of the file
      * @see FileUpload#getName()
+     * @throws NullPointerException if name is null
      */
     public FileUpload(String name) {
         super();
-        _name = name;
+        _name = name.toLowerCase();
     }
 
     /**
@@ -51,12 +55,23 @@ public class FileUpload {
     }
     
     /**
-     * Returns an input stream backed by the buffer.
+     * Returns an input stream backed by the buffer. This will automatically generate a
+     * {@link GZIPInputStream} if the data is already in GZIP format. If the raw data needs to be
+     * obtained, wrapping a {@link ByteArrayInputStream} around the buffer should be used.
      * @return an input stream
      * @see FileUpload#getBuffer()
      */
     public InputStream getInputStream() {
-    	return new ByteArrayInputStream(_buffer);
+    	InputStream is = new ByteArrayInputStream(_buffer);
+    	if (_name.endsWith(".gz")) {
+    		try {
+    			return new GZIPInputStream(is);
+    		} catch (IOException ie) {
+    			return is;
+    		}
+    	}
+    	
+    	return is;
     }
     
     /**
@@ -66,8 +81,8 @@ public class FileUpload {
      */
     public void load(InputStream is) throws IOException {
         
-        ByteArrayOutputStream outStream = new ByteArrayOutputStream(16384);
-        byte[] buffer = new byte[16384];
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream(32768);
+        byte[] buffer = new byte[32768];
         int bytesRead = is.read(buffer);
         while (bytesRead != -1) {
             outStream.write(buffer, 0, bytesRead);
