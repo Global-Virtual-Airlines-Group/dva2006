2212a337e6fa16200477e5f6402bdef8516926c7
Luke Kolin <luke@deltava.org>
2007-12-05 05:30:18 +0000

    Display with IE7
    git-svn-id: file:///home/svn/DVA2006@4772 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/googlemap/IEVMLStyleTag.java
+++ src/java/org/deltava/taglib/googlemap/IEVMLStyleTag.java
@@ -1,13 +1,15 @@
-// Copyright 2005 Luke J. Kolin. All Rights Reserved.
+// Copyright 2005, 2007 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.googlemap;
 
 import javax.servlet.jsp.*;
 import javax.servlet.jsp.tagext.TagSupport;
 
+import org.deltava.taglib.ContentHelper;
+
 /**
  * A JSP tag to render the Internet Explorer VML behavior style for Google Maps.
  * @author Luke
- * @version 1.0
+ * @version 2.0
  * @since 1.0
  */
 
@@ -16,7 +18,7 @@ public class IEVMLStyleTag extends TagSupport {
    public int doEndTag() throws JspException {
       
       // Do nothing if not IE
-      if (pageContext.getRequest().getAttribute("browser$ie") == null)
+      if (!ContentHelper.isIE6(pageContext) && !ContentHelper.isIE7(pageContext))
          return EVAL_PAGE;
       
       JspWriter out = pageContext.getOut();
