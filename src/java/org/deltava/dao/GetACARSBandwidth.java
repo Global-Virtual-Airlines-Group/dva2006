71e224e075bebc7a37ceb210de33b2f4eadff06c
Luke Kolin <luke@deltava.org>
2008-01-15 22:43:03 +0000

    Initial bandwidth tracking support
    git-svn-id: file:///home/svn/DVA2006@4908 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/dao/GetACARSBandwidth.java
@@ -0,0 +1,114 @@
+// Copyright 2008 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.dao;
+
+import java.sql.*;
+import java.util.*;
+
+import org.deltava.beans.acars.Bandwidth;
+
+/**
+ * A Data Access Object to load ACARS bandwidth statistics. 
+ * @author Luke
+ * @version 2.1
+ * @since 2.1
+ */
+
+public class GetACARSBandwidth extends DAO {
+	
+	/**
+	 * Initializes the Data Access Object.
+	 * @param c the JDBC connection to use
+	 */
+	public GetACARSBandwidth(Connection c) {
+		super(c);
+	}
+
+	/**
+	 * Returns the latest bandwidth data. 
+	 * @return a Bandwidth bean, or null if no data
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public Bandwidth getLatest() throws DAOException {
+		try {
+			prepareStatementWithoutLimits("SELECT * FROM BANDWIDTH WHERE (DURATION=?) ORDER BY "
+					+ "PERIOD DESC LIMIT 1");
+			_ps.setInt(1, 1);
+			List<Bandwidth> results = execute();
+			return results.isEmpty() ? null : results.get(0);
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+	
+	/**
+	 * Retrieves bandwidth stats that have not yet been aggregated.
+	 * @return a Collection of Bandwidth beans
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public Collection<Bandwidth> getRaw() throws DAOException {
+		try {
+			prepareStatement("SELECT * FROM acars.BANDWIDTH WHERE (DURATION=?) ORDER BY "
+					+ "PERIOD DESC");
+			_ps.setInt(1, 1);
+			return execute();
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+	
+	/**
+	 * Retrieves hourly bandwidth stats.
+	 * @return a Collection of Bandwidth beans
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public Collection<Bandwidth> getHourly() throws DAOException {
+		try {
+			prepareStatement("SELECT * FROM acars.BANDWIDTH WHERE (DURATION=?) ORDER BY "
+					+ "PERIOD DESC");
+			_ps.setInt(1, 60);
+			return execute();
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+	
+	/**
+	 * Retrieves daily bandwidth stats.
+	 * @return a Collection of Bandwidth beans
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public Collection<Bandwidth> getDaily() throws DAOException {
+		try {
+			prepareStatement("SELECT DATE(PERIOD) AS DT, 1440, AVG(CONS), SUM(BYTES_IN), "
+					+ "SUM(BYTES_OUT), SUM(MSGS_IN), SUM(MSGS_OUT), MAX(PEAK_CONS), MAX(PEAK_BYTES), "
+					+ "MAX(PEAK_MSGS) FROM acars.BANDWIDTH GROUP BY DT DESC");
+			return execute();
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+	
+	/**
+	 * Helper method to parse result sets.
+	 */
+	private List<Bandwidth> execute() throws SQLException {
+		List<Bandwidth> results = new ArrayList<Bandwidth>();
+		ResultSet rs = _ps.executeQuery();
+		while (rs.next()) {
+			Bandwidth bw = new Bandwidth(rs.getTimestamp(1));
+			bw.setInterval(rs.getInt(2));
+			bw.setConnections(rs.getInt(3));
+			bw.setBytes(rs.getLong(4), rs.getLong(5));
+			bw.setMessages(rs.getInt(6), rs.getInt(7));
+			bw.setMaxConnections(rs.getInt(8));
+			bw.setMaxBytes(rs.getLong(9));
+			bw.setMaxMsgs(rs.getInt(10));
+			results.add(bw);
+		}
+		
+		// Clean up and return
+		rs.close();
+		_ps.close();
+		return results;
+	}
+}
