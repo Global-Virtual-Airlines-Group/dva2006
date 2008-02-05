d6e0e42ae912c9851b5d87ee3ccd741ac2621f57
Luke Kolin <luke@deltava.org>
2008-02-05 20:43:37 +0000

    Add service
    git-svn-id: file:///home/svn/DVA2006@4967 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/service/navdata/AirportTerminalRouteService.java
@@ -0,0 +1,89 @@
+// Copyright 2008 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.service.navdata;
+
+import java.text.*;
+import java.util.*;
+import java.io.IOException;
+
+import static javax.servlet.http.HttpServletResponse.*;
+
+import org.jdom.*;
+
+import org.deltava.beans.navdata.TerminalRoute;
+import org.deltava.beans.schedule.Airport;
+
+import org.deltava.dao.*;
+import org.deltava.service.*;
+
+import org.deltava.util.XMLUtils;
+import org.deltava.util.system.SystemData;
+
+/**
+ * A Web Service to display all Terminal Routes for an Airport. 
+ * @author Luke
+ * @version 2.1
+ * @since 2.1
+ */
+
+public class AirportTerminalRouteService extends WebService {
+
+	/**
+	 * Executes the Web Service.
+	 * @param ctx the Web Service context
+	 * @return the HTTP status code
+	 * @throws ServiceException if an error occurs
+	 */
+	public int execute(ServiceContext ctx) throws ServiceException {
+		
+		// Get the airport
+		Airport a = SystemData.getAirport(ctx.getParameter("airport"));
+		if (a == null)
+			throw error(SC_NOT_FOUND, "Unknown Airport - " + ctx.getParameter("airport"));
+
+		Collection<TerminalRoute> routes = new ArrayList<TerminalRoute>();
+		try {
+			GetNavRoute dao = new GetNavRoute(ctx.getConnection());
+			routes.addAll(dao.getRoutes(a.getICAO(), TerminalRoute.SID));
+			routes.addAll(dao.getRoutes(a.getICAO(), TerminalRoute.STAR));
+		} catch (DAOException de) {
+			throw error(SC_INTERNAL_SERVER_ERROR, de.getMessage());
+		} finally {
+			ctx.release();
+		}
+		
+		// Generate the XML document
+		Document doc = new Document();
+		Element re = new Element("routes");
+		re.setAttribute("icao", a.getICAO());
+		re.setAttribute("iata", a.getIATA());
+		doc.setRootElement(re);
+		
+		// Format routes
+		final NumberFormat df = new DecimalFormat("#0.000000");
+		for (Iterator<TerminalRoute> i = routes.iterator(); i.hasNext(); ) {
+			TerminalRoute tr = i.next();
+			Element te = new Element("route");
+			
+		}
+		
+		// Dump the XML to the output stream
+		try {
+			ctx.getResponse().setContentType("text/xml");
+			ctx.println(XMLUtils.format(doc, "ISO-8859-1"));
+			ctx.commit();
+		} catch (IOException ie) {
+			throw error(SC_CONFLICT, "I/O Error");
+		}
+		
+		// Return success code
+		return SC_OK;
+	}
+
+	/**
+	 * Returns wether this web service requires authentication.
+	 * @return TRUE always
+	 */
+	public boolean isSecure() {
+		return true;
+	}
+}
