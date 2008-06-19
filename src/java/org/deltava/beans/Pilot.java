832eada38dbb86d24af89795bf5fe11c55d9201e
Luke Kolin <luke@deltava.org>
2008-06-19 20:08:28 +0000

    Remove Google Earth default support
    git-svn-id: file:///home/svn/DVA2006@5204 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/Pilot.java
+++ src/java/org/deltava/beans/Pilot.java
@@ -49,11 +49,10 @@ public class Pilot extends Person implements Cacheable, ComboAlias {
 	/**
 	 * Valid route mapping types.
 	 */
-	public static final String[] MAP_TYPES = {"Google Maps", "Falling Rain", "Google Earth"};
+	public static final String[] MAP_TYPES = {"Google Maps", "Falling Rain"};
 
 	public static final int MAP_GOOGLE = 0;
 	public static final int MAP_FALLINGRAIN = 1;
-	public static final int MAP_GEARTH = 2;
 
 	private static final String[] ROW_CLASSES = { null, "opt2", "opt3", "opt1", "err", "warn" };
 
