5beeda882354032ac9b7364da0a44019201bf169
Luke Kolin <luke@deltava.org>
2008-07-03 15:41:26 +0000

    Remove encoding from SecretKeyEncryptor
    git-svn-id: file:///home/svn/DVA2006@5255 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/crypt/CryptoException.java
+++ src/java/org/deltava/crypt/CryptoException.java
@@ -1,23 +1,17 @@
-// Copyright 2004 Global Virtual Airlines Grouup. All Rights Reserved.
+// Copyright 2004, 2008 Global Virtual Airlines Grouup. All Rights Reserved.
 package org.deltava.crypt;
 
 /**
  * An internal Cryptographic exception.
  * @author Luke
- * @version 1.0
+ * @version 2.2
  * @since 1.0
  */
 
 public class CryptoException extends RuntimeException {
+	
+	private Object _payload;
 
-    /**
-     * Creates a new CryptoException.
-     * @param msg
-     */
-    CryptoException(String msg) {
-        super(msg);
-    }
-    
     /**
      * Creates a new CryptoException with a nested exception.
      * @param msg the exception message
@@ -27,5 +21,25 @@ public class CryptoException extends RuntimeException {
     CryptoException(String msg, Throwable t) {
         super(msg + " - " + t.getClass().getName(), t);
         setStackTrace(t.getStackTrace());
+    }
+    
+    /**
+     * Creates a new CryptoException with a nested exception and payload.
+     * @param msg the exception message
+     * @param t the root cause of the message
+     * @param payload the payload
+     * @see Throwable#getCause()
+     */
+    CryptoException(String msg, Throwable t, Object payload) {
+        this(msg + " - " + t.getClass().getName(), t);
+        _payload = payload;
+    }
+    
+    /**
+     * Returns the invalid cryptogrphic payload.
+     * @return the payload
+     */
+    public Object getPayload() {
+    	return _payload;
     }
 }
