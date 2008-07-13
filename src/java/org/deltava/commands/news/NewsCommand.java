a6e0d9bd3e7216bd8ef4b50d38ead91d8d0bbbc8
Luke Kolin <luke@deltava.org>
2008-07-13 18:06:10 +0000

    Don't show edit link if not authorized
    git-svn-id: file:///home/svn/DVA2006@5279 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/news/NewsCommand.java
+++ src/java/org/deltava/commands/news/NewsCommand.java
@@ -1,18 +1,20 @@
+// Copyright 2005, 2008 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.news;
 
+import java.util.*;
 import java.sql.Connection;
 
-import org.deltava.commands.*;
+import org.deltava.beans.News;
 
-import org.deltava.dao.GetNews;
-import org.deltava.dao.DAOException;
+import org.deltava.commands.*;
+import org.deltava.dao.*;
 
 import org.deltava.security.command.NewsAccessControl;
 
 /**
  * A Web Site Command to display the System News.
  * @author Luke
- * @version 1.0
+ * @version 2.2
  * @since 1.0
  */
 
@@ -44,10 +46,20 @@ public class NewsCommand extends AbstractViewCommand {
             ctx.release();
         }
         
+        // Calculate access rights
+        Map<Integer, NewsAccessControl> accessMap = new HashMap<Integer, NewsAccessControl>();
+        for (Iterator i = vc.getResults().iterator(); i.hasNext(); ) {
+        	News n = (News) i.next();
+        	NewsAccessControl access = new NewsAccessControl(ctx, n);
+        	access.validate();
+        	accessMap.put(new Integer(n.getID()), access);
+        }
+        
         // Save our access
         NewsAccessControl access = new NewsAccessControl(ctx, null);
         access.validate();
         ctx.setAttribute("access", access, REQUEST);
+        ctx.setAttribute("accessMap", accessMap, REQUEST);
         
         // Set the result page and return
         CommandResult result = ctx.getResult();
