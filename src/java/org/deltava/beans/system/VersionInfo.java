d01a2a1b156ee91dd5686b05cf6637f04432c79a
Luke Kolin <luke@deltava.org>
2008-07-18 04:23:13 +0000

    Build 194
    git-svn-id: file:///home/svn/DVA2006@5306 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -13,7 +13,7 @@ public interface VersionInfo {
     public static final int MAJOR = 2;
     public static final int MINOR = 2;
     
-    public static final int BUILD = 193;
+    public static final int BUILD = 194;
     // public static final int RELEASE_CANDIDATE = 1;
     public static final boolean FINAL = false;
     
