5925a690964f7ab59774821f398b06d19023565d
Luke Kolin <luke@deltava.org>
2008-07-24 20:30:32 +0000

    Set ACARS flag on JSP
    git-svn-id: file:///home/svn/DVA2006@5318 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/pirep/ExternalPIREPCommand.java
+++ src/java/org/deltava/commands/pirep/ExternalPIREPCommand.java
@@ -100,6 +100,7 @@ public class ExternalPIREPCommand extends AbstractCommand {
 				ctx.setAttribute("filedRoute", navdao.getRouteWaypoints(StringUtils.listConcat(wps, " "), info.getAirportD()), REQUEST);
 				ctx.setAttribute("flightInfo", info, REQUEST);
 				ctx.setAttribute("conInfo", ardao.getConnection(info.getConnectionID()), REQUEST);
+				ctx.setAttribute("isACARS", Boolean.TRUE, REQUEST);
 			}
 			
 			// Save the route and map center for the Google Map
