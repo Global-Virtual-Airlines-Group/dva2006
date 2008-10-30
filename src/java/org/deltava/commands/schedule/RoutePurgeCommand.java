ff8dfd38b6d3b82b413ed4aa088afc465e6e8830
Luke Kolin <luke@deltava.org>
2008-10-30 19:58:17 +0000

    Remove preferred routes
    git-svn-id: file:///home/svn/DVA2006@5521 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/schedule/RoutePurgeCommand.java
+++ src/java/org/deltava/commands/schedule/RoutePurgeCommand.java
@@ -1,28 +1,23 @@
-// Copyright 2005, 2006 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2008 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.schedule;
 
 import java.util.Date;
-import java.sql.Connection;
-
-import java.text.*;
 
 import org.deltava.commands.*;
-
-import org.deltava.dao.SetRoute;
-import org.deltava.dao.DAOException;
+import org.deltava.dao.*;
 
 import org.deltava.security.command.ScheduleAccessControl;
+
+import org.deltava.util.StringUtils;
 
 /**
  * A Web Site Command to purge Route data.
  * @author Luke
- * @version 1.0
+ * @version 2.2
  * @since 1.0
  */
 
 public class RoutePurgeCommand extends AbstractCommand {
-
-	private static final DateFormat _df = new SimpleDateFormat("MM/dd/yyyy");
 
 	/**
 	 * Executes the command.
@@ -31,11 +26,6 @@ public class RoutePurgeCommand extends AbstractCommand {
 	 */
 	public void execute(CommandContext ctx) throws CommandException {
 
-		// Check if we are purging domestic routes
-		boolean isDomestic = "domestic".equals(ctx.getCmdParameter(Command.OPERATION, null));
-		ctx.setAttribute("purgeOceanic", Boolean.valueOf(!isDomestic), REQUEST);
-		ctx.setAttribute("purgeDomestic", Boolean.valueOf(isDomestic), REQUEST);
-
 		// Check our access level
 		ScheduleAccessControl access = new ScheduleAccessControl(ctx);
 		access.validate();
@@ -43,29 +33,21 @@ public class RoutePurgeCommand extends AbstractCommand {
 			throw securityException("Cannot purge Routes");
 
 		// If we're purging oceanic routes, figure out a date to purge before
-		Date purgeDate = null;
-		if (!isDomestic) {
-			try {
-				purgeDate = _df.parse(ctx.getParameter("purgeDate"));
-			} catch (ParseException pe) {
-				CommandException ce = new CommandException("Invalid Purge Date - " + ctx.getParameter("purgeDate"));
-				ce.setLogStackDump(false);
-				throw ce;
-			}
-		}
+		Date purgeDate = StringUtils.parseDate(ctx.getParameter("purgeDate"), "MM/dd/yyyy");
 
+		// Get the DAO and purge the routes
 		try {
-			Connection con = ctx.getConnection();
-
-			// Get the DAO and purge the routes
-			SetRoute dao = new SetRoute(con);
-			int rowsDeleted = (isDomestic) ? dao.purgeDomestic() : dao.purgeOceanic(purgeDate);
+			SetRoute dao = new SetRoute(ctx.getConnection());
+			int rowsDeleted = dao.purgeOceanic(purgeDate);
 			ctx.setAttribute("rowsDeleted", new Integer(rowsDeleted), REQUEST);
 		} catch (DAOException de) {
 			throw new CommandException(de);
 		} finally {
 			ctx.release();
 		}
+
+		// Set status attribute
+		ctx.setAttribute("purgeOceanic", Boolean.TRUE, REQUEST);
 
 		// Forward to the JSP
 		CommandResult result = ctx.getResult();
