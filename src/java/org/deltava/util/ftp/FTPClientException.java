6d01d09d6d1c74f96015a3f33985b558eca275f6
Luke Kolin <luke@deltava.org>
2008-11-03 18:00:18 +0000

    Add reply code
    git-svn-id: file:///home/svn/DVA2006@5527 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/util/ftp/FTPClientException.java
+++ src/java/org/deltava/util/ftp/FTPClientException.java
@@ -1,16 +1,20 @@
-// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2006, 2008 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.util.ftp;
+
+import com.enterprisedt.net.ftp.FTPException;
 
 import org.deltava.dao.DAOException;
 
 /**
- * A class to treat FTP exception as a DAO exception.
+ * A class to treat FTP exceptions as a DAO exception.
  * @author Luke
- * @version 1.0
+ * @version 2.3
  * @since 1.0
  */
 
 public class FTPClientException extends DAOException {
+	
+	private int _resultCode;
 
 	/**
 	 * Creates a new FTP exception from another exception.
@@ -18,6 +22,8 @@ public class FTPClientException extends DAOException {
 	 */
 	FTPClientException(Throwable t) {
 		super(t);
+		if (t instanceof FTPException)
+			_resultCode = ((FTPException) t).getReplyCode();
 	}
 	
 	/**
@@ -26,5 +32,21 @@ public class FTPClientException extends DAOException {
 	 */
 	FTPClientException(String msg) {
 		super(msg);
+	}
+	
+	/**
+	 * Returns the FTP error code.
+	 * @return the error code
+	 */
+	public int getResultCode() {
+		return _resultCode;
+	}
+	
+	/**
+	 * Sets the FTP error code.
+	 * @param code the error code
+	 */
+	public void setResultCode(int code) {
+		_resultCode = code;
 	}
 }
