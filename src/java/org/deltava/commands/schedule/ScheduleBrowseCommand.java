b9a4d990a35c68f9b282a21f07023c828791f4a7
Luke Kolin <luke@deltava.org>
2009-01-31 20:56:49 +0000

    Allow IATA and ICAO codes
    git-svn-id: file:///home/svn/DVA2006@5790 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/schedule/ScheduleBrowseCommand.java
+++ src/java/org/deltava/commands/schedule/ScheduleBrowseCommand.java
@@ -1,7 +1,7 @@
-// Copyright 2005, 2006, 2007 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007, 2009 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.schedule;
 
-import java.sql.Connection;
+import java.util.Collections;
 
 import org.deltava.beans.schedule.*;
 
@@ -13,7 +13,7 @@ import org.deltava.util.system.SystemData;
 /**
  * A Web Site Command to browse the Flight Schedule.
  * @author Luke
- * @version 1.0
+ * @version 2.4
  * @since 1.0
  */
 
@@ -45,17 +45,12 @@ public class ScheduleBrowseCommand extends AbstractViewCommand {
       // Save the search criteria
       ctx.setAttribute("airportD", aD, REQUEST);
       ctx.setAttribute("airportA", criteria.getAirportA(), REQUEST);
+      if (ctx.isAuthenticated())
+    	  ctx.setAttribute("useICAO", Boolean.valueOf(ctx.getUser().getAirportCodeType() == Airport.ICAO), REQUEST);
       
+      // Do the search
       try {
-         Connection con = ctx.getConnection();
-         
-         // Get the DAO and source/destination airports
-         GetScheduleAirport dao = new GetScheduleAirport(con);
-         ctx.setAttribute("airports", dao.getOriginAirports(null), REQUEST);
-         ctx.setAttribute("dstAP", dao.getConnectingAirports(criteria.getAirportD(), true, null), REQUEST);
-         
-         // Do the search
-         GetSchedule sdao = new GetSchedule(con);
+         GetSchedule sdao = new GetSchedule(ctx.getConnection());
          sdao.setQueryStart(vc.getStart());
          sdao.setQueryMax(vc.getCount());
          vc.setResults(sdao.search(criteria, "AIRPORT_D, AIRPORT_A"));
@@ -64,6 +59,9 @@ public class ScheduleBrowseCommand extends AbstractViewCommand {
       } finally {
          ctx.release();
       }
+      
+      // Save empty list
+      ctx.setAttribute("emptyList", Collections.emptyList(), REQUEST);
 
       // Forward to the JSP
       CommandResult result = ctx.getResult();
