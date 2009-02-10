db7667278b3d5af2eb2f08f8f30df76bf6b4ffff
Luke Kolin <luke@deltava.org>
2009-02-10 20:11:56 +0000

    Only update blank locations
    git-svn-id: file:///home/svn/DVA2006@5804 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/SetLocations.java
+++ src/test/org/deltava/SetLocations.java
@@ -1,4 +1,4 @@
-// Copyright 2008 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2008, 2009 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava;
 
 import java.sql.*;
@@ -8,32 +8,38 @@ import org.apache.log4j.*;
 
 import junit.framework.TestCase;
 
-import org.deltava.beans.GeoLocation;
+import org.deltava.beans.*;
 import org.deltava.beans.stats.GeocodeResult;
 
 import org.deltava.dao.*;
 import org.deltava.dao.http.GetGoogleGeocode;
 
+import org.deltava.util.StringUtils;
+import org.deltava.util.system.SystemData;
+
 public class SetLocations extends TestCase {
-	
+
 	private static Logger log;
-	
-	private static final String JDBC_URL ="jdbc:mysql://pollux.gvagroup.org/dva";
-	
+
+	private static final String JDBC_URL = "jdbc:mysql://pollux.gvagroup.org/dva";
+
 	private Connection _c;
 
 	protected void setUp() throws Exception {
 		super.setUp();
-		
+
 		// Init Log4j
 		PropertyConfigurator.configure("etc/log4j.properties");
 		log = Logger.getLogger(SetLocations.class);
+
+		SystemData.init();
 
 		// Connect to the database
 		Class.forName("com.mysql.jdbc.Driver");
 		_c = DriverManager.getConnection(JDBC_URL, "import", "import");
 		assertNotNull(_c);
 		_c.setAutoCommit(false);
+		assertFalse(_c.getAutoCommit());
 	}
 
 	protected void tearDown() throws Exception {
@@ -43,34 +49,37 @@ public class SetLocations extends TestCase {
 	}
 
 	public void testLoadLocations() throws Exception {
-		
+
 		// Get locations and Pilots
 		GetPilot pdao = new GetPilot(_c);
 		SetPilot pwdao = new SetPilot(_c);
 		Map<Integer, GeoLocation> locs = pdao.getPilotBoard();
-		
+
 		// Loop through the locations
-		for (Iterator<Map.Entry<Integer,GeoLocation>> i = locs.entrySet().iterator(); i.hasNext(); ) {
+		for (Iterator<Map.Entry<Integer, GeoLocation>> i = locs.entrySet().iterator(); i.hasNext();) {
 			Map.Entry<Integer, GeoLocation> e = i.next();
 			GeoLocation loc = e.getValue();
-			
+			Pilot usr = pdao.get(e.getKey().intValue());
+
 			// Read via the DAO
-			try {
-				GetGoogleGeocode gcdao = new GetGoogleGeocode();
-				gcdao.setAPIKey("ABQIAAAAWFzTV_nG8JA7h9y7QsKTgRQs34dwJzNMJEKBtecbaszCuM2KJhQfgxuxzo6F3mVptlQ6PYPapCaeaA");
-				List<GeocodeResult> locations = gcdao.getGeoData(loc.getLatitude(), loc.getLongitude());
-				if (!locations.isEmpty()) {
-					GeocodeResult gr = locations.get(0);
-					if (gr.getAccuracy().intValue() > GeocodeResult.GeocodeAccuracy.COUNTRY.intValue()) {
-						log.info("Setting " + e.getKey() + " home town to " + gr.getCityState());
-						pwdao.setHomeTown(e.getKey().intValue(), gr);
+			if (StringUtils.isEmpty(usr.getLocation())) {
+				try {
+					GetGoogleGeocode gcdao = new GetGoogleGeocode();
+					gcdao.setAPIKey("ABQIAAAAWFzTV_nG8JA7h9y7QsKTgRQs34dwJzNMJEKBtecbaszCuM2KJhQfgxuxzo6F3mVptlQ6PYPapCaeaA");
+					List<GeocodeResult> locations = gcdao.getGeoData(loc.getLatitude(), loc.getLongitude());
+					if (!locations.isEmpty()) {
+						GeocodeResult gr = locations.get(0);
+						if (gr.getAccuracy().intValue() > GeocodeResult.GeocodeAccuracy.COUNTRY.intValue()) {
+							log.info("Setting " + e.getKey() + " home town to " + gr.getCityState());
+							pwdao.setHomeTown(e.getKey().intValue(), gr);
+						}
 					}
+				} catch (Exception ex) {
+					log.error("Cannot update " + e.getKey() + " - " + ex.getMessage());
 				}
-			} catch (Exception ex) {
-				log.error("Cannot update " + e.getKey() + " - " + ex.getMessage());
 			}
 		}
-		
+
 		// Commit
 		_c.commit();
 	}
