c5c6d1d28931535b8fe4d9dfeec0000f05281ec7
Luke Kolin <luke@deltava.org>
2009-02-19 15:56:05 +0000

    Abstract out route plotters
    git-svn-id: file:///home/svn/DVA2006@5821 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/service/schedule/RoutePlanService.java
+++ src/java/org/deltava/service/schedule/RoutePlanService.java
@@ -1,4 +1,4 @@
-// Copyright 2008 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2008, 2009 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.service.schedule;
 
 import java.util.*;
@@ -12,12 +12,13 @@ import org.deltava.dao.*;
 import org.deltava.service.*;
 
 import org.deltava.util.*;
+import org.deltava.util.flightplan.*;
 import org.deltava.util.system.SystemData;
 
 /**
- * A Web Service to create Microsoft Flight Simulator flight plans. 
+ * A Web Service to create flight plans. 
  * @author Luke
- * @version 2.3
+ * @version 2.4
  * @since 2.2
  */
 
@@ -67,100 +68,23 @@ public class RoutePlanService extends WebService {
 			ctx.release();
 		}
 		
-		// Write the flight plan
+		// Get the Flight Plan generator
+		FlightPlanGenerator fpgen = null;
 		boolean isFSX = "FSX".equals(ctx.getParameter("simVersion"));
-		ctx.println("[flightplan]");
-		if (!isFSX)
-			ctx.println("AppVersion=9.0.30612");
-		ctx.println("title=" + aD.getICAO() + " to " + aA.getICAO());
-		ctx.println("description=" + aD.getICAO() + ", " + aA.getICAO());
-		ctx.println("type=IFR");
-		ctx.println("routetype=0");
-		ctx.println("cruising_altitude=" + alt);
-		if (isFSX) {
-			ctx.println("departure_id=" + aD.getICAO() + ", " + GeoUtils.formatFS9(aD) + ", +" + StringUtils.format(aD.getAltitude(), "000000.00") + ",");
-			ctx.println("destination_id=" + aA.getICAO() + ", " + GeoUtils.formatFS9(aA) + ", " + StringUtils.format(aA.getAltitude(), "000000.00") + ",");
-			ctx.println("departure_name=" + aD.getName());
-			ctx.println("destination_name=" + aA.getName());
-		} else {
-			ctx.println("departure_id=" + aD.getICAO() + ", " + GeoUtils.formatFS9(aD) + ", +000000.00,");
-			ctx.println("departure_name=" + aD.getName());
-			ctx.println("departure_position=GATE ?");
-			ctx.println("destination_id=" + aA.getICAO() + ", " + GeoUtils.formatFS9(aA) + ", +000000.00,");
-			ctx.println("destination_name=" + aA.getName());
-		}
-
-		// Write the route entries
-		int waypointIdx = 0;
-		for (Iterator<NavigationDataBean> i = routePoints.iterator(); i.hasNext(); ) {
-			NavigationDataBean nd = i.next();
-			ctx.print("waypoint." + String.valueOf(waypointIdx) + "=");
-			if (isFSX) {
-				ctx.print(nd.getCode());
-				switch (nd.getType()) {
-					case NavigationDataBean.AIRPORT:
-						ctx.print(", A, ");
-						break;
-					
-					case NavigationDataBean.NDB:
-					ctx.print(", N, ");
-						break;
-					
-					case NavigationDataBean.VOR:
-					ctx.print(", V, ");
-						break;
-			
-					default:
-						ctx.print(", I, ");
-				}
-				
-				ctx.print(GeoUtils.formatFS9(nd));
-				ctx.print(", +000000.00,");
-				if (nd.isInTerminalRoute()) {
-					String aw = nd.getAirway();
-					ctx.print(" ");
-					ctx.println(aw.substring(0, aw.indexOf('.')));
-				} else if (nd.getAirway() != null) {
-					ctx.print(" ");
-					ctx.println(nd.getAirway());
-				} else
-					ctx.println("");
-			} else {
-				if (nd.getRegion() != null)
-					ctx.print(nd.getRegion());
-				ctx.print(", ");
-				ctx.print(nd.getCode());
-				ctx.print(", ,");
-				ctx.print(nd.getCode());
-				switch (nd.getType()) {
-					case NavigationDataBean.AIRPORT:
-						ctx.print(", A, ");
-						break;
-						
-					case NavigationDataBean.NDB:
-						ctx.print(", N, ");
-						break;
-						
-					case NavigationDataBean.VOR:
-						ctx.print(", V, ");
-						break;
-				
-					default:
-						ctx.print(", I, ");
-				}
-				
-				ctx.print(GeoUtils.formatFS9(nd));
-				ctx.println(", +000000.00,");
-			}
-			
-			waypointIdx++;
-		}
+		if (isFSX)
+			fpgen = new FSXGenerator();
+		else
+			fpgen = new FS9Generator();
+		
+		// Write the flight plan
+		fpgen.setAirports(aD, aA);
+		fpgen.setCruiseAltitude(alt);
 		
 		// Flush the output buffer
 		try {
-			ctx.getResponse().setContentType("text/plain");
-			ctx.getResponse().setHeader("Content-disposition", "attachment; filename=" + aD.getICAO() +"-" + aA.getICAO() + ".pln");
-			ctx.commit();
+			ctx.getResponse().setContentType(fpgen.getMimeType());
+			ctx.getResponse().setHeader("Content-disposition", "attachment; filename=" + aD.getICAO() +"-" + aA.getICAO() + "." + fpgen.getExtension());
+			ctx.getResponse().getOutputStream().write(fpgen.generate(routePoints));
 		} catch (Exception e) {
 			throw error(SC_CONFLICT, "I/O Error", false);
 		}
