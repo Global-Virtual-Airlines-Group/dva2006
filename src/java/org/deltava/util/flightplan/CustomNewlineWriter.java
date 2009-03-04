ec7bf63e40e593b3ca86f9f1ba11066d84145c69
Luke Kolin <luke@deltava.org>
2009-03-04 20:07:55 +0000

    Use windows line separators
    git-svn-id: file:///home/svn/DVA2006@5845 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/util/flightplan/CustomNewlineWriter.java
@@ -0,0 +1,34 @@
+// Copyright 2009 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.util.flightplan;
+
+import java.io.*;
+
+/**
+ * A PrintWriter to use custom linefeed characters. 
+ * @author Luke
+ * @version 2.4
+ * @since 2.4
+ */
+
+class CustomNewlineWriter extends PrintWriter {
+	
+	private String _newLine;
+
+	/**
+	 * Initializes the Writer.
+	 * @param out the output stream to write to
+	 * @param newLine the line break string
+	 */
+	public CustomNewlineWriter(OutputStream out, String newLine) {
+		super(out);
+		_newLine = newLine;
+	}
+
+	/**
+	 * Writes a new line using the specified line break string.
+	 */
+	@Override
+	public void println() {
+		print(_newLine);
+	}
+}
