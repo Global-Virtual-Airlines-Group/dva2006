eedd245e07ad06da762d3256ce61e0f628eca327
Luke Kolin <luke@deltava.org>
2009-03-29 22:29:49 +0000

    Add new emoticon
    git-svn-id: file:///home/svn/DVA2006@5876 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/cooler/Emoticons.java
+++ src/java/org/deltava/beans/cooler/Emoticons.java
@@ -1,10 +1,10 @@
-// Copyright 2005, 2006, 2007 Global Virtual Airline Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007, 2009 Global Virtual Airline Group. All Rights Reserved.
 package org.deltava.beans.cooler;
 
 /**
  * An interface to store Water Cooler emoticon names.
  * @author Luke
- * @version 1.0
+ * @version 2.5
  * @since 1.0
  */
 
@@ -14,11 +14,12 @@ public interface Emoticons {
     * Emoticon names.
     */
    public static final String[] ICON_NAMES = {"smile", "wink", "cool", "frown", "eek", "mad", "redface", "confused",
-         "rolleyes", "biggrin", "razz", "plotting", "judge", "slitwrist", "scared", "evilgrin", "rofl", "nuts", "blahblah", "rawk"};
+         "rolleyes", "biggrin", "razz", "plotting", "judge", "slitwrist", "scared", "evilgrin", "rofl", "nuts", "blahblah", "rawk",
+         "rimshot"};
    
    /**
     * Emoticon codes.
     */
    public static final String[] ICON_CODES = {":)", ";)", null, ":(", ":O", null, null, null, null, ":D", ":p", null, null, 
-         null, null, ">)", null, null, null, null};
+         null, null, ">)", null, null, null, null, null};
 }
