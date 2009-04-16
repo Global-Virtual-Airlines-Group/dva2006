0cd9979bcb4afa715d7b07b966b1327ebc9ea549
Luke Kolin <luke@deltava.org>
2009-04-16 12:52:45 +0000

    Add METAR library
    git-svn-id: file:///home/svn/DVA2006@5924 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/wx/METAR.java
+++ src/java/org/deltava/beans/wx/METAR.java
@@ -1,16 +1,21 @@
-// Copyright 2008 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2008, 2009 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.wx;
 
 import java.util.Date;
 
+import com.feldt.metar.Metar;
+import com.feldt.metar.exceptions.MetarParseException;
+
 /**
  * A bean to store airport METAR data.
  * @author Luke
- * @version 2.2
+ * @version 2.5
  * @since 2.2
  */
 
 public class METAR extends WeatherDataBean {
+	
+	private Metar _metar;
 	
 	/**
 	 * Sets the expiration date of the bean (15 minutes after effective date).
@@ -25,6 +30,18 @@ public class METAR extends WeatherDataBean {
 	
 	public String getType() {
 		return "METAR";
+	}
+	
+	/**
+	 * Sets and parses the METAR data.
+	 */
+	public void setData(String data) {
+		super.setData(data);
+		try {
+			_metar = Metar.parse(_pos.getCode(), data, "");
+		} catch (MetarParseException mpe) {
+			
+		}
 	}
 
 	public int hashCode() {
