f413e00fe0d14a4241ee1ee2874a6ed11b5ef7e1
Luke Kolin <luke@deltava.org>
2009-05-07 03:13:15 +0000

    Initial METAR parser revision
    git-svn-id: file:///home/svn/DVA2006@5943 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/test/org/deltava/beans/wx/TestMetarParser.java
@@ -0,0 +1,57 @@
+// Copyright 2009 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.wx;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+
+import org.apache.log4j.*;
+
+import junit.framework.TestCase;
+
+public class TestMetarParser extends TestCase {
+	
+	private Logger log;
+	
+	private final DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+	private LineNumberReader lr;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		PropertyConfigurator.configure("data/log4j.test.properties");
+		log = Logger.getLogger(TestMetarParser.class);
+		
+		File f = new File("data/metar-20z.txt");
+		assertTrue(f.exists());
+		
+		lr = new LineNumberReader(new FileReader(f));
+		assertNotNull(lr);
+	}
+
+	protected void tearDown() throws Exception {
+		log.info("Processed to Line #" + lr.getLineNumber());
+		lr.close();
+		LogManager.shutdown();
+		super.tearDown();
+	}
+
+	public void testMETARs() throws Exception {
+		Date dt = null;
+		
+		while (lr.ready()) {
+			String data = lr.readLine();
+			if (data.length() < 3)
+				continue;
+			else if (data.startsWith("200")) {
+				dt = df.parse(data);
+				continue;
+			}
+			
+			// Parse the METAR
+			METAR m = MetarParser.parse(data);
+			assertNotNull(m);
+			//assertEquals(dt, m.getDate());
+			log.info("Processed " + m.getCode());
+		}
+	}
+}
