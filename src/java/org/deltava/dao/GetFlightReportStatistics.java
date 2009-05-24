84d667aad12de0545f968d1b6f576257d8e638e4
Luke Kolin <luke@deltava.org>
2009-05-24 15:39:14 +0000

    Check for disabled dispatch routes
    git-svn-id: file:///home/svn/DVA2006@5991 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetFlightReportStatistics.java
+++ src/java/org/deltava/dao/GetFlightReportStatistics.java
@@ -53,7 +53,10 @@ public class GetFlightReportStatistics extends DAO {
 		// Build the SQL statement
 		StringBuilder buf = new StringBuilder("SELECT P.AIRPORT_D, P.AIRPORT_A, COUNT(DISTINCT P.ID) AS CNT, "
 				+ "COUNT(DISTINCT R.ID) AS RCNT FROM PIREPS P LEFT JOIN acars.ROUTES R ON "
-				+ "(P.AIRPORT_D=R.AIRPORT_D) AND (P.AIRPORT_A=R.AIRPORT_A) WHERE (P.STATUS=?) ");
+				+ "(P.AIRPORT_D=R.AIRPORT_D) AND (P.AIRPORT_A=R.AIRPORT_A) ");
+		if (noRoutes)
+			buf.append("AND (R.ACTIVE=?) ");
+		buf.append("WHERE (P.STATUS=?) ");
 		if (!allFlights)
 			buf.append("AND ((P.ATTR & ?) > 0) ");
 		if (_dayFilter > 0)
@@ -66,6 +69,8 @@ public class GetFlightReportStatistics extends DAO {
 		try {
 			int pos = 0;
 			prepareStatement(buf.toString());
+			if (noRoutes)
+				_ps.setBoolean(++pos, true);
 			_ps.setInt(++pos, FlightReport.OK);
 			if (!allFlights)
 				_ps.setInt(++pos, FlightReport.ATTR_ACARS);
