427f29cec99413f527b2d9949e2c257c0c6a04a1
Luke Kolin <luke@deltava.org>
2009-08-13 00:29:50 +0000

    Inactivity tracking updates
    git-svn-id: file:///home/svn/DVA2006@6086 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetInactivity.java
+++ src/java/org/deltava/dao/GetInactivity.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2007 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2007, 2009 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao;
 
 import java.sql.*;
@@ -12,7 +12,7 @@ import org.deltava.beans.system.InactivityPurge;
  * A Data Access Object to read Inactivity purge entries. This DAO extends PilotReadDAO since it is used
  * to query pilots who may not have an Inactivity purge table entry, but are eligible for one.
  * @author Luke
- * @version 1.0
+ * @version 2.6
  * @since 1.0
  */
 
@@ -62,60 +62,46 @@ public class GetInactivity extends PilotReadDAO {
 	}
 	
 	/**
-	 * Returns purge entries for all pilots who are eligible to be purged.
-	 * @param days the number of days in the future
-	 * @return a Collection of InactivityPurge beans
-	 * @throws DAOException if a JDBC error occurs
-	 */
-	public Collection<InactivityPurge> getPurgeableSoon(int days) throws DAOException {
-		try {
-			prepareStatement("SELECT * FROM INACTIVITY WHERE (PURGE_DATE < DATE_ADD(CURDATE(), INTERVAL ? DAY))");
-			_ps.setInt(1, days);
-			return executeInactivity();
-		} catch (SQLException se) {
-			throw new DAOException(se);
-		}
-	}
-	
-	/**
-	 * Returns Pilots with no Flight Reports in a set number of days. This call can be used to purge pilots
-	 * based on no flying activity, and can set different thresholds for different levels of total Flights.
-	 * @param days the number of days with no Flight Reports
-	 * @param flights the maximum number of total Flights for each Pilot
-	 * @return a Collection of Pilot beans
-	 * @throws DAOException if a JDBC error occurs
-	 */
-	public Collection<Pilot> getNoFlightPilots(int days, int flights) throws DAOException {
-		try {
-			prepareStatement("SELECT P.*, COUNT(DISTINCT F.ID) AS LEGS, SUM(F.DISTANCE), ROUND(SUM(F.FLIGHT_TIME), 1), "
-					+ "MAX(F.DATE) AS LF FROM PILOTS P LEFT JOIN PIREPS F ON ((P.ID=F.PILOT_ID) AND (F.STATUS=?)) WHERE "
-					+ "(P.STATUS=?) GROUP BY P.ID HAVING (LEGS <= ?) AND ((DATE_ADD(LF, INTERVAL ? DAY) < CURDATE()) "
-					+ "OR (LF IS NULL))");
-			_ps.setInt(1, FlightReport.OK);
-			_ps.setInt(2, Pilot.ACTIVE);
-			_ps.setInt(3, flights);
-			_ps.setInt(4, days);
-			return execute();
-		} catch (SQLException se) {
-			throw new DAOException(se);
-		}
-	}
-	
-	/**
-	 * Returns all Pilots who have not logged in for a certain number of days.
+	 * Returns the IDs of Pilots who have not logged in for a certain number of days.
 	 * @param days the number of days
-	 * @return a Collection of Pilot beans
+	 * @return a Collection of database IDs
 	 * @throws DAOException if a JDBC error occurs
 	 */
-	public Collection<Pilot> getInactivePilots(int days) throws DAOException {
+	public Collection<Integer> getInactivePilots(int days) throws DAOException {
 		try {
-			prepareStatement("SELECT P.*, COUNT(DISTINCT F.ID) AS LEGS, SUM(F.DISTANCE), ROUND(SUM(F.FLIGHT_TIME), 1), "
-					+ "MAX(F.DATE) FROM PILOTS P LEFT JOIN PIREPS F ON ((P.ID=F.PILOT_ID) AND (F.STATUS=?)) WHERE "
-					+ "(P.STATUS=?) AND (DATE_ADD(IFNULL(P.LAST_LOGIN, P.CREATED), INTERVAL ? DAY) < CURDATE()) GROUP BY P.ID");
+			prepareStatement("SELECT ID FROM PILOTS WHERE (STATUS=?) AND (DATE_ADD(IFNULL(LAST_LOGIN, CREATED), "
+					+ "INTERVAL ? DAY) < CURDATE())");
 			_ps.setInt(1, FlightReport.OK);
 			_ps.setInt(2, Pilot.ACTIVE);
 			_ps.setInt(3, days);
-			return execute();
+			return executeIDs();
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+	
+	/**
+	 * Returns the IDs of Pilots who have not participated in the period prior to the Inactivity warning.
+	 * @param loginDays the number of days since their last login
+	 * @param activityDays the number of days to check for Flight Reports or Cooler posts
+	 * @param minPosts the minimum number of Cooler posts
+	 * @return a Collection of database IDs
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public Collection<Integer> getRepeatInactive(int loginDays, int activityDays, int minPosts) throws DAOException {
+		try {
+			prepareStatement("SELECT P.ID, COUNT(F.ID) AS FLIGHTS, COUNT(CP.POST_ID) AS POSTS FROM PILOTS P LEFT JOIN "
+					+ "PIREPS F ON ((P.ID=F.PILOT_ID) AND (F.STATUS=?) AND (F.DATE > DATE_SUB(NOW(), INTERVAL ? DAY))) LEFT JOIN "
+					+ "common.COOLER_POSTS CP ON ((P.ID=CP.AUTHOR_ID) AND (CP.CREATED > DATE_SUB(NOW(), INTERVAL ? DAY))) "
+					+ "WHERE (P.STATUS=?) AND P.LAST_LOGIN < DATE_SUB(NOW(), INTERVAL ? DAY) GROUP BY P.ID HAVING (FLIGHTS=0) "
+					+ "AND (POSTS<2)");
+			_ps.setInt(1, FlightReport.OK);
+			_ps.setInt(2, activityDays);
+			_ps.setInt(3, activityDays);
+			_ps.setInt(4, Pilot.ACTIVE);
+			_ps.setInt(5, loginDays);
+			_ps.setInt(6, minPosts);
+			return executeIDs();
 		} catch (SQLException se) {
 			throw new DAOException(se);
 		}
