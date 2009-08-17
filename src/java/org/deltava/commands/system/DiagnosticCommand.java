fe61bd97867955745a5c4ebb9ec3f9cec0fc9ff8
Luke Kolin <luke@deltava.org>
2009-08-17 14:01:49 +0000

    Track signature cache
    git-svn-id: file:///home/svn/DVA2006@6101 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/system/DiagnosticCommand.java
+++ src/java/org/deltava/commands/system/DiagnosticCommand.java
@@ -111,6 +111,7 @@ public class DiagnosticCommand extends AbstractCommand {
 		daoCaches.add(new GetCoolerThreads(null));
 		daoCaches.add(new GetExamQuestions(null));
 		daoCaches.add(new GetMessageTemplate(null));
+		daoCaches.add(new GetImage(null));
 		daoCaches.add(new GetNavData(null));
 		daoCaches.add(new GetNavRoute(null));
 		daoCaches.add(new GetStatistics(null));
