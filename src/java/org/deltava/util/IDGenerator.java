924251224828f3feb3246813a66931625c909578
Luke Kolin <luke@deltava.org>
2009-08-28 22:08:09 +0000

    Fix duplicate IDs
    git-svn-id: file:///home/svn/DVA2006@6123 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/util/IDGenerator.java
+++ src/java/org/deltava/util/IDGenerator.java
@@ -1,45 +1,30 @@
+// Copyright 2005, 2009 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.util;
-
-import org.apache.log4j.Logger;
 
 /**
  * A unique ID generator for messaging purposes.
  * @author Luke
- * @version 1.0
+ * @version 2.6
  * @since 1.0
  */
 
 public class IDGenerator {
-   
-   private static final Logger log = Logger.getLogger(IDGenerator.class);
+
 	private static long _lastID;
 
 	/**
-	 * Generates a unique ID. This currently returns a timestamp-derived value, but is not guaranteed
-	 * to do so in the future. If you need a unique timestamp, see {@link IDGenerator#generateTimestamp()}.
+	 * Generates a unique ID. This currently returns a timestamp-derived value, but is not guaranteed to do so in the future.
 	 * @return a unique number
-	 * @see IDGenerator#generateTimestamp()
 	 */
 	public synchronized static long generate() {
 
 		// Get current time and check if it's newer than what came before
 		long id = System.currentTimeMillis();
 		if (id <= _lastID)
-			id = ++_lastID;
-		
-		// Log the generated ID
-		log.debug("Generated " + Long.toHexString(id));
+			id = _lastID + 2;
 
 		// Return the id
+		_lastID = id;
 		return id;
-	}
-	
-	/**
-	 * Generates a unique timestamp. This timestamp is not guarnteed to be exactly accurate, since if this
-	 * call returns the same value as the last call, it is incremented by 1.
-	 * @return a 32-bit UNIX timestamp
-	 */
-	public synchronized static long generateTimestamp() {
-	   return generate();
 	}
 }
