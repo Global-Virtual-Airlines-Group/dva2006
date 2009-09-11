63ea1cea555fcb89f30b28956d82d7bae261ff4a
Luke Kolin <luke@deltava.org>
2009-09-11 23:03:54 +0000

    Don't purge people with an inactivity warning
    git-svn-id: file:///home/svn/DVA2006@6160 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/tasks/InactivityUpdateTask.java
+++ src/java/org/deltava/tasks/InactivityUpdateTask.java
@@ -80,9 +80,13 @@ public class InactivityUpdateTask extends Task {
 			for (Iterator<Integer> i = noWarnIDs.iterator(); i.hasNext(); ) {
 				Integer id = i.next();
 				if (!purgeBeans.containsKey(id)) {
-					InactivityPurge ip = new InactivityPurge(id.intValue());
-					ip.setInterval(notifyDays);
-					purgeBeans.put(id, ip);
+					InactivityPurge ip = dao.getInactivity(id.intValue());
+					if (ip == null) {
+						ip = new InactivityPurge(id.intValue());
+						ip.setInterval(notifyDays);
+						purgeBeans.put(id, ip);
+					} else
+						i.remove();
 				}
 			}
 			
