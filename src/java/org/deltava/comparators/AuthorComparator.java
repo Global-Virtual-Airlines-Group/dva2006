c8498325fc8aa9eadba91bc414e46c5aeaad6800
Luke Kolin <luke@deltava.org>
2010-05-25 01:23:18 +0000

    New Comparators
    git-svn-id: file:///home/svn/DVA2006@6643 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/comparators/AuthorComparator.java
@@ -0,0 +1,42 @@
+// Copyright 2010 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.comparators;
+
+import java.util.Comparator;
+
+import org.deltava.beans.AuthoredBean;
+
+/**
+ * A Comparator to sort AuthoredBean objects.
+ * @author Luke
+ * @version 3.1
+ * @since 3.1
+ */
+
+public class AuthorComparator implements Comparator<AuthoredBean>, java.io.Serializable {
+	
+	private boolean _isReverse;
+
+	/**
+	 * Creates a new AuthorComparator.
+	 */
+	public AuthorComparator() {
+		this(false);
+	}
+
+	/**
+	 * Creates a new Comparator with an optional reverse sort.
+	 * @param isReverse TRUE if a reverse sort, otherwise FALSE
+	 */
+	public AuthorComparator(boolean isReverse) {
+		super();
+		_isReverse = isReverse; 
+	}
+	
+	/**
+	 * Compares two AuthoredBeans by comparing their Author IDs.
+	 */
+	public int compare(AuthoredBean ab1, AuthoredBean ab2) {
+		int tmpResult = Integer.valueOf(ab1.getAuthorID()).compareTo(Integer.valueOf(ab2.getAuthorID()));
+		return _isReverse ? (tmpResult * -1) : tmpResult;
+	}
+}
