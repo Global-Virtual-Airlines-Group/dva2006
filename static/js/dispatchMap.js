aadaeaee84000fc9fcf836378aebf1f7c6835c2f
Luke Kolin <luke@deltava.org>
2010-05-25 01:27:34 +0000

    remove unneeded methods
    git-svn-id: file:///home/svn/DVA2006@6646 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/dispatchMap.js
+++ static/js/dispatchMap.js
@@ -26,6 +26,7 @@ return true;
 
 function toggleMarkers(mrks, visible)
 {
+if (mrks == null) return false;
 for (var idx in mrks)
 {
 	var m = mrks[idx];
@@ -35,60 +36,5 @@ for (var idx in mrks)
 		m.hide();
 }
 
-return true;
-}
-
-function clickLine(latlng)
-{
-var newLine;
-var aw_points = mrks_aw[this.AirwayID];
-var oldLine = airways[this.AirwayID];
-if (this.isSelected) {
-	if (map.getZoom() <= 7)
-		return;
-	
-	map.closeInfoWindow();
-	newLine = new GPolyline(aw_points, "#8090A0", 1.5, 0.45, {geodesic:false});
-	for (var x = 0; x < oldLine.markers.length; x++)
-		mm_aw.removeMarker(oldLine.markers[x]);
-
-	window.external.ShowAirwayInput(false, null);
-} else {
-	newLine = new GPolyline(aw_points, "#A0C0FF", 2.25, 0.8, {geodesic:false});
-	newLine.isSelected = true;
-	for (var x = 0; x < oldLine.markers.length; x++)
-		mm_aw.addMarker(oldLine.markers[x], 7);
-
-	selectedAirways.push(newLine);
-	map.openInfoWindowHtml(latlng, window.external.GetAirwayMessage(this.AirwayID), { maxWidth : 275 });
-	window.external.ShowAirwayInput(true, this.AirwayID);
-}
-
-// Add the airway line
-newLine.AirwayID = this.AirwayID;
-newLine.markers = oldLine.markers;
-map.removeOverlay(this);
-map.addOverlay(newLine);
-airways[this.AirwayID] = newLine;
-GEvent.bind(newLine, 'click', newLine, clickLine)
-gaEvent('Dispatch', 'Show Airway', this.AirwayID);
-return true;
-}
-
-function clearSelectedAirways()
-{
-mm_aw.clearMarkers();
-for (var x = 0; x < selectedAirways.length; x++)
-{
-	var aw = selectedAirways[x];
-	var oldLine = airways[this.AirwayID];
-	var aw_points = mrks_aw[this.AirwayID];
-	newLine = new GPolyline(aw_points, "#8090A0", 1.5, 0.45, {geodesic:false});
-	newLine.markers = oldLine.markers;
-	map.removeOverlay(oldLine);
-	map.addOverlay(newLine);
-}
-
-gaEvent('Dispatch', 'Clear Airways');
 return true;
 }
