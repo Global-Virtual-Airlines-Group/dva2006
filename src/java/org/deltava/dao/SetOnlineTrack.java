72bf29bd3b7b327ad101e94a38a1b863ac3fecf5
Luke Kolin <luke@deltava.org>
2010-06-19 22:00:26 +0000

    Remove unused methods
    git-svn-id: file:///home/svn/DVA2006@6683 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/SetOnlineTrack.java
+++ src/java/org/deltava/dao/SetOnlineTrack.java
@@ -1,17 +1,15 @@
-// Copyright 2009 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2009, 2010 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao;
 
 import java.sql.*;
 import java.util.*;
 
-import org.deltava.beans.OnlineNetwork;
-import org.deltava.beans.schedule.Airport;
 import org.deltava.beans.servinfo.PositionData;
 
 /**
  * A Data Access Object to write VATSIM/IVAO Online tracks.
  * @author Luke
- * @version 2.4
+ * @version 3.1
  * @since 2.4
  */
 
@@ -25,23 +23,6 @@ public class SetOnlineTrack extends DAO {
 		super(c);
 	}
 
-	/**
-	 * Purges all entries older than a specific number hours that have not been associated
-	 * with a particular Flight Report.
-	 * @param hours the number of hours
-	 * @throws DAOException if a JDBC error occurs
-	 */
-	public void purge(int hours) throws DAOException {
-		try {
-			prepareStatementWithoutLimits("DELETE FROM ONLINE_TRACK WHERE (PIREP_ID=0) AND "
-				+ "(DATE < DATE_SUB(NOW(), INTERVAL ? HOUR))");
-			_ps.setInt(1, hours);
-			executeUpdate(0);
-		} catch (SQLException se) {
-			throw new DAOException(se);
-		}
-	}
-	
 	/**
 	 * Writes position records to the database and links them to a Flight Report.
 	 * @param pirepID the Flight Report database ID
@@ -71,32 +52,6 @@ public class SetOnlineTrack extends DAO {
 			
 			_ps.executeBatch();
 			_ps.close();
-		} catch (SQLException se) {
-			throw new DAOException(se);
-		}
-	}
-	
-	/**
-	 * Assigns position entries by a Pilot between a particular airport pair within the previous time period
-	 * to a specific Flight Report. 
-	 * @param pirepID the Flight Report database ID
-	 * @param dt the end date/time
-	 * @param hours the number of hours prior to search
-	 * @param aD the departure Airport
-	 * @param aA the arrival Airport
-	 * @throws DAOException if a JDBC error occurs
-	 */
-	public void assign(int pirepID, OnlineNetwork net, java.util.Date dt, int hours, Airport aD, Airport aA) throws DAOException {
-		try {
-			prepareStatementWithoutLimits("UPDATE ONLINE_TRACK SET PIREP_ID=? WHERE (PIREP_ID=0) AND "
-				+ "(AIRPORT_D=?) AND (AIRPORT_A=?) AND (NETWORK=?) AND (DATE > DATE_SUB(?, INTERVAL ? HOUR)");
-			_ps.setInt(1, pirepID);
-			_ps.setString(2, aD.getIATA());
-			_ps.setString(3, aA.getIATA());
-			_ps.setString(4, net.toString());
-			_ps.setTimestamp(5, createTimestamp(dt));
-			_ps.setInt(6, hours);
-			executeUpdate(0);
 		} catch (SQLException se) {
 			throw new DAOException(se);
 		}
