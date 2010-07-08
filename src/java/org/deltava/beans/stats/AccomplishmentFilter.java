e2f6dd35cb8c094b1238ddf46fadeda57246d06b
Luke Kolin <luke@deltava.org>
2010-07-08 04:16:34 +0000

    Add AccomplishmentFilter
    git-svn-id: file:///home/svn/DVA2006@6725 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/stats/AccomplishmentFilter.java
@@ -0,0 +1,36 @@
+// Copyright 2010 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.stats;
+
+import java.util.*;
+
+/**
+ * A utility class to filter out Accomplishments so that only the Accomplishment
+ * with the highest value for a given unit remains.
+ * @author Luke
+ * @version 3.2
+ * @since 3.2
+ */
+
+public class AccomplishmentFilter {
+
+	// Singleton
+	private AccomplishmentFilter() {
+		super();
+	}
+	
+	/**
+	 * Filters a set of Accomoplishments.
+	 * @param accs a Collection of Accomplishment beans
+	 * @return a Collection of Accomplishments
+	 */
+	public static Collection<Accomplishment> filter(Collection<Accomplishment> accs) {
+		Map<Accomplishment.Unit, Accomplishment> results = new HashMap<Accomplishment.Unit, Accomplishment>();
+		for (Accomplishment a : accs) {
+			Accomplishment a2 = results.get(a.getUnit());
+			if ((a2 == null) || (a.getValue() >= a2.getValue()))
+				results.put(a.getUnit(), a);
+		}
+		
+		return new TreeSet<Accomplishment>(results.values());
+	}
+}
