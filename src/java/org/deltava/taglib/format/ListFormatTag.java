2360b232b25cf874f947d370f79e4c05d31b1281
Luke Kolin <luke@deltava.org>
2010-07-11 02:20:43 +0000

    Add empty attribute
    git-svn-id: file:///home/svn/DVA2006@6728 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/format/ListFormatTag.java
+++ src/java/org/deltava/taglib/format/ListFormatTag.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2009 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2009, 2010 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.format;
 
 import java.util.*;
@@ -7,11 +7,12 @@ import javax.servlet.jsp.*;
 import javax.servlet.jsp.tagext.TagSupport;
 
 import org.deltava.beans.ComboAlias;
+import org.deltava.util.CollectionUtils;
 
 /**
- * A JSP tag to format List values.
+ * A JSP tag to format Collection values.
  * @author Luke
- * @version 2.6
+ * @version 3.2
  * @since 1.0
  */
 
@@ -19,6 +20,7 @@ public class ListFormatTag extends TagSupport {
 
     private Collection<?> _values;
     private String _delim;
+    private String _empty;
     
     /**
      * Sets the delimiter for the list entries.
@@ -37,6 +39,22 @@ public class ListFormatTag extends TagSupport {
     }
     
     /**
+     * Sets the value to display if the Collection is empty.
+     * @param e the value
+     */
+    public void setEmptry(String e) {
+    	_empty = e;
+    }
+    
+    /**
+     * Releases the tag's state variables.
+     */
+    public void release() {
+    	super.release();
+    	_empty = null;
+    }
+    
+    /**
      * Renders the object as a delimited string to the JSP output stream. If an object in the Collection is a
      * {@link ComboAlias} object, then the comboName property will be rendered.
      * @return TagSupport.EVAL_PAGE
@@ -47,17 +65,19 @@ public class ListFormatTag extends TagSupport {
         
         // Generate the output string
         StringBuilder buf = new StringBuilder();
-        for (Iterator<?> i = _values.iterator(); i.hasNext(); ) {
-            Object obj = i.next();
-            if (obj instanceof ComboAlias) {
-                buf.append(((ComboAlias) obj).getComboName());
-            } else {
-                buf.append(String.valueOf(obj));
-            }
+        if (!CollectionUtils.isEmpty(_values) || (_empty == null)) {
+        	for (Iterator<?> i = _values.iterator(); i.hasNext(); ) {
+        		Object obj = i.next();
+        		if (obj instanceof ComboAlias)
+        			buf.append(((ComboAlias) obj).getComboName());
+        		else
+        			buf.append(String.valueOf(obj));
             
-            if (i.hasNext())
-                buf.append(_delim);
-        }
+        		if (i.hasNext())
+        			buf.append(_delim);
+        	}
+        } else
+        	buf.append(_empty);
         
         try {
             pageContext.getOut().write(buf.toString());
