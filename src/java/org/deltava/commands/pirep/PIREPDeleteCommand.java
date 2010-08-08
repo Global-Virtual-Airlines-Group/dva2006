a1d6e1dfcda9f5cb40de8f03b82e780a7e1446e9
Luke Kolin <luke@deltava.org>
2010-08-08 16:02:00 +0000

    Add PIREP deletion confirmation
    git-svn-id: file:///home/svn/DVA2006@6793 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/pirep/PIREPDeleteCommand.java
+++ src/java/org/deltava/commands/pirep/PIREPDeleteCommand.java
@@ -12,63 +12,77 @@ import org.deltava.security.command.PIREPAccessControl;
 /**
  * A Web Site Command to delete Flight Reports.
  * @author Luke
- * @version 3.1
+ * @version 3.2
  * @since 1.0
  */
 
 public class PIREPDeleteCommand extends AbstractCommand {
 
-   /**
-    * Executes the command.
-    * @param ctx the Command context
-    * @throws CommandException if an error occurs
-    */
-   public void execute(CommandContext ctx) throws CommandException {
+	/**
+	 * Executes the command.
+	 * @param ctx the Command context
+	 * @throws CommandException if an error occurs
+	 */
+	public void execute(CommandContext ctx) throws CommandException {
 
-      try {
-          Connection con = ctx.getConnection();
-          
-          // Get the DAO and the Flight Report
-          GetFlightReports dao = new GetFlightReports(con);
-          FlightReport fr = dao.get(ctx.getID());
-          if (fr == null)
-             throw notFoundException("Invalid Flight Report - " + ctx.getID());
-          
-          // Check our access level
-          PIREPAccessControl access = new PIREPAccessControl(ctx, fr);
-          access.validate();
-          if (!access.getCanDelete())
-             throw securityException("Cannot delete Flight Report");
-          
-          // Start a JDBC transaction
-          ctx.startTX();
-          
-          // Get the DAO and delete the PIREP from the database
-          SetFlightReport wdao = new SetFlightReport(con);
-          wdao.delete(ctx.getID());
-          
-          // If this is an ACARS PIREP, delete the data
-          if (fr instanceof ACARSFlightReport) {
-             SetACARSLog awdao = new SetACARSLog(con);
-             awdao.deleteInfo(fr.getDatabaseID(DatabaseID.ACARS));
-          }
-          
-          // Commit the transaction
-          ctx.commitTX();
-      } catch (DAOException de) {
-         ctx.rollbackTX();
-          throw new CommandException(de);
-      } finally {
-          ctx.release();
-      }
-      
-      // Update the status for the JSP
-      ctx.setAttribute("isDeleted", Boolean.TRUE, REQUEST);
+		CommandResult result = ctx.getResult();
+		try {
+			Connection con = ctx.getConnection();
 
-      // Forward to the JSP
-      CommandResult result = ctx.getResult();
-      result.setType(ResultType.REQREDIRECT);
-      result.setURL("/jsp/pilot/pirepUpdate.jsp");
-      result.setSuccess(true);
-   }
+			// Get the DAO and the Flight Report
+			GetFlightReports dao = new GetFlightReports(con);
+			FlightReport fr = dao.get(ctx.getID());
+			if (fr == null)
+				throw notFoundException("Invalid Flight Report - " + ctx.getID());
+
+			// Check our access level
+			PIREPAccessControl access = new PIREPAccessControl(ctx, fr);
+			access.validate();
+			if (!access.getCanDelete())
+				throw securityException("Cannot delete Flight Report");
+
+			// Forward to the confirmation page
+			boolean isOK = Boolean.valueOf("force".equals(ctx.getCmdParameter(OPERATION, null))).booleanValue();
+			if (!isOK) {
+				ctx.release();
+				
+				ctx.setAttribute("pirep", fr, REQUEST);
+				result.setURL("/jsp/pilot/pirepDeleteConfirm.jsp");
+				result.setSuccess(true);
+				return;
+			}
+			
+			// Save status
+			ctx.setAttribute("isOurs", Boolean.valueOf(fr.getDatabaseID(DatabaseID.PILOT) == ctx.getUser().getID()), REQUEST);
+
+			// Start a JDBC transaction
+			ctx.startTX();
+
+			// Get the DAO and delete the PIREP from the database
+			SetFlightReport wdao = new SetFlightReport(con);
+			wdao.delete(ctx.getID());
+
+			// If this is an ACARS PIREP, delete the data
+			if (fr instanceof ACARSFlightReport) {
+				SetACARSLog awdao = new SetACARSLog(con);
+				awdao.deleteInfo(fr.getDatabaseID(DatabaseID.ACARS));
+			}
+
+			// Commit the transaction
+			ctx.commitTX();
+		} catch (DAOException de) {
+			ctx.rollbackTX();
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
+
+		// Update the status for the JSP
+		ctx.setAttribute("isDeleted", Boolean.TRUE, REQUEST);
+
+		// Forward to the JSP
+		result.setType(ResultType.REQREDIRECT);
+		result.setURL("/jsp/pilot/pirepUpdate.jsp");
+		result.setSuccess(true);
+	}
 }
