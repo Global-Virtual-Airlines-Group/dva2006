a82a4c64ade1c6acde53a47c334afbbe587fcb83
Luke Kolin <luke@deltava.org>
2010-08-08 16:02:22 +0000

    Remove script language
    git-svn-id: file:///home/svn/DVA2006@6794 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/content/GoogleAnalyticsTag.java
+++ src/java/org/deltava/taglib/content/GoogleAnalyticsTag.java
@@ -1,4 +1,4 @@
-// Copyright 2007, 2008, 2009 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2007, 2008, 2009, 2010 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.content;
 
 import javax.servlet.jsp.*;
@@ -9,7 +9,7 @@ import org.deltava.util.system.SystemData;
 /**
  * A JSP Tag to embed Google analytics data.
  * @author Luke
- * @version 2.4
+ * @version 3.2
  * @since 1.0
  */
 
@@ -79,7 +79,7 @@ public class GoogleAnalyticsTag extends TagSupport {
 			out.println(".google-analytics.com/ga.js\" type=\"text/javascript\"></script>");
 			
 			// Write the analytics script
-			out.println("<script language=\"JavaScript\" type=\"text/javascript\">");
+			out.println("<script type=\"text/javascript\">");
 			out.println("try {");
 			out.print("var ");
 			out.print(_jsVar);
