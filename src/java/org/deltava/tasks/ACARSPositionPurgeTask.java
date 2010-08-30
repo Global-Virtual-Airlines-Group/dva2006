1a2fca28c7be9372c364c9bcc27f5555cf8233ed
Luke Kolin <luke@deltava.org>
2010-08-30 17:37:10 +0000

    Add position purge task
    git-svn-id: file:///home/svn/DVA2006@6848 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/tasks/ACARSPositionPurgeTask.java
@@ -0,0 +1,108 @@
+// Copyright 2010 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.tasks;
+
+import java.util.*;
+import java.sql.Connection;
+
+import org.deltava.beans.*;
+import org.deltava.beans.acars.*;
+import org.deltava.beans.flight.*;
+
+import org.deltava.dao.*;
+import org.deltava.dao.ipc.GetACARSPool;
+
+import org.deltava.taskman.*;
+import org.deltava.util.system.SystemData;
+
+/**
+ * A Scheduled Task to purge orphaned ACARS position entries. 
+ * @author Luke
+ * @version 3.2
+ * @since 3.2
+ */
+
+public class ACARSPositionPurgeTask extends Task {
+
+	/**
+	 * Initializes the Task.
+	 */
+	public ACARSPositionPurgeTask() {
+		super("ACARS Position Purge", ACARSPositionPurgeTask.class);
+	}
+
+	/**
+	 * Executes the task.
+	 */
+	@Override
+	protected void execute(TaskContext ctx) {
+		log.info("Executing");
+		
+		// Get active flights
+		GetACARSPool acdao = new GetACARSPool();
+		Collection<Integer> activeIDs = acdao.getFlightIDs();
+		
+		int flightPurge = SystemData.getInt("log.purge.flights", 48);
+		try {
+			Connection con = ctx.getConnection();
+			
+			// Get the flight IDs
+			GetACARSPurge prdao = new GetACARSPurge(con);
+			Collection<Integer> IDs = prdao.getPositionFlightIDs(Math.round(flightPurge * 1.5f) / 24);
+			IDs.removeAll(activeIDs);
+			
+			// Get the DAOs
+			GetUserData uddao = new GetUserData(con);
+			GetFlightReports frdao = new GetFlightReports(con);
+			
+			// Loop through the IDs
+			GetACARSData dao = new GetACARSData(con);
+			SetACARSPurge wdao = new SetACARSPurge(con);
+			for (Iterator<Integer> i = IDs.iterator(); i.hasNext(); ) {
+				int id = i.next().intValue();
+				
+				// Load the flight ID
+				FlightInfo fInfo = dao.getInfo(id);
+				if (fInfo == null) {
+					int purgeCount = wdao.deletePositions(id);
+					log.warn("Flight ID " + id + " not found, purged " + purgeCount + " poistions");
+				
+				// If we're supposed to be archived, archive positions
+				} else if (fInfo.getArchived()) {
+					int rowCount = wdao.archivePositions(id);
+					log.warn("Flight ID " + id + " archived, moved " + rowCount + " poistions");
+
+				// If we don't have a PIREP, nuke
+				} else if (!fInfo.getHasPIREP()) {
+					ctx.startTX();
+					wdao.deleteInfo(id);
+					int purgeCount = wdao.deletePositions(id);
+					ctx.commitTX();
+					log.warn("Flight ID " + id + " has no PIREP, purged " + purgeCount + " poistions");
+				
+				// Validate that the PIREP does exist
+				} else {
+					ConnectionEntry ce = dao.getConnection(fInfo.getConnectionID());
+					UserData ud = uddao.get(ce.getAuthorID());
+					ACARSFlightReport afr = frdao.getACARS(ud.getDB(), id);
+					if (afr == null) {
+						ctx.startTX();
+						wdao.deleteInfo(id);
+						int purgeCount = wdao.deletePositions(id);
+						ctx.commitTX();
+						log.warn("Flight ID " + id + " has no PIREP, purged " + purgeCount + " poistions");
+					} else if ((afr.getStatus() == FlightReport.OK) || (afr.getStatus() == FlightReport.REJECTED)) {
+						int archiveCount = wdao.archivePositions(id);
+						log.warn("Flight ID " + id + " is not archived, moved " + archiveCount + " poistions");
+					}
+				}
+			}
+		} catch (DAOException de) {
+			ctx.rollbackTX();
+			log.error(de.getMessage(), de);
+		} finally {
+			ctx.release();
+		}
+		
+		log.info("Completed");
+	}
+}
