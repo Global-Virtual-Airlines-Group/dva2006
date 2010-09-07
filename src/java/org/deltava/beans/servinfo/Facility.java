15fa0d87604e8a5658a3cbbc2bf717f2f278c06c
Luke Kolin <luke@deltava.org>
2010-09-07 18:52:19 +0000

    Add local facility coordinates and range
    git-svn-id: file:///home/svn/DVA2006@6891 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/servinfo/Facility.java
+++ src/java/org/deltava/beans/servinfo/Facility.java
@@ -12,16 +12,18 @@ import static org.deltava.beans.MapEntry.*;
 
 public enum Facility {
 	
-	OBS("Observer", WHITE), FSS("Flight Service Station", PURPLE), DEL("Clearance Delivery", BLUE),
-	GND("Ground", ORANGE), TWR("Tower", YELLOW), APP("Approach", GREEN), CTR("Center", RED),
-	ATIS("ATIS", WHITE);
+	OBS("Observer", WHITE, 30), FSS("Flight Service Station", PURPLE, 1750), DEL("Clearance Delivery", BLUE, 25),
+	GND("Ground", ORANGE, 35), TWR("Tower", YELLOW, 60), APP("Approach", GREEN, 150), CTR("Center", RED, 600),
+	ATIS("ATIS", WHITE, 30);
 	
 	private String _name;
 	private String _color;
+	private int _range;
 
-	Facility(String name, String color) {
+	Facility(String name, String color, int range) {
 		_name = name;
 		_color = color;
+		_range = range;
 	}
 	
 	public String getName() {
@@ -30,5 +32,9 @@ public enum Facility {
 	
 	public String getColor() {
 		return _color;
+	}
+	
+	public int getRange() {
+		return _range;
 	}
 }
