c7bd5157432a56ee0c67047ed71b55c8a11dc29e
Luke Kolin <luke@deltava.org>
2010-10-03 18:17:29 +0000

    Build 319
    git-svn-id: file:///home/svn/DVA2006@6946 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -13,7 +13,7 @@ public interface VersionInfo {
     public static final int MAJOR = 3;
     public static final int MINOR = 3;
     
-    public static final int BUILD = 318;
+    public static final int BUILD = 319;
     public static final boolean FINAL = false;
     
     //public static final String BUILD_DATE="$BUILD";
