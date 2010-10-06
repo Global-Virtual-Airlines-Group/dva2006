b72e1a661ec3f93a7406b1d3008a26ab70e125f7
Luke Kolin <luke@deltava.org>
2010-10-06 14:56:37 +0000

    Nomination purge support
    git-svn-id: file:///home/svn/DVA2006@6951 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/commands/admin/NominationPurgeCommand.java
@@ -0,0 +1,90 @@
+// Copyright 2010 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.commands.admin;
+
+import java.util.*;
+import java.sql.Connection;
+
+import org.deltava.beans.*;
+import org.deltava.beans.hr.Nomination;
+import org.deltava.beans.hr.Nomination.Status;
+
+import org.deltava.commands.*;
+import org.deltava.dao.*;
+
+import org.deltava.security.command.NominationAccessControl;
+
+/**
+ * A Web Site Command to reject all pending Senior Captain nominations.
+ * @author Luke
+ * @version 3.3
+ * @since 3.3
+ */
+
+public class NominationPurgeCommand extends AbstractCommand {
+
+	/**
+	 * Executes the Command.
+	 * @param ctx the Command context
+	 * @throws CommandException if an unhandled error occurs
+	 */
+	@Override
+	public void execute(CommandContext ctx) throws CommandException {
+		try {
+			Connection con = ctx.getConnection();
+			
+			// Get the DAOs
+			GetNominations ndao = new GetNominations(con);
+			SetStatusUpdate sudao = new SetStatusUpdate(con);
+			SetNomination nwdao = new SetNomination(con);
+			
+			// Load the nominations
+			Collection<Nomination> noms = ndao.getByStatus(Status.PENDING, null);
+			for (Iterator<Nomination> i = noms.iterator(); i.hasNext(); ) {
+				Nomination nom = i.next();
+				
+				// Check our access
+				NominationAccessControl access = new NominationAccessControl(ctx, nom);
+				access.validate();
+				if (!access.getCanDispose()) {
+					i.remove();
+					continue;
+				}
+				
+				// Set status
+				nom.setStatus(Status.REJECTED);
+				StatusUpdate upd = new StatusUpdate(nom.getID(), StatusUpdate.COMMENT);
+				upd.setAuthorID(ctx.getUser().getID());
+				upd.setCreatedOn(new Date());
+				upd.setDescription("Nomination to " + Rank.SC + " rejected");
+				
+				// Start transaction
+				ctx.startTX();
+				
+				// Write the updates
+				sudao.write(upd);
+				nwdao.update(nom);
+				
+				// Commit
+				ctx.commitTX();
+			}
+			
+			// Load the pilots we have updated
+			GetPilot pdao = new GetPilot(con);
+			ctx.setAttribute("pilots", pdao.getByID(noms, "PILOTS").values(), REQUEST);
+		} catch (DAOException de) {
+			ctx.rollbackTX();
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
+		
+		// Save status variable
+		ctx.setAttribute("isPurged", Boolean.TRUE, REQUEST);
+		
+		// Forward back to the JSP
+		CommandResult result = ctx.getResult();
+		result.setType(ResultType.REQREDIRECT);
+		result.setURL("/jsp/admin/scNominateUpdate.jsp");
+		result.setSuccess(true);
+	}
+}
