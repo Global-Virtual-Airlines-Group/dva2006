688eab472970ced712037c1ce0a851819a2b2976
Luke Kolin <luke@deltava.org>
2010-10-14 15:39:24 +0000

    Modify sort clause
    git-svn-id: file:///home/svn/DVA2006@6966 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/pirep/PIREPQueueCommand.java
+++ src/java/org/deltava/commands/pirep/PIREPQueueCommand.java
@@ -22,7 +22,7 @@ public class PIREPQueueCommand extends AbstractViewCommand {
 	
 	private static final Collection<Integer> PENDING = Arrays.asList(Integer.valueOf(FlightReport.SUBMITTED), Integer.valueOf(FlightReport.HOLD));
 
-	private static final String MY_EQ_SORT = "IF(ER.EQTYPE=?,0,1), PR.DATE, PR.SUBMITTED, PR.ID";
+	private static final String MY_EQ_SORT = "IF(INSTR(?,GROUP_CONCAT(ER.EQTYPE))=0,0,1), PR.DATE, PR.SUBMITTED, PR.ID";
 	private static final String[] SORT_CODES = {"PR.DATE, PR.SUBMITTED, PR.ID", "P.LASTNAME, P.FIRSTNAME, PR.SUBMITTED", 
 		"PR.EQTYPE, PR.DATE, PR.SUBMITTED", "$MYEQ"};
 	private static final String[] SORT_NAMES = {"Submission Date", "Pilot Name", "Equipment Type", "My Program"};
