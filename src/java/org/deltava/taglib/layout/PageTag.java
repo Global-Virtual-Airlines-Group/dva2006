1559e28b73fc714886860d270fee607e3287c681
Luke Kolin <luke@deltava.org>
2011-01-11 21:27:55 +0000

    Eliminate IE6 rendering path
    git-svn-id: file:///home/svn/DVA2006@7185 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/layout/PageTag.java
+++ src/java/org/deltava/taglib/layout/PageTag.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2006, 2008, 2009 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2008, 2009, 2011 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.layout;
 
 import javax.servlet.http.*;
@@ -9,48 +9,17 @@ import org.deltava.beans.Pilot;
 
 import org.deltava.commands.CommandContext;
 
-import org.deltava.taglib.ContentHelper;
-
 /**
- * A JSP tag to render page layouts in a browser-specific way. On Mozilla, absolutely positioned DIV elements will be
- * used, while tables will be used for Internet Explorer 6.
+ * A JSP tag to render page layouts in a user-specific way.
  * @author Luke
- * @version 2.6
+ * @version 3.6
  * @since 1.0
  */
 
 public class PageTag extends TagSupport {
 	
-	private boolean _renderTable;
 	private boolean _sideMenu;
-	private boolean _rowOpen;
 
-	/**
-	 * Checks if the table row (for Internet Explorer) is currently open. This method is package-private to allow the
-	 * {@link org.deltava.taglib.layout.RegionTag} to access it.
-	 * @return TRUE if the row is open, otherwise FALSE
-	 */
-	boolean isRowOpen() {
-		return _rowOpen;
-	}
-	
-	/**
-	 * Marks the table row (for Internet Explorer) as open or closed. This method is package-private to allow the
-	 * {@link org.deltava.taglib.layout.RegionTag} to access it.
-	 * @param isOpen TRUE if the row is open, otherwise FALSE
-	 */
-	void setRowOpen(boolean isOpen) {
-		_rowOpen = isOpen;
-	}
-	
-	/**
-	 * Tells a child tag whether we are rendering TABLE or DIV elements.
-	 * @return TRUE if rendering TABLEs, otherwise FALSE
-	 */
-	boolean renderTable() {
-		return _renderTable;
-	}
-	
 	/**
 	 * Tells a child tag wheter we are rendering side menus or navigation bars.
 	 * @return TRUE if rendering a side menu, otherwise FALSE
@@ -60,43 +29,29 @@ public class PageTag extends TagSupport {
 	}
 	
 	/**
-	 * Releases the tag's state variables.
-	 */
-	public void release() {
-		_rowOpen = false;
-		super.release();
-	}
-	
-	/**
 	 * Writes the layout element's opening tag to the JSP output stream.
 	 * @return TagSuppport.EVAL_BODY_INCLUDE always
 	 * @throws JspException if an error occurs
 	 */
 	public int doStartTag() throws JspException {
-		_renderTable = ContentHelper.isIE6(pageContext);
 		
 		// Check if our screen size is big enough
-		if (!_renderTable) {
-			HttpServletRequest hreq = (HttpServletRequest) pageContext.getRequest(); 
-			HttpSession s = hreq.getSession(false);
-			if (s != null) {
-				Pilot usr = (Pilot) hreq.getUserPrincipal();
-				_sideMenu = ((usr == null) || !usr.getShowNavBar());
-				if (!_sideMenu) {
-					Number sX = (Number) s.getAttribute(CommandContext.SCREENX_ATTR_NAME);
-					_sideMenu = (sX == null) || (sX.intValue() < 1280);
-				}
-			} else
-				_sideMenu = true;
+		HttpServletRequest hreq = (HttpServletRequest) pageContext.getRequest(); 
+		HttpSession s = hreq.getSession(false);
+		if (s != null) {
+			Pilot usr = (Pilot) hreq.getUserPrincipal();
+			_sideMenu = ((usr == null) || !usr.getShowNavBar());
+			if (!_sideMenu) {
+				Number sX = (Number) s.getAttribute(CommandContext.SCREENX_ATTR_NAME);
+				_sideMenu = (sX == null) || (sX.intValue() < 1280);
+			}
 		} else
 			_sideMenu = true;
 		
 		// Render a table for IE6
 		try {
 			JspWriter out = pageContext.getOut();
-			if (_renderTable)
-				out.print("<table id=\"ieLayout\" class=\"navside\"><tbody>");
-			else if (_sideMenu)
+			if (_sideMenu)
 				out.print("<div class=\"navside\">");
 			else
 				out.print("<div class=\"navbar\">");
@@ -115,14 +70,7 @@ public class PageTag extends TagSupport {
 	 */
 	public int doEndTag() throws JspException {
 		try {
-			JspWriter out = pageContext.getOut();
-			if (_renderTable) {
-				if (_rowOpen)
-					out.print("</tr>");
-				
-				out.print("</tbody></table>");
-			} else
-				out.print("</div>");
+			pageContext.getOut().print("</div>");
 		} catch (Exception e) {
 			throw new JspException(e);
 		} finally {
