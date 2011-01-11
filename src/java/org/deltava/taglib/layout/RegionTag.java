deea55164b3a832c81e1302b9f154c3e0852b3fe
Luke Kolin <luke@deltava.org>
2011-01-11 21:27:55 +0000

    Eliminate IE6 rendering path
    git-svn-id: file:///home/svn/DVA2006@7185 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/layout/RegionTag.java
+++ src/java/org/deltava/taglib/layout/RegionTag.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2006, 2008, 2009 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2008, 2009, 2011 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.layout;
 
 import java.util.*;
@@ -7,20 +7,16 @@ import javax.servlet.jsp.*;
 import javax.servlet.jsp.tagext.*;
 
 /**
- * A JSP tag to render page layouts in a browser-specific way. On Mozilla, absolutely positioned DIV elements will be
- * used, while tables will be used for Internet Explorer.
+ * A JSP tag to render page layouts in a user-specific way.
  * @author Luke
- * @version 2.6
+ * @version 3.6
  * @since 1.0
  */
 
 public class RegionTag extends TagSupport {
 
 	private final Map<String, String> _attrs = new HashMap<String, String>();
-	private final Map<String, String> _tableAttrs = new HashMap<String, String>(); 
-	
 	private PageTag _parent;
-	private boolean _closeRow;
 
 	/**
 	 * Sets the DOM ID of the Region element.
@@ -39,49 +35,10 @@ public class RegionTag extends TagSupport {
 	}
 
 	/**
-	 * If using Internet Explorer, close the current table row at the end of this element. <i>Note that
-	 * the PageTag will automatically insert a row closing element to the JSP output stream, so this
-	 * attribute should not be set on the last element on a page.</i>
-	 * @param doClose TRUE if a &lt;/TR&gt; element should be written, otherwise FALSE
-	 */
-	public void setCloseRow(boolean doClose) {
-		_closeRow = doClose;
-	}
-
-	/**
-	 * If using Internet Explorer, the number of rows this table cell should span.
-	 * @param rowCount the value of the table cell ROWSPAN attribute
-	 * @throws IllegalArgumentException if rowCount is negative
-	 * @see RegionTag#setCols(int)
-	 */
-	public void setRows(int rowCount) {
-		if (rowCount < 0)
-			throw new IllegalArgumentException("Invalid row count - " + rowCount);
-
-		_tableAttrs.put("rowspan", String.valueOf(rowCount));
-	}
-
-	/**
-	 * If using Internet Explorer, the number of columns this table cell should span.
-	 * @param colCount the value of the table cell COLSPAN attribute
-	 * @throws IllegalArgumentException if colCount is ngeative
-	 * @see RegionTag#setRows(int)
-	 */
-	public void setCols(int colCount) {
-		if (colCount < 0)
-			throw new IllegalArgumentException("Invalid column count - " + colCount);
-
-		_tableAttrs.put("colspan", String.valueOf(colCount));
-	}
-
-	/**
 	 * Releases the tag's state variables.
 	 */
 	public void release() {
 		_attrs.clear();
-		_tableAttrs.clear();
-		_parent = null;
-		_closeRow = false;
 		super.release();
 	}
 
@@ -99,24 +56,14 @@ public class RegionTag extends TagSupport {
 
 		JspWriter out = pageContext.getOut();
 		try {
-			if (_parent.renderTable()) {
-				_attrs.putAll(_tableAttrs);
-				if (!_parent.isRowOpen()) {
-					out.print("<tr>");
-					_parent.setRowOpen(true);
-				}
-				
-				out.print("<td ");
-			} else
-				out.print("<div ");
+			out.print("<div ");
 
 			// Write the attributes
-			for (Iterator<String> i = _attrs.keySet().iterator(); i.hasNext();) {
-				String attrName = i.next();
-				String attrValue = _attrs.get(attrName);
-				out.print(attrName);
+			for (Iterator<Map.Entry<String, String>> i = _attrs.entrySet().iterator(); i.hasNext();) {
+				Map.Entry<String, String> me = i.next();
+				out.print(me.getKey());
 				out.print("=\"");
-				out.print(attrValue);
+				out.print(me.getValue());
 				out.print(i.hasNext() ? "\" " : "\"");
 			}
 
@@ -135,17 +82,8 @@ public class RegionTag extends TagSupport {
 	 * @throws JspException if an error occurs
 	 */
 	public int doEndTag() throws JspException {
-
-		JspWriter out = pageContext.getOut();
 		try {
-			if (_parent.renderTable()) {
-				out.print("</td>");
-				if (_closeRow) {
-					out.print("</tr>");
-					_parent.setRowOpen(false);
-				}
-			} else
-				out.print("</div>");
+			pageContext.getOut().print("</div>");
 		} catch (Exception e) {
 			throw new JspException(e);
 		}
