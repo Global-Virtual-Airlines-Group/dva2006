7b19f0a5167e3752477269ac79f2591367a0c406
Luke Kolin <luke@deltava.org>
2011-01-18 15:25:46 +0000

    Build 342
    git-svn-id: file:///home/svn/DVA2006@7215 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -1,25 +1,25 @@
-// Copyright 2005, 2006, 2007, 2008, 2009, 2010 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007, 2008, 2009, 2010, 2011 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.system;
 
 /**
  * An interface to store version info constants.
  * @author Luke
- * @version 3.4
+ * @version 3.6
  * @since 1.0
  */
 
 public interface VersionInfo {
 
     public static final int MAJOR = 3;
-    public static final int MINOR = 4;
+    public static final int MINOR = 6;
     
-    public static final int BUILD = 332;
+    public static final int BUILD = 342;
     public static final boolean FINAL = false;
     
     //public static final String BUILD_DATE="$BUILD";
     
-    public static final String TXT_COPYRIGHT = "Copyright 2004 - 2010 Global Virtual Airlines Group. All Rights Reserved.";
-    public static final String HTML_COPYRIGHT = "Copyright &copy; 2004 - 2010 <a rel=\"external\" class=\"small\" href=\"http://www.gvagroup.org/\">Global Virtual Airlines Group</a>. All Rights Reserved.";
+    public static final String TXT_COPYRIGHT = "Copyright 2004 - 2011 Global Virtual Airlines Group. All Rights Reserved.";
+    public static final String HTML_COPYRIGHT = "Copyright &copy; 2004 - 2011 <a rel=\"external\" class=\"small\" href=\"http://www.gvagroup.org/\">Global Virtual Airlines Group</a>. All Rights Reserved.";
     
     public static final String APPNAME = "(Golgotha v" + String.valueOf(MAJOR) + "." + String.valueOf(MINOR) + ")";
     public static final String USERAGENT = "Golgotha/" + String.valueOf(MAJOR) + "." + String.valueOf(MINOR) + "-b" + String.valueOf(BUILD);
