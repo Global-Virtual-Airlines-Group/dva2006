f79083b86837af25790b6946f2c93228a9d25a74
Luke Kolin <luke@deltava.org>
2011-03-02 19:17:47 +0000

    Allow shortlist reset
    git-svn-id: file:///home/svn/DVA2006@7321 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/commands/hr/JobResetCommand.java
@@ -0,0 +1,73 @@
+// Copyright 2011 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.commands.hr;
+
+import java.sql.Connection;
+
+import org.deltava.beans.hr.*;
+
+import org.deltava.commands.*;
+import org.deltava.dao.*;
+
+import org.deltava.security.command.JobPostingAccessControl;
+
+/**
+ * A Web Site Command to reset a closed Job Posting.
+ * @author Luke
+ * @version 3.6
+ * @since 3.6
+ */
+
+public class JobResetCommand extends AbstractCommand {
+
+    /**
+     * Executes the command.
+     * @param ctx the Command context
+     * @throws CommandException if an unhandled error occurs
+     */
+	@Override
+	public void execute(CommandContext ctx) throws CommandException {
+		try {
+			Connection con = ctx.getConnection();
+			
+			// Load the Job Posting
+			GetJobs dao = new GetJobs(con);
+			JobPosting jp = dao.get(ctx.getID());
+			if (jp == null)
+				throw notFoundException("Unknown Job Posting - " + ctx.getID());
+			
+			// Validate our access
+			JobPostingAccessControl access = new JobPostingAccessControl(ctx, jp);
+			access.validate();
+			if (!access.getCanReset())
+				throw securityException("Cannot reset short list for Job Posting " + jp.getID());
+			
+			// Start transaction
+			ctx.startTX();
+			
+			// Update the job and its applicants
+			SetJobs jwdao = new SetJobs(con);
+			jp.setStatus(JobPosting.CLOSED);
+			jwdao.write(jp);
+			for (Application a : jp.getApplications()) {
+				if (a.getStatus() == Application.SHORTLIST) {
+					a.setStatus(Application.NEW);
+					jwdao.write(a);
+				}
+			}
+			
+			// Commit
+			ctx.commitTX();
+		} catch (DAOException de) {
+			ctx.rollbackTX();
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
+		
+		// Forward to the command
+		CommandResult result = ctx.getResult();
+		result.setURL("job", null, ctx.getID());
+		result.setType(ResultType.REDIRECT);
+		result.setSuccess(true);
+	}
+}
