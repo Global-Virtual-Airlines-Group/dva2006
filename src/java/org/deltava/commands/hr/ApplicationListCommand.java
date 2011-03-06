ad69af953761a141b452e969d96ce6144da90489
Luke Kolin <luke@deltava.org>
2011-03-06 21:55:29 +0000

    Add applicant list command
    git-svn-id: file:///home/svn/DVA2006@7337 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/commands/hr/ApplicationListCommand.java
@@ -0,0 +1,63 @@
+// Copyright 2011 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.commands.hr;
+
+import java.util.*;
+import java.sql.Connection;
+
+import org.deltava.beans.hr.*;
+
+import org.deltava.commands.*;
+import org.deltava.dao.*;
+
+import org.deltava.util.CollectionUtils;
+
+/**
+ * A Web Site Command to list all Job Applications.
+ * @author Luke
+ * @version 3.6
+ * @since 3.6
+ */
+
+public class ApplicationListCommand extends AbstractViewCommand {
+
+    /**
+     * Executes the command.
+     * @param ctx the Command context
+     * @throws CommandException if an unhandled error occurs
+     */
+	@Override
+	public void execute(CommandContext ctx) throws CommandException {
+		ViewContext vc = initView(ctx);
+		try {
+			Connection con = ctx.getConnection();
+			
+			// Get all of the jobs
+			GetJobs dao = new GetJobs(con);
+			Collection<JobPosting> allJobs = dao.getAll();
+			ctx.setAttribute("jobs", CollectionUtils.createMap(dao.getAll(), "ID"), REQUEST);
+			
+			// Get the hiring manager IDs
+			Collection<Integer> IDs = new HashSet<Integer>();
+			for (JobPosting jp : allJobs)
+				IDs.add(new Integer(jp.getHireManagerID()));
+			
+			// Load hiring managers
+			GetPilot pdao = new GetPilot(con);
+			ctx.setAttribute("hireMgrs", pdao.getByID(IDs, "PILOTS"), REQUEST);
+			
+			// Get the applications
+        	dao.setQueryStart(vc.getStart());
+        	dao.setQueryMax(vc.getCount());
+        	vc.setResults(dao.getApplications());
+		} catch (DAOException de) {
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
+		
+		// Forward to the JSP
+		CommandResult result = ctx.getResult();
+		result.setURL("/jsp/hr/jobApps.jsp");
+		result.setSuccess(true);
+	}
+}
