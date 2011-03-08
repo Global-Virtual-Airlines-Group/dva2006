2306bdc7836a2dcf910b7038433adf97ab08d51d
Luke Kolin <luke@deltava.org>
2011-03-08 06:12:48 +0000

    Build 354
    git-svn-id: file:///home/svn/DVA2006@7342 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -13,7 +13,7 @@ public interface VersionInfo {
     public static final int MAJOR = 3;
     public static final int MINOR = 6;
     
-    public static final int BUILD = 353;
+    public static final int BUILD = 354;
     public static final boolean FINAL = false;
     
     //public static final String BUILD_DATE="$BUILD";
