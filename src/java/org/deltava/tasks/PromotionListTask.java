aa726055555da2ea856c1a89b5ee300000c573bb
Luke Kolin <luke@deltava.org>
2011-04-09 23:48:32 +0000

    Automatic promotion cooler post task
    git-svn-id: file:///home/svn/DVA2006@7382 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/tasks/PromotionListTask.java
@@ -0,0 +1,122 @@
+// Copyright 2011 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.tasks;
+
+import java.util.*;
+import java.sql.Connection;
+
+import org.deltava.beans.*;
+import org.deltava.beans.cooler.*;
+
+import org.deltava.dao.*;
+import org.deltava.taskman.*;
+
+import org.deltava.util.CalendarUtils;
+import org.deltava.util.StringUtils;
+import org.deltava.util.system.SystemData;
+
+/**
+ * A Scheduled Task to display daily promotions in the Water Cooler. 
+ * @author Luke
+ * @version 3.6
+ * @since 3.6
+ */
+
+public class PromotionListTask extends Task {
+	
+	private static final List<Integer> UPD_TYPES = Arrays.asList(Integer.valueOf(StatusUpdate.CERT_ADD),
+		Integer.valueOf(StatusUpdate.RECOGNITION), Integer.valueOf(StatusUpdate.EXTPROMOTION),
+		Integer.valueOf(StatusUpdate.INTPROMOTION));
+
+	/**
+	 * Initializes the Task.
+	 */
+	public PromotionListTask() {
+		super("Daily Promotion List", PromotionListTask.class);
+	}
+
+	/**
+	 * Executes the Task.
+	 */
+	@Override
+	protected void execute(TaskContext ctx) {
+		
+		// Get the channel to post in
+		String channelName = SystemData.get("cooler.promotion.channel");
+		if (channelName == null) {
+			log.warn("No Water Cooler promotions channel");
+			return;
+		}
+		
+		try {
+			Connection con = ctx.getConnection();
+			
+			// Get the Channel
+			GetCoolerChannels cdao = new GetCoolerChannels(con);
+			Channel c = cdao.get(channelName);
+			if (c == null)
+				throw new DAOException("Unknown promotions Channel - " + channelName);
+			
+			// Load the author
+			GetPilotDirectory pdao = new GetPilotDirectory(con);
+			Pilot author = pdao.getByCode(SystemData.get("users.tasks_by"));
+			
+			// Load the updates
+			GetStatusUpdate sudao = new GetStatusUpdate(con);
+			Collection<StatusUpdate> upds = new ArrayList<StatusUpdate>();
+			for (Iterator<Integer> i = UPD_TYPES.iterator(); i.hasNext(); ) {
+				int updType = i.next().intValue();
+				upds.addAll(sudao.getByType(updType, SystemData.getInt("cooler.promotion.maxAge", 24)));
+			}
+			
+			// Create the message
+			log.info("Recording " + upds.size() + " Pilot Recognition entries in Forum");
+			DateTime now = new DateTime(new Date());
+			now.convertTo(TZInfo.get(SystemData.get("time.timezone")));
+			MessageThread mt = new MessageThread(SystemData.get("airline.name") + " promotions for "
+				+ StringUtils.format(now.getDate(), SystemData.get("time.date_format")));
+			mt.setChannel(c.getName());
+			mt.setStickyUntil(CalendarUtils.adjust(now.getDate(), 1));
+
+			Message msg = new Message(author.getID());
+			msg.setRemoteAddr("127.0.0.1");
+			msg.setRemoteHost("localhost");
+			StringBuilder msgBuf = new StringBuilder("The following " + SystemData.get("airline.name")
+				+ " pilots have received promotions, Flight Academy certifications or achieved Accomplishments:");
+			msgBuf.append("\n\n");
+			
+			// Add the pilots
+			for (Iterator<StatusUpdate> i = upds.iterator(); i.hasNext(); ) {
+				StatusUpdate upd = i.next();
+				Pilot p = pdao.get(upd.getID());
+				
+				// Add pilot name and link
+				msgBuf.append("[url=\"/profile.do?id=");
+				msgBuf.append(p.getHexID());
+				msgBuf.append("\"]");
+				msgBuf.append(p.getName());
+				msgBuf.append("[/url] - ");
+				
+				// Add status update description
+				msgBuf.append(upd.getDescription());
+				msgBuf.append('\n');
+			}
+			
+			// Set the body
+			msgBuf.append("Please join me in congratulating these " + SystemData.get("airline.name") + " Pilots.\n\n");
+			msg.setBody(msgBuf.toString());
+			mt.addPost(msg);
+			
+			// Write the thread
+			if (!upds.isEmpty()) {
+				SetCoolerMessage mwdao = new SetCoolerMessage(con);
+				mwdao.write(mt);
+			}
+		} catch (DAOException de) {
+			log.error(de.getMessage(), de);
+		} finally {
+			ctx.release();
+		}
+
+		log.info("Processing Complete");
+	}
+}
