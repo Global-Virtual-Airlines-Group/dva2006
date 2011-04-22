0e9a3ba41f343b85eaa46568c8758b4215997727
Luke Kolin <luke@deltava.org>
2011-04-22 21:18:29 +0000

    Conversion work
    git-svn-id: file:///home/svn/DVA2006@7416 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/system/IssueConvertCommand.java
+++ src/java/org/deltava/commands/system/IssueConvertCommand.java
@@ -2,12 +2,10 @@
 package org.deltava.commands.system;
 
 import java.util.*;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.*;
 import java.sql.Connection;
 
-import org.deltava.beans.system.Issue;
-import org.deltava.beans.system.IssueComment;
+import org.deltava.beans.system.*;
 
 import org.deltava.commands.*;
 import org.deltava.dao.*;
@@ -32,63 +30,75 @@ public class IssueConvertCommand extends AbstractCommand {
 	public void execute(CommandContext ctx) throws CommandException {
 		try {
 			Connection con = ctx.getConnection();
-			
-			// Get the Issue
-	         GetIssue rdao = new GetIssue(con);
-	         Issue i = rdao.get(ctx.getID());
-	         if (i == null)
-	        	 throw notFoundException("Invalid Issue ID - " + ctx.getID());
-	         
-	         // Check our access level
-	         IssueAccessControl access = new IssueAccessControl(ctx, i);
-	         access.validate();
-	         if (!access.getCanResolve())
-	        	 throw securityException("Cannot convert development Issue " + ctx.getID());
-	         
-	         // Create the help desk bean
-	         org.deltava.beans.help.Issue hi = new org.deltava.beans.help.Issue(i.getSubject());
-	         hi.setAuthorID(i.getAuthorID());
-	         hi.setCreatedOn(i.getCreatedOn());
-	         hi.setStatus(org.deltava.beans.help.Issue.OPEN);
-	         hi.setBody(i.getDescription());
-	         
-	         // Convert the comments
-	         for (Iterator<IssueComment> ici = i.getComments().iterator(); ici.hasNext(); ) {
-	        	 IssueComment ic = ici.next();
 
-	        	 
-	         }
-	         
-	         // Create new comment
-	         
-	         // Mark resolved
-	         i.setResolvedOn(new Date());
-	         i.setStatus(Issue.STATUS_WONTFIX);
-	         
-	         // Start transaction
-	         ctx.startTX();
-	         
-	         // Write new Issue
-	         SetHelp hwdao = new SetHelp(con);
-	         
-	         
-	         // Close issue
-	         SetIssue iwdao = new SetIssue(con);
-	         iwdao.write(i);
-	         
-				// Add a dummy issue comment
-				try {
-					URL url = new URL("http", ctx.getRequest().getServerName(), "/hdissue.do?id=" + hi.getHexID());
-					org.deltava.beans.system.IssueComment ic = new org.deltava.beans.system.IssueComment("Converted Help Desk Issue at " + url.toString());
-					ic.setAuthorID(ctx.getUser().getID());
-					ic.setIssueID(i.getID());
-					wdao.writeComment(ic);
-				} catch (MalformedURLException mue) {
-					// empty
-				}
+			// Get the Issue
+			GetIssue rdao = new GetIssue(con);
+			Issue i = rdao.get(ctx.getID());
+			if (i == null)
+				throw notFoundException("Invalid Issue ID - " + ctx.getID());
+
+			// Check our access level
+			IssueAccessControl access = new IssueAccessControl(ctx, i);
+			access.validate();
+			if (!access.getCanResolve())
+				throw securityException("Cannot convert development Issue " + ctx.getID());
+
+			// Create the help desk bean
+			org.deltava.beans.help.Issue hi = new org.deltava.beans.help.Issue(i.getSubject());
+			hi.setAuthorID(i.getAuthorID());
+			hi.setCreatedOn(i.getCreatedOn());
+			hi.setSubject(i.getSubject());
+			hi.setStatus(org.deltava.beans.help.Issue.OPEN);
+			hi.setBody(i.getDescription());
+
+			// Mark resolved
+			i.setResolvedOn(new Date());
+			i.setStatus(Issue.STATUS_WONTFIX);
+
+			// Start transaction
+			ctx.startTX();
+
+			// Write new Issue
+			SetHelp hwdao = new SetHelp(con);
+			hwdao.write(hi);
+
+			// Convert the comments
+			for (Iterator<IssueComment> ici = i.getComments().iterator(); ici.hasNext();) {
+				IssueComment ic = ici.next();
+				org.deltava.beans.help.IssueComment hic = new org.deltava.beans.help.IssueComment(ic.getAuthorID());
+				hic.setCreatedOn(ic.getCreatedOn());
+				hic.setBody(ic.getComments());
+				hic.setID(hi.getID());
+				hwdao.write(hic);
+			}
+
+			// Add a dummy issue comment
+			try {
+				URL url = new URL("http", ctx.getRequest().getServerName(), "/issue.do?id=" + i.getHexID());
+				org.deltava.beans.help.IssueComment hic = new org.deltava.beans.help.IssueComment(ctx.getUser().getID());
+				hic.setBody("Converted from Development Issue at " + url.toString());
+				hic.setID(hi.getID());
+				hwdao.write(hic);
+			} catch (MalformedURLException mue) {
+				// empty
+			}
+
+			// Close issue
+			SetIssue iwdao = new SetIssue(con);
+			iwdao.write(i);
 			
-	         // Commit
-	         ctx.commitTX();
+			// Add a dummy issue comment
+			try {
+				URL url = new URL("http", ctx.getRequest().getServerName(), "/hdissue.do?id=" + hi.getHexID());
+				IssueComment ic = new IssueComment(ctx.getUser().getID(), "Converted to Help Desk Issue at " + url.toString());
+				ic.setIssueID(i.getID());
+				iwdao.writeComment(ic);
+			} catch (MalformedURLException mue) {
+				// empty
+			}
+
+			// Commit
+			ctx.commitTX();
 		} catch (DAOException de) {
 			ctx.rollbackTX();
 			throw new CommandException(de);
@@ -98,7 +108,8 @@ public class IssueConvertCommand extends AbstractCommand {
 
 		// Forward to the JSP
 		CommandResult result = ctx.getResult();
-		
-		
+		result.setType(ResultType.REDIRECT);
+		result.setURL("issue", null, ctx.getID());
+		result.setSuccess(true);
 	}
 }
