c6a5f8c56b6452273463c3e099c1ee52a2ee77f1
Luke Kolin <luke@deltava.org>
2011-04-25 03:43:29 +0000

    Added workflow steps and detail to hire process
    git-svn-id: file:///home/svn/DVA2006@7424 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/hr/JobApproveCommand.java
+++ src/java/org/deltava/commands/hr/JobApproveCommand.java
@@ -1,8 +1,8 @@
-// Copyright 2011 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2010, 2011 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.hr;
 
+import java.util.*;
 import java.sql.Connection;
-import java.util.Date;
 
 import org.deltava.beans.hr.*;
 import org.deltava.commands.*;
@@ -10,14 +10,16 @@ import org.deltava.dao.*;
 
 import org.deltava.security.command.JobPostingAccessControl;
 
+import org.deltava.util.StringUtils;
+
 /**
- * A Web Site Command to mark a Job Posting hire process as complete.
+ * A Web Site Command to select short-listed applicants for a Job Posting. 
  * @author Luke
  * @version 3.6
- * @since 3.6
+ * @since 3.4
  */
 
-public class JobCompleteCommand extends AbstractCommand {
+public class JobApproveCommand extends AbstractCommand {
 
     /**
      * Executes the command.
@@ -29,30 +31,51 @@ public class JobCompleteCommand extends AbstractCommand {
 		try {
 			Connection con = ctx.getConnection();
 			
-			// Get the Job profile
+			// Load the Job Posting
 			GetJobs dao = new GetJobs(con);
 			JobPosting jp = dao.get(ctx.getID());
 			if (jp == null)
 				throw notFoundException("Unknown Job Posting - " + ctx.getID());
-
+			
 			// Validate our access
 			JobPostingAccessControl access = new JobPostingAccessControl(ctx, jp);
 			access.validate();
-			if (!access.getCanComplete())
-				throw securityException("Cannot mark Job Posting " + jp.getID() + " complete");
+			if (!access.getCanSelect())
+				throw securityException("Cannot select shortlisted Applicant for Job Posting " + jp.getID());
 			
 			// Start transaction
 			ctx.startTX();
 			
 			// Update status
 			SetJobs jwdao = new SetJobs(con);
-			jp.setStatus(JobPosting.COMPLETE);
+			jp.setStatus(JobPosting.SELECTED);
 			jwdao.write(jp);
 			
-			// Write comment
+			// Create comment buffer
+			StringBuilder buf = new StringBuilder();
+			
+			// Select applicants
+			Collection<String> appIDs = ctx.getParameters("sl");
+			Collection<Application> selected = new ArrayList<Application>();
+			for (Iterator<Application> i = jp.getApplications().iterator(); i.hasNext(); ) {
+				Application a = i.next();
+				boolean isSelected = appIDs.contains(StringUtils.formatHex(a.getAuthorID()));
+				if (a.getShortlisted() && isSelected) {
+					a.setStatus(Application.APPROVED);
+					selected.add(a);
+					buf.append("Selected " + a.getName() + " for hire\r\n");
+				} else if (a.getApproved() && !isSelected) {
+					a.setStatus(Application.SHORTLIST);
+					buf.append("De-selected " + a.getName() + " for hire\r\n");	
+				}
+				
+				jwdao.write(a);
+			}
+			
+			// Create comment
 			Comment c = new Comment(jp.getID(), ctx.getUser().getID());
 			c.setCreatedOn(new Date());
-			c.setBody("Completed hiring process");
+			c.setBody(buf.toString());
 			jwdao.write(c);
 			
 			// Commit
@@ -60,7 +83,8 @@ public class JobCompleteCommand extends AbstractCommand {
 
 			// Save status attributes
 			ctx.setAttribute("job", jp, REQUEST);
-			ctx.setAttribute("isCompleted", Boolean.TRUE, REQUEST);
+			ctx.setAttribute("selected", selected, REQUEST);
+			ctx.setAttribute("isSelected", Boolean.TRUE, REQUEST);
 		} catch (DAOException de) {
 			ctx.rollbackTX();
 			throw new CommandException(de);
