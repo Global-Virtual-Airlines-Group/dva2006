73eec99510e62b4dc2ad053eea828d0e6312241e
Luke Kolin <luke@deltava.org>
2011-04-25 01:19:35 +0000

    Allow multiple random flight legs
    git-svn-id: file:///home/svn/DVA2006@7422 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/assign/SingleAssignmentSearchCommand.java
+++ src/java/org/deltava/commands/assign/SingleAssignmentSearchCommand.java
@@ -1,4 +1,4 @@
-// Copyright 2008, 2009, 2010 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2008, 2009, 2010, 2011 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.assign;
 
 import java.util.*;
@@ -11,13 +11,14 @@ import org.deltava.beans.schedule.*;
 import org.deltava.commands.*;
 import org.deltava.dao.*;
 
+import org.deltava.util.StringUtils;
 import org.deltava.util.system.SystemData;
 
 /**
  * A Web Site Command to search the schedule to build a flight assignment that consists of a single leg selected at
  * random from the last Airport the Pilot completed a flight to in the selected aircraft.
  * @author Luke
- * @version 3.3
+ * @version 3.6
  * @since 2.2
  */
 
@@ -38,6 +39,8 @@ public class SingleAssignmentSearchCommand extends AbstractCommand {
 		criteria.setDBName(SystemData.get("airline.db"));
 		criteria.setMaxResults(1);
 
+		// Get total legs to load
+		int totalLegs = Math.min(8, Math.max(1, StringUtils.parse(ctx.getParameter("legs"), 1)));
 		try {
 			Connection con = ctx.getConnection();
 
@@ -82,26 +85,45 @@ public class SingleAssignmentSearchCommand extends AbstractCommand {
 				criteria.setAirline(SystemData.getAirline(ctx.getParameter("airline")));
 			}
 			
-			// Load the schedule entries
+			// Define Variables
+			AssignmentInfo ai = null; Airport startAirport = criteria.getAirportD();
+			Collection<ScheduleEntry> flightEntries = new ArrayList<ScheduleEntry>();
+			
+			// Load the schedule entries - allow multiple legs
 			GetSchedule sdao = new GetSchedule(con);
-			sdao.setQueryMax(criteria.getMaxResults());
-			List<ScheduleEntry> entries = sdao.search(criteria);
-			String eqType = entries.isEmpty() ? "RANDOM" : entries.get(0).getEquipmentType();
-			AssignmentInfo ai = new AssignmentInfo(eqType);
-			if (!entries.isEmpty()) {
-				ScheduleEntry entry = entries.get(0);
-				ai.addAssignment(new AssignmentLeg(entry));
-				DraftFlightReport fr = new DraftFlightReport(entry);
-				fr.setTimeD(entry.getTimeD());
-				fr.setTimeA(entry.getTimeA());
-				fr.setAttribute(FlightReport.ATTR_HISTORIC, entry.getHistoric());
-				ai.addFlight(fr);
+			sdao.setQueryMax(1);
+			for (int x = 0; x < totalLegs; x++) {
+				List<ScheduleEntry> legs = sdao.search(criteria);	
+				if (ai == null) {
+					String eqType = legs.isEmpty() ? "RANDOM" : legs.get(0).getEquipmentType();
+					ai = new AssignmentInfo(eqType);
+				}
+				
+				// Add the leg
+				if (!legs.isEmpty()) {
+					ScheduleEntry entry = legs.get(0);
+					ai.addAssignment(new AssignmentLeg(entry));
+					DraftFlightReport fr = new DraftFlightReport(entry);
+					fr.setTimeD(entry.getTimeD());
+					fr.setTimeA(entry.getTimeA());
+					fr.setAttribute(FlightReport.ATTR_HISTORIC, entry.getHistoric());
+					ai.addFlight(fr);
+					flightEntries.add(entry);
+					
+					// Set new starting airport
+					criteria.setAirportD(entry.getAirportA());
+				} else
+					break;
 			}
+			
+			// Restore starting airport
+			criteria.setAirportD(startAirport);
 				
 			// Save in the seession
 			ctx.setAttribute("assign", ai, SESSION);
-			ctx.setAttribute("entries", entries, REQUEST);
+			ctx.setAttribute("entries", flightEntries, REQUEST);
 			ctx.setAttribute("criteria", criteria, REQUEST);
+			ctx.setAttribute("totalLegs", Integer.valueOf(totalLegs), REQUEST);
 		} catch (DAOException de) {
 			throw new CommandException(de);
 		} finally {
