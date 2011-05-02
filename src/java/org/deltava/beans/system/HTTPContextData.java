6ce66d1b1dc9643705de78cbf5893b0b2b2d4e1c
Luke Kolin <luke@deltava.org>
2011-05-02 05:11:52 +0000

    Browser info object
    git-svn-id: file:///home/svn/DVA2006@7429 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/system/HTTPContextData.java
@@ -0,0 +1,106 @@
+// Copyright 2011 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.system;
+
+/**
+ * A bean to store data about the current HTTP request. 
+ * @author Luke
+ * @version 3.7
+ * @since 3.7
+ */
+
+public class HTTPContextData implements java.io.Serializable {
+	
+	private String _userAgent;
+	
+	private OperatingSystem _os;
+	private BrowserType _browser;
+	
+	private int _major;
+	private int _minor;
+	private boolean _html5;
+
+	/**
+	 * Creates the context data.
+	 * @param os the client OperatingSystem
+	 * @param browser the client browser type 
+	 */
+	public HTTPContextData(OperatingSystem os, BrowserType browser) {
+		super();
+		_os = os;
+		_browser = browser;
+	}
+
+	/**
+	 * Returns the client operating system.
+	 * @return an OperatingSystem enum
+	 */
+	public OperatingSystem getOperatingSystem() {
+		return _os;
+	}
+	
+	/**
+	 * Returns the client browser type.
+	 * @return a BrowserType enum
+	 */
+	public BrowserType getBrowserType() {
+		return _browser;
+	}
+
+	/**
+	 * Returns the browser major version number.
+	 * @return the major version
+	 */
+	public int getMajor() {
+		return _major;
+	}
+	
+	/**
+	 * Returns the browser minor version number.
+	 * @return the minor version
+	 */
+	public int getMinor() {
+		return _minor;
+	}
+	
+	/**
+	 * Returns whether the client browser has sufficient HTML5 support.
+	 * @return TRUE if full HTML5 support, otherwise FALSE
+	 */
+	public boolean getHTML5() {
+		return _html5;
+	}
+	
+	/**
+	 * Returns the browser's user-agent header.
+	 * @return the user-agent header
+	 */
+	public String getUserAgent() {
+		return _userAgent;
+	}
+
+	/**
+	 * Sets whether the client browser has sufficient HTML5 support.
+	 * @param hasHTML5 TRUE if full HTML5 support, otherwise FALSE
+	 */
+	public void setHTML5(boolean hasHTML5) {
+		_html5 = hasHTML5;
+	}
+	
+	/**
+	 * Sets the browser version.
+	 * @param major the major version number
+	 * @param minor the minor version number
+	 */
+	public void setVersion(int major, int minor) {
+		_major = Math.max(1, major);
+		_minor = Math.max(0, minor);
+	}
+	
+	/**
+	 * Updates the browser's user-agent header.
+	 * @param ua the user-agent header
+	 */
+	public void setUserAgent(String ua) {
+		_userAgent = ua;
+	}
+}
---
+++ src/java/org/deltava/beans/system/OperatingSystem.java
@@ -0,0 +1,39 @@
+// Copyright 2011 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.system;
+
+import java.util.*;
+
+/**
+ * An enumeration to store client operating system data.
+ * @author Luke
+ * @version 3.7
+ * @since 3.7
+ */
+
+public enum OperatingSystem {
+	
+	WINDOWS("Windows"), OSX("MAC OS"), LINUX("Linux", "BSD"), IOS("iPad", "iPhone", "iPod"), UNKNOWN;
+	
+	private final Collection<String> _detectStrings = new LinkedHashSet<String>(4);
+	
+	OperatingSystem(String... detectStrings) {
+		_detectStrings.addAll(Arrays.asList(detectStrings));
+	}
+	
+	/**
+	 * Detects an operating system based on a user agent header.
+	 * @param userAgent the user agent header value
+	 * @return an OperatingSystem
+	 */
+	public static OperatingSystem detect(String userAgent) {
+		for (int x = 0; x < OperatingSystem.values().length - 1; x++) {
+			OperatingSystem os = OperatingSystem.values()[x];
+			for (String keyword : os._detectStrings) {
+				if (userAgent.contains(keyword))
+					return os;
+			}
+		}
+		
+		return OperatingSystem.UNKNOWN;
+	}
+}
