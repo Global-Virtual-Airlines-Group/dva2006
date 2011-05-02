fd15bfa6ff00d11551094f7f709a6e87878ea7ef
Luke Kolin <luke@deltava.org>
2011-05-02 05:11:52 +0000

    Browser info object
    git-svn-id: file:///home/svn/DVA2006@7429 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/system/BrowserType.java
@@ -0,0 +1,86 @@
+// Copyright 2011 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.system;
+
+import java.util.*;
+
+/**
+ * An enumeration to store browser family data.
+ * @author Luke
+ * @version 3.7
+ * @since 3.7
+ */
+
+public enum BrowserType {
+	
+	IE("MSIE"), CHROME("Chrome"), FIREFOX("Firefox"), WEBKIT("WebKit", "Safari"), OPERA("Opera"), UNKNOWN;
+	
+	private final Collection<String> _detectStrings = new ArrayList<String>(4);
+	
+	public static class BrowserVersion {
+		private BrowserType _browser;
+		private String _version;
+		
+		BrowserVersion() {
+			this(UNKNOWN, "0.0");
+		}
+		
+		BrowserVersion(BrowserType bt, String version) {
+			super();
+			_browser = bt;
+			_version = version;
+		}
+		
+		public BrowserType getType() {
+			return _browser;
+		}
+		
+		public String getVersion() {
+			return _version;
+		}
+	}
+
+	BrowserType(String... detectStrings) {
+		_detectStrings.addAll(Arrays.asList(detectStrings));
+	}
+	
+	/**
+	 * Detects a browser family based on a user agent header.
+	 * @param userAgent the user agent header value
+	 * @return a BrowserType
+	 */
+	public static BrowserVersion detect(String userAgent) {
+		if (userAgent == null)
+			return new BrowserVersion();
+		
+		for (int x = 0; x < BrowserType.values().length - 1; x++) {
+			BrowserType bType = BrowserType.values()[x];
+			for (String keyword : bType._detectStrings) {
+				int pos = userAgent.indexOf(keyword);
+				if (pos != -1) {
+					pos += keyword.length();
+					
+					// Get version string
+					StringBuilder buf = new StringBuilder();
+					boolean hasMinor = false;
+					for (int y = pos + 1; y < userAgent.length(); y++) {
+						char c = userAgent.charAt(y);
+						if (Character.isDigit(c))
+							buf.append(c);
+						else if (!hasMinor && (c == '.')) {
+							hasMinor = true;
+							buf.append('.');
+						} else
+							break;
+					}
+					
+					if (!hasMinor)
+						buf.append(".0");
+					
+					return new BrowserVersion(bType, buf.toString());
+				}
+			}
+		}
+		
+		return new BrowserVersion();
+	}
+}
