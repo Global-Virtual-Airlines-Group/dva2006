4453be4fa75c06803a9d812b1b9fc0c004fb841f
Luke Kolin <luke@deltava.org>
2011-05-31 14:07:01 +0000

    Extract FuelTank into DVA enumeration
    git-svn-id: file:///home/svn/DVA2006@7508 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/test/org/deltava/beans/schedule/TestFuelTanks.java
@@ -0,0 +1,33 @@
+package org.deltava.beans.schedule;
+
+import junit.framework.TestCase;
+
+public class TestFuelTanks extends TestCase {
+
+	private static final String[] TANK_NAMES = {"Center", "Left Main", "Left Aux", "Left Tip", "Right Main",
+		"Right Aux", "Right Tip", "Center 2", "Center 3", "External", "External 2" };
+	
+	public void testTankNames() {
+		for (int x = 0; x < FuelTank.values().length; x++) {
+			FuelTank t = FuelTank.get(TANK_NAMES[x]);
+			assertNotNull(t);
+		}
+	}
+	
+	public void testTankIDs() {
+		for (int x = 0; x < FuelTank.values().length; x++) {
+			FuelTank t = FuelTank.values()[x];
+			assertNotNull(t);
+		}
+	}
+	
+	public void testFailure() {
+		try {
+			FuelTank t = FuelTank.get("crap");
+			assertNull(t);
+			fail("IllegalArgumentException expected");
+		} catch (IllegalArgumentException iae) {
+			// empty
+		}
+	}
+}
