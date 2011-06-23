920feb6f2bcb6a2732b1b9877224a08e26da479b
Luke Kolin <luke@deltava.org>
2011-06-23 21:25:46 +0000

    Initial missed airports work
    git-svn-id: file:///home/svn/DVA2006@7542 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/commands/stats/NewAirportListCommand.java
@@ -0,0 +1,97 @@
+// Copyright 2011 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.commands.stats;
+
+import java.util.*;
+import java.sql.Connection;
+
+import org.deltava.beans.Pilot;
+import org.deltava.beans.schedule.*;
+
+import org.deltava.commands.*;
+import org.deltava.dao.*;
+
+import org.deltava.util.system.SystemData;
+
+/**
+ * A Web Site Command to display airports a Pilot has not flown to.
+ * @author Luke
+ * @version 4.0
+ * @since 4.0
+ */
+
+public class NewAirportListCommand extends AbstractCommand {
+
+	/**
+	 * Executes the command.
+	 * @param ctx the Command context
+	 * @throws CommandException if an unhandled error occurs
+	 */
+	@Override
+	public void execute(CommandContext ctx) throws CommandException {
+		
+		// Get the default airline
+		String defaultCode = SystemData.get("airline.code");
+		Collection<Airport> myAirports = new HashSet<Airport>();
+		
+		// Get the user ID
+		int userID = ctx.getUser().getID();
+		if ((ctx.isUserInRole("PIREP") || ctx.isUserInRole("HR")) && (ctx.getID() != 0))
+			userID = ctx.getID();
+		
+		try {
+			Connection con = ctx.getConnection();
+			
+			// Get the pilot
+			GetPilot pdao = new GetPilot(con);
+			Pilot p = pdao.get(userID);
+			if (p == null)
+				throw notFoundException("Invalid Pilot ID - " + userID);
+			
+			ctx.setAttribute("pilot", p, REQUEST);
+
+			// Load airports
+			GetFlightReports frdao = new GetFlightReports(con);
+			Collection<RoutePair> routes = frdao.getRoutePairs(userID);
+			for (RoutePair rp : routes) {
+				myAirports.add(rp.getAirportD());
+				myAirports.add(rp.getAirportA());
+			}
+		} catch (DAOException de) {
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
+		
+		// Get all airports and remove those we've visited or which aren't active
+		Map<Airline, Collection<Airport>> airports = new TreeMap<Airline, Collection<Airport>>();
+		Collection<Airport> allAirports = new TreeSet<Airport>(SystemData.getAirports().values());
+		for (Airport a : allAirports) {
+			if (myAirports.contains(a) || a.getAirlineCodes().isEmpty())
+				continue;
+			
+			// Sort the airlines for the airport, default first
+			Collection<String> apAirlines = new LinkedHashSet<String>();
+			if (a.hasAirlineCode(defaultCode))
+				apAirlines.add(defaultCode);
+			apAirlines.addAll(a.getAirlineCodes());
+			
+			// Get the first airline and its group
+			Airline al = SystemData.getAirline(apAirlines.iterator().next());
+			Collection<Airport> aps = airports.get(al);
+			if (aps == null) {
+				aps = new LinkedHashSet<Airport>();
+				airports.put(al, aps);
+			}
+			
+			aps.add(a);
+		}
+		
+		// Save in request
+		ctx.setAttribute("allAirports", airports, REQUEST);
+		
+		// Forward to the JSP
+		CommandResult result = ctx.getResult();
+		result.setURL("/jsp/stats/missedAirports.jsp");
+		result.setSuccess(true);
+	}
+}
