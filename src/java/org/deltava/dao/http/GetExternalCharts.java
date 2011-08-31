2903061382ed8b8498bf3f1029ec50d2e3e46ef1
Luke Kolin <luke@deltava.org>
2011-08-31 19:58:56 +0000

    AirCharts API support
    git-svn-id: file:///home/svn/DVA2006@7659 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/dao/http/GetExternalCharts.java
@@ -0,0 +1,67 @@
+// Copyright 2011 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.dao.http;
+
+import java.net.*;
+import java.util.Date;
+import java.io.IOException;
+
+import org.deltava.beans.schedule.ExternalChart;
+
+import org.deltava.dao.DAOException;
+
+import org.deltava.util.StringUtils;
+
+/**
+ * A Data Access Object to handle external chart redirects.
+ * @author Luke
+ * @version 4.0
+ * @since 4.0
+ */
+
+public class GetExternalCharts extends DAO {
+	
+	/**
+	 * Validates and determines the proper size for an external chart.
+	 * @param c an ExternalChart
+	 * @throws DAOException if an error occurs
+	 */
+	public void populate(ExternalChart c) throws DAOException {
+		if (!StringUtils.isEmpty(c.getURL()) || c.isLoaded())
+			return;
+		
+		try {
+			URL u = new URL(c.getURL());
+			HttpURLConnection urlCon = (HttpURLConnection) u.openConnection();
+			urlCon.setRequestMethod("HEAD");
+			
+			// Fetch the data
+			int rspCode = urlCon.getResponseCode();
+			if (rspCode != HttpURLConnection.HTTP_OK)
+				throw new DAOException("Unknown Status Code - " + rspCode);
+
+			// Get size
+			c.setSize(urlCon.getContentLength());
+			c.setLastModified(new Date(urlCon.getLastModified()));
+		} catch (IOException ie) {
+			throw new DAOException(ie);
+		}
+	}
+	
+	/**
+	 * Loads an external chart.
+	 * @param c an ExternalChart
+	 * @throws DAOException if an error occurs
+	 */
+	public void load(ExternalChart c) throws DAOException {
+		if (!StringUtils.isEmpty(c.getURL()) || c.isLoaded())
+			return;
+		
+		try {
+			init(c.getURL());
+			c.load(getIn());
+			c.setLastModified(new Date());
+		} catch (IOException ie) {
+			throw new DAOException(ie);
+		}
+	}
+}
