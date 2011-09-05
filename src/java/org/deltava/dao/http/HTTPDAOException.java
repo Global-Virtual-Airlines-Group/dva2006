a888876de9885c7f764298c33e78abbe6c627f96
Luke Kolin <luke@deltava.org>
2011-09-05 22:01:58 +0000

    Add HTTPDAOException
    git-svn-id: file:///home/svn/DVA2006@7672 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/dao/http/HTTPDAOException.java
@@ -0,0 +1,42 @@
+// Copyright 2011 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.dao.http;
+
+import org.deltava.dao.DAOException;
+
+/**
+ * An Exception thrown by HTTP Data Access Objects.
+ * @author Luke
+ * @version 4.0
+ * @since 4.0
+ */
+
+public class HTTPDAOException extends DAOException {
+	
+	private int _statusCode;
+
+	/**
+	 * Creates a new DAO Exception.
+	 * @param t the inner Exception
+	 */
+	HTTPDAOException(Throwable t) {
+		super(t);
+	}
+
+	/**
+	 * Creates a new DAO Exception.
+	 * @param msg the message
+	 * @param code the HTTP status code
+	 */
+	HTTPDAOException(String msg, int code) {
+		super(msg + " - " + code);
+		_statusCode = code;
+	}
+
+	/**
+	 * Returns the HTTP status code.
+	 * @return the status code, or zero if not set
+	 */
+	public int getStatusCode() {
+		return _statusCode;
+	}
+}
