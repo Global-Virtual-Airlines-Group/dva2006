f0b22d78a162e1eb5eceaecda91c79be40494c45
Luke Kolin <luke@deltava.org>
2011-10-25 03:52:29 +0000

    Java 7 compatability
    git-svn-id: file:///home/svn/DVA2006@7719 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/servlet/WebServiceServlet.java
+++ src/java/org/deltava/servlet/WebServiceServlet.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2007, 2008 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2007, 2008, 2011 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.servlet;
 
 import java.util.*;
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
+import javax.servlet.annotation.MultipartConfig;
 
 import org.apache.log4j.Logger;
 
@@ -20,10 +21,11 @@ import org.deltava.util.system.SystemData;
 /**
  * A servlet to handle Web Service data requests.
  * @author Luke
- * @version 2.3
+ * @version 4.1
  * @since 1.0
  */
 
+@MultipartConfig
 public class WebServiceServlet extends BasicAuthServlet {
 
 	// Web services realm
