55b0722b4bf9d42b3d0e69441d6dd1564be06a10
Luke Kolin <luke@deltava.org>
2011-11-24 02:04:01 +0000

    Save ILS category for METARs
    git-svn-id: file:///home/svn/DVA2006@7786 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/test/org/deltava/beans/flight/TestILSCategory.java
@@ -0,0 +1,12 @@
+package org.deltava.beans.flight;
+
+import junit.framework.TestCase;
+
+public class TestILSCategory extends TestCase {
+	
+	public void testCategory() {
+		
+		assertEquals(ILSCategory.NONE, ILSCategory.categorize(3000, 4500));
+		assertEquals(ILSCategory.CATI, ILSCategory.categorize(1000, 2500));
+	}
+}
