0e0623844b3beac859ee12b6f1c1c27c769791d8
Luke Kolin <luke@deltava.org>
2012-02-09 20:27:42 +0000

    Build 397
    git-svn-id: file:///home/svn/DVA2006@7950 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -13,7 +13,7 @@ public interface VersionInfo {
     public static final int MAJOR = 4;
     public static final int MINOR = 1;
     
-    public static final int BUILD = 396;
+    public static final int BUILD = 397;
     public static final boolean FINAL = false;
     
     //public static final String BUILD_DATE="$BUILD";
