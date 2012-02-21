673243c84aabd21b65eba20b03a33ef86b01eb8f
Luke Kolin <luke@deltava.org>
2012-02-21 15:29:58 +0000

    Misc time fixes and temporary logging
    git-svn-id: file:///home/svn/DVA2006@7970 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/acars/XAFlightInfo.java
+++ src/java/org/deltava/beans/acars/XAFlightInfo.java
@@ -85,7 +85,7 @@ public class XAFlightInfo extends FDRFlightReport {
 	}
 	
     public final int getLength() {
-    	return (super.getLength() != 0) ? super.getLength() : (int)((getStartTime().getTime() - getEndTime().getTime()) / 3600000);
+    	return (int)((getStartTime().getTime() - getEndTime().getTime()) / 360000);
     }
 	
 	/**
