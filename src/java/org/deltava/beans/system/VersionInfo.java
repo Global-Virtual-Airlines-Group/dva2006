fb82fa4579308137e18b48c2bbda5800baebaf2b
Luke Kolin <luke@deltava.org>
2012-03-05 00:07:14 +0000

    Build 401
    git-svn-id: file:///home/svn/DVA2006@7989 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -13,7 +13,7 @@ public interface VersionInfo {
     public static final int MAJOR = 4;
     public static final int MINOR = 1;
     
-    public static final int BUILD = 400;
+    public static final int BUILD = 401;
     public static final boolean FINAL = false;
     
     //public static final String BUILD_DATE="$BUILD";
