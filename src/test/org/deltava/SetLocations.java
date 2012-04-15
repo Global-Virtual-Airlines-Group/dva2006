d0d1b6152a81e800c821cc367c733ba27a2165ea
Luke Kolin <luke@deltava.org>
2012-04-15 14:57:00 +0000

    Use new geocode
    git-svn-id: file:///home/svn/DVA2006@8055 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/SetLocations.java
+++ src/test/org/deltava/SetLocations.java
@@ -22,28 +22,37 @@ public class SetLocations extends TestCase {
 
 	private static Logger log;
 
-	private static final String JDBC_URL = "jdbc:mysql://pollux.gvagroup.org/dva";
+	private static final String JDBC_URL = "jdbc:mysql://cerberus.gvagroup.org/dva?useUnicode=true&characterEncoding=UTF-8";
+	private static final String JDBC2_URL = "jdbc:mysql://polaris.sce.net/dva?useUnicode=true&characterEncoding=UTF-8";
 
 	private Connection _c;
+	private Connection _c2;
 
 	protected void setUp() throws Exception {
 		super.setUp();
 
 		// Init Log4j
-		PropertyConfigurator.configure("etc/log4j.properties");
+		PropertyConfigurator.configure("etc/log4j.test.properties");
 		log = Logger.getLogger(SetLocations.class);
 
 		SystemData.init();
 
 		// Connect to the database
 		Class.forName("com.mysql.jdbc.Driver");
-		_c = DriverManager.getConnection(JDBC_URL, "import", "import");
+		DriverManager.setLoginTimeout(5);
+		_c = DriverManager.getConnection(JDBC_URL, "luke", "luke");
 		assertNotNull(_c);
 		_c.setAutoCommit(false);
 		assertFalse(_c.getAutoCommit());
+		
+		_c2 = DriverManager.getConnection(JDBC2_URL, "luke", "14072");
+		assertNotNull(_c2);
+		_c2.setAutoCommit(false);
+		assertFalse(_c2.getAutoCommit());
 	}
 
 	protected void tearDown() throws Exception {
+		_c2.close();
 		_c.close();
 		LogManager.shutdown();
 		super.tearDown();
@@ -52,7 +61,8 @@ public class SetLocations extends TestCase {
 	public void testLoadLocations() throws Exception {
 
 		// Get locations and Pilots
-		GetPilot pdao = new GetPilot(_c);
+		GetPilot pdao = new GetPilot(_c2);
+		//GetPilot pdao2 = new GetPilot(_c);
 		GetPilotBoard pbdao = new GetPilotBoard(_c);
 		SetPilot pwdao = new SetPilot(_c);
 		Map<Integer, GeoLocation> locs = pbdao.getAll();
@@ -62,19 +72,21 @@ public class SetLocations extends TestCase {
 			Map.Entry<Integer, GeoLocation> e = i.next();
 			GeoLocation loc = e.getValue();
 			Pilot usr = pdao.get(e.getKey().intValue());
+			usr.setLocation(null);
 
 			// Read via the DAO
 			if (StringUtils.isEmpty(usr.getLocation())) {
+				GeocodeResult gr = null;
 				try {
 					GetGoogleGeocode gcdao = new GetGoogleGeocode();
-					GeocodeResult gr = gcdao.getGeoData(loc);
+					gr = gcdao.getGeoData(loc);
 					int distance = GeoUtils.distance(loc, gr);
-					if (distance < 30) {
-						log.info("Setting " + e.getKey() + " home town to " + gr.getCityState());
+					if ((distance < 30) && (gr != null)) {
+						log.warn("Setting " + usr.getName() + " home town to " + gr.getCityState());
 						pwdao.setHomeTown(e.getKey().intValue(), gr);
 					}
 				} catch (Exception ex) {
-					log.error("Cannot update " + e.getKey() + " - " + ex.getMessage());
+					log.error("Cannot update " + e.getKey() + " - " + ex.getMessage(), ex);
 				}
 			}
 		}
