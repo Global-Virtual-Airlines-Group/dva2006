e7428dbfac45f17609c9dbd26cf791260f536678
Luke Kolin <luke@deltava.org>
2012-05-06 15:00:30 +0000

    Fix list pruning to make scrollbar visible in JSP
    git-svn-id: file:///home/svn/DVA2006@8078 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/cooler/MyThreadsCommand.java
+++ src/java/org/deltava/commands/cooler/MyThreadsCommand.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2009, 2011 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2009, 2011, 2012 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.cooler;
 
 import java.util.*;
@@ -18,89 +18,90 @@ import org.deltava.util.system.SystemData;
 /**
  * A Web Site Command to display a user's Water Cooler Threads.
  * @author Luke
- * @version 3.6
+ * @version 4.2
  * @since 1.0
  */
 
 public class MyThreadsCommand extends AbstractViewCommand {
 
-   /**
+	/**
 	 * Executes the command.
 	 * @param ctx the Command context
 	 * @throws CommandException if an unhandled error occurs
 	 */
-   public void execute(CommandContext ctx) throws CommandException {
+	@Override
+	public void execute(CommandContext ctx) throws CommandException {
 
 		// Get the default airline
-      AirlineInformation airline = SystemData.getApp(SystemData.get("airline.code"));
-      
-      // Get/set start/count parameters
-      ViewContext vc = initView(ctx);
-      try {
-         Connection con = ctx.getConnection();
-         
-         // Get the DAO and the Pilot's airline
-         GetUserData uddao = new GetUserData(con);
-         UserData usrData = uddao.get(ctx.getUser().getID());
-         if (usrData != null)
-        	 airline = SystemData.getApp(usrData.getAirlineCode());
+		AirlineInformation airline = SystemData.getApp(SystemData.get("airline.code"));
 
-         // Get the channel DAO and the list of channels
-         GetCoolerChannels dao = new GetCoolerChannels(con);
-         ctx.setAttribute("channels", dao.getChannels(airline, ctx.getRoles()), REQUEST);
+		// Get/set start/count parameters
+		ViewContext vc = initView(ctx);
+		try {
+			Connection con = ctx.getConnection();
 
-         // Get the Message Thread DAO
-         GetCoolerThreads dao2 = new GetCoolerThreads(con);
-         dao2.setQueryStart(vc.getStart());
-         dao2.setQueryMax(Math.round(vc.getCount() * 1.25f));
+			// Get the DAO and the Pilot's airline
+			GetUserData uddao = new GetUserData(con);
+			UserData usrData = uddao.get(ctx.getUser().getID());
+			if (usrData != null)
+				airline = SystemData.getApp(usrData.getAirlineCode());
 
-         // Initialize the access controller
-         CoolerThreadAccessControl ac = new CoolerThreadAccessControl(ctx);
+			// Get the channel DAO and the list of channels
+			GetCoolerChannels dao = new GetCoolerChannels(con);
+			ctx.setAttribute("channels", dao.getChannels(airline, ctx.getRoles()), REQUEST);
 
-         // Get either by channel or all; now filter by role
-         Collection<Integer> pilotIDs = new HashSet<Integer>();
-         List<MessageThread> threads = dao2.getByAuthor(ctx.getUser().getID(), ctx.getUser().getShowSSThreads());
-         for (Iterator<MessageThread> i = threads.iterator(); i.hasNext();) {
-            MessageThread thread = i.next();
-            
-            // Get this thread's channel and see if we can read it
-            Channel c = dao.get(thread.getChannel());
-            ac.updateContext(thread, c);
-            ac.validate();
+			// Get the Message Thread DAO
+			GetCoolerThreads dao2 = new GetCoolerThreads(con);
+			dao2.setQueryStart(vc.getStart());
+			dao2.setQueryMax(Math.round(vc.getCount() * 1.25f));
 
-            // If we cannot read the thread, remove it from the results, otherwise load the pilot profiles
-            if (ac.getCanRead()) {
-                pilotIDs.add(new Integer(thread.getAuthorID()));
-                pilotIDs.add(new Integer(thread.getLastUpdateID()));
-            } else
-               i.remove();
-         }
-         
-         // Remove excess entires
-         if (threads.size() > vc.getCount())
-        	 threads.subList(vc.getCount() - 1, threads.size()).clear();
-         
-         // Get the location of all the Pilots
-         UserDataMap udm = uddao.get(pilotIDs);
-         GetPilot pdao = new GetPilot(con);
-         ctx.setAttribute("userData", udm, REQUEST);
-         ctx.setAttribute("pilots", pdao.get(udm), REQUEST);
+			// Initialize the access controller
+			CoolerThreadAccessControl ac = new CoolerThreadAccessControl(ctx);
 
-         // Save in the view context
-         vc.setResults(threads);
-      } catch (DAOException de) {
-         throw new CommandException(de);
-      } finally {
-         ctx.release();
-      }
-      
+			// Get either by channel or all; now filter by role
+			Collection<Integer> pilotIDs = new HashSet<Integer>();
+			List<MessageThread> threads = dao2.getByAuthor(ctx.getUser().getID(), ctx.getUser().getShowSSThreads());
+			for (Iterator<MessageThread> i = threads.iterator(); i.hasNext();) {
+				MessageThread thread = i.next();
+
+				// Get this thread's channel and see if we can read it
+				Channel c = dao.get(thread.getChannel());
+				ac.updateContext(thread, c);
+				ac.validate();
+
+				// If we cannot read the thread, remove it from the results, otherwise load the pilot profiles
+				if (ac.getCanRead()) {
+					pilotIDs.add(Integer.valueOf(thread.getAuthorID()));
+					pilotIDs.add(Integer.valueOf(thread.getLastUpdateID()));
+				} else
+					i.remove();
+			}
+
+			// Remove excess entires
+			if (threads.size() > vc.getCount())
+				threads.subList(vc.getCount(), threads.size()).clear();
+
+			// Get the location of all the Pilots
+			UserDataMap udm = uddao.get(pilotIDs);
+			GetPilot pdao = new GetPilot(con);
+			ctx.setAttribute("userData", udm, REQUEST);
+			ctx.setAttribute("pilots", pdao.get(udm), REQUEST);
+
+			// Save in the view context
+			vc.setResults(threads);
+		} catch (DAOException de) {
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
+
 		// Set channel/command name attributes
-      ctx.setAttribute("viewCmd", getID(), REQUEST);
-      ctx.setAttribute("channelName", "My Discussion Threads", REQUEST);
+		ctx.setAttribute("viewCmd", getID(), REQUEST);
+		ctx.setAttribute("channelName", "My Discussion Threads", REQUEST);
 
-      // Forward to the JSP
-      CommandResult result = ctx.getResult();
-      result.setURL("/jsp/cooler/threadList.jsp");
-      result.setSuccess(true);
-   }
+		// Forward to the JSP
+		CommandResult result = ctx.getResult();
+		result.setURL("/jsp/cooler/threadList.jsp");
+		result.setSuccess(true);
+	}
 }
