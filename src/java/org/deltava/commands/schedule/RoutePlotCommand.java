15cdc8dcd7c355e0137af17e310224d576054c40
Luke Kolin <luke@deltava.org>
2012-05-14 00:49:33 +0000

    Add alternate support
    git-svn-id: file:///home/svn/DVA2006@8089 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/schedule/RoutePlotCommand.java
+++ src/java/org/deltava/commands/schedule/RoutePlotCommand.java
@@ -1,9 +1,10 @@
-// Copyright 2005, 2007, 2008, 2009, 2010 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2007, 2008, 2009, 2010, 2012 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.schedule;
 
 import java.util.*;
 
 import org.deltava.commands.*;
+import org.deltava.dao.*;
 
 import org.deltava.util.ComboUtils;
 import org.deltava.util.system.SystemData;
@@ -11,7 +12,7 @@ import org.deltava.util.system.SystemData;
 /**
  * A Web Site Command to plot a flight route.
  * @author Luke
- * @version 2.8
+ * @version 4.2
  * @since 1.0
  */
 
@@ -25,12 +26,22 @@ public class RoutePlotCommand extends AbstractCommand {
 	 * @param ctx the Command context
 	 * @throws CommandException if an unhandled error occurs
 	 */
+	@Override
 	public void execute(CommandContext ctx) throws CommandException {
 
 		// Set request attributes
 		ctx.setAttribute("emptyList", Collections.emptyList(), REQUEST);
 		ctx.setAttribute("simVersions", SIM_VERSIONS, REQUEST);
 		ctx.setAttribute("airlines", SystemData.getAirlines().values(), REQUEST);
+		
+		try {
+			GetAircraft acdao = new GetAircraft(ctx.getConnection());
+			ctx.setAttribute("eqTypes", acdao.getAircraftTypes(SystemData.get("airline.code")), REQUEST);
+		} catch (DAOException de) {
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
 
 		// Forward to the JSP
 		CommandResult result = ctx.getResult();
