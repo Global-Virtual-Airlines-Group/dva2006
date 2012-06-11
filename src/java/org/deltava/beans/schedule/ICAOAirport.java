8848b26662f359fc278a4560c26a4a68e5110334
Luke Kolin <luke@deltava.org>
2012-06-11 20:04:23 +0000

    Add magnetic variation to ICAOAirport
    git-svn-id: file:///home/svn/DVA2006@8104 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/schedule/ICAOAirport.java
+++ src/java/org/deltava/beans/schedule/ICAOAirport.java
@@ -1,4 +1,4 @@
-// Copyright 2009 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2009, 2012 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.schedule;
 
 import org.deltava.beans.GeospaceLocation;
@@ -6,7 +6,7 @@ import org.deltava.beans.GeospaceLocation;
 /**
  * An interface to mark airport beans with an ICAO code. 
  * @author Luke
- * @version 2.7
+ * @version 4.2
  * @since 2.7
  */
 
@@ -17,4 +17,10 @@ public interface ICAOAirport extends GeospaceLocation {
 	 * @return the ICAO code
 	 */
 	public String getICAO();
+	
+	/**
+	 * Returns the magnetic variation at this airport.
+	 * @return the variation in degrees
+	 */
+	public double getMagVar();
 }
