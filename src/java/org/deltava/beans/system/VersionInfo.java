f8d5437781aeffabeb0db3e5d2687924569e5086
Luke Kolin <luke@deltava.org>
2012-07-04 14:51:32 +0000

    Build 414
    git-svn-id: file:///home/svn/DVA2006@8146 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -13,7 +13,7 @@ public interface VersionInfo {
     public static final int MAJOR = 4;
     public static final int MINOR = 2;
     
-    public static final int BUILD = 413;
+    public static final int BUILD = 414;
     public static final boolean FINAL = false;
     
     //public static final String BUILD_DATE="$BUILD";
