eba0cc298182f857660d1d1367af2a928c821ddd
Luke Kolin <luke@deltava.org>
2012-07-05 22:50:04 +0000

    Fix premature stream closing
    git-svn-id: file:///home/svn/DVA2006@8157 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/service/navdata/XMLTerminalRouteService.java
+++ src/java/org/deltava/service/navdata/XMLTerminalRouteService.java
@@ -35,11 +35,11 @@ public class XMLTerminalRouteService extends DispatchDataService {
 	 */
 	@Override
 	public int execute(ServiceContext ctx) throws ServiceException {
-		
+
 		// Ensure we are a dispatcher
 		if (!ctx.isUserInRole("Pilot"))
 			throw error(SC_UNAUTHORIZED, "Not in Pilot role", false);
-		
+
 		// Check the cache
 		File f = _dataCache.get("XMLZIPSIDSTAR");
 		if (f != null) {
@@ -49,7 +49,7 @@ public class XMLTerminalRouteService extends DispatchDataService {
 			sendFile(f, ctx.getResponse());
 			return SC_OK;
 		}
-		
+
 		// Get the DAO and the SIDs/STARs
 		Collection<TerminalRoute> routes = null;
 		try {
@@ -60,75 +60,74 @@ public class XMLTerminalRouteService extends DispatchDataService {
 		} finally {
 			ctx.release();
 		}
-		
+
 		// Write to a temp file
 		File cacheDir = new File(SystemData.get("schedule.cache"));
 		f = new File(cacheDir, "sidstar.zip");
-		
+
 		// Format routes
-		final NumberFormat df = new DecimalFormat("#0.000000"); 
+		final NumberFormat df = new DecimalFormat("#0.000000");
 		try {
-			Document doc = null; String lastAirport = null;
+			Document doc = null;
+			String lastAirport = null;
 			Element re = new Element("routes");
-			ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(f));
-			for (Iterator<TerminalRoute> i = routes.iterator(); i.hasNext(); ) {
-				TerminalRoute tr = i.next();
-				boolean isNewAirport = !tr.getICAO().equals(lastAirport);
-				if (isNewAirport) {
-					if (doc != null) {
-						try (PrintWriter pw = new PrintWriter(zout)) {
-							pw.println(XMLUtils.format(doc, "UTF-8"));
-							pw.flush();
-						}
-					}
-					
-					lastAirport = tr.getICAO();
-					ZipEntry ze = new ZipEntry("ss_" + tr.getICAO() + ".xml");
-					ze.setMethod(ZipEntry.DEFLATED);
-					doc = new Document();
-					re = new Element("routes");
-					doc.setRootElement(re);
-					zout.putNextEntry(ze);
-				}
-				
-				// Create the SID/STAR element
-				Element tre = new Element(tr.getTypeName().toLowerCase());
-				tre.setAttribute("name", tr.getName());
-				tre.setAttribute("id", tr.getCode());
-				tre.setAttribute("transition", tr.getTransition());
-				tre.setAttribute("runway", tr.getRunway());
-				re.addContent(tre);
-				
-				// Add the waypoint elements
-				int mrkID = 0; 
-				for (Iterator<NavigationDataBean> ii = tr.getWaypoints().iterator(); ii.hasNext(); ) {
-					NavigationDataBean ai = ii.next();
-					Element we = new Element("wp");
-					we.setAttribute("code", ai.getCode());
-					we.setAttribute("idx", String.valueOf(++mrkID));
-					we.setAttribute("lat", df.format(ai.getLatitude()));
-					we.setAttribute("lon", df.format(ai.getLongitude()));
-					we.setAttribute("type", ai.getTypeName());
-					if (ai.getRegion() != null)
-						we.setAttribute("region", ai.getRegion());
-					tre.addContent(we);
-				}
-				
-				i.remove();
-			}
-			
-			// Write the last entry
-			if (doc != null) {
+			try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(f))) {
 				try (PrintWriter pw = new PrintWriter(zout)) {
-					pw.println(XMLUtils.format(doc, "UTF-8"));
-					pw.flush();
+					for (Iterator<TerminalRoute> i = routes.iterator(); i.hasNext();) {
+						TerminalRoute tr = i.next();
+						boolean isNewAirport = !tr.getICAO().equals(lastAirport);
+						if (isNewAirport) {
+							if (doc != null) {
+								pw.println(XMLUtils.format(doc, "UTF-8"));
+								pw.flush();
+							}
+
+							lastAirport = tr.getICAO();
+							ZipEntry ze = new ZipEntry("ss_" + tr.getICAO() + ".xml");
+							ze.setMethod(ZipEntry.DEFLATED);
+							doc = new Document();
+							re = new Element("routes");
+							doc.setRootElement(re);
+							zout.putNextEntry(ze);
+						}
+
+						// Create the SID/STAR element
+						Element tre = new Element(tr.getTypeName().toLowerCase());
+						tre.setAttribute("name", tr.getName());
+						tre.setAttribute("id", tr.getCode());
+						tre.setAttribute("transition", tr.getTransition());
+						tre.setAttribute("runway", tr.getRunway());
+						re.addContent(tre);
+
+						// Add the waypoint elements
+						int mrkID = 0;
+						for (Iterator<NavigationDataBean> ii = tr.getWaypoints().iterator(); ii.hasNext();) {
+							NavigationDataBean ai = ii.next();
+							Element we = new Element("wp");
+							we.setAttribute("code", ai.getCode());
+							we.setAttribute("idx", String.valueOf(++mrkID));
+							we.setAttribute("lat", df.format(ai.getLatitude()));
+							we.setAttribute("lon", df.format(ai.getLongitude()));
+							we.setAttribute("type", ai.getTypeName());
+							if (ai.getRegion() != null)
+								we.setAttribute("region", ai.getRegion());
+							tre.addContent(we);
+						}
+
+						i.remove();
+					}
+
+					// Write the last entry
+					if (doc != null) {
+						pw.println(XMLUtils.format(doc, "UTF-8"));
+						pw.flush();
+					}
 				}
 			}
-			
+
 			// Close the file and add to the cache
-			zout.close();
 			addCacheEntry("XMLZIPSIDSTAR", f);
-		
+
 			// Format and write
 			ctx.getResponse().setHeader("Content-disposition", "attachment; filename=xmlsidstar.zip");
 			ctx.getResponse().setContentType("application/zip");
@@ -137,10 +136,10 @@ public class XMLTerminalRouteService extends DispatchDataService {
 		} catch (Exception e) {
 			throw error(SC_INTERNAL_SERVER_ERROR, "I/O Error", false);
 		}
-		
+
 		return SC_OK;
 	}
-	
+
 	/**
 	 * Returns whether this web service requires authentication.
 	 * @return TRUE always
