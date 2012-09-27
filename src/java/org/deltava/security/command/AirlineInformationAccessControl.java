33133ee55d977ef826e7f865deae5986e7cc9595
Luke Kolin <luke@deltava.org>
2012-09-27 19:15:54 +0000

    Allow AirlineInformation edits
    git-svn-id: file:///home/svn/DVA2006@8286 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/security/command/AirlineInformationAccessControl.java
@@ -0,0 +1,64 @@
+// Copyright 2012 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.security.command;
+
+import org.deltava.beans.Pilot;
+import org.deltava.beans.system.AirlineInformation;
+
+import org.deltava.security.SecurityContext;
+
+/**
+ * An access controller for Virtual Airlines application profiles.
+ * @author Luke
+ * @version 5.0
+ * @since 5.0
+ */
+
+public class AirlineInformationAccessControl extends AccessControl {
+
+	private final AirlineInformation _aInfo;
+	
+	private boolean _canRead;
+	private boolean _canEdit;
+	
+	/**
+	 * Creates the Access Controller.
+	 * @param ai the AirlineInformation bean
+	 * @param ctx the SecurityContext
+	 */
+	public AirlineInformationAccessControl(AirlineInformation ai, SecurityContext ctx) {
+		super(ctx);
+		_aInfo = ai;
+	}
+
+	/**
+	 * Calculates access rights.
+	 */
+	@Override
+	public void validate() {
+		validateContext();
+		
+		// Detect our airline
+		if (!_ctx.isAuthenticated()) return;
+		Pilot usr = (Pilot) _ctx.getUser();
+		boolean isOurs = usr.getPilotCode().startsWith(_aInfo.getCode());
+		
+		_canRead =  _ctx.isUserInRole("HR");
+		_canEdit = isOurs && _ctx.isUserInRole("Admin"); 
+	}
+	
+	/**
+	 * Returns whether the profile can be read.
+	 * @return TRUE if readable, otherwise FALSE
+	 */
+	public boolean getCanRead() {
+		return _canRead;
+	}
+	
+	/**
+	 * Returns whether the profile can be edited.
+	 * @return TRUE if editable, otherwise FALSE
+	 */
+	public boolean getCanEdit() {
+		return _canEdit;
+	}
+}
