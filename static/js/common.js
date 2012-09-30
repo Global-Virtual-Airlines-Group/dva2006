05bf7ebc0a92f7da43d42d8276e6b114a8c294fe
Luke Kolin <luke@deltava.org>
2012-09-30 15:42:57 +0000

    Reduce namespace pollution
    git-svn-id: file:///home/svn/DVA2006@8292 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/common.js
+++ static/js/common.js
@@ -134,16 +134,16 @@ combo.selectedIndex = -1;
 return false;
 }
 
-function getChild(e, name)
+golgotha.getChild = function(e, name)
 {
 var children = e.getElementsByTagName(name);
 return (children.length == 0) ? null : children[0];
 }
 
 if (window.Element != undefined)
-	Element.prototype.getChild = function(name) { return getChild(this, name); };
+	Element.prototype.getChild = function(name) { return golgotha.getChild(this, name); };
 
-getCDATA = function(e)
+golgotha.getCDATA = function(e)
 {
 var child = e.firstChild;	
 while ((child != null) && (child.nodeType != 4))
@@ -153,7 +153,7 @@ return child;
 }
 
 if (window.Element != undefined)
-	Element.prototype.getCDATA = function() { return getCDATA(this); };
+	Element.prototype.getCDATA = function() { return golgotha.getCDATA(this); };
 
 Array.prototype.remove = function(obj) {
 for (var x = 0; x < this.length; x++) {
