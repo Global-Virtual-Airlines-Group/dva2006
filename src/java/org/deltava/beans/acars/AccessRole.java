99593766f269e46caea6afd029f8f536b7b4209c
Luke Kolin <luke@deltava.org>
2012-10-07 21:44:18 +0000

    Refactor AccessRole into its own class
    git-svn-id: file:///home/svn/DVA2006@8337 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/acars/AccessRole.java
@@ -0,0 +1,13 @@
+// Copyright 2012 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.acars;
+
+/**
+ * An enumeration of ACARS client access roles. 
+ * @author Luke
+ * @version 5.0
+ * @since 5.0
+ */
+
+public enum AccessRole {
+	CONNECT, UPLOAD;
+}
