fdb67178b23b1334ec992ac17928769bf7364114
Luke Kolin <luke@deltava.org>
2012-10-09 23:58:04 +0000

    Fix casing
    git-svn-id: file:///home/svn/DVA2006@8347 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/schedule/Aircraft.java
+++ src/java/org/deltava/beans/schedule/Aircraft.java
@@ -31,7 +31,7 @@ public class Aircraft implements Comparable<Aircraft>, Cacheable, ViewEntry {
 		}
 		
 		public String toString() {
-			return name().substring(0, 1) + name().substring(1);
+			return name().substring(0, 1) + name().substring(1).toLowerCase();
 		}
 	}
 
