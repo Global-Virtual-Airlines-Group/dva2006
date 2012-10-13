034588960883922695faec130e5702e92ba556bc
Luke Kolin <luke@deltava.org>
2012-10-13 15:53:02 +0000

    Update VATSIM tracker
    git-svn-id: file:///home/svn/DVA2006@8351 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/usageTrack.sh
+++ src/test/org/deltava/usageTrack.sh
@@ -2,11 +2,11 @@
 # VATSIM Usage tracker script
 
 JAVA_HOME=/usr/local/java
-CPATH=log4j-1.2.16.jar:jdom-1.1.1.jar:mysql-connector-java-5.1.13-bin.jar
+CPATH=log4j-1.2.17.jar:jdom-2.0.3.jar:mysql-connector-java-5.1.22-bin.jar
 INFO=/var/cache/servinfo/vatsim.info
 
 # Start the JVM
 cd /usr/local/usageTrack
-$JAVA_HOME/bin/java -server -Xmx40m -Xbootclasspath/a:$CPATH -jar usageTracker.jar $INFO >>/var/log/usageTrack.log 2>&1 &
+$JAVA_HOME/bin/java -Xmx32m -Xint -Xbootclasspath/a:$CPATH -jar usageTracker.jar $INFO >>/var/log/usageTrack.log 2>&1 &
 echo $! > /var/run/usageTrack.pid
 exit 0
