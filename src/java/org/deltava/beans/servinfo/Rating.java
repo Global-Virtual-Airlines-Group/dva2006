8768ba815404373481324626533491a280c5576f
Luke Kolin <luke@deltava.org>
2012-10-27 22:07:48 +0000

    Add DISABLED
    git-svn-id: file:///home/svn/DVA2006@8394 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/servinfo/Rating.java
+++ src/java/org/deltava/beans/servinfo/Rating.java
@@ -1,24 +1,28 @@
-// Copyright 2010 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2010, 2012 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.servinfo;
 
 /**
- * An enumeration to store VATSIM/IVAO user ratings.
+ * An enumeration to store VATSIM/IVAO/FSD user ratings.
  * @author Luke
- * @version 3.2
+ * @version 5.0
  * @since 3.2
  */
 
 public enum Rating {
-	NULL(""), OBS("Observer"), S1("Student"), S2("Senior Student"), S3("Senior Student"),
+	DISABLED("Disabled"), OBS("Observer"), S1("Student"), S2("Senior Student"), S3("Senior Student"),
 	C1("Controller"), C2("Senior Controller"), C3("Senior Controller"), I1("Instructor"),
 	I2("Senior Instructor"), I3("Senior Instructor"), SUP("Supervisor"), ADM("Administrator");
 
-	private String _desc;
+	private final String _desc;
 	
 	Rating(String desc) {
 		_desc = desc;
 	}
 	
+	/**
+	 * Returns the rating description.
+	 * @return the description
+	 */
 	public String getName() {
 		return _desc;
 	}
