dc82c2ca45755842029ef25df0cc9bc9d3e71aa5
Luke Kolin <luke@deltava.org>
2012-11-01 15:08:28 +0000

    Use try-with-resources
    git-svn-id: file:///home/svn/DVA2006@8409 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/http/GetNATs.java
+++ src/java/org/deltava/dao/http/GetNATs.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2006, 2007, 2008, 2009, 2010 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007, 2008, 2009, 2010, 2012 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao.http;
 
 import java.io.*;
@@ -11,17 +11,15 @@ import org.deltava.util.StringUtils;
 /**
  * A Data Access Object to get North Atlantic Track data.
  * @author Luke
- * @version 3.4
+ * @version 5.0
  * @since 1.0
  */
 
-public class GetNATs extends DAO implements TrackDAO {
-	
-	private static final String CRLF = System.getProperty("line.separator");
-	
-	private String _url;
+public class GetNATs extends TrackDAO {
+
+	private final String _url;
 	private String _notam;
-	
+
 	/**
 	 * Initializes the Data Access Object.
 	 * @param url the URL to fetch from
@@ -30,7 +28,7 @@ public class GetNATs extends DAO implements TrackDAO {
 		super();
 		_url = url;
 	}
-	
+
 	/**
 	 * Retrieves the NAT information.
 	 * @return a String with the formatted NAT data
@@ -39,38 +37,34 @@ public class GetNATs extends DAO implements TrackDAO {
 	public String getTrackInfo() throws DAOException {
 		try {
 			init(_url);
-			LineNumberReader br = new LineNumberReader(new InputStreamReader(getIn()));
 			StringBuilder buf = new StringBuilder();
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(getIn()))) {
+				boolean isWriting = false;
+				String data = br.readLine();
+				while (data != null) {
+					data = data.trim();
 
-			// Read through the URL results
-			boolean isWriting = false;
-			String data = br.readLine();
-			while (data != null) {
-				data = data.trim();
-				
-				// Check for start/end of NAT segment
-				if (!isWriting && data.contains("NAT-")) {
-					buf.append(data);
-					buf.append("<br />");
-					buf.append(CRLF);
-					isWriting = true;
-				} else if (isWriting && (data.startsWith("END OF PART"))) {
-					buf.append(data);
-					buf.append("<br /><hr />");
-					buf.append(CRLF);
-					isWriting = false;
-				} else if (isWriting) {
-					buf.append(data);
-					buf.append("<br />");
-					buf.append(CRLF);
+					// Check for start/end of NAT segment
+					if (!isWriting && data.contains("NAT-")) {
+						buf.append(data);
+						buf.append("<br />");
+						buf.append(CRLF);
+						isWriting = true;
+					} else if (isWriting && (data.startsWith("END OF PART"))) {
+						buf.append(data);
+						buf.append("<br /><hr />");
+						buf.append(CRLF);
+						isWriting = false;
+					} else if (isWriting) {
+						buf.append(data);
+						buf.append("<br />");
+						buf.append(CRLF);
+					}
+
+					data = br.readLine();
 				}
-
-				// Read next line
-				data = br.readLine();
 			}
 
-			// Return the data
-			br.close();
 			_notam = buf.toString();
 			return _notam;
 		} catch (IOException ie) {
@@ -86,37 +80,37 @@ public class GetNATs extends DAO implements TrackDAO {
 	public Map<String, Collection<String>> getWaypoints() throws DAOException {
 		if (_notam == null)
 			getTrackInfo();
-		
+
 		// Parse the NOTAM data
 		try {
 			Map<String, Collection<String>> results = new TreeMap<String, Collection<String>>();
-			BufferedReader br = new BufferedReader(new StringReader(_notam));
-			while (br.ready()) {
-				String data = br.readLine();
-				br.mark(512);
-				
-				// Check if the track code is the first character
-				if ((data != null) && (data.length() > 2) && (data.charAt(1) == ' ')) {
-					String el = br.ready() ? br.readLine() : null;
-					String wl = br.ready() ? br.readLine() : null;
-					
-					// Validate the next two lines - if they're good then parse the track
-					if ((el != null) && (el.startsWith("EAST LVLS") && (wl != null) && (wl.startsWith("WEST LVLS")))) {
-						String code = data.substring(0, 1);
-						int end = data.indexOf('<');
-						if (end == -1)
-							end = data.length();
-						
-						Collection<String> wps = new LinkedHashSet<String>(StringUtils.split(data.substring(2, end), " "));
-						results.put(code, wps);
-					}
-				} else if (data == null)
-					break;
-				
-				br.reset();
+			try (BufferedReader br = new BufferedReader(new StringReader(_notam))) {
+				while (br.ready()) {
+					String data = br.readLine();
+					br.mark(512);
+
+					// Check if the track code is the first character
+					if ((data != null) && (data.length() > 2) && (data.charAt(1) == ' ')) {
+						String el = br.ready() ? br.readLine() : null;
+						String wl = br.ready() ? br.readLine() : null;
+
+						// Validate the next two lines - if they're good then parse the track
+						if ((el != null) && (el.startsWith("EAST LVLS") && (wl != null) && (wl.startsWith("WEST LVLS")))) {
+							String code = data.substring(0, 1);
+							int end = data.indexOf('<');
+							if (end == -1)
+								end = data.length();
+
+							Collection<String> wps = new LinkedHashSet<String>(StringUtils.split(data.substring(2, end), " "));
+							results.put(code, wps);
+						}
+					} else if (data == null)
+						break;
+
+					br.reset();
+				}
 			}
-			
-			br.close();
+
 			return results;
 		} catch (IOException ie) {
 			throw new DAOException(ie);
