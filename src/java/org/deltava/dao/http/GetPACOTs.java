7d940a2acf7491821d2008da74d9654cc893ca55
Luke Kolin <luke@deltava.org>
2012-11-01 15:08:28 +0000

    Use try-with-resources
    git-svn-id: file:///home/svn/DVA2006@8409 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/http/GetPACOTs.java
+++ src/java/org/deltava/dao/http/GetPACOTs.java
@@ -1,4 +1,4 @@
-// Copyright 2006, 2008, 2009, 2010 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2006, 2008, 2009, 2010, 2012 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao.http;
 
 import java.io.*;
@@ -11,14 +11,12 @@ import org.deltava.util.StringUtils;
 /**
  * A Data Access Object to load Pacific Track data.
  * @author Luke
- * @version 3.4
+ * @version 5.0
  * @since 1.0
  */
 
-public class GetPACOTs extends DAO implements TrackDAO {
-	
-	private static final String CRLF = System.getProperty("line.separator");
-	
+public class GetPACOTs extends TrackDAO {
+
 	private String _url;
 	private String _notam;
 
@@ -40,22 +38,22 @@ public class GetPACOTs extends DAO implements TrackDAO {
 		StringBuilder buf = new StringBuilder();
 		try {
 			init(_url);
-			LineNumberReader br = new LineNumberReader(new InputStreamReader(getIn()));
-			String data = br.readLine();
-			while (data != null) {
-				buf.append(data);
-				buf.append("<BR />");
-				buf.append(CRLF);
-				data = br.readLine();
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(getIn()))) {
+				String data = br.readLine();
+				while (data != null) {
+					buf.append(data);
+					buf.append("<BR />");
+					buf.append(CRLF);
+					data = br.readLine();
+				}
 			}
-			
-			br.close();
 		} catch (IOException ie) {
 			throw new DAOException(ie);
 		}
-		
+
 		// Split out the data
-		String notam = buf.toString().toUpperCase(); int pos = notam.indexOf("<INPUT TYPE=\"CHECKBOX\"");
+		String notam = buf.toString().toUpperCase();
+		int pos = notam.indexOf("<INPUT TYPE=\"CHECKBOX\"");
 		buf.setLength(0);
 		while (pos != -1) {
 			int startPos = notam.indexOf("<PRE>", pos);
@@ -68,74 +66,72 @@ public class GetPACOTs extends DAO implements TrackDAO {
 				buf.append("<br /><hr />");
 				buf.append(CRLF);
 			}
-			
+
 			// Find next occurrance
 			pos = notam.indexOf("<INPUT TYPE=\"CHECKBOX\"", endPos);
 		}
-		
-		// Return the data
+
 		_notam = buf.toString();
 		return _notam;
 	}
-	
+
 	/**
 	 * Returns the Waypoints for each Pacific Track.
 	 * @return a Map of waypoint codes, keyed by track code
 	 * @throws DAOException if an I/O error occurs
 	 */
 	public Map<String, Collection<String>> getWaypoints() throws DAOException {
-		if (_notam == null)
-			getTrackInfo();
+		if (_notam == null) getTrackInfo();
 
 		try {
 			Map<String, Collection<String>> info = new LinkedHashMap<String, Collection<String>>();
-			LineNumberReader br = new LineNumberReader(new StringReader(_notam));
-			String data = br.readLine();
-			while (data != null) {
-				int pos = data.indexOf("TDM TRK");
-				if (pos != -1) {
-					int endpos = data.indexOf(' ', pos + 9);
-					String trackID = data.substring(pos + 8, endpos).trim();
-					Collection<String> wps = new LinkedHashSet<String>();
-					data = br.readLine();
-					if (data != null)
-						data = br.readLine();
-					
-					while ((data != null) && (!data.startsWith("RTS"))) {
-						data = data.replace("<BR />", "");
-						wps.addAll(StringUtils.split(data.trim(), " "));
-						data = br.readLine();
-					}
-					
-					// Save the track
-					info.put(trackID, wps);
-				} else if (data.startsWith("TRACK ") && data.endsWith(".<BR />")) {
-					int tPos = data.indexOf(' ');
-					int tePos = data.indexOf('.', tPos);
-					if (tePos != -1) {
-						String trackID = data.substring(tPos + 1, data.length() - 7);
+			try (LineNumberReader br = new LineNumberReader(new StringReader(_notam))) {
+				String data = br.readLine();
+				while (data != null) {
+					int pos = data.indexOf("TDM TRK");
+					if (pos != -1) {
+						int endpos = data.indexOf(' ', pos + 9);
+						String trackID = data.substring(pos + 8, endpos).trim();
 						Collection<String> wps = new LinkedHashSet<String>();
 						data = br.readLine();
-						while ((data != null) && data.startsWith(" ")) {
+						if (data != null)
+							data = br.readLine();
+
+						while ((data != null) && (!data.startsWith("RTS"))) {
 							data = data.replace("<BR />", "");
-							if (data.contains(":")) {
-								int wPos = data.indexOf(':') + 1; 
-								wps.addAll(StringUtils.split(data.substring(wPos).trim(), " "));
-							} else
-								wps.addAll(StringUtils.split(data.trim(), " "));
-							
+							wps.addAll(StringUtils.split(data.trim(), " "));
 							data = br.readLine();
 						}
-						
+
 						// Save the track
 						info.put(trackID, wps);
+					} else if (data.startsWith("TRACK ") && data.endsWith(".<BR />")) {
+						int tPos = data.indexOf(' ');
+						int tePos = data.indexOf('.', tPos);
+						if (tePos != -1) {
+							String trackID = data.substring(tPos + 1, data.length() - 7);
+							Collection<String> wps = new LinkedHashSet<String>();
+							data = br.readLine();
+							while ((data != null) && data.startsWith(" ")) {
+								data = data.replace("<BR />", "");
+								if (data.contains(":")) {
+									int wPos = data.indexOf(':') + 1;
+									wps.addAll(StringUtils.split(data.substring(wPos).trim(), " "));
+								} else
+									wps.addAll(StringUtils.split(data.trim(), " "));
+
+								data = br.readLine();
+							}
+
+							// Save the track
+							info.put(trackID, wps);
+						}
 					}
+
+					data = br.readLine();
 				}
-				
-				data = br.readLine();
 			}
-			
-			br.close();
+
 			return info;
 		} catch (IOException ie) {
 			throw new DAOException(ie);
--- src/java/org/deltava/dao/http/TrackDAO.java
+++ src/java/org/deltava/dao/http/TrackDAO.java
@@ -1,4 +1,4 @@
-// Copyright 2006, 2009 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2006, 2009, 2012 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao.http;
 
 import java.util.*;
@@ -8,23 +8,28 @@ import org.deltava.dao.DAOException;
 /**
  * A Data Access Object that loads trans-oceanic track NOTAMs.
  * @author Luke
- * @version 2.6
+ * @version 5.0
  * @since 1.0
  */
 
-public interface TrackDAO {
+public abstract class TrackDAO extends DAO {
+	
+	/**
+	 * Line separator
+	 */
+	protected static final String CRLF = System.getProperty("line.separator");
 
 	/**
 	 * Downloads track data.
 	 * @return the track data
 	 * @throws DAOException if an I/O error occurs
 	 */
-	public String getTrackInfo() throws DAOException;
+	public abstract String getTrackInfo() throws DAOException;
 	
 	/**
 	 * Returns the Waypoints for each Track.
 	 * @return a Map of waypoint codes, keyed by track code
 	 * @throws DAOException if an I/O error occurs
 	 */
-	public Map<String, Collection<String>> getWaypoints() throws DAOException;
+	public abstract Map<String, Collection<String>> getWaypoints() throws DAOException;
 }
