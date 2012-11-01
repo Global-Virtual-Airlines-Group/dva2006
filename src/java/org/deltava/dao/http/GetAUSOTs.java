33536f19bf541ec4f440415bd4d9b6cee0380173
Luke Kolin <luke@deltava.org>
2012-11-01 15:08:28 +0000

    Use try-with-resources
    git-svn-id: file:///home/svn/DVA2006@8409 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/http/GetAUSOTs.java
+++ src/java/org/deltava/dao/http/GetAUSOTs.java
@@ -1,4 +1,4 @@
-// Copyright 2009, 2010 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2009, 2010, 2012 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao.http;
 
 import java.io.*;
@@ -10,14 +10,12 @@ import org.deltava.util.StringUtils;
 /**
  * A Data Access Object to get Australian Track data.
  * @author Luke
- * @version 3.4
+ * @version 5.0
  * @since 2.7
  */
 
-public class GetAUSOTs extends DAO implements TrackDAO {
-	
-	private static final String CRLF = System.getProperty("line.separator");
-	
+public class GetAUSOTs extends TrackDAO {
+
 	private String _url;
 	private String _notam;
 
@@ -39,70 +37,67 @@ public class GetAUSOTs extends DAO implements TrackDAO {
 		StringBuilder buf = new StringBuilder();
 		try {
 			init(_url);
-			LineNumberReader br = new LineNumberReader(new InputStreamReader(getIn()));
-			String data = br.readLine();
-			while (data != null) {
-				buf.append(data.toUpperCase());
-				buf.append("<BR />");
-				buf.append(CRLF);
-				data = br.readLine();
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(getIn()))) {
+				String data = br.readLine();
+				while (data != null) {
+					buf.append(data.toUpperCase());
+					buf.append("<BR />");
+					buf.append(CRLF);
+					data = br.readLine();
+				}
 			}
-
-			br.close();
 		} catch (IOException ie) {
 			throw new DAOException(ie);
 		}
-		
+
 		// Search for the code
 		String data = buf.toString();
 		int sPos = data.indexOf("<PRE>");
 		int ePos = data.indexOf("</PRE>", sPos);
 		if (ePos == -1)
 			ePos = data.length();
-		
+
 		// Copy the data
-		_notam = data.substring(sPos +  5, ePos);
+		_notam = data.substring(sPos + 5, ePos);
 		return _notam;
 	}
-	
+
 	/**
 	 * Returns the Waypoints for each Australian Track.
 	 * @return a Map of {@link org.deltava.beans.navdata.OceanicTrack} beans, keyed by track code
 	 * @throws DAOException if an I/O error occurs
 	 */
 	public Map<String, Collection<String>> getWaypoints() throws DAOException {
-		if (_notam == null)
-			getTrackInfo();
-		
-		// Parse the NOTAM data
-		try {
-			Map<String, Collection<String>> results = new TreeMap<String, Collection<String>>();			
-			LineNumberReader br = new LineNumberReader(new StringReader(_notam));
-			String data = br.readLine();
-			while (data != null) {
-				int pos = data.indexOf("TDM TRK");
-				if (pos != -1) {
-					int endpos = data.indexOf(' ', pos + 9);
-					String trackID = data.substring(pos + 8, endpos).trim();
-					Collection<String> wps = new LinkedHashSet<String>();
-					data = br.readLine();
-					if (data != null)
-						data = br.readLine();
+		if (_notam == null) getTrackInfo();
 
-					while ((data != null) && (!data.startsWith("RTS"))) {
-						data = data.replace("<BR />", "");
-						wps.addAll(StringUtils.split(data.trim(), " "));
+		try {
+			Map<String, Collection<String>> results = new TreeMap<String, Collection<String>>();
+			try (BufferedReader br = new BufferedReader(new StringReader(_notam))) {
+				String data = br.readLine();
+				while (data != null) {
+					int pos = data.indexOf("TDM TRK");
+					if (pos != -1) {
+						int endpos = data.indexOf(' ', pos + 9);
+						String trackID = data.substring(pos + 8, endpos).trim();
+						Collection<String> wps = new LinkedHashSet<String>();
 						data = br.readLine();
+						if (data != null)
+							data = br.readLine();
+
+						while ((data != null) && (!data.startsWith("RTS"))) {
+							data = data.replace("<BR />", "");
+							wps.addAll(StringUtils.split(data.trim(), " "));
+							data = br.readLine();
+						}
+
+						// Save the track
+						results.put(trackID, wps);
 					}
 
-					// Save the track
-					results.put(trackID, wps);
+					data = br.readLine();
 				}
-				
-				data = br.readLine();
 			}
-			
-			br.close();
+
 			return results;
 		} catch (IOException ie) {
 			throw new DAOException(ie);
