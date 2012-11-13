22114ea2a8b322ee83cd13de546c0d854f80d063
Luke Kolin <luke@deltava.org>
2012-11-13 01:13:15 +0000

    Add time zone list tag
    git-svn-id: file:///home/svn/DVA2006@8448 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/taglib/content/TZListTag.java
@@ -0,0 +1,36 @@
+// Copyright 2012 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.taglib.content;
+
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.deltava.beans.TZInfo;
+
+/**
+ * A JSP Tag to save the list of time zones as a request attribute.
+ * @author Luke
+ * @version 5.0
+ * @since 5.0
+ */
+
+public class TZListTag extends TagSupport {
+	
+	private String _varName;
+
+	/**
+	 * Sets the request attribute name.
+	 * @param vName the name of the request attribute to store the data in
+	 */
+	public void setVar(String vName) {
+		_varName = vName;
+	}
+
+	/**
+	 * Saves the time zone list in the request body.
+	 * @return EVAL_PAGE always
+	 */
+	public int doEndTag() {
+		pageContext.setAttribute(_varName, TZInfo.getAll(), PageContext.REQUEST_SCOPE);
+		return EVAL_PAGE;
+	}
+}
