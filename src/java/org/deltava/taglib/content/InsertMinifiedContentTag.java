3929e0a449c5eeaf114ded919f3aa3dc816fb542
Luke Kolin <luke@deltava.org>
2012-11-19 19:49:41 +0000

    Support JS/CSS minification
    git-svn-id: file:///home/svn/DVA2006@8468 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/taglib/content/InsertMinifiedContentTag.java
@@ -0,0 +1,55 @@
+// Copyright 2012 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.taglib.content;
+
+import org.deltava.util.system.SystemData;
+
+/**
+ * A JSP tag to insert optionally minified content. Content will be minified if the
+ * SystemData environment name starts with &quot;prod&quot;.
+ * @author Luke
+ * @version 5.0
+ * @since 5.0
+ */
+
+abstract class InsertMinifiedContentTag extends InsertContentTag {
+
+	private String _actualResourceName;
+	private boolean _isMin;
+	
+	/**
+	 * Returns the actual file name of the requested resource.
+	 * @return the file name
+	 */
+	protected String getFileName() {
+		return _actualResourceName;
+	}
+	
+	/**
+	 * Returns whether this resource is minified.
+	 * @return TRUE if minified, otherwise FALSE
+	 */
+	protected boolean isMinified() {
+		return _isMin;
+	}
+	
+	/**
+	 * Determines whether content is minified.
+	 */
+	@Override
+	public int doStartTag() {
+		String env = SystemData.get(SystemData.CFG_NAME);
+		_isMin = ((env != null) && env.startsWith("prod"));
+		
+		// Detect minification override
+		boolean minOV = Boolean.valueOf(pageContext.getRequest().getParameter("minOV")).booleanValue();
+		if (minOV) _isMin = !_isMin;
+		
+		// If running in prod, minify - otherwise 
+		StringBuilder buf = new StringBuilder(_resourceName);
+		if (_isMin)
+			buf.append("-min");
+		
+		_actualResourceName = buf.toString();
+		return SKIP_BODY;
+	}
+}
