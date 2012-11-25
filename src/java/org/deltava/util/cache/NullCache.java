57988f92228b99d26df95e4154b80d0ceb32a451
Luke Kolin <luke@deltava.org>
2012-11-25 02:51:04 +0000

    Add NullCache
    git-svn-id: file:///home/svn/DVA2006@8496 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/util/cache/NullCache.java
@@ -0,0 +1,55 @@
+// Copyright 2012 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.util.cache;
+
+/**
+ * A cache implementation that does no caching. This is used to ensure that
+ * CacheManager never returns null. 
+ * @author Luke
+ * @version 5.0
+ * @since 5.0
+ */
+
+public class NullCache<T extends Cacheable> extends Cache<T> {
+	
+	/**
+	 * Creates a new null cache.
+	 */
+	NullCache() {
+		super(1);
+	}
+	
+	/**
+	 * Returns the maximum size of the cache.
+	 * @return the maximum number of entries in the cache
+	 */
+	@Override
+	public final int getMaxSize() {
+		return 0;
+	}
+
+	/**
+	 * Adds an entry to the cache - not implemented.
+	 */
+	@Override
+	protected void addEntry(T entry) {
+		// empty
+	}
+
+	/**
+	 * Adds an entry to the cache - not implemented.
+	 */
+	@Override
+	protected void addNullEntry(Object key) {
+		// empty
+	}
+
+	/**
+	 * Retrieves an entry from the cache - not implemented.
+	 * @returns null
+	 */
+	@Override
+	public T get(Object key) {
+		request();
+		return null;
+	}
+}
---
+++ src/test/org/deltava/util/cache/TestNullCache.java
@@ -0,0 +1,69 @@
+package org.deltava.util.cache;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.hansel.CoverageDecorator;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+import org.deltava.beans.schedule.Airline;
+
+public class TestNullCache extends TestCase {
+   
+   private NullCache<Cacheable> _cache;
+   
+   public static Test suite() {
+      return new CoverageDecorator(TestNullCache.class, new Class[] { NullCache.class } );
+  }
+   
+   protected void setUp() throws Exception {
+      super.setUp();
+      _cache = new NullCache<Cacheable>();
+   }
+
+   protected void tearDown() throws Exception {
+      _cache = null;
+      super.tearDown();
+   }
+   
+   public void testCache() {
+      _cache.add(new Airline("AF", "Air France"));
+      Airline dva = new Airline("DVA", "Delta Virtual");
+      _cache.add(dva);
+      assertEquals(0, _cache.size());
+      assertFalse(_cache.contains("AF"));
+      assertFalse(_cache.contains("DVA"));
+      assertEquals(_cache.getMaxSize(), _cache.size());
+      
+      _cache.add(new Airline("COA", "Continental Airlines"));
+      assertEquals(0, _cache.size());
+      assertFalse(_cache.contains("AF"));
+      assertNull(_cache.get("AF"));
+      assertFalse(_cache.contains("DVA"));
+      assertFalse(_cache.contains("COA"));
+   }
+   
+   public void testNull() {
+	   _cache.addNull("DVA");
+	   assertEquals(0, _cache.size());
+	   assertFalse(_cache.contains("DVA"));
+	   assertNull(_cache.get("DVA"));
+   }
+   
+   public void testLargeCache() {
+	   Collection<Cacheable> entries = new ArrayList<Cacheable>();
+	   for (int x = 0; x < 16384; x++)
+		   entries.add(new CacheableLong(Integer.valueOf(x), x));
+	   
+	   assertEquals(16384, entries.size());
+	   _cache.setMaxSize(entries.size());
+	   assertEquals(0, _cache.getMaxSize());
+	   
+	   long start = System.currentTimeMillis();
+	   _cache.addAll(entries);
+	   assertTrue((System.currentTimeMillis() - start) < 500);
+	   assertEquals(0, _cache.size());
+   }
+}
