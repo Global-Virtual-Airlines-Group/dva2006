a6073e189edea57267415ad0bd6a2f06116d422e
Luke Kolin <luke@deltava.org>
2012-12-16 19:27:28 +0000

    Add resolution in feet
    git-svn-id: file:///home/svn/DVA2006@8554 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/comparators/GeoComparator.java
+++ src/java/org/deltava/comparators/GeoComparator.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2006, 2008, 2010, 2011 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2008, 2010, 2011, 2012 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.comparators;
 
 import java.util.Comparator;
@@ -6,41 +6,54 @@ import java.util.Comparator;
 import org.deltava.beans.GeoLocation;
 import org.deltava.beans.schedule.GeoPosition;
 
+import org.deltava.util.GeoUtils;
+
 /**
  * A comparator to sort geographic locations by their distance from a fixed point.
  * @author Luke
- * @version 4.1
+ * @version 5.1
  * @since 1.0
  */
 
 public class GeoComparator implements Comparator<GeoLocation>, java.io.Serializable {
-   
-   private final GeoPosition _point;
 
-   /**
-    * Creates a new GeoComparator, comparing distance from an arbitrary location.
-    * @param loc the location
-    */
-   public GeoComparator(GeoLocation loc) {
-      super();
-      _point = (loc == null) ? new GeoPosition(0, 0) : new GeoPosition(loc);
-   }
-   
-   /**
-    * Returns the reference location.
-    * @return a GeoLocation
-    */
-   public GeoLocation getLocation() {
-	   return _point;
-   }
+	private final GeoPosition _point;
+	private final boolean _useFeet;
 
-   /**
-    * Compares two GeoLocations by comparing their distance from a common point.
-    * @see Comparator#compare(Object, Object)
-    */
-   public int compare(GeoLocation l1, GeoLocation l2) {
-      int d1 = _point.distanceTo(l1);
-      int d2 = _point.distanceTo(l2);
-      return Integer.valueOf(d1).compareTo(Integer.valueOf(d2));
-   }
+	/**
+	 * Creates a new GeoComparator, comparing distance from an arbitrary location.
+	 * @param loc the location
+	 */
+	public GeoComparator(GeoLocation loc) {
+		this(loc, false);
+	}
+
+	/**
+	 * Creates a new GeoComparator, comparing distance from an arbitrary location.
+	 * @param loc the location
+	 * @param useFeet TRUE if the calculation should be measured in feet, otherwise FALSE for miles
+	 */
+	public GeoComparator(GeoLocation loc, boolean useFeet) {
+		super();
+		_useFeet = useFeet;
+		_point = (loc == null) ? new GeoPosition(0, 0) : new GeoPosition(loc);
+	}
+
+	/**
+	 * Returns the reference location.
+	 * @return a GeoLocation
+	 */
+	public GeoLocation getLocation() {
+		return _point;
+	}
+
+	/**
+	 * Compares two GeoLocations by comparing their distance from a common point.
+	 */
+	@Override
+	public int compare(GeoLocation l1, GeoLocation l2) {
+		int d1 = _useFeet ? GeoUtils.distanceFeet(_point, l1) : _point.distanceTo(l1);
+		int d2 = _useFeet ? GeoUtils.distanceFeet(_point, l2) : _point.distanceTo(l2);
+		return Integer.valueOf(d1).compareTo(Integer.valueOf(d2));
+	}
 }
