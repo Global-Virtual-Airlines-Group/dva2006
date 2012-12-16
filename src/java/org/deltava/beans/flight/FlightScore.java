58295267d2a1cb1f69e8aa6d4956a79689c44c23
Luke Kolin <luke@deltava.org>
2012-12-16 19:28:08 +0000

    Add FlightScorer
    git-svn-id: file:///home/svn/DVA2006@8555 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/flight/FlightScore.java
@@ -0,0 +1,37 @@
+// Copyright 2012 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.flight;
+
+/**
+ * An enumeration of Flight Report scores. 
+ * @author Luke
+ * @version 5.1
+ * @since 5.1
+ */
+
+public enum FlightScore {
+	OPTIMAL("green"), ACCEPTABLE("orange"), DANGEROUS("red"), INCOMPLETE("grey");
+	
+	private final String _name;
+	private final String _color;
+	
+	FlightScore(String color) {
+		_name = name().substring(0, 1) + name().substring(1).toLowerCase();
+		_color = color;
+	}
+	
+	/**
+	 * Returns the score label.
+	 * @return the label
+	 */
+	public String getName() {
+		return _name;
+	}
+	
+	/**
+	 * Returns the score color.
+	 * @return the color
+	 */
+	public String getColor() {
+		return _color;
+	}
+}
