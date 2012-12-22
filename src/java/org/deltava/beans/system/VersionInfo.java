80d75e4d45b803d7128d3c7b2f016540916ee861
Luke Kolin <luke@deltava.org>
2012-12-22 05:42:20 +0000

    Build 441
    git-svn-id: file:///home/svn/DVA2006@8618 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -13,7 +13,7 @@ public interface VersionInfo {
     public static final int MAJOR = 5;
     public static final int MINOR = 1;
     
-    public static final int BUILD = 440;
+    public static final int BUILD = 441;
     
     //public static final String BUILD_DATE="$BUILD";
     
