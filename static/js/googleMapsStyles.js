fad4d38ca7b4e426ec39cb24252f84b1fef34010
Luke Kolin <luke@deltava.org>
2013-06-03 00:14:27 +0000

    Styled map support
    git-svn-id: file:///home/svn/DVA2006@8803 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ static/js/googleMapsStyles.js
@@ -0,0 +1,58 @@
+golgotha.maps.styles.TRACKMAP =
+[
+  {
+    "featureType": "administrative",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "poi",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "transit",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "road",
+    "stylers": [
+      { "visibility": "simplified" },
+      { "invert_lightness": true },
+      { "gamma": 0.6 },
+      { "weight": 0.5 }
+    ]
+  },{
+    "featureType": "water",
+    "stylers": [
+      { "visibility": "simplified" },
+      { "invert_lightness": true },
+      { "gamma": 0.6 }
+    ]
+  },{
+    "featureType": "water",
+    "elementType": "labels",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "landscape",
+    "stylers": [
+      { "visibility": "simplified" },
+      { "invert_lightness": true },
+      { "gamma": 0.9 }
+    ]
+  },{
+    "featureType": "road.local",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "road",
+    "elementType": "labels",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  }
+];
