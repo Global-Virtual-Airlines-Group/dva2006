089ee61de08708e555751c4ba03ef57a6c81d650
Luke Kolin <luke@deltava.org>
2013-09-01 16:26:52 +0000

    Updated BZIP2 test
    git-svn-id: file:///home/svn/DVA2006@8872 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/beans/TestFileUpload.java
+++ src/test/org/deltava/beans/TestFileUpload.java
@@ -1,12 +1,12 @@
-// Copyright (c) 2005 Luke J. Kolin. All Rights Reserved.
 package org.deltava.beans;
 
 import java.io.*;
+import java.util.zip.*;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
+import junit.framework.*;
 
 import org.hansel.CoverageDecorator;
+import org.apache.commons.compress.compressors.bzip2.*;
 
 public class TestFileUpload extends TestCase {
 	
@@ -39,5 +39,66 @@ public class TestFileUpload extends TestCase {
         _fu.load(is);
         assertNotNull(_fu.getBuffer());
         assertEquals(f.length(), _fu.getSize());
+	}
+	
+	public void testGZStream() throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try (GZIPOutputStream gzout = new GZIPOutputStream(out)) {
+			try (PrintWriter pw = new PrintWriter(gzout)) {
+				pw.println("Line 1");
+				pw.println("Line 2");
+			}
+		}
+		
+		ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+		try (GZIPInputStream gzin = new GZIPInputStream(in)) {
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(gzin))) {
+				assertEquals("Line 1", br.readLine());
+				assertEquals("Line 2", br.readLine());
+				assertFalse(br.ready());
+			}
+		}
+	}
+	
+	public void testBZ2Stream() throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try (BZip2CompressorOutputStream bzout = new BZip2CompressorOutputStream(out, 4)) {
+			try (PrintWriter pw = new PrintWriter(bzout)) {
+				pw.println("Line 1");
+				pw.println("Line 2");
+			}
+		}
+		
+		ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+		try (BZip2CompressorInputStream bzin = new BZip2CompressorInputStream(in)) {
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(bzin))) {
+				assertEquals("Line 1", br.readLine());
+				assertEquals("Line 2", br.readLine());
+				assertFalse(br.ready());
+			}
+		}
+	}
+	
+	public void testBZ2File() throws IOException {
+		
+		File f = new File("data/test.txt.bz2");
+		assertTrue(f.exists());
+		try (BZip2CompressorInputStream bzin = new BZip2CompressorInputStream(new BufferedInputStream(new FileInputStream(f)), true)) {
+			try (LineNumberReader br = new LineNumberReader(new InputStreamReader(bzin))) {
+				assertEquals("Line 1", br.readLine());
+				assertEquals("Line 2", br.readLine());
+			}
+		}
+		
+		FileUpload fu = new FileUpload("test.txt.bz2");
+		fu.load(new FileInputStream(f));
+		
+		try (InputStream is = fu.getInputStream()) {
+			assertTrue(is instanceof BZip2CompressorInputStream);
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+				assertEquals("Line 1", br.readLine());
+				assertEquals("Line 2", br.readLine());
+			}
+		}
 	}
 }
