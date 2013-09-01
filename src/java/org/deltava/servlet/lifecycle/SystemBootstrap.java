3f313bb1a1f4b95c09e01b8ec53ef36363191956
Luke Kolin <luke@deltava.org>
2013-09-01 17:06:02 +0000

    Add metadata support for user highwater mark
    git-svn-id: file:///home/svn/DVA2006@8876 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/servlet/lifecycle/SystemBootstrap.java
+++ src/java/org/deltava/servlet/lifecycle/SystemBootstrap.java
@@ -1,33 +1,27 @@
-// Copyright 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.servlet.lifecycle;
 
 import java.io.*;
 import java.util.*;
 import java.sql.*;
+
 import javax.servlet.*;
 
 import org.apache.log4j.*;
-
 import org.deltava.beans.econ.EconomyInfo;
 import org.deltava.beans.fb.FacebookCredentials;
 import org.deltava.beans.flight.ETOPSHelper;
 import org.deltava.beans.schedule.Airport;
 import org.deltava.beans.stats.AirlineTotals;
-
 import org.deltava.dao.*;
-import org.deltava.dao.file.*;
 import org.deltava.dao.mc.MemcachedDAO;
-
 import org.deltava.mail.MailerDaemon;
-
 import org.deltava.security.*;
 import org.deltava.taskman.*;
-
 import org.deltava.util.*;
 import org.deltava.util.cache.CacheLoader;
 import org.deltava.util.ipc.IPCDaemon;
 import org.deltava.util.system.SystemData;
-
 import org.gvagroup.common.*;
 import org.gvagroup.jdbc.*;
 
@@ -87,7 +81,7 @@ public class SystemBootstrap implements ServletContextListener, Thread.UncaughtE
 		_jdbcPool = new ConnectionPool(SystemData.getInt("jdbc.pool_max_size", 2), SystemData.get("airline.code"));
 		_jdbcPool.setProperties((Map<?, ?>) SystemData.getObject("jdbc.connectProperties"));
 		_jdbcPool.setCredentials(SystemData.get("jdbc.user"), SystemData.get("jdbc.pwd"));
-		_jdbcPool.setProperty("url", SystemData.get("jdbc.url"));
+		_jdbcPool.setURL(SystemData.get("jdbc.url"));
 		_jdbcPool.setMaxRequests(SystemData.getInt("jdbc.max_reqs", 0));
 		_jdbcPool.setLogStack(SystemData.getBoolean("jdbc.log_stack"));
 
@@ -180,13 +174,9 @@ public class SystemBootstrap implements ServletContextListener, Thread.UncaughtE
 			}
 			
 			// Load User Pool max values
-			try (InputStream is = ConfigLoader.getStream("/etc/maxUsers.properties")) {
-				if (is != null) {
-					GetProperties pdao = new GetProperties(is);
-					Properties p = pdao.read();
-					UserPool.init(StringUtils.parse(p.getProperty("users"), 0), StringUtils.parseDate(p.getProperty("date"), "MM/dd/yyyy HH:mm"));
-				}
-			}
+			String prefix = SystemData.get("airline.code").toLowerCase();
+			GetMetadata mddao = new GetMetadata(c);
+			UserPool.init(StringUtils.parse(mddao.get(prefix + ".users.max.count"), 0), StringUtils.parseDate(mddao.get(prefix + ".users.max.date"), "MM/dd/yyyy HH:mm"));
 			
 			// Load facebook credentials
 			if (!StringUtils.isEmpty(SystemData.get("users.facebook.id"))) {
