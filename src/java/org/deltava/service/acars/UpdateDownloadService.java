9422da30628d9a7a362138fabf96b4518781d8ad
Luke Kolin <luke@deltava.org>
2013-09-06 14:35:49 +0000

    Separate incrementals for beta/release
    git-svn-id: file:///home/svn/DVA2006@8898 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/service/acars/UpdateDownloadService.java
+++ src/java/org/deltava/service/acars/UpdateDownloadService.java
@@ -1,4 +1,4 @@
-// Copyright 2011, 2012 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2011, 2012, 2013 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.service.acars;
 
 import static javax.servlet.http.HttpServletResponse.*;
@@ -12,7 +12,7 @@ import org.deltava.util.system.SystemData;
 /**
  * A Web Service to download the ACARS incremental installer.
  * @author Luke
- * @version 5.0
+ * @version 5.1
  * @since 4.1
  */
 
@@ -29,15 +29,21 @@ public class UpdateDownloadService extends DownloadService {
 		
 		// Get the installer to download
 		boolean isDispatch = Boolean.valueOf(ctx.getParameter("dispatch")).booleanValue();
-		String fName = SystemData.get("airline.code") + (isDispatch ? "-DispatchInc.exe" : "-ACARS3Inc.exe");
+		boolean isBeta = Boolean.valueOf(ctx.getParameter("beta")).booleanValue();
+		StringBuilder buf = new StringBuilder(SystemData.get("airline.code"));
+		buf.append(isDispatch ? "-Dispatch" : "-ACARS3");
+		if (isBeta)
+			buf.append("Beta");
+		
+		buf.append("Inc.exe");
 
 		// Get the installer
-		File f = new File(SystemData.get("path.library"), fName);
+		File f = new File(SystemData.get("path.library"), buf.toString());
 		if (!f.exists())
 			return SC_NOT_FOUND;
 		
 		// Download the file
-		ctx.getResponse().setHeader("Content-disposition", "attachment; filename=" + fName);
+		ctx.getResponse().setHeader("Content-disposition", "attachment; filename=" + buf.toString());
 		ctx.getResponse().setContentType("application/octet-stream");
 		ctx.setExpiry(3600);
 		sendFile(f, ctx.getResponse());
