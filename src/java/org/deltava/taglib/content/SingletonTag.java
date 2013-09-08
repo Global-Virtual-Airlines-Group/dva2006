3341e62f1c86d06c5d5c748003b8fa48faec3831
Luke Kolin <luke@deltava.org>
2013-09-08 16:14:47 +0000

    Add singleton collection tag
    git-svn-id: file:///home/svn/DVA2006@8907 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/taglib/content/SingletonTag.java
@@ -0,0 +1,47 @@
+// Copyright 2013 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.taglib.content;
+
+import java.util.*;
+
+import javax.servlet.jsp.*;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+/**
+ * A JSP tag to create a singleton collection.
+ * @author Luke
+ * @version 5.1
+ * @since 5.1
+ */
+
+public class SingletonTag extends SimpleTagSupport {
+	
+	private String _var;
+	private Object _value;
+	
+	/**
+	 * Sets the page attribute to store the result in.
+	 * @param varName the page attribute name
+	 */
+	public void setVar(String varName) {
+		_var = varName;
+	}
+	
+	/**
+	 * Sets the value.
+	 * @param o the value
+	 */
+	public void setValue(Object o) {
+		_value = o;
+	}
+
+	/**
+	 * Saves the collection in the request.  
+	 */
+	@Override
+	public void doTag() {
+		List<Object> v = new ArrayList<Object>(2);
+		v.add(_value);
+		JspContext ctx = getJspContext();
+		ctx.setAttribute(_var, v, PageContext.PAGE_SCOPE);
+	}
+}
