9b50f682a958dea720e0a68b8287320ff90f79db
Luke Kolin <luke@deltava.org>
2013-09-17 12:57:27 +0000

    Add metadata view
    git-svn-id: file:///home/svn/DVA2006@8936 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/commands/system/MetadataViewCommand.java
@@ -0,0 +1,47 @@
+// Copyright 2013 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.commands.system;
+
+import java.util.Map;
+
+import org.deltava.commands.*;
+import org.deltava.dao.*;
+
+/**
+ * A Web Site Command to view metadata.
+ * @author Luke
+ * @version 5.1
+ * @since 5.1
+ */
+
+public class MetadataViewCommand extends AbstractViewCommand {
+
+    /**
+     * Executes the command.
+     * @param ctx the Command context
+     * @throws CommandException if an unhandled error occurs
+     */
+	@Override
+	public void execute(CommandContext ctx) throws CommandException {
+		
+		ViewContext vctxt = initView(ctx);
+		try {
+			GetMetadata mddao = new GetMetadata(ctx.getConnection());
+			mddao.setQueryStart(vctxt.getStart());
+			mddao.setQueryMax(vctxt.getCount());
+			
+			// Save in view cotnext so pageUp/pageDn tags work
+			Map<String, String> results = mddao.getAll();
+			ctx.setAttribute("data", results, REQUEST);
+			vctxt.setResults(results.values());
+		} catch (DAOException de) {
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
+		
+		// Forward to the JSP
+		CommandResult result = ctx.getResult();
+		result.setURL("/jsp/admin/metadata.jsp");
+		result.setSuccess(true);
+	}
+}
