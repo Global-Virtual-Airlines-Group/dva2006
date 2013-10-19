1911065b586e0f68a84e0ff92c845fc020b2c5ae
Luke Kolin <luke@deltava.org>
2013-10-19 03:10:20 +0000

    Elminate attribute on subitem element
    git-svn-id: file:///home/svn/DVA2006@9005 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/layout/SubMenuItemTag.java
+++ src/java/org/deltava/taglib/layout/SubMenuItemTag.java
@@ -1,43 +1,24 @@
-// Copyright 2009, 2010 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2009, 2010, 2013 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.layout;
 
 import javax.servlet.jsp.*;
 import javax.servlet.jsp.tagext.TagSupport;
 
-import org.deltava.util.StringUtils;
-
 /**
  * A JSP tag to render CSS sub-menu items.
  * @author Luke
- * @version 3.3
+ * @version 5.2
  * @since 2.6
  */
 
 public class SubMenuItemTag extends MenuElementTag {
-	
-	private String _className;
-	
-	/**
-	 * Sets the CSS style to use for the rendered element.
-	 * @param cName the CSS selector(s)
-	 */
-	public void setClassName(String cName) {
-		_className = cName;
-	}
-	
-	/**
-	 * Releases the tag's state variables.
-	 */
-	public void release() {
-		super.release();
-		_className = null;
-	}
 	
 	/**
 	 * Writes the sub-menu item's opening tag to the JSP output stream.
 	 * @return TagSuppport.EVAL_BODY_INCLUDE always
 	 * @throws JspException if an error occurs
 	 */
+	@Override
 	public int doStartTag() throws JspException {
 		super.doStartTag();
 		
@@ -46,28 +27,9 @@ public class SubMenuItemTag extends MenuElementTag {
 		if (parent == null)
 			throw new JspException("Must be contained within a SUBMENU tag");
 		
-		// Check what we're writing
 		try {
-			JspWriter out = pageContext.getOut();
 			parent.writeSubMenu();
-			if (_renderTable) {
-				out.print("<tr class=\"submenu");
-				if (!StringUtils.isEmpty(_className)) {
-					out.print(' ');
-					out.print(_className);
-				}
-				
-				out.print("\"><td>");
-			} else {
-				out.print("<li");
-				if (!StringUtils.isEmpty(_className)) {
-					out.print(" class=\"");
-					out.print(_className);
-					out.print('"');
-				}
-				
-				out.print('>');
-			}
+			pageContext.getOut().print(_renderTable ? "<tr class=\"submenu\"><td>" : "<li>");
 		} catch (Exception e) {
 			throw new JspException(e);
 		}
@@ -80,6 +42,7 @@ public class SubMenuItemTag extends MenuElementTag {
 	 * 	@return TagSuppport.EVAL_PAGE always
 	 * @throws JspException if an error occurs
 	 */
+	@Override
 	public int doEndTag() throws JspException {
 		try {
 			pageContext.getOut().print(_renderTable ? "</td></tr>" : "</li>");
