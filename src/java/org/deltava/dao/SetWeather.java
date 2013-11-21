82de3cdef3532abfa64cd62431dbba788bc268cb
Luke Kolin <luke@deltava.org>
2013-11-21 20:09:53 +0000

    Persist wind data
    git-svn-id: file:///home/svn/DVA2006@9083 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/SetWeather.java
+++ src/java/org/deltava/dao/SetWeather.java
@@ -1,4 +1,4 @@
-// Copyright 2009, 2011, 2012 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2009, 2011, 2012, 2013 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao;
 
 import java.sql.*;
@@ -9,7 +9,7 @@ import org.deltava.util.cache.CacheManager;
 /**
  * A Data Access Object to save weather data in the database.
  * @author Luke
- * @version 5.0
+ * @version 5.2
  * @since 2.7
  */
 
@@ -64,6 +64,27 @@ public class SetWeather extends DAO {
 	}
 	
 	/**
+	 * Writes wind data to the database.
+	 * @param effDate the effective date of the data
+	 * @param wd the WindData
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public void write(java.util.Date effDate, WindData wd) throws DAOException {
+		try {
+			prepareStatement("REPLACE INTO common.WINDS (DATE, LAT, LNG, ALT, WSPD, WDIR) VALUES (?, ?, ?, ?, ?, ?)");
+			_ps.setTimestamp(1, createTimestamp(effDate));
+			_ps.setDouble(2, wd.getLatitude());
+			_ps.setDouble(3, wd.getLongitude());
+			_ps.setInt(4, wd.getAltitude());
+			_ps.setInt(5, wd.getJetStreamSpeed());
+			_ps.setInt(6, wd.getJetStreamDirection());
+			executeUpdate(1);
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+	
+	/**
 	 * Purges METAR data older than a particular age.
 	 * @param age the age in minutes
 	 * @throws DAOException if a JDBC error occurs
@@ -77,6 +98,21 @@ public class SetWeather extends DAO {
 			throw new DAOException(se);
 		} finally {
 			CacheManager.invalidate("METAR");
+		}
+	}
+	
+	/**
+	 * Purges wind data older than a particular age.
+	 * @param age the age in minutes
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public void purgeWinds(int age) throws DAOException {
+		try {
+			prepareStatementWithoutLimits("DELETE FROM common.WINDS WHERE (DATE < DATE_SUB(NOW(), INTERVAL ? MINUTE))");
+			_ps.setInt(1, age);
+			executeUpdate(0);
+		} catch (SQLException se) {
+			throw new DAOException(se);
 		}
 	}
 	
