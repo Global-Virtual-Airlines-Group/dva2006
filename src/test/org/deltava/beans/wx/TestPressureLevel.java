582c10dbf76f6253a69e1643b078be52052ce9e5
Luke Kolin <luke@deltava.org>
2013-11-29 16:17:36 +0000

    Multiple pressure level support
    git-svn-id: file:///home/svn/DVA2006@9109 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/test/org/deltava/beans/wx/TestPressureLevel.java
@@ -0,0 +1,14 @@
+// Copyright 2013 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.wx;
+
+import junit.framework.TestCase;
+
+public class TestPressureLevel extends TestCase {
+
+	public void testClosest() {
+		
+		assertEquals(PressureLevel.LOW, PressureLevel.getClosest(7000));
+		assertEquals(PressureLevel.JET, PressureLevel.getClosest(36000));
+		assertEquals(PressureLevel.HIGH, PressureLevel.getClosest(41000));
+	}
+}
