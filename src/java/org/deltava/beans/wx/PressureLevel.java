da21eef12611d5a74e372f483a9dd6fb154c17ab
Luke Kolin <luke@deltava.org>
2013-11-29 16:17:36 +0000

    Multiple pressure level support
    git-svn-id: file:///home/svn/DVA2006@9109 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/wx/PressureLevel.java
@@ -0,0 +1,57 @@
+// Copyright 2013 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.wx;
+
+/**
+ * An enumeration to store GFS pressure levels.
+ * @author Luke
+ * @version 5.2
+ * @since 5.2
+ */
+
+public enum PressureLevel {
+
+	LOW(875, 4000), MIDLOW(625, 12000), MID(475, 20000), LOJET(325, 28000), JET(275, 32000), HIGH(175, 42000);
+	
+	private final int _mb;
+	private final int _alt;
+	
+	PressureLevel(int mb, int alt) {
+		_mb = mb;
+		_alt = alt;
+	}
+	
+	/**
+	 * Returns the pressure level.
+	 * @return the pressure in millibars
+	 */
+	public int getPressure() {
+		return _mb;
+	}
+	
+	/**
+	 * Returns the typical altitude. 
+	 * @return the altitude in feet MSL
+	 */
+	public int getAltitude() {
+		return _alt;
+	}
+
+	/**
+	 * Returns the closest pressure level for an altitude.
+	 * @param alt the altitude in feet
+	 */
+	public static PressureLevel getClosest(int alt) {
+		PressureLevel l = LOW; int delta = Math.abs(alt - l.getAltitude());
+		PressureLevel[] lv = values();
+		for (int x = 1; x < lv.length; x++) {
+			PressureLevel pl = lv[x];
+			int d = Math.abs(alt - pl.getAltitude());
+			if (d <= delta) {
+				delta = d;
+				l = pl;
+			}
+		}
+		
+		return l;
+	}
+}
