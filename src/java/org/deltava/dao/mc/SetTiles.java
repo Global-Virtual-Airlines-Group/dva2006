391b607619683ec0dab703d7efcb81a59c4990e8
Luke Kolin <luke@deltava.org>
2013-12-01 23:26:30 +0000

    Convert imageSeries to map
    git-svn-id: file:///home/svn/DVA2006@9112 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/mc/SetTiles.java
+++ src/java/org/deltava/dao/mc/SetTiles.java
@@ -6,7 +6,6 @@ import java.util.*;
 import net.spy.memcached.*;
 
 import org.deltava.dao.DAOException;
-
 import org.deltava.util.tile.*;
 
 /**
@@ -21,22 +20,47 @@ public class SetTiles extends MemcachedDAO implements SeriesWriter {
 	/**
 	 * Writes an ImageSeries to memcached. 
 	 * @param is the ImageSeries
-	 * @throws DAOException
+	 * @throws DAOException if an error occured
 	 */
 	@Override
 	public void write(ImageSeries is) throws DAOException {
 		checkConnection();
+		Long seriesDate = (is.getDate() == null) ? null : Long.valueOf(is.getDate().getTime());
 		
 		addImageType(is.getType());
 		addImageDate(is.getType(), is.getDate());
 		
 		// Write the Tiles
-		setBucket("mapTiles", is.getType(), String.valueOf(is.getDate().getTime()));
+		setBucket("mapTiles", is.getType(), seriesDate);
 		_client.add(createKey("$ME"), _expiry, Boolean.TRUE);
-		_client.add(createKey("$SIZE"), _expiry, Integer.valueOf(is.size()));	
-		for (PNGTile pt : is) {
-			String key = createKey(pt.getAddress().getName());
-			_client.add(key, _expiry, pt);
+		_client.add(createKey("$SIZE"), _expiry, Integer.valueOf(is.size()));
+		_client.add(createKey("$KEYS"), _expiry, new ArrayList<TileAddress>(is.keySet()));
+		for (Map.Entry<TileAddress, PNGTile> me : is.entrySet()) {
+			String key = createKey(me.getKey().getName());
+			_client.add(key, _expiry, me.getValue());
+		}
+	}
+	
+	/**
+	 * Purges an image series from memcached.
+	 * @param is the ImageSeries
+	 * @throws DAOException if an error occured
+	 */
+	public void purge(ImageSeries is) throws DAOException {
+		checkConnection();
+		Long seriesDate = (is.getDate() == null) ? null : Long.valueOf(is.getDate().getTime());
+		
+		setBucket("mapTiles", is.getType(), seriesDate);
+		boolean hasSeries = (_client.get(createKey("$ME")) != null);
+		if (!hasSeries) return;
+		
+		@SuppressWarnings("unchecked")
+		Collection<TileAddress> keys = (Collection<TileAddress>) _client.get(createKey("$KEYS"));
+		if (keys != null) {
+			for (TileAddress addr : keys) {
+				String k = createKey(addr.getName());
+				_client.delete(k);
+			}
 		}
 	}
 	
@@ -68,6 +92,7 @@ public class SetTiles extends MemcachedDAO implements SeriesWriter {
 	 * Helper method to use CAS to add an imagery effective date.
 	 */
 	private void addImageDate(String type, final Date effDate) throws DAOException {
+		if (effDate == null) return;
 		setBucket("mapTiles", type);
 
 		CASMutation<Object> mutation = new CASMutation<Object>() {
