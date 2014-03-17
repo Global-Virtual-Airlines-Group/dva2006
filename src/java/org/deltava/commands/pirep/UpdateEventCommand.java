962db60491e74dc9d5a393e486223496aba9632e
Luke Kolin <luke@deltava.org>
2014-03-17 22:27:56 +0000

    Add updateEvent command
    git-svn-id: file:///home/svn/DVA2006@9238 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/commands/pirep/UpdateEventCommand.java
@@ -0,0 +1,70 @@
+// Copyright 2014 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.commands.pirep;
+
+import java.sql.Connection;
+
+import org.deltava.beans.flight.*;
+import org.deltava.beans.event.Event;
+
+import org.deltava.commands.*;
+import org.deltava.dao.*;
+
+import org.deltava.security.command.PIREPAccessControl;
+
+import org.deltava.util.StringUtils;
+
+/**
+ * A Web Site Command to update Online Events linked to a Flight Report. 
+ * @author Luke
+ * @version 5.3
+ * @since 5.3
+ */
+
+public class UpdateEventCommand extends AbstractCommand {
+
+	/**
+	 * Executes the command.
+	 * @param ctx the Command context
+	 * @throws CommandException if an unhandled error occurs
+	 */
+	@Override
+	public void execute(CommandContext ctx) throws CommandException {
+		try {
+			Connection con = ctx.getConnection();
+			
+			// Get the Flight Report
+			GetFlightReports frdao = new GetFlightReports(con);
+			FlightReport fr = frdao.get(ctx.getID());
+			if (fr == null)
+				throw notFoundException("Invalid Flight Report - " + ctx.getID());
+			
+			// Get the Event
+			int eventID = StringUtils.parse(ctx.getParameter("onlineEvent"), 0);
+			GetEvent edao = new GetEvent(con);
+			Event e = edao.get(eventID);
+			if ((e == null) && (eventID != 0))
+				throw notFoundException("Invalid Online Event - " + eventID);
+			
+			// Check our access
+			PIREPAccessControl ac = new PIREPAccessControl(ctx, fr);
+			ac.validate();
+			if (!ac.getCanDispose())
+				throw securityException("Cannot update Flight Report Online Event");
+			
+			// Set the event and save
+			SetFlightReport frwdao = new SetFlightReport(con);
+			fr.setDatabaseID(DatabaseID.EVENT, e.getID());
+			frwdao.write(fr);
+		} catch (DAOException de) {
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
+
+		// Return to the Flight Report
+		CommandResult result = ctx.getResult();
+		result.setType(ResultType.REDIRECT);
+		result.setURL("pirep", null, ctx.getID());
+		result.setSuccess(true);
+	}
+}
