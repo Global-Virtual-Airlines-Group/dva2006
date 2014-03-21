1bcc78a424f83e5d58369fdc11609bfe152be7a6
Luke Kolin <luke@deltava.org>
2014-03-21 14:58:52 +0000

    Use guaranteed character set
    git-svn-id: file:///home/svn/DVA2006@9260 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/file/DAO.java
+++ src/java/org/deltava/dao/file/DAO.java
@@ -1,53 +1,51 @@
-// Copyright 2005, 2006, 2008, 2010 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2008, 2010, 2014 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao.file;
 
 import java.io.*;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * An abstract class to support stream-based Data Access Objects.
  * @author Luke
- * @version 3.2
+ * @version 5.4
  * @since 1.0
  */
 
 public abstract class DAO {
 
-    private InputStream _is;
-    private int _size = 10240;
-    
-    private final Charset cs = Charset.forName("UTF-8");
-    
-    /**
-     * Initializes the Data Access Object with a particular input stream.
-     * @param is the input stream
-     */
-    protected DAO(InputStream is) {
-    	super();
-    	_is = is;
-    }
-    
-    /**
-     * Sets the buffer size for the connection.
-     * @param size the size in bytes
-     */
-    public void setBufferSize(int size) {
-        _size = Math.max(0, size);
-    }
+	private InputStream _is;
+	private int _size = 10240;
 
-    /**
-     * Returns a reader suitable for text reads on the HTTP connection.
-     * @return a BufferedReader
-     */
-    protected LineNumberReader getReader() {
-        return new LineNumberReader(new InputStreamReader(_is, cs), _size);
-    }
-    
-    /**
-     * Returns the input stream used for input.
-     * @return an InputStream
-     */
-    protected InputStream getStream() {
-    	return _is;
-    }
+	/**
+	 * Initializes the Data Access Object with a particular input stream.
+	 * @param is the input stream
+	 */
+	protected DAO(InputStream is) {
+		super();
+		_is = is;
+	}
+
+	/**
+	 * Sets the buffer size for the connection.
+	 * @param size the size in bytes
+	 */
+	public void setBufferSize(int size) {
+		_size = Math.max(0, size);
+	}
+
+	/**
+	 * Returns a reader suitable for text reads on the HTTP connection.
+	 * @return a BufferedReader
+	 */
+	protected LineNumberReader getReader() {
+		return new LineNumberReader(new InputStreamReader(_is, StandardCharsets.UTF_8), _size);
+	}
+
+	/**
+	 * Returns the input stream used for input.
+	 * @return an InputStream
+	 */
+	protected InputStream getStream() {
+		return _is;
+	}
 }
