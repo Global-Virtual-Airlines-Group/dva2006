57f685a86ca866096541ed8ae80197aaa1e20491
Luke Kolin <luke@deltava.org>
2014-05-11 14:07:19 +0000

    Misc changes
    git-svn-id: file:///home/svn/DVA2006@9369 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/acars/PlotLocalMap.java
+++ src/test/org/deltava/acars/PlotLocalMap.java
@@ -9,17 +9,18 @@ import org.deltava.util.system.SystemData;
 public class PlotLocalMap extends PlotMap {
 	
 	private static final int MIN_ZOOM = 10;
-	private static final int MAX_ZOOM = 12;
+	private static final int MAX_ZOOM = 13;
 	private static final int MAX_TILES = 163840;
 	
 	private static final List<String> _airports = Arrays.asList("ATL", "JFK", "LAX", "SLC", "YYZ", "MIA", "MSP", "DTW", "BOS", "SEA", "LAS", "SAN", "SFO", "NRT", 
-			"MCO", "DFW", "IAD", "CDG", "AMS", "HKX", "HKG", "HNL", "OGG", "CVG", "YUL", "LHR", "ICN", "BDA", "YVR", "SYD"); 
+			"MCO", "DFW", "IAD", "CDG", "AMS", "HKX", "HKG", "HNL", "OGG", "CVG", "YUL", "LHR", "ICN", "BDA", "YVR", "SYD", "ZRH"); 
 	
 	protected void setUp() throws Exception {
 		super.setUp();
-		_zooms.put(Integer.valueOf(10), new ProjectInfo(10, 30, 3));
-		_zooms.put(Integer.valueOf(11), new ProjectInfo(11, 22, 3));
-		_zooms.put(Integer.valueOf(12), new ProjectInfo(12, 20, 3));
+		_zooms.put(Integer.valueOf(10), new ProjectInfo(10, 36, 5));
+		_zooms.put(Integer.valueOf(11), new ProjectInfo(11, 28, 4));
+		_zooms.put(Integer.valueOf(12), new ProjectInfo(12, 22, 4));
+		_zooms.put(Integer.valueOf(13), new ProjectInfo(13, 18, 3));
 	}
 	
 	public void testPlotMap() throws Exception {
@@ -30,7 +31,7 @@ public class PlotLocalMap extends PlotMap {
 			airports.add(SystemData.getAirport(iata));
 		
 		AirportEntryFilter f = new AirportEntryFilter(MAX_ZOOM, 75, airports);
-		f.setMaxAltitude(10000);
+		f.setMaxAltitude(10200);
 		
 		// Load flight IDs
 		log.info("Loading Flight IDs");
