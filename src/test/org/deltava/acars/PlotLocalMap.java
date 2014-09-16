45d61a058a942d51d6ea3145ace42d572c24d8a1
Luke Kolin <luke@deltava.org>
2014-09-16 19:44:55 +0000

    Adjust levels and misc stuff
    git-svn-id: file:///home/svn/DVA2006@9483 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/acars/PlotLocalMap.java
+++ src/test/org/deltava/acars/PlotLocalMap.java
@@ -1,5 +1,6 @@
 package org.deltava.acars;
 
+import java.sql.*;
 import java.util.*;
 import java.util.concurrent.*;
 
@@ -10,32 +11,83 @@ public class PlotLocalMap extends PlotMap {
 	
 	private static final int MIN_ZOOM = 10;
 	private static final int MAX_ZOOM = 13;
-	private static final int MAX_TILES = 163840;
+	private static final int MAX_TILES = 321 * 1024;
 	
-	private static final List<String> _airports = Arrays.asList("ATL", "JFK", "LAX", "SLC", "YYZ", "MIA", "MSP", "DTW", "BOS", "SEA", "LAS", "SAN", "SFO", "NRT", 
-			"MCO", "DFW", "IAD", "CDG", "AMS", "HKX", "HKG", "HNL", "OGG", "CVG", "YUL", "LHR", "ICN", "BDA", "YVR", "SYD", "ZRH"); 
+	private static final List<String> _airports = Arrays.asList("YYZ", "MIA", "NRT",  "DFW", "IAD", "HKX", "HKG", "HNL", "OGG", "YUL", "LHR", "ICN", "BDA", "YVR", "SYD", "ZRH", "PPT", "ANC", "GVA"); 
 	
 	protected void setUp() throws Exception {
 		super.setUp();
-		_zooms.put(Integer.valueOf(10), new ProjectInfo(10, 36, 5));
-		_zooms.put(Integer.valueOf(11), new ProjectInfo(11, 28, 4));
-		_zooms.put(Integer.valueOf(12), new ProjectInfo(12, 22, 4));
-		_zooms.put(Integer.valueOf(13), new ProjectInfo(13, 18, 3));
+		_zooms.put(Integer.valueOf(10), new ProjectInfo(10, 32, 3));
+		_zooms.put(Integer.valueOf(11), new ProjectInfo(11, 24, 3));
+		_zooms.put(Integer.valueOf(12), new ProjectInfo(12, 16, 2));
+		_zooms.put(Integer.valueOf(13), new ProjectInfo(13, 10, 2));
+	}
+	
+	protected Collection<String> getPopularAirports(int max) throws Exception {
+		try (Connection c = DriverManager.getConnection(URL)) {
+			c.setAutoCommit(false);
+			try (Statement s = c.createStatement()) {
+				s.execute("create temporary table AP_COUNT (IATA CHAR(3) NOT NULL, CNT INTEGER UNSIGNED NOT NULL DEFAULT 0, PRIMARY KEY (IATA))");
+				s.execute("insert into AP_COUNT (SELECT AIRPORT_D, COUNT(ID) AS ACNT FROM FLIGHTS GROUP BY AIRPORT_D ORDER BY ACNT)");
+				s.execute("insert into AP_COUNT (SELECT AIRPORT_A, COUNT(ID) AS ACNT FROM FLIGHTS GROUP BY AIRPORT_A ORDER BY ACNT) on duplicate key update CNT=CNT+VALUES(CNT)");
+			}
+			
+			c.commit();
+			Collection<String> aps = new LinkedHashSet<String>();
+			try (Statement s = c.createStatement()) {
+				StringBuilder sqlBuf = new StringBuilder("SELECT IATA FROM AP_COUNT ORDER BY CNT DESC");
+				if (max > 0)
+					sqlBuf.append(" LIMIT ").append(max);
+				
+				try (ResultSet rs = s.executeQuery(sqlBuf.toString())) {
+					while (rs.next())
+						aps.add(rs.getString(1));
+				}
+			}
+			
+			// Write the track airport count
+			try (Statement s = c.createStatement()) {
+				s.execute("TRUNCATE TRACK_AIRPORTS");
+			}
+			
+			Collection<String> allAP = new LinkedHashSet<String>(aps); allAP.addAll(_airports);
+			StringBuilder buf = new StringBuilder("INSERT INTO TRACK_AIRPORTS (SELECT IATA, CNT FROM AP_COUNT WHERE IATA IN (");
+			for (Iterator<?> i = allAP.iterator(); i.hasNext(); ) {
+				i.next(); buf.append('?');
+				if (i.hasNext())
+					buf.append(',');
+			}
+			
+			buf.append("))"); 
+			try (PreparedStatement ps = c.prepareStatement(buf.toString())) {
+				int ofs = 0;
+				for (String apCode : allAP)
+					ps.setString(++ofs, apCode);
+				
+				ps.executeUpdate();
+			}
+			
+			c.commit();
+			return aps;
+		}
 	}
 	
 	public void testPlotMap() throws Exception {
 		
 		// Init the filter
+		Collection<String> apCodes = getPopularAirports(70);
+		apCodes.addAll(_airports);
+
 		Collection<Airport> airports = new ArrayList<Airport>();
-		for (String iata : _airports)
+		for (String iata : apCodes)
 			airports.add(SystemData.getAirport(iata));
 		
-		AirportEntryFilter f = new AirportEntryFilter(MAX_ZOOM, 75, airports);
-		f.setMaxAltitude(10200);
+		AirportEntryFilter f = new AirportEntryFilter(MAX_ZOOM, 120, airports);
+		f.setMaxAltitude(18900);
 		
 		// Load flight IDs
 		log.info("Loading Flight IDs");
-		BlockingQueue<Integer> IDs = getIDs(0, _airports);
+		BlockingQueue<Integer> IDs = getIDs(0, apCodes);
 		
 		// Load flights - separate thread per flight
 		SparseGlobalTile gt = new SparseGlobalTile(MAX_ZOOM, MAX_TILES);
