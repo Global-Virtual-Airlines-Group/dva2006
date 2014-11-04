721bc7c3467c333ca8c4e5bdb58ed8acbb6af460
Luke Kolin <luke@deltava.org>
2014-11-04 19:05:41 +0000

    Add MapEntry interface
    git-svn-id: file:///home/svn/DVA2006@9516 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/schedule/AbstractRoute.java
+++ src/java/org/deltava/beans/schedule/AbstractRoute.java
@@ -1,32 +1,39 @@
-// Copyright 2012 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2012, 2014 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.schedule;
 
 import java.util.*;
 
-import org.deltava.beans.GeoLocation;
-
+import org.deltava.beans.*;
 import org.deltava.util.GeoUtils;
 
 /**
  * A bean to store route frequency data.
  * @author Luke
- * @version 4.1
+ * @version 5.4
  * @since 4.1
  */
 
-public abstract class AbstractRoute implements RoutePair {
-	
+public abstract class AbstractRoute implements RoutePair, MapEntry {
+
 	private final Airport _aD;
 	private final Airport _aA;
-	
+	private final GeoLocation _mp;
+
 	private final String _code;
 	protected int _frequency;
-	
+
+	/**
+	 * Creates the route.
+	 * @param aD the departure Airport
+	 * @param aA the arrival Airport
+	 * @throws NullPointerException if aD or aA are null
+	 */
 	protected AbstractRoute(Airport aD, Airport aA) {
 		super();
 		_aD = aD;
 		_aA = aA;
-		
+		_mp = aD.getPosition().midPoint(aA);
+
 		// Build code
 		Collection<String> airports = new TreeSet<String>();
 		airports.add(aD.getICAO());
@@ -43,15 +50,23 @@ public abstract class AbstractRoute implements RoutePair {
 	public Airport getAirportA() {
 		return _aA;
 	}
-	
+
+	@Override
+	public double getLatitude() {
+		return _mp.getLatitude();
+	}
+
+	@Override
+	public double getLongitude() {
+		return _mp.getLongitude();
+	}
+
 	/**
-	 * Returns a list of route points for inclusion on a Google Map. If this route crosses the 
-	 * International Date Line, internal Great Circle calculations are used to get around a
-	 * Google Maps bug.
+	 * Returns the departure and arrival airports.
 	 * @return a Collection of GeoLocations
 	 */
 	public Collection<? extends GeoLocation> getPoints() {
-		return GeoUtils.greatCircle(_aD, _aA, 100);
+		return Arrays.asList(_aD, _aA);
 	}
 
 	@Override
@@ -66,11 +81,39 @@ public abstract class AbstractRoute implements RoutePair {
 	public int getFlights() {
 		return _frequency;
 	}
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.deltava.beans.MapEntry#getInfoBox()
+	 */
+	@Override
+	public String getInfoBox() {
+		StringBuilder buf = new StringBuilder("<div class=\"mapInfoBox\"><span class=\"bld\">");
+		buf.append(_aD.getName());
+		buf.append("</span> (");
+		buf.append(_aD.getIATA());
+		buf.append(" / ");
+		buf.append(_aD.getICAO());
+		buf.append(")<br /><span class=\"bld\">");
+		buf.append(_aA.getName());
+		buf.append("</span> (");
+		buf.append(_aA.getIATA());
+		buf.append(" / ");
+		buf.append(_aA.getICAO());
+		buf.append(")<br /><br />Distance: ");
+		buf.append(getDistance());
+		buf.append(" miles<br />");
+		buf.append(_frequency);
+		buf.append(" Flights</div>");
+		return buf.toString();
+	}
+
 	/**
 	 * Returns the route pair's hash code.
 	 * @see ScheduleRoute#toString()
 	 */
+	@Override
 	public int hashCode() {
 		return _code.hashCode();
 	}
@@ -78,9 +121,10 @@ public abstract class AbstractRoute implements RoutePair {
 	/**
 	 * Returns the route pair.
 	 */
+	@Override
 	public String toString() {
 		StringBuilder buf = new StringBuilder(_aD.getICAO());
-		buf.append('-');				
+		buf.append('-');
 		buf.append(_aA.getICAO());
 		return buf.toString();
 	}
