87ad6ef80cd4d74aeb4ba7e87dbfc94b874bb86c
Luke Kolin <luke@deltava.org>
2014-12-13 16:38:43 +0000

    Misc optimizations
    git-svn-id: file:///home/svn/DVA2006@9545 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/acars/TestRawTile.java
+++ src/test/org/deltava/acars/TestRawTile.java
@@ -5,19 +5,28 @@ import junit.framework.TestCase;
 public class TestRawTile extends TestCase {
 
 	public void testOverflow() {
-		RawTile rt = new RawTile();
+		RawTile srt = RawTile.getTile(255);
+		assertTrue(srt instanceof SmallRawTile);
+		testTile(srt);
+		
+		srt.set(3, 1, 254);
+		assertEquals(254, srt.getCount(3, 1));
+		srt.increment(3, 1);
+		assertEquals(255, srt.getCount(3, 1));
+		srt.increment(3, 1);
+		assertEquals(255, srt.getCount(3, 1));
+		
+		RawTile lrt = RawTile.getTile(2048);
+		assertTrue(lrt instanceof LargeRawTile);
+		testTile(lrt);
+	}
+	
+	private static void testTile(RawTile rt) {
 		rt.set(1, 1, 5);
 		assertEquals(5, rt.getCount(1, 1));
 		
 		rt.set(2, 1, 150);
 		assertEquals(150, rt.getCount(2, 1));
-		
-		rt.set(3, 1, 254);
-		assertEquals(254, rt.getCount(3, 1));
-		rt.increment(3, 1);
-		assertEquals(255, rt.getCount(3, 1));
-		rt.increment(3, 1);
-		assertEquals(255, rt.getCount(3, 1));
 		
 		rt.set(4,  1, 127);
 		assertEquals(127, rt.getCount(4, 1));
