3164bb1efc75874ab8eb04a5c61f847de0a14915
Luke Kolin <luke@deltava.org>
2015-01-01 20:43:08 +0000

    Add Tuple class
    git-svn-id: file:///home/svn/DVA2006@9556 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/util/Tuple.java
@@ -0,0 +1,52 @@
+// Copyright 2014 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.util;
+
+import org.deltava.util.cache.Cacheable;
+
+/**
+ * A utility class to create a tuple. 
+ * @author Luke
+ * @version 5.5
+ * @since 5.5
+ */
+
+public class Tuple<K, V> implements Cacheable {
+
+	private final K _k;
+	private final V _v;
+
+	private Tuple(K k, V v) {
+		super();
+		_k = k;
+		_v = v;
+	}
+	
+	public K getLeft() {
+		return _k;
+	}
+	
+	public V getRight() {
+		return _v;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder buf = new StringBuilder(String.valueOf(_k));
+		buf.append('#').append(String.valueOf(_v));
+		return buf.toString();
+	}
+	
+	@Override
+	public int hashCode() {
+		return toString().hashCode();
+	}
+	
+	public final static <K, V> Tuple<K, V> create(K k, V v) {
+		return new Tuple<K, V>(k, v);
+	}
+
+	@Override
+	public Object cacheKey() {
+		return toString();
+	}
+}
