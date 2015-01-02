73e8fb5027e2a65463ef0217ff3625034638171b
Luke Kolin <luke@deltava.org>
2015-01-02 16:44:40 +0000

    INTERIM - don't write winds to memcached until we figure out how to persist GFS grid size data (metadata table?)
    git-svn-id: file:///home/svn/DVA2006@9566 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/mc/GetWinds.java
+++ src/java/org/deltava/dao/mc/GetWinds.java
@@ -1,6 +1,8 @@
 // Copyright 2014 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao.mc;
 
+import java.util.concurrent.*;
+
 import org.deltava.beans.GeospaceLocation;
 import org.deltava.beans.wx.*;
 
@@ -9,7 +11,7 @@ import org.deltava.dao.DAOException;
 /**
  * A Data Access Object to load winds aloft data from memcached.
  * @author Luke
- * @version 5.4
+ * @version 5.5
  * @since 5.4
  */
 
@@ -23,7 +25,22 @@ public class GetWinds extends MemcachedDAO {
 	 */
 	public WindData getWinds(GeospaceLocation loc) throws DAOException {
 		PressureLevel lvl = PressureLevel.getClosest(loc.getAltitude());
-		setBucket("winds", lvl.toString());
+		
+		// Generate the key by rounding to the nearest half-degree
+		double rLat = Math.round(loc.getLatitude() / 2) * 2; double rLng = Math.round(loc.getLongitude() / 2) * 2;
+		WindData keygen = new WindData(lvl, rLat, rLng);
+		String key = String.valueOf(keygen.cacheKey());
+		
+		Future<Object> f = null;
+		try {
+			checkConnection();
+			setBucket("winds", lvl.toString());
+			f = _client.asyncGet(createKey(key));
+			return (WindData) f.get(100, TimeUnit.MILLISECONDS);
+		} catch (Exception e) {
+			cancel(f);
+		}
+		
 		throw new UnsupportedOperationException();
 	}
 }
