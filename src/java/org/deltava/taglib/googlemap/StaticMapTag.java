687b8b7cfb754808fcb6ef2261f89ce5abbbfc22
Luke Kolin <luke@deltava.org>
2015-02-04 16:34:10 +0000

    Add Google static map support
    git-svn-id: file:///home/svn/DVA2006@9627 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/taglib/googlemap/StaticMapTag.java
@@ -0,0 +1,153 @@
+// Copyright 2015 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.taglib.googlemap;
+
+import java.util.*;
+
+import javax.servlet.jsp.JspException;
+
+import org.deltava.beans.*;
+import org.deltava.taglib.html.ElementTag;
+import org.deltava.util.StringUtils;
+import org.deltava.util.system.SystemData;
+
+/**
+ * A JSP tag to insert a static Google Map on the page.
+ * @author Luke
+ * @version 6.0
+ * @since 6.0
+ */
+
+public class StaticMapTag extends ElementTag {
+	
+	private int _h;
+	private int _w;
+	
+	private final List<MapEntry> _mrks = new ArrayList<MapEntry>();
+	private final Collection<GeoLocation> _path = new ArrayList<GeoLocation>();
+
+	/**
+	 * Creates a new map IMG Tag.
+	 */
+	public StaticMapTag() {
+		super("img");
+		release();
+	}
+
+	/**
+	 * Releases the tag's state variables.
+	 */
+	@Override
+	public void release() {
+		super.release();
+		_mrks.clear();
+		_path.clear();
+	}
+	
+	/**
+	 * Adds markers to the map.
+	 * @param mrks a Collection of MapEntry beans
+	 */
+	public void setMarkers(Collection<MapEntry> mrks) {
+		_mrks.addAll(mrks);
+	}
+	
+	/**
+	 * Adds a path to the map.
+	 * @param path a Collection of GeoLocations
+	 */
+	public void setPath(Collection<GeoLocation> path) {
+		_path.addAll(path);
+	}
+	
+	/**
+	 * Sets the height of the map image.
+	 * @param h the height in pixels
+	 */
+	public void setH(int h) {
+		_h = Math.max(1, h);
+		setNumericAttr("height", _h, 1);
+	}
+	
+	/**
+	 * Sets the width of the map image.
+	 * @param w the width in pixels
+	 */
+	public void setW(int w) {
+		_w = Math.max(1, w);
+		setNumericAttr("width", _w, 1);
+	}
+	
+	@Override
+	public int doEndTag() throws JspException {
+		APIUsage.track(APIUsage.Type.STATIC);
+		
+		// Build params
+		StringBuilder buf = new StringBuilder("maptype=terrain&size=");
+		buf.append(_w).append('x').append(_h);
+		
+		// Add markers
+		if (!_mrks.isEmpty()) {
+			buf.append("&markers=");
+			MapEntry me = _mrks.get(0);
+			if (me instanceof IconMapEntry) {
+				buf.append("icon:http");
+				if (pageContext.getRequest().isSecure())
+					buf.append('s');
+				
+				IconMapEntry ime = (IconMapEntry) me;
+				if (ime.getPaletteCode() == 0) {
+					buf.append("://").append(pageContext.getRequest().getServerName()).append('/');
+					buf.append(SystemData.get("path.img")).append("/maps/pal");
+				} else
+					buf.append("://maps.google.com/mapfiles/kml/pal");
+				
+				buf.append(ime.getPaletteCode()).append("/icon").append(ime.getIconCode()).append(".png");
+			} else if (me instanceof MarkerMapEntry) {
+				buf.append("color:");
+				MarkerMapEntry mme = (MarkerMapEntry) me;
+				buf.append(mme.getIconColor());
+			}
+			
+			buf.append('|');
+			for (Iterator<? extends GeoLocation> i = _mrks.iterator(); i.hasNext(); ) {
+				GeoLocation loc = i.next();
+				buf.append(StringUtils.format(loc.getLatitude(), "##0.00000"));
+				buf.append(',');
+				buf.append(StringUtils.format(loc.getLongitude(), "##0.00000"));
+				if (i.hasNext())
+					buf.append('|');
+			}
+		}
+		
+		// Add path
+		if (!_path.isEmpty()) {
+			buf.append("&path=weight:2|geodesic:true|");
+			for (Iterator<GeoLocation> i = _path.iterator(); i.hasNext(); ) {
+				GeoLocation loc = i.next();
+				buf.append(StringUtils.format(loc.getLatitude(), "##0.00000"));
+				buf.append(',');
+				buf.append(StringUtils.format(loc.getLongitude(), "##0.00000"));
+				if (i.hasNext())
+					buf.append('|');
+			}
+		}
+		
+		// Build the image URL
+		StringBuilder url = new StringBuilder("http");
+		if (pageContext.getRequest().isSecure())
+			url.append('s');
+		
+		url.append("://maps.googleapis.com/maps/api/staticmap?").append(buf.toString());
+		_data.setAttribute("src", url.toString());
+		
+		try {
+			_out.print(_data.open(true, true));
+		} catch (Exception e) {
+			throw new JspException(e);
+		} finally {
+			release();
+		}
+		
+		return EVAL_PAGE;
+	}
+}
