58ae4e7a62e3d85853ee11bf73fce48f01abb756
Luke Kolin <luke@deltava.org>
2015-05-19 16:10:35 +0000

    FB Graph API 2.3 support
    git-svn-id: file:///home/svn/DVA2006@9875 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/http/GetFacebookData.java
+++ src/java/org/deltava/dao/http/GetFacebookData.java
@@ -1,4 +1,4 @@
-// Copyright 2010, 2012 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2010, 2012, 2015 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao.http;
 
 import java.io.*;
@@ -16,7 +16,7 @@ import org.deltava.util.system.SystemData;
 /**
  * A Data Access Object to fetch user data from Facebook via the Graph API. 
  * @author Luke
- * @version 4.2
+ * @version 6.0
  * @since 3.4
  */
 
@@ -107,6 +107,42 @@ public class GetFacebookData extends FacebookDAO {
 				String appID = ja.optString("id", "");
 				if (appID.equals(pageID))
 					return ja.optString("access_token");
+			}
+
+			return null;
+		} catch (Exception e) {
+			if (_warnMode) {
+				log.error(e.getClass().getName() + " - " + e.getMessage());
+				return null;
+			}
+			
+			throw new DAOException(e);
+		}
+	}
+
+	/**
+	 * Exchanges a Facebook short-term token for a long-term token.
+	 * @return a long-term token, or null
+	 * @throws DAOException if an error occurs
+	 */
+	public String getLongLifeToken() throws DAOException {
+
+		// Build the URL
+		StringBuilder buf = new StringBuilder(SystemData.get("users.facebook.url.accessToken"));
+		buf.append("?grant_type=fb_exchange_token&client_id=");
+		buf.append(SystemData.get("users.facebook.id"));
+		buf.append("&client_secret=");
+		buf.append(SystemData.get("users.facebook.secret"));
+		buf.append("&fb_exchange_token=");
+		buf.append(_token);
+
+		try {
+			init(buf.toString());
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(getIn()))) {
+				String data = br.readLine();
+				int pos = data.indexOf("_token=");
+				if (pos > 0)
+					return data.substring(pos + 7);
 			}
 
 			return null;
