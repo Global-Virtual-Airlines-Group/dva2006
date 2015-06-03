433edc0f3ab1c2bbf8228f8a8d13b7aea44652e3
Luke Kolin <luke@deltava.org>
2015-06-03 14:07:34 +0000

    Fix JS
    git-svn-id: file:///home/svn/DVA2006@9887 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/acarsLog.js
+++ static/js/acarsLog.js
@@ -2,35 +2,26 @@ golgotha.local.validate = function(f)
 {
 if (!golgotha.form.check()) return false;
 
-// Check the search type
-var sType;
-for (var x = 0; x < f.searchType.length; x++) {
-	if (f.searchType[x].checked)
-		sType = f.searchType[x].value;
+// Do different validation depending on the search type
+var isOK = false;
+if (f.pilotCode.length > 0) { 
+	golgotha.form.validate({f:f.pilotCode, l:4, t:'Pilot Code'});
+	isOK = true;
+}
+	
+if ((f.startDate.length > 0) || (f.endDate.length > 0)) {
+	golgotha.form.validate({f:f.startDate, l:10, t:'Start Date'});
+	golgotha.form.validate({f:f.endDate, l:10, t:'End Date'});
+	isOK = true;
+}
+	
+if ((f.viewCount) && (f.viewCount.length > 0)) {
+	golgotha.form.validate({f:f.viewCount, min:0, t:'Maximum Results'});
+	isOK = true;
 }
 
-// Do different validation depending on the search type
-switch (sType) {
-	case 'USR' :
-		golgotha.form.validate({f:f.pilotCode, l:4, t:'Pilot Code'});
-		break;
-	
-	case 'id' :
-		golgotha.form.validate({f:f.pilotCode, min:1000, t:'Pilot Database ID'});
-		break;
-		
-	case 'DATE' :
-		golgotha.form.validate({f:f.startDate, l:10, t:'Start Date'});
-		golgotha.form.validate({f:f.endDate, l:10, t:'End Date'});
-		break;
-		
-	case 'LATEST' :
-		golgotha.form.validate({f:f.viewCount, min:0, t:'Maximum Results'});
-		break;
-	
-	default:
-		throw new golgotha.event.ValidationError('Please select a Search Type.', f.searchType[0]);
-}
+if (!isOK)
+	throw new golgotha.event.ValidationError('Please select a Search Type.', f.searchType[0]);
 
 golgotha.form.submit(f);
 return true;
