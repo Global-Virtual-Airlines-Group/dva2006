7a599717f778e6264d2f2893bdd43c8b5de8bf52
Luke Kolin <luke@deltava.org>
2015-06-20 15:21:22 +0000

    Sync with ACARS code
    git-svn-id: file:///home/svn/DVA2006@9915 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/schedule/AlternateAirportHelper.java
+++ src/java/org/deltava/beans/schedule/AlternateAirportHelper.java
@@ -1,9 +1,11 @@
-// Copyright 2012 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2012, 2015 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.schedule;
 
 import java.util.*;
 
 import org.deltava.beans.*;
+
+import org.deltava.comparators.GeoComparator;
 
 import org.deltava.util.system.SystemData;
 
@@ -11,35 +13,43 @@ import org.deltava.util.system.SystemData;
  * A helper class to calculate diversion airports. Airports are given a score based on maximum
  * runway length in excess of minimums and distance from the destination.
  * @author Luke
- * @version 5.1
+ * @version 6.0
  * @since 4.2
  */
 
 @Helper(RoutePair.class)
 public class AlternateAirportHelper {
+	
+	// Optimum distance from destination in miles
+	private static final int MIN_DEST_DELTA = 55;
 
+	// static class
 	private AlternateAirportHelper() {
 		super();
 	}
 	
 	private static class Score implements Comparable<Score> {
 		private final int _distance;
+		private final int _airlineScore;
 		private final int _aggScore;
 		private final int _rwyScore;
 		private final int _dstScore;
 		
-		Score(int rwyDelta, int distance, int max) {
+		Score(int airlines, int rwyDelta, int distance, int max) {
 			super();
 			_rwyScore = Math.min(50, (rwyDelta / 250));
 			_dstScore = Math.min(50, ((max - distance) / 10));
-			_aggScore = ((_rwyScore * 4) + (_dstScore * 6)) / 10;
+			_airlineScore = Math.min(40, (airlines * 10));
+			_aggScore = ((_rwyScore * 9) + (_dstScore * 6) + (_airlineScore * 5)) / 20;
 			_distance = distance;
 		}
 		
+		@Override
 		public String toString() {
-			return "R:" + _rwyScore + ",D:" + _dstScore + ",DST:" + _distance;
+			return "RWY:" + _rwyScore + ",DST:" + _dstScore + ",AL:" + _airlineScore + ",DIST:" + _distance;
 		}
 		
+		@Override
 		public int compareTo(Score s2) {
 			int tmpResult = Integer.valueOf(_aggScore).compareTo(Integer.valueOf(s2._aggScore));
 			return (tmpResult == 0) ? Integer.valueOf(_distance).compareTo(Integer.valueOf(s2._distance)) : tmpResult;
@@ -54,27 +64,28 @@ public class AlternateAirportHelper {
 	 */
 	public static List<Airport> calculateAlternates(Aircraft ac, GeoLocation dst) {
 		
-		int maxDistance = Math.round(ac.getCruiseSpeed() / 0.75f);
-		Map<Score, Airport> results = new TreeMap<Score, Airport>(Collections.reverseOrder());
-		Collection<Airport> airports = new HashSet<Airport>(SystemData.getAirports().values());
+		int maxDistance = ac.getCruiseSpeed() * 3 / 4;
+		TreeSet<Airport> airports = new TreeSet<Airport>(new GeoComparator(dst));
+		airports.addAll(SystemData.getAirports().values());
 		String dstCode = (dst instanceof ICAOAirport) ? ((ICAOAirport) dst).getICAO() : null;
 		
 		// Filter airports
+		Map<Score, Airport> results = new TreeMap<Score, Airport>(Collections.reverseOrder());
 		for (Airport ap : airports) {
 			int airlines = ap.getAirlineCodes().size();
 			int distance = ap.getPosition().distanceTo(dst);
 			int rwyDelta = (ap.getMaximumRunwayLength() - ac.getTakeoffRunwayLength());
-			if ((rwyDelta < 0) || (distance > maxDistance) || (ap.getICAO().equals(dstCode)))
+			if ((distance > maxDistance) && (results.size() > 2))
+				break;
+			if ((rwyDelta < 0) || (ap.getICAO().equals(dstCode)))
 				continue;
 			
-			// Adjust distance based on airlines served
-			if (airlines == 0)
-				distance = Math.min(maxDistance, distance+120);
-			else if (airlines > 1)
-				distance -= (airlines * 50);
+			// Adjust the distance if we are less than the minimum delta
+			if ((dstCode != null) && (distance < MIN_DEST_DELTA))
+				distance += (Math.abs(distance - MIN_DEST_DELTA) * 4);
 			
 			// Calculate the score
-			Score s = new Score(rwyDelta, Math.max(10, distance), maxDistance);
+			Score s = new Score(airlines, rwyDelta, distance, maxDistance);
 			results.put(s, ap);
 		}
 		
