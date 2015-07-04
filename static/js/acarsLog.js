a2d26fb986f7553d9a9122b315b50e2f9a16c51b
Luke Kolin <luke@deltava.org>
2015-07-04 12:28:30 +0000

    Fix validation
    git-svn-id: file:///home/svn/DVA2006@9940 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/acarsLog.js
+++ static/js/acarsLog.js
@@ -9,19 +9,24 @@ if (f.pilotCode.length > 0) {
 	isOK = true;
 }
 	
-if ((f.startDate.length > 0) || (f.endDate.length > 0)) {
+if ((f.startDate.value.length > 0) || (f.endDate.value.length > 0)) {
 	golgotha.form.validate({f:f.startDate, l:10, t:'Start Date'});
 	golgotha.form.validate({f:f.endDate, l:10, t:'End Date'});
 	isOK = true;
 }
 	
-if ((f.viewCount) && (f.viewCount.length > 0)) {
+if ((f.viewCount) && (f.viewCount.value.length > 0)) {
 	golgotha.form.validate({f:f.viewCount, min:0, t:'Maximum Results'});
 	isOK = true;
 }
 
+if ((f.searchStr) && (f.searchStr.value.length > 0)) {
+	golgotha.form.validate({f:f.searchStr, l:4, t:'Search Text'});
+	isOK = true;
+}
+
 if (!isOK)
-	throw new golgotha.event.ValidationError('Please select a Search Type.', f.searchType[0]);
+	throw new golgotha.event.ValidationError('Please select a Search Type.');
 
 golgotha.form.submit(f);
 return true;
