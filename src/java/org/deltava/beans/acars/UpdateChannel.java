ed46ce88872216ae0514d0f0a0a3306c1e158ea4
Luke Kolin <luke@deltava.org>
2015-07-19 15:20:30 +0000

    Handle update channels properly
    git-svn-id: file:///home/svn/DVA2006@9948 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/acars/UpdateChannel.java
@@ -0,0 +1,13 @@
+// Copyright 2015 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.acars;
+
+/**
+ * An enumeration to store ACARS update channels.  
+ * @author Luke
+ * @version 6.1
+ * @since 6.1
+ */
+
+public enum UpdateChannel {
+	RELEASE, BETA
+}
