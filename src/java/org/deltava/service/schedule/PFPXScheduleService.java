572ad23216f6d778f1cc9e8cf283f27c32515271
Luke Kolin <luke@deltava.org>
2015-07-29 19:27:53 +0000

    Add PFPX schedule export
    git-svn-id: file:///home/svn/DVA2006@9978 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/service/schedule/PFPXScheduleService.java
@@ -0,0 +1,140 @@
+// Copyright 2015 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.service.schedule;
+
+import static javax.servlet.http.HttpServletResponse.*;
+
+import java.io.*;
+import java.util.*;
+import java.sql.Connection;
+
+import org.jdom2.*;
+
+import org.deltava.beans.schedule.*;
+
+import org.deltava.dao.*;
+import org.deltava.service.*;
+
+import org.deltava.util.*;
+import org.deltava.util.system.SystemData;
+
+/**
+ * A Web Serivce to export the Flight Schedule in PFPX format. 
+ * @author Luke
+ * @version 6.1
+ * @since 6.1
+ */
+
+public class PFPXScheduleService extends DownloadService {
+
+	/**
+	 * Executes the Web Service.
+	 * @param ctx the Web Service context
+	 * @return the HTTP status code
+	 * @throws ServiceException if an error occurs
+	 */
+	@Override
+	public int execute(ServiceContext ctx) throws ServiceException {
+		
+		// Check if the file exists
+		File cacheDir = new File(SystemData.get("schedule.cache"));
+		File f = new File(cacheDir, "pfpxsched.xml");
+		
+		// Check if the schedule has been updated
+		boolean isInvalid = true;
+		try {
+			GetMetadata mddao = new GetMetadata(ctx.getConnection());
+			String lastImport = mddao.get(SystemData.get("airline.code").toLowerCase() + ".schedule.import", "0");
+			long schedAge = StringUtils.parse(lastImport, 0) * 1000L;
+			isInvalid = (f.lastModified() == 0) || (schedAge > (f.lastModified() + 60_000));
+		} catch (DAOException de) {
+			throw error(SC_INTERNAL_SERVER_ERROR, de.getMessage(), de); 
+		} finally {
+			ctx.release();
+		}
+		
+		// Check the cache
+		if (!isInvalid) {
+			ctx.setHeader("Content-disposition", "attachment; filename=pfpxsched.xml");
+			ctx.setContentType("text/xml", "UTF-8");
+			ctx.setHeader("max-age", 3600);
+			sendFile(f, ctx.getResponse());
+			return SC_OK;
+		}
+		
+		// Load the flights
+		Collection<ScheduleEntry> sched = null;
+		Map<String, Aircraft> acInfo = new HashMap<String, Aircraft>();
+		try {
+			Connection con = ctx.getConnection();
+			
+			// Load all aircraft
+			GetAircraft acdao = new GetAircraft(con);
+			acInfo.putAll(CollectionUtils.createMap(acdao.getAircraftTypes(), "name"));
+			
+			// Load the schedule
+			GetSchedule sdao = new GetSchedule(con);
+			sched = sdao.export();
+		} catch (DAOException de) {
+			throw error(SC_INTERNAL_SERVER_ERROR, de.getMessage(), de);
+		} finally {
+			ctx.release();
+		}
+		
+		// Add to XML
+		Calendar cld = CalendarUtils.getInstance(null, true);
+		String today = String.valueOf(cld.get(Calendar.DAY_OF_WEEK));
+		Document doc = new Document();
+		Element re = new Element("PFPX_FLIGHT_LIST");
+		doc.setRootElement(re);
+		for (ScheduleEntry se : sched) {
+			Aircraft ac = acInfo.get(se.getEquipmentType());
+			if (ac == null) continue;
+			
+			Element fe = new Element("FLIGHT");
+			fe.addContent(XMLUtils.createElement("Airline", se.getAirline().getCode()));
+			fe.addContent(XMLUtils.createElement("FlightNumber", String.valueOf(se.getFlightNumber())));
+			fe.addContent(XMLUtils.createElement("From", se.getAirportD().getICAO()));
+			fe.addContent(XMLUtils.createElement("To", se.getAirportA().getICAO()));
+			fe.addContent(XMLUtils.createElement("InitAlt", "OPT"));
+			fe.addContent(XMLUtils.createElement("MTOW", String.valueOf(ac.getMaxTakeoffWeight())));
+			fe.addContent(XMLUtils.createElement("MLW", String.valueOf(ac.getMaxLandingWeight())));
+			fe.addContent(XMLUtils.createElement("STD", StringUtils.format(se.getDateTimeD().getDate(), "MM/dd/yyyy HH:mm")));
+			fe.addContent(XMLUtils.createElement("STA", StringUtils.format(se.getDateTimeA().getDate(), "MM/dd/yyyy HH:mm")));
+			fe.addContent(XMLUtils.createElement("MaxPax", String.valueOf(ac.getSeats())));
+			fe.addContent(XMLUtils.createElement("MaxCargo", "-1"));
+			fe.addContent(XMLUtils.createElement("Type", "1"));
+			fe.addContent(XMLUtils.createElement("Repetative", "1"));
+			fe.addContent(XMLUtils.createElement("IsMaster", "1"));
+			fe.addContent(XMLUtils.createElement("Unlimited", "Y"));
+			fe.addContent(XMLUtils.createElement("Days", today));
+			fe.addContent(XMLUtils.createElement("Payload", "-1"));
+			fe.addContent(XMLUtils.createElement("Notes", ""));
+			re.addContent(fe);
+		}
+		
+		// Write to the file and send to the client
+		try {
+			try (PrintWriter pw = new PrintWriter(new FileWriter(f))) {
+				pw.println(XMLUtils.format(doc, "UTF-8"));
+			}
+			
+			ctx.setHeader("Content-disposition", "attachment; filename=pfpxsched.xml");
+			ctx.setContentType("text/xml", "UTF-8");
+			ctx.setHeader("max-age", 3600);
+			sendFile(f, ctx.getResponse());
+		} catch (IOException ie) {
+			throw error(SC_INTERNAL_SERVER_ERROR, "I/O Error", false);
+		}
+		
+		return SC_OK;
+	}
+	
+	/**
+	 * Returns whether this web service requires authentication.
+	 * @return TRUE
+	 */
+	@Override
+	public boolean isSecure() {
+		return true;
+	}
+}
