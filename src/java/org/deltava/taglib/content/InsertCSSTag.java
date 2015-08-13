cb3f82eb19dafd99349de690f859b7315eef8dd9
Luke Kolin <luke@deltava.org>
2015-08-13 14:45:12 +0000

    SSL fixes
    git-svn-id: file:///home/svn/DVA2006@10003 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/content/InsertCSSTag.java
+++ src/java/org/deltava/taglib/content/InsertCSSTag.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2015 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.content;
 
 import java.net.*;
@@ -16,7 +16,7 @@ import org.deltava.util.system.SystemData;
 /**
  * A JSP tag to insert a Cascading Style Sheet.
  * @author Luke
- * @version 5.0
+ * @version 6.1
  * @since 1.0
  */
 
@@ -91,8 +91,7 @@ public class InsertCSSTag extends InsertMinifiedContentTag {
 		try {
 			// Build the resource name if host specified
 			if (_host != null) {
-				String protocol = pageContext.getRequest().getProtocol(); 
-				URL url = new URL(protocol.substring(0, protocol.indexOf('/')), _host, buf.toString());
+				URL url = new URL(pageContext.getRequest().isSecure() ? "https" : "http", _host, buf.toString());
 				buf = new StringBuilder(url.toString());
 			}
 			
