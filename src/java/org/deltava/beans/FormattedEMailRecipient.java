6433d6995a32e92897a74030660542280d3bee0c
Luke Kolin <luke@deltava.org>
2015-09-28 15:05:31 +0000

    Add formatted email recipient interface
    git-svn-id: file:///home/svn/DVA2006@10102 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/FormattedEMailRecipient.java
@@ -0,0 +1,51 @@
+// Copyright 2015 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans;
+
+import org.deltava.beans.schedule.Airport.Code;
+
+/**
+ * An interface to describe recipients of formatted e-mail messages, where
+ * numbers, airports and distances are formatted according to user preferences.
+ * @author Luke
+ * @version 6.2
+ * @since 6.2
+ */
+
+public interface FormattedEMailRecipient extends EMailAddress {
+
+	/**
+	 * Returns the preferred date format pattern.
+	 * @return the date format pattern
+	 */
+	public String getDateFormat();
+
+	/**
+	 * Returns the preferred time format pattern.
+	 * @return the time format pattern
+	 */
+	public String getTimeFormat();
+
+	/**
+	 * Returns the preferred number format pattern.
+	 * @return the number format pattern
+	 */
+	public String getNumberFormat();
+	
+	/**
+	 * Return this preferred Time Zone.
+	 * @return the time zone
+	 */
+	public TZInfo getTZ();
+	
+	/**
+	 * Returns the preferred distance unit.
+	 * @return the unit type
+	 */
+	public DistanceUnit getDistanceType();
+	
+	/**
+	 * Returns the preferred airport code type (IATA/ICAO).
+	 * @return the Airport Code type
+	 */
+	public Code getAirportCodeType();
+}
