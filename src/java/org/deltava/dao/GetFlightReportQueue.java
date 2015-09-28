68b39413f27c89242583c257d4b2c68de84149ab
Luke Kolin <luke@deltava.org>
2015-09-28 15:04:31 +0000

    Adjust disposal queue stats
    git-svn-id: file:///home/svn/DVA2006@10099 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetFlightReportQueue.java
+++ src/java/org/deltava/dao/GetFlightReportQueue.java
@@ -1,4 +1,4 @@
-// Copyright 2012 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2012, 2015 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao;
 
 import java.sql.*;
@@ -10,7 +10,7 @@ import org.deltava.beans.stats.DisposalQueueStats;
 /**
  * A Data Access Object to return Flight Report disposal queue information.
  * @author Luke
- * @version 5.0
+ * @version 6.1
  * @since 5.0
  */
 
@@ -26,49 +26,41 @@ public class GetFlightReportQueue extends DAO {
 
 	/**
 	 * Returns the size of the pending Flight Report queue for an Equipment program.
-	 * @param eqType the equipment program name
+	 * @param eqType the equipment program name or null for all programs
 	 * @return the number of pending or held PIREPs
 	 * @throws DAOException if a JDBC error occurs
 	 */
 	public DisposalQueueStats getDisposalQueueStats(String eqType) throws DAOException {
+		
+		// Build the SQL statement
+		StringBuilder sqlBuf = new StringBuilder("SELECT COUNT(PR.ID) AS CNT, AVG(TIMESTAMPDIFF(HOUR,PR.SUBMITTED,NOW())) AS AV, "
+				+ "MAX(TIMESTAMPDIFF(HOUR,PR.SUBMITTED,NOW())) AS MX, MIN(TIMESTAMPDIFF(HOUR,PR.SUBMITTED,NOW())) AS MN FROM "
+				+ "PIREPS PR");
+		if (eqType != null)
+			sqlBuf.append(", EQRATINGS ER WHERE (PR.EQTYPE=ER.RATED_EQ) AND (ER.RATING_TYPE=?) AND (ER.EQTYPE=?) AND");
+		else
+			sqlBuf.append(" WHERE");
+		
+		sqlBuf.append(" (PR.STATUS=?)");
 		try {
-			prepareStatementWithoutLimits("SELECT COUNT(PR.ID) AS CNT, AVG(TIMESTAMPDIFF(HOUR,PR.SUBMITTED,NOW())) FROM PIREPS PR, "
-					+ "EQRATINGS ER WHERE (PR.STATUS=?) AND (PR.EQTYPE=ER.RATED_EQ) AND (ER.RATING_TYPE=?) AND (ER.EQTYPE=?)");
-			_ps.setInt(1, FlightReport.SUBMITTED);
-			_ps.setInt(2, EquipmentType.Rating.PRIMARY.ordinal());
+			prepareStatementWithoutLimits(sqlBuf.toString());
+			if (eqType != null) {
+				_ps.setInt(1, EquipmentType.Rating.PRIMARY.ordinal());
+				_ps.setString(2, eqType);
+				_ps.setInt(3, FlightReport.SUBMITTED);
+			} else
+				_ps.setInt(1, FlightReport.SUBMITTED);
 
 			DisposalQueueStats dq = new DisposalQueueStats(new java.util.Date(), 0, 0);
 			try (ResultSet rs = _ps.executeQuery()) {
 				if (rs.next()) {
 					dq = new DisposalQueueStats(new java.util.Date(), rs.getInt(1), rs.getDouble(2));
-					dq.addCount(eqType, dq.getSize());
+					dq.setMinMax(rs.getDouble(4), rs.getDouble(3));
 				}
 			}
 
 			_ps.close();
-			return dq;
-		} catch (SQLException se) {
-			throw new DAOException(se);
-		}
-	}
-
-	/**
-	 * Returns statistics about the pending Flight Report queue.
-	 * @return a DisposalQueueStats bean
-	 * @throws DAOException if a JDBC error occurs
-	 */
-	public DisposalQueueStats getDisposalQueueStats() throws DAOException {
-		try {
-			prepareStatementWithoutLimits("SELECT COUNT(ID), AVG(TIMESTAMPDIFF(HOUR,SUBMITTED,NOW())) FROM PIREPS WHERE (STATUS=?)");
-			_ps.setInt(1, FlightReport.SUBMITTED);
-
-			DisposalQueueStats dq = new DisposalQueueStats(new java.util.Date(), 0, 0);
-			try (ResultSet rs = _ps.executeQuery()) {
-				if (rs.next())
-					dq = new DisposalQueueStats(new java.util.Date(), rs.getInt(1), rs.getDouble(2));
-			}
-
-			_ps.close();
+			
 			prepareStatementWithoutLimits("SELECT ER.EQTYPE, COUNT(PR.ID) AS CNT FROM PIREPS PR, EQRATINGS ER WHERE (PR.STATUS=?) "
 					+ "AND (PR.EQTYPE=ER.RATED_EQ) AND (ER.RATING_TYPE=?) GROUP BY ER.EQTYPE ORDER BY CNT DESC, ER.EQTYPE");
 			_ps.setInt(1, FlightReport.SUBMITTED);
