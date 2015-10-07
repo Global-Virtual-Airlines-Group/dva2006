3ba878333b2d2054a532438573bdc5829f02fa4a
Luke Kolin <luke@deltava.org>
2015-10-07 15:08:34 +0000

    Write more flights at once
    git-svn-id: file:///home/svn/DVA2006@10121 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/tasks/PositionArchiveTask.java
+++ src/java/org/deltava/tasks/PositionArchiveTask.java
@@ -36,23 +36,28 @@ public class PositionArchiveTask extends Task {
 	@Override
 	protected void execute(TaskContext ctx) {
 		log.info("Executing");
-		
 		try {
 			Connection con = ctx.getConnection();
 			
 			// Get the IDs
 			GetACARSPurge pdao = new GetACARSPurge(con);
-			pdao.setQueryMax(500);
+			pdao.setQueryMax(2500);
 			Collection<Integer> IDs = pdao.getDatabaseFlights();
 			
 			// Write each flight entry to the database
+			long startTime = System.currentTimeMillis();
 			GetACARSPositions prdao = new GetACARSPositions(con);
 			SetACARSArchive awdao = new SetACARSArchive(con);
 			for (Integer ID : IDs) {
 				String hash = Integer.toHexString(ID.intValue() % 2048);
 				File path = new File(SystemData.get("path.archive"), hash); path.mkdirs();
 				Collection<? extends RouteEntry> entries = prdao.getRouteEntries(ID.intValue(), true);
-				try (OutputStream os = new FileOutputStream(new File(path, Integer.toHexString(ID.intValue()) + ".dat"))) {
+				File dt = new File(path, Integer.toHexString(ID.intValue()) + ".dat");
+				if (dt.exists())
+					log.warn(dt.getAbsolutePath() + " already exists!");
+
+				// Write to the file system
+				try (OutputStream os = new FileOutputStream(dt)) {
 					try (OutputStream bos = new GZIPOutputStream(os, 8192)) {
 						SetSerializedPosition pwdao = new SetSerializedPosition(bos);
 						pwdao.archivePositions(ID.intValue(), entries);	
@@ -61,6 +66,15 @@ public class PositionArchiveTask extends Task {
 					awdao.clear(ID.intValue());
 				} catch (IOException ie) {
 					log.warn(ie.getMessage());
+				}
+				
+				// Reset the connections
+				if ((System.currentTimeMillis() - startTime) > 45_000) {
+					ctx.release();
+					con = ctx.getConnection();
+					prdao = new GetACARSPositions(con);
+					awdao = new SetACARSArchive(con);
+					startTime = System.currentTimeMillis();
 				}
 			}
 		} catch (DAOException de) {
