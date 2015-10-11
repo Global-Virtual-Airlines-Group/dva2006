57b864e223fb266b9eca8947372ba451acb57db7
Luke Kolin <luke@deltava.org>
2015-10-11 19:36:21 +0000

    Add TZ geolocation
    git-svn-id: file:///home/svn/DVA2006@10148 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetTimeZone.java
+++ src/java/org/deltava/dao/GetTimeZone.java
@@ -1,14 +1,15 @@
-// Copyright 2004, 2005, 2006, 2010, 2011 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2004, 2005, 2006, 2010, 2011, 2015 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao;
 
 import java.sql.*;
+import java.util.*;
 
-import org.deltava.beans.TZInfo;
+import org.deltava.beans.*;
 
 /**
  * A Data Access Object for loading Time Zones.
  * @author Luke
- * @version 4.1
+ * @version 6.2
  * @since 1.0
  */
 
@@ -30,13 +31,10 @@ public class GetTimeZone extends DAO {
     public int initAll() throws DAOException {
         try {
             prepareStatementWithoutLimits("SELECT CODE, NAME, ABBR FROM common.TZ");
-            
-            // Execute the query
             int rowsLoaded = 0;
             try (ResultSet rs = _ps.executeQuery()) {
             	while (rs.next()) {
-            		String id = rs.getString(1);
-            		TZInfo.init(id, rs.getString(2), rs.getString(3));
+            		TZInfo.init(rs.getString(1), rs.getString(2), rs.getString(3));
             		rowsLoaded++;
             	}
             }
@@ -46,5 +44,50 @@ public class GetTimeZone extends DAO {
         } catch (SQLException se) {
             throw new DAOException(se);
         }
+    }
+    
+    /**
+     * Provides the Time Zone for a given geographic location.
+     * @param loc the GeoLocation
+     * @return a TZInfo object, or null if unknown
+     * @throws DAOException if a JDBC error occurs
+     */
+    public TZInfo locate(GeoLocation loc) throws DAOException {
+    	String pt = formatLocation(loc); 
+    	try {
+    		TimeZone tz = null;
+    		prepareStatementWithoutLimits("SELECT NAME FROM geoip.TZ WHERE ST_Contains(DATA, ST_PointFromText(?,?))");
+    		_ps.setString(1, pt);
+    		_ps.setInt(2, GEO_SRID);
+    		try (ResultSet rs = _ps.executeQuery()) {
+    			if (rs.next()) tz = TimeZone.getTimeZone(rs.getString(1));
+    		}
+    		
+    		if (tz == null) {
+    			prepareStatementWithoutLimits("SELECT NAME FROM geoip.TZ WHERE ST_Intersects(DATA, ST_PointFromText(?,?))");
+    			_ps.setString(1, pt);
+        		_ps.setInt(2, GEO_SRID);
+        		try (ResultSet rs = _ps.executeQuery()) {
+        			if (rs.next()) tz = TimeZone.getTimeZone(rs.getString(1));
+        		}
+    		}
+    		
+    		if (tz == null)
+    			return null;
+    		
+    		// Converrt into our time zone
+    		Collection<TZInfo> allTZ = TZInfo.getAll();
+    		for (TZInfo zoneInfo : allTZ) {
+    			TimeZone zi = zoneInfo.getTimeZone();
+    			if (zi.getID().equals(tz.getID()) || zi.hasSameRules(tz))
+    				return zoneInfo;
+    			else if ((zi.getRawOffset() == tz.getRawOffset()) && (zi.observesDaylightTime() == tz.observesDaylightTime()))
+    				return zoneInfo;
+    		}
+    		
+    		return null;
+    	} catch (SQLException se) {
+    		throw new DAOException(se);
+    	}
     }
 }
