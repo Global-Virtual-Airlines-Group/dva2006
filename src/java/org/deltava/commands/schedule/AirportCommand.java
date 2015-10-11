2dd529968fb36411c6b7c5a85c3ba16e516d2cc5
Luke Kolin <luke@deltava.org>
2015-10-11 20:58:52 +0000

    Try to geolocate timezone
    git-svn-id: file:///home/svn/DVA2006@10150 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/schedule/AirportCommand.java
+++ src/java/org/deltava/commands/schedule/AirportCommand.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2015 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.schedule;
 
 import java.util.*;
@@ -19,7 +19,7 @@ import org.gvagroup.common.*;
 /**
  * A Web Site Command to modify Airport data.
  * @author Luke
- * @version 5.1
+ * @version 6.2
  * @since 1.0
  */
 
@@ -173,6 +173,10 @@ public class AirportCommand extends AbstractFormCommand {
 							a = new Airport((aCode.length() == 3) ? aCode : "", icao, "");
 							a.setLocation(al.getLatitude(), al.getLongitude());
 							a.setAltitude(al.getAltitude());
+							
+							// Look up the time zone
+							GetTimeZone tzdao = new GetTimeZone(con);
+							a.setTZ(tzdao.locate(a));
 						}
 					}
 
