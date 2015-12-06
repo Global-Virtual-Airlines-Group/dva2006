92324d0c753af28c28ac81d8c50074998d5f33ec
Luke Kolin <luke@deltava.org>
2015-12-06 17:09:07 +0000

    Add AirportInfo and gate editing
    git-svn-id: file:///home/svn/DVA2006@10356 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/commands/navdata/AirportInformationCommand.java
@@ -0,0 +1,103 @@
+// Copyright 2015 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.commands.navdata;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.sql.Connection;
+
+import org.deltava.beans.navdata.*;
+import org.deltava.beans.schedule.*;
+import org.deltava.beans.wx.METAR;
+
+import org.deltava.commands.*;
+import org.deltava.comparators.*;
+import org.deltava.dao.*;
+
+import org.deltava.util.*;
+import org.deltava.util.system.SystemData;
+
+/**
+ * A Web Site Command to display Airport runway and gate information.
+ * @author Luke
+ * @version 6.3
+ * @since 6.3
+ */
+
+public class AirportInformationCommand extends AbstractCommand {
+
+	/**
+	 * Executes the command.
+	 * @param ctx the Command context
+	 * @throws CommandException if an unhandled error occurs
+	 */
+	@Override
+	public void execute(CommandContext ctx) throws CommandException {
+		
+		// Get the airport
+		Airport a = SystemData.getAirport((String) ctx.getCmdParameter(ID, null));
+		if (a == null)
+			throw notFoundException("Unknown Airport - " + ctx.getCmdParameter(ID, null));
+		
+		try {
+			Connection con = ctx.getConnection();
+			
+			// Get weather
+			GetWeather wxdao = new GetWeather(con);
+			METAR m = wxdao.getMETAR(a, 25);
+			ctx.setAttribute("wx", m, REQUEST);
+			if (m == null)
+				m = new METAR();
+			
+			// Get runway data
+			Comparator<Runway> rC = new RunwayComparator(m.getWindDirection(), m.getWindSpeed());
+			GetNavData nddao = new GetNavData(con);
+			Map<String, Runway> allRwys = CollectionUtils.createMap(nddao.getRunways(a), "name");
+			
+			// Build airport bounding box
+			ctx.setAttribute("bbox", GeoUtils.getBoundingBox(allRwys.values()), REQUEST);
+			
+			// Load takeoff/landing runways
+			GetACARSRunways rwdao = new GetACARSRunways(con);
+			List<Runway> toRwys = rwdao.getPopularRunways(a, null, true); toRwys.sort(rC);
+			List<Runway> ldgRwys = rwdao.getPopularRunways(null, a, false); ldgRwys.sort(rC);
+			ctx.setAttribute("toRwys", toRwys, REQUEST);
+			ctx.setAttribute("ldgRwys", ldgRwys, REQUEST);
+
+			// Determine valid runways for winds
+			Collection<String> validRunwayIDs = new HashSet<String>();
+			Collection<String> rwyIDs = toRwys.stream().map(Runway::getName).collect(Collectors.toSet());
+			rwyIDs.addAll(ldgRwys.stream().map(Runway::getName).collect(Collectors.toSet()));
+			for (Iterator<Map.Entry<String, Runway>> i = allRwys.entrySet().iterator(); i.hasNext(); ) {
+				Map.Entry<String, Runway> me = i.next();
+				if (rwyIDs.contains(me.getKey()))
+					i.remove();
+				
+				double wDelta = GeoUtils.delta(me.getValue().getHeading(), m.getWindDirection());
+				double hW = StrictMath.cos(Math.toRadians(wDelta)) * m.getWindSpeed();
+				if (hW >= 0)
+					validRunwayIDs.add(me.getKey());
+			}
+			
+			// Save in request
+			ctx.setAttribute("airport", a, REQUEST);
+			ctx.setAttribute("localTime", new Date(), REQUEST);
+			ctx.setAttribute("runways", allRwys.values(), REQUEST);
+			ctx.setAttribute("validRunways", validRunwayIDs, REQUEST);
+			ctx.setAttribute("airlines", a.getAirlineCodes().stream().map(c -> SystemData.getAirline(c)).filter(al -> !al.getHistoric()).collect(Collectors.toCollection(TreeSet::new)), REQUEST);
+		} catch (DAOException de) {
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
+		
+		// Load active airports
+		Collection<Airport> airports = new TreeSet<Airport>(new AirportComparator(AirportComparator.NAME)); 
+		airports.addAll(SystemData.getAirports().values().stream().filter(ap -> !ap.getAirlineCodes().isEmpty()).collect(Collectors.toList()));
+		ctx.setAttribute("airports", airports, REQUEST);
+		
+		// Forward to the JSP
+		CommandResult result = ctx.getResult();
+		result.setURL("/jsp/navdata/airportInfo.jsp");
+		result.setSuccess(true);
+	}
+}
---
+++ src/java/org/deltava/service/navdata/GateUpdateService.java
@@ -0,0 +1,94 @@
+// Copyright 2015 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.service.navdata;
+
+import java.util.*;
+import java.sql.Connection;
+
+import org.json.*;
+
+import static javax.servlet.http.HttpServletResponse.*;
+
+import org.deltava.beans.Simulator;
+import org.deltava.beans.navdata.Gate;
+import org.deltava.beans.schedule.Airport;
+
+import org.deltava.dao.*;
+import org.deltava.service.*;
+import org.deltava.util.CollectionUtils;
+import org.deltava.util.system.SystemData;
+
+/**
+ * A Web Service to update preferred Gate data. 
+ * @author Luke
+ * @version 6.3
+ * @since 6.3
+ */
+
+public class GateUpdateService extends WebService {
+
+	/**
+	 * Executes the Web Service.
+	 * @param ctx the Web Service context
+	 * @return the HTTP status code
+	 * @throws ServiceException if an error occurs
+	 */
+	@Override
+	public int execute(ServiceContext ctx) throws ServiceException {
+		
+		// Get the airport
+		Airport a = SystemData.getAirport(ctx.getParameter("id"));
+		if (a == null)
+			return SC_NOT_FOUND;
+
+		JSONTokener jt = new JSONTokener(ctx.getParameter("data"));
+		JSONArray ja = new JSONArray(jt);
+		Simulator sim = Simulator.fromName(ctx.getParameter("sim"), Simulator.FSX);
+		try {
+			Connection con = ctx.getConnection();
+			GetGates gdao = new GetGates(con);
+			Map<String, Gate> gm = CollectionUtils.createMap(gdao.getAllGates(a, sim), "name");
+			
+			// Update based on data
+			Collection<Gate> updGates = new HashSet<Gate>(); 
+			for (int x = 0; x < ja.length(); x++) {
+				JSONObject go = ja.getJSONObject(x);
+				JSONArray ga = go.optJSONArray("airlines");
+				Gate g = gm.get(go.getString("id"));
+				g.clearAirlines();
+				for (int y = 0; (ga != null) && (y < ga.length()); y++)
+					g.addAirline(SystemData.getAirline(ga.getString(y)));
+				
+				g.setIntl(!g.getAirlines().isEmpty() && go.optBoolean("intl"));
+				updGates.add(g);
+			}
+			
+			// Save gate data
+			SetGates gwdao = new SetGates(con);
+			gwdao.update(updGates);
+		} catch (DAOException de) {
+			throw error(SC_INTERNAL_SERVER_ERROR, de.getMessage());
+		} finally {
+			ctx.release();
+		}
+		
+		return SC_OK;
+	}
+
+	/**
+	 * Returns whether this web service requires authentication.
+	 * @return TRUE always
+	 */
+	@Override
+	public final boolean isSecure() {
+		return true;
+	}
+
+	/**
+	 * Tells the Web Service Servlet not to log invocations of this service.
+	 * @return FALSE always
+	 */
+	@Override
+	public final boolean isLogged() {
+		return false;
+	}
+}
