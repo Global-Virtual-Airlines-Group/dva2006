39a4306e2b74b2ff4cbbdfc3b5dcb59c451d70c8
Luke Kolin <luke@deltava.org>
2016-03-11 16:06:25 +0000

    OnlineNetwork.fromName
    git-svn-id: file:///home/svn/DVA2006@10663 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/OnlineNetwork.java
+++ src/java/org/deltava/beans/OnlineNetwork.java
@@ -1,13 +1,26 @@
-// Copyright 2006, 2008, 2011, 2014 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2006, 2008, 2011, 2014, 2016 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans;
 
 /**
  * An interface to store Online Network constants.
  * @author Luke
- * @version 5.3
+ * @version 7.0
  * @since 1.0
  */
 
 public enum OnlineNetwork {
 	VATSIM, IVAO, FPI, INTVAS, ACARS;
+
+	/**
+	 * Parses an online network name.
+	 * @param name the name
+	 * @return an OnlineNetwork, or null
+	 */
+	public static OnlineNetwork fromName(String name) {
+		try {
+			return OnlineNetwork.valueOf(name.toUpperCase());
+		} catch (Exception e) {
+			return null;
+		}
+	}
 }
