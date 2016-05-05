912c57ce5568a89517dd0510653e08636aba7402
Luke Kolin <luke@deltava.org>
2016-05-05 15:16:11 +0000

    Add max-width param to navbar for tablets
    git-svn-id: file:///home/svn/DVA2006@10781 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/layout/SubMenuTag.java
+++ src/java/org/deltava/taglib/layout/SubMenuTag.java
@@ -1,16 +1,17 @@
-// Copyright 2009, 2010, 2015 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2009, 2010, 2015, 2016 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.layout;
 
-import java.io.IOException;
-
 import javax.servlet.jsp.*;
+import javax.servlet.jsp.tagext.TagSupport;
 
 import org.deltava.beans.system.DeviceType;
+
+import org.deltava.util.StringUtils;
 
 /**
  * A JSP tag to render a top level menu item in a JSP tag.
  * @author Luke
- * @version 6.3
+ * @version 7.0
  * @since 2.6
  */
 
@@ -38,9 +39,8 @@ public class SubMenuTag extends MenuElementTag {
 
 	/**
 	 * Writes a sub-menu &lt;URL&gt; element.
-	 * @throws IOException if an I/O error occurs
 	 */
-	void writeSubMenu() throws IOException {
+	void writeSubMenu() throws Exception {
 		if (_hasSubMenu || _renderTable)
 			return;
 		
@@ -48,8 +48,11 @@ public class SubMenuTag extends MenuElementTag {
 		JspWriter out = pageContext.getOut();
 		out.println();
 		out.print("<ul class=\"submenu\"");
-		if (_width > 0)
-			out.print(" style=\"width:" + String.valueOf(_width) + "px;\"");
+		if (_width > 0) {
+			out.print(" style=\"width:");
+			out.print(_width);
+			out.print("px;\"");
+		}
 		
 		out.println('>');
 	}
@@ -76,10 +79,13 @@ public class SubMenuTag extends MenuElementTag {
 			if (_renderTable)
 				out.print("<tr class=\"submenuTitle\"><td>");
 			else {
+				MenuTag parent = (MenuTag) TagSupport.findAncestorWithClass(this, MenuTag.class);
+				if (parent == null)
+					throw new JspException("Not contained within MenuTag");
+				
 				out.print("<ul class=\"menu\"");
 				if (_width > 0) {
-					
-					// Shrink if we're on a table
+					// Shrink if we're on a tablet
 					int w = _width;
 					if (getBrowserContext().getDeviceType() == DeviceType.TABLET) {
 						w -= 24;
@@ -89,7 +95,18 @@ public class SubMenuTag extends MenuElementTag {
 					
 					out.print(" style=\"width:");
 					out.print(w);
-					out.print("px;\"");
+					out.print("px");
+					
+					// Render maxWidth if present
+					String maxWidth = parent.getMaxMenuWidth();
+					if (!StringUtils.isEmpty(maxWidth)) {
+						out.print(";max-width:");
+						out.print(maxWidth);
+						if (!maxWidth.endsWith("%"))
+							out.print("px");
+					}
+					
+					out.print(";\"");
 				}
 				
 				out.print("><li class=\"submenuTitle\"><span>");
