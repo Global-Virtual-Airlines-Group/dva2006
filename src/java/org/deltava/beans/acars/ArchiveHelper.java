82bd6f212a2fefa2ee6f70bc75b02cbcec1ffff7
Luke Kolin <luke@deltava.org>
2016-05-06 15:49:02 +0000

    Add serialized route/track DAOs
    git-svn-id: file:///home/svn/DVA2006@10803 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/acars/ArchiveHelper.java
+++ src/java/org/deltava/beans/acars/ArchiveHelper.java
@@ -1,4 +1,4 @@
-// Copyright 2015 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2015, 2016 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.acars;
 
 import java.io.*;
@@ -9,7 +9,7 @@ import org.deltava.util.system.SystemData;
 /**
  * A utility class to handle ACARS position archive hash buckets.
  * @author Luke
- * @version 6.2
+ * @version 7.0
  * @since 6.2
  */
 
@@ -24,14 +24,35 @@ public class ArchiveHelper {
 	}
 
 	/**
-	 * Returns the File containing archived ACARS flight data.
+	 * Returns the File containing archived ACARS position data.
 	 * @param id the ACARS Flight ID
 	 * @return a File, which may or may not exist
 	 */
-	public static File getFile(int id) {
-		
+	public static File getPositions(int id) {
 		String hash = Integer.toHexString(id % BUCKETS);
 		File path = new File(SystemData.get("path.archive"), hash); path.mkdirs();
 		return new File(path, Integer.toHexString(id) + ".dat");
+	}
+	
+	/**
+	 * Returns the File containing archived route data.
+	 * @param id the Flight ID
+	 * @return a File, which may or may not exist
+	 */
+	public static File getRoute(int id) {
+		String hash = Integer.toHexString(id % BUCKETS);
+		File path = new File(SystemData.get("path.archive"), hash); path.mkdirs();
+		return new File(path, Integer.toHexString(id) + ".rte");
+	}
+	
+	/**
+	 * Returns the File containing archived online data.
+	 * @param id the Flight ID
+	 * @return a File, which may or may not exist
+	 */
+	public static File getOnline(int id) {
+		String hash = Integer.toHexString(id % BUCKETS);
+		File path = new File(SystemData.get("path.archive"), hash); path.mkdirs();
+		return new File(path, Integer.toHexString(id) + ".onl");
 	}
 }
