a4720b21ee2660cab874d6a4bf8d6b41cdf8322e
Luke Kolin <luke@deltava.org>
2016-05-22 13:37:10 +0000

    Use CAS cache and use async code
    git-svn-id: file:///home/svn/DVA2006@10882 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/mc/SetTrack.java
+++ src/java/org/deltava/dao/mc/SetTrack.java
@@ -1,15 +1,13 @@
 // Copyright 2016 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao.mc;
 
-import java.util.*;
-
 import org.apache.log4j.Logger;
 
-import org.deltava.beans.acars.RouteEntry;
+import org.deltava.beans.GeoLocation;
+import org.deltava.beans.schedule.GeoPosition;
 
 import org.deltava.util.MemcachedUtils;
-
-import net.spy.memcached.*;
+import org.deltava.util.cache.*;
 
 /**
  * A Data Access Object to save temporary ACARS track data to memcached.
@@ -21,35 +19,32 @@ import net.spy.memcached.*;
 public class SetTrack extends MemcachedDAO {
 	
 	private static final Logger log = Logger.getLogger(SetTrack.class);
-	private static boolean _logStack = true;
+	
+	private static final Cache<CacheableCollection<GeoLocation>> _casCache = CacheManager.getCollection(GeoLocation.class, "ACARSTrackCAS");
 
 	/**
 	 * Adds a route entry to memcached.
-	 * @param entry a RouteEntry
+	 * @param flightID the Flight ID
+	 * @param gl a GeoLocation
 	 */
-	public void write(RouteEntry entry) {
+	@SuppressWarnings("unchecked")
+	public void write(int flightID, GeoLocation gl) {
 		setBucket("acarsTrack");
+		String rawKey = String.valueOf(flightID);
+		String key = createKey(rawKey);
 		
-		CASMutation<Object> mutation = new CASMutation<Object>() {
-	    	@Override
-			public Collection<RouteEntry> getNewValue(Object current) {
-	    		@SuppressWarnings("unchecked")
-	            Collection<RouteEntry> c2 = new TreeSet<RouteEntry>((Collection<RouteEntry>) current);
-	            c2.add(entry);
-	            return c2;
-	        }
-	    };
-	    
 	    try {
-	    	CASMutator<Object> m = MemcachedUtils.getMutator();
-	    	m.cas(createKey(String.valueOf(entry.getID())), Collections.singletonList(entry), 1800, mutation);
-	    	_logStack = true;
+			CacheableCollection<GeoLocation> data = _casCache.get(rawKey);
+	    	if (data == null)
+	    		data = (CacheableCollection<GeoLocation>) MemcachedUtils.get(key, 175, false);
+	    	if (data == null)
+	    		data = new CacheableList<GeoLocation>(rawKey);
+	    	
+	    	data.add(new GeoPosition(gl));
+	    	_casCache.add(data);
+	    	MemcachedUtils.write(key, 900, data);
 	    } catch (Exception e) {
-	    	if (_logStack) {
-	    		log.error(e.getMessage(), e);
-	    		_logStack = false;
-	    	} else
-	    		log.warn(e.getMessage());
+    		log.warn(e.getMessage());
 	    }
 	}
 
@@ -58,7 +53,9 @@ public class SetTrack extends MemcachedDAO {
 	 * @param flightID the Flight ID
 	 */
 	public void clear(int flightID) {
+		String rawKey = String.valueOf(flightID);
 		setBucket("acarsTrack");
-		MemcachedUtils.delete(createKey(String.valueOf(flightID)));
+		MemcachedUtils.delete(createKey(rawKey));
+		_casCache.remove(rawKey);
 	}
 }
