23a04cc8bb2a52593ee814c316900be589dea36b
Luke Kolin <luke@deltava.org>
2016-08-01 13:55:41 +0000

    Change promotion logging
    git-svn-id: file:///home/svn/DVA2006@11012 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/flight/FlightPromotionHelper.java
+++ src/java/org/deltava/beans/flight/FlightPromotionHelper.java
@@ -1,21 +1,17 @@
-// Copyright 2009, 2010, 2011 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2009, 2010, 2011, 2016 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.flight;
-
-import org.apache.log4j.Logger;
 
 import org.deltava.beans.*;
 
 /**
  * A utility class to determine whether a Flight counts for promotion to Captain in a particular Equipment Type program.
  * @author Luke
- * @version 4.1
+ * @version 7.0
  * @since 2.7
  */
 
 @Helper(FlightReport.class)
 public final class FlightPromotionHelper {
-
-	private static final Logger log = Logger.getLogger(FlightPromotionHelper.class);
 
 	private final FlightReport _fr;
 	private String _comment;
@@ -44,33 +40,30 @@ public final class FlightPromotionHelper {
 	 * @see FlightPromotionHelper#getLastComment()
 	 */
 	public boolean canPromote(EquipmentType eq) {
-		_comment = null;
-		if ((_fr == null) || (eq == null))
+		if ((_fr == null) || (eq == null)) {
+			_comment = "No flight/equipment profile";
 			return false;
+		}
 
 		FDRFlightReport ffr = (_fr instanceof FDRFlightReport) ? (FDRFlightReport) _fr : null;
 		ACARSFlightReport afr = (_fr instanceof ACARSFlightReport) ? (ACARSFlightReport) _fr : null;
 		if ((ffr == null) && eq.getACARSPromotionLegs()) {
-			log.info(eq.getName() + " requires flights using ACARS/XACARS for promotion");
 			_comment = eq.getName() + " requires flights using ACARS/XACARS for promotion";
 			return false;
 		} else if (_fr.getDistance() < eq.getPromotionMinLength()) {
-			log.info("Minimum " + eq.getName() + " flight length is "  +  eq.getPromotionMinLength() + ", distance=" + _fr.getDistance());
 			_comment = "Minimum flight length for promotion to Captain in " + eq.getName() + " is "  +  eq.getPromotionMinLength() + " statute miles";
 			return false;
 		} else if (_fr.hasAttribute(FlightReport.ATTR_CHARTER)) {
-			log.info("Charter flights not eligible for promotion");
 			_comment = "Charter flights not eligible for promotion";
 		} else if ((afr != null) && (_fr.getDistance() < eq.getPromotionSwitchLength()) && ((afr.getTime(2) + afr.getTime(4)) > eq.getMaximumAccelTime())) {
-			log.info("Time at 1X = " + afr.getTime(1) + " time at 2X/4X = " + (afr.getTime(2) + afr.getTime(4)));
 			_comment = "Time at 1X = " + afr.getTime(1) + "sec, time at 2X/4X = " + (afr.getTime(2) + afr.getTime(4)) + "sec";
 			return false;
 		} else if ((afr != null) && (_fr.getDistance() >= eq.getPromotionSwitchLength()) && (afr.getTime(1) < eq.getMinimum1XTime())) {
-			log.info("Time at 1X = " + afr.getTime(1) + " time at 2X/4X = " + (afr.getTime(2) + afr.getTime(4)));
 			_comment = "Time at 1X = " + afr.getTime(1) + "sec, time at 2X/4X = " + (afr.getTime(2) + afr.getTime(4)) + "sec";
 			return false;
 		}
 
+		_comment = null;
 		return true;
 	}
 }
