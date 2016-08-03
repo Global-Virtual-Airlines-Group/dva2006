abd2a0863e5cbb3933831d50fd9854f3aa05213b
Luke Kolin <luke@deltava.org>
2016-08-03 02:12:03 +0000

    Golgotha 7.1 - Build 559
    git-svn-id: file:///home/svn/DVA2006@11037 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -4,16 +4,16 @@ package org.deltava.beans.system;
 /**
  * An interface to store version info constants.
  * @author Luke
- * @version 7.0
+ * @version 7.1
  * @since 1.0
  */
 
 public interface VersionInfo {
 
     public static final int MAJOR = 7;
-    public static final int MINOR = 0;
+    public static final int MINOR = 1;
     
-    public static final int BUILD = 558;
+    public static final int BUILD = 559;
     
     public static final String TXT_COPYRIGHT = "Copyright 2004 - 2016 Global Virtual Airlines Group. All Rights Reserved.";
     public static final String HTML_COPYRIGHT = "Copyright &copy; 2004 - 2016 <a rel=\"external\" class=\"small\" href=\"http://www.gvagroup.org/\">Global Virtual Airlines Group</a>. All Rights Reserved.";
