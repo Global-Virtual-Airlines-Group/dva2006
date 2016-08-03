9b61b007146b1216fa64127fcd6672f5cbc3263e
Luke Kolin <luke@deltava.org>
2016-08-03 02:10:48 +0000

    Adjust logging
    git-svn-id: file:///home/svn/DVA2006@11031 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/flight/FlightPromotionHelper.java
+++ src/java/org/deltava/beans/flight/FlightPromotionHelper.java
@@ -41,20 +41,21 @@ public final class FlightPromotionHelper {
 	 */
 	public boolean canPromote(EquipmentType eq) {
 		if ((_fr == null) || (eq == null)) {
-			_comment = "No flight/equipment profile";
+			_comment = "No flight/equipment type";
 			return false;
 		}
 
 		FDRFlightReport ffr = (_fr instanceof FDRFlightReport) ? (FDRFlightReport) _fr : null;
 		ACARSFlightReport afr = (_fr instanceof ACARSFlightReport) ? (ACARSFlightReport) _fr : null;
 		if ((ffr == null) && eq.getACARSPromotionLegs()) {
-			_comment = eq.getName() + " requires flights using ACARS/XACARS for promotion";
+			_comment = eq.getName() + " requires flights using ACARS/XACARS/simFDR for promotion";
 			return false;
 		} else if (_fr.getDistance() < eq.getPromotionMinLength()) {
 			_comment = "Minimum flight length for promotion to Captain in " + eq.getName() + " is "  +  eq.getPromotionMinLength() + " statute miles";
 			return false;
 		} else if (_fr.hasAttribute(FlightReport.ATTR_CHARTER)) {
 			_comment = "Charter flights not eligible for promotion";
+			return false;
 		} else if ((afr != null) && (_fr.getDistance() < eq.getPromotionSwitchLength()) && ((afr.getTime(2) + afr.getTime(4)) > eq.getMaximumAccelTime())) {
 			_comment = "Time at 1X = " + afr.getTime(1) + "sec, time at 2X/4X = " + (afr.getTime(2) + afr.getTime(4)) + "sec";
 			return false;
