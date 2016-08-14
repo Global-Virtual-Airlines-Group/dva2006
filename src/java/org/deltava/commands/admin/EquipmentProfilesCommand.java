bcf0ea29cc072820fe364ac36222fc3f30909499
Luke Kolin <luke@deltava.org>
2016-08-14 15:01:04 +0000

    Load CP profile seperately
    git-svn-id: file:///home/svn/DVA2006@11065 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/admin/EquipmentProfilesCommand.java
+++ src/java/org/deltava/commands/admin/EquipmentProfilesCommand.java
@@ -1,6 +1,10 @@
 // Copyright 2005, 2006, 2009, 2011, 2016 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.admin;
 
+import java.util.Collection;
+import java.sql.Connection;
+import java.util.stream.Collectors;
+
 import org.deltava.beans.EquipmentType;
 
 import org.deltava.commands.*;
@@ -9,7 +13,7 @@ import org.deltava.dao.*;
 /**
  * A Web Site command to display Equipment Type profiles.
  * @author Luke
- * @version 7.0
+ * @version 7.1
  * @since 1.0
  */
 
@@ -25,10 +29,18 @@ public class EquipmentProfilesCommand extends AbstractViewCommand {
 		
 		ViewContext<EquipmentType> vc = initView(ctx, EquipmentType.class);
 		try {
-			GetEquipmentType dao = new GetEquipmentType(ctx.getConnection());
+			Connection con = ctx.getConnection();
+			
+			// Load the profiles
+			GetEquipmentType dao = new GetEquipmentType(con);
 			dao.setQueryStart(vc.getStart());
 			dao.setQueryMax(vc.getCount());
 			vc.setResults(dao.getAll());
+			Collection<Integer> IDs = vc.getResults().stream().map(EquipmentType::getCPID).collect(Collectors.toSet());
+			
+			// Load the Chief Pilots
+			GetPilot pdao = new GetPilot(con);
+			ctx.setAttribute("pilots", pdao.getByID(IDs, "PILOTS"), REQUEST);
 		} catch (DAOException de) {
 			throw new CommandException(de);
 		} finally {
