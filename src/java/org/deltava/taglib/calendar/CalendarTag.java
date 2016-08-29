fb1d4b7c5e98940d5bdd56096cf5f0b6af03bb43
Luke Kolin <luke@deltava.org>
2016-08-29 20:03:41 +0000

    Selectively render forward/back tags
    git-svn-id: file:///home/svn/DVA2006@11093 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/calendar/CalendarTag.java
+++ src/java/org/deltava/taglib/calendar/CalendarTag.java
@@ -5,7 +5,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
+import java.time.temporal.*;
 
 import javax.servlet.jsp.*;
 import javax.servlet.jsp.tagext.*;
@@ -18,12 +18,12 @@ import org.deltava.util.*;
 /**
  * A JSP tag to display a calendar view table.
  * @author Luke
- * @version 7.0
+ * @version 7.1
  * @since 1.0
  */
 
 abstract class CalendarTag extends TagSupport {
-	
+
 	private final DateTimeFormatter _df = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 	private static final Collection<String> RESERVED_PARAMS = Collections.singleton("startDate");
 
@@ -35,7 +35,7 @@ abstract class CalendarTag extends TagSupport {
 	protected ZonedDateTime _currentDate;
 	protected ZonedDateTime _today;
 	protected ZoneId _tz;
-	
+
 	private String _currentDateAttr;
 
 	protected final Collection<CalendarEntry> _entries = new TreeSet<CalendarEntry>();
@@ -47,14 +47,14 @@ abstract class CalendarTag extends TagSupport {
 	protected String _contentClass;
 	private String _scrollRowClass;
 	private String _cmdName;
-	
+
 	protected int _border;
 	protected boolean _showDaysOfWeek = true;
 	private boolean _showScrollTags = true;
-	
+
 	protected XMLRenderer _table;
 	protected XMLRenderer _day;
-	
+
 	protected ChronoUnit _intervalType = ChronoUnit.DAYS;
 	protected int _intervalLength = 7;
 
@@ -63,21 +63,21 @@ abstract class CalendarTag extends TagSupport {
 	 * @param dt the start date/time
 	 */
 	public abstract void setStartDate(ZonedDateTime dt);
-	
+
 	/**
 	 * Returns the label for the scroll backwards link.
 	 * @return the link label
 	 * @see CalendarTag#getForwardLabel()
 	 */
 	protected abstract String getBackLabel();
-	
+
 	/**
 	 * Returns the label for the scroll forwards link
 	 * @return the link label
 	 * @see CalendarTag#getBackLabel()
 	 */
 	protected abstract String getForwardLabel();
-	
+
 	/**
 	 * Calculates the CSS class(es) for the content table cell.
 	 * @param dt the cell date
@@ -91,13 +91,13 @@ abstract class CalendarTag extends TagSupport {
 			if (isToday)
 				buf.append(' ');
 		}
-		
+
 		if (isToday)
 			buf.append("calendarToday");
-		
+
 		return buf.toString();
 	}
-	
+
 	/**
 	 * Calculates the CSS class(es) for the header table cell.
 	 * @param dt the cell date
@@ -111,16 +111,16 @@ abstract class CalendarTag extends TagSupport {
 			if (isToday)
 				buf.append(' ');
 		}
-		
+
 		if (isToday)
 			buf.append("calendarToday");
-		
+
 		return buf.toString();
 	}
-	
+
 	/**
-	 * Calculcates the end date based on the start date and a particular interval amount. <i>This must be called
-	 * by a subclass for the forward/backward links to work properly</i>.
+	 * Calculcates the end date based on the start date and a particular interval amount. <i>This must be called by a subclass for the
+	 * forward/backward links to work properly</i>.
 	 * @param intervalType the interval type (use Calendar constants)
 	 * @param amount the size of the interval
 	 * @see java.util.Calendar
@@ -130,7 +130,7 @@ abstract class CalendarTag extends TagSupport {
 		_intervalType = intervalType;
 		_intervalLength = amount;
 	}
-	
+
 	/**
 	 * Sets the request attribute name for the current date.
 	 * @param attrName the request attribute name
@@ -138,7 +138,7 @@ abstract class CalendarTag extends TagSupport {
 	public void setDate(String attrName) {
 		_currentDateAttr = attrName;
 	}
-	
+
 	/**
 	 * Sets the CSS class name for the view table.
 	 * @param cName the CSS class name
@@ -146,7 +146,7 @@ abstract class CalendarTag extends TagSupport {
 	public void setTableClass(String cName) {
 		_tableClass = cName;
 	}
-	
+
 	/**
 	 * Sets the CSS ID for the view table.
 	 * @param id the ID
@@ -154,7 +154,7 @@ abstract class CalendarTag extends TagSupport {
 	public void setTableID(String id) {
 		_tableID = id;
 	}
-	
+
 	/**
 	 * Sets the CSS class name for the view table title bar.
 	 * @param cName the CSS class name
@@ -162,7 +162,7 @@ abstract class CalendarTag extends TagSupport {
 	public void setTopBarClass(String cName) {
 		_topBarClass = cName;
 	}
-	
+
 	/**
 	 * Sets the CSS class name for the view table day bar.
 	 * @param cName the CSS class name
@@ -170,7 +170,7 @@ abstract class CalendarTag extends TagSupport {
 	public void setDayBarClass(String cName) {
 		_dayBarClass = cName;
 	}
-	
+
 	/**
 	 * Sets the CSS class name for the view table content bar.
 	 * @param cName the CSS class name
@@ -178,7 +178,7 @@ abstract class CalendarTag extends TagSupport {
 	public void setContentClass(String cName) {
 		_contentClass = cName;
 	}
-	
+
 	/**
 	 * Sets the CSS class name for the forwad/backward scroll links.
 	 * @param cName the CSS class name
@@ -186,7 +186,7 @@ abstract class CalendarTag extends TagSupport {
 	public void setScrollClass(String cName) {
 		_scrollRowClass = cName;
 	}
-	
+
 	/**
 	 * Sets the command name to use when scrolling the view.
 	 * @param cmdName the command name
@@ -194,7 +194,7 @@ abstract class CalendarTag extends TagSupport {
 	public void setCmd(String cmdName) {
 		_cmdName = cmdName;
 	}
-	
+
 	/**
 	 * Sets whether the days of the week should be displayed below the title bar.
 	 * @param showDOW TRUE if the days of the week should be displayed, otherwise FALSE
@@ -202,7 +202,7 @@ abstract class CalendarTag extends TagSupport {
 	public void setShowDaysOfWeek(boolean showDOW) {
 		_showDaysOfWeek = showDOW;
 	}
-	
+
 	/**
 	 * Sets whether to display the forward/backward scroll tags.
 	 * @param showScroll TRUE if forward/back links are displayed, otherwise FALSE
@@ -210,18 +210,17 @@ abstract class CalendarTag extends TagSupport {
 	public void setScrollTags(boolean showScroll) {
 		_showScrollTags = showScroll;
 	}
-	
-    /**
-     * Sets the BORDER value for this table.
-     * @param border the border width attribute value
-     */
-    public void setBorder(int border) {
-    	_border = border;
-    }
-    
+
 	/**
-	 * Sets the entries to display in this calendar view table. Entries outside the table's date range will not be
-	 * added.
+	 * Sets the BORDER value for this table.
+	 * @param border the border width attribute value
+	 */
+	public void setBorder(int border) {
+		_border = border;
+	}
+
+	/**
+	 * Sets the entries to display in this calendar view table. Entries outside the table's date range will not be added.
 	 * @param entries a Collection of CalendarEntry beans
 	 * @see CalendarTag#getCurrentEntries()
 	 */
@@ -231,7 +230,7 @@ abstract class CalendarTag extends TagSupport {
 				_entries.add(ce);
 		}
 	}
-	
+
 	/**
 	 * Sets the page context for the tag, and sets the user's time zone.
 	 * @param ctx the JSP page context
@@ -239,38 +238,38 @@ abstract class CalendarTag extends TagSupport {
 	@Override
 	public void setPageContext(PageContext ctx) {
 		super.setPageContext(ctx);
-		
+
 		// Determine the user's time zone
 		HttpServletRequest hreq = (HttpServletRequest) ctx.getRequest();
 		Person usr = (Person) hreq.getUserPrincipal();
 		_tz = (usr != null) ? usr.getTZ().getZone() : ZoneId.systemDefault();
-		
+
 		// Determine today's date in user's time zone
 		_today = ZonedDateTime.ofInstant(Instant.now(), _tz).truncatedTo(ChronoUnit.DAYS);
 	}
-	
-    /*
-     * Helper method to bundle request parameters into a URL string.
-     */
-    private String buildURL(Map<String, Object> params) {
-        StringBuilder url = new StringBuilder("/");
-        url.append(_cmdName);
-        url.append(".do?");
-        
-        // Loop through the parameters
-        for (Iterator<String> i = params.keySet().iterator(); i.hasNext(); ) {
-            String pName = i.next();
-            String[] pValues = (String[]) params.get(pName);
-            url.append(StringUtils.stripInlineHTML(pName));
-            url.append('=');
-            url.append(StringUtils.stripInlineHTML(pValues[0]));
-            if (i.hasNext())
-                url.append("&amp;");
-        }
-        
-        // Return the string
-        return url.toString();
-    }
+
+	/*
+	 * Helper method to bundle request parameters into a URL string.
+	 */
+	private String buildURL(Map<String, Object> params) {
+		StringBuilder url = new StringBuilder("/");
+		url.append(_cmdName);
+		url.append(".do?");
+
+		// Loop through the parameters
+		for (Iterator<String> i = params.keySet().iterator(); i.hasNext();) {
+			String pName = i.next();
+			String[] pValues = (String[]) params.get(pName);
+			url.append(StringUtils.stripInlineHTML(pName));
+			url.append('=');
+			url.append(StringUtils.stripInlineHTML(pValues[0]));
+			if (i.hasNext())
+				url.append("&amp;");
+		}
+
+		// Return the string
+		return url.toString();
+	}
 
 	/**
 	 * Returns all Calendar entries for the currently rendered Date, from midnight to 11:59PM.
@@ -285,23 +284,21 @@ abstract class CalendarTag extends TagSupport {
 	}
 
 	/**
-	 * Determines if we have further days to render in the calendar. Subclasses are responsible for
-	 * opening and closing the table cells.
-	 * @return EVAL_BODY_AGAIN if current date is before endDate, otherwise SKIP_BODY 
+	 * Determines if we have further days to render in the calendar. Subclasses are responsible for opening and closing the table cells.
+	 * @return EVAL_BODY_AGAIN if current date is before endDate, otherwise SKIP_BODY
 	 * @throws JspException never
 	 */
 	@Override
 	public int doAfterBody() throws JspException {
-		_currentDate= _currentDate.plus(1, ChronoUnit.DAYS);
+		_currentDate = _currentDate.plus(1, ChronoUnit.DAYS);
 		if (_currentDateAttr != null)
 			pageContext.setAttribute(_currentDateAttr, _currentDate, PageContext.REQUEST_SCOPE);
-			
+
 		return _currentDate.isBefore(_endDate) ? EVAL_BODY_AGAIN : SKIP_BODY;
 	}
 
 	/**
-	 * Starts the Calendar tag by setting the current date to render. Subclasses are responsible for
-	 * opening and closing the table cells.
+	 * Starts the Calendar tag by setting the current date to render. Subclasses are responsible for opening and closing the table cells.
 	 * @return TagSupport.EVAL_BODY_INCLUDE always
 	 * @throws JspException never
 	 */
@@ -310,11 +307,11 @@ abstract class CalendarTag extends TagSupport {
 
 		// Generate the current date
 		_currentDate = _startDate.truncatedTo(ChronoUnit.DAYS);
-		
+
 		// Save the current date in the request
 		if (_currentDateAttr != null)
 			pageContext.setAttribute(_currentDateAttr, _currentDate, PageContext.REQUEST_SCOPE);
-		
+
 		// Generate the view table
 		_table = new XMLRenderer("table");
 		_table.setAttribute("id", _tableID);
@@ -326,74 +323,85 @@ abstract class CalendarTag extends TagSupport {
 		_out = pageContext.getOut();
 		return EVAL_BODY_INCLUDE;
 	}
-	
+
 	/**
-	 * Ends the Calendar tag by adding the scroll forward/backward link row. Subclasses are responsible
-	 * for closing the current table row and closing the table.
+	 * Ends the Calendar tag by adding the scroll forward/backward link row. Subclasses are responsible for closing the current table row
+	 * and closing the table.
 	 * @return EVAL_PAGE always
-	 * @throws JspException if an I/O error occurs  
+	 * @throws JspException if an I/O error occurs
 	 */
 	@Override
 	public int doEndTag() throws JspException {
 		if (!_showScrollTags)
 			return EVAL_PAGE;
-		
-    	// Get the URL parameters
-        Map<String, Object> params = new HashMap<String, Object>(pageContext.getRequest().getParameterMap());
-        params.keySet().removeAll(RESERVED_PARAMS);
-        
-        // Calculate the next and previous page startDates
-        ZonedDateTime fd = _startDate.plus(_intervalLength, _intervalType);
-        ZonedDateTime bd = _startDate.minus(_intervalLength, _intervalType);
-        
-        try {
-        	// Build the backward URL
-        	XMLRenderer backURL = new XMLRenderer("a");
-        	params.put("startDate", new String[] { _df.format(bd) } );
-        	backURL.setAttribute("href", buildURL(params));
 
-        	// Build the forward URL
-        	XMLRenderer fwdURL = new XMLRenderer("a");
-        	params.put("startDate", new String[] { _df.format(fd) } );
-        	fwdURL.setAttribute("href", buildURL(params));
-        	
-        	// Render the scroll tag bar row
-        	XMLRenderer scrollRow = new XMLRenderer("tr");
-        	scrollRow.setAttribute("class", _scrollRowClass);
-        	_out.println(scrollRow.open(true));
-        	
-        	// Render the scroll back cell and link
-        	XMLRenderer sBackCell = new XMLRenderer("td");
-        	sBackCell.setAttribute("class", "left");
-        	sBackCell.setAttribute("colspan", "3");
-        	_out.print(sBackCell.open(true));
-        	_out.print(backURL.open(true));
-        	_out.print(getBackLabel());
-        	_out.print(backURL.close());
-        	_out.print(sBackCell.close());
-        	
-        	// Render the middle cell
-        	_out.print("<td>&nbsp;</td>");
-        	
-        	// Render the scroll forward cell and link
-        	XMLRenderer sFwdCell = new XMLRenderer("td");
-        	sFwdCell.setAttribute("class", "right");
-        	sFwdCell.setAttribute("colspan", "3");
-        	_out.print(sFwdCell.open(true));
-        	_out.print(fwdURL.open(true));
-        	_out.print(getForwardLabel());
-        	_out.print(fwdURL.close());
-        	_out.print(sFwdCell.close());
+		// Get the URL parameters
+		Map<String, Object> params = new HashMap<String, Object>(pageContext.getRequest().getParameterMap());
+		params.keySet().removeAll(RESERVED_PARAMS);
 
-        	// Close the row
-        	_out.println(scrollRow.close());
-        } catch (Exception ie) {
-        	throw new JspException(ie);
-        } finally {
-        	release();
-        }
-        
-        return EVAL_PAGE;
+		// Calculate the next and previous page startDates
+		ZonedDateTime fd = _startDate.plus(_intervalLength, _intervalType);
+		ZonedDateTime bd = _startDate.minus(_intervalLength, _intervalType);
+		Duration d = Duration.between(Instant.now(), fd.toInstant());
+		boolean hasPrev = (bd.get(ChronoField.YEAR) < 2000);
+		boolean hasNext = !d.isNegative() && (d.toDays() > 740);
+
+		try {
+			// Build the backward URL
+			XMLRenderer backURL = new XMLRenderer("a");
+			params.put("startDate", new String[] { _df.format(bd) });
+			backURL.setAttribute("href", buildURL(params));
+
+			// Build the forward URL
+			XMLRenderer fwdURL = new XMLRenderer("a");
+			params.put("startDate", new String[] { _df.format(fd) });
+			fwdURL.setAttribute("href", buildURL(params));
+
+			// Render the scroll tag bar row
+			XMLRenderer scrollRow = new XMLRenderer("tr");
+			scrollRow.setAttribute("class", _scrollRowClass);
+			_out.println(scrollRow.open(true));
+
+			// Render the scroll back cell and link
+			XMLRenderer sBackCell = new XMLRenderer("td");
+			sBackCell.setAttribute("class", "left");
+			sBackCell.setAttribute("colspan", "3");
+			_out.print(sBackCell.open(true));
+			if (hasPrev) {
+				_out.print(backURL.open(true));
+				_out.print(getBackLabel());
+				_out.print(backURL.close());
+			} else
+				_out.print("&nbsp;");
+			
+			_out.print(sBackCell.close());
+
+			// Render the middle cell
+			_out.print("<td>&nbsp;</td>");
+
+			// Render the scroll forward cell and link
+			XMLRenderer sFwdCell = new XMLRenderer("td");
+			sFwdCell.setAttribute("class", "right");
+			sFwdCell.setAttribute("colspan", "3");
+			_out.print(sFwdCell.open(true));
+			if (hasNext) {
+				_out.print(fwdURL.open(true));
+				_out.print(getForwardLabel());
+				_out.print(fwdURL.close());
+			} else
+				_out.print("&nbsp;");
+			
+			_out.print(sFwdCell.close());
+
+			// Close the row
+			_out.println(scrollRow.close());
+		} catch (Exception ie) {
+			throw new JspException(ie);
+		} finally {
+			release();
+		}
+
+		return EVAL_PAGE;
 	}
 
 	/**
