b0bcf1ff43e3ec998f698450242b324acc7e1d2e
Luke Kolin <luke@deltava.org>
2017-02-08 03:42:06 +0000

    Read/write VATSIM academy data
    git-svn-id: file:///home/svn/DVA2006@11363 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/servinfo/PilotRating.java
@@ -0,0 +1,110 @@
+// Copyright 2017 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.servinfo;
+
+import java.time.Instant;
+
+/**
+ * A bean to store VATSIM Pilot Rating certificates.
+ * @author Luke
+ * @version 7.2
+ * @since 7.2
+ */
+
+public class PilotRating extends NetworkUser {
+	
+	private String _atoName;
+	private String _atoURL;
+	
+	private int _instructor;
+	private final String _rating;
+	private Instant _date;
+
+	/**
+	 * Creates the bean.
+	 * @param id the certificate ID
+	 * @param rating the rating code
+	 */
+	public PilotRating(int id, String rating) {
+		super(id);
+		_rating = rating;
+	}
+	
+	/**
+	 * Returns the Authorized Training Organization name.
+	 * @return the name
+	 */
+	public String getATOName() {
+		return _atoName;
+	}
+	
+	/**
+	 * Returns the Authorized Training Organization URL.
+	 * @return the URL
+	 */
+	public String getATO() {
+		return _atoURL;
+	}
+	
+	/**
+	 * Returns the Instructor's certificate ID.
+	 * @return the cert ID
+	 */
+	public int getInstructor() {
+		return _instructor;
+	}
+	
+	/**
+	 * Returns the rating code.
+	 * @return the code
+	 */
+	public String getRatingCode() {
+		return _rating;
+	}
+	
+	/**
+	 * Returns the certification's issuance date.
+	 * @return the issue date/time
+	 */
+	public Instant getIssueDate() {
+		return _date;
+	}
+	
+	/**
+	 * Sets the Authorized Training Organizataion.
+	 * @param name the ATO name
+	 * @param url the ATO URL
+	 */
+	public void setATO(String name, String url) {
+		_atoName = name;
+		_atoURL = url;
+	}
+	
+	/**
+	 * Sets the rating's date of issue
+	 * @param dt the date/time
+	 */
+	public void setIssueDate(Instant dt) {
+		_date = dt;
+	}
+	
+	/**
+	 * Sets the Instructor Certificate ID.
+	 * @param cid the Instructor's cert ID
+	 */
+	public void setInstructorID(int cid) {
+		_instructor = Math.max(0, cid);
+	}
+
+	@Override
+	public Type getType() {
+		return Type.RATING;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder buf = new StringBuilder(getID());
+		buf.append(' ');
+		buf.append(_rating);
+		return buf.toString();
+	}
+}
---
+++ src/java/org/deltava/dao/http/GetATOData.java
@@ -0,0 +1,137 @@
+// Copyright 2017 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.dao.http;
+
+import static java.net.HttpURLConnection.HTTP_OK;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.time.Instant;
+
+import org.json.*;
+
+import org.deltava.beans.servinfo.*;
+
+import org.deltava.dao.DAOException;
+
+import org.deltava.util.StringUtils;
+import org.deltava.util.cache.*;
+
+/**
+ * A Data Access Object to read VATSIM Authorized Training Organization data.
+ * @author Luke
+ * @version 7.2
+ * @since 7.2
+ */
+
+public class GetATOData extends DAO {
+	
+	private static final Cache<CacheableCollection<PilotRating>> _rCache = CacheManager.getCollection(PilotRating.class, "ATORatings");
+	private static final Cache<CacheableCollection<Certificate>> _insCache = CacheManager.getCollection(Certificate.class, "ATOInstructors");
+	
+	/**
+	 * Returns information about a Pilot's Certifications.
+	 * @param cid the Pilot's certificate ID
+	 * @return a Collection of Certificate beans
+	 * @throws DAOException if an error occurs
+	 */
+	public Collection<PilotRating> get(String cid) throws DAOException {
+		int id = StringUtils.parse(cid, 0);
+		return getCertificates().stream().filter(c -> (c.getID() == id)).collect(Collectors.toList());
+	}
+	
+	/**
+	 * Returns information about an ATO instructor.
+	 * @param cid the Instructor's certificate ID
+	 * @return a Collection of Certificate beans
+	 * @throws DAOException if an error occurs
+	 */
+	public Collection<Certificate> getInstructor(String cid) throws DAOException {
+		int id = StringUtils.parse(cid, 0);
+		return getInstructors().stream().filter(c -> (c.getID() == id)).collect(Collectors.toList());
+	}
+
+	/**
+	 * Retrieves all ATO Instructors.
+	 * @return a Collection of Certificate beans
+	 * @throws DAOException if an error occurs
+	 */
+	public Collection<Certificate> getInstructors() throws DAOException {
+		
+		// Check the cache
+		CacheableCollection<Certificate> results = _insCache.get(Certificate.class);
+		if (results != null)
+			return results.clone();
+		
+		try {
+			init("https://ptd.vatsim.net/api/instructors.json");
+			if (getResponseCode() != HTTP_OK)
+				return Collections.emptyList();
+
+			// Parse the JSON
+			JSONArray ja = new JSONArray(new JSONTokener(getIn()));
+			results = new CacheableList<Certificate>(Certificate.class);
+			for (int x = 0; x < ja.length(); x++) {
+				JSONObject co = ja.getJSONObject(x);
+				JSONArray cro = co.getJSONArray("Roles");
+				Certificate c = new Certificate(co.getInt("cid"));
+				c.setName(co.getString("name").replace(" (" + c.getID() + ")", ""));
+				
+				// Parse roles
+				boolean isActive = true;
+				for (int y = 0; isActive && (y < cro.length()); y++)
+					isActive &= !cro.getJSONObject(y).getString("name").equals("Inactive");
+				
+				c.setActive(isActive);
+				results.add(c);
+			}
+
+			_insCache.add(results);
+			return results.clone();
+		} catch (IOException ie) {
+			throw new HTTPDAOException(ie);
+		}
+	}
+
+	/**
+	 * Returns all Pilot certifications.
+	 * @return a Collection of Certificate beans
+	 * @throws DAOException if an error occurs
+	 */
+	public Collection<PilotRating> getCertificates() throws DAOException {
+		
+		// Check the cache
+		CacheableCollection<PilotRating> results = _rCache.get(PilotRating.class);
+		if (results != null)
+			return results.clone();
+		
+		try {
+			init("https://ptd.vatsim.net/api/certifications.json");
+			if (getResponseCode() != HTTP_OK)
+				return Collections.emptyList();
+
+			// Parse the JSON
+			JSONArray ja = new JSONArray(new JSONTokener(getIn()));
+			results = new CacheableList<PilotRating>(PilotRating.class);
+			for (int x = 0; x < ja.length(); x++) {
+				JSONObject co = ja.getJSONObject(x);
+				String cid = co.optString("pilot_name", null);
+				if (cid == null) continue;
+				
+				JSONObject ao = co.getJSONObject("ato");
+				int id = StringUtils.parse(cid.substring(cid.lastIndexOf('(') + 1, cid.lastIndexOf(')')), 0);
+				PilotRating r = new PilotRating(id, co.getJSONObject("rating").getString("name"));
+				r.setName(cid.substring(0, cid.indexOf(" (")));
+				r.setIssueDate(Instant.parse(co.getString("created_at")));
+				r.setInstructorID(co.getJSONObject("user").getInt("cid"));
+				r.setATO(ao.getString("name"), ao.getString("url"));
+				results.add(r);
+			}
+
+			_rCache.add(results);
+			return results.clone();
+		} catch (IOException ie) {
+			throw new HTTPDAOException(ie);
+		}
+	}
+}
