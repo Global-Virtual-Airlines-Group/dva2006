924e284d079b16b40d9ce2b55ac2374ba60a5ffd
Luke Kolin <luke@deltava.org>
2017-03-07 17:58:32 +0000

    Remove duplicate VATSIM/DVA ratings
    git-svn-id: file:///home/svn/DVA2006@11421 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/functions/AcademyFunctions.java
+++ src/java/org/deltava/taglib/functions/AcademyFunctions.java
@@ -1,13 +1,17 @@
-// Copyright 2007, 2010, 2012 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2007, 2010, 2012, 2017 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.functions;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 import org.deltava.beans.CalendarEntry;
 import org.deltava.beans.academy.*;
+import org.deltava.beans.servinfo.PilotRating;
 
 /**
  * A JSP Function Library to define FlightAcademy-related functions.
  * @author Luke
- * @version 5.0
+ * @version 7.2
  * @since 1.0
  */
 
@@ -52,5 +56,26 @@ public class AcademyFunctions {
 	 */
 	public static boolean isActive(Course c) {
 		return ((c != null) && (c.getStatus() == Status.STARTED));
+	}
+	
+	/**
+	 * Filters out Pilot Ratings that have been achieved via a Flight Academy course.
+	 * @param ratings a Collection of PilotRatings
+	 * @param certs a Collection of Certifications
+	 * @return a Collection of filtered PilotRatings
+	 */
+	public static Collection<PilotRating> filterRatings(Collection<PilotRating> ratings, Collection<Certification> certs) {
+		Collection<String> codes = certs.stream().map(Certification::getNetworkRatingCode).filter(Objects::nonNull).collect(Collectors.toSet());
+		return ratings.stream().filter(r -> !codes.contains(r.getRatingCode())).collect(Collectors.toSet());
+	}
+	
+	/**
+	 * Filters a list of Certifications to only include Certifications obtained by a Pilot.
+	 * @param codes a Collection containing the codes of the Pilot's Ceritification
+	 * @param allCerts a Collection containing all Certifications
+	 * @return a Collection of filtered Certifications
+	 */
+	public static Collection<Certification> filterCerts(Collection<String> codes, Collection<Certification> allCerts) {
+		return allCerts.stream().filter(c -> codes.contains(c.getCode())).collect(Collectors.toSet());
 	}
 }
