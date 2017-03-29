1988a1be4c99b56d9b926c39f0f0a84f8364b0d7
Luke Kolin <luke@deltava.org>
2017-03-29 17:25:27 +0000

    JSON web services
    git-svn-id: file:///home/svn/DVA2006@11521 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/service/InstallerInfoService.java
+++ src/java/org/deltava/service/InstallerInfoService.java
@@ -1,15 +1,13 @@
-// Copyright 2005, 2007, 2010, 2012 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2007, 2010, 2012, 2017 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.service;
 
 import java.util.*;
-import java.net.URL;
 import java.io.IOException;
 
 import static javax.servlet.http.HttpServletResponse.*;
 
-import org.jdom2.*;
+import org.json.*;
 
-import org.deltava.beans.Simulator;
 import org.deltava.beans.fleet.Installer;
 import org.deltava.beans.system.AirlineInformation;
 
@@ -21,7 +19,7 @@ import org.deltava.util.system.SystemData;
 /**
  * A Web Service to display Fleet Library Information.
  * @author Luke
- * @version 5.1
+ * @version 7.3
  * @since 1.0
  */
 
@@ -71,35 +69,25 @@ public class InstallerInfoService extends WebService {
 		if (nFmt.contains("."))
 			nFmt = nFmt.substring(0, nFmt.indexOf('.'));
 		
-		// Generate the XML document
-		Document doc = new Document();
-		Element re = new Element("wsdata");
-		doc.setRootElement(re);
-
-		// Create the installer entry
-		Element le = new Element("installer");
-		re.addContent(le);
-		le.setAttribute("code", i.getCode());
-		le.setAttribute("filename", i.getFileName());
-		le.setAttribute("title", i.getName());
-		le.setAttribute("size", StringUtils.format(i.getSize(), nFmt));
+		// Generate the JSON document
+		JSONObject jo = new JSONObject();
+		jo.put("code", i.getCode());
+		jo.put("fileName", i.getFileName());
+		jo.put("title", i.getName());
+		jo.put("size", StringUtils.format(i.getSize(), nFmt));
 		if (i.getLastModified() != null)
-			le.setAttribute("date", StringUtils.format(i.getLastModified(), dFmt));
-		le.setAttribute("version", i.getVersion());
-		le.setAttribute("dl", StringUtils.format(i.getDownloadCount(), nFmt));
-		le.addContent(XMLUtils.createElement("desc", i.getDescription(), true));
-		for (Simulator sim : i.getFSVersions())
-			le.addContent(XMLUtils.createElement("version", sim.getName()));
+			jo.put("date", StringUtils.format(i.getLastModified(), dFmt));
+		jo.put("version", i.getVersion());
+		jo.put("dl", StringUtils.format(i.getDownloadCount(), nFmt));
+		jo.put("img", "/" + SystemData.get("path.img") + "/fleet/" + i.getImage());
+		i.getFSVersions().forEach(sim -> jo.append("sims", sim.getName()));
+		jo.put("desc", i.getDescription());
 		
-		// Dump the XML to the output stream
+		// Dump the JSON to the output stream
+		JSONUtils.ensureArrayPresent(jo, "version");
 		try {
-			int ofs = ctx.getRequest().getProtocol().indexOf('/');
-			String img = "/" + SystemData.get("path.img") + "/fleet/" + i.getImage();	
-			URL url = new URL(ctx.getRequest().getProtocol().substring(0, ofs), "www." + ai.getDomain(), img);
-			le.setAttribute("img", url.toString());
-			
-			ctx.setContentType("text/xml", "UTF-8");
-			ctx.println(XMLUtils.format(doc, "UTF-8"));
+			ctx.setContentType("application/json", "UTF-8");
+			ctx.println(jo.toString());
 			ctx.commit();
 		} catch (IOException ie) {
 			throw new ServiceException(SC_CONFLICT, "I/O Error");
