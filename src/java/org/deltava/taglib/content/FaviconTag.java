d16b633c5d7779fd36c1f53ba76f8ac002d3798b
Luke Kolin <luke@deltava.org>
2017-04-03 16:44:04 +0000

    Add favicon tag
    git-svn-id: file:///home/svn/DVA2006@11537 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/taglib/content/FaviconTag.java
@@ -0,0 +1,67 @@
+// Copyright 2017 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.taglib.content;
+
+import javax.servlet.jsp.*;
+
+import org.deltava.beans.system.*;
+
+import org.deltava.taglib.BrowserInfoTag;
+
+/**
+ * A JSP tag to display favicon link elements.
+ * @author Luke
+ * @version 7.3
+ * @since 7.3
+ */
+
+public class FaviconTag extends BrowserInfoTag {
+	
+	/*
+	 * Helper method to append build number into path.
+	 */
+	private static String generateTag(String base, String path) {
+		StringBuilder buf = new StringBuilder("<");
+		buf.append(base);
+		buf.append(" href=\"/favicon/v");
+		buf.append(VersionInfo.BUILD);
+		buf.append('/');
+		buf.append(path);
+		buf.append("\">");
+		return buf.toString();
+	}
+
+	/**
+	 * Renders the favicon elements.
+	 * @return EVAL_PAGE always
+	 * @throws JspException if an error occurs
+	 */
+	@Override
+	public int doEndTag() throws JspException {
+
+		HTTPContextData bCtxt = getBrowserContext();
+		try {
+			JspWriter out = pageContext.getOut();
+			if (bCtxt.getBrowserType() == BrowserType.WEBKIT) {
+				out.println(generateTag("link rel=\"apple-touch-icon\" sizes=\"180x180\"", "apple-touch-icon.png"));
+				out.println(generateTag("link rel=\"mask-icon\" color=\"#5bbad5\"", "safari-pinned-tab.svg"));
+			}
+			
+			out.println(generateTag("link rel=\"icon\" type=\"image/png\" sizes=\"32x32\"", "favicon-32x32.png"));
+			out.println(generateTag("link rel=\"icon\" type=\"image/png\" sizes=\"16x16\"", "favicon-16x16.png"));
+			out.println(generateTag("link rel=\"shortcut icon\"", "favicon.ico"));
+			out.println(generateTag("link rel=\"manifest\"", "manifest.json"));
+			if ((bCtxt.getBrowserType() == BrowserType.IE) && (bCtxt.getMajor() > 9)) {
+				out.print("meta name=\"msapplication-config\" content=\"/favicon/v");
+				out.print(VersionInfo.BUILD);
+				out.println("/browserconfig.xml\">");
+				out.println("<meta name=\"theme-color\" content=\"#ffffff\">");
+			}
+		} catch (Exception e) {
+			throw new JspException(e);
+		} finally {
+			release();
+		}
+		
+		return EVAL_PAGE;
+	}
+}
