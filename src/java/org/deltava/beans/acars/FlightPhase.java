4c115cc97064ad01f282682ee0ba911335f96ce6
Luke Kolin <luke@deltava.org>
2017-04-30 14:03:15 +0000

    Add exception-safe fromName
    git-svn-id: file:///home/svn/DVA2006@11607 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/acars/FlightPhase.java
+++ src/java/org/deltava/beans/acars/FlightPhase.java
@@ -1,20 +1,19 @@
-// Copyright 2008, 2011 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2008, 2011, 2017 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.acars;
 
 /**
  * An enumeration to list valid ACARS flight phases. 
  * @author Luke
- * @version 4.1
+ * @version 7.3
  * @since 2.1
  */
 
 public enum FlightPhase {
 	
-	UNKNOWN("Unknown"), PREFLIGHT("Pre-Flight"), PUSHBACK("Pushback"), TAXIOUT("Taxi Out"), TAKEOFF("Takeoff"),
-	AIRBORNE("Airborne"), ROLLOUT("Rollout"), TAXIIN("Taxi In"), ATGATE("At Gate"), SHUTDOWN("Shutdown"),
-	COMPLETE("Completed"), ABORTED("Abort"), ERROR("Error"), PIREPFILE("File PIREP");
+	UNKNOWN("Unknown"), PREFLIGHT("Pre-Flight"), PUSHBACK("Pushback"), TAXIOUT("Taxi Out"), TAKEOFF("Takeoff"), AIRBORNE("Airborne"), ROLLOUT("Rollout"), 
+	TAXIIN("Taxi In"), ATGATE("At Gate"), SHUTDOWN("Shutdown"), COMPLETE("Completed"), ABORTED("Abort"), ERROR("Error"), PIREPFILE("File PIREP");
 	
-	private String _name;
+	private final String _name;
 
 	FlightPhase(String name) {
 		_name = name;
@@ -26,5 +25,18 @@ public enum FlightPhase {
 	 */
 	public String getName() {
 		return _name;
+	}
+
+	/**
+	 * Exception-safe phase parser.
+	 * @param phase the phase name
+	 * @return a FlightPhase, or UNKNOWN
+	 */
+	public static FlightPhase fromString(String phase) {
+		try {
+			return FlightPhase.valueOf(phase);
+		} catch (Exception e) {
+			return UNKNOWN;
+		}
 	}
 }
