a57a1818e83c7228231144766ab356aa89b9f3a8
Luke Kolin <luke@deltava.org>
2017-05-10 15:46:10 +0000

    Add Geo support
    git-svn-id: file:///home/svn/DVA2006@11624 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetCountry.java
+++ src/java/org/deltava/dao/GetCountry.java
@@ -1,19 +1,24 @@
-// Copyright 2010, 2011 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2010, 2011, 2017 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao;
 
 import java.sql.*;
 
+import org.deltava.beans.GeoLocation;
 import org.deltava.beans.schedule.Country;
 
+import org.deltava.util.cache.*;
+
 /**
- * A Data Access Object to load ISO-3316 country codes.
+ * A Data Access Object to load ISO-3316 country codes and perform geolocation.
  * @author Luke
- * @version 4.1
+ * @version 7.3
  * @since 3.2
  */
 
 public class GetCountry extends DAO {
-
+	
+	private static final GeoCache<CacheableString> _cache = CacheManager.getGeo(CacheableString.class, "geoCountry");
+	
 	/**
 	 * Initializes the Data Access Object.
 	 * @param c the JDBC connection to use
@@ -40,6 +45,43 @@ public class GetCountry extends DAO {
 			
 			_ps.close();
 			return rowsLoaded;
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+	
+	/**
+	 * Determines what country covers a particular point.
+	 * @param loc a GeoLocation
+	 * @return a Country object, or null if none found
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public Country find(GeoLocation loc) throws DAOException {
+		
+		// Check the cache
+		CacheableString id = _cache.get(loc);
+		if (id != null)
+			return Country.get(id.getValue());
+		
+		try {
+			prepareStatementWithoutLimits("SELECT CODE FROM common.COUNTRY WHERE ST_Contains(DATA, ST_PointFromText(?,?)) LIMIT 1");
+			_ps.setString(1, formatLocation(loc));
+			_ps.setInt(2, GEO_SRID);
+			
+			try (ResultSet rs = _ps.executeQuery()) {
+				if (rs.next())
+					id = new CacheableString(rs.getString(1), rs.getString(1));
+			}
+
+			_ps.close();
+			
+			// Add to cache
+			if (id != null) {
+				_cache.add(loc, id);
+				return Country.get(id.getValue());
+			}
+			
+			return null;
 		} catch (SQLException se) {
 			throw new DAOException(se);
 		}
