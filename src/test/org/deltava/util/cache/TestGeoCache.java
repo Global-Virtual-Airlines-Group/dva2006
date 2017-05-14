24653e9728efeb4db7b166fc6f1d978073f8d0fe
Luke Kolin <luke@deltava.org>
2017-05-14 14:41:33 +0000

    Add GeoCache unit test
    git-svn-id: file:///home/svn/DVA2006@11631 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/util/cache/TestGeoCache.java
+++ src/test/org/deltava/util/cache/TestGeoCache.java
@@ -6,7 +6,9 @@ import java.util.*;
 
 import junit.framework.*;
 
+import org.deltava.beans.GeoLocation;
 import org.deltava.beans.schedule.Airline;
+import org.deltava.beans.schedule.GeoPosition;
 
 public class TestGeoCache extends TestCase {
 
@@ -29,19 +31,44 @@ public class TestGeoCache extends TestCase {
 	}
 
 	public void testClone() throws Exception {
-		
+		Cacheable o1 = new CacheableLong(Integer.valueOf(1), 1);
+		GeoLocation loc = new GeoPosition(44.5, -85.5);
+		_cache.add(loc, o1);
+		assertEquals(1, _cache.size());
+		Cacheable o2 = _cache.get(loc);
+		assertNotNull(o2);
+		assertSame(o1, o2);
 	}
 
 	public void testCacheOverflow() {
-		
+		_cache.add(new GeoPosition(44.5,-85.5), new Airline("DVA", "Delta Virtual"));
+		_cache.add(new GeoPosition(52.5, 5.5), new Airline("AFV", "Air France Virtual"));
+		assertEquals(_cache.getMaxSize(), _cache.size());
+		_cache.add(new GeoPosition(42.5,-85.5), new Airline("DVA", "Delta Virtual"));
+		assertEquals(_cache.getMaxSize(), _cache.size());
+		assertTrue(_cache.contains(new GeoPosition(52.5, 5.5)));
+		assertTrue(_cache.contains(new GeoPosition(42.5, -85.5)));
 	}
 
 	public void testCacheExpiry() throws Exception {
-		
+		_cache.add(new GeoPosition(44.5,-85.5), new Airline("DVA", "Delta Virtual"));
+		_cache.add(new GeoPosition(52.5, 5.5), new Airline("AFV", "Air France Virtual"));
+		assertEquals(_cache.getMaxSize(), _cache.size());
+		Thread.sleep(1050);
+		assertFalse(_cache.contains(new GeoPosition(52.5, 5.5)));
+		assertFalse(_cache.contains(new GeoPosition(42.5, -85.5)));
 	}
 
 	public void testNull() {
-		
+		_cache.addNull(new GeoPosition(52.5, 5.5));
+		assertEquals(1, _cache.size());
+		assertTrue(_cache.contains(new GeoPosition(52.5, 5.5)));
+	}
+	
+	public void testRounding() {
+		_cache.addNull(new GeoPosition(52.5, 5.5));
+		assertEquals(1, _cache.size());
+		assertTrue(_cache.contains(new GeoPosition(52.51, 5.51)));
 	}
 
 	public void testLargeCache() {
