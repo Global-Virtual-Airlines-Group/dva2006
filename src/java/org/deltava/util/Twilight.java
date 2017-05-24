0701dffacd538c388a87ed4faa2f75eb78363388
Luke Kolin <luke@deltava.org>
2017-05-24 15:24:25 +0000

    Sunrise / Sunset calculator
    git-svn-id: file:///home/svn/DVA2006@11682 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/util/Twilight.java
+++ src/java/org/deltava/util/Twilight.java
@@ -1,29 +1,27 @@
 // Copyright 2017 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.util;
 
-import java.math.BigDecimal;
-
 /**
  * An enumeration for storing twilight types. 
  * @author Luke
- * @version 7.3
+ * @version 7.4
  * @since 7.3
  */
 
 public enum Twilight {
-	OFFICIAL(90.8333), CIVIL(96), NAUTICAL(102), ASTRO(108);
+	EXACT(90), OFFICIAL(90.8333), CIVIL(96), NAUTICAL(102), ASTRO(108);
 
-	private final BigDecimal _degrees;
+	private final double _degrees;
 	
 	Twilight(double degrees) {
-		_degrees = BigDecimal.valueOf(degrees);
+		_degrees = degrees;
 	}
 
 	/**
 	 * The distance the Sun is below the vertical.
 	 * @return the distance in degrees
 	 */
-	public BigDecimal getDegrees() {
+	public double getDegrees() {
 		return _degrees;
 	}
 }
