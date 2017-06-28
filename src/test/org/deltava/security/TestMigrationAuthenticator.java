904511926eb5e01b7e4b15dbdc42d7969102db5b
Luke Kolin <luke@deltava.org>
2017-06-28 19:34:45 +0000

    Remove compiler warnings
    git-svn-id: file:///home/svn/DVA2006@11804 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/security/TestMigrationAuthenticator.java
+++ src/test/org/deltava/security/TestMigrationAuthenticator.java
@@ -8,38 +8,41 @@ import org.apache.log4j.PropertyConfigurator;
 
 public class TestMigrationAuthenticator extends TestCase {
 
-    private MigrationAuthenticator _auth;
-    
-    @Override
-	protected void setUp() throws Exception {
-        super.setUp();
-        PropertyConfigurator.configure("data/log4j.test.properties");
-        _auth = new MigrationAuthenticator();
-        _auth.init(Authenticator.DEFAULT_PROPS_FILE);
-    }
+	private MigrationAuthenticator _auth;
 
-    @Override
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		PropertyConfigurator.configure("data/log4j.test.properties");
+		_auth = new MigrationAuthenticator();
+		_auth.init(Authenticator.DEFAULT_PROPS_FILE);
+	}
+
+	@Override
 	protected void tearDown() throws Exception {
-        _auth = null;
-        super.tearDown();
-    }
-    
-    public void testSrcAuthenticationAndCopy() {
-    	Person usr2 = new AuthPerson("Luke", "Kolin", "cn=Luke Kolin,ou=afv,o=sce");
-    	assertFalse(_auth.getDestination().contains(usr2));
-    	_auth.authenticate(usr2, "maddog");
-    	assertTrue(_auth.getDestination().contains(usr2));
-    	_auth.remove(usr2);
-    	assertFalse(_auth.getDestination().contains(usr2));
-    }
-    
-    public void testAddRemove() throws Exception {
-    	Person usr2 = new AuthPerson("Test", "User", "cn=Test User,ou=dva,o=sce");
-    	_auth.add(usr2, "test");
-    	assertTrue(_auth.contains(usr2));
-    	assertTrue(_auth.getDestination().contains(usr2));
-    	assertFalse(_auth.getSource().contains(usr2));
-    	_auth.authenticate(usr2, "test");
-    	_auth.remove(usr2);
-    }
+		_auth = null;
+		super.tearDown();
+	}
+
+	public void testSrcAuthenticationAndCopy() {
+		Person usr2 = new AuthPerson("Luke", "Kolin", "cn=Luke Kolin,ou=afv,o=sce");
+		assertFalse(_auth.getDestination().isEmpty());
+		Authenticator dst = _auth.getDestination().iterator().next();
+		assertFalse(dst.contains(usr2));
+		_auth.authenticate(usr2, "maddog");
+		assertTrue(dst.contains(usr2));
+		_auth.remove(usr2);
+		assertFalse(dst.contains(usr2));
+	}
+
+	public void testAddRemove() throws Exception {
+		assertFalse(_auth.getDestination().isEmpty());
+		Person usr2 = new AuthPerson("Test", "User", "cn=Test User,ou=dva,o=sce");
+		_auth.add(usr2, "test");
+		assertTrue(_auth.contains(usr2));
+		assertTrue(_auth.getDestination().iterator().next().contains(usr2));
+		assertFalse(_auth.getSource().contains(usr2));
+		_auth.authenticate(usr2, "test");
+		_auth.remove(usr2);
+	}
 }
