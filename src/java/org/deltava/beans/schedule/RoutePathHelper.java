a1ccc6e59b2ab66d224612c98b4de580df40bd91
Luke Kolin <luke@deltava.org>
2017-07-25 03:41:46 +0000

    Further work
    git-svn-id: file:///home/svn/DVA2006@11848 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/schedule/RoutePathHelper.java
+++ src/java/org/deltava/beans/schedule/RoutePathHelper.java
@@ -3,9 +3,11 @@ package org.deltava.beans.schedule;
 
 import java.util.*;
 
+import org.apache.log4j.Logger;
+
 import org.deltava.beans.Helper;
 
-import org.deltava.util.GeoUtils;
+import org.deltava.util.*;
 
 /**
  * A utility class that uses Dijkstra's algorithm to calculate the shortest multi-airport leg.
@@ -17,9 +19,11 @@ import org.deltava.util.GeoUtils;
 @Helper(RoutePair.class)
 public class RoutePathHelper {
 	
+	private static final Logger log = Logger.getLogger(RoutePathHelper.class);
+	
 	private final Map<Airport, Collection<Airport>> _links = new HashMap<Airport, Collection<Airport>>();
 	
-	private class Vertex {
+	private class Vertex implements Comparable<Vertex> {
 		private final Airport _a;
 		private Vertex _prev;
 		private int _distance = Integer.MAX_VALUE;
@@ -50,25 +54,17 @@ public class RoutePathHelper {
 		
 		@Override
 		public String toString() {
-			return _a.toString();
+			return _a.getICAO();
+		}
+
+		@Override
+		public int compareTo(Vertex v) {
+			if (_a.equals(v._a)) return 0;
+			int tmpResult = Integer.valueOf(_distance).compareTo(Integer.valueOf(v._distance));
+			return (tmpResult == 0) ? _a.compareTo(v._a) : tmpResult;
 		}
 	}
 	
-	private class VertexComparator implements Comparator<Vertex> {
-
-		VertexComparator() {
-			super();
-		}
-		
-		@Override
-		public int compare(Vertex v1, Vertex v2) {
-			if (v1.getAirport().equals(v2.getAirport())) return 0;
-			int tmpResult = (v1.getDistance() == v2.getDistance()) ? 0 : ((v1.getDistance() > v2.getDistance()) ? 1 : -1);
-			return (tmpResult == 0) ? v1.getAirport().compareTo(v2.getAirport()) : tmpResult;
-		}
-	}
-
-	// static class
 	public RoutePathHelper() {
 		super();
 	}
@@ -87,21 +83,25 @@ public class RoutePathHelper {
 		Vertex v = new Vertex(rp.getAirportD());
 		v.setDistance(0);
 
-		Collection<Vertex> uvV = new TreeSet<Vertex>(new VertexComparator());
-		Map<Airport, Vertex> uvA = new HashMap<Airport, Vertex>();
-		Collection<Vertex> all = new ArrayList<Vertex>(_links.size());
-		_links.keySet().forEach(ap -> { Vertex vx = new Vertex(ap); uvV.add(vx); all.add(vx); uvA.put(ap, vx); });
-		uvA.put(rp.getAirportD(), v);
-		uvV.add(v);
+		Collection<Vertex> uvV = new TreeSet<Vertex>() {{ add(v); }};
+		Map<Airport, Vertex> uvA = new HashMap<Airport, Vertex>() {{ put(rp.getAirportD(), v); }};
+		_links.keySet().forEach(ap -> { Vertex vx = new Vertex(ap); uvV.add(vx); uvA.put(ap, vx); });
 				
 		// Loop through the unvisited
 		uvV.stream().forEach(vv -> {
+			log.info("Starting at " + vv);
 			Collection<Airport> neighbors = _links.get(vv.getAirport());
 			for (Airport aN : neighbors) {
 				Vertex vN = uvA.get(aN);
-				if ((vN == null) || (vN.getDistance() < Integer.MAX_VALUE)) continue;
-				int distance = vv.getDistance() + GeoUtils.distance(vv.getAirport(), aN);
+				if (vN == null)  continue;
+				
+				int distance = (vv.getDistance() == Integer.MAX_VALUE ? 0 : vv.getDistance()) + GeoUtils.distance(vv.getAirport(), aN);
 				if (distance < vN.getDistance()) {
+					if (vN.getDistance() == Integer.MAX_VALUE)
+						log.info("Shortest path to " + vN + " is from " + vv + ", distance=" + distance);
+					else
+						log.info("Decreasing shortest path from " + rp.getAirportD().getICAO() + " to " + vN + " from " + vv + "(was " + vN + ") distance=" + vN.getDistance() + " to " + distance);
+						
 					vN.setDistance(distance);
 					vN.setPrevious(vv);
 				}
@@ -115,10 +115,18 @@ public class RoutePathHelper {
 		
 		Collection<RoutePair> route = new ArrayList<RoutePair>();
 		while (vA.getPrevious() != null) {
-			route.add(new ScheduleRoute(vA.getAirport(), vA.getPrevious().getAirport()));
+			RoutePair rt = new ScheduleRoute(vA.getAirport(), vA.getPrevious().getAirport());
+			route.add(rt);
+			log.info(rt);
+			if ((vA.getPrevious() != null) && (vA.getPrevious().getPrevious() == vA)) {
+				log.warn("Loop between " + vA + " and " + vA.getPrevious());
+				break;
+			}
+				
 			vA = vA.getPrevious();
 		}
 		
+		log.info("Route is " + route);
 		return route;
 	}
 }
