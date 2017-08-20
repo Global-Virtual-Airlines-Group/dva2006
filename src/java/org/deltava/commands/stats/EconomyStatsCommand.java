6e32e1ba9484e88ef1c0d8a4c429f151c72dbc0f
Luke Kolin <luke@deltava.org>
2017-08-20 15:14:10 +0000

    Add economy stats
    git-svn-id: file:///home/svn/DVA2006@11921 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/commands/stats/EconomyStatsCommand.java
@@ -0,0 +1,49 @@
+// Copyright 2017 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.commands.stats;
+
+import org.deltava.beans.stats.*;
+
+import org.deltava.commands.*;
+import org.deltava.dao.*;
+
+/**
+ * A Web Site Command to display flight load/passenger statistics. 
+ * @author Luke
+ * @version 7.5
+ * @since 7.5
+ */
+
+public class EconomyStatsCommand extends AbstractViewCommand {
+
+	/**
+	 * Execute the command.
+	 * @param ctx the Command context
+	 * @throws CommandException if an unhandled error occurs
+	 */
+	@Override
+	public void execute(CommandContext ctx) throws CommandException {
+		
+		// Get grouping / sorting
+		ViewContext<FlightStatsEntry> vc = initView(ctx, FlightStatsEntry.class); 
+		FlightStatsSort srt = FlightStatsSort.from(vc.getSortType(), FlightStatsSort.DATE);
+		FlightStatsGroup grp = FlightStatsGroup.from(ctx.getParameter("groupType"), FlightStatsGroup.MONTH);
+		if (!grp.isDateGroup() && (srt == FlightStatsSort.DATE)) srt = FlightStatsSort.LEGS;
+		vc.setSortType(srt.name()); ctx.setAttribute("groupType", grp, REQUEST);
+
+		try {
+			GetAggregateStatistics dao = new GetAggregateStatistics(ctx.getConnection());
+			dao.setQueryStart(vc.getStart());
+			dao.setQueryMax(vc.getCount());
+			vc.setResults(dao.getPIREPStatistics(srt, grp));
+		} catch (DAOException de) {
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
+
+		// Forward to the JSP
+		CommandResult result = ctx.getResult();
+		result.setURL("/jsp/stats/econStats.jsp");
+		result.setSuccess(true);
+	}
+}
