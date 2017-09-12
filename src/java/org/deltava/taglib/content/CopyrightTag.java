9027bc85a75d328556b4f273f2f43857197f644e
Luke Kolin <luke@deltava.org>
2017-09-12 16:13:09 +0000

    Fix httpss tag
    git-svn-id: file:///home/svn/DVA2006@12010 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/content/CopyrightTag.java
+++ src/java/org/deltava/taglib/content/CopyrightTag.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2006, 2007, 2010, 2012, 2015 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007, 2010, 2012, 2015, 2017 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.content;
 
 import javax.servlet.jsp.*;
@@ -9,9 +9,9 @@ import org.deltava.beans.system.VersionInfo;
 import org.deltava.util.system.SystemData;
 
 /**
- * A JSP Tag to insert a copyright notice. This tag is a useful test to ensure that the tag libraries are being loaded.
+ * A JSP Tag to insert a copyright notice.
  * @author Luke
- * @version 6.2
+ * @version 8.0
  * @since 1.0
  */
 
@@ -27,41 +27,6 @@ public class CopyrightTag extends TagSupport {
 		_visible = isVisible;
 	}
 
-	private void displayCopyrightComment() throws Exception {
-		JspWriter jw = pageContext.getOut();
-		jw.print("<!-- ");
-		jw.print(pageContext.getServletContext().getServletContextName());
-		jw.print(' ');
-		jw.print(VersionInfo.APPNAME);
-		jw.print(' ');
-		jw.print(VersionInfo.TXT_COPYRIGHT);
-		jw.print(" (Build ");
-		jw.print(String.valueOf(VersionInfo.BUILD));
-		jw.print(") -->");
-	}
-
-	private void displayCopyright() throws Exception {
-		JspWriter jw = pageContext.getOut();
-		jw.println("<hr />");
-		jw.print("<div class=\"small copyright\">");
-		jw.print(pageContext.getServletContext().getServletContextName());
-		jw.print(' ');
-		jw.print(VersionInfo.APPNAME );
-		jw.print(' ');
-		jw.print(VersionInfo.HTML_COPYRIGHT .replace("http", pageContext.getRequest().getScheme()));
-		jw.print(" (Build ");
-		jw.print(VersionInfo.BUILD);
-		jw.print(")</div>");
-
-		// Display disclaimer
-		String disclaimer = SystemData.get("airline.copyright");
-		if (disclaimer != null) {
-			jw.print("\n<span class=\"copyright small\">");
-			jw.print(disclaimer);
-			jw.print("</span>");
-		}
-	}
-	
 	/**
 	 * Renders the copyright tag to the JSP output stream.
 	 * @return TagSupport#EVAL_PAGE always
@@ -70,10 +35,37 @@ public class CopyrightTag extends TagSupport {
 	@Override
 	public int doEndTag() throws JspException {
 		try {
-			if (_visible)
-				displayCopyright();
-			else
-				displayCopyrightComment();
+			JspWriter jw = pageContext.getOut();
+			if (_visible) {
+				jw.println("<hr />");
+				jw.print("<div class=\"small copyright\">");
+				jw.print(pageContext.getServletContext().getServletContextName());
+				jw.print(' ');
+				jw.print(VersionInfo.APPNAME );
+				jw.print(' ');
+				jw.print(VersionInfo.HTML_COPYRIGHT);
+				jw.print(" (Build ");
+				jw.print(VersionInfo.BUILD);
+				jw.print(")</div>");
+
+				// Display disclaimer
+				String disclaimer = SystemData.get("airline.copyright");
+				if (disclaimer != null) {
+					jw.print("\n<span class=\"copyright small\">");
+					jw.print(disclaimer);
+					jw.print("</span>");
+				}
+			} else {
+				jw.print("<!-- ");
+				jw.print(pageContext.getServletContext().getServletContextName());
+				jw.print(' ');
+				jw.print(VersionInfo.APPNAME);
+				jw.print(' ');
+				jw.print(VersionInfo.TXT_COPYRIGHT);
+				jw.print(" (Build ");
+				jw.print(String.valueOf(VersionInfo.BUILD));
+				jw.print(") -->");
+			}
 		} catch (Exception e) {
 			throw new JspException(e);
 		} finally {
