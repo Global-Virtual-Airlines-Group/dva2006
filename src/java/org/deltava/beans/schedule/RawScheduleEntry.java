7debd3288b7d4ebc03fc17a22a7b3be736251ce7
Luke Kolin <luke@deltava.org>
2017-10-24 03:53:01 +0000

    Raw schedule tweaks
    git-svn-id: file:///home/svn/DVA2006@12124 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/schedule/RawScheduleEntry.java
+++ src/java/org/deltava/beans/schedule/RawScheduleEntry.java
@@ -1,10 +1,13 @@
 // Copyright 2017 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.schedule;
 
+import java.util.*;
+import java.time.DayOfWeek;
+
 import org.deltava.util.StringUtils;
 
 /**
- * A Schedule Entry with passenger capacity data. 
+ * A Schedule Entry with code share and day of week data. 
  * @author Luke
  * @version 8.0
  * @since 8.0
@@ -13,44 +16,18 @@ import org.deltava.util.StringUtils;
 public class RawScheduleEntry extends ScheduleEntry {
 
 	private String _codeShare;
-	
-	private int _first;
-	private int _biz;
-	private int _economy;
+	private final Collection<DayOfWeek> _days = new TreeSet<DayOfWeek>();
 	
 	/**
 	 * Creates the bean.
 	 * @param a the Airline
 	 * @param fNumber the flight number
+	 * @param leg the leg number
 	 */
-	public RawScheduleEntry(Airline a, int fNumber) {
-		super(a, fNumber, 1);
+	public RawScheduleEntry(Airline a, int fNumber, int leg) {
+		super(a, fNumber, leg);
 	}
 
-	/**
-	 * Returns the number of seats available in First class.
-	 * @return the number of seats
-	 */
-	public int getFirst() {
-		return _first;
-	}
-	
-	/**
-	 * Returns the number of seats available in Business class.
-	 * @return the number of seats
-	 */
-	public int getBusiness() {
-		return _biz;
-	}
-	
-	/**
-	 * Returns the number of seats available in Economy class.
-	 * @return the number of seats
-	 */
-	public int getEconomy() {
-		return _economy;
-	}
-	
 	/**
 	 * If a codeshare, the flight code of the operator's flight.
 	 * @return the flight code, or null if none
@@ -58,17 +35,21 @@ public class RawScheduleEntry extends ScheduleEntry {
 	public String getCodeShare() {
 		return _codeShare;
 	}
+	
+	/**
+	 * Returns the days of the week this flight is operated on.
+	 * @return a Collection of DayOfWeek enums
+	 */
+	public Collection<DayOfWeek> getDays() {
+		return _days;
+	}
 
 	/**
-	 * Sets the available seats on this flight.
-	 * @param first the number of first class seats
-	 * @param biz the number of business class seats
-	 * @param economy the number of economy class seats
+	 * Adds a day of the week that this flight is operated on.
+	 * @param d a DayOfWeek enum
 	 */
-	public void setCapacity(int first, int biz, int economy) {
-		_first = first;
-		_biz = biz;
-		_economy = economy;
+	public void addDay(DayOfWeek d) {
+		_days.add(d);
 	}
 	
 	/**
