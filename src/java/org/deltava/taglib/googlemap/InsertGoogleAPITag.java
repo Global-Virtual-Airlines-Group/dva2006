aacdb57c357edfc97711a3dcda41ce4d50e843b4
Luke Kolin <luke@deltava.org>
2017-11-28 16:58:38 +0000

    Google maps 3.30
    git-svn-id: file:///home/svn/DVA2006@12196 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/googlemap/InsertGoogleAPITag.java
+++ src/java/org/deltava/taglib/googlemap/InsertGoogleAPITag.java
@@ -27,7 +27,7 @@ public class InsertGoogleAPITag extends TagSupport {
 	static final String API_VER_ATTR_NAME = "$googleMapAPIVersion$";
 	
 	private static final int MIN_API_VERSION = 3;
-	private static final String DEFAULT_V3_MINOR = "29";
+	private static final String DEFAULT_V3_MINOR = "30";
 	
 	private static final String V3_API_URL = "maps.googleapis.com/maps/api/js?v=";
 	
