60cdf3260ff261646d217e1ca9d6db1b1246a49f
Luke Kolin <luke@deltava.org>
2018-01-06 21:27:57 +0000

    Display warning aggregation
    git-svn-id: file:///home/svn/DVA2006@12279 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/pirep/PIREPCommand.java
+++ src/java/org/deltava/commands/pirep/PIREPCommand.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.pirep;
 
 import java.io.*;
@@ -34,7 +34,7 @@ import org.deltava.util.system.SystemData;
 /**
  * A Web Site Command to handle editing/saving Flight Reports.
  * @author Luke
- * @version 8.0
+ * @version 8.1
  * @since 1.0
  */
 
@@ -478,7 +478,7 @@ public class PIREPCommand extends AbstractFormCommand {
 					
 					// Get the flight score
 					ScorePackage pkg = new ScorePackage(acInfo, afr, info.getRunwayD(), info.getRunwayA());
-					if (afr.hasAttribute(FlightReport.ATTR_CHECKRIDE) && (afr.getFDR() != Recorder.XACARS)) {
+					if ((ctx.isUserInRole("PIREP") || afr.hasAttribute(FlightReport.ATTR_CHECKRIDE)) && (afr.getFDR() != Recorder.XACARS)) {
 						GetACARSPositions posdao = new GetACARSPositions(con);
 						Collection<GeospaceLocation> positions = posdao.getRouteEntries(info.getID(), true, info.getArchived());
 						positions.stream().filter(ACARSRouteEntry.class::isInstance).map(ACARSRouteEntry.class::cast).forEach(pkg::add);
@@ -486,7 +486,7 @@ public class PIREPCommand extends AbstractFormCommand {
 					
 					FlightScore score = FlightScorer.score(pkg);
 					if (score != FlightScore.INCOMPLETE)
-						ctx.setAttribute("flightScore", score, REQUEST);
+						ctx.setAttribute("flightScore", pkg, REQUEST);
 					
 					// Get the IP address
 					if (ctx.isUserInRole("HR")) {
