33374346c59cfab1e2715e172852c50b84d65653
Luke Kolin <luke@deltava.org>
2018-06-18 02:11:06 +0000

    Adjust TabbedMapEntry interface
    git-svn-id: file:///home/svn/DVA2006@12581 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/TabbedMapEntry.java
+++ src/java/org/deltava/beans/TabbedMapEntry.java
@@ -1,13 +1,13 @@
-// Copyright 2006 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2006, 2018 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans;
 
-import java.util.List;
+import java.util.LinkedHashMap;
 
 /**
  * An interface to define icon formatting for entries that can be displayed in Google Maps v2
  * with tabbed infoboxes.
  * @author Luke
- * @version 1.0
+ * @version 8.3
  * @since 1.0
  */
 
@@ -17,11 +17,5 @@ public interface TabbedMapEntry extends MapEntry {
 	 * Returns the tab names. 
 	 * @return a List of tab names
 	 */
-	public List<String> getTabNames();
-	
-	/**
-	 * Returns the tab contents.
-	 * @return a List of tab contents
-	 */
-	public List<String> getTabContents();
+	public LinkedHashMap<String, String> getTabs();
 }
