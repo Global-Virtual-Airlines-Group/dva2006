95a7a4cfb64e343e0bf205ca465fc39b2a6c18c7
Luke Kolin <luke@deltava.org>
2018-09-05 14:38:58 +0000

    Initial import work
    git-svn-id: file:///home/svn/DVA2006@12712 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetRawSchedule.java
+++ src/java/org/deltava/dao/GetRawSchedule.java
@@ -1,4 +1,4 @@
-// Copyright 2017 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2017, 2018 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao;
 
 import java.sql.*;
@@ -13,9 +13,10 @@ import org.deltava.util.system.SystemData;
 /**
  * A Data Access Object to load raw schedule entries and tail codes.
  * @author Luke
- * @version 8.0
+ * @version 8.3
  * @since 8.0
  */
+
 public class GetRawSchedule extends DAO {
 
 	/**
@@ -26,10 +27,6 @@ public class GetRawSchedule extends DAO {
 		super(c);
 	}
 	
-	private static DayOfWeek getDayOfWeek(Instant dt) {
-		return ZonedDateTime.ofInstant(dt.truncatedTo(ChronoUnit.DAYS), ZoneOffset.UTC).getDayOfWeek();
-	}
-
 	/**
 	 * Loads all raw schedule entries for a particular day of the week.
 	 * @param dt the schedule effective date
@@ -37,27 +34,22 @@ public class GetRawSchedule extends DAO {
 	 * @throws DAOException if a JDBC error occurs
 	 */
 	public List<RawScheduleEntry> load(Instant dt) throws DAOException {
-		Instant tdt = dt.truncatedTo(ChronoUnit.DAYS);
+		DayOfWeek dow = ZonedDateTime.ofInstant(dt.truncatedTo(ChronoUnit.DAYS), ZoneOffset.UTC).getDayOfWeek();
 		try {
-			prepareStatement("SELECT * FROM RAW_SCHEDULE WHERE ((DAYS & ?) > 0)");
-			_ps.setInt(1, (1 << getDayOfWeek(dt).ordinal()));
+			prepareStatement("SELECT * FROM RAW_SCHEDULE WHERE (DAY=?)");
+			_ps.setInt(1, dow.ordinal());
 			
 			List<RawScheduleEntry> results = new ArrayList<RawScheduleEntry>();
 			try (ResultSet rs = _ps.executeQuery()) {
 				while (rs.next()) {
-					RawScheduleEntry se = new RawScheduleEntry(SystemData.getAirline(rs.getString(1)), rs.getInt(2), 1);
-					se.setAirportD(SystemData.getAirport(rs.getString(3)));
-					se.setAirportA(SystemData.getAirport(rs.getString(4)));
-					se.setEquipmentType(rs.getString(5));
-					se.setTimeD(rs.getTimestamp(6).toLocalDateTime().plusSeconds(tdt.getEpochSecond()));
-					se.setTimeA(rs.getTimestamp(7).toLocalDateTime().plusSeconds(tdt.getEpochSecond()));
-					se.setCodeShare(rs.getString(9));
-					int rawDays = rs.getInt(8);
-					for (DayOfWeek dow : DayOfWeek.values()) {
-						if ((rawDays & (1 << dow.ordinal())) > 0)
-							se.addDay(dow);
-					}
-
+					RawScheduleEntry se = new RawScheduleEntry(SystemData.getAirline(rs.getString(2)), rs.getInt(3), rs.getInt(4));
+					se.setAirportD(SystemData.getAirport(rs.getString(5)));
+					se.setAirportA(SystemData.getAirport(rs.getString(6)));
+					se.setEquipmentType(rs.getString(7));
+					se.setTimeD(rs.getTimestamp(8).toLocalDateTime());
+					se.setTimeA(rs.getTimestamp(9).toLocalDateTime());
+					se.setCodeShare(rs.getString(10));
+					se.setDay(dow);
 					results.add(se);
 				}
 			}
@@ -77,8 +69,8 @@ public class GetRawSchedule extends DAO {
 	 */
 	public boolean hasEntries(Instant dt) throws DAOException {
 		try {
-			prepareStatementWithoutLimits("SELECT COUNT(*) FROM RAW_SCHEDULE WHERE ((DAYS & ?) > 0)");
-			_ps.setInt(1, (1 << getDayOfWeek(dt).ordinal()));
+			prepareStatementWithoutLimits("SELECT COUNT(*) FROM RAW_SCHEDULE WHERE (DAY=?)");
+			_ps.setInt(1, ZonedDateTime.ofInstant(dt.truncatedTo(ChronoUnit.DAYS), ZoneOffset.UTC).getDayOfWeek().ordinal());
 			
 			boolean hasEntries = false;
 			try (ResultSet rs = _ps.executeQuery()) {
@@ -88,29 +80,6 @@ public class GetRawSchedule extends DAO {
 
 			_ps.close();
 			return hasEntries;
-		} catch (SQLException se) {
-			throw new DAOException(se);
-		}
-	}
-
-	/**
-	 * Loads all schedule provider tail code data.
-	 * @return a map of TailCode beans, keyed by registration code
-	 * @throws DAOException if a JDBC error occurs
-	 */
-	public Map<String, TailCode> getTailCodes() throws DAOException {
-		try {
-			prepareStatement("SELECT TAILCODE, ICAO FROM RAW_SCHEDULE_AC");
-			Map<String, TailCode> results = new TreeMap<String, TailCode>();
-			try (ResultSet rs = _ps.executeQuery()) {
-				while (rs.next()) {
-					TailCode tc = new TailCode(rs.getString(1), rs.getString(2));
-					results.put(tc.getTailCode(), tc);
-				}
-			}
-			
-			_ps.close();
-			return results;
 		} catch (SQLException se) {
 			throw new DAOException(se);
 		}
