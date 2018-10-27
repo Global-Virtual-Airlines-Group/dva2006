5bbe7697d653c7351f274004d49f5845703b532d
Luke Kolin <luke@deltava.org>
2018-10-27 14:22:27 +0000

    Add isPopulated default method
    git-svn-id: file:///home/svn/DVA2006@12830 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/schedule/RoutePair.java
+++ src/java/org/deltava/beans/schedule/RoutePair.java
@@ -1,10 +1,10 @@
-// Copyright 2009 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2009, 2018 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.schedule;
 
 /**
  * An interface to mark Airport pairs. 
  * @author Luke
- * @version 2.6
+ * @version 8.4
  * @since 2.6
  */
 
@@ -27,4 +27,12 @@ public interface RoutePair {
 	 * @return the distance in miles
 	 */
 	public int getDistance();
+
+	/**
+	 * Returns whether both airports are populated.
+	 * @return TRUE if both Airports are set, otherwise FALSE 
+	 */
+	default boolean isPopulated() {
+		return ((getAirportD() != null) && (getAirportA() != null));
+	}
 }
