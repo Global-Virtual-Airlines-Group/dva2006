42ab44263606eb35013b2e9dbef4b21dc9c5d066
Luke Kolin <luke@deltava.org>
2018-12-09 15:39:32 +0000

    git-svn-id: file:///home/svn/DVA2006@12967 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/flight/FlightStatus.java
+++ src/java/org/deltava/beans/flight/FlightStatus.java
@@ -6,21 +6,23 @@ import org.deltava.beans.ViewEntry;
 /**
  * An enumeration to store Flight Report states. 
  * @author Luke
- * @version 8.1
+ * @version 8.4
  * @since 8.1
  */
 
 public enum FlightStatus implements ViewEntry {
-	DRAFT("opt2"), SUBMITTED("opt1"), HOLD("warn"), OK(null), REJECTED("err");
+	DRAFT("opt2", ""), SUBMITTED("opt1", ""), HOLD("warn", "hold"), OK(null, "approve"), REJECTED("err", "reject");
 
 	private final String _viewClass;
+	private final String _verb;
 	
 	/**
 	 * Creates the enumeration value.
 	 * @param viewClass the row CSS class name
 	 */
-	FlightStatus(String viewClass) {
+	FlightStatus(String viewClass, String verb) {
 		_viewClass = viewClass;
+		_verb = verb;
 	}
 	
 	/**
@@ -30,9 +32,31 @@ public enum FlightStatus implements ViewEntry {
 	public String getDescription() {
 		return name().substring(0, 1) + name().substring(1).toLowerCase();
 	}
+	
+	/**
+	 * Returns the operation verb name.
+	 * @return the operation
+	 */
+	public String getVerb() {
+		return _verb;
+	}
 
 	@Override
 	public String getRowClassName() {
 		return _viewClass;
+	}
+	
+	/**
+	 * Exception-safe way to load FlightStatus from Flight Report approval verb.
+	 * @param verb the verb
+	 * @return a FlightStatus, or null if unknown
+	 */
+	public static FlightStatus fromVerb(String verb) {
+		for (FlightStatus fs : values()) {
+			if (fs.getVerb().equalsIgnoreCase(verb))
+				return fs;
+		}
+		
+		return null;
 	}
 }
