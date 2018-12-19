2cd866c57be8b2ec435ae5a67753bb25d3e5b3ef
Luke Kolin <luke@deltava.org>
2018-12-19 21:03:21 +0000

    Add MailUtils
    git-svn-id: file:///home/svn/DVA2006@12977 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetPilotNotify.java
+++ src/java/org/deltava/dao/GetPilotNotify.java
@@ -1,15 +1,16 @@
-// Copyright 2005, 2010, 2011, 2014, 2016 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2010, 2011, 2014, 2016, 2018 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao;
 
 import java.util.*;
 import java.sql.*;
 
 import org.deltava.beans.*;
+import org.deltava.util.MailUtils;
 
 /**
  * A Data Access Object to get Pilot notification lists.
  * @author Luke
- * @version 7.0
+ * @version 8.5
  * @since 1.0
  */
 
@@ -23,43 +24,6 @@ public class GetPilotNotify extends GetPilot {
       super(c);
    }
 
-   public class EMailNotificationImpl implements EMailAddress {
-      
-      private final String _name;
-      private final String _addr;
-      
-      protected EMailNotificationImpl(String firstName, String lastName, String addr) {
-         super();
-         _addr = addr;
-         _name = firstName + " " + lastName;
-      }
-      
-      @Override
-      public String getName() {
-         return _name;
-      }
-      
-      @Override
-      public String getEmail() {
-         return _addr;
-      }
-      
-      @Override
-      public boolean isInvalid() {
-    	  return false;
-      }
-      
-      @Override
-      public int hashCode() {
-    	  return _addr.hashCode();
-      }
-      
-      @Override
-      public boolean equals(Object o) {
-    	  return (o instanceof EMailAddress) ? _addr.equals(((EMailAddress) o).getEmail()) : false; 
-      }
-   }
-   
    /**
     * Returns Pilots signed up to receive a particular notification type.
     * @param notificationType the Notification Type to filter on
@@ -69,8 +33,7 @@ public class GetPilotNotify extends GetPilot {
     */
    public List<EMailAddress> getNotifications(Notification notificationType) throws DAOException {
       try {
-         prepareStatement("SELECT FIRSTNAME, LASTNAME, EMAIL FROM PILOTS WHERE (STATUS=?) AND "
-        		 + "((NOTIFY & ?) > 0) ORDER BY LASTNAME, FIRSTNAME");
+         prepareStatement("SELECT CONCAT_WS(' ', FIRSTNAME, LASTNAME) AS NM, EMAIL FROM PILOTS WHERE (STATUS=?) AND ((NOTIFY & ?) > 0) ORDER BY LASTNAME, FIRSTNAME");
          _ps.setInt(1, Pilot.ACTIVE);
          _ps.setInt(2, notificationType.getCode());
          
@@ -78,7 +41,7 @@ public class GetPilotNotify extends GetPilot {
          List<EMailAddress> results = new ArrayList<EMailAddress>();
          try (ResultSet rs = _ps.executeQuery()) {
         	 while (rs.next())
-            results.add(new EMailNotificationImpl(rs.getString(1), rs.getString(2), rs.getString(3)));	
+        		 results.add(MailUtils.makeAddress(rs.getString(2), rs.getString(1)));	
          }
          
          _ps.close();
