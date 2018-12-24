1d5167c51e2d826b6fd69754cb85a1b2e684374b
Luke Kolin <luke@deltava.org>
2018-12-24 14:56:17 +0000

    Add DeliveryType (bounce/complaint) support
    git-svn-id: file:///home/svn/DVA2006@12997 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/system/DeliveryType.java
@@ -0,0 +1,13 @@
+// Copyright 2018 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.system;
+
+/**
+ * An enumeration of e-mail delivery notification types.
+ * @author Luke
+ * @version 8.5
+ * @since 8.5
+ */
+
+public enum DeliveryType {
+	DELIVER, BOUNCE, COMPLAINT
+}
