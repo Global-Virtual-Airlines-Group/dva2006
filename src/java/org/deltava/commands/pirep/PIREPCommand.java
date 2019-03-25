131e0dd8e1b6dd46a1e8c2201c2d8c87854df8db
Luke Kolin <luke@deltava.org>
2019-03-25 23:17:40 +0000

    Load runways from actual airports, not just filed
    git-svn-id: file:///home/svn/DVA2006@13270 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/pirep/PIREPCommand.java
+++ src/java/org/deltava/commands/pirep/PIREPCommand.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.pirep;
 
 import java.io.*;
@@ -34,7 +34,7 @@ import org.deltava.util.system.SystemData;
 /**
  * A Web Site Command to handle editing/saving Flight Reports.
  * @author Luke
- * @version 8.1
+ * @version 8.6
  * @since 1.0
  */
 
@@ -567,11 +567,11 @@ public class PIREPCommand extends AbstractFormCommand {
 					
 					// Load departure and arrival runways
 					if (ac.getCanDispose()) {
-						Collection<Runway> dRwys = navdao.getRunways(info.getAirportD(), fr.getSimulator());
+						Collection<Runway> dRwys = navdao.getRunways(fr.getAirportD(), fr.getSimulator());
 						if (info.getRunwayD() != null)
 							dRwys = CollectionUtils.sort(dRwys, new RunwayComparator(info.getRunwayD().getHeading(), 5));	
 					
-						Collection<Runway> aRwys = navdao.getRunways(info.getAirportA(), fr.getSimulator());
+						Collection<Runway> aRwys = navdao.getRunways(fr.getAirportA(), fr.getSimulator());
 						if (info.getRunwayA() != null)
 							aRwys = CollectionUtils.sort(aRwys, new RunwayComparator(info.getRunwayA().getHeading(), 5));	
 					
