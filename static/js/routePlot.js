88d8a946a8c9a180b6255b799005898fda4d32fe
Luke Kolin <luke@deltava.org>
2019-06-11 21:38:59 +0000

    Adjust label
    git-svn-id: file:///home/svn/DVA2006@13548 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/routePlot.js
+++ static/js/routePlot.js
@@ -73,7 +73,7 @@ combo.options.length = data.length + 1;
 combo.options[0] = new Option('-', '');
 for (var i = 0; i < data.length; i++) {
 	var g = data[i];
-	var o = new Option(g.name + ' (Used ' + g.useCount + 'x)', g.name);
+	var o = new Option(g.name + ' (' + g.useCount + ' flights)', g.name);
 	o.ll = g.ll;
 	combo.options[i+1] = o; 
 	if (oldCode == g.name) combo.selectedIndex = (i+1);
