57404aacb012207263b91e10aea43604fa39a1d0
Luke Kolin <luke@deltava.org>
2019-07-01 03:19:29 +0000

    Show network outages
    git-svn-id: file:///home/svn/DVA2006@13616 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/servinfo/NetworkOutage.java
@@ -0,0 +1,129 @@
+// Copyright 2019 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.servinfo;
+
+import java.util.*;
+import java.time.*;
+
+import org.deltava.beans.*;
+
+/**
+ * A bean to track Online Network data feed outages.
+ * @author Luke
+ * @version 8.6
+ * @since 8.6
+ */
+
+public class NetworkOutage implements TimeSpan {
+	
+	private final OnlineNetwork _net;
+	private Instant _startTime;
+	private Instant _endTime;
+
+	/**
+	 * Creates the bean.
+	 * @param net the OnlineNetwork 
+	 */
+	public NetworkOutage(OnlineNetwork net) {
+		super();
+		_net = net;
+	}
+	
+	/**
+	 * Return the Online Network that had an outage.
+	 * @return the OnlineNetwork
+	 */
+	public OnlineNetwork getNetwork() {
+		return _net;
+	}
+
+	@Override
+	public Instant getDate() {
+		return _startTime;
+	}
+
+	@Override
+	public Instant getStartTime() {
+		return _startTime;
+	}
+
+	@Override
+	public Instant getEndTime() {
+		return _endTime;
+	}
+	
+	public Duration getDuration() {
+		return Duration.between(_startTime, _endTime);
+	}
+	
+	
+	/**
+	 * Updates the start of the outage.
+	 * @param dt the start date/time
+	 */
+	public void setStartTime(Instant dt) {
+		_startTime = dt;
+	}
+	
+	/**
+	 * Updates the end of the outage.
+	 * @param dt the end date/time
+	 */
+	public void setEndTime(Instant dt) {
+		_endTime = dt;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		return (o instanceof NetworkOutage) && (compareTo(o) == 0);
+	}
+	
+	@Override
+	public int hashCode() {
+		return toString().hashCode();
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder buf = new StringBuilder(_net.toString());
+		buf.append('-').append(_startTime);
+		buf.append('-').append(_endTime);
+		return buf.toString();
+	}
+
+	@Override
+	public int compareTo(Object o2) {
+		NetworkOutage no2 = (NetworkOutage) o2;
+		int tmpResult = _net.compareTo(no2._net);
+		if (tmpResult == 0)
+			tmpResult = _startTime.compareTo(no2._startTime);
+		
+		return (tmpResult == 0) ? _endTime.compareTo(no2._endTime) : tmpResult;
+	}
+	
+	/**
+	 * Creates a list of data feed outages from a Collection of data feed pull times.
+	 * @param net the OnlineNetwork 
+	 * @param fetchTimes a Collection of pull date/times
+	 * @param updateInterval the fetch interval in seconds
+	 * @return a Collection of NetworkOutage beans
+	 */
+	public static Collection<NetworkOutage> calculate(OnlineNetwork net, Collection<Instant> fetchTimes, int updateInterval) {
+		Collection<NetworkOutage> results = new ArrayList<NetworkOutage>();
+		if (fetchTimes.size() < 2) return results;
+		
+		List<Instant> ft2 = new ArrayList<Instant>(fetchTimes); Instant lastPull = ft2.get(0);
+		for (Instant ft : ft2) {
+			long fetchInterval = ft.getEpochSecond() - lastPull.getEpochSecond();
+			if (fetchInterval > (updateInterval + 60)) {
+				NetworkOutage no = new NetworkOutage(net);
+				no.setStartTime(lastPull.plusSeconds(updateInterval));
+				no.setEndTime(ft);
+				results.add(no);
+			}
+			
+			lastPull = ft;
+		}
+		
+		return results;
+	}
+}
