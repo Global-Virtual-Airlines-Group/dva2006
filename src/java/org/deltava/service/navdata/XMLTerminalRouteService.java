91635a78742e4e71e5007c51b9b4e919911e129e
Luke Kolin <luke@deltava.org>
2019-07-14 02:24:44 +0000

    Add hashing to response headers
    git-svn-id: file:///home/svn/DVA2006@13681 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/service/navdata/XMLTerminalRouteService.java
+++ src/java/org/deltava/service/navdata/XMLTerminalRouteService.java
@@ -1,4 +1,4 @@
-// Copyright 2007, 2008, 2009, 2012, 2015 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2007, 2008, 2009, 2012, 2015, 2019 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.service.navdata;
 
 import java.io.*;
@@ -12,6 +12,8 @@ import org.jdom2.*;
 
 import org.deltava.beans.navdata.*;
 
+import org.deltava.crypt.MessageDigester;
+
 import org.deltava.dao.*;
 import org.deltava.service.*;
 
@@ -21,11 +23,41 @@ import org.deltava.util.system.SystemData;
 /**
  * A Web Service to display Terminal Route data to ACARS clients.
  * @author Luke
- * @version 6.1
+ * @version 8.6
  * @since 2.4
  */
 
 public class XMLTerminalRouteService extends DownloadService {
+	
+	private Metadata _md;
+
+	private static class Metadata {
+		private final String _hash;
+		private final String _hashType;
+		private int _airportCount;
+		
+		protected Metadata(String hash, String hashType) {
+			super();
+			_hash = hash;
+			_hashType = hashType;
+		}
+		
+		public String getHash() {
+			return _hash;
+		}
+		
+		public String getHashType() {
+			return _hashType;
+		}
+		
+		public int getAirportCount() {
+			return _airportCount;
+		}
+		
+		public void setAirportCount(int cnt) {
+			_airportCount = cnt;
+		}
+	}
 
 	/**
 	 * Executes the Web Service.
@@ -46,10 +78,13 @@ public class XMLTerminalRouteService extends DownloadService {
 		long age = f.exists() ? (System.currentTimeMillis() - f.lastModified()) : Long.MAX_VALUE;
 
 		// Check the cache
-		if ((age < 3600_000) && (f.length() > 10240)) {
+		if ((age < 3600_000) && (f.length() > 10240) && (_md != null)) {
 			ctx.setHeader("Content-disposition", "attachment; filename=xmlsidstar.zip");
 			ctx.setContentType("application/zip");
 			ctx.setHeader("max-age", 1800);
+			ctx.setHeader("X-Airport-Count", _md.getAirportCount());
+			ctx.setHeader("X-Signature", _md.getHash());
+			ctx.setHeader("X-Signature-Type", _md.getHashType());
 			sendFile(f, ctx.getResponse());
 			return SC_OK;
 		}
@@ -125,12 +160,21 @@ public class XMLTerminalRouteService extends DownloadService {
 					}
 				}
 			}
+			
+			// Calculate the metadata
+			MessageDigester md = new MessageDigester("SHA-256", 8192);
+			try (InputStream is = new BufferedInputStream(new FileInputStream(f), 65536)) {
+				_md = new Metadata(MessageDigester.convert(md.digest(is)), md.getAlgorithm());
+				_md.setAirportCount(apCount);
+			}
 
 			// Format and write
 			ctx.setHeader("Content-disposition", "attachment; filename=xmlsidstar.zip");
 			ctx.setContentType("application/zip");
 			ctx.setExpiry(1800);
-			ctx.setHeader("airportCount", apCount);
+			ctx.setHeader("X-Airport-Count", _md.getAirportCount());
+			ctx.setHeader("X-Signature", _md.getHash());
+			ctx.setHeader("X-Signature-Type", _md.getHashType());
 			sendFile(f, ctx.getResponse());
 		} catch (Exception e) {
 			throw error(SC_INTERNAL_SERVER_ERROR, "I/O Error", false);
@@ -138,7 +182,7 @@ public class XMLTerminalRouteService extends DownloadService {
 
 		return SC_OK;
 	}
-
+	
 	/**
 	 * Returns whether this web service requires authentication.
 	 * @return TRUE always
