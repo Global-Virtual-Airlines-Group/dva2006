26492a703421a7a36c068432cd4010bbc8323f90
Luke Kolin <luke@deltava.org>
2019-09-24 15:47:35 +0000

    Add airline refresh and ECMAScript2015
    git-svn-id: file:///home/svn/DVA2006@13891 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/airportRefresh.js
+++ static/js/airportRefresh.js
@@ -1,6 +1,6 @@
 golgotha.airportLoad = golgotha.airportLoad || {config:{doICAO:false, notVisited:false, useSched:true, dst:false, myRated:false}};
 golgotha.airportLoad.config.clone = function() {
-	var o = {};
+	let o = {};
 	for (p in this) {
 		if (this.hasOwnProperty(p))
 			o[p] = this[p];
@@ -10,9 +10,9 @@ golgotha.airportLoad.config.clone = function() {
 };
 
 golgotha.airportLoad.config.URLParams = function() {
-	var params = [];
+	let params = [];
 	for (p in this) {
-		var v = this[p];
+		let v = this[p];
 		if (this.hasOwnProperty(p) && !golgotha.util.isFunction(v) && (v != null))
 			params.push(p + '=' + escape(v));
 	}
@@ -38,17 +38,25 @@ golgotha.airportLoad.setHelpers = function(combo, addSIDSTARHook) {
 	return true;
 };
 
-golgotha.airportLoad.updateAirportCode = function() {
-	var txt = this.form[this.name + 'Code'];
+golgotha.airportLoad.updateAirlineCode = function() {
+	let txt = this.form[this.name + 'Code'];
 	if (!txt) return false;
 	var o = this.options[this.selectedIndex];
+	txt.value = o.value;
+	return true;
+};
+
+golgotha.airportLoad.updateAirportCode = function() {
+	let txt = this.form[this.name + 'Code'];
+	if (!txt) return false;
+	let o = this.options[this.selectedIndex];
 	txt.value = golgotha.airportLoad.config.getCode(o);
 	return true;
 };
 
 golgotha.airportLoad.updateOrigin = function(combo) {
-	var f = document.forms[0];
-	var cfg = golgotha.airportLoad.config.clone();
+	let f = document.forms[0];
+	let cfg = golgotha.airportLoad.config.clone();
 	cfg.useSched = true; cfg.notVisited = combo.notVisited; cfg.dst = true;
 	cfg.airline = golgotha.form.getCombo(f.airline); 
 	cfg.code = (combo.selectedIndex > 0) ? golgotha.form.getCombo(combo) : null;
@@ -56,56 +64,75 @@ golgotha.airportLoad.updateOrigin = function(combo) {
 	return true;
 };
 
-golgotha.airportLoad.setAirport = function(code, fireEvent, sender)
-{
-if (code == null) return false;
-if (code.length < 2) {
-	var oldIdx = this.selectedIndex; this.selectedIndex = 0;
-	if (fireEvent && this.onchange && (oldIdx != 0)) this.onchange();
-	return true;
-}
-
-code = code.toUpperCase();
-for (var x = 0; x < this.options.length; x++) {
-	var opt = this.options[x]; var ap = opt.airport || {};
-	if ((code == opt.value) || (code == ap.icao) || (code == ap.iata)) {
-		this.selectedIndex = x;
-		if (fireEvent && this.onchange) this.onchange();
+golgotha.airportLoad.setAirline = function(cb, sender, fireEvent) {
+	let code = sender.value;
+	if (code.length < 2) {
+		var oldIdx = cb.selectedIndex; cb.selectedIndex = 0;
+		if (fireEvent && cb.onchange && (oldIdx != 0)) cb.onchange();
 		return true;
 	}
-}
 
-if (sender != null) sender.value = '';
-return false;
+	code = code.toUpperCase();
+	for (var x = 0; x < cb.options.length; x++) {
+		if (code == cb.options[x].value) {
+			cb.selectedIndex = x;
+			if (fireEvent && cb.onchange) cb.onchange();
+			return true;
+		}
+	}
+
+	sender.value = '';
+	return false;
 };
 
-golgotha.airportLoad.setOptions = function(combo, data, opts)
-{
-combo.options.length = data.length + 1;
-combo.options[0] = new Option('-', '');
-var codeAttr = (opts.doICAO) ? 'icao' : 'iata';
-for (var i = 0; i < data.length; i++) {
-	var a = data[i];
-	var apCode = a[codeAttr];
-	var opt = new Option(a.name + ' (' + apCode + ')', apCode);
-	opt.airport = a;
-	combo.options[i+1] = opt;
-}
+golgotha.airportLoad.setAirport = function(code, fireEvent, sender) {
+	if (code == null) return false;
+	if (code.length < 2) {
+		let oldIdx = this.selectedIndex; this.selectedIndex = 0;
+		if (fireEvent && this.onchange && (oldIdx != 0)) this.onchange();
+		return true;
+	}
 
-return true;
+	code = code.toUpperCase();
+	for (var x = 0; x < this.options.length; x++) {
+		let opt = this.options[x]; let ap = opt.airport || {};
+		if ((code == opt.value) || (code == ap.icao) || (code == ap.iata)) {
+			this.selectedIndex = x;
+			if (fireEvent && this.onchange) this.onchange();
+			return true;
+		}
+	}
+
+	if (sender != null) sender.value = '';
+	return false;
+};
+
+golgotha.airportLoad.setOptions = function(combo, data, opts) {
+	combo.options.length = data.length + 1;
+	combo.options[0] = new Option('-', '');
+	let codeAttr = (opts.doICAO) ? 'icao' : 'iata';
+	for (var i = 0; i < data.length; i++) {
+		let a = data[i];
+		let apCode = a[codeAttr];
+		let opt = new Option(a.name + ' (' + apCode + ')', apCode);
+		opt.airport = a;
+		combo.options[i+1] = opt;
+	}
+
+	return true;
 };
 
 golgotha.airportLoad.loadAirports = function(opts)
 {
-var oldCode = golgotha.form.getCombo(this); var combo = this;
-var xmlreq = new XMLHttpRequest();
+let oldCode = golgotha.form.getCombo(this); let combo = this;
+let xmlreq = new XMLHttpRequest();
 xmlreq.open('GET', 'airports.ws?' + opts.URLParams(), true);
 xmlreq.onreadystatechange = function() {
 	if ((xmlreq.readyState != 4) || (xmlreq.status != 200)) return false;
-	var o = combo.options[combo.selectedIndex];
-	var oldCodes = ((o) && (o.airport)) ? [o.airport.iata, o.airport.icao] : [null];
-	var isChanged = (oldCodes.indexOf(oldCode) < 0);
-	var jsData = JSON.parse(xmlreq.responseText);
+	let o = combo.options[combo.selectedIndex];
+	let oldCodes = ((o) && (o.airport)) ? [o.airport.iata, o.airport.icao] : [null];
+	let isChanged = (oldCodes.indexOf(oldCode) < 0);
+	let jsData = JSON.parse(xmlreq.responseText);
 	golgotha.airportLoad.setOptions(combo, jsData, opts);
 	combo.setAirport(oldCode, isChanged);
 	combo.disabled = false;
@@ -120,13 +147,13 @@ return true;
 
 golgotha.airportLoad.loadSIDSTAR = function(code, type)
 {
-var oldValue = golgotha.form.getCombo(this); var combo = this;
-var xmlreq = new XMLHttpRequest();
+let oldValue = golgotha.form.getCombo(this); let combo = this;
+let xmlreq = new XMLHttpRequest();
 xmlreq.open('GET', 'troutes.ws?airportD=' + code + '&airportA=' + code, true);
 xmlreq.onreadystatechange = function() {
 	if ((xmlreq.readyState != 4) || (xmlreq.status != 200)) return false;
-	var js = JSON.parse(xmlreq.responseText);
-	var jsData = js[type];
+	let js = JSON.parse(xmlreq.responseText);
+	let jsData = js[type];
 	combo.options.length = jsData.length + 1;
 	combo.options[0] = new Option('-', '');
 	for (var i = 0; i < trs.length; i++)
@@ -144,24 +171,20 @@ return true;
 };
 
 golgotha.airportLoad.changeAirline = function(combos, opts) {
-	for (var x = 0; x < combos.length; x++)
-		combos[x].loadAirports(opts);
-
+	combos.forEach(function(c) { c.loadAirports(opts); });
 	golgotha.event.beacon('Airports', 'Change Airline');
 	return true;
 };
 
 golgotha.airportLoad.massageSelects = function(root) {
-	var opts = golgotha.util.getElementsByClass('airport', 'option', root);
-	for (var op = opts.pop(); (op != null); op = opts.pop())
-		op.airport = {name:op.text, iata:op.getAttribute('iata'), icao:op.getAttribute('icao')};
-
+	let opts = golgotha.util.getElementsByClass('airport', 'option', root);
+	opts.forEach(function(op) { op.airport = {name:op.text, iata:op.getAttribute('iata'), icao:op.getAttribute('icao')}; });
 	return opts.length;
 };
 
 golgotha.airportLoad.codeMassage = function() {
-	var e = window.event;
-	var c = e.which || e.keyCode;
+	let e = window.event;
+	let c = e.which || e.keyCode;
 	if (((c > 64) && (c < 91)) || ((c > 96) && (c < 123)))
 		return true;
 
