edd94b537eec0f5abeee97b342270f873bc70a7e
Luke Kolin <luke@deltava.org>
2019-10-27 13:33:20 +0000

    Allow date filtering
    git-svn-id: file:///home/svn/DVA2006@14033 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/pilot/PilotRouteMapCommand.java
+++ src/java/org/deltava/commands/pilot/PilotRouteMapCommand.java
@@ -1,22 +1,35 @@
-// Copyright 2007, 2009, 2012, 2014 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2007, 2009, 2012, 2014, 2019 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.pilot;
 
-import org.deltava.beans.Pilot;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.sql.Connection;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import org.deltava.beans.*;
+import org.deltava.beans.flight.*;
 import org.deltava.beans.schedule.*;
 
 import org.deltava.commands.*;
 import org.deltava.dao.*;
 
+import org.deltava.util.ComboUtils;
+import org.deltava.util.StringUtils;
 import org.deltava.util.system.SystemData;
 
 /**
  * A Web Site Command to disply all the routes the pilot has flown.
  * @author Luke
- * @version 5.4
+ * @version 8.7
  * @since 1.0
  */
 
 public class PilotRouteMapCommand extends AbstractCommand {
+	
+	private static final Collection<ComboAlias> DATE_OPTS = ComboUtils.fromArray(new String[] {"All Flights", "30 Days", "90 Days", "180 Days", "1 Year", "2 Years", "5 Years", "10 Years", "15 Years", "20 Years"}, 
+			new String[] {"0", "30", "90", "180", "365", "720", "1825", "3650", "5475", "7300"});
 
 	/**
 	 * Executes the command.
@@ -32,10 +45,20 @@ public class PilotRouteMapCommand extends AbstractCommand {
 			userID = ctx.getID();
 		
 		try {
-			GetPilot pdao = new GetPilot(ctx.getConnection());
+			Connection con = ctx.getConnection();
+			GetPilot pdao = new GetPilot(con);
 			Pilot usr = pdao.get(userID);
 			if (usr == null)
 				throw notFoundException("Unknown Pilot ID - " + userID);
+			
+			// Get the first flight date
+			GetFlightReports frdao = new GetFlightReports(con);
+			FlightReport fr = frdao.getByPilot(userID, new ScheduleSearchCriteria("DATE, PR.SUBMITTED")).stream().filter(f -> (f.getStatus() != FlightStatus.DRAFT && f.getStatus() != FlightStatus.REJECTED)).findFirst().orElse(null);
+			Instant firstFlight = (fr != null) ? fr.getDate() : Instant.now().minus(1, ChronoUnit.DAYS);
+			long days = Duration.between(firstFlight, Instant.now()).toDays();
+			
+			// Filter the dateOps
+			Collection<ComboAlias> dateOpts = DATE_OPTS.stream().filter(ca -> (StringUtils.parse(ca.getComboAlias(), Integer.MAX_VALUE) < days)).collect(Collectors.toList());
 			
 			// Save the user's home airport
 			Airport airportH = SystemData.getAirport(usr.getHomeAirport());
@@ -43,6 +66,7 @@ public class PilotRouteMapCommand extends AbstractCommand {
 				airportH = SystemData.getAirport("LFPG");
 			
 			// Save in request
+			ctx.setAttribute("dateOptions", dateOpts, REQUEST);
 			ctx.setAttribute("home", airportH, REQUEST);
 			ctx.setAttribute("pilot", usr, REQUEST);
 		} catch (DAOException de) {
