8c5b2c06e5ab15c942ea26f6e1b493876ca471fa
Luke Kolin <luke@deltava.org>
2019-11-29 16:00:44 +0000

    Refactor schedule DAOs
    git-svn-id: file:///home/svn/DVA2006@14126 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/dao/file/GetDeltaSchedule.java
@@ -0,0 +1,171 @@
+// Copyright 2019 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.dao.file;
+
+import java.io.*;
+import java.util.*;
+import java.time.*;
+import java.time.format.*;
+import java.time.temporal.*;
+
+import org.apache.log4j.Logger;
+
+import org.deltava.beans.schedule.*;
+
+import org.deltava.dao.DAOException;
+import org.deltava.util.FlightCodeParser;
+import org.deltava.util.StringUtils;
+import org.deltava.util.Tuple;
+import org.deltava.util.system.SystemData;
+
+/**
+ * A Data Access Object to load the Delta flight schedule.
+ * @author Luke
+ * @version 9.0
+ * @since 9.0
+ */
+
+public class GetDeltaSchedule extends ScheduleLoadDAO {
+	
+	private final DateTimeFormatterBuilder _dfb = new DateTimeFormatterBuilder().appendPattern("MMM-dd");
+	private final DateTimeFormatter _tf = new DateTimeFormatterBuilder().appendPattern("h[h]:mma").toFormatter();
+	private DateTimeFormatter _df;
+	
+	private LocalDate _effDate = LocalDate.now();
+	
+	private static final Logger log = Logger.getLogger(GetDeltaSchedule.class);
+
+	/**
+	 * Initializes the Data Access Object.
+	 * @param is the InputStream to read
+	 */
+	public GetDeltaSchedule(InputStream is) {
+		super(ScheduleSource.DELTA, is);
+	}
+	
+	/**
+	 * Sets the flight schedule effective date.
+	 * @param ldt a LocalDateTime
+	 */
+	public void setEffectiveDate(LocalDateTime ldt) {
+		if (ldt != null)
+			_effDate = ldt.truncatedTo(ChronoUnit.DAYS).toLocalDate();
+	}
+
+	@Override
+	public Collection<RawScheduleEntry> process() throws DAOException {
+		_df = _dfb.parseDefaulting(ChronoField.YEAR, _effDate.getYear()).toFormatter();
+		
+		try (LineNumberReader lr = getReader()) {
+			Collection<RawScheduleEntry> results = new ArrayList<RawScheduleEntry>();
+		
+			while (lr.ready()) {
+				String data = lr.readLine(); boolean isOK = true; FlightData fd = null;
+				try {
+					fd = parse(data);
+				} catch (Exception e) {
+					log.warn("Parse error at Line " + lr.getLineNumber() + " - " + e.getMessage());
+				}
+				
+				if (fd == null) continue;
+				Tuple<LocalDate, LocalDate> dts = parseDates(fd.startDate, fd.endDate);
+				if (dts == null) continue;
+				
+				boolean isCodeShare = fd.flightNumber.endsWith("*");
+				if (isCodeShare)
+					fd.flightNumber = fd.flightNumber.substring(0, fd.flightNumber.length() - 1);
+						
+				RawScheduleEntry rse = new RawScheduleEntry(FlightCodeParser.parse(fd.flightNumber));
+				rse.setAirportD(SystemData.getAirport(fd.airportD));
+				rse.setAirportA(SystemData.getAirport(fd.airportA));
+				rse.setEquipmentType(getEquipmentType(fd.eqType));
+				rse.setStartDate(dts.getLeft());
+				rse.setEndDate(dts.getRight());
+				rse.setSource(ScheduleSource.DELTA);
+				rse.setLineNumber(lr.getLineNumber());
+				rse.setTimeD(LocalDateTime.of(_effDate, LocalTime.parse(fd.timeD, _tf)));
+				rse.setTimeA(LocalDateTime.of(_effDate, LocalTime.parse(fd.timeA, _tf)));
+				for (char c : fd.daysOfWeek.toCharArray()) {
+					if (Character.isDigit(c))
+						rse.addDayOfWeek(DayOfWeek.of(Character.getNumericValue(c)));
+				}
+				
+				if (rse.getAirportD() == null) {
+					isOK = false;
+					_status.addInvalidAirport(fd.airportD);
+					log.warn("Unknown airport at Line " + lr.getLineNumber() + " - " + fd.airportD);
+					_status.addMessage("Unknown airport at Line " + lr.getLineNumber() + " - " + fd.airportD);
+				} else if (rse.getAirportA() == null) {
+					isOK = false;
+					_status.addInvalidAirport(fd.airportA);
+					log.warn("Unknown airport at Line " + lr.getLineNumber() + " - " + fd.airportA);
+					_status.addMessage("Unknown airport at Line " + lr.getLineNumber() + " - " + fd.airportA);
+				} else if (rse.getEquipmentType() == null) {
+					isOK = false;
+					_status.addInvalidEquipment(fd.eqType);
+					log.warn("Unknown equipment code at Line " + lr.getLineNumber() + " - " + fd.eqType + " (" + data + ")");
+					_status.addMessage("Unknown equipment code at Line " + lr.getLineNumber() + " - " + fd.eqType);
+				} else if (rse.getAirline() == null) {
+					isOK = false;
+					_status.addInvalidAirline(fd.flightNumber.substring(0, 2));
+					log.warn("Unknown airline at Line " + lr.getLineNumber() + " - " + fd.flightNumber + " (" + data + ")");
+					_status.addMessage("Unknown airline at Line " + lr.getLineNumber() + " - " + fd.flightNumber);
+				} else if (!rse.getAirline().getApplications().contains(SystemData.get("airline.code"))) {
+					isOK = false;
+					log.info("Disabled airline at Line " + lr.getLineNumber() + " - " + rse.getAirline().getCode() + " (" + fd.flightNumber.substring(0, 2) + ")");
+				}
+					
+				if (isOK && (!isCodeShare || (rse.getFlightNumber() < 6100)))
+					results.add(rse);
+				else if (isOK)
+					log.info("Skipping codeshare " + fd.flightNumber);
+			}
+
+			return results;
+		} catch (IOException ie) {
+			throw new DAOException(ie);
+		}
+	}
+	
+	private static FlightData parse(String data) {
+		if (StringUtils.isEmpty(data)) return null;
+		List<String> tkns = StringUtils.split(data, " ");
+		if (tkns.size() < 14) return null;
+		
+		FlightData fd = new FlightData();
+		fd.flightNumber = tkns.get(13);
+		fd.startDate = tkns.get(0);
+		fd.endDate = tkns.get(1);
+		fd.airportD = tkns.get(2);
+		fd.airportA = tkns.get(4);
+		fd.eqType = tkns.get(tkns.size() - 4);
+		fd.timeD = tkns.get(3).toUpperCase() + "M";
+		String tA = tkns.get(5);
+		if (tA.endsWith("+1"))
+			tA = tA.substring(0, tA.length() - 2);
+		
+		fd.timeA = tA.toUpperCase() + "M";
+		
+		StringBuilder dwBuf = new StringBuilder();
+		for (int ofs = 6; ofs < 13; ofs++) {
+			char c = tkns.get(ofs).charAt(0);
+			if (Character.isDigit(c))
+				dwBuf.append(c);
+		}
+		
+		fd.daysOfWeek = dwBuf.toString();
+		return fd;
+	}
+	
+	private Tuple<LocalDate, LocalDate> parseDates(String startDate, String endDate) {
+		try {
+			LocalDate startD = "-".equals(startDate) ? LocalDate.of(_effDate.getYear(), 1, 1) : LocalDate.parse(startDate, _df);
+			LocalDate endD = "-".equals(endDate) ? LocalDate.of(_effDate.getYear(), 12, 31) : LocalDate.parse(startDate, _df);
+			if (endD.isBefore(startD))
+				endD = endD.plusYears(1);
+			
+			return Tuple.create(startD, endD);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
