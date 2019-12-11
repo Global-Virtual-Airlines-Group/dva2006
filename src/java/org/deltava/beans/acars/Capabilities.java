2f9a738a88f2c6626f69a86f150353eb7c7a2088
Luke Kolin <luke@deltava.org>
2019-12-11 16:20:31 +0000

    Add xPilot capability
    git-svn-id: file:///home/svn/DVA2006@14184 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/acars/Capabilities.java
+++ src/java/org/deltava/beans/acars/Capabilities.java
@@ -4,14 +4,14 @@ package org.deltava.beans.acars;
 /**
  * An enumeration to store Simulator capabilities.
  * @author Luke
- * @version 8.7
+ * @version 9.0
  * @since 8.6
  */
 
 public enum Capabilities {
-	GSX(0, "GSX", true), RAAS(1, "RAAS", true), XSB(2, "XSquawkbox",true), FUEL_LOAD(3, "Fuel Loading"), PAX_LOAD(4, "Passenger Loading"), 
+	GSX(0, "GSX", true), RAAS(1, "RAAS", true), XSB(2, "XSquawkbox", true), FUEL_LOAD(3, "Fuel Loading"), PAX_LOAD(4, "Passenger Loading"), 
 	FMC_LOAD(5, "FMC Fuel/Passenger Loaidng"), ANNOUNCE(6, "Cabin Announcements"), AFTERBURNER(7, "Afterburner"), JETWAY(8, "Jetway Control"), 
-	FMC(9, "FMC", true), VPILOT(10, "vPilot", true), XIVAP(11, "X-IvAp", true), IVAP(12, "IvAp", true);
+	FMC(9, "FMC", true), VPILOT(10, "vPilot", true), XIVAP(11, "X-IvAp", true), IVAP(12, "IvAp", true), CABINSIZE(13, "Cabin Size"), XPILOT(14, "xPilot", true);
 	
 	private final long _mask;
 	private final String _desc;
