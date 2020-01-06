e457fde5c68e76e04918f232deba20ad002f8f05
Luke Kolin <luke@deltava.org>
2020-01-06 22:55:00 +0000

    Use Servlet 4.0 HttpFilter
    git-svn-id: file:///home/svn/DVA2006@14299 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/servlet/filter/FileUploadFilter.java
+++ src/java/org/deltava/servlet/filter/FileUploadFilter.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2007, 2009, 2011, 2012, 2015, 2017 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2007, 2009, 2011, 2012, 2015, 2017, 2020 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.servlet.filter;
 
 import java.io.*;
@@ -18,19 +18,15 @@ import org.deltava.util.StringUtils;
 /**
  * A servlet filter to support saving multi-part form upload data into the servlet request.
  * @author Luke
- * @version 7.5
+ * @version 9.0
  * @since 1.0
  */
 
-public class FileUploadFilter implements Filter {
+public class FileUploadFilter extends HttpFilter {
 
 	private static final Logger log = Logger.getLogger(FileUploadFilter.class);
 	private static final String CONTENT_TYPE = "multipart/form-data";
 
-	/**
-	 * Called by the servlet container when the filter is started. Logs a message.
-	 * @param cfg the Filter Configuration
-	 */
 	@Override
 	public void init(FilterConfig cfg) throws ServletException {
 		log.info("Started");
@@ -38,27 +34,26 @@ public class FileUploadFilter implements Filter {
 
 	/**
 	 * Called by the servlet container on each request. Saves file upload fields in the request.
-	 * @param req the Servlet Request
-	 * @param rsp the Servlet Response
+	 * @param req the request
+	 * @param rsp the response
 	 * @param fc the Filter Chain
 	 * @throws IOException if an I/O error occurs
 	 * @throws ServletException if a general error occurs
 	 */
 	@Override
-	public void doFilter(ServletRequest req, ServletResponse rsp, FilterChain fc) throws IOException, ServletException {
+	public void doFilter(HttpServletRequest req, HttpServletResponse rsp, FilterChain fc) throws IOException, ServletException {
 
 		// Convert the request type and encoding
-		HttpServletRequest hreq = (HttpServletRequest) req;
-		hreq.setCharacterEncoding("UTF-8");
+		req.setCharacterEncoding("UTF-8");
 
 		// Check if we're doing a POST
-		String cType = hreq.getContentType();
-		if (("POST".equalsIgnoreCase(hreq.getMethod())) && (!StringUtils.isEmpty(cType)) && (cType.startsWith(CONTENT_TYPE))) {
-			FileUploadRequestWrapper reqWrap = new FileUploadRequestWrapper(hreq);
+		String cType = req.getContentType();
+		if (("POST".equalsIgnoreCase(req.getMethod())) && (!StringUtils.isEmpty(cType)) && (cType.startsWith(CONTENT_TYPE))) {
+			FileUploadRequestWrapper reqWrap = new FileUploadRequestWrapper(req);
 			if (log.isDebugEnabled())
 				log.debug("Processing form upload request");
 
-			for (Part p : hreq.getParts()) {
+			for (Part p : req.getParts()) {
 				if (log.isDebugEnabled())
 					log.debug(p.getName() + " " + p.getHeaderNames());
 				String fName = p.getSubmittedFileName();
@@ -69,7 +64,7 @@ public class FileUploadFilter implements Filter {
 					FileUpload upload = new FileUpload(fName);
 					try {
 						upload.load(p.getInputStream());
-						hreq.setAttribute("FILE$" + p.getName(), upload);
+						req.setAttribute("FILE$" + p.getName(), upload);
 					} catch (IOException ie) {
 						log.warn("Cannot load attachment - " + ie.getMessage());
 						reqWrap.setAttribute(INVALIDREQ_ATTR_NAME, ie);
@@ -92,10 +87,10 @@ public class FileUploadFilter implements Filter {
 			}
 
 			// Add requests from the command line and convert to the proper character set
-			Enumeration<String> pNames = hreq.getParameterNames();
+			Enumeration<String> pNames = req.getParameterNames();
 			while (pNames.hasMoreElements()) {
 				String pName = pNames.nextElement();
-				String[] rawValues = hreq.getParameterValues(pName);
+				String[] rawValues = req.getParameterValues(pName);
 				for (int x = 0; x < rawValues.length; x++)
 					rawValues[x] = new String(rawValues[x].getBytes("ISO-8859-1"), "UTF-8");
 
@@ -108,9 +103,6 @@ public class FileUploadFilter implements Filter {
 			fc.doFilter(req, rsp);
 	}
 
-	/**
-	 * Called by the servlet container when the filter is stopped. Logs a message.
-	 */
 	@Override
 	public void destroy() {
 		log.info("Stopped");
--- src/java/org/deltava/servlet/filter/RequestWrapperFilter.java
+++ src/java/org/deltava/servlet/filter/RequestWrapperFilter.java
@@ -1,10 +1,10 @@
-// Copyright 2005, 2008, 2015 Global Virtual Airline Group. All Rights Reserved.
+// Copyright 2005, 2008, 2015, 2020 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.servlet.filter;
 
 import java.io.IOException;
+
 import javax.servlet.*;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.*;
 
 import org.apache.log4j.Logger;
 
@@ -12,19 +12,14 @@ import org.apache.log4j.Logger;
  * A servlet filter to wrap HTTP servlet requests with a custom wrapper. This filter will
  * also extract cookies into servlet request attributes.
  * @author Luke
- * @version 6.3
+ * @version 9.0
  * @since 1.0
  */
 
-public class RequestWrapperFilter implements Filter {
+public class RequestWrapperFilter extends HttpFilter {
     
     private static final Logger log = Logger.getLogger(RequestWrapperFilter.class);
 
-    
-    /**
-     * Called by the servlet container when the filter is started. Logs a message.
-     * @param cfg the Filter Configuration
-     */
     @Override
     public void init(FilterConfig cfg) throws ServletException {
         log.info("Started");
@@ -32,38 +27,32 @@ public class RequestWrapperFilter implements Filter {
 
     /**
      * Called by the servlet container on each request. Wraps the request with a custom wrapper.
-     * @param req the Servlet Request
-     * @param rsp the Servlet Response
+     * @param req the request
+     * @param rsp the response
      * @param fc the Filter Chain
      * @throws IOException if an I/O error occurs
      * @throws ServletException if a general error occurs
      */
     @Override
-    public void doFilter(ServletRequest req, ServletResponse rsp, FilterChain fc) throws IOException, ServletException {
+    public void doFilter(HttpServletRequest req, HttpServletResponse rsp, FilterChain fc) throws IOException, ServletException {
 
-       	HttpServletRequest hreq = (HttpServletRequest) req;
-        	
        	// Get cookies
-       	Cookie[] cookies = hreq.getCookies();
+       	Cookie[] cookies = req.getCookies();
        	if (cookies != null) {
        		for (int x = 0; x < cookies.length; x++) {
        			Cookie c = cookies[x];
-       			hreq.setAttribute("COOKIE$" + c.getName(), c);
+       			req.setAttribute("COOKIE$" + c.getName(), c);
        		}
        	}
         	
        	// Save HTTP version
-       	String p = hreq.getProtocol();
-       	hreq.setAttribute("HTTP$Version", p.substring(p.lastIndexOf('/') + 1));
+       	String p = req.getProtocol();
+       	req.setAttribute("HTTP$Version", p.substring(p.lastIndexOf('/') + 1));
         	
        	// Roll the request wrapper
-       	ServletRequest newReq = new CustomRequestWrapper(hreq);
-       	fc.doFilter(newReq, rsp);
+       	fc.doFilter(new CustomRequestWrapper(req), rsp);
     }
 
-    /**
-     * Called by the servlet container when the filter is stopped. Logs a message. 
-     */
     @Override
     public void destroy() {
         log.info("Stopped");
