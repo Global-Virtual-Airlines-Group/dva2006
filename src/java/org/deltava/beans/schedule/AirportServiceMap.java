9959ee171aa45b7c8c06a940c78bd43a31e46e6f
Luke Kolin <luke@deltava.org>
2020-01-26 23:43:44 +0000

    Only import 1 source per routePair
    git-svn-id: file:///home/svn/DVA2006@14326 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/schedule/AirportServiceMap.java
+++ src/java/org/deltava/beans/schedule/AirportServiceMap.java
@@ -1,4 +1,4 @@
-// Copyright 2006, 2007, 2012, 2017 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2006, 2007, 2012, 2017, 2020 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.schedule;
 
 import java.util.*;
@@ -6,7 +6,7 @@ import java.util.*;
 /**
  * A bean to track which Airports are served by particular Airlines. 
  * @author Luke
- * @version 7.4
+ * @version 9.0
  * @since 1.0
  */
 
@@ -15,16 +15,17 @@ public class AirportServiceMap extends TreeMap<Airline, Collection<Airport>> {
 	/**
 	 * Adds a particular Airport/Airline pair.
 	 * @param a the Airline bean
-	 * @param ap the Airport bean
+	 * @param aps the Airport bean(s)
 	 */
-	public void add(Airline a, Airport ap) {
+	public void add(Airline a, Airport... aps) {
 		Collection<Airport> airports = get(a);
 		if (airports == null) {
 			airports = new HashSet<Airport>();
 			put(a, airports);
 		}
 
-		airports.add(ap);
+		for (int x = 0; x < aps.length; x++)
+			airports.add(aps[x]);
 	}
 
 	/**
@@ -34,8 +35,7 @@ public class AirportServiceMap extends TreeMap<Airline, Collection<Airport>> {
 	 * @return TRUE if the Airline serves the Airport, otherwise FALSE
 	 */
 	public boolean isServiced(Airport ap, Airline a) {
-		Collection<Airport> airports = get(a);
-		return (airports != null) && airports.contains(ap);
+		return getOrDefault(a, Collections.emptyList()).contains(ap);
 	}
 
 	/**
@@ -47,11 +47,7 @@ public class AirportServiceMap extends TreeMap<Airline, Collection<Airport>> {
 	 */
 	public Collection<String> getAirlineCodes(Airport a) {
 		Collection<String> results = new TreeSet<String>();
-		for (Map.Entry<Airline, Collection<Airport>> me : entrySet()) {
-			if (me.getValue().contains(a))
-				results.add(me.getKey().getCode());
-		}
-
+		entrySet().stream().filter(me -> me.getValue().contains(a)).map(me -> me.getKey().getCode()).forEach(results::add);
 		return results;
 	}
 }
