f8e006b79704b7fe3316e0d69e647d602f3c525f
Luke Kolin <luke@deltava.org>
2020-05-20 21:07:18 +0000

    Clean up log writing
    git-svn-id: file:///home/svn/DVA2006@14712 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/SetSystemData.java
+++ src/java/org/deltava/dao/SetSystemData.java
@@ -4,12 +4,13 @@ package org.deltava.dao;
 import java.sql.*;
 import java.util.*;
 import java.time.Instant;
-import java.time.zone.ZoneRules;
 
 import org.deltava.beans.TZInfo;
 import org.deltava.beans.servlet.CommandLog;
-import org.deltava.beans.system.APIRequest;
-import org.deltava.beans.system.BlacklistEntry;
+import org.deltava.beans.stats.APIUsage;
+import org.deltava.beans.system.*;
+
+import org.deltava.util.cache.*;
 
 /**
  * A Data Access Object to write system logging (user commands, tasks) entries.
@@ -19,6 +20,8 @@ import org.deltava.beans.system.BlacklistEntry;
  */
 
 public class SetSystemData extends DAO {
+
+	private static final Cache<APIUsage> _todayAPICache = CacheManager.get(APIUsage.class, "APIStats");
 
 	/**
 	 * Initialize the Data Access Object.
@@ -55,28 +58,33 @@ public class SetSystemData extends DAO {
 			throw new DAOException(se);
 		}
 	}
-	
-	   /**
-	    * Writes external API requests to the database.
-	    * @param reqs a Collection of APIRequest beans
-	    * @throws DAOException if a JDBC error occurs
-	    */
-	   public void logAPIRequests(Collection<APIRequest> reqs) throws DAOException {
-		 try (PreparedStatement ps = prepareWithoutLimits("INSERT INTO SYS_API_USAGE (USAGE_DATE, API, USE_COUNT, ANONYMOUS) VALUES (?, ?, 1, ?) ON DUPLICATE KEY UPDATE USE_COUNT=USE_COUNT+1, ANONYMOUS=ANONYMOUS+?")) {
-			 for (APIRequest req : reqs) {
-				 ps.setTimestamp(1, createTimestamp(req.getTime()));
-				 ps.setString(2, req.getName());
-				 ps.setInt(3, req.getIsAnonymous() ? 1 : 0);
-				 ps.setInt(4, req.getIsAnonymous() ? 1 : 0);
-				 ps.addBatch();
-			 }
-			 
-			 executeUpdate(ps, 1, reqs.size());
-		 } catch (SQLException se) {
-			 throw new DAOException(se);
-		 }
-	   }
-	
+
+	/**
+	 * Writes external API requests to the database.
+	 * @param reqs a Collection of APIRequest beans
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public void logAPIRequests(Collection<APIRequest> reqs) throws DAOException {
+		Collection<String> cacheKeys = new HashSet<String>();
+		try (PreparedStatement ps = prepareWithoutLimits("INSERT INTO SYS_API_USAGE (USAGE_DATE, API, USE_COUNT, ANONYMOUS) VALUES (?, ?, 1, ?) ON DUPLICATE KEY UPDATE USE_COUNT=USE_COUNT+1, ANONYMOUS=ANONYMOUS+?")) {
+			for (APIRequest req : reqs) {
+				cacheKeys.add(req.getName());
+				ps.setTimestamp(1, createTimestamp(req.getTime()));
+				ps.setString(2, req.getName());
+				ps.setInt(3, req.getIsAnonymous() ? 1 : 0);
+				ps.setInt(4, req.getIsAnonymous() ? 1 : 0);
+				ps.addBatch();
+			}
+
+			executeUpdate(ps, 1, reqs.size());
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+		
+		// Invalidate cache keys
+		cacheKeys.forEach(key -> _todayAPICache.remove(key));
+	}
+
 	/**
 	 * Logs the execution time of a Scheduled Task.
 	 * @param name the Scheduled Task name
@@ -85,39 +93,29 @@ public class SetSystemData extends DAO {
 	 */
 	public void logTaskExecution(String name, long execTime) throws DAOException {
 		try (PreparedStatement ps = prepareWithoutLimits("REPLACE INTO SYS_TASKS (ID, LASTRUN, RUNTIME) VALUES (?, NOW(), ?)")) {
-	      ps.setString(1, name);
-	      ps.setLong(2, execTime);
-	      executeUpdate(ps, 1);
-	   } catch (SQLException se) {
-	      throw new DAOException(se);
-	   }
-	}
-	
-	/**
-	 * Purges entries out of a System log table.
-	 * @param tableName the table name
-	 * @param colName the date column name
-	 * @param days the number of days back to set the cutoff date
-	 * @return the number of entries deleted
-	 * @throws DAOException if a JDBC error occurs
-	 */
-	public int purge(String tableName, String colName, int days) throws DAOException {
-		
-		// Build the SQL statement
-		StringBuilder sqlBuf = new StringBuilder("DELETE FROM SYS_");
-		sqlBuf.append(tableName.toUpperCase());
-		sqlBuf.append(" WHERE (");
-		sqlBuf.append(colName.toUpperCase());
-		sqlBuf.append(" < DATE_SUB(NOW(), INTERVAL ? DAY))");
-		
-		try (PreparedStatement ps = prepareWithoutLimits(sqlBuf.toString())) {
-			ps.setInt(1, days);
-			return ps.executeUpdate();
+			ps.setString(1, name);
+			ps.setLong(2, execTime);
+			executeUpdate(ps, 1);
 		} catch (SQLException se) {
 			throw new DAOException(se);
 		}
 	}
-	
+
+	/**
+	 * Purges entries out of a System commands log table.
+	 * @param days the number of days back to set the cutoff date
+	 * @return the number of entries deleted
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public int purgeCommands(int days) throws DAOException {
+		try (PreparedStatement ps = prepareWithoutLimits("DELETE FROM SYS_COMMANDS WHERE (CMDDATE < DATE_SUB(NOW(), INTERVAL ? DAY))")) {
+			ps.setInt(1, days);
+			return executeUpdate(ps, 0);
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+
 	/**
 	 * Logs user authentication.
 	 * @param dbName the database name
@@ -128,12 +126,12 @@ public class SetSystemData extends DAO {
 	 */
 	public void login(String dbName, int id, String addr, String host) throws DAOException {
 		String h = (host == null) ? addr : host;
-		
+
 		// Build the SQL statement
 		StringBuilder sqlBuf = new StringBuilder("INSERT INTO ");
 		sqlBuf.append(formatDBName(dbName));
 		sqlBuf.append(".SYS_LOGINS (ID, REMOTE_ADDR, REMOTE_HOST, LOGINS) VALUES (?, INET6_ATON(?), ?, 1) ON DUPLICATE KEY UPDATE LOGINS=LOGINS+1, REMOTE_HOST=?");
-		
+
 		try (PreparedStatement ps = prepare(sqlBuf.toString())) {
 			ps.setInt(1, id);
 			ps.setString(2, addr);
@@ -158,7 +156,7 @@ public class SetSystemData extends DAO {
 			throw new DAOException(se);
 		}
 	}
-	
+
 	/**
 	 * Writes a login/registration blacklist entry to the database.
 	 * @param be a BlacklistEntry bean
@@ -182,8 +180,8 @@ public class SetSystemData extends DAO {
 	 * @throws DAOException if a JDBC error occurs
 	 */
 	public void write(TZInfo tz) throws DAOException {
-		ZoneRules zr = tz.getZone().getRules(); Instant now = Instant.now();
-		try (PreparedStatement ps =  prepareWithoutLimits("INSERT INTO common.TZ (CODE, NAME, ABBR, GMT_OFFSET, DST) VALUES (?, ?, ?, ?, ?)")) {
+		java.time.zone.ZoneRules zr = tz.getZone().getRules(); Instant now = Instant.now();
+		try (PreparedStatement ps = prepareWithoutLimits("INSERT INTO common.TZ (CODE, NAME, ABBR, GMT_OFFSET, DST) VALUES (?, ?, ?, ?, ?)")) {
 			ps.setString(1, tz.getID());
 			ps.setString(2, tz.getName());
 			ps.setString(3, tz.getAbbr());
@@ -194,7 +192,7 @@ public class SetSystemData extends DAO {
 			throw new DAOException(se);
 		}
 	}
-	
+
 	/**
 	 * Updates an existing Time Zone entry in the database.
 	 * @param oldID the old Time Zone code
@@ -202,8 +200,8 @@ public class SetSystemData extends DAO {
 	 * @throws DAOException if a JDBC error occurs
 	 */
 	public void update(String oldID, TZInfo tz) throws DAOException {
-		ZoneRules zr = tz.getZone().getRules(); Instant now = Instant.now();
-		try (PreparedStatement ps = prepare("UPDATE common.TZ SET CODE=?, NAME=?, ABBR=?, GMT_OFFSET=?, DST=? WHERE (CODE=?)")) {
+		java.time.zone.ZoneRules zr = tz.getZone().getRules(); Instant now = Instant.now();
+		try (PreparedStatement ps = prepareWithoutLimits("UPDATE common.TZ SET CODE=?, NAME=?, ABBR=?, GMT_OFFSET=?, DST=? WHERE (CODE=?)")) {
 			ps.setString(1, tz.getID());
 			ps.setString(2, tz.getName());
 			ps.setString(3, tz.getAbbr());
