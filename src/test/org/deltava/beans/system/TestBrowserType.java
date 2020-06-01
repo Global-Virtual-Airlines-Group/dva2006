f9b0413ad2589701a5995fb9d970ecbf48ed76c5
Luke Kolin <luke@deltava.org>
2020-06-01 12:51:08 +0000

    Detect GoogleBot properly
    git-svn-id: file:///home/svn/DVA2006@14837 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/beans/system/TestBrowserType.java
+++ src/test/org/deltava/beans/system/TestBrowserType.java
@@ -10,7 +10,8 @@ public class TestBrowserType extends TestCase {
 	public void testBrowserDetection() {
 		
 		assertType(BrowserType.SPIDER, "Mozilla/5.0 (compatible; SemrushBot/6~bl; +http://www.semrush.com/bot.html)");
-		assertType(BrowserType.CHROME, "Mozilla/5.0 (Linux; Android 10; SM-G975U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.83 Mobile Safari/537.36");	
+		assertType(BrowserType.CHROME, "Mozilla/5.0 (Linux; Android 10; SM-G975U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.83 Mobile Safari/537.36");
+		assertType(BrowserType.SPIDER, "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.92 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
 	}
 	
 	public static void assertType(BrowserType bt, String userAgent) {
