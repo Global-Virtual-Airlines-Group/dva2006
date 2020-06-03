fbe0b8abf24dff009c5197f202331c7b26432391
Luke Kolin <luke@deltava.org>
2020-06-03 23:03:32 +0000

    Add enumDescription
    git-svn-id: file:///home/svn/DVA2006@14858 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/schedule/TankType.java
+++ src/java/org/deltava/beans/schedule/TankType.java
@@ -1,22 +1,13 @@
-// Copyright 2006, 2007, 2008, 2010, 2011, 2012, 2016, 2017 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2006, 2007, 2008, 2010, 2011, 2012, 2016, 2017, 2020 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.schedule;
 
 /**
  * An enumeration of aircraft fuel tank types.
  * @author Luke
- * @version 8.7
+ * @version 9.0
  * @since 8.7
  */
 
-public enum TankType {
+public enum TankType implements org.deltava.beans.EnumDescription {
 	PRIMARY, SECONDARY, OTHER;
-	
-	public String getDescription() {
-		return toString();
-	}
-	
-	@Override
-	public String toString() {
-		return name().substring(0, 1) + name().substring(1).toLowerCase();
-	}
 }
