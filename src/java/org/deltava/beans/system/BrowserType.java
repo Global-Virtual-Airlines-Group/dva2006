8fb1aea0b3cafe6f7cb5023ec1c78fe25ad11a8b
Luke Kolin <luke@deltava.org>
2020-07-04 22:15:06 +0000

    Add new spider
    git-svn-id: file:///home/svn/DVA2006@15014 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/BrowserType.java
+++ src/java/org/deltava/beans/system/BrowserType.java
@@ -13,7 +13,8 @@ import java.util.*;
 public enum BrowserType {
 	
 	IE("MSIE", "Trident"), FIREFOX("Firefox"), WEBKIT("WebKit", "Safari"), CHROME("Chrome"), OPERA("Opera"), 
-	SPIDER("Googlebot", "bingbot", "YandexBot", "ezooms.bot", "msnbot", "MJ12bot", "Baiduspider",  "Yahoo! Slurp",  "SemrushBot", "AhrefsBot", "Bytespider", "TurnitinBot", "PetalBot"), UNKNOWN;
+	SPIDER("Googlebot", "bingbot", "YandexBot", "ezooms.bot", "msnbot", "MJ12bot", "Baiduspider",  "Yahoo! Slurp",  "SemrushBot", "AhrefsBot", "Bytespider", "TurnitinBot", "PetalBot", "Barkrowler"),
+	UNKNOWN;
 	
 	private final Collection<String> _detectStrings = new ArrayList<String>(4);
 	
