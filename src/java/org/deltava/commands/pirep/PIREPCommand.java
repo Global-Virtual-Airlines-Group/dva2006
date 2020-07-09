f6458a404f2108d1be633b47dbad558bee7bf570
Luke Kolin <luke@deltava.org>
2020-07-09 12:53:31 +0000

    Record online network change
    git-svn-id: file:///home/svn/DVA2006@15035 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/pirep/PIREPCommand.java
+++ src/java/org/deltava/commands/pirep/PIREPCommand.java
@@ -184,7 +184,10 @@ public class PIREPCommand extends AbstractFormCommand {
 			} catch (Exception e) {
 				net = null;
 			} finally {
-				fr.setNetwork(net);
+				if (fr.getNetwork() != net) {
+					fr.addStatusUpdate(ctx.getUser().getID(), HistoryType.SYSTEM, "Updated online network from " + fr.getNetwork() + " to " + ((net == null) ? "Offline" : net) + " by " + ctx.getUser().getName());
+					fr.setNetwork(net);
+				}
 			}
 			
 			// Get the flight time
@@ -467,7 +470,7 @@ public class PIREPCommand extends AbstractFormCommand {
 			
 			// If we're online and not on an event, list possible event
 			if (ac.getCanDispose() && fr.hasAttribute(FlightReport.ATTR_ONLINE_MASK) && (fr.getDatabaseID(DatabaseID.EVENT) == 0))
-				ctx.setAttribute("possibleEvents", evdao.getPossibleEvents(fr), REQUEST);
+				ctx.setAttribute("possibleEvents", evdao.getPossibleEvents(fr, SystemData.get("airline.code")), REQUEST);
 
 			// Get the Navdata DAO
 			GetNavRoute navdao = new GetNavRoute(con);
