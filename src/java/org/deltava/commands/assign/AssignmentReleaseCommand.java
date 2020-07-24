dff01364f7bd4872cd49dd0f376d0d4a8f1053d3
Luke Kolin <luke@deltava.org>
2020-07-24 14:17:28 +0000

    Log Flight Assignment release
    git-svn-id: file:///home/svn/DVA2006@15118 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/assign/AssignmentReleaseCommand.java
+++ src/java/org/deltava/commands/assign/AssignmentReleaseCommand.java
@@ -1,7 +1,8 @@
-// Copyright 2005, 2006, 2007, 2009, 2010, 2016, 2018 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007, 2009, 2010, 2016, 2018, 2020 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.assign;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import java.sql.Connection;
 
 import org.deltava.beans.assign.*;
@@ -17,7 +18,7 @@ import org.deltava.util.system.SystemData;
 /**
  * A Web Site Command to release a Flight Assignment.
  * @author Luke
- * @version 8.5
+ * @version 9.0
  * @since 1.0
  */
 
@@ -50,11 +51,23 @@ public class AssignmentReleaseCommand extends AbstractCommand {
 			Collection<FlightReport> pireps = frdao.getByAssignment(ctx.getID(), SystemData.get("airline.db"));
 
 			// Delete PIREPs in draft status, and remove the Assignment ID for the others
-			int flightsDeleted = (int) pireps.stream().filter(fr -> (fr.getStatus() == FlightStatus.DRAFT)).count();
+			Collection<FlightReport> remainingFlights = pireps.stream().filter(fr -> (fr.getStatus() != FlightStatus.DRAFT)).collect(Collectors.toList());
 
 			// Save the totals
-			ctx.setAttribute("flightsDeleted", Integer.valueOf(flightsDeleted), REQUEST);
-			ctx.setAttribute("flightsUpdated",Integer.valueOf(pireps.size() - flightsDeleted), REQUEST);
+			ctx.setAttribute("flightsDeleted", Integer.valueOf(pireps.size() - remainingFlights.size()), REQUEST);
+			ctx.setAttribute("flightsUpdated",Integer.valueOf(remainingFlights.size()), REQUEST);
+			
+			// Start transaction
+			ctx.startTX();
+			
+			// Update history for remaining flights
+			if (!remainingFlights.isEmpty()) {
+				SetFlightReport frwdao = new SetFlightReport(con);
+				for (FlightReport fr : remainingFlights) {
+					fr.addStatusUpdate(ctx.getUser().getID(), HistoryType.LIFECYCLE, "Released Flight Assignment");
+					frwdao.write(fr);
+				}
+			}
 
 			// Delete or release the Assignment
 			SetAssignment wdao = new SetAssignment(con);
@@ -65,7 +78,10 @@ public class AssignmentReleaseCommand extends AbstractCommand {
 				wdao.delete(assign);
 				ctx.setAttribute("isDelete", Boolean.TRUE, REQUEST);
 			}
+			
+			ctx.commitTX();
 		} catch (DAOException de) {
+			ctx.rollbackTX();
 			throw new CommandException(de);
 		} finally {
 			ctx.release();
