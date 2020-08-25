2f3557a1b2f01c07c43a4d26dfae1626d0bdf09d
Luke Kolin <luke@deltava.org>
2020-08-25 19:42:37 +0000

    Refactor /proc reader
    git-svn-id: file:///home/svn/DVA2006@15241 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/file/GetProcData.java
+++ src/java/org/deltava/dao/file/GetProcData.java
@@ -1,4 +1,4 @@
-// Copyright 2007, 2008, 2012, 2016, 2017 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2007, 2008, 2012, 2016, 2017, 2020 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao.file;
 
 import java.io.*;
@@ -10,12 +10,13 @@ import org.deltava.util.StringUtils;
 /**
  * A Data Access Object to read from the Linux /proc filesystem.
  * @author Luke
- * @version 8.1
+ * @version 9.1
  * @since 1.0
  */
 
-@SuppressWarnings("static-method")
 public class GetProcData extends DAO {
+	
+	private final File _proc = new File("/proc");
 
 	/**
 	 * Initializes the Data Access Object.
@@ -29,56 +30,54 @@ public class GetProcData extends DAO {
 	 * @return the machine's uptime in milliseconds
 	 * @throws DAOException if an I/O error occurs
 	 */
-	public int getUptime() throws DAOException {
-		try (InputStream is = new FileInputStream("/proc/uptime")) {
-			try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-				String data = br.readLine();
-				if (data == null)
-					throw new IOException("Empty /proc/uptime");
-			
-				// Get the first entry
-				if (data.indexOf(' ') != -1)
-					data = data.substring(0, data.indexOf(' '));
-				try {
-					return (int) Math.floor(Double.parseDouble(data));
-				} catch (NumberFormatException nfe) {
-					throw new IOException("Unparseable uptime - " + data);
-				}
+	public long getUptime() throws DAOException {
+		try (InputStream is = new FileInputStream(new File(_proc, "uptime")); BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+			String data = br.readLine();
+			if (data == null)
+				throw new IOException("Empty /proc/uptime");
+
+			// Get the first entry
+			if (data.indexOf('.') > -1)
+				data = data.substring(0, data.indexOf('.'));
+			if (data.indexOf(' ') > -1)
+				data = data.substring(0, data.indexOf(' '));
+
+			try {
+				return Long.parseLong(data);
+			} catch (NumberFormatException nfe) {
+				throw new IOException("Unparseable uptime - " + data);
 			}
 		} catch (IOException ie) {
 			throw new DAOException(ie);
 		}
 	}
-	
+
 	/**
 	 * Returns the system load average from /proc/loadavg.
 	 * @return a List of doubles, with processes waiting in the last 1, 5 and 15 minutes
 	 * @throws DAOException if an I/O error occurs
 	 */
 	public List<Double> getLoad() throws DAOException {
-		try (InputStream is = new FileInputStream("/proc/loadavg")) {
+		try (InputStream is = new FileInputStream(new File(_proc, "loadavg")); BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
 			List<Double> results = new ArrayList<Double>(3);
-			try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-				String data = br.readLine();
-				if (data == null)
-					throw new IOException("Empty /proc/loadavg");
-			
-				// Split the entries
-				
-				StringTokenizer tkns = new StringTokenizer(data, " ");
-				if (tkns.countTokens() < 3)
-					throw new IOException("Unparseable load average - " + data);
-			
-				// Parse the entries
-				for (int x = 0; x < 3; x++) {
-					try {
-						results.add(Double.valueOf(Double.parseDouble(tkns.nextToken())));
-					} catch (NumberFormatException nfe) {
-						results.add(Double.valueOf(0));
-					}
+			String data = br.readLine();
+			if (data == null)
+				throw new IOException("Empty /proc/loadavg");
+
+			// Split the entries
+			StringTokenizer tkns = new StringTokenizer(data, " ");
+			if (tkns.countTokens() < 3)
+				throw new IOException("Unparseable load average - " + data);
+
+			// Parse the entries
+			for (int x = 0; x < 3; x++) {
+				try {
+					results.add(Double.valueOf(Double.parseDouble(tkns.nextToken())));
+				} catch (NumberFormatException nfe) {
+					results.add(Double.valueOf(0));
 				}
 			}
-			
+
 			return results;
 		} catch (IOException ie) {
 			throw new DAOException(ie);
@@ -91,23 +90,21 @@ public class GetProcData extends DAO {
 	 * @throws DAOException if an I/O error occurs
 	 */
 	public Map<String, Integer> getMemory() throws DAOException {
-		try (InputStream is = new FileInputStream("/proc/meminfo")) {
+		try (InputStream is = new FileInputStream(new File(_proc, "meminfo")); BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
 			Map<String, Integer> results = new LinkedHashMap<String, Integer>();
-			try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-				String data = br.readLine();
-				while (data != null) {
-					int pos = data.indexOf(':');
-					if ((pos != -1) && data.endsWith(" kB")) {
-						String key = data.substring(0, pos);
-						String rawValue = data.substring(pos + 1, data.lastIndexOf(' '));
-						int value = StringUtils.parse(rawValue.substring(rawValue.lastIndexOf(' ') + 1), 0);
-						results.put(key, Integer.valueOf(value));
-					}
-				
-					data = br.readLine();
+			String data = br.readLine();
+			while (data != null) {
+				int pos = data.indexOf(':');
+				if ((pos != -1) && data.endsWith(" kB")) {
+					String key = data.substring(0, pos);
+					String rawValue = data.substring(pos + 1, data.lastIndexOf(' '));
+					int value = StringUtils.parse(rawValue.substring(rawValue.lastIndexOf(' ') + 1), 0);
+					results.put(key, Integer.valueOf(value));
 				}
+
+				data = br.readLine();
 			}
-			
+
 			return results;
 		} catch (IOException ie) {
 			throw new DAOException(ie);
