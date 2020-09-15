f9ab4a8bcd9dc3cf16cadc62db4c8202ecbf9c5c
Luke Kolin <luke@deltava.org>
2020-09-15 13:31:16 +0000

    Optimize geoUnits
    git-svn-id: file:///home/svn/DVA2006@15325 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/stats/AccomplishUnit.java
+++ src/java/org/deltava/beans/stats/AccomplishUnit.java
@@ -1,4 +1,4 @@
-// Copyright 2010, 2011, 2012, 2014, 2015, 2016 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2010, 2011, 2012, 2014, 2015, 2016, 2020 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.stats;
 
 import org.deltava.beans.ComboAlias;
@@ -6,21 +6,17 @@ import org.deltava.beans.ComboAlias;
 /**
  * An enumeration to track Accomplishment units of measurement.
  * @author Luke
- * @version 7.2
+ * @version 9.1
  * @since 6.3
  */
 
 public enum AccomplishUnit implements ComboAlias {
 	
-	LEGS("Flight Legs"), MILES("Flight Miles"), OLEGS("Online Legs"), VLEGS("VATSIM Legs"),
-	ILEGS("IVAO Legs"), HLEGS("Historic Legs"), EVENTS("Events"), DLEGS("Dispatch Legs"),
-	ALEGS("ACARS Legs"), AIRPORTS("Airports Visited"), AIRCRAFT("Aircraft Used"), 
-	COUNTRIES("Countries Visited"), STATES("States Visited"), 
-	MEMBERDAYS("Days since joining", Data.NONE), AIRLINES("Airlines"), 
-	DFLIGHTS("Flights Dispatched", Data.DISPATCH), DHOURS("Dispatch Hours", Data.DISPATCH),
-	EQLEGS("Legs in Aircraft"), CONTINENTS("Continents Visited"), PAX("Passengers Carried"),
-	AIRPORTD("Departure Airport"), AIRPORTA("Arrival Airport"), PROMOLEGS("Promotion Legs"),
-	ADLEGS("Departures from Airport"), AALEGS("Arrivals at Airport");
+	LEGS("Flight Legs"), MILES("Flight Miles"), OLEGS("Online Legs"), VLEGS("VATSIM Legs"), ILEGS("IVAO Legs"), HLEGS("Historic Legs"), EVENTS("Events"), 
+	DLEGS("Dispatch Legs"), ALEGS("ACARS Legs"), AIRPORTS("Airports Visited", true), AIRCRAFT("Aircraft Used"), COUNTRIES("Countries Visited", true), 
+	STATES("States Visited", true), MEMBERDAYS("Days since joining", Data.NONE, false), AIRLINES("Airlines"), 	DFLIGHTS("Flights Dispatched", Data.DISPATCH, false), 
+	DHOURS("Dispatch Hours", Data.DISPATCH, false), EQLEGS("Legs in Aircraft"), CONTINENTS("Continents Visited", true), PAX("Passengers Carried"),
+	AIRPORTD("Departure Airport", true), AIRPORTA("Arrival Airport", true), PROMOLEGS("Promotion Legs"), ADLEGS("Departures from Airport", true), AALEGS("Arrivals at Airport", true);
 
 	public enum Data {
 		NONE, DISPATCH, FLIGHTS
@@ -28,23 +24,35 @@ public enum AccomplishUnit implements ComboAlias {
 	
 	private final String _name;
 	private final Data _data;
+	private final boolean _isGeo;
 	
 	/**
 	 * Creates the Accomplishment Unit with Flights as the type of data.
 	 * @param name the Unit name
 	 */
 	AccomplishUnit(String name) {
-		this(name, Data.FLIGHTS);
+		this(name, Data.FLIGHTS, false);
+	}
+	
+	/**
+	 * Creates the Accomplishment Unit with Flights as the type of data.
+	 * @param name the Unit name
+	 * @param isGeo TRUE if a geolocation is utilized, otherwise FALSE
+	 */
+	AccomplishUnit(String name, boolean isGeo) {
+		this(name, Data.FLIGHTS, isGeo);
 	}
 	
 	/**
 	 * Creates the Accomplishment Unit.
 	 * @param name the Unit name
 	 * @param d the type of data to track
+	 * @param isGeo TRUE if a geolocation is utilized, otherwise FALSE
 	 */
-	AccomplishUnit(String name, Data d) {
+	AccomplishUnit(String name, Data d, boolean isGeo) {
 		_name = name;
 		_data = d;
+		_isGeo= isGeo;
 	}
 	
 	/**
@@ -61,6 +69,14 @@ public enum AccomplishUnit implements ComboAlias {
 	 */
 	public Data getDataRequired() {
 		return _data;
+	}
+	
+	/**
+	 * Returns whether this Unit tracks geographic locations.
+	 * @return TRUE if a geolocation is utilized, otherwise FALSE
+	 */
+	public boolean isGeo() {
+		return _isGeo;
 	}
 	
 	@Override
