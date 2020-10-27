cc4a1a56418edeb7927a50100222d59cb9bbc1bf
Luke Kolin <luke@deltava.org>
2020-10-27 13:32:12 +0000

    MakeRwys support
    git-svn-id: file:///home/svn/DVA2006@15457 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/SimGateLoader.java
+++ src/test/org/deltava/SimGateLoader.java
@@ -1,4 +1,3 @@
-// Copyright 2009, 2012 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava;
 
 import java.io.*;
@@ -10,37 +9,16 @@ import org.apache.log4j.Logger;
 import org.deltava.beans.Simulator;
 import org.deltava.beans.navdata.*;
 
-import org.jdom2.*;
-import org.jdom2.filter.ElementFilter;
+import org.deltava.util.*;
 
 public class SimGateLoader extends SceneryLoaderTestCase {
 
-	private static final String JDBC_URL = "jdbc:mysql://polaris/common";
+	private static final String JDBC_URL = "jdbc:mysql://sirius.sce.net/common";
 	private Connection _c;
 
-	private static final String SCENERY_ROOT = "D:\\Program Files\\FSX\\Addon Scenery\\VHHX\\scenery";
-	private static final String XML_PATH = "E:\\temp\\bgxml_vhhx-x";
+	private static final String XML_PATH = "E:\\temp\\rwy";
 
-	private static final String BGLXML = "data/bglxml/bglxml.exe";
-	private static final Simulator SIM = Simulator.FSX;
-
-	final class SceneryFilter implements FileFilter {
-		private final String _prefix;
-
-		SceneryFilter() {
-			this("");
-		}
-
-		SceneryFilter(String prefix) {
-			_prefix = prefix;
-		}
-
-		@Override
-		public boolean accept(File f) {
-			String fn = f.getName().toLowerCase();
-			return (f.isFile() && fn.startsWith(_prefix) && fn.endsWith(".bgl"));
-		}
-	}
+	private static final Simulator SIM = Simulator.FS2020;
 
 	@Override
 	protected void setUp() throws Exception {
@@ -54,6 +32,7 @@ public class SimGateLoader extends SceneryLoaderTestCase {
 
 		// Connect to the database
 		Class.forName("com.mysql.cj.jdbc.Driver");
+		DriverManager.setLoginTimeout(3);
 		_c = DriverManager.getConnection(JDBC_URL, "luke", "test");
 		assertNotNull(_c);
 		_c.setAutoCommit(false);
@@ -65,64 +44,10 @@ public class SimGateLoader extends SceneryLoaderTestCase {
 		super.tearDown();
 	}
 
-	public void testConvertBGLs() throws Exception {
-		// assertFalse(true);
-
-		// Check that we're running Windows and the file exists
-		assertTrue(System.getProperty("os.name").contains("Windows"));
-		File exe = new File(BGLXML);
-		assertTrue(exe.exists() && exe.isFile());
-
-		File rt = new File(SCENERY_ROOT);
-		assertTrue(rt.isDirectory());
-
-		Collection<File> bglFiles = getSingleFiles(rt, new SceneryFilter());
-		assertNotNull(bglFiles);
-
-		// Process the BGLs
-		for (Iterator<File> i = bglFiles.iterator(); i.hasNext();) {
-			File bgl = i.next();
-			String fRoot = bgl.getName().substring(0, bgl.getName().lastIndexOf('.'));
-			File xml = new File(XML_PATH, fRoot + ".xml");
-
-			// Covert the BGL
-			if (!xml.exists()) {
-				log.info("Converting " + bgl.getCanonicalPath() + " to XML");
-				ProcessBuilder pb = new ProcessBuilder(exe.getAbsolutePath(), "-t", bgl.getPath(), xml.getPath());
-				pb.redirectErrorStream(true);
-				Process p = pb.start();
-				int result = p.waitFor();
-				if (result != 0) {
-					try (InputStream is = new BufferedInputStream(p.getInputStream(), 512)) {
-						try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-							while (br.ready())
-								log.info(br.readLine());
-						}
-					}
-
-					fail("Cannot convert to XML");
-				}
-
-				// Load the XML
-				Document doc = null;
-				if (xml.exists()) {
-					try {
-						filterAmpersands(xml);
-						doc = loadXML(new FileReader(xml));
-					} catch (Exception e) {
-						log.error(e.getMessage(), e);
-					}
-				}
-
-				assertTrue(!xml.exists() || (doc != null));
-			}
-		}
-	}
-
 	public void testLoadXML() throws Exception {
 		// assertTrue(false);
 
-		File rt = new File(XML_PATH);
+		File rt = new File(XML_PATH, SIM.name());
 		assertTrue(rt.isDirectory());
 
 		// Load ICAO codes
@@ -135,72 +60,79 @@ public class SimGateLoader extends SceneryLoaderTestCase {
 					codes.add(rs.getString(1));
 			}
 		}
+		
+		codes.clear();
+		codes.add("KATL");
 
 		// Clear the table
-		/*
-		 * try (PreparedStatement ps = _c.prepareStatement("DELETE FROM common.GATES WHERE (SIMVERSION=?)")) { ps.setInt(1, SIM.getCode());
-		 * ps.executeUpdate(); }
-		 */
+		/* try (PreparedStatement ps = _c.prepareStatement("DELETE FROM common.GATES WHERE (SIMVERSION=?)")) { 
+			ps.setInt(1, SIM.getCode());
+			ps.executeUpdate();
+		} */
+		
+		// Open the file
+		File f = new File(rt, "g5.csv");
+		assertTrue(f.isFile());
 
 		// Init the prepared statement
-		try (PreparedStatement ps = _c.prepareStatement("REPLACE INTO common.GATES (ICAO, NAME, SIMVERSION, LATITUDE, LONGITUDE, HDG) VALUES (?, ?, ?, ?, ?, ?)")) {
+		try (PreparedStatement ps = _c.prepareStatement("INSERT INTO common.GATES (ICAO, NAME, SIMVERSION, LATITUDE, LONGITUDE, HDG) VALUES (?, ?, ?, ?, ?, ?) AS G ON DUPLICATE KEY UPDATE "
+			+ "LATITUDE=G.LATITUDE, LONGITUDE=G.LONGITUDE, HDG=G.HDG")) {
 			ps.setInt(3, SIM.getCode());
 
-			// Load the XML files
-			File[] xmls = rt.listFiles(new XMLFilter());
-			assertNotNull(xmls);
-			for (int x = 0; x < xmls.length; x++) {
-				log.info("Loading " + xmls[x]);
-				Document doc = loadXML(new FileReader(xmls[x]));
-				assertNotNull(doc);
-
-				// Get the airports
-				Iterator<Element> ali = doc.getDescendants(new ElementFilter("Airport"));
-				while (ali.hasNext()) {
-					Element ae = ali.next();
-					String apCode = ae.getAttributeValue("ident").toUpperCase();
-					if (!codes.contains(apCode))
+			// Load the CSV
+			String lastAP = null; boolean hasData = false;
+			try (InputStream is = new FileInputStream(f); LineNumberReader lr = new LineNumberReader(new InputStreamReader(is), 262144)) {
+				String data = lr.readLine();
+				while (data != null) {
+					CSVTokens tkns = StringUtils.parseCSV(data);
+					String apCode = tkns.get(0).toUpperCase();
+					if (!codes.contains(apCode)) {
+						data = lr.readLine();
 						continue;
+					}
 
-					log.info("Processing " + apCode + " from " + xmls[x]);
-					ps.setString(1, apCode);
-
-					Map<String, Gate> gates = new HashMap<String, Gate>();
-					Iterator<Element> rli = ae.getDescendants(new ElementFilter("TaxiwayParking"));
-					while (rli.hasNext()) {
-						Element re = rli.next();
-						double lat = Double.parseDouble(re.getAttributeValue("lat"));
-						double lng = Double.parseDouble(re.getAttributeValue("lon"));
-						float hdg = Float.parseFloat(re.getAttributeValue("heading"));
-
-						// Get the gate code
-						String gateCode = re.getAttributeValue("name", "GATE") + " " + re.getAttributeValue("number");
-						gateCode = gateCode.replace('_', ' ');
-						while (gateCode.indexOf("  ") > -1)
-							gateCode = gateCode.replace("  ", " ");
-
-						Gate g = new Gate(lat, lng);
+					if (!apCode.equals(lastAP)) {
+						if (hasData) ps.executeBatch();
+						log.info("Processing " + apCode + " at Line " + lr.getLineNumber());
+						ps.setString(1, apCode);
+						lastAP = apCode;
+						hasData = false;
+					}
+					
+					String gateName = tkns.get(1);
+					if ("Park".equalsIgnoreCase(gateName))
+						gateName = "PARKING";
+					else if (gateName.startsWith("Pk"))
+						gateName = gateName.substring(2) + " PARKING";
+					else
+						gateName = "GATE " + gateName;
+					
+					try {
+						String gateNumber = tkns.get(2);
+						Gate g = new Gate(Double.parseDouble(tkns.get(3)), Double.parseDouble(tkns.get(4)));
 						g.setCode(apCode);
-						g.setName(gateCode);
-						g.setHeading(Math.round(hdg));
-						gates.put(gateCode, g);
+						g.setSimulator(SIM);
+						g.setName(gateName + " " + gateNumber);
+						g.setHeading(Math.round(Float.parseFloat(tkns.get(6))));
+						
+						// Save the entry
+						ps.setString(2, g.getName());
+						ps.setDouble(4, g.getLatitude());
+						ps.setDouble(5, g.getLongitude());
+						ps.setInt(6, g.getHeading());
+						ps.addBatch();
+						hasData = true;
+					} catch (Exception e) {
+						log.warn(e.getMessage() + " at Line " + lr.getLineNumber());
 					}
-
-					// Save the entries
-					if (!gates.isEmpty()) {
-						for (Gate g : gates.values()) {
-							ps.setString(2, g.getName());
-							ps.setDouble(4, g.getLatitude());
-							ps.setDouble(5, g.getLongitude());
-							ps.setInt(6, g.getHeading());
-							ps.addBatch();
-						}
-
-						ps.executeBatch();
-						_c.commit();
-					}
+						
+					data = lr.readLine();
 				}
 			}
+			
+			if (hasData) ps.executeBatch();
 		}
+		
+		_c.commit();
 	}
 }
