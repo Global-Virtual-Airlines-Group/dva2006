75485b6825dbf2dbe762c8cf900c9d71523f845c
Luke Kolin <luke@deltava.org>
2021-04-14 19:58:41 +0000

    Download Runway/Gate/SID/STAR in single service
    git-svn-id: file:///home/svn/DVA2006@16213 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/service/navdata/XMLClientDataService.java
+++ src/java/org/deltava/service/navdata/XMLClientDataService.java
@@ -5,7 +5,9 @@ import java.io.*;
 import java.util.*;
 import java.util.zip.*;
 import java.time.*;
+import java.sql.Connection;
 import java.nio.file.attribute.FileTime;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static javax.servlet.http.HttpServletResponse.*;
 
@@ -21,16 +23,18 @@ import org.deltava.util.*;
 import org.deltava.util.system.SystemData;
 
 /**
- * A Web Service to display Terminal Route data to ACARS clients.
+ * A Web Service to serve Terminal Route/Gate/Runway data to ACARS clients.
  * @author Luke
  * @version 10.0
- * @since 2.4
+ * @since 10.0
  */
 
-public class XMLTerminalRouteService extends DownloadService {
+public class XMLClientDataService extends DownloadService {
 	
-	private static final Logger log = Logger.getLogger(XMLTerminalRouteService.class);
-	private static final String XML_ZIP = "xmlsidstar.zip";
+	private static final Logger log = Logger.getLogger(XMLClientDataService.class);
+	
+	private static final String XML_ZIP = "xmldata.zip";
+	private static final String XML_LEGACY_ZIP = "xmlsidstar.zip";
 	
 	private Metadata _md;
 
@@ -42,10 +46,14 @@ public class XMLTerminalRouteService extends DownloadService {
 	 */
 	@Override
 	public int execute(ServiceContext ctx) throws ServiceException {
+		
+		// Determine if we are pulling old/new format data
+		boolean isOld = Boolean.valueOf(ctx.getParameter("legacy")).booleanValue();
+		final String ZIP_NAME = isOld ? XML_LEGACY_ZIP : XML_ZIP;
 
 		// Check if the file exists
 		File cacheDir = new File(SystemData.get("schedule.cache"));
-		File f = new File(cacheDir, "sidstar.zip");
+		File f = new File(cacheDir, ZIP_NAME);
 		Duration d = null;
 		if (f.exists()) {
 			Instant fileAge = Instant.ofEpochMilli(f.lastModified());
@@ -61,40 +69,54 @@ public class XMLTerminalRouteService extends DownloadService {
 		
 		// Check the cache
 		if ((d != null) && (d.toHours() < 8) && (f.length() > 10240) && (_md != null)) {
-			ctx.setHeader("Content-disposition", String.format("attachment; filename=%s", XML_ZIP));
+			ctx.setHeader("Content-disposition", String.format("attachment; filename=%s", ZIP_NAME));
 			ctx.setContentType("application/zip");
 			ctx.setHeader("max-age", 1800);
-			ctx.setHeader("X-Airport-Count", _md.getAirportCount());
+			ctx.setHeader(isOld ? "X-Airport-Count" : "X-File-Count", _md.getAirportCount());
 			ctx.setHeader("X-Signature", _md.getHash());
 			ctx.setHeader("X-Signature-Type", _md.getHashType());
 			sendFile(f, ctx.getResponse());
 			return SC_OK;
 		}
 
-		// Get the DAO and the SIDs/STARs
-		Map<String, Collection<TerminalRoute>> routes = new TreeMap<String, Collection<TerminalRoute>>();
+		// Get the data
+		Map<String, String> docs = new ConcurrentHashMap<String, String>();
 		try {
-			GetNavRoute navdao = new GetNavRoute(ctx.getConnection());
+			Connection con = ctx.getConnection();
+			
+			// Load Runways
+			GetNavRoute navdao = new GetNavRoute(con);
+			if (!isOld) {
+				Map<String, Collection<Runway>> rwys = new TreeMap<String, Collection<Runway>>();
+				navdao.getRunways().forEach(r -> CollectionUtils.addMapCollection(rwys, r.getCode(), r, ArrayList::new));
+				rwys.entrySet().parallelStream().map(XMLFormatter::formatRunway).filter(Objects::nonNull).forEach(me -> docs.put(me.getKey(), me.getValue())); rwys.clear();
+			
+				// Load Gates
+				GetGates gdao = new GetGates(con);
+				Map<String, Collection<Gate>> gates = new TreeMap<String, Collection<Gate>>();
+				gdao.getAll().forEach(g -> CollectionUtils.addMapCollection(gates, g.getCode(), g, ArrayList::new));
+				gates.entrySet().parallelStream().map(XMLFormatter::formatGate).filter(Objects::nonNull).forEach(me -> docs.put(me.getKey(), me.getValue())); gates.clear();
+			}
+			
+			// Load Terminal Routes
+			Map<String, Collection<TerminalRoute>> routes = new TreeMap<String, Collection<TerminalRoute>>();
 			navdao.getAll().forEach(tr -> CollectionUtils.addMapCollection(routes, tr.getICAO(), tr));
+			routes.entrySet().parallelStream().map(XMLFormatter::formatTR).filter(Objects::nonNull).forEach(me -> docs.put(me.getKey(), me.getValue())); routes.clear();
 		} catch (DAOException de) {
-			throw error(SC_INTERNAL_SERVER_ERROR, de.getMessage());
+			throw error(SC_INTERNAL_SERVER_ERROR, de.getMessage(), de);
 		} finally {
 			ctx.release();
 		}
 
 		// Convert to XML
-		Map<String, String> docs = new TreeMap<String, String>();
-		routes.entrySet().parallelStream().map(XMLFormatter::formatTR).forEach(me -> docs.put(me.getKey(), me.getValue()));
-		routes.clear();
-		
 		try {
-			int apCount = 0;
+			int apCount = 0; FileTime now = FileTime.from(Instant.now());
 			try (ZipOutputStream zout = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(f), 262144))) {
 				for (Iterator<Map.Entry<String, String>> i = docs.entrySet().iterator(); i.hasNext(); ) {
 					Map.Entry<String, String> me = i.next();
-					ZipEntry ze = new ZipEntry("ss_" + me.getKey() + ".xml");
+					ZipEntry ze = new ZipEntry(me.getKey() + ".xml");
 					ze.setMethod(ZipEntry.DEFLATED);
-					ze.setCreationTime(FileTime.from(Instant.now()));
+					ze.setCreationTime(now);
 					zout.putNextEntry(ze);
 					PrintWriter pw = new PrintWriter(zout);
 					pw.print(me.getValue());
@@ -112,10 +134,10 @@ public class XMLTerminalRouteService extends DownloadService {
 			}
 
 			// Format and write
-			ctx.setHeader("Content-disposition", String.format("attachment; filename=%s", XML_ZIP));
+			ctx.setHeader("Content-disposition", String.format("attachment; filename=%s", ZIP_NAME));
 			ctx.setContentType("application/zip");
 			ctx.setExpiry(1800);
-			ctx.setHeader("X-Airport-Count", _md.getAirportCount());
+			ctx.setHeader(isOld ? "X-Airport-Count" : "X-File-Count", _md.getAirportCount());
 			ctx.setHeader("X-Signature", _md.getHash());
 			ctx.setHeader("X-Signature-Type", _md.getHashType());
 			sendFile(f, ctx.getResponse());
---
+++ src/java/org/deltava/service/navdata/XMLFormatter.java
@@ -0,0 +1,139 @@
+// Copyright 2021 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.service.navdata;
+
+import java.util.*;
+import java.text.*;
+
+import org.jdom2.*;
+import org.deltava.beans.UseCount;
+import org.deltava.beans.navdata.*;
+import org.deltava.beans.schedule.Airport;
+
+import org.deltava.util.*;
+import org.deltava.util.system.SystemData;
+
+/**
+ * A utility class to handle XML translation for the {@link XMLClientDataService}.
+ * @author Luke
+ * @version 10.0
+ * @since 10.0
+ */
+
+class XMLFormatter {
+
+	// static class
+	private XMLFormatter() {
+		super();
+	}
+	
+	/**
+	 * Converts a collection of Gates into an XML document.
+	 * @param me a Map.Entry with a Collection of Gates, keyed by Airport
+	 * @return a Map.Entry with an XML document string, keyed by Airport ICAO code
+	 */
+	static Map.Entry<String, String> formatGate(Map.Entry<String, Collection<Gate>> me) {
+		Airport a = SystemData.getAirport(me.getKey());
+		if ((a == null) || StringUtils.isEmpty(a.getRegion())) return null;
+		final NumberFormat df = new DecimalFormat("#0.000000");
+		Document doc = new Document();
+		Element re = new Element("gates");
+		re.setAttribute("icao", a.getICAO());
+		re.setAttribute("iata", a.getIATA());
+		re.setAttribute("region", a.getRegion());
+		doc.setRootElement(re);
+
+		for (Gate g : me.getValue()) {
+			Element ge = new Element("gate");
+			ge.setAttribute("name", g.getName());
+			ge.setAttribute("sim", g.getSimulator().toString());
+			ge.setAttribute("hdg", String.valueOf(g.getHeading()));
+			ge.setAttribute("lat", df.format(g.getLatitude()));
+			ge.setAttribute("lng", df.format(g.getLongitude()));
+			ge.setAttribute("zone", g.getZone().name());
+			g.getAirlines().forEach(al -> ge.addContent(XMLUtils.createElement("airline", al.getCode(), false)));
+			re.addContent(ge);
+		}
+		
+		return Map.entry("gate_" + a.getICAO().toLowerCase(), XMLUtils.format(doc, "UTF-8"));
+	}
+
+	/**
+	 * Converts a collection of Terminal Rotues into an XML document.
+	 * @param me a Map.Entry with a Collection of TerminalRoutes, keyed by Airport
+	 * @return a Map.Entry with an XML document string, keyed by Airport ICAO code
+	 */
+	static Map.Entry<String, String> formatTR(Map.Entry<String, Collection<TerminalRoute>> me) {
+		Airport a = SystemData.getAirport(me.getKey());
+		if ((a == null) || StringUtils.isEmpty(a.getRegion())) return null;
+		final NumberFormat df = new DecimalFormat("#0.000000");
+		Document doc = new Document();
+		Element re = new Element("routes");
+		re.setAttribute("icao", a.getICAO());
+		re.setAttribute("iata", a.getIATA());
+		re.setAttribute("region", a.getRegion());
+		doc.setRootElement(re);
+		for (TerminalRoute tr : me.getValue()) {
+			Element tre = new Element(tr.getType().name().toLowerCase());
+			tre.setAttribute("name", tr.getName());
+			tre.setAttribute("id", tr.getCode());
+			tre.setAttribute("transition", tr.getTransition());
+			tre.setAttribute("runway", tr.getRunway());
+			re.addContent(tre);			
+
+			// Add the waypoint elements
+			int mrkID = 0;
+			for (NavigationDataBean ai : tr.getWaypoints()) {
+				Element we = new Element("wp");
+				we.setAttribute("code", ai.getCode());
+				we.setAttribute("idx", String.valueOf(++mrkID));
+				we.setAttribute("lat", df.format(ai.getLatitude()));
+				we.setAttribute("lon", df.format(ai.getLongitude()));
+				we.setAttribute("type", ai.getType().getName());
+				if (ai.getRegion() != null)
+					we.setAttribute("region", ai.getRegion());
+				tre.addContent(we);
+			}
+		}
+		
+		return Map.entry("ss_" + a.getICAO().toLowerCase(), XMLUtils.format(doc, "UTF-8"));
+	}
+	
+	/**
+	 * Converts a collection of Runways into an XML document.
+	 * @param me a Map.Entry with a Collection of Runways, keyed by Airport
+	 * @return a Map.Entry with an XML document string, keyed by Airport ICAO code
+	 */
+	static Map.Entry<String, String> formatRunway(Map.Entry<String, Collection<Runway>> me) {
+		Airport a = SystemData.getAirport(me.getKey());
+		if ((a == null) || StringUtils.isEmpty(a.getRegion())) return null;
+		final NumberFormat df = new DecimalFormat("#0.000000");
+		Document doc = new Document();
+		Element re = new Element("runways");
+		re.setAttribute("icao", a.getICAO());
+		re.setAttribute("iata", a.getIATA());
+		re.setAttribute("region", a.getRegion());
+		doc.setRootElement(re);
+		for (Runway r : me.getValue()) {
+			Element rwe = new Element("runway");			
+			rwe.setAttribute("name", r.getName());
+			rwe.setAttribute("lat", df.format(r.getLatitude()));
+			rwe.setAttribute("lng", df.format(r.getLongitude()));			
+			rwe.setAttribute("width", String.valueOf(r.getWidth()));
+			rwe.setAttribute("hdg", String.valueOf(r.getHeading()));
+			rwe.setAttribute("length", String.valueOf(r.getLength()));
+			rwe.setAttribute("sim", r.getSimulator().name());
+			rwe.setAttribute("sfc", r.getSurface().name());
+			rwe.setAttribute("isHardSfc", String.valueOf(r.getSurface().isHard()));
+			rwe.setAttribute("magVar", String.valueOf(r.getMagVar()));
+			XMLUtils.addIfPresent(rwe, XMLUtils.createIfPresent("oldCode", r.getNewCode()));
+			if ((r.getFrequency() != null) && (!"-".equals(r.getFrequency())))
+				rwe.addContent(XMLUtils.createElement("freq", r.getFrequency()));
+			if (r instanceof UseCount)
+				rwe.addContent(XMLUtils.createElement("useCount", String.valueOf(((UseCount) r).getUseCount())));
+				
+			re.addContent(rwe);
+		}
+		
+		return Map.entry("rwy_" + a.getICAO().toLowerCase(), XMLUtils.format(doc, "UTF-8"));		
+	}
+}
