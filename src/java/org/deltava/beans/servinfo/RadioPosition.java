fee96c2e568f6181a1b16d6c070e6c8fa502eb6e
Luke Kolin <luke@deltava.org>
2021-04-21 20:18:36 +0000

    Fix Javadoc
    git-svn-id: file:///home/svn/DVA2006@16269 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/servinfo/RadioPosition.java
+++ src/java/org/deltava/beans/servinfo/RadioPosition.java
@@ -7,7 +7,7 @@ import org.deltava.beans.GeospaceLocation;
 import org.deltava.beans.schedule.GeoPosition;
 
 /**
- * 
+ * A bean to store VATSIM transceiver data. 
  * @author Luke
  * @version 10.0
  * @since 10.0
@@ -27,15 +27,28 @@ public class RadioPosition {
 		_callsign = callSign;
 	}
 	
+	/**
+	 * Returns the user's callsign.
+	 * @return the callsign
+	 */
 	public String getCallsign() {
 		return _callsign;
 	}
 
+	/**
+	 * Returns all of the transceiver locations.
+	 * @return a Collection of GeospaceLocation beans
+	 */
 	public Collection<GeospaceLocation> getPositions() {
 		return _positions;
 	}
-	
+
+	/**
+	 * Returns the average of the transceiver locations.
+	 * @return a GeospaceLocation
+	 */
 	public GeospaceLocation getCenter() {
+		if (_positions.isEmpty()) return null;
 		double lat = 0; double lng = 0; int alt = 0;
 		for (GeospaceLocation loc : _positions) {
 			lat += loc.getLatitude();
@@ -46,7 +59,21 @@ public class RadioPosition {
 		return new GeoPosition(lat / _positions.size(), lng / _positions.size(), alt / _positions.size());
 	}
 	
+	/**
+	 * Adds a transceiver location.
+	 * @param loc the GeospaceLocation
+	 */
 	public void addPosition(GeospaceLocation loc) {
 		_positions.add(loc);
+	}
+	
+	@Override
+	public int hashCode() {
+		return _callsign.hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		return ((o instanceof RadioPosition) && (hashCode() == o.hashCode()));
 	}
 }
