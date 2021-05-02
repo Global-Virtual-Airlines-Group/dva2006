d259bbe1b4bf4cb96039b0a06c3d76cf931ba927
Luke Kolin <luke@deltava.org>
2021-05-02 17:23:52 +0000

    Add amazonbot
    git-svn-id: file:///home/svn/DVA2006@16336 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/BrowserType.java
+++ src/java/org/deltava/beans/system/BrowserType.java
@@ -13,7 +13,7 @@ import java.util.*;
 public enum BrowserType {
 	
 	IE("MSIE", "Trident"), FIREFOX("Firefox"), WEBKIT("WebKit", "Safari"), CHROME("Chrome"), OPERA("Opera"), SPIDER("Googlebot", "bingbot", "YandexBot", "ezooms.bot", "msnbot", "MJ12bot", "Baiduspider",  
-			"Yahoo! Slurp",  "SemrushBot", "AhrefsBot", "Bytespider", "TurnitinBot", "PetalBot", "Barkrowler", "Adsbot", "internetnl", "DotBot", "Applebot", "MauiBot"), UNKNOWN;
+			"Yahoo! Slurp",  "SemrushBot", "AhrefsBot", "Bytespider", "TurnitinBot", "PetalBot", "Barkrowler", "Adsbot", "internetnl", "DotBot", "Applebot", "MauiBot", "amazonbot"), UNKNOWN;
 	
 	private final Collection<String> _detectStrings = new ArrayList<String>(4);
 	
