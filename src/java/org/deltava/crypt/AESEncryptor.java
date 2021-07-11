2a208a641dc31c73b03ba472caa9e7a347612617
Luke Kolin <luke@deltava.org>
2021-07-11 00:25:56 +0000

    Handle IV and AES256
    git-svn-id: file:///home/svn/DVA2006@16528 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/crypt/AESEncryptor.java
+++ src/java/org/deltava/crypt/AESEncryptor.java
@@ -1,3 +1,4 @@
+// Copyright 2016, 2021 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.crypt;
 
 import javax.crypto.spec.SecretKeySpec;
@@ -5,21 +6,36 @@ import javax.crypto.spec.SecretKeySpec;
 /**
  * A class to encrypt/decrypt data using AES.
  * @author Luke
- * @version 1.0
- * @since 1.0
+ * @version 10.1
+ * @since 6.4
  */
 
 public final class AESEncryptor extends SecretKeyEncryptor {
 
     /**
      * Create a new AES encryptor given an arbitrary key.
-     * @param keyData the key data. Only the first 16 bytes are used
+     * @param keyData the key data. Only the first 16 or 32 bytes are used
      * @throws CryptoException if the encryptor cannot be initialized
      */
     public AESEncryptor(byte[] keyData) throws CryptoException {
         super("AES");
         try {
-            setKey(new SecretKeySpec(trimKeySize(keyData, 16), "AES"));
+            setKey(new SecretKeySpec(trimKeySize(keyData, (keyData.length > 32) ? 32 : 16), "AES"), null);
+        } catch (Exception e) {
+            throw new CryptoException("Cannot generate AES Encryptor", e);
+        }
+    }
+    
+    /**
+     * Create a new AES encryptor given an arbitrary key.
+     * @param keyData the key data. Only the first 16 or 32 bytes are used
+     * @param iv the Initialization Vector, or null
+     * @throws CryptoException if the encryptor cannot be initialized
+     */
+    public AESEncryptor(byte[] keyData, byte[] iv) throws CryptoException {
+        super("AES/CBC/PKCS5Padding");
+        try {
+            setKey(new SecretKeySpec(trimKeySize(keyData, (keyData.length > 32) ? 32 : 16), "AES"), iv);
         } catch (Exception e) {
             throw new CryptoException("Cannot generate AES Encryptor", e);
         }
