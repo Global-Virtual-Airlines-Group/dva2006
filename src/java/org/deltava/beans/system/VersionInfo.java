36eb136c3ca23afae1dbb2941f1ba20bcd0d0a55
Luke Kolin <luke@deltava.org>
2021-12-04 14:42:57 +0000

    Build 800
    git-svn-id: file:///home/svn/DVA2006@16989 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -13,7 +13,7 @@ public class VersionInfo {
     public static final int MAJOR = 10;
     public static final int MINOR = 2;
     
-    private static final int BUILD = 799;
+    private static final int BUILD = 800;
     private static final int HOTFIX = 0;
     
     public static final String TXT_COPYRIGHT = "Copyright 2004 - 2021 Global Virtual Airlines Group. All Rights Reserved.";
