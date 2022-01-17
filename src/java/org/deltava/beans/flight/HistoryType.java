c69606d57847cc5c3fe8c051940423e7a7601a62
Luke Kolin <luke@deltava.org>
2022-01-17 13:54:36 +0000

    Add UpdateHistory events from PIREP submission
    git-svn-id: file:///home/svn/DVA2006@17115 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/flight/HistoryType.java
+++ src/java/org/deltava/beans/flight/HistoryType.java
@@ -1,4 +1,4 @@
-// Copyright 2020 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2020, 2022 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.flight;
 
 import org.deltava.beans.*;
@@ -6,11 +6,11 @@ import org.deltava.beans.*;
 /**
  * An enumeration of Flight Report status entry types.
  * @author Luke
- * @version 9.0
+ * @version 10.2
  * @since 9.0
  */
 
 @Helper(FlightHistoryEntry.class)
 public enum HistoryType implements EnumDescription {
-	LIFECYCLE, SYSTEM, UPDATE
+	LIFECYCLE, SYSTEM, UPDATE, USER
 }
