22ba1a3d40696bfadf8036a2c8de1c87348b9b3d
Luke Kolin <luke@deltava.org>
2022-02-13 13:26:33 +0000

    Better bot detection
    git-svn-id: file:///home/svn/DVA2006@17223 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/BrowserType.java
+++ src/java/org/deltava/beans/system/BrowserType.java
@@ -11,8 +11,7 @@ import java.util.*;
  */
 
 public enum BrowserType {
-	IE("MSIE", "Trident"), FIREFOX("Firefox"), WEBKIT("WebKit", "Safari"), CHROME("Chrome"), OPERA("Opera"), SPIDER("Googlebot", "bingbot", "YandexBot", "ezooms.bot", "msnbot", "MJ12bot", "Baiduspider",  
-			"Yahoo! Slurp",  "SemrushBot", "AhrefsBot", "Bytespider", "Turnitin", "PetalBot", "Barkrowler", "Adsbot", "internetnl", "DotBot", "Applebot", "MauiBot", "amazonbot", "lanaibot", "serpstatbot", "DataForSeoBot"), UNKNOWN;
+	IE("MSIE", "Trident"), FIREFOX("Firefox"), WEBKIT("WebKit", "Safari"), CHROME("Chrome"), OPERA("Opera"), SPIDER("Bot", "bot", "Baiduspider", "Yahoo! Slurp", "Bytespider", "Turnitin", "Barkrowler", "internetnl"), UNKNOWN;
 	
 	private final Collection<String> _detectStrings = new ArrayList<String>(4);
 	
