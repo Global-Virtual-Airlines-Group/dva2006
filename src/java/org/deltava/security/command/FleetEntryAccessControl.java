c96d250f3b730708a60b47692f774e825c2dbb94
Luke Kolin <luke@deltava.org>
2022-04-02 12:51:07 +0000

    Update cross-airline permissions
    git-svn-id: file:///home/svn/DVA2006@17330 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/security/command/FleetEntryAccessControl.java
+++ src/java/org/deltava/security/command/FleetEntryAccessControl.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2006, 2007, 2010, 2014, 2016 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007, 2010, 2014, 2016, 2022 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.security.command;
 
 import org.deltava.security.SecurityContext;
@@ -8,94 +8,92 @@ import org.deltava.beans.fleet.LibraryEntry;
 /**
  * An Access Controller to support Fleet/File/Document Library entry operations.
  * @author Luke
- * @version 7.2
+ * @version 10.2
  * @since 1.0
  */
 
 public class FleetEntryAccessControl extends AccessControl {
 
-   protected LibraryEntry _entry;
-   
-   protected boolean _canView;
-   protected boolean _canEdit;
-   protected boolean _canCreate;
-   private boolean _canDelete;
+	protected LibraryEntry _entry;
 
-   /**
-    * Initializes the access controller.
-    * @param ctx the Command context
-    * @param e the LibraryEntry
-    */
-   public FleetEntryAccessControl(SecurityContext ctx, LibraryEntry e) {
-      super(ctx);
-      _entry = e;
-   }
+	protected boolean _canView;
+	protected boolean _canEdit;
+	protected boolean _canCreate;
+	private boolean _canDelete;
 
-   /**
-    * Updates the Fleet Entry to validate access to.
-    * @param e the entry
-    */
-   public void setEntry(LibraryEntry e) {
-      _entry = e;
-   }
+	/**
+	 * Initializes the access controller.
+	 * @param ctx the Command context
+	 * @param e the LibraryEntry
+	 */
+	public FleetEntryAccessControl(SecurityContext ctx, LibraryEntry e) {
+		super(ctx);
+		_entry = e;
+	}
 
-   /**
-    * Calculates access rights.
-    */
-   @Override
-public void validate() {
-      validateContext();
-      _canCreate = _ctx.isUserInRole("Fleet");
-      if (_entry == null) return;
+	/**
+	 * Updates the Fleet Entry to validate access to.
+	 * @param e the entry
+	 */
+	public void setEntry(LibraryEntry e) {
+		_entry = e;
+	}
 
-      // Set access variables
-      _canEdit = _ctx.isUserInRole("Fleet");
-      _canDelete = _ctx.isUserInRole("HR");
-      switch (_entry.getSecurity()) {
-         case PUBLIC:
-            _canView = true;
-            break;
+	@Override
+	public void validate() {
+		validateContext();
+		_canCreate = _ctx.isUserInRole("Fleet");
+		if (_entry == null) return;
 
-         case AUTH:
-            _canView = _ctx.isAuthenticated();
-            break;
+		// Set access variables
+		boolean isOurs = _ctx.isAuthenticated() && _ctx.getUser().getAirlineCode().equals(_entry.getOwner().getCode());
+		_canEdit = isOurs && _ctx.isUserInRole("Fleet");
+		_canDelete = isOurs && _ctx.isUserInRole("HR");
+		switch (_entry.getSecurity()) {
+		case PUBLIC:
+			_canView = true;
+			break;
 
-         default:
-         case STAFF:
-            _canView = (_ctx.getRoles().size() > 1);
-            break;
-      }
-   }
+		case AUTH:
+			_canView = isOurs;
+			break;
 
-   /**
-    * Returns if the Fleet/Document Library entry can be edited.
-    * @return TRUE if the entry can be edited, otherwise FALSE
-    */
-   public boolean getCanEdit() {
-      return _canEdit;
-   }
+		default:
+		case STAFF:
+			_canView = isOurs && (_ctx.getRoles().size() > 1);
+			break;
+		}
+	}
 
-   /**
-    * Returns if the Fleet/Document Library entry can be deleted.
-    * @return TRUE if the entry can be deleted, otherwise FALSE
-    */
-   public boolean getCanDelete() {
-      return _canDelete;
-   }
+	/**
+	 * Returns if the Fleet/Document Library entry can be edited.
+	 * @return TRUE if the entry can be edited, otherwise FALSE
+	 */
+	public boolean getCanEdit() {
+		return _canEdit;
+	}
 
-   /**
-    * Returns if the Fleet/Document Library entry can be viewed.
-    * @return TRUE if the entry can be viewed, otherwise FALSE
-    */
-   public boolean getCanView() {
-      return _canView;
-   }
-   
-   /**
-    * Returns if a new Fleet/Document Library entry can be created.
-    * @return TRUE if a new entry can be created, otherwise FALSE
-    */
-   public boolean getCanCreate() {
-      return _canCreate;
-   }
+	/**
+	 * Returns if the Fleet/Document Library entry can be deleted.
+	 * @return TRUE if the entry can be deleted, otherwise FALSE
+	 */
+	public boolean getCanDelete() {
+		return _canDelete;
+	}
+
+	/**
+	 * Returns if the Fleet/Document Library entry can be viewed.
+	 * @return TRUE if the entry can be viewed, otherwise FALSE
+	 */
+	public boolean getCanView() {
+		return _canView;
+	}
+
+	/**
+	 * Returns if a new Fleet/Document Library entry can be created.
+	 * @return TRUE if a new entry can be created, otherwise FALSE
+	 */
+	public boolean getCanCreate() {
+		return _canCreate;
+	}
 }
