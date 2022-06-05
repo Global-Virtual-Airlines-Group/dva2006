261360ed1ceabdbb11ebf91af31998fcc87348c5
Luke Kolin <luke@deltava.org>
2022-06-05 11:29:54 +0000

    Initial XP12 support
    git-svn-id: file:///home/svn/DVA2006@17428 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/Simulator.java
+++ src/java/org/deltava/beans/Simulator.java
@@ -1,10 +1,10 @@
-// Copyright 2012, 2016, 2017, 2020 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2012, 2016, 2017, 2020, 2022 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans;
 
 /**
  * An enumeration of Simulator versions.
  * @author Luke
- * @version 9.0
+ * @version 10.2
  * @since 5.1
  */
 
@@ -13,7 +13,7 @@ public enum Simulator implements ComboAlias {
 	FS2002(2002, "Microsoft Flight Simulator 2002"),	FS9(2004, "Microsoft Flight Simulator 2004"), 
 	FSX(2006, "Microsoft Flight Simulator X"), P3D(2008, "Lockheed-Martin Prepar3D"), P3Dv4(2017, "Lockheed-Martin Prepar3D/64"),
 	XP9(100, "Laminar Research X-Plane 9"), XP10(101, "Laminar Research X-Plane 10"), XP11(102, "Laminar Research X-Plane 11"),
-	FS2020(2020, "Microsoft Flight Simulator 2020");
+	FS2020(2020, "Microsoft Flight Simulator 2020"), XP12(103, "Laminar Research X-Plane 12");
 
 	private final String _name;
 	private final int _code;
