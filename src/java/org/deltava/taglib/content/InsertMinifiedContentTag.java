01863d3f80b2f7198fd2b13b6d8edaad959867d8
Luke Kolin <luke@deltava.org>
2022-06-21 22:03:48 +0000

    Boolean.valueOf(String).booleanValue() -> Boolean.parseBoolean(String)
    git-svn-id: file:///home/svn/DVA2006@17476 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/content/InsertMinifiedContentTag.java
+++ src/java/org/deltava/taglib/content/InsertMinifiedContentTag.java
@@ -1,13 +1,12 @@
-// Copyright 2012 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2012, 2022 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.content;
 
 import org.deltava.util.system.SystemData;
 
 /**
- * A JSP tag to insert optionally minified content. Content will be minified if the
- * SystemData environment name starts with &quot;prod&quot;.
+ * A JSP tag to insert optionally minified content. Content will be minified if the SystemData environment name starts with &quot;prod&quot;.
  * @author Luke
- * @version 5.0
+ * @version 10.2
  * @since 5.0
  */
 
@@ -32,16 +31,13 @@ abstract class InsertMinifiedContentTag extends InsertContentTag {
 		return _isMin;
 	}
 	
-	/**
-	 * Determines whether content is minified.
-	 */
 	@Override
 	public int doStartTag() {
 		String env = SystemData.get(SystemData.CFG_NAME);
 		_isMin = ((env != null) && env.startsWith("prod"));
 		
 		// Detect minification override
-		boolean minOV = Boolean.valueOf(pageContext.getRequest().getParameter("minOV")).booleanValue();
+		boolean minOV = Boolean.parseBoolean(pageContext.getRequest().getParameter("minOV"));
 		if (minOV) _isMin = !_isMin;
 		
 		// If running in prod, minify - otherwise 
