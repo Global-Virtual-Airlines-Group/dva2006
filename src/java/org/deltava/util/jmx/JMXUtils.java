523b6dcd67c0c5c76bcd2498e989c7ac4adc4324
Luke Kolin <luke@deltava.org>
2022-07-06 15:09:13 +0000

    JMX utilities
    git-svn-id: file:///home/svn/DVA2006@17523 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/util/jmx/JMXUtils.java
@@ -0,0 +1,76 @@
+// Copyright 2022 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.util.jmx;
+
+import java.util.*;
+
+import javax.management.*;
+import java.lang.management.ManagementFactory;
+
+import org.apache.log4j.Logger;
+
+/**
+ * A utility class for registering JMX Objects.
+ * @author Luke
+ * @version 10.2
+ * @since 10.2
+ */
+
+public class JMXUtils {
+	
+	private static final Logger log = Logger.getLogger(JMXUtils.class);
+	private static final Collection<String> _objNames = new HashSet<String>();
+	
+	// static class
+	private JMXUtils() {
+		super();
+	}
+	
+	/**
+	 * Registers a JMX object with the MBean server.
+	 * @param name the JMX object name
+	 * @param o the JMX object
+	 */
+	public static synchronized void register(String name, Object o) {
+		MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+		try {
+			ObjectName jmsName = new ObjectName(name);
+			mbs.registerMBean(o, jmsName);
+			_objNames.add(name);
+			log.info(String.format("Registered JMX bean %s", jmsName.getCanonicalName()));
+		} catch (JMException jme) {
+			log.error(String.format("Error registering %s - %s", name, jme.getMessage()), jme);
+		}
+	}
+	
+	/**
+	 * Removes a JMX object from the the MBean server.
+	 * @param name the JMX object name
+	 */
+	public static synchronized void remove(String name) {
+		MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+		try {
+			ObjectName jmsName = new ObjectName(name);
+			mbs.unregisterMBean(jmsName);
+			_objNames.remove(name);
+			log.info(String.format("Unregistered JMX bean %s", jmsName.getCanonicalName()));
+		} catch (JMException jme) {
+			log.warn(String.format("Error unregistering %s - %s", name, jme.getMessage()), jme);
+		}
+	}
+	
+	/**
+	 * Returns all registered JMX names.
+	 * @return a List of names
+	 */
+	public static Collection<String> getNames() {
+		return new ArrayList<String>(_objNames);
+	}
+
+	/**
+	 * Removes all registered JMX object from the MBean server. 
+	 */
+	public static synchronized void clear() {
+		Collection<String> names = new ArrayList<String>(_objNames); // clone since we modify the collection in the loop
+		names.forEach(JMXUtils::remove);
+	}
+}
