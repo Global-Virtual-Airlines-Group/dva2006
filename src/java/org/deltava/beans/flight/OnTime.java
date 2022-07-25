b76be13ed3a047834de74c31dd943b759906afea
Luke Kolin <luke@deltava.org>
2022-07-25 03:00:48 +0000

    Add EnumDescription
    git-svn-id: file:///home/svn/DVA2006@17542 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/flight/OnTime.java
+++ src/java/org/deltava/beans/flight/OnTime.java
@@ -1,22 +1,27 @@
-// Copyright 2018 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2018, 2022 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.flight;
 
-import org.deltava.beans.ViewEntry;
+import org.deltava.beans.*;
 
 /**
  * An enumeration of on-time values. 
  * @author Luke
- * @version 8.4
+ * @version 10.3
  * @since 8.4
  */
 
-public enum OnTime implements ViewEntry {
+public enum OnTime implements ViewEntry, EnumDescription {
 	UNKNOWN(null), EARLY("sec"), ONTIME("pri"), LATE("warn");
 	
 	private final String _className;
 	
 	OnTime(String className) {
 		_className = className;
+	}
+	
+	@Override
+	public String getDescription() {
+		return (this == ONTIME) ? "On Time" : EnumDescription.super.getDescription();
 	}
 
 	@Override
