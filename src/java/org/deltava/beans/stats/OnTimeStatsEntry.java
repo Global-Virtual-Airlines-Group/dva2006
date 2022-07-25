4f5ffe3ac4b49f46e1f9842d8f33f95c3b890ddd
Luke Kolin <luke@deltava.org>
2022-07-25 06:03:10 +0000

    Add OnTimeStatsEntry
    git-svn-id: file:///home/svn/DVA2006@17544 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/stats/OnTimeStatsEntry.java
@@ -0,0 +1,51 @@
+// Copyright 2022 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.stats;
+
+import java.time.Instant;
+
+import org.deltava.beans.flight.OnTime;
+
+/**
+ * A bean to provide on-time statistics by date.
+ * @author Luke
+ * @version 10.3
+ * @since 10.3
+ */
+
+public class OnTimeStatsEntry extends LegHoursDateStatsEntry<OnTime> {
+
+	/**
+	 * Creates the bean.
+	 * @param dt the date
+	 */
+	public OnTimeStatsEntry(Instant dt) {
+		super(dt);
+	}
+	
+	/**
+	 * Returns the total number of flight legs during this period.
+	 * @return the number of legs
+	 */
+	public int getTotalLegs() {
+		return getKeys().stream().mapToInt(this::getLegs).sum();
+	}
+	
+	/**
+	 * Returns the total number of on-time flight legs during this period.
+	 * @return the number of legs
+	 */
+	public int getOnTimeLegs() {
+		return getKeys().stream().filter(ot -> ((ot == OnTime.ONTIME) || (ot == OnTime.EARLY))).mapToInt(this::getLegs).sum();
+	}
+	
+	/**
+	 * Adds a statistics entry.
+	 * @param ot the OnTime entry
+	 * @param legs the number of legs
+	 * @param hours the number of hours
+	 */
+	@Override
+	public void set(OnTime ot, int legs, double hours) {
+		super.set(ot, legs, hours);
+	}
+}
