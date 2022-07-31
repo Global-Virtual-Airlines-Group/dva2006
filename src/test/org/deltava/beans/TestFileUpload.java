62e684e9f0c5145d11769a1ea517b6bf6966f8b4
Luke Kolin <luke@deltava.org>
2022-07-31 17:17:52 +0000

    Add multi-threaded BZIP2 stream support
    git-svn-id: file:///home/svn/DVA2006@17570 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/beans/TestFileUpload.java
+++ src/test/org/deltava/beans/TestFileUpload.java
@@ -7,9 +7,15 @@ import junit.framework.*;
 
 import org.hansel.CoverageDecorator;
 
+import org.apache.commons.compress.compressors.bzip2.*;
+
+import org.deltava.util.BZip2MultiInputStream;
+
 public class TestFileUpload extends TestCase {
 	
 	private FileUpload _fu;
+
+	private static final int MAX_LINES = 2500;
 	
 	public static Test suite() {
 		return new CoverageDecorator(TestFileUpload.class, new Class[] { FileUpload.class } );
@@ -47,20 +53,61 @@ public class TestFileUpload extends TestCase {
 	@SuppressWarnings("static-method")
 	public void testGZStream() throws IOException {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		try (GZIPOutputStream gzout = new GZIPOutputStream(out)) {
-			try (PrintWriter pw = new PrintWriter(gzout)) {
-				pw.println("Line 1");
-				pw.println("Line 2");
-			}
+		try (GZIPOutputStream gzout = new GZIPOutputStream(out); PrintWriter pw = new PrintWriter(gzout)) {
+			for (int x = 0; x < MAX_LINES; x++)
+				pw.println("Line " + (x+1));	
 		}
 		
 		ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
-		try (GZIPInputStream gzin = new GZIPInputStream(in)) {
-			try (BufferedReader br = new BufferedReader(new InputStreamReader(gzin))) {
-				assertEquals("Line 1", br.readLine());
-				assertEquals("Line 2", br.readLine());
-				assertFalse(br.ready());
+		try (GZIPInputStream gzin = new GZIPInputStream(in); BufferedReader br = new BufferedReader(new InputStreamReader(gzin))) {
+			for (int x = 0; x < MAX_LINES; x++)
+				assertEquals("Line " + (x+1), br.readLine());
+			
+			assertFalse(br.ready());
+		}
+	}
+	
+	@SuppressWarnings("static-method")
+	public void testBZStream() throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try (BZip2CompressorOutputStream bzout = new BZip2CompressorOutputStream(out); PrintWriter pw = new PrintWriter(bzout)) {
+			for (int x = 0; x < MAX_LINES; x++)
+				pw.println("Line " + (x+1));
+		}
+
+		ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+		try (BZip2MultiInputStream bzin = new BZip2MultiInputStream(in); BufferedReader br = new BufferedReader(new InputStreamReader(bzin))) {
+			for (int x = 0; x < MAX_LINES; x++)
+				assertEquals("Line " + (x+1), br.readLine());
+			
+			assertFalse(br.ready());
+		}
+	}
+	
+	@SuppressWarnings("static-method")
+	public void testBZ2MultiStream() throws IOException {
+		File f = new File("data/pssawy.dat.bz2");
+		assertTrue(f.exists());
+		
+		int lineCount = 0;
+		try (BZip2MultiInputStream bzin = new BZip2MultiInputStream(new FileInputStream(f)); BufferedReader br = new BufferedReader(new InputStreamReader(bzin), 131072)) {
+			String data = br.readLine();
+			while (data != null) {
+				lineCount++;
+				data = br.readLine();
 			}
+		}
+		
+		// Multi-stream; regular BZ2 stream will fail
+		assertTrue(lineCount > 0);
+		try (BZip2CompressorInputStream bzin = new BZip2CompressorInputStream(new FileInputStream(f)); BufferedReader br = new BufferedReader(new InputStreamReader(bzin), 131072)) {
+			int lineCount2 = 0; String data = br.readLine();
+			while (data != null) {
+				lineCount2++;
+				data = br.readLine();
+			}
+				
+			assertTrue(lineCount > lineCount2);
 		}
 	}
 }
