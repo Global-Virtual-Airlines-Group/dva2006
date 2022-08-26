b904e42dcba3b0e8950f589314a5b7652058176a
Luke Kolin <luke@deltava.org>
2022-08-26 13:25:33 +0000

    Be more forgiving of bad state
    git-svn-id: file:///home/svn/DVA2006@17628 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- scripts/sec/mb_create.c
+++ scripts/sec/mb_create.c
@@ -2,6 +2,7 @@
 #include <pwd.h>
 #include <stdio.h>
 #include <string.h>
+#include <syslog.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -16,7 +17,9 @@ mkdir(fp, S_IRWXU | S_IRWXG);
 pwd = getpwnam("postfix");
 if (pwd) {
 	if (chown(fp, pwd->pw_uid, pwd->pw_gid) == -1)
-		printf("Error setting %s owner - %s\n", fp, strerror(errno)); 
+		syslog(LOG_ERR, "Error setting %s owner - %s", fp, strerror(errno));
+	else
+		syslog(LOG_INFO, "Set permissions on %s", fp);
 }
 }
 
@@ -27,6 +30,8 @@ if (argc < 3) {
 	return 1;
 }
 
+openlog("mbcreate", LOG_PID | LOG_PERROR, LOG_USER); 
+
 struct stat st = {0};
 if (stat(argv[2], &st) == -1) {
 	printf("%s does not exist\n", argv[2]);
@@ -35,23 +40,26 @@ if (stat(argv[2], &st) == -1) {
 
 char fullpath[256];
 snprintf(fullpath, sizeof(fullpath), "%s/%s", argv[2], argv[1]);
-if (stat(fullpath, &st) != -1) {
-	printf("%s already exists!\n", fullpath);
-	return 3;
-}
-
-mkfolder(argv[2], argv[1]);
-mkfolder(fullpath, "cur");
-mkfolder(fullpath, "new");
-mkfolder(fullpath, "tmp");
+if (stat(fullpath, &st) == -1) {
+	mkfolder(argv[2], argv[1]);
+	mkfolder(fullpath, "cur");
+	mkfolder(fullpath, "new");
+	mkfolder(fullpath, "tmp");
+	syslog(LOG_INFO, "Created %s", fullpath);
+} else
+	syslog(LOG_NOTICE, "%s already exists!\n", fullpath);
 
 char spampath[256];
-snprintf(spampath, sizeof(spampath), "%s/%s", fullpath, ".SPAM");
-mkfolder(fullpath, ".SPAM");
-mkfolder(spampath, "cur");
-mkfolder(spampath, "new");
-mkfolder(spampath, "tmp");
+snprintf(spampath, sizeof(spampath), "%s/%s", fullpath, ".!SPAM");
+if (stat(spampath, &st) == -1) {
+	mkfolder(fullpath, ".SPAM");
+	mkfolder(spampath, "cur");
+	mkfolder(spampath, "new");
+	mkfolder(spampath, "tmp");
+	syslog(LOG_INFO, "Created %s", spampath);
+} else
+	syslog(LOG_NOTICE, "%s already exists!\n", spampath);
 
+closelog();
 return 0;
 }
-
