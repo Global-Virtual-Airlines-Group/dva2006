ce31f65fe53df437e0ce96a8e9beb3be96df27b5
Luke Kolin <luke@deltava.org>
2022-08-28 15:17:36 +0000

    Batch load history
    git-svn-id: file:///home/svn/DVA2006@17640 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetFlightReportHistory.java
+++ src/java/org/deltava/dao/GetFlightReportHistory.java
@@ -1,16 +1,18 @@
-// Copyright 2020 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2020, 2022 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao;
 
 import java.sql.*;
 import java.util.*;
 
 import org.deltava.beans.flight.FlightHistoryEntry;
+import org.deltava.beans.flight.FlightReport;
 import org.deltava.beans.flight.HistoryType;
+import org.deltava.util.CollectionUtils;
 
 /**
  * A Data Access Object to load Flight Report status updates from the database.
  * @author Luke
- * @version 9.0
+ * @version 10.3
  * @since 9.0
  */
 
@@ -42,6 +44,31 @@ public class GetFlightReportHistory extends DAO {
 			}
 			
 			return results;
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+	
+	/**
+	 * Loads all status updates for a Pilot's Flight Reports. 
+	 * @param pilotID the Pilot's database ID
+	 * @param pireps a Collection of FlightReports for this Pilot
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public void loadStatus(int pilotID, Collection<FlightReport> pireps) throws DAOException {
+		Map<Integer, FlightReport> pMap = CollectionUtils.createMap(pireps, FlightReport::getID);
+		try (PreparedStatement ps = prepareWithoutLimits("SELECT PSH.* FROM PIREP_STATUS_HISTORY PSH, PIREPS P WHERE (P.ID=PSH.ID) AND (P.PILOT_ID=?)")) {
+			ps.setFetchSize(Math.min(pireps.size() * 4, 1000));
+			ps.setInt(1, pilotID);
+			try (ResultSet rs = ps.executeQuery()) {
+				while (rs.next()) {
+					FlightReport fr = pMap.get(Integer.valueOf(rs.getInt(1)));
+					if (fr != null) {
+						FlightHistoryEntry upd = new FlightHistoryEntry(fr.getID(), HistoryType.values()[rs.getInt(3)], rs.getInt(2), toInstant(rs.getTimestamp(4)), rs.getString(5));
+						fr.addStatusUpdate(upd);
+					}
+				}
+			}
 		} catch (SQLException se) {
 			throw new DAOException(se);
 		}
