5bcdf586922f3f961b7636268c6adad44a58c9f7
Luke Kolin <luke@deltava.org>
2022-09-02 14:02:38 +0000

    Add SimBrief services
    git-svn-id: file:///home/svn/DVA2006@17657 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/service/simbrief/APICodeService.java
@@ -0,0 +1,60 @@
+// Copyright 2022 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.service.simbrief;
+
+import static javax.servlet.http.HttpServletResponse.*;
+
+import java.io.IOException;
+
+import org.deltava.crypt.MessageDigester;
+
+import org.deltava.service.*;
+
+import org.deltava.util.system.SystemData;
+
+/**
+ * A Web Service to generate SimBrief API codes.
+ * @author Luke
+ * @version 10.3
+ * @since 10.3
+ */
+
+public class APICodeService extends WebService {
+
+	/**
+	 * Executes the Web Service.
+	 * @param ctx the Web Service context
+	 * @return the HTTP status code
+	 * @throws ServiceException if an error occurs
+	 */
+	@Override
+	public int execute(ServiceContext ctx) throws ServiceException {
+		
+		// Combine API key and payload
+		StringBuilder keyBuf = new StringBuilder(SystemData.get("security.key.simbrief"));
+		keyBuf.append(ctx.getParameter("api_req"));
+		
+		// Calculate MD5
+		MessageDigester md = new MessageDigester("MD5", 512);
+		String md5 = MessageDigester.convert(md.digest(keyBuf.toString().getBytes()));
+		
+		try {
+			ctx.setContentType("text/javascript", "utf-8");
+			ctx.println("var api_code = '" + md5 + "';");
+			ctx.commit();
+		} catch (IOException ie) {
+			throw error(SC_CONFLICT, "I/O Error", false);
+		}
+		
+		return SC_OK;
+	}
+	
+	@Override
+	public final boolean isLogged() {
+		return false;
+	}
+	
+	@Override
+	public final boolean isSecure() {
+		return true;
+	}
+}
---
+++ src/java/org/deltava/service/simbrief/URLCheckService.java
@@ -0,0 +1,119 @@
+// Copyright 2022 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.service.simbrief;
+
+import static javax.servlet.http.HttpServletResponse.*;
+
+import java.io.*;
+import java.sql.Connection;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.log4j.Logger;
+
+import org.deltava.beans.flight.*;
+
+import org.deltava.dao.*;
+import org.deltava.dao.http.*;
+import org.deltava.dao.file.GetSimBrief;
+
+import org.deltava.service.*;
+
+import org.deltava.util.StringUtils;
+
+/**
+ * A Web Service to check for a generated SimBrief flight plan.
+ * @author Luke
+ * @version 10.3
+ * @since 10.3
+ */
+
+public class URLCheckService extends WebService {
+	
+	private static final Logger log = Logger.getLogger(URLCheckService.class);
+	
+	/**
+	 * Executes the Web Service.
+	 * @param ctx the Web Service context
+	 * @return the HTTP status code
+	 * @throws ServiceException if an error occurs
+	 */
+	@Override
+	public int execute(ServiceContext ctx) throws ServiceException {
+		
+		// Get ID and variable
+		String varName = ctx.getParameter("var");
+		String id = ctx.getParameter("id");
+		if (StringUtils.isEmpty(id))
+			throw error(SC_BAD_REQUEST, "No flight plan ID", false);
+		
+		SimBrief sbdata = null;
+		try {
+			String url = String.format("https://www.simbrief.com/ofp/flightplans/xml/%s.xml", id);
+			GetURL urldao = new GetURL(url, null);
+			byte[] data = urldao.load();
+			
+			// Parse the data
+			GetSimBrief sbdao = new GetSimBrief(new ByteArrayInputStream(data));
+			sbdata = sbdao.parse();
+			sbdata.setSimBriefID(id);
+			sbdata.setXML(new String(data, StandardCharsets.UTF_8));
+			sbdata.setURL(url);
+			log.info("Downloaded " + url + ", " + data.length + " bytes");
+			
+			// Load the Flight Report
+			Connection con = ctx.getConnection();
+			GetFlightReports frdao = new GetFlightReports(con);
+			FlightReport fr = frdao.get(sbdata.getID(), ctx.getDB());
+			if (fr == null)
+				throw error(SC_BAD_REQUEST, "Invalid Flight Report - " + sbdata.getID(), false);
+			
+			// From here, load the draft
+			DraftFlightReport dfr = frdao.getDraft(fr.getID(), ctx.getDB());
+			
+			// Update route
+			dfr.setAttribute(FlightReport.ATTR_SIMBRIEF, true);
+			if (!sbdata.getRoute().equalsIgnoreCase(dfr.getRoute())) {
+				dfr.setRoute(sbdata.getRoute());
+				dfr.addStatusUpdate(ctx.getUser().getID(), HistoryType.UPDATE, "Updated Route via SimBrief");
+			}
+			
+			// Start transaction
+			ctx.startTX();
+			
+			// Persist the data
+			SetFlightReport frwdao = new SetFlightReport(con);
+			frwdao.write(dfr, ctx.getDB());
+			frwdao.writeSimBrief(sbdata);
+			ctx.commitTX();
+		} catch (HTTPDAOException hde) {
+			log.info(String.format("Response %d downloading %s", Integer.valueOf(hde.getStatusCode()), id));
+			// not downloaded yet
+		} catch (DAOException de) {
+			ctx.rollbackTX();
+			throw error(SC_INTERNAL_SERVER_ERROR, de.getMessage(), de);
+		} finally {
+			ctx.release();
+		}
+		
+		try {
+			ctx.setContentType("text/javascript", "utf-8");
+			ctx.println(String.format("var %s = %s;", varName, Boolean.valueOf(sbdata != null)));
+			if (sbdata != null)
+				ctx.println(String.format("var apiFileName = '%s';", sbdata.getURL()));
+			ctx.commit();
+		} catch (IOException ie) {
+			throw error(SC_CONFLICT, "I/O Error", false);
+		}
+		
+		return SC_OK;
+	}
+
+	@Override
+	public final boolean isLogged() {
+		return false;
+	}
+	
+	@Override
+	public final boolean isSecure() {
+		return true;
+	}
+}
