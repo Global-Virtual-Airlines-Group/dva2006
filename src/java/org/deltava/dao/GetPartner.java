6a422d1fe8417a4ad3d94811650d4894b5e0ccd6
Luke Kolin <luke@deltava.org>
2022-10-04 14:26:04 +0000

    Initial ParnterInfo work
    git-svn-id: file:///home/svn/DVA2006@17873 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/dao/GetPartner.java
@@ -0,0 +1,75 @@
+// Copyright 2022 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.dao;
+
+import java.sql.*;
+import java.util.*;
+
+import org.deltava.beans.PartnerInfo;
+
+/**
+ * A Data Access Object to load virtual airline partner information. 
+ * @author Luke
+ * @version 10.3
+ * @since 10.3
+ */
+
+public class GetPartner extends DAO {
+
+	/**
+	 * Initializes the Data Access Object.
+	 * @param c the JDBC connection to use
+	 */
+	public GetPartner(Connection c) {
+		super(c);
+	}
+
+	/**
+	 * Loads a Partner record from the database.
+	 * @param id the database ID
+	 * @return a PartnerInfo bean, or null
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public PartnerInfo get(int id) throws DAOException {
+		try (PreparedStatement ps = prepareWithoutLimits("SELECT P.*, PI.X, PI.Y, PI.EXT FROM PARTNERS P LEFT JOIN PARTNER_IMGS PI ON (P.ID=PI.ID) WHERE (P.ID=?) LIMIT 1")) {
+			ps.setInt(1, id);
+			return execute(ps).stream().findFirst().orElse(null);
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+	
+	/**
+	 * Loads Partner information from the database.
+	 * @return a List of PartnerInfo beans
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public List<PartnerInfo> getPartners() throws DAOException {
+		try (PreparedStatement ps = prepare("SELECT P.*, PI.X, PI.Y, PI.EXT FROM PARTNERS P LEFT JOIN PARTNER_IMGS PI ON (P.ID=PI.ID)")) {
+			return execute(ps);
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+
+	/*
+	 * Helper method to parser Partner result sets.
+	 */
+	private static List<PartnerInfo> execute(PreparedStatement ps) throws SQLException {
+		List<PartnerInfo> results = new ArrayList<PartnerInfo>();
+		try (ResultSet rs = ps.executeQuery()) {
+			while (rs.next()) {
+				PartnerInfo pi = new PartnerInfo(rs.getString(2));
+				pi.setID(rs.getInt(1));
+				pi.setURL(rs.getString(3));
+				pi.setDescription(rs.getString(4));
+				pi.setReferCount(rs.getInt(5));
+				pi.setWidth(rs.getInt(6));
+				pi.setHeight(rs.getInt(7));
+				pi.setBannerExtension(rs.getString(8));
+				results.add(pi);
+			}
+		}
+		
+		return results;
+	}
+}
---
+++ src/java/org/deltava/dao/SetPartner.java
@@ -0,0 +1,79 @@
+// Copyright 2022 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.dao;
+
+import java.sql.*;
+
+import org.deltava.beans.PartnerInfo;
+
+/**
+ * A Data Access Object to write virtual airline Partner information to the database. 
+ * @author Luke
+ * @version 10.3
+ * @since 10.3
+ */
+
+public class SetPartner extends DAO {
+
+	/**
+	 * Initializes the Data Access Object.
+	 * @param c the JDBC connection to use
+	 */
+	public SetPartner(Connection c) {
+		super(c);
+	}
+
+	/**
+	 * Updates a Partner record in the database. 
+	 * @param pi the PartnerInfo bean
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public void write(PartnerInfo pi) throws DAOException {
+		boolean isNew = (pi.getID() == 0);
+		try {
+			startTransaction();
+			try (PreparedStatement ps = prepare("INSERT INTO PARTNERS (ID, NAME, URL, DESCRIPTION, REFERCOUNT) VALUES (?, ?, ?, ?, ?) AS N ON DUPLICATE KEY UPDATE NAME=N.NAME, URL=N.URL, DESCRIPTION=N.DESCRIPTION, REFERCOUNT=N.REFERCOUNT")) {
+				ps.setInt(1, pi.getID());
+				ps.setString(2, pi.getName());
+				ps.setString(3, pi.getURL());
+				ps.setString(4, pi.getDescription());
+				ps.setInt(5, pi.getReferCount());
+				executeUpdate(ps, 1);
+			}
+			
+			// Get the ID
+			if (isNew)
+				pi.setID(getNewID());
+			
+			// Save image
+			if (pi.isLoaded()) {
+				try (PreparedStatement ps = prepareWithoutLimits("REPLACE INTO PARTNER_IMGS (ID, IMG, X, Y, EXT) VALUES (?, ?, ?, ?, ?) LIMIT 1")) {
+					ps.setInt(1, pi.getID());
+					ps.setBlob(2, pi.getInputStream());
+					ps.setInt(3, pi.getWidth());
+					ps.setInt(4, pi.getHeight());
+					ps.setString(5, pi.getTypeName());
+					executeUpdate(ps, 1);
+				}
+			}
+			
+			commitTransaction();
+		} catch (SQLException se) {
+			rollbackTransaction();
+			throw new DAOException(se);
+		}
+	}
+	
+	/**
+	 * Increments the refer count for a particular Partner. 
+	 * @param id the Partner database ID
+	 * @throws DAOException if a JDBC error occurs
+	 */
+	public void refer(int id) throws DAOException {
+		try (PreparedStatement ps = prepareWithoutLimits("UPDATE PARTNERS SET REFERCOUNT=REFERCOUNT+1 WHERE (ID=?) LIMIT 1")) {
+			ps.setInt(1, id);
+			executeUpdate(ps, 0);
+		} catch (SQLException se) {
+			throw new DAOException(se);
+		}
+	}
+}
