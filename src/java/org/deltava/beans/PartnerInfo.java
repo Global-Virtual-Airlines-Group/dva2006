d69fed20cad7f4389213fecc9aae753d516043ee
Luke Kolin <luke@deltava.org>
2022-10-04 14:26:04 +0000

    Initial ParnterInfo work
    git-svn-id: file:///home/svn/DVA2006@17873 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/PartnerInfo.java
@@ -0,0 +1,111 @@
+// Copyright 2022 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans;
+
+import org.deltava.util.StringUtils;
+
+/**
+ * A bean to store virtual airline partner information.
+ * @author Luke
+ * @version 10.3
+ * @since 10.3
+ */
+
+public class PartnerInfo extends ImageBean {
+
+	private String _name;
+	private String _url;
+	
+	private String _desc;
+	private int _referCount;
+	
+	private String _bannerExt;
+	
+	/**
+	 * Creates the bean.
+	 * @param name the partner name 
+	 */
+	public PartnerInfo(String name) {
+		super();
+		setName(name);
+	}
+
+	/**
+	 * Returns the partner name.
+	 * @return the name
+	 */
+	public String getName() {
+		return _name;
+	}
+	
+	/**
+	 * Returns the partner URL.
+	 * @return the URL
+	 */
+	public String getURL() {
+		return _url;
+	}
+
+	/**
+	 * Returns the partner description text.
+	 * @return the description
+	 */
+	public String getDescription() {
+		return _desc;
+	}
+	
+	/**
+	 * Returns the partner referral count.
+	 * @return the count
+	 */
+	public int getReferCount() {
+		return _referCount;
+	}
+	
+	/**
+	 * Queries if the Partner has a banner image.
+	 * @return TRUE if the partner has an image, otherwise FALSE
+	 */
+	public boolean getHasBanner() {
+		return (_bannerExt != null);
+	}
+	
+	/**
+	 * Updates the partner name.
+	 * @param name the name
+	 */
+	public void setName(String name) {
+		_name = name.trim();
+	}
+	
+	/**
+	 * Updates the partner URL.
+	 * @param url the URL
+	 */
+	public void setURL(String url) {
+		_url = url;
+	}
+	
+	/**
+	 * Updates the partner description text.
+	 * @param desc the description
+	 */
+	public void setDescription(String desc) {
+		_desc = desc;
+	}
+	
+	/**
+	 * Updates the partner referral count.
+	 * @param cnt the referral count
+	 */
+	public void setReferCount(int cnt) {
+		_referCount = Math.max(0, cnt);
+	}
+	
+	/**
+	 * Sets if this Partner has a banner image available.
+	 * @param ext the banner extension, or null
+	 */
+	public void setBannerExtension(String ext) {
+		_bannerExt = StringUtils.isEmpty(ext) ? null : ext.toLowerCase();
+	}
+}
--- src/java/org/deltava/commands/main/PartnerListCommand.java
+++ src/java/org/deltava/commands/main/PartnerListCommand.java
@@ -1,8 +1,10 @@
 // Copyright 2022 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.main;
 
+import org.deltava.beans.PartnerInfo;
+
 import org.deltava.commands.*;
-import org.deltava.util.system.SystemData;
+import org.deltava.dao.*;
 
 /**
  * A Web Site Command to display partner organization information.
@@ -11,7 +13,7 @@ import org.deltava.util.system.SystemData;
  * @since 10.3
  */
 
-public class PartnerInfoCommand extends AbstractCommand {
+public class PartnerListCommand extends AbstractViewCommand {
 
     /**
      * Executes the command.
@@ -20,10 +22,21 @@ public class PartnerInfoCommand extends AbstractCommand {
      */
 	@Override
 	public void execute(CommandContext ctx) throws CommandException {
+		ViewContext<PartnerInfo> vc = initView(ctx, PartnerInfo.class);
+		try {
+			GetPartner dao = new GetPartner(ctx.getConnection());
+			dao.setQueryStart(vc.getStart());
+			dao.setQueryMax(vc.getCount());
+			vc.setResults(dao.getPartners());
+		} catch (DAOException de) {
+			throw new CommandException(de);
+		} finally {
+			ctx.release();
+		}
 
 		// Foward to the JSP
 		CommandResult result = ctx.getResult();
-		result.setURL(String.format("/jsp/main/%sPartners.jsp", SystemData.get("airline.code").toLowerCase()));
+		result.setURL("/jsp/main/partnerList.jsp");
 		result.setSuccess(true);
 	}
 }
