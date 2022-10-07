6e92e48e852e72533c429c18432991a62099f88e
Luke Kolin <luke@deltava.org>
2022-10-07 11:34:48 +0000

    Calculate historic gate usage better
    git-svn-id: file:///home/svn/DVA2006@17891 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/stats/GateUsage.java
+++ src/java/org/deltava/beans/stats/GateUsage.java
@@ -1,16 +1,17 @@
-// Copyright 2021 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2021, 2022 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.stats;
 
 import java.util.*;
 
 import org.deltava.beans.schedule.*;
 
+import org.deltava.util.Tuple;
 import org.deltava.util.cache.Cacheable;
 
 /**
  * A bean to store gate usage statistics.
  * @author Luke
- * @version 10.2
+ * @version 10.3
  * @since 10.0
  */
 
@@ -19,19 +20,22 @@ public class GateUsage implements Cacheable, RoutePair {
 	private final Airport _aD;
 	private final Airport _aA;
 	private final boolean _isDeparture;
+	private final int _dayRange;
 	
-	private final Map<String, Integer> _usage = new HashMap<String, Integer>();
+	private final Map<String, Tuple<Integer, Integer>> _usage = new HashMap<String, Tuple<Integer, Integer>>();
 
 	/**
 	 * Creates the bean.
-	 *  @param rp
-	 *  @param isDeparture
+	 *  @param rp the RoutePair
+	 *  @param isDeparture TRUE if departure gates, otherwise FALSE
+	 *  @param daysBack the number of days usage depicted, or zero for all
 	 */
-	public GateUsage(RoutePair rp, boolean isDeparture) {
+	public GateUsage(RoutePair rp, boolean isDeparture, int daysBack) {
 		super();
 		_aD = rp.getAirportD();
 		_aA = rp.getAirportA();
 		_isDeparture = isDeparture;
+		_dayRange = daysBack;
 	}
 
 	@Override
@@ -45,6 +49,30 @@ public class GateUsage implements Cacheable, RoutePair {
 	}
 	
 	/**
+	 * Returns the number of Gates with usage data.
+	 * @return the number of Gates
+	 */
+	public int size() {
+		return _usage.size();
+	}
+	
+	/**
+	 * Returns the number of Gates with recent usage data.
+	 * @return the number of Gates
+	 */
+	public int getRecentSize() {
+		return _usage.values().stream().filter(t -> t.getRight().intValue() > 0).mapToInt(e -> 1).sum(); // count that returns an int instead of a long
+	}
+	
+	/**
+	 * Returns the number of days usage depicted.
+	 * @return the number of days, or zero for all
+	 */
+	public int getDayRange() {
+		return _dayRange;
+	}
+	
+	/**
 	 * Returns whether these are departure gate statistics.
 	 * @return TRUE if departure statistics, otherwise FALSE
 	 */
@@ -55,10 +83,12 @@ public class GateUsage implements Cacheable, RoutePair {
 	/**
 	 * Adds usage count for a particular Gate.
 	 * @param gateName the Gate name
-	 * @param usage the usage count
+	 * @param totalUsage the total usage count
+	 * @param recentUsage the recent usage count
 	 */
-	public void addGate(String gateName, int usage) {
-		_usage.put(gateName, Integer.valueOf(usage));
+	public void addGate(String gateName, int totalUsage, int recentUsage) {
+		if (totalUsage > 0)
+			_usage.put(gateName, Tuple.create(Integer.valueOf(totalUsage), Integer.valueOf(recentUsage)));
 	}
 	
 	/**
@@ -66,8 +96,19 @@ public class GateUsage implements Cacheable, RoutePair {
 	 * @param gateName the Gate name
 	 * @return the usage count, or zero if unknown
 	 */
-	public int getUsage(String gateName) {
-		return _usage.getOrDefault(gateName, Integer.valueOf(0)).intValue();
+	public int getTotalUsage(String gateName) {
+		Tuple<Integer, Integer> t = _usage.getOrDefault(gateName, Tuple.create(Integer.valueOf(0), Integer.valueOf(0)));
+		return t.getLeft().intValue();
+	}
+	
+	/**
+	 * Returns the usage count for a particular Gate.
+	 * @param gateName the Gate name
+	 * @return the usage count, or zero if unknown
+	 */
+	public int getRecentUsage(String gateName) {
+		Tuple<Integer, Integer> t = _usage.getOrDefault(gateName, Tuple.create(Integer.valueOf(0), Integer.valueOf(0)));
+		return t.getRight().intValue();
 	}
 	
 	/**
@@ -75,9 +116,17 @@ public class GateUsage implements Cacheable, RoutePair {
 	 * @return the total usage count
 	 */
 	public int getTotal() {
-		return _usage.values().stream().mapToInt(Integer::intValue).sum();
+		return _usage.values().stream().mapToInt(t -> t.getLeft().intValue()).sum();
 	}
-
+	
+	/**
+	 * Returns the total recent usage across all gates.
+	 * @return the total recent usage count
+	 */
+	public int getTotalRecent() {
+		return _usage.values().stream().mapToInt(t -> t.getRight().intValue()).sum();
+	}
+	
 	@Override
 	public Object cacheKey() {
 		return toString();
