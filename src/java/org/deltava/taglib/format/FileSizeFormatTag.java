61d2c5fef6eebea1131599f7a56b5c8443424922
Luke Kolin <luke@deltava.org>
2022-10-10 16:23:39 +0000

    Add FileSizeFormatTag
    git-svn-id: file:///home/svn/DVA2006@17918 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/taglib/format/FileSizeFormatTag.java
@@ -0,0 +1,52 @@
+// Copyright 2022 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.taglib.format;
+
+import javax.servlet.jsp.*;
+
+/**
+ * A JSP tag to format file sizes. 
+ * @author Luke
+ * @version 10.3
+ * @since 10.3
+ */
+
+public class FileSizeFormatTag extends IntegerFormatTag {
+
+	private static final String[] UNITS = {"B", "KB", "MB", "GB", "TB", "PB"};
+	
+	private int _unit;
+	
+   /**
+     * Sets the value to format. <i>This is a nasty hack for Tomcat 6.0.18+ EL casting.</i>
+     * @param value the value to format
+     */
+	@Override
+    public void setValue(Long value) {
+		double l10 = Math.log10(value.doubleValue());
+    	_unit = Math.max(0, Math.min((int)Math.round((l10 / 3) - 1), UNITS.length - 1));
+        super.setValue(Long.valueOf(Math.round(value.longValue() / Math.pow(10, (_unit * 3)))));
+    }
+    
+    /**
+     * Formats the number and writes it to the JSP output writer.
+     * @return TagSupport.EVAL_PAGE
+     * @throws JspException if an error occurs
+     */
+	@Override
+    public int doEndTag() throws JspException {
+		fmtNoDecimals();
+    	try {
+    		JspWriter out = pageContext.getOut();
+    		openSpan();
+    		out.print(_nF.format(_value.doubleValue()));
+    		out.print(UNITS[_unit]);
+    		closeSpan();
+    	} catch (Exception e) {
+    		throw new JspException(e);
+    	} finally {
+    		release();
+    	}
+    	
+    	return EVAL_PAGE;
+    }
+}
