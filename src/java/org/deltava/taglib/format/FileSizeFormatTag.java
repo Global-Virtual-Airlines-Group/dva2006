cc4e527f8be7e355d04a75ca59bf81a5963cc981
Luke Kolin <luke@deltava.org>
2022-10-12 01:20:14 +0000

    Add showBytes and unit test
    git-svn-id: file:///home/svn/DVA2006@17923 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/format/FileSizeFormatTag.java
+++ src/java/org/deltava/taglib/format/FileSizeFormatTag.java
@@ -15,6 +15,11 @@ public class FileSizeFormatTag extends IntegerFormatTag {
 	private static final String[] UNITS = {"B", "KB", "MB", "GB", "TB", "PB"};
 	
 	private int _unit;
+	private boolean _showBytes = true;
+	
+	public void setShowBytes(boolean showBytes) {
+		_showBytes = showBytes;
+	}
 	
    /**
      * Sets the value to format. <i>This is a nasty hack for Tomcat 6.0.18+ EL casting.</i>
@@ -26,6 +31,12 @@ public class FileSizeFormatTag extends IntegerFormatTag {
     	_unit = Math.max(0, Math.min((int)Math.round((l10 / 3) - 1), UNITS.length - 1));
         super.setValue(Long.valueOf(Math.round(value.longValue() / Math.pow(10, (_unit * 3)))));
     }
+	
+	@Override
+	public void release() {
+		_showBytes = true;
+		super.release();
+	}
     
     /**
      * Formats the number and writes it to the JSP output writer.
@@ -39,7 +50,7 @@ public class FileSizeFormatTag extends IntegerFormatTag {
     		JspWriter out = pageContext.getOut();
     		openSpan();
     		out.print(_nF.format(_value.doubleValue()));
-    		out.print(UNITS[_unit]);
+    		if (_showBytes || (_unit > 0)) out.print(UNITS[_unit]);
     		closeSpan();
     	} catch (Exception e) {
     		throw new JspException(e);
