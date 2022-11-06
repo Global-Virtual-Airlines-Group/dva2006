4858061a13e3c478fde9de96d3dba59a682fc91f
Luke Kolin <luke@deltava.org>
2022-11-06 01:22:07 +0000

    Don't load invalidated email addresses
    git-svn-id: file:///home/svn/DVA2006@18066 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/dao/GetPilotNotify.java
+++ src/java/org/deltava/dao/GetPilotNotify.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2010, 2011, 2014, 2016, 2018, 2019 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2010, 2011, 2014, 2016, 2018, 2019, 2022 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao;
 
 import java.util.*;
@@ -10,7 +10,7 @@ import org.deltava.util.MailUtils;
 /**
  * A Data Access Object to get Pilot notification lists.
  * @author Luke
- * @version 9.0
+ * @version 10.3
  * @since 1.0
  */
 
@@ -32,9 +32,10 @@ public class GetPilotNotify extends GetPilot {
     * @throws IllegalArgumentException if notificationType is invalid
     */
    public List<EMailAddress> getNotifications(Notification notificationType) throws DAOException {
-	   try (PreparedStatement ps = prepare("SELECT CONCAT_WS(' ', FIRSTNAME, LASTNAME) AS NM, EMAIL FROM PILOTS WHERE (STATUS=?) AND ((NOTIFY & ?) > 0) ORDER BY LASTNAME, FIRSTNAME")) {
+	   try (PreparedStatement ps = prepare("SELECT CONCAT_WS(' ', FIRSTNAME, LASTNAME) AS NM, EMAIL FROM PILOTS WHERE (STATUS=?) AND ((NOTIFY & ?) > 0) AND (EMAIL_INVALID=?) ORDER BY LASTNAME, FIRSTNAME")) {
          ps.setInt(1, PilotStatus.ACTIVE.ordinal());
          ps.setInt(2, notificationType.getCode());
+         ps.setBoolean(3, false);
          
          // Execute the query
          List<EMailAddress> results = new ArrayList<EMailAddress>();
@@ -56,9 +57,10 @@ public class GetPilotNotify extends GetPilot {
     * @throws DAOException if a JDBC error occurs
     */
    public Collection<Pilot> gePilots(Notification notificationType) throws DAOException {
-	   try (PreparedStatement ps = prepare("SELECT ID FROM PILOTS WHERE (STATUS=?) AND ((NOTIFY & ?) > 0)")) {
+	   try (PreparedStatement ps = prepare("SELECT ID FROM PILOTS WHERE (STATUS=?) AND ((NOTIFY & ?) > 0) AND (EMAIL_INVALID=?)")) {
 		   ps.setInt(1, PilotStatus.ACTIVE.ordinal());
 		   ps.setInt(2, notificationType.getCode());
+		   ps.setBoolean(3, false);
 		   return getByID(executeIDs(ps), "PILOTS").values();
 	   } catch (SQLException se) {
 		   throw new DAOException(se);
