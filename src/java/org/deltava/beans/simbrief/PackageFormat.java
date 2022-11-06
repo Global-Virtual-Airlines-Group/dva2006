dd2ae1470f1e0c90438eb451d92dd4c7de7cf381
Luke Kolin <luke@deltava.org>
2022-11-06 14:11:36 +0000

    Fix AF2017 package name
    git-svn-id: file:///home/svn/DVA2006@18070 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/simbrief/PackageFormat.java
+++ src/java/org/deltava/beans/simbrief/PackageFormat.java
@@ -1,8 +1,6 @@
 // Copyright 2022 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.simbrief;
 
-import org.deltava.beans.EnumDescription;
-
 /**
  * An enumeration of SimBrief package formats.
  * @author Luke
@@ -10,8 +8,8 @@ import org.deltava.beans.EnumDescription;
  * @since 10.3
  */
 
-public enum PackageFormat implements EnumDescription {
-	LIDO("LIDO"), AFR("Air France 2012"), AFR17("Air France 2017"), DAL("Delta Air Lines"), KLM("KLM Royal Dutch Airlines");
+public enum PackageFormat implements org.deltava.beans.EnumDescription {
+	LIDO("LIDO"), AFR("Air France 2012"), AFR2017("Air France 2017"), DAL("Delta Air Lines"), KLM("KLM Royal Dutch Airlines");
 	
 	private final String _desc;
 	
