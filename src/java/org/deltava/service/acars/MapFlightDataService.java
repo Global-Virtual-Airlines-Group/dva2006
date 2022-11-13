8145fb8cbcd11415c2b3ce8529709b7261ff27d4
Luke Kolin <luke@deltava.org>
2022-11-13 17:03:05 +0000

    Display pt relative to threshold
    git-svn-id: file:///home/svn/DVA2006@18116 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/service/acars/MapFlightDataService.java
+++ src/java/org/deltava/service/acars/MapFlightDataService.java
@@ -177,7 +177,7 @@ public class MapFlightDataService extends WebService {
 		ro.put("location", JSONUtils.format(rd));
 		ro.put("distance", rd.getDistance());
 		ro.put("hdg", rd.getHeading());		
-		ro.put("pt", JSONUtils.format(GeoUtils.bearingPointS(rd, distanceMiles, rd.getHeading() - rd.getMagVar())));
+		ro.put("pt", JSONUtils.format(GeoUtils.bearingPointS(rd.getThreshold(), distanceMiles, rd.getHeading() - rd.getMagVar())));
 		ro.put("thresholdLength", rd.getThresholdLength());
 		if (rd.getThresholdLength() > 0)
 			ro.put("threshold", JSONUtils.format(rd.getThreshold()));
