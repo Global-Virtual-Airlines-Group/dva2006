7b468aadb8ae309d454960b3e947cc7cacfa5bf8
Luke Kolin <luke@deltava.org>
2022-11-20 12:19:15 +0000

    Add EVENT, make enumDescription
    git-svn-id: file:///home/svn/DVA2006@18146 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/acars/ClientType.java
+++ src/java/org/deltava/beans/acars/ClientType.java
@@ -1,21 +1,20 @@
-// Copyright 2011 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2011, 2022 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.acars;
+
+import org.deltava.beans.EnumDescription;
 
 /**
  * An enumeration to list valid ACARS client types. 
  * @author Luke
- * @version 4.1
+ * @version 10.3
  * @since 4.1
  */
 
-public enum ClientType {
-	PILOT, DISPATCH, ATC;
-	
-	/**
-	 * Returns a formatted name.
-	 * @return the name
-	 */
-	public String getName() {
-		return (this == ATC) ? name() : name().substring(0, 1) + name().substring(1).toLowerCase(); 
+public enum ClientType implements EnumDescription {
+	PILOT, DISPATCH, ATC, EVENT;
+
+	@Override
+	public String getDescription() {
+		return (this == ATC) ? name() : EnumDescription.super.getDescription(); 
 	}
 }
