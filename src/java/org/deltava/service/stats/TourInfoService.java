f310552c417ecd7e4a982d8e0b3e2c97866828da
Luke Kolin <luke@deltava.org>
2022-11-20 12:21:13 +0000

    Add audit entries
    git-svn-id: file:///home/svn/DVA2006@18147 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/service/stats/TourInfoService.java
+++ src/java/org/deltava/service/stats/TourInfoService.java
@@ -3,6 +3,13 @@ package org.deltava.service.stats;
 
 import static javax.servlet.http.HttpServletResponse.*;
 
+import java.util.*;
+import java.sql.Connection;
+import java.util.stream.Collectors;
+
+import org.json.JSONObject;
+
+import org.deltava.beans.*;
 import org.deltava.beans.stats.Tour;
 
 import org.deltava.dao.*;
@@ -10,7 +17,7 @@ import org.deltava.service.*;
 
 import org.deltava.security.command.TourAccessControl;
 
-import org.deltava.util.StringUtils;
+import org.deltava.util.*;
 
 /**
  * A Web Service to display a JSON-serialized Flight Tour.
@@ -29,28 +36,61 @@ public class TourInfoService extends TourService {
 	 */
 	@Override
 	public int execute(ServiceContext ctx) throws ServiceException {
-		int id = StringUtils.parse(ctx.getParameter("id"), 0); Tour t = null;
+		int id = StringUtils.parse(ctx.getParameter("id"), 0); JSONObject jo = null;
 		try {
-			GetTour dao = new GetTour(ctx.getConnection());
-			t = dao.get(id, ctx.getDB());
+			Connection con = ctx.getConnection();
+			
+			// Get Tour data
+			GetTour dao = new GetTour(con);
+			Tour t = dao.get(id, ctx.getDB());
 			if (t == null)
 				return SC_NOT_FOUND;
-		} catch (DAOException de) {
-			throw error(SC_INTERNAL_SERVER_ERROR, de.getMessage(), de);
+			
+			// Check our access
+			TourAccessControl ac = new TourAccessControl(ctx, t);
+			ac.validate();
+			if (!ac.getCanRead())
+				return SC_FORBIDDEN;
+			
+			// Get audit info
+			GetAuditLog aldao = new GetAuditLog(con);
+			Collection<AuditLog> entries =  aldao.getEntries(t);
+			Collection<Integer> IDs = entries.stream().map(AuthoredBean::getAuthorID).collect(Collectors.toSet());
+			
+			// Load author IDs
+			GetPilot pdao = new GetPilot(con);
+			Map<Integer, Pilot> authors = pdao.getByID(IDs, "PILOTS");
+			
+			// Serialize the object and its audit log
+			jo = serialize(t);
+			for (AuditLog ae : entries) {
+				Pilot p = authors.get(Integer.valueOf(ae.getAuthorID()));
+				JSONObject ao = new JSONObject();
+				ao.put("date", ae.getDate().toEpochMilli() / 1000);
+				ao.put("description", ae.getDescription());
+				if (p != null) {
+					JSONObject po = new JSONObject();
+					po.put("id", p.getID());
+					po.put("pilotCode", p.getPilotCode());
+					po.put("firstName", p.getFirstName());
+					po.put("lastName", p.getLastName());
+					ao.put("auhtor", po);
+				}
+				
+				jo.accumulate("revisionLog", ao);
+			}
+			
+		} catch (Exception e) {
+			throw error(SC_INTERNAL_SERVER_ERROR, e.getMessage(), e);
 		} finally {
 			ctx.release();
 		}
 		
-		// Check our access
-		TourAccessControl ac = new TourAccessControl(ctx, t);
-		ac.validate();
-		if (!ac.getCanRead())
-			return SC_FORBIDDEN;
-		
 		// Dump the JSON to the output stream
+		JSONUtils.ensureArrayPresent(jo, "revisionLog");
 		try {
 			ctx.setContentType("application/json", "utf-8");
-			ctx.println(serialize(t).toString());
+			ctx.println(jo.toString());
 			ctx.commit();			
 		} catch (Exception e) {
 			throw error(SC_CONFLICT, "I/O Error", false);
