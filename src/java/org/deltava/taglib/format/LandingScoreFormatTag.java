8a101c412ec77a38f181efadd28c2387181cd079
Luke Kolin <luke@deltava.org>
2023-01-21 12:44:21 +0000

    Add landing score
    git-svn-id: file:///home/svn/DVA2006@18411 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/taglib/format/LandingScoreFormatTag.java
@@ -0,0 +1,90 @@
+// Copyright 2023 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.taglib.format;
+
+import java.text.DecimalFormatSymbols;
+
+import javax.servlet.jsp.*;
+
+import org.deltava.util.StringUtils;
+
+/**
+ * A JSP tag to format landing scores.
+ * @author Luke
+ * @version 10.4
+ * @since 10.4
+ */
+
+public class LandingScoreFormatTag extends NumberFormatTag {
+	
+	private static final String DEFAULT_PATTERN = "#0.00";
+	
+	private String _default;
+	
+	/**
+     * Initializes the tag and the number formatter.
+     */
+	public LandingScoreFormatTag() {
+		super(DEFAULT_PATTERN);
+	}
+	
+	/**
+	 * Updates the default to display if no score has been calculated. 
+	 * @param s the default string, or null to suppress display
+	 */
+	public void setDefault(String s) {
+		_default = s;
+	}
+	
+    @Override
+	public void setPageContext(PageContext ctxt) {
+        super.setPageContext(ctxt);
+        if (_user != null) {
+        	DecimalFormatSymbols sym = _nF.getDecimalFormatSymbols();
+        	_nF.applyPattern(DEFAULT_PATTERN);
+        	_nF.setDecimalFormatSymbols(sym);
+        }
+    }
+	
+    @Override
+    public int doStartTag() {
+    	if (_value == null) _value = Double.valueOf(-1);
+    	
+    	StringBuilder buf = getClassNameBuilder();
+    	int s = _value.intValue();
+    	if ((s >= 0) && (s < 60))
+			buf.append("error");
+		else if (s < 75)
+			buf.append("warn");
+		else if (s < 95)
+			buf.append("pri");
+		else
+			buf.append("ok");
+    	
+    	setClassName(buf.toString());
+    	return SKIP_BODY;
+    }
+	
+	@Override
+	public int doEndTag() throws JspException {
+		boolean noValue = (_value.intValue() < 0);
+		if (noValue && StringUtils.isEmpty(_default))
+			return EVAL_PAGE;
+		
+		try {
+			openSpan();
+			pageContext.getOut().print(noValue ? _default : _nF.format(_value.doubleValue()));
+			closeSpan();
+			return EVAL_PAGE;
+		} catch (Exception e) {
+			throw new JspException(e);
+		} finally {
+			release();
+		}
+	}
+
+	@Override
+	public void release() {
+		_default = null;
+		super.release(DEFAULT_PATTERN);
+	}
+}
