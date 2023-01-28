bc1bd4c9a0479c02f724bc8dd2acb6ac54fc2f3d
Luke Kolin <luke@deltava.org>
2023-01-28 03:18:08 +0000

    Show served airports
    git-svn-id: file:///home/svn/DVA2006@18440 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/stats/AccomplishmentAirportMapCommand.java
+++ src/java/org/deltava/commands/stats/AccomplishmentAirportMapCommand.java
@@ -2,14 +2,15 @@
 package org.deltava.commands.stats;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import java.sql.Connection;
 
 import org.json.*;
 
-import org.deltava.beans.Pilot;
+import org.deltava.beans.*;
 import org.deltava.beans.flight.FlightReport;
 import org.deltava.beans.stats.*;
-import org.deltava.beans.schedule.Airport;
+import org.deltava.beans.schedule.*;
 
 import org.deltava.commands.*;
 import org.deltava.dao.*;
@@ -24,7 +25,61 @@ import org.deltava.util.system.SystemData;
  */
 
 public class AccomplishmentAirportMapCommand extends AbstractCommand {
+	
+	private class AirportMapEntry implements IconMapEntry, ICAOAirport, Comparable<AirportMapEntry> {
+		private final Airport _a;
+		
+		AirportMapEntry(Airport a) {
+			super();
+			_a = a;
+		}
+		
+		@Override
+		public String getICAO() {
+			return _a.getICAO();
+		}
+		
+		@Override
+		public double getLatitude() {
+			return _a.getLatitude();
+		}
+		
+		@Override
+		public double getLongitude() {
+			return _a.getLongitude();
+		}
+		
+		@Override
+		public int getAltitude() {
+			return _a.getAltitude();
+		}
+		
+		@Override
+		public int getIconCode() {
+			return _a.getIconCode();
+		}
+		
+		@Override
+		public int getPaletteCode() {
+			return _a.getPaletteCode();
+		}
+		
+		@Override
+		public String getInfoBox() {
+			StringBuilder buf = new StringBuilder(_a.getInfoBox());
+			buf.setLength(buf.length() - 6); // strip closing div tag
+			buf.append("<br /><br /><span class=\"small\">Airlines served:<br />");
+			_a.getAirlineCodes().stream().map(SystemData::getAirline).filter(Objects::nonNull).forEach(al -> buf.append(al.getName()).append("<br />"));
+			buf.append("</div>");
+			return buf.toString();
+		}
 
+		@Override
+		public int compareTo(AirportMapEntry ae2) {
+			return _a.compareTo(ae2._a);
+		}
+	}
+	
 	/**
 	 * Execute the command.
 	 * @param ctx the Command context
@@ -36,11 +91,11 @@ public class AccomplishmentAirportMapCommand extends AbstractCommand {
 		// Determine who to display
         int id = ctx.isUserInRole("HR") ? ctx.getID() : ctx.getUser().getID();
         if (id == 0)
-        		id = ctx.getUser().getID();
+       		id = ctx.getUser().getID();
         
         ctx.setAttribute("isOurs", Boolean.valueOf(id == ctx.getUser().getID()), REQUEST);
 		
-        Map<String, Collection<?>> missingAirports = new HashMap<String, Collection<?>>();
+        Map<String, Collection<AirportMapEntry>> missingAirports = new HashMap<String, Collection<AirportMapEntry>>();
 		try {
 			Connection con = ctx.getConnection();
 			
@@ -61,10 +116,10 @@ public class AccomplishmentAirportMapCommand extends AbstractCommand {
 			GetFlightReports frdao = new GetFlightReports(con);
 			Collection<FlightReport> flights = frdao.getByPilot(p.getID(), null);
 			frdao.getCaptEQType(flights);
-			flights.forEach(fr -> helper.add(fr));
+			flights.forEach(helper::add);
 			
 			// Map the missing airports to the accomplishments
-			accs.stream().filter(Accomplishment::getActive).forEach(a -> missingAirports.put(a.getComboAlias(), helper.missing(a)));
+			accs.stream().filter(Accomplishment::getActive).forEach(a -> missingAirports.put(a.getComboAlias(), helper.missing(a).stream().map(ap -> new AirportMapEntry((Airport) ap)).collect(Collectors.toList())));
 			
 			// Save in request
 			ctx.setAttribute("accs", accs, REQUEST);
@@ -78,14 +133,11 @@ public class AccomplishmentAirportMapCommand extends AbstractCommand {
 		
 		// Convert to JSON
 		JSONObject jo = new JSONObject();
-		Collection<Airport> allAirports = new TreeSet<Airport>();
-		for (Map.Entry<String, Collection<?>> me : missingAirports.entrySet()) {
-			@SuppressWarnings("unchecked")
-			Collection<Airport> airports = (Collection<Airport>) me.getValue();
-			allAirports.addAll(airports);
-
+		Collection<AirportMapEntry> allAirports = new TreeSet<AirportMapEntry>();
+		for (Map.Entry<String, Collection<AirportMapEntry>> me : missingAirports.entrySet()) {
+			allAirports.addAll(me.getValue());
 			JSONArray ja = new JSONArray();
-			airports.forEach(a -> ja.put(a.getICAO()));
+			me.getValue().forEach(a -> ja.put(a.getICAO()));
 			jo.put(me.getKey(), ja);
 		}
 		
