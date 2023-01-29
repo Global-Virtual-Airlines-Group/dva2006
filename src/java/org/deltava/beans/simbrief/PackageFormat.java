a0c30d7f5703f93923565048b12134c0857f547f
Luke Kolin <luke@deltava.org>
2023-01-29 16:36:04 +0000

    Parse weight and Package format correctly
    git-svn-id: file:///home/svn/DVA2006@18445 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/simbrief/PackageFormat.java
+++ src/java/org/deltava/beans/simbrief/PackageFormat.java
@@ -1,24 +1,54 @@
-// Copyright 2022 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2022, 2023 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.simbrief;
 
 /**
  * An enumeration of SimBrief package formats.
  * @author Luke
- * @version 10.3
+ * @version 10.4
  * @since 10.3
  */
 
 public enum PackageFormat implements org.deltava.beans.EnumDescription {
-	LIDO("LIDO"), AFR("Air France 2012"), AFR2017("Air France 2017"), DAL("Delta Air Lines"), KLM("KLM Royal Dutch Airlines");
+	LIDO("LIDO"), AFR("Air France 2012", "AFR '12"), AFR2017("Air France 2017", "AFR '17"), DAL("Delta Air Lines"), KLM("KLM Royal Dutch Airlines");
 	
 	private final String _desc;
+	private final String _alt;
 	
 	PackageFormat(String desc) {
+		this(desc, null);
+	}
+	
+	PackageFormat(String desc, String alt) {
 		_desc = desc;
+		_alt = alt;
+	}
+	
+	/**
+	 * Returns the alternate format code.
+	 * @return the code
+	 */
+	public String getAlternate() {
+		return _alt;
 	}
 	
 	@Override
 	public String getDescription() {
 		return _desc;
+	}
+	
+	/**
+	 * Parses a SimBrief package type name, including alternates.
+	 * @param v the value to parse
+	 * @param defaultValue the default value if unknown
+	 * @return a PackageFormat
+	 */
+	public static PackageFormat parse(String v, PackageFormat defaultValue) {
+		if (v == null) return defaultValue;
+		for (PackageFormat pkg : values()) {
+			if (v.equalsIgnoreCase(pkg.name()) || v.equalsIgnoreCase(pkg._alt))
+				return pkg;
+		}
+		
+		return defaultValue; 
 	}
 }
