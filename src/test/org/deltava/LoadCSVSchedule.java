60f94737053adfce83aad15ee284f7201f50fa7c
Luke Kolin <luke@deltava.org>
2023-03-12 13:15:26 +0000

    Updated DL/WJA code. Now not O(n2)
    git-svn-id: file:///home/svn/DVA2006@18573 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/LoadCSVSchedule.java
+++ src/test/org/deltava/LoadCSVSchedule.java
@@ -5,7 +5,6 @@ import java.sql.*;
 import java.time.*;
 import java.time.format.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import org.apache.log4j.*;
 
@@ -24,11 +23,23 @@ public class LoadCSVSchedule extends TestCase {
 	private Logger log;
 	
 	private Connection _c;
+	private final Collection<Aircraft> _allAC = new ArrayList<Aircraft>();
 	
-	private final ScheduleSource SRC = ScheduleSource.LEGACY; 
+	private final ScheduleSource SRC = ScheduleSource.DELTA; 
 
 	private final DateTimeFormatter _df = new DateTimeFormatterBuilder().appendPattern("MM/dd/yyyy").toFormatter();
 	private final DateTimeFormatter _tf = new DateTimeFormatterBuilder().appendPattern("HH:mm").toFormatter();
+	
+	private class ScheduleEntryComparator implements Comparator<RawScheduleEntry> {
+		@Override
+		public int compare(RawScheduleEntry rse1, RawScheduleEntry rse2) {
+			int tmpResult = rse1.getFlightCode().compareTo(rse2.getFlightCode());
+			if (tmpResult != 0)
+				tmpResult = rse1.createKey().compareTo(rse2.createKey());
+			
+			return (tmpResult == 0) ? rse1.getEndDate().compareTo(rse2.getEndDate()) : tmpResult;
+		}
+	}
 
 	@Override
 	protected void setUp() throws Exception {
@@ -51,6 +62,11 @@ public class LoadCSVSchedule extends TestCase {
 		SystemData.add("airports", apdao.getAll());
 		GetAirline aldao = new GetAirline(_c);
 		SystemData.add("airlines", aldao.getAll());
+		
+		// Load aircraft
+		GetAircraft acdao = new GetAircraft(_c);
+		_allAC.addAll(acdao.getAll());
+		_c.setAutoCommit(false);
 	}
 
 	@Override
@@ -60,10 +76,95 @@ public class LoadCSVSchedule extends TestCase {
 		LogManager.shutdown();
 		super.tearDown();
 	}
-
-	public void testWestJet() throws Exception {
+	
+	private RawScheduleEntry parse(int ln, CSVTokens csv, String airlineCode) {
+		RawScheduleEntry rse = new RawScheduleEntry(FlightCodeParser.parse(csv.get(6), airlineCode));
+		rse.setAirportD(SystemData.getAirport(csv.get(1)));
+		rse.setAirportA(SystemData.getAirport(csv.get(3)));
+		if (rse.getAirportD() == null) {
+			log.warn("Unknown Airport " + csv.get(1) + " at Line " + ln);
+			return null;
+		} else if (rse.getAirportA() == null) {
+			log.warn("Unknown Airport " + csv.get(3) + " at Line " + ln);
+			return null;
+		}
+		
+		rse.setStartDate(LocalDate.parse(csv.get(11), _df));
+		rse.addDayOfWeek(rse.getStartDate().getDayOfWeek());
+		rse.setEndDate(rse.getStartDate());
+		rse.setTimeD(LocalDateTime.of(rse.getStartDate(), LocalTime.parse(csv.get(7), _tf)));
+		rse.setTimeA(LocalDateTime.of(rse.getStartDate(), LocalTime.parse(csv.get(8), _tf)));
+		rse.setSource(SRC);
+		rse.setForceInclude(true);
+		
+		String ac = csv.get(9).toUpperCase();
+		Aircraft a = _allAC.stream().filter(ap -> ap.getIATA().contains(ac)).findAny().orElse(null);
+		if (a == null) {
+			log.warn("Unknown aircraft code - " + ac);
+			return null;
+		}
+		
+		rse.setEquipmentType(a.getName());
+		return rse;
+	}
+	
+	public void testWestjet() throws Exception {
 		
 		File f = new File("C:\\Temp", "wja_schedule_march.csv");
+		assertTrue(f.exists());
+
+		// Get source info
+		GetRawSchedule rsdao = new GetRawSchedule(_c);
+		ScheduleSourceInfo srcInfo = rsdao.getSources(false, "dva").stream().filter(ssi -> ssi.getSource() == SRC).findFirst().orElse(null);
+		int baseLine = (srcInfo == null) ? 0 : srcInfo.getMaxLineNumber();
+
+		// Put entries into buckets
+		Map<String, Collection<RawScheduleEntry>> buckets = new HashMap<String, Collection<RawScheduleEntry>>();
+		try (LineNumberReader lr = new LineNumberReader(new FileReader(f), 131072)) {
+			String data = lr.readLine(); // skip first line
+			while (data != null) {
+				data = lr.readLine();
+				if (data == null) break;
+				CSVTokens csv = StringUtils.parseCSV(data);
+				RawScheduleEntry rse = parse(lr.getLineNumber(), csv, "WJA");
+				if (rse == null) continue;
+				rse.setLineNumber(baseLine + lr.getLineNumber());
+				CollectionUtils.addMapCollection(buckets, String.format("%s-%s",rse.createKey(), rse.getFlightCode()), rse, ArrayList::new);
+			}
+		}
+		
+		ScheduleEntryComparator cmp = new ScheduleEntryComparator();
+		List<RawScheduleEntry> entries = new ArrayList<RawScheduleEntry>();
+		for (Map.Entry<String, Collection<RawScheduleEntry>> me : buckets.entrySet()) {
+			LinkedList<RawScheduleEntry> b2 = new LinkedList<RawScheduleEntry>(me.getValue());
+			b2.sort(cmp);
+			RawScheduleEntry rse = b2.removeFirst(); RawScheduleEntry rse2 = b2.peek();
+			entries.add(rse);
+			while (rse2 != null) {
+				b2.removeFirst(); // remove rse2
+				if (rse2.getTimeD().toLocalTime().equals(rse.getTimeD().toLocalTime()) && rse2.getTimeA().toLocalTime().equals(rse.getTimeA().toLocalTime())) {
+					rse.setEndDate(rse2.getEndDate());
+					rse2.getDays().forEach(rse::addDayOfWeek);
+				} else {
+					rse = rse2;
+					entries.add(rse);
+				}
+					
+				rse2 = b2.peek();
+			}
+		}
+		
+		SetSchedule swdao = new SetSchedule(_c);
+		//swdao.purge(SRC);
+		for (RawScheduleEntry rse : entries)
+				swdao.writeRaw(rse, false);
+		
+		_c.commit();
+	}
+
+	public void testDelta() throws Exception {
+		
+		File f = new File("C:\\Temp", "dal_schedule_march.csv");
 		assertTrue(f.exists());
 		
 		// Get source info
@@ -71,60 +172,46 @@ public class LoadCSVSchedule extends TestCase {
 		ScheduleSourceInfo srcInfo = rsdao.getSources(false, "dva").stream().filter(ssi -> ssi.getSource() == SRC).findFirst().orElse(null);
 		int baseLine = (srcInfo == null) ? 0 : srcInfo.getMaxLineNumber();
 		
-		// Load aircraft
-		GetAircraft acdao = new GetAircraft(_c);
-		Collection<Aircraft> allAC = acdao.getAll();
-		_c.setAutoCommit(false);
-		
-		Collection<RawScheduleEntry> entries = new ArrayList<RawScheduleEntry>();
+		// Put entries into buckets
+		Map<String, Collection<RawScheduleEntry>> buckets = new HashMap<String, Collection<RawScheduleEntry>>();
 		try (LineNumberReader lr = new LineNumberReader(new FileReader(f), 131072)) {
 			String data = lr.readLine(); // skip first line
 			while (data != null) {
 				data = lr.readLine();
 				if (data == null) break;
 				CSVTokens csv = StringUtils.parseCSV(data);
-				RawScheduleEntry rse = new RawScheduleEntry(FlightCodeParser.parse(csv.get(2), "WJA"));
-				rse.setAirportD(SystemData.getAirport(csv.get(0)));
-				rse.setAirportA(SystemData.getAirport(csv.get(1)));
-				if (rse.getAirportD() == null) {
-					log.warn("Unknown Airport " + csv.get(0) + " at Line " + lr.getLineNumber());
-					continue;
-				} else if (rse.getAirportA() == null) {
-					log.warn("Unknown Airport " + csv.get(1) + " at Line " + lr.getLineNumber());
-					continue;
-				}
-				
-				rse.setStartDate(LocalDate.parse(csv.get(6), _df));
-				rse.setEndDate(rse.getStartDate());
-				rse.setTimeD(LocalDateTime.of(rse.getStartDate(), LocalTime.parse(csv.get(3), _tf)));
-				rse.setTimeA(LocalDateTime.of(rse.getStartDate(), LocalTime.parse(csv.get(4), _tf)));
-				rse.setSource(SRC);
+				RawScheduleEntry rse = parse(lr.getLineNumber(), csv, "DAL");
+				if (rse == null) continue;
 				rse.setLineNumber(baseLine + lr.getLineNumber());
-				rse.setForceInclude(true);
-				
-				String ac = csv.get(5).toUpperCase();
-				Aircraft a = allAC.stream().filter(ap -> ap.getIATA().contains(ac)).findAny().orElse(null);
-				if (a == null) {
-					log.warn("Unknown aircraft code - " + ac);
-					continue;
-				}
-				
-				rse.setEquipmentType(a.getName());
-				
-				List<RawScheduleEntry> rsMatches = entries.stream().filter(re -> re.getFlightCode().equals(rse.getFlightCode())).collect(Collectors.toList());
-				if (rsMatches.size() > 0) {
-					RawScheduleEntry rse2 = rsMatches.get(rsMatches.size() - 1);
-					if (rse2.getTimeD().toLocalTime().equals(rse.getTimeD().toLocalTime()) && rse2.getTimeA().toLocalTime().equals(rse.getTimeA().toLocalTime()))
-						rse2.setEndDate(rse.getEndDate());
-					else
-						entries.add(rse);
-				} else 
-					entries.add(rse);
+				CollectionUtils.addMapCollection(buckets, String.format("%s-%s",rse.createKey(), rse.getFlightCode()), rse, ArrayList::new);
 			}
 		}
 		
+		ScheduleEntryComparator cmp = new ScheduleEntryComparator();
+		List<RawScheduleEntry> entries = new ArrayList<RawScheduleEntry>();
+		for (Map.Entry<String, Collection<RawScheduleEntry>> me : buckets.entrySet()) {
+			LinkedList<RawScheduleEntry> b2 = new LinkedList<RawScheduleEntry>(me.getValue());
+			b2.sort(cmp);
+			RawScheduleEntry rse = b2.removeFirst(); RawScheduleEntry rse2 = b2.peek();
+			entries.add(rse);
+			while (rse2 != null) {
+				b2.removeFirst(); // remove rse2
+				if (rse2.getTimeD().toLocalTime().equals(rse.getTimeD().toLocalTime()) && rse2.getTimeA().toLocalTime().equals(rse.getTimeA().toLocalTime())) {
+					rse.setEndDate(rse2.getEndDate());
+					rse2.getDays().forEach(rse::addDayOfWeek);
+				} else {
+					rse = rse2;
+					entries.add(rse);
+				}
+					
+				rse2 = b2.peek();
+			}
+		}
+		
+		entries.stream().filter(rse -> "B737-900".equals(rse.getEquipmentType())).forEach(rse -> rse.setEquipmentType("B737-900ER"));
+		
 		SetSchedule swdao = new SetSchedule(_c);
-		swdao.purge(ScheduleSource.LEGACY);
+		//swdao.purge(SRC);
 		for (RawScheduleEntry rse : entries)
 			swdao.writeRaw(rse, false);
 		
