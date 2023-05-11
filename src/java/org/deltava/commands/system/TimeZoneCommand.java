ae8c2339cb0e4277f73ed5d96ffccf8f8be6ffc6
Luke Kolin <luke@deltava.org>
2023-05-11 18:17:51 +0000

    Better UI for zone ids
    git-svn-id: file:///home/svn/DVA2006@18797 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/system/TimeZoneCommand.java
+++ src/java/org/deltava/commands/system/TimeZoneCommand.java
@@ -1,26 +1,39 @@
-// Copyright 2007, 2012, 2016, 2021 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2007, 2012, 2016, 2021, 2023 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.system;
 
-import java.time.ZoneId;
+import java.util.*;
+import java.time.*;
+import java.util.stream.Collectors;
 
 import org.deltava.beans.TZInfo;
 
 import org.deltava.commands.*;
 import org.deltava.dao.*;
 
-import org.deltava.util.ComboUtils;
-
 import org.gvagroup.common.*;
 
 /**
  * A Web Site Command to edit time zone profiles.
  * @author Luke
- * @version 10.2
+ * @version 10.6
  * @since 1.0
  */
 
 public class TimeZoneCommand extends AbstractFormCommand {
 
+	private class TZComparator implements Comparator<ZoneId> {
+		
+		private final Instant NOW = Instant.now();
+
+		@Override
+		public int compare(ZoneId tz1, ZoneId tz2) {
+			ZoneOffset zo1 = tz1.getRules().getOffset(NOW);
+			ZoneOffset zo2 = tz2.getRules().getOffset(NOW);
+			int tmpResult = zo1.compareTo(zo2);
+			return (tmpResult == 0) ? tz1.getId().compareTo(tz2.getId()) : tmpResult;
+		}
+	}
+	
 	/**
 	 * Callback method called when editing an existing Time Zone.
 	 * @param ctx the Command context
@@ -36,10 +49,14 @@ public class TimeZoneCommand extends AbstractFormCommand {
 		TZInfo tz = TZInfo.get(id);
 		if ((tz == null) && (id != null))
 			throw notFoundException("Unknown Time Zone - " + id);
+		
+		// Get time zone IDs and convert to combo util
+		List<ZoneId> zoneIDs = ZoneId.getAvailableZoneIds().stream().map(ZoneId::of).collect(Collectors.toList());
+		zoneIDs.sort(new TZComparator());
 
 		// Save the time zone and JVM options
 		ctx.setAttribute("tz", tz, REQUEST);
-		ctx.setAttribute("tzIDs", ComboUtils.fromList(ZoneId.getAvailableZoneIds()), REQUEST);
+		ctx.setAttribute("tzIDs", zoneIDs, REQUEST);
 		
 		// Forward to the JSP
 		CommandResult result = ctx.getResult();
