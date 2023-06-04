4c47ce6f65f7768086edd73544f04031e92afa73
Luke Kolin <luke@deltava.org>
2023-06-04 14:34:56 +0000

    June 2023 schedule
    git-svn-id: file:///home/svn/DVA2006@18874 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/LoadCSVSchedule.java
+++ src/test/org/deltava/LoadCSVSchedule.java
@@ -88,15 +88,15 @@ public class LoadCSVSchedule extends TestCase {
 			return null;
 		}
 		
-		rse.setStartDate(LocalDate.parse(csv.get(11), _df));
+		rse.setStartDate(LocalDate.parse(csv.get(14), _df));
 		rse.addDayOfWeek(rse.getStartDate().getDayOfWeek());
 		rse.setEndDate(rse.getStartDate());
-		rse.setTimeD(LocalDateTime.of(rse.getStartDate(), LocalTime.parse(csv.get(7), _tf)));
-		rse.setTimeA(LocalDateTime.of(rse.getStartDate(), LocalTime.parse(csv.get(8), _tf)));
+		rse.setTimeD(LocalDateTime.of(rse.getStartDate(), LocalTime.parse(csv.get(9), _tf)));
+		rse.setTimeA(LocalDateTime.of(rse.getStartDate(), LocalTime.parse(csv.get(10), _tf)));
 		rse.setSource(SRC);
 		rse.setForceInclude(true);
 		
-		String ac = csv.get(9).toUpperCase();
+		String ac = csv.get(11).toUpperCase();
 		Aircraft a = _allAC.stream().filter(ap -> ap.getIATA().contains(ac)).findAny().orElse(null);
 		if (a == null) {
 			log.warn("Unknown aircraft code - " + ac);
@@ -109,7 +109,7 @@ public class LoadCSVSchedule extends TestCase {
 	
 	public void testWestjet() throws Exception {
 		
-		File f = new File("C:\\Temp", "wja_schedule_march.csv");
+		File f = new File("C:\\Temp", "WJA_June-2023.csv");
 		assertTrue(f.exists());
 
 		// Get source info
@@ -163,7 +163,7 @@ public class LoadCSVSchedule extends TestCase {
 
 	public void testDelta() throws Exception {
 		
-		File f = new File("C:\\Temp", "dal_schedule_march.csv");
+		File f = new File("C:\\Temp", "DAL_June-2023.csv");
 		assertTrue(f.exists());
 		
 		// Get source info
@@ -208,6 +208,60 @@ public class LoadCSVSchedule extends TestCase {
 		}
 		
 		entries.stream().filter(rse -> "B737-900".equals(rse.getEquipmentType())).forEach(rse -> rse.setEquipmentType("B737-900ER"));
+		
+		SetSchedule swdao = new SetSchedule(_c);
+		//swdao.purge(SRC);
+		for (RawScheduleEntry rse : entries)
+			swdao.writeRaw(rse, false);
+		
+		_c.commit();
+	}
+	
+	public void testAeromexico() throws Exception {
+
+		File f = new File("C:\\Temp", "AMX_June-2023.csv");
+		assertTrue(f.exists());
+		
+		// Get source info
+		GetRawSchedule rsdao = new GetRawSchedule(_c);
+		ScheduleSourceInfo srcInfo = rsdao.getSources(false, "dva").stream().filter(ssi -> ssi.getSource() == SRC).findFirst().orElse(null);
+		int baseLine = (srcInfo == null) ? 0 : srcInfo.getMaxLineNumber();
+		
+		// Put entries into buckets
+		Map<String, Collection<RawScheduleEntry>> buckets = new HashMap<String, Collection<RawScheduleEntry>>();
+		try (LineNumberReader lr = new LineNumberReader(new FileReader(f), 131072)) {
+			String data = lr.readLine(); // skip first line
+			while (data != null) {
+				data = lr.readLine();
+				if (data == null) break;
+				CSVTokens csv = StringUtils.parseCSV(data);
+				RawScheduleEntry rse = parse(lr.getLineNumber(), csv, "AMX");
+				if (rse == null) continue;
+				rse.setLineNumber(baseLine + lr.getLineNumber());
+				CollectionUtils.addMapCollection(buckets, String.format("%s-%s",rse.createKey(), rse.getFlightCode()), rse, ArrayList::new);
+			}
+		}
+		
+		ScheduleEntryComparator cmp = new ScheduleEntryComparator();
+		List<RawScheduleEntry> entries = new ArrayList<RawScheduleEntry>();
+		for (Map.Entry<String, Collection<RawScheduleEntry>> me : buckets.entrySet()) {
+			LinkedList<RawScheduleEntry> b2 = new LinkedList<RawScheduleEntry>(me.getValue());
+			b2.sort(cmp);
+			RawScheduleEntry rse = b2.removeFirst(); RawScheduleEntry rse2 = b2.peek();
+			entries.add(rse);
+			while (rse2 != null) {
+				b2.removeFirst(); // remove rse2
+				if (rse2.getTimeD().toLocalTime().equals(rse.getTimeD().toLocalTime()) && rse2.getTimeA().toLocalTime().equals(rse.getTimeA().toLocalTime())) {
+					rse.setEndDate(rse2.getEndDate());
+					rse2.getDays().forEach(rse::addDayOfWeek);
+				} else {
+					rse = rse2;
+					entries.add(rse);
+				}
+					
+				rse2 = b2.peek();
+			}
+		}
 		
 		SetSchedule swdao = new SetSchedule(_c);
 		//swdao.purge(SRC);
