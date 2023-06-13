28029bfa02f5d0722efc208905724bf66e9e34f2
Luke Kolin <luke@deltava.org>
2023-06-13 02:14:39 +0000

    Additional sort work
    git-svn-id: file:///home/svn/DVA2006@18896 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/tableSort.js
+++ static/js/tableSort.js
@@ -8,15 +8,15 @@ golgotha.sort.mapRows = function(cName) {
 };
 
 golgotha.sort.exec = function(prefix, t) {
-	const rowData = golgotha.sort.mapRows(prefix);
+	const rowData = golgotha.sort.mapRows(prefix + 'Data');
 	const p = golgotha.sort.lastSort[prefix] || {type:t,isReverse:false};
-	const cmp = function(e1, e2) { 	return p.isReverse ? (e1[t] - e2[t]) : (e2[t] - e1[t]); };
+	const cmp = function(e1, e2) { return p.isReverse ? (e1[t] - e2[t]) : (e2[t] - e1[t]); };
 	const data = golgotha.sort.data[prefix].slice();
 	data.sort(cmp);
 
 	// Iterate through the table and add rows
 	const pr = document.getElementById(prefix + 'Label');
-	data.forEach(function(d) { pr.parentNode.insertBefore(rowData[prefix + '-' + d.id], null); });
+	data.forEach(function(d) { pr.parentNode.insertBefore(rowData[prefix + '-' + d.id], pr.parentNode.lastChild); });
 
 	// Save settings	
 	p.isReverse = !p.isReverse;
