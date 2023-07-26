9b22f75ae5390a1d0a7878ea2e18e9fb5ce7739e
Luke Kolin <luke@deltava.org>
2023-07-26 00:16:12 +0000

    Common Runway/Gate usage classes
    git-svn-id: file:///home/svn/DVA2006@19049 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/stats/GateUsage.java
+++ src/java/org/deltava/beans/stats/GateUsage.java
@@ -6,48 +6,27 @@ import java.util.stream.Collectors;
 
 import org.deltava.beans.schedule.*;
 
-import org.deltava.util.cache.Cacheable;
-
 /**
- * A bean to store gate usage statistics.
+ * A bean to store Runway/Gate usage statistics.
  * @author Luke
- * @version 11.0
+ * @version 11.1
  * @since 10.0
  */
 
-public class GateUsage implements Cacheable, RoutePair {
+public class GateUsage extends RunwayGateUsage {
 	
 	/**
 	 * Number of years back to aggregate gate/airline usage.
 	 */
 	public static final int GATE_USAGE_YEARS = 6;
 	
-	private final Airport _aD;
-	private final Airport _aA;
-	private final boolean _isDeparture;
-	
-	private final Collection<GateTotal> _usage = new TreeSet<GateTotal>();
-
 	/**
 	 * Creates the bean.
 	 *  @param rp the RoutePair
-	 *  @param isDeparture TRUE if departure gates, otherwise FALSE
+	 *  @param isDeparture TRUE if departure gates/runways, otherwise FALSE
 	 */
 	public GateUsage(RoutePair rp, boolean isDeparture) {
-		super();
-		_aD = rp.getAirportD();
-		_aA = rp.getAirportA();
-		_isDeparture = isDeparture;
-	}
-	
-	@Override
-	public Airport getAirportD() {
-		return _aD;
-	}
-
-	@Override
-	public Airport getAirportA() {
-		return _aA;
+		super(rp, isDeparture);
 	}
 	
 	/**
@@ -55,100 +34,42 @@ public class GateUsage implements Cacheable, RoutePair {
 	 * @return a Collection of Airline codes
 	 */
 	public Collection<Airline> getAirlines() {
-		return _usage.stream().map(GateTotal::getAirline).collect(Collectors.toSet());
+		return _usage.stream().map(RunwayGateTotal::getAirline).collect(Collectors.toSet());
 	}
 	
 	/**
-	 * Returns the number of Gates with usage data.
-	 * @return the number of Gates
-	 */
-	public int size() {
-		return _usage.size();
-	}
-	
-	/**
-	 * Returns whether these are departure gate statistics.
-	 * @return TRUE if departure statistics, otherwise FALSE
-	 */
-	public boolean getIsDeparture() {
-		return _isDeparture;
-	}
-	
-	/**
-	 * Returns whether there is any Gate date for a particular Airline.
+	 * Returns whether there is any Runway/Gate date for a particular Airline.
 	 * @param airlineCode the Airline code
-	 * @return TRUE if at least one Gate has been used with this Airline, otherwise FALSE
+	 * @return TRUE if at least one Runway/Gate has been used with this Airline, otherwise FALSE
 	 */
 	public boolean hasAriline(String airlineCode) {
 		return _usage.stream().anyMatch(gt -> airlineCode.equals(gt.getAirline().getCode()));
 	}
 	
 	/**
-	 * Adds usage count for a particular Gate and Airline.
-	 * @param gateName the Gate name
-	 * @param a the Airline
-	 * @param totalUsage the total usage count
-	 */
-	public void addGate(String gateName, Airline a, int totalUsage) {
-		if (totalUsage > 0)
-			_usage.add(new GateTotal(gateName, a, totalUsage));
-	}
-	
-	/**
-	 * Returns the usage count for a particular Gate.
-	 * @param gateName the Gate name
-	 * @return the usage count, or zero if unknown
-	 */
-	public int getTotalUsage(String gateName) {
-		return _usage.stream().filter(gt -> gt.getGateName().equals(gateName)).mapToInt(GateTotal::getTotal).sum();
-	}
-	
-	/**
-	 * Returns the total usage across all gates.
-	 * @return the total usage count
-	 */
-	public int getTotal() {
-		return _usage.stream().mapToInt(GateTotal::getTotal).sum();
-	}
-	
-	/**
 	 * Deep clones the object.
-	 * @return a cloned GateUsage
+	 * @return a cloned RunwayGateUsage
 	 */
 	@Override
 	public GateUsage clone() {
-		GateUsage gu = new GateUsage(this, _isDeparture);
-		_usage.stream().map(gt -> new GateTotal(gt.getGateName(), gt.getAirline(), gt.getTotal())).forEach(gu._usage::add);
-		return gu;
+		GateUsage rgu = new GateUsage(this, getIsDeparture());
+		_usage.stream().map(gt -> new RunwayGateTotal(gt.getName(), gt.getAirline(), gt.getTotal())).forEach(rgu._usage::add);
+		return rgu;
 	}
 	
 	/**
-	 * Creates a GateUsage object for a single Airline.
+	 * Creates a RunwayGateUsage object for a single Airline.
 	 * @param a the Airline
-	 * @return a new GateUsage bean with statistics for that Airline
+	 * @return a new RunwayGateUsage bean with statistics for that Airline
 	 */
 	public GateUsage filter(Airline a) {
-		GateUsage gu = new GateUsage(this, _isDeparture);
-		_usage.stream().filter(gt -> a.equals(gt.getAirline())).forEach(gu._usage::add);
-		return gu;
-	}
-	
-	public void clear() {
-		_usage.clear();
-	}
-	
-	@Override
-	public Object cacheKey() {
-		return toString();
+		GateUsage rgu = new GateUsage(this, getIsDeparture());
+		_usage.stream().filter(gt -> a.equals(gt.getAirline())).forEach(rgu._usage::add);
+		return rgu;
 	}
 	
 	@Override
 	public String toString() {
-		return String.format("RT-%1s-%2s-%3b", (_aD == null) ? "null" : _aD.getICAO(), (_aA == null) ? "null" : _aA.getICAO(), Boolean.valueOf(_isDeparture));
-	}
-
-	@Override
-	public int hashCode() {
-		return toString().hashCode();
+		return String.format("GateUse-%1s-%2s-%3b", (getAirportD() == null) ? "null" : getAirportD().getICAO(), (getAirportA() == null) ? "null" : getAirportA().getICAO(), Boolean.valueOf(getIsDeparture()));
 	}
 }
