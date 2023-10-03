eda14f551633aaaefc6c5ca36a85e3c66396d940
Luke Kolin <luke@deltava.org>
2023-10-03 01:47:08 +0000

    Templating cleanup
    git-svn-id: file:///home/svn/DVA2006@19255 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/discord/EmbedGenerator.java
+++ src/java/org/deltava/discord/EmbedGenerator.java
@@ -66,6 +66,17 @@ class EmbedGenerator {
                 .setTimestampToNow();
     }
     
+    static EmbedBuilder createInsufficientAccess(MessageCreateEvent e) {
+    	String code = SystemData.get("airline.code");
+    	return new EmbedBuilder().setColor(Color.RED)
+    			.setFooter("Insufficient Access")
+    			.setTitle(":octagonal-sign: Access Denied")
+    			.addInlineField("User", e.getMessageAuthor().getDisplayName())
+    			.setDescription(String.format("You do not have the necessary access to join the %s Discord server", code))
+    			.addInlineField("User", e.getMessageAuthor().getDisplayName())
+    			.setTimestampToNow();
+    }
+    
     /**
 	 * Generates an embedded temporary nickname message.
 	 * @param e the MessageCreateEvent
@@ -77,7 +88,7 @@ class EmbedGenerator {
     	return new EmbedBuilder().setColor(Color.BLUE)
                 .setFooter("Temporary Nickname Assignment")
                 .setTitle(":exclamation: Temporary Nickname Assigned")
-                .setDescription(Bot.findRole("administrator").getMentionTag() + " I've assigned a temporary nickname to the following staff member.")
+                .setDescription(Bot.findRole("administrator").getMentionTag() + " I've assigned a temporary nickname to the following member.")
                 .addInlineField("Name", e.getMessageAuthor().getDisplayName())
                 .addInlineField("Permissions Level", roleName)
                 .setTimestampToNow();
@@ -91,12 +102,13 @@ class EmbedGenerator {
 	 * @return an EmbedBuilder
 	 */
     static EmbedBuilder createNicknameError(MessageCreateEvent e, Pilot p, String roleName) {
+    	String code = SystemData.get("airline.code");
     	return new EmbedBuilder()
         .setColor(Color.RED)
         .setTitle("Unable to Assign Nickname")
         .setFooter("User Creation Error")
         .setTimestampToNow()
-        .setDescription(Bot.findRole("administrator").getMentionTag() + " I ran into an error when attempting to assign a nickname to the following user. Please have an administrator update the user's nickname by hand in accordance with DVA policy.")
+        .setDescription(String.format("%s I ran into an error when attempting to assign a nickname to the following user. Please have an administrator update the user's nickname by hand in accordance with %s policy.", Bot.findRole("administrator").getMentionTag(), code))
         .addInlineField("User", e.getMessageAuthor().getDiscriminatedName())
         .addInlineField("Name", p.getName())
         .addInlineField("Pilot ID", p.getPilotCode())
@@ -109,16 +121,18 @@ class EmbedGenerator {
 	 * @return an EmbedBuilder
 	 */
     static EmbedBuilder register(long id) {
+    	String code = SystemData.get("airline.code");
+    	String host = SystemData.get("airline.url");
         return new EmbedBuilder()
                 .setTitle(":wave: Welcome Aboard!")
                 .setDescription("Welcome to the Delta Virtual Airlines Discord Server. To gain access, you must be an active member of Delta Virtual Airlines at www.deltava.org. We are a non-profit organization dedicated to flight simulation and education and are not affiliated with the real Delta Air Lines in any way. To complete your discord registration with us, follow the steps below.")
-                .setThumbnail("https://www.deltava.org/img/favicon/v852/favicon-32x32.png")
-                .setImage("https://www.deltava.org/img/DeltaBanner_delta_2007.png")
-                .setFooter(String.format("%s Discord New Member Registration", SystemData.get("airline.code")))
+                .setThumbnail(String.format("https://%s/img/favicon/v852/favicon-32x32.png", host))
+                .setImage(String.format("https://%s/img/DeltaBanner_delta_2007.png", host))
+                .setFooter(String.format("%s Discord New Member Registration", code))
                 .setTimestampToNow()
                 .setColor(new Color(1, 0, 100))
-                .addField("Step 1: Link your Discord and DVA User Accounts", "To associate your discord account with your DVA pilot ID and receive access to the rest of the server, follow this personalized link and sign into your DVA account:\n\nhttps://www.deltava.org/discordreg.do?id=" + id)
+                .addField("Step 1: Link your Discord and DVA User Accounts", String.format("To associate your discord account with your %s pilot ID and receive access to the rest of the server, follow this personalized link and sign into your %s account:\n\nhttps://%s/discordreg.do?id=" + id, code, code, host))
                 .addField("Step 2: Request your Roles", "Return to the #" + ChannelName.WELCOME.getName() + " channel and send \"done\" when you've completed linking your accounts and your roles will be assigned.")
-                .addField("Didn't work?", "If you follow the above process and are still not able to gain access, open a help desk ticket here: https://www.deltava.org/helpdesk.do");
+                .addField("Didn't work?", String.format("If you follow the above process and are still not able to gain access, open a help desk ticket here: https://%s/helpdesk.do", host));
     }
 }
