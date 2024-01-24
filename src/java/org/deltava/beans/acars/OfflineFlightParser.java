63b6c2f17445d9bd8cd954c99e093bd5c9c949e8
Luke Kolin <luke@deltava.org>
2024-01-24 03:44:42 +0000

    Adjust taxi time logging
    git-svn-id: file:///home/svn/DVA2006@19683 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/acars/OfflineFlightParser.java
+++ src/java/org/deltava/beans/acars/OfflineFlightParser.java
@@ -1,4 +1,4 @@
-// Copyright 2009, 2010, 2011, 2012, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2009, 2010, 2011, 2012, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.acars;
 
 import java.util.*;
@@ -23,7 +23,7 @@ import org.deltava.util.system.SystemData;
 /**
  * A utility class to parse XML-format offline Flight Reports.
  * @author Luke
- * @version 11.1
+ * @version 11.2
  * @since 2.4
  */
 
@@ -319,6 +319,7 @@ public final class OfflineFlightParser {
 		afr.setBoardTime(StringUtils.parse(ie.getChildTextTrim("timeBoard"), 0));
 		afr.setDeboardTime(StringUtils.parse(ie.getChildTextTrim("timeDeboard"), 0));
 		afr.setOnlineTime(StringUtils.parse(ie.getChildText("timeOnline"), 0));
+		result.setTaxiTimes(StringUtils.parse(ie.getChildTextTrim("timeTaxiIn"), 0), StringUtils.parse(ie.getChildTextTrim("timeTaxiOut"), 0));
 		result.setFlightReport(afr);
 		return result;
 	}
