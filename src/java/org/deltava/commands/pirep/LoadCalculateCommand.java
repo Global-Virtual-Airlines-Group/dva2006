c88074ebf35f56038b9acf32b87496de22b8821b
Luke Kolin <luke@deltava.org>
2024-04-04 13:13:13 +0000

    Allow recalculation as date changes
    git-svn-id: file:///home/svn/DVA2006@19844 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/pirep/LoadCalculateCommand.java
+++ src/java/org/deltava/commands/pirep/LoadCalculateCommand.java
@@ -1,6 +1,7 @@
-// Copyright 2021, 2022 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2021, 2022, 2024 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands.pirep;
 
+import java.time.*;
 import java.sql.Connection;
 
 import org.deltava.beans.econ.*;
@@ -12,12 +13,13 @@ import org.deltava.dao.*;
 
 import org.deltava.security.command.PIREPAccessControl;
 
+import org.deltava.util.StringUtils;
 import org.deltava.util.system.SystemData;
 
 /**
  * A Web Site Command to calculate load factors on draft Flight Reports. 
  * @author Luke
- * @version 10.3
+ * @version 11.2
  * @since 10.0
  */
 
@@ -30,7 +32,6 @@ public class LoadCalculateCommand extends AbstractCommand {
 	 */
 	@Override
 	public void execute(CommandContext ctx) throws CommandException {
-
 		try {
 			Connection con = ctx.getConnection();
 			
@@ -53,6 +54,13 @@ public class LoadCalculateCommand extends AbstractCommand {
 					fr = dfr;
 			}
 			
+			// Check for prior calculated load factor
+			if (fr.getLoadFactor() > 0) {
+				ctx.setAttribute("oldLoadFactor", Double.valueOf(fr.getLoadFactor()), REQUEST);
+				fr.addStatusUpdate(ctx.getUser().getID(), HistoryType.UPDATE, "Updated pre-flight Load Factor");
+			} else
+				fr.addStatusUpdate(ctx.getUser().getID(), HistoryType.UPDATE, "Requested pre-flight Load Factor");
+			
 			// Get the aircraft data
 			GetAircraft acdao = new GetAircraft(con);
 			Aircraft a = acdao.get(fr.getEquipmentType());
@@ -68,7 +76,12 @@ public class LoadCalculateCommand extends AbstractCommand {
 			double loadFactor = lf.generate(fr.getDate());
 			fr.setPassengers((int) Math.round(opts.getSeats() * loadFactor));
 			fr.setLoadFactor(loadFactor);
-			fr.addStatusUpdate(ctx.getUser().getID(), HistoryType.UPDATE, "Requested pre-flight Load Factor");
+			
+			// If the date is in the past, move it forward to today
+			if ((fr instanceof DraftFlightReport dfr) && dfr.getTimeD().isBefore(ZonedDateTime.now())) {
+				fr.setDate(LocalDate.now().atTime(12, 0).toInstant(ZoneOffset.UTC));
+				fr.addStatusUpdate(0, HistoryType.SYSTEM, String.format("Adjusted draft flight date to %s", StringUtils.format(fr.getDate(), ctx.getUser().getDateFormat())));
+			}
 			
 			// Write the flight report
 			SetFlightReport frwdao = new SetFlightReport(con);
@@ -77,6 +90,7 @@ public class LoadCalculateCommand extends AbstractCommand {
 			// Save status attributes
 			ctx.setAttribute("pirep", fr, REQUEST);
 			ctx.setAttribute("calcLoadFactor", Boolean.TRUE, REQUEST);
+			ctx.setAttribute("targetLoad", Double.valueOf(lf.getTargetLoad(fr.getDate())), REQUEST);
 		} catch (DAOException de) {
 			throw new CommandException(de);
 		} finally {
