47071aa5b63cfdf957630ee09aa2c9d21265f8eb
Luke Kolin <luke@deltava.org>
2024-06-08 15:44:36 +0000

    Use log book cache for performance boost
    git-svn-id: file:///home/svn/DVA2006@19971 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/commands/AbstractTestHistoryCommand.java
+++ src/java/org/deltava/commands/AbstractTestHistoryCommand.java
@@ -1,4 +1,4 @@
-// Copyright 2005, 2006, 2007, 2009, 2010, 2012, 2016, 2017, 2021 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2005, 2006, 2007, 2009, 2010, 2012, 2016, 2017, 2021, 2024 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.commands;
 
 import java.util.*;
@@ -18,13 +18,14 @@ import org.deltava.util.system.SystemData;
  * A class to support Web Site Commands use a {@link TestingHistoryHelper} object to determine what
  * examinations/transfers a Pilot is eligible for.
  * @author Luke
- * @version 10.1
+ * @version 11.2
  * @since 1.0
  */
 
 public abstract class AbstractTestHistoryCommand extends AbstractCommand {
 	
 	private static final Cache<TestingHistoryHelper> _cache = CacheManager.get(TestingHistoryHelper.class, "TestingHistory");
+	private static final Cache<CacheableCollection<FlightReport>> _logCache = CacheManager.getCollection(FlightReport.class, "Logbook");
 
 	/**
 	 * Populates the Testing History Helper by calling the proper DAOs in the right order.
@@ -39,12 +40,19 @@ public abstract class AbstractTestHistoryCommand extends AbstractCommand {
 		TestingHistoryHelper helper = _cache.get(p.cacheKey());
 		if (helper != null)
 			return helper;
-
-		// Load the PIREP beans
+		
+		// Load the Log book
 		String db = SystemData.get("airline.db");
-		GetFlightReports frdao = new GetFlightReports(c);
-		Collection<FlightReport> pireps = frdao.getByPilot(p.getID(), new LogbookSearchCriteria(null, db));
-		frdao.loadCaptEQTypes(p.getID(), pireps, db);
+		CacheableCollection<FlightReport> pireps = _logCache.get(p.cacheKey());
+		if (pireps == null) {
+			GetFlightReports frdao = new GetFlightReports(c);
+			Collection<FlightReport> data = frdao.getByPilot(p.getID(), new LogbookSearchCriteria(null, db));
+			frdao.loadCaptEQTypes(p.getID(), data, db);
+			
+			// Add to cache
+			pireps = new CacheableList<FlightReport>(p.cacheKey(), data);
+			_logCache.add(pireps);
+		}
 
 		// Get the Pilot's equipment program and all equipment types
 		GetEquipmentType eqdao = new GetEquipmentType(c);
