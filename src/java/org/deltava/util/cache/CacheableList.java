587ac86476dfb03b0797fff815883814da6a80db
Luke Kolin <luke@deltava.org>
2024-06-08 15:15:27 +0000

    Add convenience copy constructor from regular Collection
    git-svn-id: file:///home/svn/DVA2006@19967 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/util/cache/CacheableList.java
+++ src/java/org/deltava/util/cache/CacheableList.java
@@ -1,12 +1,12 @@
-// Copyright 2007, 2010, 2012, 2016 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2007, 2010, 2012, 2016, 2024 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.util.cache;
 
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * A utility class to create a cacheable List.
  * @author Luke
- * @version 7.2
+ * @version 11.2
  * @since 1.1
  * @see CacheableSet
  * @param <E> the cacheable object type
@@ -17,12 +17,22 @@ public class CacheableList<E> extends ArrayList<E> implements CacheableCollectio
 	private final Object _key;
 	
 	/**
-	 * Initializes the cachable list.
+	 * Initializes the cachable List.
 	 * @param key the cache key
 	 */
 	public CacheableList(Object key) {
 		super();
 		_key = key;
+	}
+	
+	/**
+	 * Converts a Collection into a cacheable List.
+	 * @param key the cache key
+	 * @param c the Collection
+	 */
+	public CacheableList(Object key, Collection<E> c) {
+		this(key);
+		addAll(c);
 	}
 	
 	/**
