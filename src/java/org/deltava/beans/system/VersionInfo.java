f28b6c155c1ef3d7e27d72a28abf709264607954
Luke Kolin <luke@deltava.org>
2024-11-21 20:31:43 +0000

    Build 927
    git-svn-id: file:///home/svn/DVA2006@20450 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -4,17 +4,17 @@ package org.deltava.beans.system;
 /**
  * A class to store version info constants.
  * @author Luke
- * @version 11.3
+ * @version 11.4
  * @since 1.0
  */
 
 public class VersionInfo {
 
     public static final int MAJOR = 11;
-    public static final int MINOR = 3;
+    public static final int MINOR = 4;
     
-    private static final int BUILD = 926;
-    private static final int HOTFIX = 3;
+    private static final int BUILD = 927;
+    private static final int HOTFIX = 0;
     
     public static final String TXT_COPYRIGHT = "Copyright 2004 - 2024 Global Virtual Airlines Group. All Rights Reserved.";
     public static final String HTML_COPYRIGHT = "Copyright &copy; 2004 - 2024 <a rel=\"external\" class=\"small\" href=\"https://www.gvagroup.org/\">Global Virtual Airlines Group</a>. All Rights Reserved.";
