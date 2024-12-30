89878df9029bd95846d33bd34d609bd94949c49b
Luke Kolin <luke@deltava.org>
2024-12-30 14:38:10 +0000

    Add IntervalFormatTag
    git-svn-id: file:///home/svn/DVA2006@20545 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/taglib/format/IntervalFormatTag.java
@@ -0,0 +1,76 @@
+// Copyright 2024 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.taglib.format;
+
+import java.util.List;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A JSP tag to display very short time intervals.
+ * @author Luke
+ * @version 11.4
+ * @since 11.4
+ * @see DurationFormatTag
+ */
+
+public class IntervalFormatTag extends DecimalFormatTag {
+	
+	private static final List<String> ABBRS = List.of("ns", "us", "ms", "s", "m", "h", "d");
+	
+	private Duration _d;
+	private TimeUnit _u = TimeUnit.SECONDS;
+	
+	/**
+	 * Sets the duration to render.
+	 * @param d the Duration
+	 */
+	public void setDuration(Duration d) {
+		_d = d;
+	}
+	
+	/**
+	 * Sets the time unit to display the interval with.
+	 * @param u the unit abbreviation
+	 */
+	public void setUnit(String u) {
+		if (u == null) return;
+		int idx = ABBRS.indexOf(u.toLowerCase());
+		if (idx != -1)
+			_u = TimeUnit.values()[idx];
+	}
+	
+	@Override
+	public void release() {
+		super.release();
+		_u = TimeUnit.SECONDS;
+	}
+	
+	/**
+     * Appends the unit abbreviation to the value.
+     */
+    @Override
+    protected void printValue() throws Exception {
+    	super.printValue();
+    	pageContext.getOut().print(ABBRS.get(_u.ordinal()));
+    }
+	
+    /**
+     * Checks that a non-null interval duration has been provided, and performs unit conversion.
+     * @return SKIP_BODY always
+     */
+	@Override
+	public int doStartTag() {
+		if (_d == null) _d = Duration.ZERO;
+		long v = _u.convert(_d);
+		if ((Math.abs(v) < 5) && (_u != TimeUnit.NANOSECONDS)) { // If we get a low number, move to the next unit down
+			TimeUnit u2 = TimeUnit.values()[_u.ordinal() - 1];
+			long v2 = u2.convert(_d);
+			double scale = _u.toNanos(1) / u2.toNanos(1);
+			setValue(Double.valueOf(v2 / scale));
+			setFmt((v == 0) ? "#0.000" : "#0.00");
+		} else
+			setValue(Long.valueOf(v));
+			
+		return SKIP_BODY;
+	}
+}
