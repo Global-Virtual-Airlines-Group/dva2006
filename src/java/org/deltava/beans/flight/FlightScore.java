ca5d87eb9ef583999aca3e19b7dff977be6328cb
Luke Kolin <luke@deltava.org>
2024-12-30 20:49:13 +0000

    Adjust colors
    git-svn-id: file:///home/svn/DVA2006@20558 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/flight/FlightScore.java
+++ src/java/org/deltava/beans/flight/FlightScore.java
@@ -1,4 +1,4 @@
-// Copyright 2012, 2017, 2020, 2023 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2012, 2017, 2020, 2023, 2024 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.flight;
 
 import static java.awt.Color.*;
@@ -8,12 +8,12 @@ import org.deltava.beans.*;
 /**
  * An enumeration of Flight Report scores. 
  * @author Luke
- * @version 11.1
+ * @version 11.4
  * @since 5.1
  */
 
 public enum FlightScore implements EnumDescription, RGBColor {
-	OPTIMAL(GREEN.getRGB()), ACCEPTABLE(ORANGE.getRGB()), DANGEROUS(RED.getRGB()), INCOMPLETE(GRAY.getRGB());
+	OPTIMAL(GREEN.darker().getRGB()), ACCEPTABLE(ORANGE.darker().getRGB()), DANGEROUS(RED.getRGB()), INCOMPLETE(GRAY.getRGB());
 	
 	private final int _color;
 
