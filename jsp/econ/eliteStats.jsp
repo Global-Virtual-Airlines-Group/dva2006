9b9234ed6bc630c3b7cf99915cdb8238f3e2e236
Luke Kolin <luke@deltava.org>
2025-01-14 14:34:38 +0000

    Display status properly in rollover period
    git-svn-id: file:///home/svn/DVA2006@20629 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- jsp/econ/eliteStats.jsp
+++ jsp/econ/eliteStats.jsp
@@ -83,7 +83,7 @@ golgotha.local.showChart = function() {
 	if (golgotha.local.chartData) return false;
 	const xmlreq = new XMLHttpRequest();
 	xmlreq.timeout = 7500;
-	xmlreq.open('get', 'elitestats.ws', true);
+	xmlreq.open('get', 'elitestats.ws?year=${statsYear}', true);
 	xmlreq.onreadystatechange = function() {
 		if ((xmlreq.readyState != 4) || (xmlreq.status != 200)) return false;
 		const js = JSON.parse(xmlreq.responseText);
--- src/java/org/deltava/commands/econ/EliteStatsCommand.java
+++ src/java/org/deltava/commands/econ/EliteStatsCommand.java
@@ -17,7 +17,7 @@ import org.deltava.util.system.SystemData;
 /**
  * A Web Site Command to view statistics about the Elite program.
  * @author Luke
- * @version 11.4
+ * @version 11.5
  * @since 9.2
  */
 
@@ -31,42 +31,47 @@ public class EliteStatsCommand extends AbstractCommand {
 	@Override
 	public void execute(CommandContext ctx) throws CommandException {
 		
-		int year = EliteScorer.getStatusYear(Instant.now());
+		final int year = EliteScorer.getStatusYear(Instant.now());
 		LocalDate startDate = LocalDate.of(year, 1, 1);
 		try {
 			Connection con = ctx.getConnection();
 			
+			// Check for completed rollover period
+			GetEliteStatistics elsdao = new GetEliteStatistics(con);
+			boolean doNextYear = EliteScorer.isRollover() && elsdao.isRolloverComplete(year + 1);
+			ctx.setAttribute("statsYear", Integer.valueOf(year + (doNextYear ? 1 : 0)), REQUEST);
+			
 			// Load current levels
 			GetElite eldao = new GetElite(con);
 			SortedSet<EliteLevel> lvls = eldao.getLevels(year);
-			lvls.remove(lvls.first()); // remove dummy "Member"
+			lvls.removeFirst(); // remove dummy "Member"
 			ctx.setAttribute("cyLevels", lvls, REQUEST);
 			
-			// Load stats
-			GetEliteStatistics elsdao = new GetEliteStatistics(con);
+			// Check if we're in the rollover period
+			boolean isRollover = EliteScorer.isRollover();
+			ctx.setAttribute("isRollover", Boolean.valueOf(isRollover), REQUEST);
+			
+			// Load elite stats
 			List<YearlyTotal> eTotals = elsdao.getPilotTotals(startDate);
 			FlightPercentileHelper eHelper = new FlightPercentileHelper(eTotals, 1);
 			ctx.setAttribute("elpse", eHelper.getLegs(), REQUEST);
 			ctx.setAttribute("edpse", eHelper.getDistance(), REQUEST);
 			ctx.setAttribute("eppse", eHelper.getPoints(), REQUEST);
+
+			// Load flight stats from one year back
+			GetFlightReportStatistics frsdao = new GetFlightReportStatistics(con);
+			LocalDate sd = isRollover ? startDate : LocalDate.now().minusMonths(12).minusDays(LocalDate.now().getDayOfMonth() - 1);
+			List<YearlyTotal> fTotals = frsdao.getPilotTotals(sd);
+			FlightPercentileHelper fHelper = new FlightPercentileHelper(fTotals, 1);
 			
 			// Get prediction of next year
 			TreeSet<EliteLevel> nyLevels = eldao.getLevels(year + 1);
 			if (nyLevels.isEmpty()) {
-				GetFlightReportStatistics frsdao = new GetFlightReportStatistics(con);
-				List<YearlyTotal> fTotals = frsdao.getPilotTotals(startDate);
-				FlightPercentileHelper fHelper = new FlightPercentileHelper(fTotals, 1);
-				
-				// Check if we're in the rollover period
-				boolean isRollover = EliteScorer.isRollover();
-				ctx.setAttribute("isRollover", Boolean.valueOf(isRollover), REQUEST);
-
-				// Go one year back
-				LocalDate sd = isRollover ? startDate : LocalDate.now().minusMonths(12).minusDays(LocalDate.now().getDayOfMonth() - 1);
 				ctx.setAttribute("estimateStart", sd, REQUEST);
 				ctx.setAttribute("estimateEnd", sd.plusYears(1), REQUEST);
 				ctx.setAttribute("estimatedLevels", Boolean.TRUE, REQUEST);
 
+				// Map to levels/perenctiles
 				PercentileStatsEntry lpse = fHelper.getLegs(); PercentileStatsEntry dpse = fHelper.getDistance(); PercentileStatsEntry ppse = fHelper.getPoints();
 				for (EliteLevel ol : lvls) {
 					EliteLevel nl = new EliteLevel(ol.getYear() + 1, ol.getName(), ctx.getDB());
@@ -79,14 +84,15 @@ public class EliteStatsCommand extends AbstractCommand {
 					nl.setPoints(EliteLevel.round(ppse.getPoints(nl.getTargetPercentile()), SystemData.getInt("econ.elite.round.points", 5000)));
 					nyLevels.add(nl);
 				}
-				
-				// Map percentiles to targets
-				ctx.setAttribute("targetLvls", CollectionUtils.createMap(nyLevels, EliteLevel::getTargetPercentile), REQUEST);
-				
-				// Save percentiles
-				ctx.setAttribute("flpse", fHelper.getLegs(), REQUEST);
-				ctx.setAttribute("fdpse", fHelper.getDistance(), REQUEST);
-			}
+			} else
+				nyLevels.removeFirst(); // remove dummy "Member"
+			
+			// Save flight percentiles
+			ctx.setAttribute("flpse", fHelper.getLegs(), REQUEST);
+			ctx.setAttribute("fdpse", fHelper.getDistance(), REQUEST);
+			
+			// Map percentiles to targets
+			ctx.setAttribute("targetLvls", CollectionUtils.createMap(nyLevels, EliteLevel::getTargetPercentile), REQUEST);
 			
 			ctx.setAttribute("nyLevels", nyLevels, REQUEST);
 			ctx.setAttribute("currentYear", Integer.valueOf(year), REQUEST);
