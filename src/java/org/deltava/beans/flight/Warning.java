0bf9e48d13ecd3edae59a6e3634b4609c6e7ee0f
Luke Kolin <luke@deltava.org>
2025-03-01 17:04:22 +0000

    Allow warning invocation count
    git-svn-id: file:///home/svn/DVA2006@20759 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/flight/Warning.java
+++ src/java/org/deltava/beans/flight/Warning.java
@@ -1,45 +1,53 @@
-// Copyright 2017, 2018, 2020 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2017, 2018, 2020, 2025 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.flight;
 
 /**
  * An enumeration to list flight condition warnings.
  * @author Luke
- * @version 9.1
+ * @version 11.5
  * @since 8.0
  */
 
-public enum Warning {
+public enum Warning implements org.deltava.beans.EnumDescription {
 	
-	OVER250K("250 under 10K"), DESCENTRATE("Descent Rate"), BANK("Bank Angle"), PITCH("Pitch Angle"), GFORCE("G-Force"), TAXISPEED("Taxi Speed"), 
-	NOFUEL("No Fuel", FlightScore.DANGEROUS), OVERSPEED("Overspeed", FlightScore.DANGEROUS), STALL("Stall", FlightScore.DANGEROUS), 
-	AIRSPACE("Airspace", FlightScore.DANGEROUS), ALTITUDE("Altitude"), GEARSPEED("Gear Extension", FlightScore.DANGEROUS), 
-	GEARUP("Gear Up"), CRASH("Crash", FlightScore.DANGEROUS), ENGOUT("Engine Out", FlightScore.DANGEROUS);
+	OVER250K("250 under 10K", FlightScore.ACCEPTABLE, 3), DESCENTRATE("Descent Rate"), BANK("Bank Angle"), PITCH("Pitch Angle"), GFORCE("G-Force"),	TAXISPEED("Taxi Speed"), 
+	NOFUEL("No Fuel", FlightScore.DANGEROUS), OVERSPEED("Overspeed", FlightScore.DANGEROUS, 2), STALL("Stall", FlightScore.DANGEROUS), AIRSPACE("Airspace", FlightScore.DANGEROUS),
+	ALTITUDE("Altitude"), GEARSPEED("Gear Extension", FlightScore.DANGEROUS), GEARUP("Gear Up"), CRASH("Crash", FlightScore.DANGEROUS), ENGOUT("Engine Out", FlightScore.DANGEROUS);
 
 	private final String _desc;
 	private final FlightScore _score;
+	private final int _minCount;
 
 	/**
 	 * Creates the warning with an Acceptable score.
-	 * @param description the warning text
+	 * @param desc the warning text
 	 */
-	Warning(String description) {
-		this(description, FlightScore.ACCEPTABLE);
+	Warning(String desc) {
+		this(desc, FlightScore.ACCEPTABLE, 1);
 	}
 	
 	/**
-	 * Creates a warning.
-	 * @param description the warning text
+	 * Creates a warning with a specific score.
+	 * @param desc the warning text
 	 * @param score the FlightScore maximum
 	 */
-	Warning(String description, FlightScore score) {
-		_desc = description;
-		_score = score;
+	Warning(String desc, FlightScore score) {
+		this(desc, score, 1);
 	}
 	
 	/**
-	 * Returns a description of this warning condition.
-	 * @return the Description
+	 * Creates a warning wiht a specific score and invocation count.
+	 * @param desc the warning text
+	 * @param score the FlightScore maximum
+	 * @param minCount the minimum instance count for this warning to adjust the flight score
 	 */
+	Warning(String desc, FlightScore score, int minCount) {
+		_desc = desc;
+		_score = score;
+		_minCount = minCount;
+	}
+	
+	@Override
 	public String getDescription() {
 		return _desc;
 	}
@@ -50,5 +58,13 @@ public enum Warning {
 	 */
 	public FlightScore getScore() {
 		return _score;
+	}
+	
+	/**
+	 * Returns the minimum instances of this warning to adjust the Flight Score.
+	 * @return the minimum instance count
+	 */
+	public int getMinCount() {
+		return _minCount;
 	}
 }
