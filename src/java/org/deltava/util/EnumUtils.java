0243557a6e044babf314e8fe58ba5cd3edec8125
Luke Kolin <luke@deltava.org>
2025-03-01 16:59:33 +0000

    Add max() and test case
    git-svn-id: file:///home/svn/DVA2006@20758 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/util/EnumUtils.java
+++ src/java/org/deltava/util/EnumUtils.java
@@ -1,4 +1,4 @@
-// Copyright 2020 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2020, 2025 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.util;
 
 import java.lang.reflect.Method;
@@ -6,7 +6,7 @@ import java.lang.reflect.Method;
 /**
  * A utility class for enumeration operations.
  * @author Luke
- * @version 9.0
+ * @version 11.5
  * @since 9.0
  */
 
@@ -34,5 +34,15 @@ public class EnumUtils {
 		} catch (Exception e) {
 			return defaultValue;
 		}
+	}
+	
+	/**
+	 * Returns the higher of two enumerations. 
+	 * @param e1 the first enum
+	 * @param e2 the second enum
+	 * @return the larger enum based on ordinal
+	 */
+	public static <E extends Enum<E>> E max(E e1, E e2) {
+		return (e1.ordinal() > e2.ordinal()) ? e1 : e2;
 	}
 }
