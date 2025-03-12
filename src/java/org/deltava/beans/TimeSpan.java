d8a4119f46dbcc04ab703f718f375da22850d219
Luke Kolin <luke@deltava.org>
2025-03-12 13:42:00 +0000

    Add hasTimes()
    git-svn-id: file:///home/svn/DVA2006@20790 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/TimeSpan.java
+++ src/java/org/deltava/beans/TimeSpan.java
@@ -1,4 +1,4 @@
-// Copyright 2010, 2016, 2021 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2010, 2016, 2021, 2025 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans;
 
 import java.time.*;
@@ -6,7 +6,7 @@ import java.time.*;
 /**
  * An interface to describe beans with a start and an end time. 
  * @author Luke
- * @version 10.0
+ * @version 11.6
  * @since 3.1
  */
 
@@ -28,7 +28,15 @@ public interface TimeSpan extends CalendarEntry {
 	 * The duration of this span, or null if both times are not set.
 	 * @return a Duration, or null
 	 */
-	default Duration getDuration() {
+	public default Duration getDuration() {
 		return ((getStartTime() == null) || (getEndTime() == null)) ? null : Duration.between(getStartTime(), getEndTime());
+	}
+	
+	/**
+	 * Returns whether the start and end times are populated.
+	 * @return TRUE if both are populated, otherwise FALSE
+	 */
+	public default boolean hasTimes() {
+		return (getStartTime() != null) && (getEndTime() != null);
 	}
 }
