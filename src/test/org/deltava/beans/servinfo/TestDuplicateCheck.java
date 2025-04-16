8ae72692d254788047a7b88394afc2a5df18a9be
Luke Kolin <luke@deltava.org>
2025-04-16 08:38:04 +0000

    Remove unneeded warning
    git-svn-id: file:///home/svn/DVA2006@20891 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/deltava/beans/servinfo/TestDuplicateCheck.java
+++ src/test/org/deltava/beans/servinfo/TestDuplicateCheck.java
@@ -10,7 +10,6 @@ import junit.framework.TestCase;
 @SuppressWarnings("static-method")
 public class TestDuplicateCheck extends TestCase {
 
-	@SuppressWarnings("unlikely-arg-type")
 	public void testCIDCheck() {
 		
 		Controller c = new Controller(12345, OnlineNetwork.VATSIM);
