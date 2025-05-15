f297285150e957f0e219e0841eb9d3431536ca28
Luke Kolin <luke@deltava.org>
2025-05-15 13:02:28 +0000

    Add CAPTCHA detection method
    git-svn-id: file:///home/svn/DVA2006@21036 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/BrowserInfoTag.java
+++ src/java/org/deltava/taglib/BrowserInfoTag.java
@@ -1,16 +1,18 @@
-// Copyright 2011 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2011, 2026 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib;
+
+import static javax.servlet.jsp.PageContext.SESSION_SCOPE;
+import static org.deltava.commands.HTTPContext.*; 
 
 import javax.servlet.jsp.tagext.TagSupport;
 
+import org.deltava.beans.system.CAPTCHAResult;
 import org.deltava.beans.system.HTTPContextData;
-
-import org.deltava.commands.HTTPContext;
 
 /**
  * A JSP tag that operates differently based on browser type. 
  * @author Luke
- * @version 3.7
+ * @version 11.6
  * @since 3.7
  * @see org.deltava.servlet.filter.BrowserTypeFilter
  */
@@ -22,6 +24,15 @@ public abstract class BrowserInfoTag extends TagSupport {
 	 * @return an HTTPContextData bean, or none if null
 	 */
 	protected HTTPContextData getBrowserContext() {
-		return (HTTPContextData) pageContext.getRequest().getAttribute(HTTPContext.HTTPCTXT_ATTR_NAME);
+		return (HTTPContextData) pageContext.getRequest().getAttribute(HTTPCTXT_ATTR_NAME);
+	}
+	
+	/**
+	 * Returns whether the user has passed CAPTCHA validation.
+	 * @return TRUE if the user has passed the CAPTCHA, otherwise FALSE
+	 */
+	protected boolean passedCAPTCHA() {
+		CAPTCHAResult cr = (CAPTCHAResult) pageContext.getAttribute(CAPTCHA_ATTR_NAME, SESSION_SCOPE);
+		return (cr != null) && cr.getIsSuccess();
 	}
 }
