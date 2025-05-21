5e403c7c59a6a877d9fc0029da979b10916eddeb
Luke Kolin <luke@deltava.org>
2025-05-21 11:50:05 +0000

    Convert to MapBox
    git-svn-id: file:///home/svn/DVA2006@21055 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/acarsFlightMap.js
+++ static/js/acarsFlightMap.js
@@ -8,7 +8,7 @@ f.showFDR.disabled = true; f.showRoute.disabled = true; f.showAirspace.disabled 
 golgotha.util.disable(f.rwyDebug, true);
 
 // Fetch the data
-const p = fetch('acars_pirep.ws?id=' + pirepID + '&showAirspace=' + showAirspace, {signal:AbortSignal.timeout(2500)});
+const p = fetch('acars_pirep.ws?id=' + pirepID + '&showAirspace=' + showAirspace, {signal:AbortSignal.timeout(5500)});
 p.then(function(rsp) {
 	const errE = document.getElementById('archiveError');
 	if (rsp.status != 200) {
@@ -25,12 +25,12 @@ p.then(function(rsp) {
 			golgotha.util.display('archiveOK', true);
 
 		js.positions.forEach(function(p) {
-			var mrk;
-			golgotha.maps.acarsFlight.routePoints.push(p.ll);
+			let mrk; const ll = golgotha.maps.toLL(p.ll);
+			golgotha.maps.acarsFlight.routePoints.push(ll);
 			if (p.icon)
-				mrk = new golgotha.maps.IconMarker({pal:p.pal, icon:p.icon, info:p.info, opacity:0.75}, p.ll);
+				mrk = new golgotha.maps.IconMarker({pal:p.pal, icon:p.icon, info:p.info, opacity:0.75, pt:ll});
 			else if (p.color)
-				mrk = new golgotha.maps.Marker({color:p.color, info:p.info, opacity:0.75}, p.ll);
+				mrk = new golgotha.maps.Marker({color:p.color, info:p.info, opacity:0.75, scale:0.5, pt:ll});
 			else
 				return false;
 
@@ -39,13 +39,13 @@ p.then(function(rsp) {
 			if (p.atc) {
 				if ((p.atc.type != 'CTR') && (p.atc.type != 'FSS')) {
 					mrk.range = p.atc.range; mrk.atcPosition = p.atc.ll;
-					google.maps.event.addListener(mrk, 'click', function() { golgotha.maps.acarsFlight.showAPP(this.atcPosition, this.range); });
+					mrk.on('click', function() { golgotha.maps.acarsFlight.showAPP(this.atcPosition, this.range); });
 				} else {
 					mrk.atcID = p.atc.id;
-					google.maps.event.addListener(mrk, 'click', function() { golgotha.maps.acarsFlight.showFIR(this.atcID); });
+					mrk.on('click', function() { golgotha.maps.acarsFlight.showFIR(this.atcID); });
 				}
 			} else
-				google.maps.event.addListener(mrk, 'click', golgotha.maps.acarsFlight.hideATC);
+				mrk.on('click', golgotha.maps.acarsFlight.hideATC);
 		});
 
 		js.airspace.forEach(function(a) {
@@ -54,7 +54,9 @@ p.then(function(rsp) {
 			golgotha.maps.acarsFlight.airspace.push(a);
 		});
 
-		golgotha.maps.acarsFlight.gRoute = new google.maps.Polyline({path:golgotha.maps.acarsFlight.routePoints, strokeColor:'#4080af', strokeWeight:3, strokeOpacity:0.85, geodesic:true, zIndex:golgotha.maps.z.POLYLINE});
+		// Create the line, but don't show it
+		golgotha.maps.acarsFlight.gRoute = new golgotha.maps.Line('flightPath', {color:'#4080af', width:3, opacity:0.85, visible:false}, golgotha.maps.acarsFlight.routePoints);
+		map.addLine(golgotha.maps.acarsFlight.gRoute);
 		golgotha.event.beacon('ACARS', 'Flight Data');
 
 		if (f.rwyDebug) {
@@ -83,7 +85,7 @@ return true;
 
 golgotha.maps.acarsFlight.hideATC = function() {
 	while (golgotha.maps.acarsFlight.selectedFIRs.length > 0) {
-		let mrk = golgotha.maps.acarsFlight.selectedFIRs.shift();
+		const mrk = golgotha.maps.acarsFlight.selectedFIRs.shift();
 		mrk.setMap(null);
 	}
 };
@@ -95,13 +97,20 @@ golgotha.maps.acarsFlight.showAPP = function(ctr, range) {
 };
 
 golgotha.maps.acarsFlight.addAirspace = function(as) {
-	as.poly = new google.maps.Polygon({map:map, paths:[as.border], strokeColor:as.c.c, strokeWeight:1, strokeOpacity:as.c.tx, fillColor:'#802020', fillOpacity:0.2, zIndex:golgotha.maps.z.POLYGON+as.c.z});
-	as.poly.info = as.info; as.poly.ll = as.ll;
-	google.maps.event.addListener(as.poly, 'click', function() { map.infoWindow.setContent(this.info); map.infoWindow.open(map); map.infoWindow.setPosition(this.ll); });
+	as.poly = new golgotha.maps.Polygon(as.id, {color:as.c.c, width:1, opacity:as.c.tx, fillColor:'#802020', fillOpacity:0.2, info:as.info}, as.border);
+	map.addLine(as.poly);
+	if (as.info) {
+		map.on('click', as.poly.name, function(e) { 
+			const p = new mapboxgl.Popup({closeOnClick:true,focusAfterOpen:false,maxWidth:'300px'});
+			p.setHTML(e.features[0].properties.info).setLngLat(e.lngLat).addTo(map);
+		});
+	}
 };
 
 golgotha.maps.acarsFlight.removeAirspace = function(as) {
-	as.poly.setMap(null);
+	if (!as.poly) return false;
+	map.removeLayer(as.poly.name);
+	map.removeSource(as.poly.name);
 	delete as.poly;
 };
 
@@ -113,12 +122,12 @@ golgotha.maps.acarsFlight.showRunway = function(rd, pd) {
 	const rw = rd.threshold || rd.location;
 	const dst = golgotha.maps.distance(rd.pt, pd);
 	golgotha.maps.acarsFlight.debugMarkers.push(new google.maps.Polyline({map:map, path:[rw,rd.pt], strokeWeight:7.5, strokeColor:'#0000a1', strokeOpacity:0.25, zIndex:golgotha.maps.z.POLYLINE+1}));
-	golgotha.maps.acarsFlight.debugMarkers.push(new google.maps.Circle({map:map, center:rd.pt, radius:golgotha.maps.feet2Meter(Math.abs(rd.distance)), strokeColor:'#0000a1', strokeOpacity:0.5, strokeWeight:1, fillColor:'#0000a1', fillOpacity:0.2, zIndex:golgotha.maps.z.POLYGON}));
-	golgotha.maps.acarsFlight.debugMarkers.push(new golgotha.maps.IconMarker({map:map, pal:3, icon:38, opacity:0.5, info:'Actual Takeoff/Touchdown'}, pd));
+	//golgotha.maps.acarsFlight.debugMarkers.push(new google.maps.Circle({map:map, center:rd.pt, radius:golgotha.maps.feet2Meter(Math.abs(rd.distance)), strokeColor:'#0000a1', strokeOpacity:0.5, strokeWeight:1, fillColor:'#0000a1', fillOpacity:0.2, zIndex:golgotha.maps.z.POLYGON}));
+	golgotha.maps.acarsFlight.debugMarkers.push(new golgotha.maps.IconMarker({map:map, pal:3, icon:38, opacity:0.5, info:'Actual Takeoff/Touchdown', pt:pd}));
 	if (dst > 15)
 		golgotha.maps.acarsFlight.debugMarkers.push(new golgotha.maps.IconMarker({map:map, pal:3, icon:53, opacity:0.5, info:'Runway Takeoff/Touchdown, distance=' + Math.round(dst) + ' meters'}, rd.pt));
-	if (rd.threshold)
-		golgotha.maps.acarsFlight.debugMarkers.push(new google.maps.Circle({map:map, center:rd.threshold, radius:golgotha.maps.feet2Meter(Math.abs(rd.distance)), strokeColor:'#a000a1', strokeOpacity:0.5, strokeWeight:1, fillColor:'#a000a1', fillOpacity:0.2, zIndex:golgotha.maps.z.POLYGON}));
+	/* if (rd.threshold)
+		golgotha.maps.acarsFlight.debugMarkers.push(new google.maps.Circle({map:map, center:rd.threshold, radius:golgotha.maps.feet2Meter(Math.abs(rd.distance)), strokeColor:'#a000a1', strokeOpacity:0.5, strokeWeight:1, fillColor:'#a000a1', fillOpacity:0.2, zIndex:golgotha.maps.z.POLYGON})); */
 };
 
 golgotha.maps.acarsFlight.toggleDebug = function(isEnabled) {
@@ -126,8 +135,7 @@ golgotha.maps.acarsFlight.toggleDebug = function(isEnabled) {
 	localStorage.setItem('golgotha.rwyDebug', isEnabled);
 };
 
-golgotha.maps.distance = function (ll1, ll2) {
-	const l1 = new google.maps.LatLng(ll1); const l2 = new google.maps.LatLng(ll2); // this allows us to take LatLngLiterals
+golgotha.maps.distance = function (l1, l2) {
 	const R = 3958.8; // Radius of the Earth in miles
 	const rlat1 = l1.lat() * (Math.PI/180); // Convert to radians
 	const rlat2 = l2.lat() * (Math.PI/180); // Convert to radians
@@ -137,23 +145,20 @@ golgotha.maps.distance = function (ll1, ll2) {
 	return golgotha.maps.miles2Meter(d);
 };
 
-golgotha.maps.acarsFlight.showFIR = function(code)
-{
-const xmlreq = new XMLHttpRequest();
-xmlreq.open('get', 'fir.ws?id=' + code, true);
-xmlreq.onreadystatechange = function() {
-	if ((xmlreq.readyState != 4) || (xmlreq.status != 200)) return false;
-	const js = JSON.parse(xmlreq.responseText);
-	golgotha.maps.acarsFlight.hideATC();
-	js.firs.forEach(function(fe) {
-		if (fe.border.length == 0) return false;
-		golgotha.maps.acarsFlight.selectedFIRs.push(new google.maps.Polygon({map:map, paths:[fe.border], strokeColor:'#efefff', strokeWeight:1, strokeOpacity:0.85, fillColor:'#7f7f80', fillOpacity:0.25, zIndex:golgotha.maps.z.POLYGON}));
+golgotha.maps.acarsFlight.showFIR = function(code) {
+	const p = fetch('fir.ws?id=' + code);
+	p.then(function(rsp) {
+		if (rsp.status != 200) return false;
+		rsp.json().then(function(js) {
+			golgotha.maps.acarsFlight.hideATC();	
+			js.firs.forEach(function(fe) {
+				if (fe.border.length == 0) return false;
+				const p = new golgotha.maps.Polygon(fe.id, {color:'#efefff', width:1, opacity:0.5, fillColor:'#7f7f80', fillOpacity:0.25}, fe.border);
+				map.addLine(p);
+				golgotha.maps.acarsFlight.selectedFIRs.push(p);
+			});		
+
+			golgotha.event.beacon('ACARS', 'Show FIR', code);			
+		});
 	});
-
-	golgotha.event.beacon('ACARS', 'Show FIR', code);
-	return true;
-};
-
-xmlreq.send(null);
-return true;
 };
