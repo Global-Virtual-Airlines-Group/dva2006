f5214a219ecc707ec676cee7e4970425c4dd0563
Luke Kolin <luke@deltava.org>
2025-05-25 13:03:51 +0000

    Move to MapBox
    git-svn-id: file:///home/svn/DVA2006@21086 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/routeMap.js
+++ static/js/routeMap.js
@@ -2,11 +2,12 @@ golgotha.routeMap = golgotha.routeMap || {routes:[], airports:[], aps:[]};
 golgotha.routeMap.updateAirports = function(combo)
 {
 golgotha.util.setHTML('isLoading', ' - CLEARING...');
+const showInfoCB = document.getElementById('showInfo');
+const showInfo = ((showInfoCB) && (showInfoCB.checked));
 
 // Remove airports/routes from the map
 map.removeMarkers(golgotha.routeMap.aps);
 map.removeMarkers(golgotha.routeMap.routes);
-map.closeWindow();
 if (!golgotha.form.comboSet(combo)) {
 	golgotha.util.setHTML('isLoading', '');
 	return false;
@@ -14,6 +15,7 @@ if (!golgotha.form.comboSet(combo)) {
 
 // Check if we have cached airports
 const aCode = golgotha.form.getCombo(combo);
+
 golgotha.routeMap.aps = golgotha.routeMap.airports[aCode];
 if (golgotha.routeMap.aps) {
 	map.addMarkers(golgotha.routeMap.aps);
@@ -28,16 +30,13 @@ p.then(function(rsp) {
 	if (!rsp.ok) return false;
 	golgotha.util.setHTML('isLoading', ' - REDRAWING...');
 	golgotha.routeMap.aps = [];
-
-	// Parse the JSON
 	rsp.json().then(function(js) {
 		js.airports.forEach(function(a) {
-			const mrk = new golgotha.maps.Marker({color:a.color}, a.ll);
+			const mrk = new golgotha.maps.Marker({color:a.color, info:(showInfo ? a.info : null), pt:a.ll});
 			mrk.icao = a.icao; mrk.iata = a.iata;
 			mrk.infoShow = golgotha.routeMap.showRoutes;
-			mrk.infoLabel = a.info;
-			google.maps.event.addListener(mrk, 'click', mrk.infoShow);
-			golgotha.routeMap.aps.push(mrk);	
+			mrk.getElement().addEventListener('click', mrk.infoShow);
+			golgotha.routeMap.aps.push(mrk);
 		});
 
 		// Save in the hashmap
@@ -51,44 +50,32 @@ p.then(function(rsp) {
 return true;
 }
 
-golgotha.routeMap.showRoutes = function()
-{
-// Update status
-golgotha.util.setHTML('isLoading', ' - LOADING...');
+golgotha.routeMap.showRoutes = function() {
+	golgotha.util.setHTML('isLoading', ' - LOADING...');
 
-// Get the airline code
-const aCombo = document.getElementById('airlineCode');
-const aCode = aCombo.options[aCombo.selectedIndex].value;
-map.removeMarkers(golgotha.routeMap.routes);
-map.closeWindow();
+	// Get the airline code
+	const mrk = this.marker;
+	const aCode = golgotha.form.getCombo(document.getElementById('airlineCode'));
+	map.removeMarkers(golgotha.routeMap.routes);
 
-// Fetch the data
-const p = fetch('rmap_routes.ws?icao=' + this.icao + '&airline=' + aCode, {signal:AbortSignal.timeout(5000)});
-p.then(function(rsp) {
-	if (!rsp.ok) return false;
-	golgotha.util.setHTML('isLoading', ' - REDRAWING...');
-	golgotha.routeMap.routes.length = 0;
-	
-	// Parse the JSON
-	rsp.json().then(function(js) {
-		js.routes.forEach(function(rt) {
-			if (rt.airline != aCode) return false;
-			const routeLine = new google.maps.Polyline({map:map, path:rt.positions, strokeColor:rt.color, strokeWeight:2, strokeOpacity:0.8, geodesic:true, zIndex:golgotha.maps.z.POLYLINE});
-			golgotha.routeMap.routes.push(routeLine);
-		});	
+	// Fetch the data
+	const p = fetch('rmap_routes.ws?icao=' + mrk.icao + '&airline=' + aCode, {signal:AbortSignal.timeout(5000)});
+	p.then(function(rsp) {
+		if (!rsp.ok) return false;
+		golgotha.util.setHTML('isLoading', ' - REDRAWING...');
+		golgotha.routeMap.routes.length = 0;
+		rsp.json().then(function(js) {
+			js.routes.forEach(function(rt) {
+				if (rt.airline != aCode) return false;
+				const routeLine = new golgotha.maps.Line(rt.from + '-' + rt.to, {color:rt.color, width:2, opacity:0.625}, rt.positions);
+				map.addLine(routeLine);
+				golgotha.routeMap.routes.push(routeLine);
+			});	
 		
-		// Focus on the map
-		golgotha.util.setHTML('isLoading', '');
-		golgotha.event.beacon('Route Map', 'Routes', this.icao);
-		return true;
+			// Focus on the map
+			golgotha.util.setHTML('isLoading', '');
+			golgotha.event.beacon('Route Map', 'Routes', this.icao);
+			return true;
+		});
 	});
-});
-
-const showInfo = document.getElementById('showInfo');
-if ((showInfo) && (showInfo.checked)) {
-	map.infoWindow.setContent(this.infoLabel);
-	map.infoWindow.open(map, this);
-}
-
-return true;
 };
