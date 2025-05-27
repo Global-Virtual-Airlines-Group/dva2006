41261d20c1f8f9866b9fd42095c86ad12c4388aa
Luke Kolin <luke@deltava.org>
2025-05-27 13:53:47 +0000

    Allow manual minification override
    git-svn-id: file:///home/svn/DVA2006@21104 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/taglib/content/InsertMinifiedContentTag.java
+++ src/java/org/deltava/taglib/content/InsertMinifiedContentTag.java
@@ -1,4 +1,4 @@
-// Copyright 2012, 2022 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2012, 2022, 2025 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.taglib.content;
 
 import org.deltava.util.system.SystemData;
@@ -6,7 +6,7 @@ import org.deltava.util.system.SystemData;
 /**
  * A JSP tag to insert optionally minified content. Content will be minified if the SystemData environment name starts with &quot;prod&quot;.
  * @author Luke
- * @version 10.2
+ * @version 12.0
  * @since 5.0
  */
 
@@ -14,6 +14,7 @@ abstract class InsertMinifiedContentTag extends InsertContentTag {
 
 	private String _actualResourceName;
 	private boolean _isMin;
+	private Boolean _override;
 	
 	/**
 	 * Returns the actual file name of the requested resource.
@@ -31,14 +32,30 @@ abstract class InsertMinifiedContentTag extends InsertContentTag {
 		return _isMin;
 	}
 	
+	/**
+	 * Manually overrides the minification state of this resource. 
+	 * @param override TRUE to force minification, FALSE to prevent
+	 */
+	public void setMinify(boolean override) {
+		_override = Boolean.valueOf(override);
+	}
+	
+	@Override
+	public void release() {
+		super.release();
+		_override = null;
+	}
+	
 	@Override
 	public int doStartTag() {
 		String env = SystemData.get(SystemData.CFG_NAME);
 		_isMin = ((env != null) && env.startsWith("prod"));
 		
-		// Detect minification override
+		// Detect minification overrides
 		boolean minOV = Boolean.parseBoolean(pageContext.getRequest().getParameter("minOV"));
 		if (minOV) _isMin = !_isMin;
+		if (_override != null)
+			_isMin = _override.booleanValue();
 		
 		// If running in prod, minify - otherwise 
 		StringBuilder buf = new StringBuilder(_resourceName);
