1d0e8083b0b988ee7938d838c21ca4022555090e
Luke Kolin <luke@deltava.org>
2025-05-29 17:41:29 +0000

    Add ContentSecurityFilter
    git-svn-id: file:///home/svn/DVA2006@21124 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/deltava/beans/system/ContentSecurity.java
@@ -0,0 +1,13 @@
+// Copyright 2025 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.beans.system;
+
+/**
+ * An enumeration of Content Security Policy types.
+ * @author Luke
+ * @version 12.0
+ * @since 12.0
+ */
+
+public enum ContentSecurity {
+	DEFAULT, CONNECT, STYLE, IMG, SCRIPT, FRAME, FONT, WORKER;
+}
