9865dfdefe42404b93fb666ab2873687bb72f27f
Luke Kolin <luke@deltava.org>
2025-05-30 12:38:07 +0000

    Add reporting URI support
    git-svn-id: file:///home/svn/DVA2006@21138 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/deltava/beans/system/ContentSecurityPolicy.java
+++ src/java/org/deltava/beans/system/ContentSecurityPolicy.java
@@ -11,9 +11,17 @@ import java.util.*;
  */
 
 public class ContentSecurityPolicy {
+	
+	/**
+	 * Self-referential constant.
+	 */
+	public static final String SELF = "'self'";
 
 	private final boolean _enforce;
 	private final Map<ContentSecurity, Collection<String>> _data = new LinkedHashMap<ContentSecurity, Collection<String>>();
+	
+	private String _reportGroup;
+	private String _reportURI;
 
 	/**
 	 * Creates the bean and populates default values.
@@ -22,18 +30,18 @@ public class ContentSecurityPolicy {
 	public ContentSecurityPolicy(boolean enforce) {
 		super();
 		_enforce = enforce;
-		add(ContentSecurity.DEFAULT, "'self'");
-		add(ContentSecurity.CONNECT, "'self'");
-		add(ContentSecurity.SCRIPT, "'self'");
+		add(ContentSecurity.DEFAULT, SELF);
+		add(ContentSecurity.CONNECT, SELF);
+		add(ContentSecurity.SCRIPT, SELF);
 		add(ContentSecurity.SCRIPT, "'unsafe-inline'");
-		add(ContentSecurity.STYLE, "'self'");
+		add(ContentSecurity.STYLE, SELF);
 		add(ContentSecurity.STYLE, "'unsafe-inline'");
-		add(ContentSecurity.IMG, "'self'");
-		add(ContentSecurity.WORKER, "'self'");
+		add(ContentSecurity.IMG, SELF);
+		add(ContentSecurity.WORKER, SELF);
 	}
 	
 	/**
-	 * Adds an entry to this Security policy.
+	 * Adds an entry to this Security Policy.
 	 * @param cs a ContentSecurity type
 	 * @param host a permitted host
 	 */
@@ -41,6 +49,24 @@ public class ContentSecurityPolicy {
 		Collection<String> hosts = _data.getOrDefault(cs, new LinkedHashSet<String>());
 		hosts.add(host);
 		_data.put(cs, hosts);
+	}
+	
+	/**
+	 * Updates a Reporting API endpoint for this Security Policy.
+	 * @param group the group name
+	 * @param url the endpoint URL
+	 */
+	public void setReportURI(String group, String url) {
+		_reportGroup = group;
+		_reportURI = url;
+	}
+	
+	/**
+	 * Returns whether a Reporting API endpoint has been defined for this Security Policy.
+	 * @return TRUE if a URI has been defined, otherwise FALSE
+	 */
+	public boolean hasReportURI() {
+		return (_reportGroup != null);
 	}
 	
 	/**
@@ -52,11 +78,26 @@ public class ContentSecurityPolicy {
 	}
 	
 	/**
-	 * Generates the value to place into the security policy HTTP header.
-	 * @return a Header value
+	 * Generates the value to place into Reporting API header.
+	 * @return the Header value
+	 */
+	public String getReportHeader() {
+		StringBuilder buf = new StringBuilder();
+		if (_reportGroup != null) {
+			buf.append(_reportGroup);
+			buf.append("=\"");
+			buf.append(_reportURI);
+			buf.append('\"');
+		}
+		
+		return buf.toString();
+	}
+	
+	/**
+	 * Generates the value to place into the Security Plicy HTTP header.
+	 * @return the Header value
 	 */
 	public String getData() {
-		
 		StringBuilder buf = new StringBuilder(64);
 		for (Map.Entry<ContentSecurity, Collection<String>> me : _data.entrySet()) {
 			buf.append(me.getKey().name().toLowerCase());
@@ -66,6 +107,12 @@ public class ContentSecurityPolicy {
 				buf.append(host);
 			}
 			
+			buf.append("; ");
+		}
+		
+		if (_reportURI != null) {
+			buf.append("report-to ");
+			buf.append(_reportGroup);
 			buf.append("; ");
 		}
 		
