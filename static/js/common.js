9783f2215903307bbb1811c8b41233a5444c2aba
Luke Kolin <luke@deltava.org>
2025-06-06 13:55:04 +0000

    Fire GA4 events properly
    git-svn-id: file:///home/svn/DVA2006@21235 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/common.js
+++ static/js/common.js
@@ -2,7 +2,7 @@ var golgotha = {event:{},util:{},form:{isSubmitted:false,invalidDomains:[]},loca
 golgotha.nav.touch = ("ontouchend" in document);
 golgotha.util.getTimestamp = function(ms) { var d = new Date(); return d.getTime() - (d.getTime() % ms); };
 golgotha.util.darkMode = false; // (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
-golgotha.event.beacon = function() { if (gtag) gtag(arguments); };
+golgotha.event.beacon = function() { if (gtag) gtag('event', arguments); };
 golgotha.event.stop = function(e) { if (e) { e.stopPropagation(); e.preventDefault(); } return false; };
 golgotha.event.Error = function(msg, showAlert) { var e = new Error(msg); e.showAlert = showAlert; return e; };
 golgotha.event.ValidationError = function(msg, el) { var e = new golgotha.event.Error(msg, true); e.focusElement = el; return e; };
