611e9ce01c9472b8c48976aeca9ce3cf5caa7ed3
Luke Kolin <luke@deltava.org>
2025-07-03 13:48:27 -0400

    Make generic collection more specific

--- src/java/org/deltava/beans/schedule/ScheduleSearchCriteria.java
+++ src/java/org/deltava/beans/schedule/ScheduleSearchCriteria.java
@@ -12,7 +12,7 @@ import org.deltava.util.*;
 /**
  * A bean to store search criteria for the Flight Schedule.
  * @author Luke
- * @version 11.4
+ * @version 12.0
  * @since 1.0
  */
 
@@ -20,9 +20,9 @@ public class ScheduleSearchCriteria extends Flight {
 
 	private static final String[] SORT_NAMES = {"Random", "Flight Number", "Equipment Type", "Origin", "Destination", "Departure Time", "Arrival Time", "Length", "Distance", "Flight Count", "Last Flown"};
 	public static final String[] SORT_CODES = {"RAND()", "FLIGHT", "EQTYPE", "AIRPORT_D", "AIRPORT_A", "TIME_D", "TIME_A", "FLIGHT_TIME", "DISTANCE", "FCNT", "LF"};
-	public static final List<?> SORT_OPTIONS = ComboUtils.fromArray(SORT_NAMES, SORT_CODES);
+	public static final List<ComboAlias> SORT_OPTIONS = ComboUtils.fromArray(SORT_NAMES, SORT_CODES);
 
-	public static final List<?> HOURS = ComboUtils.fromArray(new String[] { "-", "Midnight", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "Noon", "1 PM", "2 PM", "3 PM", "4 PM",
+	public static final List<ComboAlias> HOURS = ComboUtils.fromArray(new String[] { "-", "Midnight", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "Noon", "1 PM", "2 PM", "3 PM", "4 PM",
 			"5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM" }, new String[] { "-1", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23" });
 
 	private int _distance;
