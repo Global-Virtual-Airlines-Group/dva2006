c1f0aca6eb6d30ac6d52c9214181bd0d0a955318
Luke Kolin <luke@deltava.org>
2025-09-18 06:28:48 -0400

    Update NAT URL

--- src/test/org/deltava/dao/http/TestGetOceanic.java
+++ src/test/org/deltava/dao/http/TestGetOceanic.java
@@ -18,7 +18,7 @@ public class TestGetOceanic extends TestCase {
 
 	@SuppressWarnings("static-method")
 	public void testNAT() throws DAOException {
-		GetNATs dao = new GetNATs("https://www.notams.faa.gov/common/nat.html");
+		GetNATs dao = new GetNATs("https://notams.aim.faa.gov/nat.html");
 		String info = dao.getTrackInfo();
 		assertNotNull(info);
 		assertTrue(info.length() > 0);
