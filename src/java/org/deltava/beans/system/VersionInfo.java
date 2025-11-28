ceeb48b233dd684bbaa0b5219f83438c2ef04820
Luke Kolin <luke@deltava.org>
2025-11-28 18:00:29 -0500

    Build 975

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -13,7 +13,7 @@ public class VersionInfo {
     public static final int MAJOR = 12;
     public static final int MINOR = 4;
     
-    public static final int BUILD = 974;
+    public static final int BUILD = 975;
     private static final int HOTFIX = 0;
     
     public static final String TXT_COPYRIGHT = "Copyright 2004 - 2025 Global Virtual Airlines Group. All Rights Reserved.";
