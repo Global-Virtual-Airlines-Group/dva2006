d0cc81b2c2479678f2bd4df5010bfee5eb987f70
Luke Kolin <luke@deltava.org>
2025-12-30 07:24:02 -0500

    Add entries to unit test

--- src/test/org/deltava/dao/http/TestGetSimBrief.java
+++ src/test/org/deltava/dao/http/TestGetSimBrief.java
@@ -1,27 +1,48 @@
 package org.deltava.dao.http;
 
+import junit.framework.TestCase;
+
 import org.deltava.beans.simbrief.SimBriefParser;
 
-import junit.framework.TestCase;
+import org.deltava.dao.http.GetSimBrief.SimBriefException; 
 
 public class TestGetSimBrief extends TestCase {
 
 	private static final String USER = "248733";
 	
-	@SuppressWarnings("static-method")
-	public void testRefreshError() throws Exception {
-		
-		GetSimBrief sbdao = new GetSimBrief();
+	private GetSimBrief sbdao;
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		sbdao = new GetSimBrief();
 		sbdao.setConnectTimeout(2500);
 		sbdao.setReadTimeout(4500);
-		sbdao.setCompression(Compression.BROTLI, Compression.GZIP);
+		sbdao.setCompression(Compression.GZIP);
 		sbdao.setReturnErrorStream(true);
-		
+	}
+	
+	public void testLatestPlan() throws Exception {
+		String xml = sbdao.refresh("LukeKolin", null);
+		assertNotNull(xml);
+	}
+	
+	public void testInvalidURL() throws Exception {
+		try {
+			sbdao.refresh("5619", null);
+		} catch (SimBriefException sbe) {
+			assertNotNull(sbe.getMessage());
+			String errorMsg = SimBriefParser.parseError(sbe.getMessage());
+			assertNotNull(errorMsg);
+		}
+	}
+	
+	public void testRefreshError() throws Exception {
 		try {
 			sbdao.refresh(USER, "0x19f4cd");
 			fail("SimBriefException expected");
-		} catch (HTTPDAOException hde) {
-			assertTrue(hde instanceof GetSimBrief.SimBriefException);
+		} catch (SimBriefException hde) {
+			assertNotNull(hde.getMessage());
 			String errorMsg = SimBriefParser.parseError(hde.getMessage());
 			assertNotNull(errorMsg);
 		}
