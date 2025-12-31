1745211ed9cf97c45c5cbd7d811c756f32fe5747
Luke Kolin <luke@deltava.org>
2025-12-31 07:58:44 -0500

    Add fileExists property

--- src/java/org/deltava/beans/acars/ArchiveValidationException.java
+++ src/java/org/deltava/beans/acars/ArchiveValidationException.java
@@ -1,21 +1,25 @@
-// Copyright 2024 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2024, 2025 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.beans.acars;
 
 /**
  * An exception thrown when validating an ACARS position archive. This is an unchecked exception to allow selective handling.
  * @author Luke
- * @version 11.2
+ * @version 12.4
  * @since 11.2
  */
 
 public class ArchiveValidationException extends RuntimeException {
+	
+	private final boolean _fileExists;
 
 	/**
 	 * Creates the exception.
 	 * @param msg the exception message
+	 * @param fileExists TRUE if the archive file exists, otherwise FALSE
 	 */
-	public ArchiveValidationException(String msg) {
+	public ArchiveValidationException(String msg, boolean fileExists) {
 		super(msg);
+		_fileExists = fileExists;
 	}
 
 	/**
@@ -24,5 +28,14 @@ public class ArchiveValidationException extends RuntimeException {
 	 */
 	public ArchiveValidationException(Exception e) {
 		super(e);
+		_fileExists = true;
+	}
+	
+	/**
+	 * Returns whether the position archive file exists on disk.
+	 * @return TRUE if the file exists, otherwise FALSE
+	 */
+	public boolean getFileExists() {
+		return _fileExists;
 	}
 }
