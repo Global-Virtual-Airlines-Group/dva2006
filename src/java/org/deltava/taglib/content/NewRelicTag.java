3de0aa8a9d2b6569250a420449b6ad4716583e99
Luke Kolin <luke@deltava.org>
2026-01-17 09:32:43 -0500

    NewRelic Instrumentation tag

---
+++ src/java/org/deltava/taglib/content/NewRelicTag.java
@@ -0,0 +1,43 @@
+// Copyright 2026 Global Virtual Airlines Group. All Rights Reserved.
+package org.deltava.taglib.content;
+
+import jakarta.servlet.jsp.JspException;
+
+import com.newrelic.api.agent.NewRelic;
+
+import org.deltava.beans.system.ContentSecurity;
+import org.deltava.taglib.CSPNonceTag;
+import org.deltava.taglib.ContentHelper;
+import org.deltava.util.StringUtils;
+
+/**
+ * A JSP tag to inject NewRelic browser instrumentation with a CSP nonce.
+ * @author Luke
+ * @version 12.4
+ * @since 12.4
+ */
+
+public class NewRelicTag extends CSPNonceTag {
+
+	@Override
+	public int doEndTag() throws JspException {
+
+		// Get the data with the nonce
+		String nonce = getNonce();
+		String src = (nonce == null) ? NewRelic.getBrowserTimingHeader() : NewRelic.getBrowserTimingHeader(nonce);
+		if (StringUtils.isEmpty(src))
+			return EVAL_BODY_INCLUDE;
+		
+		try {
+			pageContext.getOut().println(src);
+		} catch (Exception e) {
+			throw new JspException(e);
+		}
+		
+		// Update CSP policy
+		ContentHelper.addCSP(pageContext, ContentSecurity.SCRIPT, "js-agent.newrelic.com");
+		ContentHelper.addCSP(pageContext, ContentSecurity.CONNECT, "bam.nr-data.net");
+		ContentHelper.addCSP(pageContext, ContentSecurity.CONNECT, "bam-cell.nr-data.net"); 
+		return EVAL_BODY_INCLUDE;
+	}
+}
