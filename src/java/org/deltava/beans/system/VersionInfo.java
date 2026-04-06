9383f1200d7d93b7f02a3a12d4a23efb044fad46
Luke Kolin <luke@deltava.org>
2026-04-06 06:29:19 -0400

    Golgotha 12.5

--- src/java/org/deltava/beans/system/VersionInfo.java
+++ src/java/org/deltava/beans/system/VersionInfo.java
@@ -4,17 +4,17 @@ package org.deltava.beans.system;
 /**
  * A class to store version info constants.
  * @author Luke
- * @version 12.4
+ * @version 12.5
  * @since 1.0
  */
 
 public class VersionInfo {
 
     public static final int MAJOR = 12;
-    public static final int MINOR = 4;
+    public static final int MINOR = 5;
     
-    public static final int BUILD = 990;
-    private static final int HOTFIX = 3;
+    public static final int BUILD = 991;
+    private static final int HOTFIX = 0;
     
     public static final String TXT_COPYRIGHT = "Copyright 2004 - 2026 Global Virtual Airlines Group. All Rights Reserved.";
     public static final String HTML_COPYRIGHT = "Copyright &copy; 2004 - 2026 <a rel=\"external\" class=\"small\" href=\"https://www.gvagroup.org/\">Global Virtual Airlines Group</a>. All Rights Reserved.";
