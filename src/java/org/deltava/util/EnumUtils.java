94c2ce4e3d57a1de09849af7898f20354da7c061
Luke Kolin <luke@deltava.org>
2026-04-19 14:56:54 -0400

    Add iterator() utility method

--- src/java/org/deltava/util/EnumUtils.java
+++ src/java/org/deltava/util/EnumUtils.java
@@ -1,12 +1,13 @@
-// Copyright 2020, 2025 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2020, 2025, 2026 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.util;
 
+import java.util.*;
 import java.lang.reflect.Method;
 
 /**
  * A utility class for enumeration operations.
  * @author Luke
- * @version 11.5
+ * @version 12.4
  * @since 9.0
  */
 
@@ -44,5 +45,21 @@ public class EnumUtils {
 	 */
 	public static <E extends Enum<E>> E max(E e1, E e2) {
 		return (e1.ordinal() > e2.ordinal()) ? e1 : e2;
+	}
+
+	/**
+	 * Creates an Iterator of Enumeration values.
+	 * @param c the Enumeration class
+	 * @return an Iterator of Enumeration values
+	 */
+	@SuppressWarnings("unchecked")
+	public static <E extends Enum<E>> Iterator<E> iterator(Class<E> c) {
+		try {
+			Method m = c.getMethod("values");
+			E[] values = (E[]) m.invoke(null);
+			return List.of(values).iterator();
+		} catch (Exception e) {
+			return Collections.emptyIterator();
+		}
 	}
 }
