b3459f562a7ddae8953c37d1d3360a879291a5e2
Luke Kolin <luke@deltava.org>
2026-04-26 08:39:18 -0400

    Multi-threaded serialize to temp file

--- src/java/org/deltava/service/logbook/DataExportService.java
+++ src/java/org/deltava/service/logbook/DataExportService.java
@@ -5,24 +5,21 @@ import static jakarta.servlet.http.HttpServletResponse.*;
 
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.zip.*;
+import java.util.concurrent.*;
+
 import java.sql.Connection;
 import java.time.Instant;
 import java.nio.file.attribute.FileTime;
 
-import org.json.JSONObject;
 import org.apache.logging.log4j.*;
 
 import org.deltava.beans.Pilot;
 import org.deltava.beans.acars.*;
-import org.deltava.beans.acars.FlightInfo;
 import org.deltava.beans.flight.*;
 import org.deltava.beans.schedule.Aircraft;
-import org.deltava.beans.servinfo.PositionData;
 
 import org.deltava.dao.*;
-import org.deltava.dao.file.*;
 import org.deltava.service.*;
 import org.deltava.util.*;
 import org.deltava.util.cache.*;
@@ -34,15 +31,80 @@ import org.deltava.util.cache.*;
  * @since 12.4
  */
 
-public class DataExportService extends WebService {
+public class DataExportService extends DownloadService {
 	
 	private static final Logger log = LogManager.getLogger(DataExportService.class);
 	
 	private static final Cache<CacheableCollection<FlightReport>> _cache = CacheManager.getCollection(FlightReport.class, "Logbook");
 	
-	private record FlightData (FlightReport PIREP, Aircraft aircraft, SequencedCollection<RouteEntry> positions, String error) {}
-	private record FlightJS (int id, String js) {}
+	/*
+	 * This is a hack. Ordinarily we would do a map() on a queue, but stream iterators do not mutate their underlying stream and we will
+	 * OOM. Therefore, we submit work into a ThreadPoolExecutor which should allow the raw data to be garbage collected post serialization. 
+	 */
+	private class ExportWork implements Callable<Void> {
+		private final FlightData _fd;
+		private final Queue<FlightJS> _out;
 		
+		ExportWork(FlightData fd, Queue<FlightJS> out) {
+			super();
+			_fd = fd;
+			_out= out;
+		}
+
+		@Override
+		public Void call() throws Exception {
+			FlightJS js = DataSerializer.serialize(_fd);
+			_out.add(js);
+			return null;
+		}
+	}
+	
+	/**
+	 * Helper method to asynchronously write JSON data to a ZIP file. 
+	 */
+	private class ZIPWorker implements Runnable {
+		private final BlockingQueue<FlightJS> _work;
+		private File _f;
+		
+		ZIPWorker(BlockingQueue<FlightJS> work) {
+			super();
+			_work = work;
+		}
+		
+		@Override
+		public String toString() {
+			return "ZIPWorker";
+		}
+		
+		File getFile() {
+			return _f;
+		}
+		
+		@Override
+		public void run() {
+			try {
+				_f = File.createTempFile("dataExport", "zip");
+				try (OutputStream os = new BufferedOutputStream(new FileOutputStream(_f), 65536); ZipOutputStream zout = new ZipOutputStream(os)) {
+					while (!Thread.currentThread().isInterrupted()) {
+						FlightJS js = _work.take();
+						log.info("Wrote Flight {} to ZIP", Integer.valueOf(js.id()));
+						ZipEntry ze = new ZipEntry(String.valueOf(js.id()) + ".json");
+						ze.setMethod(ZipEntry.DEFLATED);
+						ze.setCreationTime(FileTime.from(Instant.now()));
+						zout.putNextEntry(ze);
+						PrintWriter pw = new PrintWriter(zout);
+						pw.print(js.js());
+						pw.flush();
+					}
+				}
+			} catch (IOException ie) {
+				log.atError().withThrowable(ie).log("Error writing ZIP file - {}", ie.getMessage());
+			} catch (InterruptedException ixe) {
+				log.info("ZIPWorker Interrupted");
+			}
+		}
+	}
+	
 	/**
 	 * Executes the Web Service.
 	 * @param ctx the Web Service context
@@ -57,16 +119,16 @@ public class DataExportService extends WebService {
 		if (ctx.isUserInRole("HR"))
 			userID = StringUtils.parse(ctx.getParameter("id"), userID);
 		
+		Pilot p = null;
+		Collection<FlightData> work = new ArrayList<FlightData>();
 		IntervalTaskTimer tt = new IntervalTaskTimer();
-		LogbookSearchCriteria lsc = new LogbookSearchCriteria("DATE, PR.SUBMITTED", ctx.getDB());
-		lsc.setLoadComments(true);
 		try {
 			Connection con = ctx.getConnection();
 			
 			// Load the Pilot and aircraft profiles
 			GetPilot pdao = new GetPilot(con);
 			GetAircraft acdao = new GetAircraft(con);
-			Pilot p = pdao.get(userID);
+			p = pdao.get(userID);
 			Map<String,Aircraft> acTypes = CollectionUtils.createMap(acdao.getAircraftTypes(), Aircraft::getName);
 			tt.mark("data");
 			
@@ -74,6 +136,9 @@ public class DataExportService extends WebService {
 			GetFlightReports frdao = new GetFlightReports(con);
 			CacheableCollection<FlightReport> pireps = _cache.get(Integer.valueOf(userID));
 			if (pireps == null) {
+				LogbookSearchCriteria lsc = new LogbookSearchCriteria("DATE, PR.SUBMITTED", ctx.getDB());
+				lsc.setLoadComments(true);
+				
 				pireps = new CacheableList<FlightReport>(Integer.valueOf(userID));
 				pireps.addAll(frdao.getByPilot(userID, lsc));
 				_cache.add(pireps);
@@ -85,11 +150,9 @@ public class DataExportService extends WebService {
 			tt.mark("logbook");
 			
 			// Load flight data
-			Collection<FlightData> work = new ArrayList<FlightData>();
 			GetACARSData fidao = new GetACARSData(con);
 			GetACARSPositions posdao = new GetACARSPositions(con);
 			for (FlightReport fr : pireps) {
-				Aircraft ac = acTypes.get(fr.getEquipmentType());
 				SequencedCollection<RouteEntry> rtData = new ArrayList<RouteEntry>();
 				String error = null;
 				
@@ -98,103 +161,57 @@ public class DataExportService extends WebService {
 					FlightInfo fi = fidao.getInfo(fr.getDatabaseID(DatabaseID.ACARS));
 					if (fi == null)
 						error = String.format("No ACARS Flight for Flight Report %d (ACARS ID = %d)", Integer.valueOf(fr.getID()), Integer.valueOf(fr.getDatabaseID(DatabaseID.ACARS)));
-					else 
+					else if (!fi.getArchived())
 						rtData.addAll(posdao.getRouteEntries(userID, fi.getArchived()));
 				}
-				
-				work.add(new FlightData(fr, ac, rtData, error));
+
+				Aircraft ac = acTypes.get(fr.getEquipmentType());
+				FlightData fd = new FlightData(fr, ac, rtData, error);
+				work.add(fd);
 			}
 			
-			// Serialize in a multi-threaded fashion
-			Collection<FlightJS> jsData = work.parallelStream().map(DataExportService::serialize).collect(Collectors.toList());
-			
-			// Write to ZIP file
-			tt.mark("serialize");
-			FileTime now = FileTime.from(Instant.now());
-			ByteArrayOutputStream bos = new ByteArrayOutputStream(262144);
-			try (ZipOutputStream zout = new ZipOutputStream(bos)) {
-				for (Iterator<FlightJS> i = jsData.iterator(); i.hasNext(); ) {
-					FlightJS js = i.next();
-					ZipEntry ze = new ZipEntry(String.valueOf(js.id()) + ".json");
-					ze.setMethod(ZipEntry.DEFLATED);
-					ze.setCreationTime(now);
-					zout.putNextEntry(ze);
-					PrintWriter pw = new PrintWriter(zout);
-					pw.print(js.js());
-					pw.flush();
-					i.remove();
-				}
-			}
-			
-			// Dump to the output stream
-			tt.stop();
-			log.error("Timings = {}", tt);
-			ctx.setHeader("Content-disposition", String.format("attachment; filename=FlightData_%s.zip", p.getPilotCode()));
-			ctx.setHeader("Content-Length", bos.size());
-			ctx.setContentType("application/zip");
-			ctx.setExpiry(1800);
-			try (OutputStream os = ctx.getResponse().getOutputStream()) {
-				os.write(bos.toByteArray());
-				os.flush();
-			}
-		} catch (IOException | DAOException de) {
+			tt.mark("positions");
+		} catch (DAOException de) {
 			throw error(SC_INTERNAL_SERVER_ERROR, de.getMessage(), de);
 		} finally {
 			ctx.release();
 		}
 		
+		// Abort if no flights
+		if (work.isEmpty()) return SC_NOT_FOUND;
+		
+		// Build the worker
+		BlockingQueue<FlightJS> outWork = new LinkedBlockingQueue<FlightJS>();
+		ZIPWorker zw = new ZIPWorker(outWork);
+		Thread zwt = new Thread(zw, zw.toString());
+		zwt.setDaemon(true);
+		
+		// Serialize in a multi-threaded fashion
+		int poolSize = Runtime.getRuntime().availableProcessors();
+		try (ThreadPoolExecutor exec = new ThreadPoolExecutor(poolSize, poolSize, 100, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>())) {
+			work.stream().map(fd -> new ExportWork(fd, outWork)).forEach(exec::submit);
+			tt.mark("submit");
+			work.clear();
+			zwt.start();
+			exec.shutdown();
+			exec.awaitTermination(150, TimeUnit.SECONDS);
+			zwt.interrupt();
+			tt.mark("serialize");
+		} catch (InterruptedException ie) {
+			log.atError().withThrowable(ie).log("Error executing ThreadPool - {}", ie.getMessage());
+		}
+		
+		// Dump to the output stream
+		tt.stop();
+		log.error("Timings = {}", tt);
+		File df = zw.getFile();
+		ctx.setHeader("Content-disposition", String.format("attachment; filename=FlightData_%s.zip", p.getPilotCode()));
+		ctx.setHeader("Content-Length", (int)df.length());
+		ctx.setContentType("application/zip");
+		//ctx.setExpiry(1800);
+		sendFile(df, ctx.getResponse());
+
 		return SC_OK;
-	}
-
-	/*
-	 * Helper method to serialize data into JSON.
-	 */
-	private static FlightJS serialize(FlightData fd) {
-
-		// Serialize the flight report and positions
-		JSONObject fo = new JSONObject();
-		fo.put("flight", JSONFlightExport.format(fd.aircraft(), fd.PIREP));
-		fo.putOpt("error", fd.error());
-		fd.positions().forEach(re -> fo.accumulate("data", JSONFlightExport.format(fd.PIREP.getFDR(), re)));
-		JSONUtils.ensureArrayPresent(fo, "data");
-
-		// Load serialized route data -- Move to outsade
-		File rf = ArchiveHelper.getRoute(fd.PIREP.getID());
-		if (!StringUtils.isEmpty(fd.PIREP.getRoute()) && rf.exists()) {
-			JSONObject ro = new JSONObject();
-			try (InputStream is = new BufferedInputStream(new FileInputStream(rf), 4096)) {
-				GetSerializedRoute rtdao = new GetSerializedRoute(is);
-				ArchivedRoute rt = rtdao.read();
-				ro.put("airac", rt.getAIRACVersion());
-				ro.put("text", rt.getRoute());
-				rt.getWaypoints().forEach(wp -> ro.accumulate("pts", JSONFlightExport.format(wp)));
-				JSONUtils.ensureArrayPresent(ro, "pts");
-			} catch (Exception e) {
-				ro.put("error", e.getMessage());
-			} finally {
-				fo.put("route", ro);
-			}
-		}
-
-		// Load serialized online positions
-		File of = ArchiveHelper.getOnline(fd.PIREP.getID());
-		if (of.exists()) {
-			JSONObject oo = new JSONObject();
-			try (InputStream is = new BufferedInputStream(new FileInputStream(of), 4096)) {
-				GetSerializedOnline otdao = new GetSerializedOnline(is);
-				List<PositionData> pts = otdao.read();
-				oo.put("network", fd.PIREP.getNetwork());
-				pts.forEach(pd -> oo.accumulate("pts", JSONFlightExport.format(pd)));
-				JSONUtils.ensureArrayPresent(oo, "pts");
-			} catch (Exception e) {
-				oo.put("error", e.getMessage());
-			} finally {
-				fo.put("onlineTrack", oo);
-			}
-		}
-		
-		
-		return new FlightJS(fd.PIREP.getID(), fo.toString(2));
 	}
 
 	@Override
