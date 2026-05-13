ecdbfa17c3c51aea946d11420fefc373368c07d5
Luke Kolin <luke@deltava.org>
2026-05-13 10:26:52 -0400

    Handle corrupted data without blowing out heap

--- src/java/org/deltava/dao/file/GetSerializedOnline.java
+++ src/java/org/deltava/dao/file/GetSerializedOnline.java
@@ -1,9 +1,11 @@
-// Copyright 2016, 2023 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2016, 2023, 2026 Global Virtual Airlines Group. All Rights Reserved.
 package org.deltava.dao.file;
 
 import java.io.*;
 import java.util.*;
 import java.time.Instant;
+
+import org.apache.logging.log4j.*;
 
 import org.deltava.beans.schedule.GeoPosition;
 import org.deltava.beans.servinfo.PositionData;
@@ -13,11 +15,13 @@ import org.deltava.dao.DAOException;
 /**
  * A Data Access Object to read serialized online track data.
  * @author Luke
- * @version 11.1
+ * @version 12.4
  * @since 7.0
  */
 
 public class GetSerializedOnline extends DAO {
+	
+	private static final Logger log = LogManager.getLogger(GetSerializedOnline.class);
 
 	/**
 	 * Initializes the Data Access Object.
@@ -40,8 +44,13 @@ public class GetSerializedOnline extends DAO {
 				return Collections.emptyList();	
 				
 			int size = in.readInt();
+			if (size > 1024) {
+				log.warn("Possibly corrupt file - couunt={}", Integer.valueOf(size));
+				size = 1024;
+			}
+			
 			List<PositionData> results = new ArrayList<PositionData>(size + 2);
-			for (int x = 0; x < size; x++) {
+			for (int x = 0; (x < size) && (in.available() > 24); x++) {
 				PositionData pd = new PositionData(Instant.ofEpochMilli(in.readLong()), new GeoPosition(in.readDouble(), in.readDouble(), in.readInt()));
 				pd.setAirSpeed(in.readInt());
 				pd.setHeading(in.readShort());
